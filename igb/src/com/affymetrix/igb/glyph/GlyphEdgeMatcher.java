6526de2fc056aa5c058d44265c55532a175728ec
chyekk <chyekk@mycompany.com>
2005-03-19 00:18:43 +0000

    Allowing this to use the more generic List, rather than requiring Vector.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@154 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/glyph/GlyphEdgeMatcher.java
+++ igb/src/com/affymetrix/igb/glyph/GlyphEdgeMatcher.java
@@ -13,8 +13,8 @@
 
 package com.affymetrix.igb.glyph;
 
-import java.awt.*;
-import java.util.*;
+import java.awt.Color;
+import java.util.List;
 
 import com.affymetrix.genoviz.bioviews.*;
 import com.affymetrix.genoviz.glyph.*;
@@ -66,7 +66,7 @@ public class GlyphEdgeMatcher  {
    *
    *  Glyphs added for matches are collected and returned in match_glyphs vector.
    */
-  public void matchEdges(NeoMapI map, Vector query_glyphs, Vector target_glyphs, Vector match_glyphs) {
+  public void matchEdges(NeoMapI map, List query_glyphs, List target_glyphs, List match_glyphs) {
     call_count = 0;
     start_match_count = 0;
     end_match_count = 0;
@@ -74,19 +74,19 @@ public class GlyphEdgeMatcher  {
     intersect_count = 0;
     //    System.out.println("called GlyphEdgeMatcher.matchEdges(), query_glyph count = " + query_glyphs.size());
     for (int i=0; i<query_glyphs.size(); i++) {
-      GlyphI query = (GlyphI)query_glyphs.elementAt(i);
+      GlyphI query = (GlyphI) query_glyphs.get(i);
       matchEdges(map, query, target_glyphs, match_glyphs);
     }
   }
 
-  public void matchEdges(NeoMapI map, GlyphI query, Vector target_glyphs, Vector match_glyphs) {
+  public void matchEdges(NeoMapI map, GlyphI query, List target_glyphs, List match_glyphs) {
     for (int k=0; k<target_glyphs.size(); k++) {
-      GlyphI target = (GlyphI)target_glyphs.elementAt(k);
+      GlyphI target = (GlyphI) target_glyphs.get(k);
       matchEdges(map, query, target, match_glyphs);
     }
   }
   
-  public void matchEdges(NeoMapI map, GlyphI query, GlyphI target, Vector match_glyphs) {
+  public void matchEdges(NeoMapI map, GlyphI query, GlyphI target, List match_glyphs) {
     //    System.out.println("trying to match edges: query = " + query + ", target = " + target);
     call_count++;
     

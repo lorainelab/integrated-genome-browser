d43a3a47cf3b6b8807e3f60365ca40d2ff4a4230
gregghelt2 <gregghelt2@mycompany.com>
2005-04-21 08:46:59 +0000

    Added getFeatures() method to do DAS2 feature queries.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@225 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das2/Das2Region.java
+++ igb/src/com/affymetrix/igb/das2/Das2Region.java
@@ -13,11 +13,17 @@
 
 package com.affymetrix.igb.das2;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.xml.sax.*;
+
 import com.affymetrix.genometry.*;
 import com.affymetrix.genometry.span.SimpleSeqSpan;
+import com.affymetrix.genometry.util.SeqUtils;
 import com.affymetrix.igb.genometry.SmartAnnotBioSeq;
 import com.affymetrix.igb.genometry.AnnotatedSeqGroup;
-
+import com.affymetrix.igb.parsers.Das2FeatureSaxParser;
 
 public class Das2Region {
   String region_id;
@@ -56,6 +62,7 @@ public class Das2Region {
     return segment_span;
   }
 
+  /** or should this return a SmartAnnotbioSeq??? */
   public MutableAnnotatedBioSeq getAnnotatedSeq() {
     if (aseq == null) {
       initRegion();
@@ -84,6 +91,76 @@ public class Das2Region {
     System.out.println("in initRegion() method, start = " + start + ", end = " + end);
     System.out.println("    seq = " + aseq.getID() + ", genome = " + genome.getID());
 
+  }
+
+  /**
+   *  Takes an uninitialized Das2FeatureRequestSym as an argument,
+   *    constructs a DAS2 query based on sym,
+   *    sends query to DAS2 server
+   *    hands returned data to a parser, get List of annotations back from parser
+   *    adds annotations as children to Das2FeatureRequestSym
+   *    (should it also add request_sym to SmartAnnotBioSeq (becomes child of container, or
+   *         should that be handled before/after getFeatures() is called?  leaning towards the latter...)
+   *    also at some point needs to figure out what format to request returned result in
+   *         (based on Das2Type of request_sym)
+   *
+   *  returns true if feature query returns successfully, false otherwise
+   *
+   */
+  public boolean getFeatures(Das2FeatureRequestSym request_sym) {
+    boolean success = false;
+
+    SeqSpan overlap_span = request_sym.getOverlapSpan();
+    String overlap_filter = Das2FeatureSaxParser.getPositionString(overlap_span, false);
+    SeqSpan inside_span = request_sym.getInsideSpan();
+    String inside_filter = Das2FeatureSaxParser.getPositionString(inside_span, false);
+    System.out.println("in Das2Region.getFeatures(), overlap = " + overlap_filter + ", inside = " + inside_filter);
+
+    String version_url =
+      getVersionedSource().getSource().getServerInfo().getRootUrl() + "/" +
+      getVersionedSource().getSource().getID() + "/" +
+      getVersionedSource().getID();
+    System.out.println("version url: " + version_url);
+
+    StringBuffer buf = new StringBuffer(200);
+    buf.append(version_url);
+    buf.append("/feature?");
+    buf.append("overlaps=");
+    buf.append(overlap_filter);
+    buf.append(";");
+    if (inside_filter != null) {
+      buf.append("inside=");
+      buf.append(inside_filter);
+      buf.append(";");
+    }
+    buf.append("type=");
+    buf.append(request_sym.getDas2Type().getID());
+
+    String feature_query = buf.toString();
+    System.out.println("feature query:  " + feature_query);
+
+    // need to move actual parsing out of here and into something more generalized
+    //     also need to figure out where to interject a Das2QueryOptimizer...
+    try {
+      Das2FeatureSaxParser parser = new Das2FeatureSaxParser();
+      URL query_url = new URL(feature_query);
+      URLConnection query_con = query_url.openConnection();
+      BufferedInputStream bis = new BufferedInputStream(query_con.getInputStream());
+      List feats = parser.parse(new InputSource(bis), getVersionedSource().getGenome(), false);
+      int feat_count = feats.size();
+      System.out.println("parsed query results, annot count = " + feat_count);
+      for (int i=0; i<feat_count; i++) {
+	SeqSymmetry feat = (SeqSymmetry)feats.get(i);
+	//	SeqUtils.printSymmetry(feat);
+	request_sym.addChild(feat);
+      }
+      success = true;
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    return success;
   }
 
 }

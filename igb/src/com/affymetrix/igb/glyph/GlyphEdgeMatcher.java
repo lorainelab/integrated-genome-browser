f84ba370853caf699ba4b9459004f02942b9bd6c
chyekk <chyekk@mycompany.com>
2005-04-26 22:49:43 +0000

    Now adding the match glyphs to the TierGlyph rather than directly to the NeoMap. Adding to the NeoMap could cause problems when tiers were moved around and even sometimes when vertical zooming was done.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@244 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/glyph/GlyphEdgeMatcher.java
+++ igb/src/com/affymetrix/igb/glyph/GlyphEdgeMatcher.java
@@ -94,6 +94,7 @@ public class GlyphEdgeMatcher  {
     if ((target instanceof TransientGlyph) || (query instanceof TransientGlyph)) {
       return;
     }
+
     // pre-emptively eliminate non 1D overlappers...
     Rectangle2D qbox = query.getCoordBox();
     Rectangle2D tbox = target.getCoordBox();
@@ -137,10 +138,11 @@ public class GlyphEdgeMatcher  {
           mglyph.setHitable(false);
           mglyph.setCoords(tstart, tbox.y-1, 1, tbox.height+2);
           mglyph.setColor(col);
-          // Can add to map directly, or as child of target or of target.getParent()
+          // Can add mglyph to TierGlyph, or to the target or target.getParent()
           // There are advantages to each.  See note below.
-          map.addItem(mglyph);
           //map.addItem(target.getParent(), mglyph);
+          map.addItem(getTier(target), mglyph);
+          //map.addItem(mglyph); // do not do this, breaks when tiers are moved.
           match_glyphs.add(mglyph);
         }
       }
@@ -152,8 +154,9 @@ public class GlyphEdgeMatcher  {
           mglyph.setHitable(false);
           mglyph.setCoords(tend-1, tbox.y-1, 1, tbox.height+2);
           mglyph.setColor(col);
-          map.addItem(mglyph);
           //map.addItem(target.getParent(), mglyph);
+          map.addItem(getTier(target), mglyph);
+          //map.addItem(mglyph); // do not do this
           match_glyphs.add(mglyph);
         }
       }
@@ -161,7 +164,7 @@ public class GlyphEdgeMatcher  {
       if (qend == tstart) { } // does this count? not for now...
       
       // NOTE:
-      // Can add match glyphs directly to map or as child of target.getParent().
+      // Can add match glyphs directly to TierGlyph or as child of target.getParent().
       // 
       // If added to target, or target's parent:
       //   1) That parent may decide not to show the match glyphs when zoomed-out:
@@ -169,15 +172,28 @@ public class GlyphEdgeMatcher  {
       //   at all zoom levels.
       //   2) It may be possible to speed-up looking for matches.   (But this speed-up
       //   is probably minimal because we can easily reject matching the match
-      //   glyphs because they return false for isHittable.
+      //   glyphs because they return false for isHittable.)
       //
-      // If added directly to map, the main benefit is that you can always see
-      //   the matching glyphs at any zoom level.  For me right now, that is
-      //   desirable.
+      // If added directly to TierGlyph, the main benefit is that you can always see
+      //   the matching glyphs at any zoom level.  
+      //
+      // Do NOT add directly to map itself.  That doesn't work properly when
+      //   tiers are resized or moved.
       
     }
   }
 
+
+  // Returns the tier that the glyph is in.
+  // Result undefined if the glyph is not in a tier.
+  GlyphI getTier(GlyphI target) {
+    GlyphI p = target;
+    while (p.getParent() != null && ! (p instanceof com.affymetrix.igb.tiers.TierGlyph)) {
+      p = p.getParent();
+    }
+    return p;
+  }
+  
 }
 
 

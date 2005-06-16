6be5212e5e1b1d98fcb0e14795e83a874237402c
gregghelt2 <gregghelt2@mycompany.com>
2005-06-16 21:02:38 +0000

    Fleshing out Das2Type some more.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@311 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das2/Das2Type.java
+++ igb/src/com/affymetrix/igb/das2/Das2Type.java
@@ -1,11 +1,11 @@
 /**
 *   Copyright (c) 2001-2005 Affymetrix, Inc.
-*    
+*
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
 *   this source code.
 *   Distributions from Affymetrix, Inc., place this in the
-*   IGB_LICENSE.html file.  
+*   IGB_LICENSE.html file.
 *
 *   The license is also available at
 *   http://www.opensource.org/licenses/cpl.php
@@ -16,22 +16,27 @@ package com.affymetrix.igb.das2;
 import java.util.*;
 
 public class Das2Type {
-  String id;
   Das2VersionedSource versioned_source;
-  Map props;
+  String id;
   String ontology;
-  String derivation;
+  String derivation;  // in DAS2 XML, this is source attribute
   String info_url;
+  Map props;
+  Map formats;
 
-  public Das2Type(String id, Das2VersionedSource version) {
-    this.id = id;
+  public Das2Type(Das2VersionedSource version, String id, String ontology, String derivation, String href) {
     this.versioned_source = version;
+    this.id = id;
+    this.ontology = ontology;
+    this.derivation = derivation;
+    this.info_url = href;
   }
 
-  public String getID() { return id; }
   public Das2VersionedSource getVersionedSource() { return versioned_source; }
-  public Map getProps() { return props; }
+  public String getID() { return id; }
   public String getOntology() { return ontology; }
   public String getDerivation() { return derivation; }  // source attribute, but getSource() clashes with getVersionedSource();
   public String getInfoUrl() { return info_url; }
+  public Map getProps() { return props; }
+  public Map getFormats() { return formats; }
 }

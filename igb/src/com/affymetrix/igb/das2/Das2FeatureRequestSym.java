c9ffa2ac70a7115015224722aa42d349eb974681
gregghelt2 <gregghelt2@mycompany.com>
2005-07-18 14:06:21 +0000

    Added calculation of sym's span based on child spans.  Also overriding some mutable methods to throw errors.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@388 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das2/Das2FeatureRequestSym.java
+++ igb/src/com/affymetrix/igb/das2/Das2FeatureRequestSym.java
@@ -14,7 +14,9 @@
 package com.affymetrix.igb.das2;
 
 import com.affymetrix.genometry.*;
+import com.affymetrix.genometry.symmetry.LeafSingletonSymmetry;
 import com.affymetrix.igb.genometry.*;
+import com.affymetrix.genometry.span.SimpleMutableSeqSpan;
 
 /**
  *  Encapsulates a _constrained_ DAS2 feature query and the features returned from the query
@@ -38,8 +40,7 @@ import com.affymetrix.igb.genometry.*;
 // public class Das2FeatureRequestSym extends TypeContainerAnnot {
 public class Das2FeatureRequestSym extends SimpleSymWithProps implements TypedSym  {  // or should extend TypeContainerAnnot?
 
-  boolean initialized = false;
-  SeqSpan overlap_span;
+  LeafSingletonSymmetry overlap_span; // LeafSingletonSym also implements SeqSymmetry interface
   SeqSpan inside_span;
   // SeqSpan encompass_span;  // not needed, this is actually standard span of a single-span symmetry
 
@@ -49,7 +50,8 @@ public class Das2FeatureRequestSym extends SimpleSymWithProps implements TypedSy
   //     uses TypeContainerAnnot instead.  So for now make parent the TypeContainerAnnot
   //  Das2ContainerAnnot parent_container;
   TypeContainerAnnot parent_container;
-
+  BioSeq aseq;
+  MutableSeqSpan sum_child_spans = null;
   /*
     actually should be able to populate with fewer args to constructor
     given Das2Type and Das2Region, should be able to figure out TypeContainerAnnot?
@@ -60,8 +62,10 @@ public class Das2FeatureRequestSym extends SimpleSymWithProps implements TypedSy
     das2_type = type;
     das2_region = region;
     //    parent_container = container;
-    overlap_span = overlap;
+    //    overlap_span = overlap;
+    overlap_span = new LeafSingletonSymmetry(overlap);
     inside_span = inside;
+    aseq = overlap_span.getBioSeq();
   }
 
   public String getType() { return das2_type.getID(); }
@@ -72,6 +76,11 @@ public class Das2FeatureRequestSym extends SimpleSymWithProps implements TypedSy
    *  May need to returns a sym instead of span to better match up with SeqSymmetrySummarizer methods??
    */
   public SeqSpan getOverlapSpan() { return overlap_span; }
+
+  /**
+   *  Convenience method for returning overlap span as a SeqSymmetry with 1 span and 0 children
+   */
+  public SeqSymmetry getOverlapSym() { return overlap_span; }
 
   /**
    *  Returns the inside span, the span specified in the original DAS query
@@ -94,37 +103,67 @@ public class Das2FeatureRequestSym extends SimpleSymWithProps implements TypedSy
   public Das2Region getRegion() { return das2_region; }
   public Das2Type getDas2Type() { return das2_type; }
 
-  public boolean isInitialized() { return initialized; }
+  /**
+      overriding MutableSeqSymmetry addSpan(), reomoveSpan(), setSpan(), removeSpans(), clear() methods,
+      since span is determined by children
+  */
+  public void addSpan(SeqSpan span) {
+    throw new RuntimeException("Can't add span to Das2FeatureRequestSym directly!");
+  }
+  public void removeSpan(SeqSpan span) {
+    throw new RuntimeException("Can't remove span from Das2FeatureRequestSym directly!");
+  }
+  public void setSpan(int index, SeqSpan span) {
+    throw new RuntimeException("Can't set span for Das2FeatureRequestSym directly!");
+  }
+  public void removeSpans() {
+    throw new RuntimeException("Can't remove spans from Das2FeatureRequestSym directly!");
+  }
+  public void clear() {
+    throw new RuntimeException("Can't call clear() for Das2FeatureRequestSym directly!");
+  }
 
   // for now just consider overlap_span to be the actual span...
   public SeqSpan getSpan(int index) {
-    if (index == 0) { return overlap_span; }
+    if (index == 0) { return sum_child_spans; }
     else { return null; }
   }
 
-  public int getSpanCount() { return 1; }
-
-  /**  override getChildCount() to initialize (populate via DAS2 feature query) if not yet initialized? */
-  /*
-  public int getChildCount() {
-    if (! initialized) { init(); }
-    return super.getChildCount();
+  public SeqSpan getSpan(BioSeq seq) {
+    if (seq == aseq) { return sum_child_spans; }
+    else { return null; }
   }
-  */
 
-  /**  override getChildren() to initialize (populate via DAS2 feature query) if not yet initialized? */
-  /*
-  public SeqSymmetry getChild(int index) {
-    if (! initialized) { init(); }
-    return super.getChild(index);
+  public boolean getSpan(int index, MutableSeqSpan span) {
+    SeqSpan vspan = getSpan(index);
+    if (vspan == null) { return false; }
+    span.set(vspan.getStart(), vspan.getEnd(), aseq);
+    return true;
   }
-  */
 
-  /*
-  public void init() {
-    // initialize via call to Das2Region.getFeatures(this)?
-    intialized = true;
+  public boolean getSpan(BioSeq seq, MutableSeqSpan span) {
+    SeqSpan vspan = getSpan(seq);
+    if (vspan == null) { return false; }
+    span.set(vspan.getStart(), vspan.getEnd(), aseq);
+    return true;
   }
-  */
+
+  public int getSpanCount() { return ((sum_child_spans == null) ? 0 : 1); }
+
+  public void addChild(SeqSymmetry child) {
+    SeqSpan cspan = child.getSpan(aseq);
+    if (cspan != null) {
+      if (sum_child_spans == null) {
+	sum_child_spans = new SimpleMutableSeqSpan(cspan);
+      }
+      else {
+	sum_child_spans.set(Math.min(sum_child_spans.getMin(), cspan.getMin()),
+			    Math.max(sum_child_spans.getMax(), cspan.getMax()),
+			    aseq);
+      }
+    }
+    super.addChild(child);
+  }
+
 
 }

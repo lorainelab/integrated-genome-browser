f576f0e98bfffeb1d87b09159688ba686727f678
gregghelt2 <gregghelt2@mycompany.com>
2005-08-10 15:56:22 +0000

    Incrementing IGB version to 3.61
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@468 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/IGBConstants.java
+++ igb/src/com/affymetrix/igb/IGBConstants.java
@@ -23,5 +23,5 @@ public abstract class IGBConstants {
 
   public static String APP_NAME = "Integrated Genome Browser";
   public static String APP_SHORT_NAME = "IGB";
-  public static String IGB_VERSION = "3.58";
+  public static String IGB_VERSION = "3.61";
 }

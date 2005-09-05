04aa67899ded5fc228f29082d821e29fe28e9239
gregghelt2 <gregghelt2@mycompany.com>
2005-09-05 02:41:05 +0000

    Removced setGraphSeq() and setGraphCoords() methods, these can now only be set internally or via constructor.  Removed methods for setting and getting stored thresholds, this is no longer used (dynamic thresholds are handled in SmartGraphGlyph) Added constructor that just takes a bioseq (mainly to allow use in CompositeGraphSym subclass).
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@542 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/genometry/GraphSym.java
+++ igb/src/com/affymetrix/igb/genometry/GraphSym.java
@@ -27,9 +27,6 @@ public class GraphSym extends SimpleSymWithProps implements Cloneable {
   BioSeq graph_original_seq;
   String graph_name;
 
-  List thresh_names = null;
-  FloatList thresh_vals = null;
-  
   /** Property name that can be used to set/get the desired initial graph style.
    *  The property value should be an Integer where the int value can
    *  be used in GraphState.setGraphStyle().
@@ -47,17 +44,84 @@ public class GraphSym extends SimpleSymWithProps implements Cloneable {
     return newsym;
   }
 
-  public GraphSym(int[] x, float[] y, String name, BioSeq seq) {
+  public GraphSym(BioSeq seq) { 
     super();
+    this.graph_original_seq = seq;
+  }
+
+  /** add a constructor to explicitly set span? */
+  //  this would be for slices, which need a span that expresses the bounds of the slice 
+  //     (which will often be slightly bigger than the xcoord min and max)
+  //  public GraphSym(int[] x, float[] y, String name, BioSeq seq, SeqSpan span) {
+
+  public GraphSym(int[] x, float[] y, String name, BioSeq seq) {
+    this(seq);
     this.xcoords = x;
     this.ycoords = y;
     this.graph_name = name;
-    this.graph_original_seq = seq;
+    //    this.graph_original_seq = seq;
     // should at some point probably make seqspan shrink-wrap to bounds of xcoords...
     SeqSpan span = new SimpleSeqSpan(0, seq.getLength(), seq);
     this.addSpan(span);
   }
 
+  public void setGraphName(String name) {
+    this.graph_name = name;
+  }
+
+  public String getGraphName() {
+    return graph_name;
+  }
+
+  /* removed setGraphCoords() method, now can only set graph coord arrays in constructor
+    public void setGraphCoords(int[] x, float[] y) {
+    this.xcoords = x;
+    this.ycoords = y;
+  }
+  */
+
+  public int getPointCount() {
+    if (xcoords == null) { return 0; }
+    else { return xcoords.length; }
+  }
+
+  public int[] getGraphXCoords() {
+    return xcoords;
+  }
+
+  public float[] getGraphYCoords() {
+    return ycoords;
+  }
+
+  /*  removed setGraphSeq() method, now graph seq can only be set in constructor
+  public void setGraphSeq(BioSeq seq) {
+    this.graph_original_seq = seq;
+  }
+  */
+
+  /**
+   *  Get the seq that the graph's xcoords are specified in
+   */
+  public BioSeq getGraphSeq() {
+    return graph_original_seq;
+  }
+
+  /**
+   *  Overriding request for property "method" to return graph name.
+   */
+  public Object getProperty(String key) {
+    if (key.equals("method")) {
+      return getGraphName();
+    }
+    else {
+      return super.getProperty(key);
+    }
+  }
+
+  //  List thresh_names = null;
+  //  FloatList thresh_vals = null;
+
+  /*
   public void addStoredThreshold(String thresh_name, float score_thresh) {
     if (thresh_names == null) { thresh_names = new ArrayList(); }
     if (thresh_vals == null) { thresh_vals = new FloatList(); }
@@ -79,54 +143,6 @@ public class GraphSym extends SimpleSymWithProps implements Cloneable {
     if (thresh_vals == null) { return Float.NEGATIVE_INFINITY; }
     return thresh_vals.get(i);
   }
+  */
 
-  public void setGraphName(String name) {
-    this.graph_name = name;
-  }
-
-  public String getGraphName() {
-    return graph_name;
-  }
-
-  public void setGraphCoords(int[] x, float[] y) {
-    this.xcoords = x;
-    this.ycoords = y;
-  }
-
-  public int getPointCount() {
-    if (xcoords == null) { return 0; }
-    else { return xcoords.length; }
-  }
-
-  public int[] getGraphXCoords() {
-    return xcoords;
-  }
-
-  public float[] getGraphYCoords() {
-    return ycoords;
-  }
-
-  public void setGraphSeq(BioSeq seq) {
-    this.graph_original_seq = seq;
-  }
-
-  /**
-   *  Get the seq that the graph's xcoords are specified in
-   */
-  public BioSeq getGraphSeq() {
-    return graph_original_seq;
-  }
-
-  /**
-   *  Overriding request for property "method" to return graph name.
-   */
-  public Object getProperty(String key) {
-
-    if (key.equals("method")) {
-      return getGraphName();
-    }
-    else {
-      return super.getProperty(key);
-    }
-  }
 }

8c5919bad13332a5e0321265c87529aad6030807
chyekk <chyekk@mycompany.com>
2005-10-20 18:35:39 +0000

    Changes to how the default colors are determined; now come from the default AnnotStyle
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@616 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/glyph/CoverageSummarizerFactory.java
+++ igb/src/com/affymetrix/igb/glyph/CoverageSummarizerFactory.java
@@ -1,5 +1,5 @@
 /**
-*   Copyright (c) 2001-2004 Affymetrix, Inc.
+*   Copyright (c) 2001-2005 Affymetrix, Inc.
 *
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
@@ -21,16 +21,13 @@ import com.affymetrix.genometry.util.SeqUtils;
 import com.affymetrix.igb.view.*;
 import com.affymetrix.igb.tiers.*;
 
-//TODO: finish this class, use in TierLabelManager.addSymCoverageTier
 public class CoverageSummarizerFactory implements MapViewGlyphFactoryI  {
-  static Color default_color = Color.red;
-  static Color default_tier_color = Color.darkGray;
   static int default_glyph_height = 50;
 
-  Color glyph_color = default_color;
+  Color glyph_color;
   int glyph_height = default_glyph_height;
 
-  /**
+  /*
    *  style options:
    *    COVERAGE: standard viualization, shows plot of for each pixel in view, base-pair coverage
    *        (coords covered by spans / total coords in pixel)
@@ -41,8 +38,8 @@ public class CoverageSummarizerFactory implements MapViewGlyphFactoryI  {
   public CoverageSummarizerFactory() { }
 
   public void init(Map options) {
-    glyph_color = (Color)options.get("color");
-    if (glyph_color == null) { glyph_color = default_color; }
+    //glyph_color = (Color)options.get("color");
+    //if (glyph_color == null) { glyph_color = default_color; }
     if (options.get("height") != null) {
       try {
 	glyph_height = Integer.parseInt((String)options.get("height"));
@@ -63,20 +60,22 @@ public class CoverageSummarizerFactory implements MapViewGlyphFactoryI  {
   public void createGlyph(SeqSymmetry sym, SeqMapView gviewer) {
     SeqSpan span = sym.getSpan(0);
 
-    //    AffyTieredMap map = gviewer.getSeqMap();
     String meth = gviewer.determineMethod(sym);
     AnnotStyle annot_style = AnnotStyle.getInstance(meth , false);
 
     if (meth != null) {
-      //      System.out.println("in CoverageSummarizerFactory, annot type = " + meth);
       TierGlyph[] tiers = gviewer.getTiers(meth,
 					   false, // next_to_axis = false
 					   annot_style);
       TierGlyph ftier = tiers[0];
       //TierGlyph rtier = tiers[1]; // ignore the reverse tier
             
-      Color background_color = tiers[0].getFillColor(); // a default fallback
-      if (annot_style != null) {
+      Color background_color;
+      if (annot_style == null) {
+        glyph_color = AnnotStyle.getDefaultInstance().getColor();
+        background_color = AnnotStyle.getDefaultInstance().getBackground();
+      }
+      else {
         glyph_color = annot_style.getColor();
         background_color = annot_style.getBackground();
       }
@@ -105,7 +104,7 @@ public class CoverageSummarizerFactory implements MapViewGlyphFactoryI  {
       cov.setCoords(0, 0, coordseq.getLength(), glyph_height);
       ftier.addChild(cov);
 
-      // make sure set data model to the original sym (if transform was nneded then tsym will 
+      // make sure set data model to the original sym (if transform was needed then tsym will 
       //    probably be a DerivedSeqSymmetry with a reference to the original sym
       if (tsym instanceof DerivedSeqSymmetry)  {
 	map.setDataModel(cov, ((DerivedSeqSymmetry)tsym).getOriginalSymmetry());

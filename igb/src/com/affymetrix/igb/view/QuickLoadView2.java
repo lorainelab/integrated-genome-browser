cb6c9ba0cc29abc7b93f62e6acd7322e5e4ed5b7
gregghelt2 <gregghelt2@mycompany.com>
2005-10-27 02:11:18 +0000

    Added loading of sequence residues (as in old QuickLoaderView). Started to add in GUI for quickload options.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@658 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/QuickLoadView2.java
+++ igb/src/com/affymetrix/igb/view/QuickLoadView2.java
@@ -7,13 +7,20 @@ import java.util.*;
 import javax.swing.*;
 import java.util.regex.Pattern;
 
+import com.affymetrix.genometry.*;
+import com.affymetrix.genometry.span.*;
+import com.affymetrix.genometry.seq.*;
+import com.affymetrix.genometry.symmetry.*;
+import com.affymetrix.genometry.util.*;
+
+import com.affymetrix.igb.IGB;
 import com.affymetrix.igb.event.*;
 import com.affymetrix.igb.genometry.*;
 import com.affymetrix.igb.util.*;
 import com.affymetrix.igb.parsers.LiftParser;
 import com.affymetrix.igb.parsers.ChromInfoParser;
 import com.affymetrix.igb.menuitem.LoadFileAction;
-import com.affymetrix.igb.IGB;
+import com.affymetrix.igb.parsers.NibbleResiduesParser;
 
 public class QuickLoadView2 extends JComponent
          implements ItemListener, ActionListener, GroupSelectionListener, SeqSelectionListener  {
@@ -24,6 +31,11 @@ public class QuickLoadView2 extends JComponent
   static final String DEFUNCT_SERVER = "205.217.46.81";
   static final String SELECT_A_GENOME = "Select a genome to load";
   public static final String PREF_QUICKLOAD_URL = "QuickLoad URL";
+
+  public static final String PREF_DAS_DNA_SERVER_URL = "DAS DNA Server URL";
+  public static final String DEFAULT_DAS_DNA_SERVER = "http://genome.cse.ucsc.edu/cgi-bin/das";
+  static Map cache_usage_options;
+  static Map usage2str;
   //  static boolean LOAD_DEFAULT_ANNOTS = true;
 
   JComboBox serverCB;
@@ -32,10 +44,48 @@ public class QuickLoadView2 extends JComponent
   Map url2quickload = new LinkedHashMap();   // maps quickload root url to QuickLoadServerModel object
   QuickLoadServerModel current_server;
   AnnotatedSeqGroup current_group;
+  String current_genome_name;
+  AnnotatedBioSeq current_seq;
   Map cb2filename = new HashMap();
   SeqMapView gviewer;
 
+  JButton all_residuesB;
+  JButton partial_residuesB;
+  JButton optionsB;
+
+  // components for options dialog
+  JPanel optionsP;
+  JButton clear_cacheB;
+  JCheckBox cache_annotsCB;
+  JCheckBox cache_residuesCB;
+  JComboBox cache_usage_selector;
+  JButton reset_das_dna_serverB = new JButton("Reset");
+  JButton reset_quickload_urlB = new JButton("Reset");
+
+  static {
+    String norm = "Normal Usage";
+    String ignore = "Ignore Cache";
+    String only = "Use Only Cache";
+    Integer normal = new Integer(LocalUrlCacher.NORMAL_CACHE);
+    Integer ignore_cache = new Integer(LocalUrlCacher.IGNORE_CACHE);
+    Integer cache_only = new Integer(LocalUrlCacher.ONLY_CACHE);
+
+    cache_usage_options = new LinkedHashMap();
+    cache_usage_options.put(norm, normal);
+    cache_usage_options.put(ignore, ignore_cache);
+    cache_usage_options.put(only, cache_only);
+
+    usage2str = new LinkedHashMap();
+    usage2str.put(normal, norm);
+    usage2str.put(ignore_cache, ignore);
+    usage2str.put(cache_only, only);
+  }
+
+
   public QuickLoadView2() {
+
+    initOptionsDialog();
+
     gviewer = IGB.getSingletonIGB().getMapView();
     this.setLayout(new BorderLayout());
     JPanel choice_panel = new JPanel();
@@ -53,8 +103,30 @@ public class QuickLoadView2 extends JComponent
     choice_panel.add(genomeCB);
     JPanel panel2 = new JPanel(new BorderLayout());
     panel2.add("West", choice_panel);
+
+    JPanel buttonP = new JPanel();
+    buttonP.setLayout(new GridLayout(1, 3));
+    if (IGB.isSequenceAccessible()) {
+      all_residuesB = new JButton("Load All Sequence Residues");
+      all_residuesB.addActionListener(this);
+      buttonP.add(all_residuesB);
+      partial_residuesB = new JButton("Load Sequence Residues in View");
+      if (IGB.ALLOW_PARTIAL_SEQ_LOADING) {
+	partial_residuesB.addActionListener(this);
+	buttonP.add(partial_residuesB);
+      }
+    }
+    else {
+      buttonP.add(new JLabel("No sequence residues available", JLabel.CENTER));
+    }
+    optionsB = new JButton("Quickload Options");
+    buttonP.add(optionsB);
+    optionsB.addActionListener(this);
+
     this.add("North", panel2);
     this.add("Center", new JScrollPane(types_panel));
+    this.add("South", buttonP);
+
     //    this.add("North", choice_panel);
     gmodel.addGroupSelectionListener(this);
     gmodel.addSeqSelectionListener(this);
@@ -85,6 +157,35 @@ public class QuickLoadView2 extends JComponent
 	//	boolean loaded = current_server.getLoadState(current_group, filename);
       }
     }
+    /* handles residues loading based on partial or full sequence load buttons */
+    else if (src == partial_residuesB) {
+      if (current_seq==null) { ErrorHandler.errorPanel("Error", "No sequence selected.", gviewer); return; }
+      String seq_name = current_seq.getID();
+      SeqSpan viewspan = gviewer.getVisibleSpan();
+      if (viewspan.getBioSeq() != current_seq) {
+	System.err.println("Error in QuickLoaderView: " +
+			   "SeqMapView seq and QuickLoaderView current_seq not the same!");
+	System.exit(0);
+      }
+      loadPartialResidues(viewspan);
+    }
+    else if (src == all_residuesB) {
+      if (current_seq==null) { ErrorHandler.errorPanel("Error", "No sequence selected.", gviewer); return; }
+      String seq_name = current_seq.getID();
+      loadAllResidues(seq_name);
+    }
+    else if (src == optionsB) {
+      showOptions();
+    } else if (src == reset_das_dna_serverB) {
+      UnibrowPrefsUtil.getLocationsNode().put(PREF_DAS_DNA_SERVER_URL, DEFAULT_DAS_DNA_SERVER);
+    } else if (src == reset_quickload_urlB) {
+      String url_from_xml_file = (String) IGB.getIGBPrefs().get("QuickLoadUrl");
+      if (url_from_xml_file != null) {
+        UnibrowPrefsUtil.getLocationsNode().put(PREF_QUICKLOAD_URL, url_from_xml_file);
+      } else {
+        UnibrowPrefsUtil.getLocationsNode().put(PREF_QUICKLOAD_URL, DEFAULT_QUICKLOAD_URL);
+      }
+    }
 
   }
 
@@ -101,6 +202,7 @@ public class QuickLoadView2 extends JComponent
 	if (gmodel.getSelectedSeqGroup() != group) {
 	  // need to initialize genome before setting it as selected seq group, in
 	  //    case it hasn't been seen before
+	  current_genome_name = genome_name;
 	  current_server.initGenome(genome_name);
 	  // calling gmodel.setSelectedSeqGroup() should also bounce event back to this.groupSelectionChanged()
 	  gmodel.setSelectedSeq(null);
@@ -127,15 +229,15 @@ public class QuickLoadView2 extends JComponent
       types_panel.removeAll();
 
       current_group = group;
-      String genome_name = current_server.getGenomeName(group);
-      if (genome_name == null) {
+      current_genome_name = current_server.getGenomeName(group);
+      if (current_genome_name == null) {
 	// what should behavior be if no genome in quickload server matches selected AnnotatedSeqGroup?
 	refreshGenomeChoices();
       }
       else {
-	current_server.initGenome(genome_name);
-	genomeCB.setSelectedItem(genome_name);
-	Map load_states = current_server.getLoadStates(genome_name);
+	current_server.initGenome(current_genome_name);
+	genomeCB.setSelectedItem(current_genome_name);
+	Map load_states = current_server.getLoadStates(current_genome_name);
 	Iterator iter = load_states.entrySet().iterator();
 	// populate list of checkboxes for annotation types
 	while (iter.hasNext()) {
@@ -157,6 +259,7 @@ public class QuickLoadView2 extends JComponent
 
   public void seqSelectionChanged(SeqSelectionEvent evt) {
     if (DEBUG_EVENTS) { System.out.println("QuickLoadView2.seqSelectionChanged() called"); }
+    current_seq = evt.getSelectedSeq();
   }
 
 
@@ -192,6 +295,190 @@ public class QuickLoadView2 extends JComponent
     return quickload_url;
   }
 
+  /**
+   *  Load sequence residues for a span along a sequence.
+   *  Access residues via DAS reference server
+   *
+   *  DAS reference server can be specified in igb_prefs.xml file by setting DasDnaServer element
+   *  Currently defaults to UCSC DAS reference server (this will cause problems if genome is not
+   *     available at UCSC)
+   */
+  public void loadPartialResidues(SeqSpan span)  {
+    String das_dna_server = UnibrowPrefsUtil.getLocation(PREF_DAS_DNA_SERVER_URL, DEFAULT_DAS_DNA_SERVER);
+    AnnotatedBioSeq aseq = (AnnotatedBioSeq)span.getBioSeq();
+    String seqid = aseq.getID();
+    System.out.println("trying to load residues for span: " + SeqUtils.spanToString(span));
+    System.out.println("current genome name: " + current_genome_name);
+    //    System.out.println("seq_id: " + seqid);
+    int min = span.getMin();
+    int max = span.getMax();
+    int length = span.getLength();
+
+    if ((min <= 0) && (max >= aseq.getLength())) {
+      System.out.println("loading all residues");
+      loadAllResidues(aseq.getID());
+    }
+    else if (aseq instanceof NibbleBioSeq)  {
+      String residues = null;
+      try {
+	String das_dna_source = DasUtils.findDasSource(das_dna_server, current_genome_name);
+	if (das_dna_source == null)  {
+	  ErrorHandler.errorPanel("Error", "Couldn't access sequence residues via DAS", gviewer);
+	  return;
+	}
+	String das_seqid = DasUtils.findDasSeqID(das_dna_server, das_dna_source, seqid);
+	if (das_seqid == null)  {
+	  ErrorHandler.errorPanel("Error", "Couldn't access sequence residues via DAS", gviewer);
+	  return;
+	}
+	 residues = DasUtils.getDasResidues(das_dna_server, das_dna_source, das_seqid,
+						  min, max);
+	System.out.println("DAS DNA request length: " + length);
+	System.out.println("DAS DNA response length: " + residues.length());
+      }
+      catch (Exception ex) {
+	ex.printStackTrace();
+      }
+
+      if (residues != null) {
+	BioSeq subseq = new SimpleBioSeq(aseq.getID() + ":" + min + "-" + max, residues);
+
+	SeqSpan span1 = new SimpleSeqSpan(0, length, subseq);
+	SeqSpan span2 = span;
+	MutableSeqSymmetry subsym = new SimpleMutableSeqSymmetry();
+	subsym.addSpan(span1);
+	subsym.addSpan(span2);
+
+	NibbleBioSeq compseq = (NibbleBioSeq)aseq;
+	MutableSeqSymmetry compsym = (MutableSeqSymmetry)compseq.getComposition();
+	if (compsym == null) {
+	  System.err.println("composite symmetry is null!");
+	  compsym = new SimpleMutableSeqSymmetry();
+	  compsym.addChild(subsym);
+	  compsym.addSpan(new SimpleSeqSpan(span2.getMin(), span2.getMax(), aseq));
+	  compseq.setComposition(compsym);
+	}
+	else {
+	  compsym.addChild(subsym);
+	  SeqSpan compspan = compsym.getSpan(aseq);
+	  int compmin = Math.min(compspan.getMin(), min);
+	  int compmax = Math.max(compspan.getMax(), max);
+	  SeqSpan new_compspan = new SimpleSeqSpan(compmin, compmax, aseq);
+	  compsym.removeSpan(compspan);
+	  compsym.addSpan(new_compspan);
+	  //	System.out.println("adding to composition: " );
+	  //	SeqUtils.printSymmetry(compsym);
+	  gviewer.setAnnotatedSeq(aseq, true, true);
+	}
+      }
+    }
+    else {
+      System.err.println("quickloaded seq is _not_ a NibbleBioSeq: " + aseq);
+    }
+  }
+
+  public void loadAllResidues(String seq_name) {
+    System.out.println("processing request to load residues for sequence: " +
+		       seq_name + ", version = " + current_genome_name);
+    if (current_seq.isComplete()) {
+      System.out.println("already have residues for " + seq_name);
+      return;
+    }
+    else {
+      try {
+	String http_root = getQuickLoadUrl();
+	String url_path = http_root + current_genome_name + "/" + seq_name + ".bnib";
+	System.out.println("location of bnib file: " + url_path);
+	System.out.println("current seq: id = " + current_seq.getID() + ", " + current_seq);
+	InputStream istr = LocalUrlCacher.getInputStream(url_path, QuickLoadServerModel.cache_usage, QuickLoadServerModel.cache_residues);
+	//	istr = (new URL(url_path)).openStream();
+	// NibbleResiduesParser handles creating a BufferedInputStream from the input stream
+	current_seq = NibbleResiduesParser.parse(istr, (NibbleBioSeq)current_seq);
+	istr.close();
+      }
+      catch(Exception ex) {
+	ErrorHandler.errorPanel("Error", "cannot access sequence for seq = " + seq_name +
+			   ", version = " + current_genome_name, gviewer, ex);
+      }
+      gviewer.setAnnotatedSeq(current_seq, true, true);
+    }
+  }
+
+
+
+  public void initOptionsDialog() {
+    //    System.out.println("showing quickload options");
+    if (optionsP == null) {
+      optionsP = new JPanel();
+      optionsP.setLayout(new BoxLayout(optionsP, BoxLayout.Y_AXIS));
+
+      Box url_box = new Box(BoxLayout.X_AXIS);
+      url_box.setBorder(new javax.swing.border.TitledBorder("QuickLoad URL"));
+      JTextField quickload_url_TF = UnibrowPrefsUtil.createTextField(UnibrowPrefsUtil.getLocationsNode(), PREF_QUICKLOAD_URL, DEFAULT_QUICKLOAD_URL);
+      url_box.add(quickload_url_TF);
+      url_box.add(reset_quickload_urlB);
+      reset_quickload_urlB.addActionListener(this);
+      optionsP.add(url_box);
+      optionsP.add(Box.createRigidArea(new Dimension(0, 5)));
+
+      Box server_box = new Box(BoxLayout.X_AXIS);
+      server_box.setBorder(new javax.swing.border.TitledBorder("Das DNA Server URL"));
+      JTextField das_dna_server_TF = UnibrowPrefsUtil.createTextField(UnibrowPrefsUtil.getLocationsNode(), PREF_DAS_DNA_SERVER_URL, DEFAULT_DAS_DNA_SERVER);
+      server_box.add(das_dna_server_TF);
+      server_box.add(reset_das_dna_serverB);
+      reset_das_dna_serverB.addActionListener(this);
+      optionsP.add(server_box);
+      optionsP.add(Box.createRigidArea(new Dimension(0, 5)));
+
+      JPanel cache_options_box = new JPanel();
+      cache_options_box.setBorder(new javax.swing.border.TitledBorder(""));
+      cache_options_box.setLayout(new GridLayout(4, 1));
+      optionsP.add(cache_options_box);
+
+      cache_annotsCB = new JCheckBox("Cache Annotations", QuickLoadServerModel.cache_annots);
+      cache_residuesCB = new JCheckBox("Cache DNA Residues", QuickLoadServerModel.cache_residues);
+      clear_cacheB = new JButton("Clear Cache");
+      cache_usage_selector = new JComboBox();
+      Iterator iter = cache_usage_options.keySet().iterator();
+      while (iter.hasNext()) {
+	String str = (String)iter.next();
+	cache_usage_selector.addItem(str);
+      }
+      cache_usage_selector.setSelectedItem(usage2str.get(new Integer(QuickLoadServerModel.cache_usage)));
+
+      cache_options_box.add(cache_annotsCB);
+      cache_options_box.add(cache_residuesCB);
+      JPanel usageP = new JPanel();
+      usageP.setLayout(new GridLayout(1,2));
+      usageP.add(new JLabel("Cache Usage"));
+      usageP.add(cache_usage_selector);
+      cache_options_box.add(usageP);
+      cache_options_box.add(clear_cacheB);
+
+      cache_annotsCB.addActionListener(this);
+      cache_residuesCB.addActionListener(this);
+      cache_usage_selector.addItemListener(this);
+      clear_cacheB.addActionListener(this);
+    }
+  }
+
+
+  public void showOptions() {
+    //TODO: before showing the options dialog, need to reset its GUI to actual current values
+    JOptionPane.showMessageDialog(this, optionsP, "Quickload Options", JOptionPane.PLAIN_MESSAGE);
+
+    //TODO: Give the user a "Cancel" options as well as "OK"
+    //int option = JOptionPane.showConfirmDialog(this, optionsP, "Quickload Options", JOptionPane.OK_
+    //if (option == JOptionPane.OK_OPTION) {
+        String usage_str = (String)cache_usage_selector.getSelectedItem();
+	int usage = ((Integer)cache_usage_options.get(usage_str)).intValue();
+        QuickLoadServerModel.setCacheBehavior(usage, cache_annotsCB.isSelected(), cache_residuesCB.isSelected());
+        // Note that the preferred DAS_DNA_SERVER_URL gets set immediately when the JTextBox is changed
+        // Note that the preferred QUICK_LOAD_URL gets set immediately when the JTextBox is changed
+        //  ... but we have to update the GUI in response to changes in QUICK_LOAD_URL
+	//        setQuickLoadURL(getQuickLoadUrl());
+    //}
+  }
 
 }
 
@@ -241,6 +528,25 @@ class QuickLoadServerModel {
     root_url = url;
     loadGenomeNames();
   }
+
+  protected static void setCacheBehavior(int behavior, boolean annots, boolean residues) {
+    if (behavior != cache_usage) {
+      cache_usage = behavior;
+      UnibrowPrefsUtil.saveIntParam("quickload_cache_usage", cache_usage);
+      System.out.println("storing cache_usage behavior: " + cache_usage);
+    }
+    if (residues != cache_residues) {
+      cache_residues = residues;
+      UnibrowPrefsUtil.saveBooleanParam("quickload_cache_residues", cache_residues);
+      System.out.println("storing cache_residues behavior: " + cache_residues);
+    }
+    if (annots != cache_annots) {
+      cache_annots = annots;
+      UnibrowPrefsUtil.saveBooleanParam("quickload_cache_annots", cache_annots);
+      System.out.println("storing cache_annots behavior: " + cache_annots);
+    }
+  }
+
 
   public String getRootUrl() { return root_url; }
   public Map getGenomeNames() { return name2group; }
@@ -442,6 +748,7 @@ class QuickLoadServerModel {
       setLoadState(current_group, filename, true);
     }
   }
+
 
 
 

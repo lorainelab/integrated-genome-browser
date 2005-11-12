753349271e4c7b9e575386f8fc279c11a29cebaa
mrjc42 <mrjc42@mycompany.com>
2005-11-12 01:52:02 +0000

    These classes help to allow the Das2OntologyVersionedSource to return back Ontology terms and their parents from a das 2 server.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@691 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ igb/src/com/affymetrix/igb/das2/ontology/Das2OntologySource.java
@@ -0,0 +1,45 @@
+/*
+ * Das2OntologySource.java
+ *
+ * Created on November 8, 2005, 1:29 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package com.affymetrix.igb.das2.ontology;
+
+import com.affymetrix.igb.das2.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.xml.sax.*;
+import org.w3c.dom.*;
+
+import com.affymetrix.igb.IGB;
+import com.affymetrix.igb.util.DasUtils;
+import com.affymetrix.igb.util.SynonymLookup;
+import com.affymetrix.igb.genometry.AnnotatedSeqGroup;
+import com.affymetrix.igb.genometry.SingletonGenometryModel;
+import com.affymetrix.igb.util.ErrorHandler;
+
+/**
+ *
+ * @author Marc Carlson
+ *
+ */
+public class Das2OntologySource extends Das2Source {
+    
+    /** Creates a new instance of Das2OntologySource */
+    public Das2OntologySource(Das2OntologyServerInfo source_server, String source_id, boolean init) {
+        super(source_server, source_id, init);
+    }
+    
+    //overridden methods    
+    public void addVersion(Das2OntologyVersionedSource version) {
+        this.versions.put(version.getID(), version);
+    }    
+        
+}

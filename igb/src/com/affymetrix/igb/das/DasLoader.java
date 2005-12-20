b3abd52ccfc3d4e6b0bb13e6aacf964004ce9cb6
gregghelt2 <gregghelt2@mycompany.com>
2005-12-20 19:09:42 +0000

    Added modification from SF to populate IGB sym hash when loading DAS1 annotations
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@741 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das/DasLoader.java
+++ igb/src/com/affymetrix/igb/das/DasLoader.java
@@ -28,6 +28,7 @@ import org.w3c.dom.Text;
 
 import com.affymetrix.genometry.*;
 import com.affymetrix.genometry.span.*;
+import com.affymetrix.igb.IGB;
 import com.affymetrix.igb.menuitem.LoadFileAction;
 
 import com.affymetrix.igb.parsers.BpsParser;
@@ -245,7 +246,7 @@ public abstract class DasLoader {
         new_seq = parser.parse(bis, current_seq, type);
       }
       else {
-        parser.parse(bis, type, null, seqhash, false, true);
+        parser.parse(bis, type, null, seqhash, IGB.getSymHash(), false, true);
         new_seq = current_seq;
       }
     } finally {

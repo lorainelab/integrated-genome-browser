6386c738e3a4054885895632e101035d10347493
gregghelt2 <gregghelt2@mycompany.com>
2005-12-20 19:06:17 +0000

    Added modification from SF to allow for large URLs.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@739 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/servlets/UnibrowControlServer.java
+++ igb/src/com/affymetrix/igb/servlets/UnibrowControlServer.java
@@ -1,11 +1,11 @@
 /**
 *   Copyright (c) 2001-2004 Affymetrix, Inc.
-*    
+*
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
 *   this source code.
 *   Distributions from Affymetrix, Inc., place this in the
-*   IGB_LICENSE.html file.  
+*   IGB_LICENSE.html file.
 *
 *   The license is also available at
 *   http://www.opensource.org/licenses/cpl.php
@@ -36,11 +36,11 @@ public class UnibrowControlServer {
   public final static String SERVLET_NAME = "UnibrowControl";
   static int ports_to_try = 5;
   int server_port;
-  
+
   public static final String DEFAULT_SERVLET_URL = "http://localhost:"
       + UnibrowControlServer.default_server_port +
       "/" + UnibrowControlServer.SERVLET_NAME;
-      
+
   public UnibrowControlServer(IGB uni) {
     try {
 
@@ -94,6 +94,9 @@ public class UnibrowControlServer {
 
         // Start the http server
         server.start();
+
+        // set the form content size limit high to allow for long urls
+        System.setProperty("org.mortbay.http.HttpRequest.maxFormContentSize", "100000000");
 	UnibrowControlServlet igb_controller = (UnibrowControlServlet)sholder.getServlet();
 	igb_controller.setUnibrowInstance(uni);
       }
@@ -128,7 +131,7 @@ public class UnibrowControlServer {
         }
         catch (Exception ex) {
           System.out.println("no igb found at port: " + current_port);
-        }	
+        }
       }
     }
     return other_igb_port;

a78af7fc76124d7302d8e38ae9341ce336da7020
chyekk <chyekk@mycompany.com>
2006-01-24 00:40:43 +0000

    Restored the ability of the display to update itself after a shortcut has been changed.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@810 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/prefs/KeyStrokesView.java
+++ igb/src/com/affymetrix/igb/prefs/KeyStrokesView.java
@@ -1,5 +1,5 @@
 /**
-*   Copyright (c) 2001-2004 Affymetrix, Inc.
+*   Copyright (c) 2001-2006 Affymetrix, Inc.
 *    
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
@@ -103,7 +103,7 @@ public class KeyStrokesView extends JPanel implements ListSelectionListener,
   }
 
   public void refresh() {
-    //showShortcuts();
+    showShortcuts();
   }
 
   /** This is called when the user selects a row of the table;

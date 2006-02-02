42d0192dc1b771368e537a0c304d8ce46af3feb1
chyekk <chyekk@mycompany.com>
2006-02-02 00:19:33 +0000

    Added a method to scroll a JTable so a particular row and column is visible.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@827 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/swing/DisplayUtils.java
+++ igb/src/com/affymetrix/swing/DisplayUtils.java
@@ -13,7 +13,8 @@
 
 package com.affymetrix.swing;
 
-import java.awt.Frame;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *  General Swing display utilities.
@@ -32,4 +33,32 @@ public class DisplayUtils {
     frame.toFront();
   }
   
+  /**
+   *  Scroll a JTable such that the given cell is visible.
+   *  (Only works if the JTable is inside a scrollable parent.)
+   *  From "The Java Developers Almanac 1.4."
+   *  See http://javaalmanac.com/egs/javax.swing.table/Vis.html
+   */
+  public static void scrollToVisible(JTable table, int rowIndex, int vColIndex) {
+    if (!(table.getParent() instanceof JViewport)) {
+      return;
+    }
+    JViewport viewport = (JViewport)table.getParent();
+    
+    // This rectangle is relative to the table where the
+    // northwest corner of cell (0,0) is always (0,0).
+    Rectangle rect = table.getCellRect(rowIndex, vColIndex, true);
+    
+    // The location of the viewport relative to the table
+    Point pt = viewport.getViewPosition();
+    
+    // Translate the cell location so that it is relative
+    // to the view, assuming the northwest corner of the
+    // view is (0,0)
+    rect.setLocation(rect.x-pt.x, rect.y-pt.y);
+    
+    // Scroll the area into view
+    viewport.scrollRectToVisible(rect);
+  }
+
 }

8c9cf46433ca19c25339f17ee5f632d1c2948988
gregghelt2 <gregghelt2@mycompany.com>
2006-02-07 05:37:12 +0000

    Modified to only point to codesprint DAS/2 server.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@846 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das2/Das2Discovery.java
+++ igb/src/com/affymetrix/igb/das2/Das2Discovery.java
@@ -22,10 +22,11 @@ public class Das2Discovery {
   static boolean servers_initialized = false;
 
   static {
-    name2url.put("netaffx", "http://netaffxdas.affymetrix.com/das2/genome");
-    name2url.put("das.biopackages.net", "http://das.biopackages.net/das/genome");
+    name2url.put("codesprint", "http://das.biopackages.net/codesprint");
+    //    name2url.put("localhost", "http://localhost:9092/das2");
+    //    name2url.put("netaffx", "http://netaffxdas.affymetrix.com/das2");
+    //    name2url.put("das.biopackages.net", "http://das.biopackages.net/das");
     //    name2url.put("riva",  "http://riva.ev.affymetrix.com:9092/das2/genome");
-    //    name2url.put("localhost", "http://localhost:9092/das2/genome");
     //    name2url.put("bad test", "http://this.is.a.test/hmmm");
   }
 

54c1de585b2124816f9b6b53bdb0d6818ee94ba8
gregghelt2 <gregghelt2@mycompany.com>
2006-02-13 16:30:43 +0000

    In comments adding some thoughts on how to deal with issue of handling SeqSymmetries that have multiple SeqSpans pointing to the same BioSeq.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@898 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometry/src/com/affymetrix/genometry/SeqSymmetry.java
+++ genometry/src/com/affymetrix/genometry/SeqSymmetry.java
@@ -21,6 +21,29 @@ package com.affymetrix.genometry;
 public interface SeqSymmetry {
   public String getID();
   public int getSpanCount();
+
+  //  public int getSpanCount(BioSeq seq);
+
+  /**
+   *  Convenience method to get a SeqSpan whose BioSeq is seq
+   *  If the SeqSymmetry has no spans with the specified seq, returns null
+   *  If the SeqSymmetry contains only one SeqSpan whose BioSeq is seq, then that span is returned
+   *  If the SeqSymmetry contains multiple spans whose BioSeq is seq, 
+   *      then only one these spans is returned -- which one is unspecified
+   *  
+   *  In many situations it will already be known by the calling code which SeqSymmetrys have 
+   *     only one span per seq, in which case getSpan(seq) is very convenient.  However, in 
+   *     general a SeqSymmetry can have multiple spans with the same seq, so unless the calling code 
+   *     already known a symmetry has only one span per seq, it should only use getSpan(seq) if 
+   *     sym.getSpanCount(seq) <= 1, otherwise it should iterate through all the spans 
+   *     via sym.getSpan(index) to find each span that points to the seq 
+   *
+   *  Other possibilities considered:
+   *    public List getSeqSpans(BioSeq seq)  -- but too much potential extra List creation and/or storage
+   *    public void getSeqSpans(BioSeq seq, List spans)  -- spans with BioSeq seq are added to passed in list
+   *  If either of these methods is added could probably remove getSpanCount(seq) method.
+   *  If add getSeqSpans(seq, spans) could possibly also remove getSpan(seq)
+   */
   public SeqSpan getSpan(BioSeq seq);
   public SeqSpan getSpan(int index);
   /** maybe this method should be called copySpan() ? */

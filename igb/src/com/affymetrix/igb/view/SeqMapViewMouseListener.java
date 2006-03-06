9454842db1fe0796d8823bd9cdccf0b72e5033c0
gregghelt2 <gregghelt2@mycompany.com>
2006-03-06 04:43:22 +0000

    Compensating for changes to SeqMapView.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@984 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/SeqMapViewMouseListener.java
+++ igb/src/com/affymetrix/igb/view/SeqMapViewMouseListener.java
@@ -68,7 +68,7 @@ public class SeqMapViewMouseListener implements MouseListener, NeoRubberBandList
 
   SeqMapViewMouseListener(SeqMapView smv) {
     this.smv = smv;
-    this.map = smv.main_map;
+    this.map = smv.seqmap;
   }
 
   public void mouseEntered(MouseEvent evt) { }

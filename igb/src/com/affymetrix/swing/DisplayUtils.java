a2871cb3585516fc16477a696aa3c18561233ab3
gregghelt2 <gregghelt2@mycompany.com>
2006-03-28 13:12:55 +0000

    Added adjustColumnPreferredWidths() method to adjust JTable column preferred widths based on table contents. Adapted from "Swing Hacks" by Joshua Marinacci and Chris Adamson, published by O'Reilly.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1098 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/swing/DisplayUtils.java
+++ igb/src/com/affymetrix/swing/DisplayUtils.java
@@ -15,6 +15,7 @@ package com.affymetrix.swing;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.*;
 
 /**
  *  General Swing display utilities.
@@ -59,6 +60,56 @@ public class DisplayUtils {
     
     // Scroll the area into view
     viewport.scrollRectToVisible(rect);
+  }
+
+
+  /**
+   * adjusts JTable column preferred widths based on table contents
+   * Adapted from "Swing Hacks" by Joshua Marinacci and Chris Adamson, 
+   *    copyright 2005 O'Reilly Media, ISBN 0-596-00907-0
+   */
+  public static void adjustColumnPreferredWidths(JTable table, boolean include_headers) {
+    // strategy - get max width for cells in column and
+    // make that the preferred width
+    TableColumnModel columnModel = table.getColumnModel();
+    for (int col=0; col<table.getColumnCount(); col++) {
+      int maxwidth = 0;
+      for (int row=0; row<table.getRowCount(); row++) {
+	TableCellRenderer rend = table.getCellRenderer (row, col);
+	Object value = table.getValueAt (row, col);
+	Component comp = rend.getTableCellRendererComponent (table,
+							     value,
+							     false,
+							     false,
+							     row,
+							     col);
+	maxwidth = Math.max (comp.getPreferredSize().width,
+			     maxwidth);
+      } // for row
+      if (include_headers) {
+	// include the column header's preferred width too
+	TableColumn column = columnModel.getColumn (col);
+	TableCellRenderer headerRenderer = column.getHeaderRenderer();
+	if (headerRenderer == null) {
+	  headerRenderer = table.getTableHeader().getDefaultRenderer();
+	}
+	Object headerValue = column.getHeaderValue();
+	Component headerComp =
+	  headerRenderer.getTableCellRendererComponent (table,
+							headerValue,
+							false,
+							false,
+							0,
+							col);
+	maxwidth = Math.max (maxwidth, headerComp.getPreferredSize().width);
+	column.setPreferredWidth (maxwidth);
+      }
+      else {
+	// don't include the column header's preferred width
+	TableColumn column = columnModel.getColumn (col);
+	column.setPreferredWidth (maxwidth);
+      }
+    } // for col
   }
 
 }

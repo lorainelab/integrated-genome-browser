4e61b6984a672d17bbd3e6ce96f6d12c2b329706
gregghelt2 <gregghelt2@mycompany.com>
2006-04-05 11:10:37 +0000

    Changed getID(), getGraphName(), clone() to support policy of unique ids for GraphSyms on same seq.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1139 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/genometry/GraphSym.java
+++ igb/src/com/affymetrix/igb/genometry/GraphSym.java
@@ -14,10 +14,12 @@
 package com.affymetrix.igb.genometry;
 
 import com.affymetrix.genometry.BioSeq;
+import com.affymetrix.genometry.AnnotatedBioSeq;
 import com.affymetrix.genometry.SeqSpan;
 import com.affymetrix.genometry.span.SimpleSeqSpan;
 import com.affymetrix.igb.glyph.GraphGlyph;
 import com.affymetrix.igb.glyph.GraphState;
+import com.affymetrix.igb.util.GraphSymUtils;
 
 /**
  *  A SeqSymmetry for holding graph data.
@@ -27,6 +29,7 @@ public class GraphSym extends SimpleSymWithProps implements Cloneable {
   float ycoords[];
   BioSeq graph_original_seq;
   GraphState state;
+  String gid;
 
  /** Property name that can be used to set/get the strand this graph corresponds to.
    *  The property value should be a Character, equal to '+', '-' or '.'.
@@ -36,18 +39,10 @@ public class GraphSym extends SimpleSymWithProps implements Cloneable {
   public Object clone() throws CloneNotSupportedException {
     GraphSym newsym = (GraphSym)super.clone();
     newsym.setGraphName(this.getGraphName() + ":clone");
-    newsym.state = new GraphState(state);
+    //    newsym.setID(this.getID() + ":clone");
+    newsym.gid = GraphSymUtils.getUniqueGraphID(this.getID() + ":clone", (AnnotatedBioSeq)this.getGraphSeq());
+    newsym.setGraphState(new GraphState(state));
     return newsym;
-  }
-
-  public GraphSym(BioSeq seq) {
-    super();
-    this.graph_original_seq = seq;
-    this.state = new GraphState();
-    //    this.graph_original_seq = seq;
-    // should at some point probably make seqspan shrink-wrap to bounds of xcoords...
-    SeqSpan span = new SimpleSeqSpan(0, seq.getLength(), seq);
-    this.addSpan(span);
   }
 
   /** add a constructor to explicitly set span? */
@@ -55,21 +50,46 @@ public class GraphSym extends SimpleSymWithProps implements Cloneable {
   //     (which will often be slightly bigger than the xcoord min and max)
   //  public GraphSym(int[] x, float[] y, String name, BioSeq seq, SeqSpan span) {
 
-  public GraphSym(int[] x, float[] y, String name, BioSeq seq) {
-    this(seq);
+  public GraphSym(int[] x, float[] y, String id, BioSeq seq) {
+    super();
+    this.graph_original_seq = seq;
+    this.state = new GraphState();
+    SeqSpan span = new SimpleSeqSpan(0, seq.getLength(), seq);
+    this.addSpan(span);
     this.xcoords = x;
     this.ycoords = y;
-    setGraphName(name);
+    this.gid = id;
+    //    System.out.println("GraphSym created, id = " + gid);
+    //    setID(id);
+    //    setGraphName(name);
   }
 
   public void setGraphName(String name) {
+    //    System.out.println("called GraphSym.setGraphName(): " + name);
     state.setLabel(name);
+    setProperty("name", name);
   }
 
   public String getGraphName() {
-    String name = state.getLabel();
-    if (name == null) { name = (String)this.getID(); }
-    return name;
+    //    System.out.println("called GraphSym.getGraphName()");
+    String gname = state.getLabel();
+    if (gname == null) {
+      gname = this.getID();
+    }
+    //    return state.getLabel();
+    return gname;
+  }
+
+  public String getID() {
+    //    System.out.println("called GraphSym.getID()");
+    return gid;
+  }
+
+  /**
+   *  Not allowed to call GraphSym.setID(), id
+   */
+  public void setID(String id) {
+    throw new RuntimeException("Attempted to call GraphSym.setID(), but not allowed to modify GraphSym id!");
   }
 
   public int getPointCount() {
@@ -116,10 +136,26 @@ public class GraphSym extends SimpleSymWithProps implements Cloneable {
     if (key.equals("method")) {
       return getGraphName();
     }
+    else if (key.equals("id")) {
+      return this.getID();
+    }
     else {
       return super.getProperty(key);
     }
   }
+
+  public boolean setProperty(String name, Object val) {
+    if (name.equals("id")) {
+      this.setID(name);
+      return false;
+    }
+    else {
+      return super.setProperty(name, val);
+    }
+  }
+
+
+
 
   //  List thresh_names = null;
   //  FloatList thresh_vals = null;

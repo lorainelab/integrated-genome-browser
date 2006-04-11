2859fd29ee62fcbf2067d6f29e4b33c59132e30c
chyekk <chyekk@mycompany.com>
2006-04-11 21:00:34 +0000

    Updated copyright
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1180 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/glyph/GraphSelectionManager.java
+++ igb/src/com/affymetrix/igb/glyph/GraphSelectionManager.java
@@ -1,5 +1,5 @@
 /**
-*   Copyright (c) 2001-200 Affymetrix, Inc.
+*   Copyright (c) 2001-2006 Affymetrix, Inc.
 *
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of

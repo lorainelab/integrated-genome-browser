eeb082e74ef1b1eeb841e97f3b0dc61f59de8f40
gregghelt2 <gregghelt2@mycompany.com>
2006-05-25 17:40:23 +0000

    Lots of changes to CurationControl, to make it work with latest version of IGB and to fix previous bugs.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1247 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/CurationControl.java
+++ igb/src/com/affymetrix/igb/view/CurationControl.java
@@ -1,11 +1,11 @@
 /**
 *   Copyright (c) 2001-2005 Affymetrix, Inc.
-*    
+*
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
 *   this source code.
 *   Distributions from Affymetrix, Inc., place this in the
-*   IGB_LICENSE.html file.  
+*   IGB_LICENSE.html file.
 *
 *   The license is also available at
 *   http://www.opensource.org/licenses/cpl.php
@@ -49,6 +49,7 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
 
   SeqMapView gviewer;
   CurationSym prev_curation;
+  String current_type = "Curation";
 
   // A fake menu item, prevents null pointer exceptions in actionPerformed()
   // for menu items whose real definitions are commented-out in the code
@@ -56,18 +57,16 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
   JMenuItem copyAsCurationMI = empty_menu_item;
   JMenuItem addToCurationMI = empty_menu_item;
   JMenuItem deleteFromCurationMI = empty_menu_item;
-  JMenuItem splitCurationMI = empty_menu_item;
   JMenuItem flipCurationMI = empty_menu_item;
   JMenuItem undoCurationMI = empty_menu_item;
   JMenuItem redoCurationMI = empty_menu_item;
+  JMenuItem newCurTypeMI = empty_menu_item;
+  JMenuItem selectCurationMI = empty_menu_item;
+  //  JMenuItem splitCurationMI = empty_menu_item;
 
   TierLabelManager tier_manager;
   JPopupMenu annot_popup;
-  JMenuItem newCurTypeMI = empty_menu_item;
-  JMenuItem selectCurationMI = empty_menu_item;
   JMenu curationM;
-
-  JMenu saveJM = new JMenu("Save");
 
   public CurationControl(SeqMapView smv) {
     gviewer = smv;
@@ -81,9 +80,9 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
     addToCurationMI = setUpMenuItem(curationM, "Add to current curation");
     deleteFromCurationMI = setUpMenuItem(curationM, "Delete from current curation");
     flipCurationMI = setUpMenuItem(curationM, "Flip current curation");
-    //splitCurationMI = setUpMenuItem(curationM, "Split Curation");
     undoCurationMI = setUpMenuItem(curationM, "Undo previous edit");
     redoCurationMI = setUpMenuItem(curationM, "Redo previous edit");
+    //splitCurationMI = setUpMenuItem(curationM, "Split Curation");
 
     annot_popup.add(curationM);
     gviewer.addPopupListener(this);
@@ -92,21 +91,18 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
   public void actionPerformed(ActionEvent evt) {
     //Object src = evt.getSource();
     String com = evt.getActionCommand();
-    System.out.println("Event: " + evt);
+    //    System.out.println("Event: " + evt);
     if (copyAsCurationMI.getText().equals(com)) { makeNewCuration(); }
     else if (addToCurationMI.getText().equals(com)) { addToCuration(); }
     else if (deleteFromCurationMI.getText().equals(com)) { deleteFromCuration(); }
-    else if (splitCurationMI.getText().equals(com)) { splitCuration(); }
     else if (flipCurationMI.getText().equals(com)) { flipCuration(); }
     else if (undoCurationMI.getText().equals(com)) { undoCuration(); }
     else if (redoCurationMI.getText().equals(com)) { redoCuration(); }
     else if (newCurTypeMI.getText().equals(com)) { makeNewCurationType(); }
     else if (selectCurationMI.getText().equals(com)) { setCurrentCuration(); }
+    //    else if (splitCurationMI.getText().equals(com)) { splitCuration(); }
   }
 
-  String current_type = "Curation";
-  // hash of curation types to tier glyphs
-  // HashMap curation_types = new HashMap();
   int new_type_count = 0;
 
   /**
@@ -114,20 +110,12 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
    *  new type of curation.
    */
   public void makeNewCurationType() {
-    /*
-    String new_type = JOptionPane.showInputDialog(gviewer.getFrame(),
-						    "Please enter new type",
-						    "New Curation Type",
-						    JOptionPane.QUESTION_MESSAGE,
-    						    null, null, null);
-    */
     String new_type = JOptionPane.showInputDialog("Please enter new type");
     if (new_type != null) {
       current_type = new_type;
       new_type_count++;
       makeNewCuration();
     }
-    //    current_type = "curation_" + new_type_count;
   }
 
   public void setCurrentCuration() {
@@ -157,15 +145,11 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
 
   // make two new curations based on splitting the current curation at the selected region...
   /** NOT YET IMPLEMENTED */
-  public void splitCuration() {
-    MutableAnnotatedBioSeq aseq = (MutableAnnotatedBioSeq)gmodel.getSelectedSeq();
-    SeqSymmetry annot_sym = gviewer.getSelectedSymmetry();
-    SeqSymmetry region_sym = gviewer.getSelectedRegion();
-
-  }
+  //  public void splitCuration() {
+  //  }
 
   public void makeNewCuration() {
-    System.out.println("making new curation");
+    System.out.println("making new curation, type = " + current_type);
     MutableAnnotatedBioSeq aseq =
       (MutableAnnotatedBioSeq) gmodel.getSelectedSeq();
     if (aseq == null) {
@@ -186,35 +170,14 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
       aseq.addAnnotation(curation_sym);
 
       // place annotation tiers next to axis...
-      //          gviewer.addAnnotationTiers(curation_sym, true);  // replacing with factory call...
       gviewer.addAnnotationGlyphs(curation_sym);
-
-      // need to filter out empty tier(s) here, because
-      //   SeqMapView.addAnnotationTiers(sym) may make both + and - new tiers...
       AffyTieredMap tmap = gviewer.getSeqMap();
-      TierGlyph tgl = null;
-      if (curation_sym.getSpan(aseq).isForward()) {
-	tgl = tmap.getTier(current_type + " (+)");
-      }
-      else {
-	tgl = tmap.getTier(current_type + " (-)");
-      }
+      TierGlyph[] tiers = gviewer.getTiers(current_type, true, AnnotStyle.getInstance(current_type));
+      TierGlyph tgl = (curation_sym.getSpan(aseq).isForward() ? tiers[0] : tiers[1]);
       tgl.pack(tmap.getView());
-      tmap.packTiers(false, true, false);
+      tmap.stretchToFit(false, false);
+      //      tmap.packTiers(false, true, false);  // already called in tmap.stretchToFit()
       tmap.updateWidget();
-      //TODO: convert to using TierLabelManager.PopupListener
-      /*
-      java.util.List tier_menu_list = tier_manager.getMenuList(tgl);
-      boolean tier_initialized = false;
-      for (int i=0; i<tier_menu_list.size(); i++) {
-	Object item = tier_menu_list.get(i);
-	if (item == saveJM) { tier_initialized = true; break; }
-      }
-      if (! tier_initialized) {
-	tier_menu_list.add(saveJM);
-      }
-       */
-
       prev_curation = curation_sym;
     }
   }
@@ -243,14 +206,17 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
       gviewer.addAnnotationGlyphs(curation_sym);
 
       AffyTieredMap tmap = gviewer.getSeqMap();
-      TierGlyph tgl = tmap.getTier(current_type + " (+)");
+      TierGlyph[] tiers = gviewer.getTiers(current_type, true, AnnotStyle.getInstance(current_type));
+      TierGlyph tgl = (curation_sym.getSpan(aseq).isForward() ? tiers[0] : tiers[1]);
+
       if (! KEEP_PREVIOUS_CURATION) {
 	aseq.removeAnnotation(prev_curation);
 	GlyphI prevgl = tmap.getItem(prev_curation);
 	if (prevgl != null) { tmap.removeItem(prevgl); }
       }
       tgl.pack(tmap.getView());
-      tmap.packTiers(false, true, false);
+      tmap.stretchToFit(false, false);
+      //      tmap.packTiers(false, true, false);  // already called in tmap.stretchToFit()
       tmap.updateWidget();
       prev_curation = curation_sym;
     }
@@ -272,14 +238,44 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
       System.out.println("deleting from curation");
       if (annot_sym == null) { annot_sym = region_sym; }
       CurationSym curation_sym = new CurationSym();
+      TierGlyph[] tiers = gviewer.getTiers(current_type, true, AnnotStyle.getInstance(current_type));
+      TierGlyph tgl = (prev_curation.getSpan(aseq).isForward() ? tiers[0] : tiers[1]);
+
       curation_sym.setPredecessor(prev_curation);
       prev_curation.setSuccessor(curation_sym);
       curation_sym.setProperty("method", current_type);
       SeqSymmetry inverted_annot = SeqUtils.inverse(annot_sym, aseq, true);
       SeqUtils.intersection(prev_curation, inverted_annot, curation_sym, aseq);
-      aseq.addAnnotation(curation_sym);
+      symCleanupHack(curation_sym, aseq);
+      //      SeqUtils.printSymmetry(curation_sym);
+      // if entire prev_curation is deleted, then don't want to add new curation
+      //     if prev_curation is deleted, curation_sym will be empty, so check based on presence of aseq span in curation_sym
+      if (curation_sym.getSpan(aseq) != null)  {
+        aseq.addAnnotation(curation_sym);
+        gviewer.addAnnotationGlyphs(curation_sym);
+      }
+
+      AffyTieredMap tmap = gviewer.getSeqMap();
+
+      if (! KEEP_PREVIOUS_CURATION) {
+	aseq.removeAnnotation(prev_curation);
+	GlyphI prevgl = tmap.getItem(prev_curation);
+	if (prevgl != null) { tmap.removeItem(prevgl); }
+      }
+      tgl.pack(tmap.getView());
+      tmap.stretchToFit(false, false);
+      //      tmap.packTiers(false, true, false);  // already called in tmap.stretchToFit()
+      tmap.updateWidget();
+      if (curation_sym.getSpan(aseq) == null)  {  // no curation sym added, so null out prev_curation
+        prev_curation = null;
+      }
+      else  { prev_curation = curation_sym; }
+    }
+  }
+
+  protected void symCleanupHack(CurationSym curation_sym, AnnotatedBioSeq aseq) {
       // little hack to clean up, because intersection() ends up creating 0-length
-      //   symmetries if two compared spans abut -- 
+      //   symmetries if two compared spans abut --
       //   (which is why genometry implementation needs to be fixed so that
       //    abutment isn't considered intersection)
       ArrayList clist = new ArrayList();
@@ -301,21 +297,6 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
 	curation_sym.addSpan(new_pspan);
       }
       // end zero-length workaround hack
-
-      gviewer.addAnnotationGlyphs(curation_sym);
-
-      AffyTieredMap tmap = gviewer.getSeqMap();
-      TierGlyph tgl = tmap.getTier(current_type + " (+)");
-      if (! KEEP_PREVIOUS_CURATION) {
-	aseq.removeAnnotation(prev_curation);
-	GlyphI prevgl = tmap.getItem(prev_curation);
-	if (prevgl != null) { tmap.removeItem(prevgl); }
-      }
-      tgl.pack(tmap.getView());
-      tmap.packTiers(false, true, false);
-      tmap.updateWidget();
-      prev_curation = curation_sym;
-    }
   }
 
   public void undoCuration() {
@@ -331,13 +312,15 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
       aseq.removeAnnotation(current_curation);
       aseq.addAnnotation(prev_curation);
       AffyTieredMap tmap = gviewer.getSeqMap();
-      TierGlyph tgl = tmap.getTier(current_type + " (+)");
+      TierGlyph[] tiers = gviewer.getTiers(current_type, true, AnnotStyle.getInstance(current_type));
+      TierGlyph tgl = (current_curation.getSpan(aseq).isForward() ? tiers[0] : tiers[1]);
       GlyphI curgl = tmap.getItem(current_curation);
       if (curgl != null) { tmap.removeItem(curgl); }
       gviewer.addAnnotationGlyphs(prev_curation);
 
       tgl.pack(tmap.getView());
-      tmap.packTiers(false, true, false);
+      tmap.stretchToFit(false, false);
+      //      tmap.packTiers(false, true, false);  // already called in tmap.stretchToFit()
       tmap.updateWidget();
     }
   }
@@ -355,14 +338,15 @@ public class CurationControl implements ActionListener, ContextualPopupListener 
       aseq.removeAnnotation(current_curation);
       aseq.addAnnotation(next_curation);
       AffyTieredMap tmap = gviewer.getSeqMap();
-      TierGlyph tgl = tmap.getTier(current_type + " (+)");
+      TierGlyph[] tiers = gviewer.getTiers(current_type, true, AnnotStyle.getInstance(current_type));
+      TierGlyph tgl = (next_curation.getSpan(aseq).isForward() ? tiers[0] : tiers[1]);
       GlyphI curgl = tmap.getItem(current_curation);
       if (curgl != null) { tmap.removeItem(curgl); }
-
       gviewer.addAnnotationGlyphs(next_curation);
-
+      prev_curation = next_curation;
       tgl.pack(tmap.getView());
-      tmap.packTiers(false, true, false);
+      tmap.stretchToFit(false, false);
+      //      tmap.packTiers(false, true, false);  // already called in tmap.stretchToFit()
       tmap.updateWidget();
     }
   }

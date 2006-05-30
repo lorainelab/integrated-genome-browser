a41f95907b61191efbdeedf31e7f4afcab12c7cb
chyekk <chyekk@mycompany.com>
2006-05-30 23:16:54 +0000

    Removed some unused variables.  This should save a few bytes.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1266 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/genometry/UcscGeneSym.java
+++ igb/src/com/affymetrix/igb/genometry/UcscGeneSym.java
@@ -1,5 +1,5 @@
 /**
-*   Copyright (c) 2001-2004 Affymetrix, Inc.
+*   Copyright (c) 2001-2006 Affymetrix, Inc.
 *    
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
@@ -20,7 +20,7 @@ import com.affymetrix.genometry.span.*;
 import com.affymetrix.genometry.symmetry.*;
 
 /**
- *  a SeqSymmetry (as well as SeqSpan) representation of UCSC MySQL RefSeq annotations.
+ *  A SeqSymmetry (as well as SeqSpan) representation of UCSC MySQL RefSeq annotations.
  *
  *
  *  from http://genome.cse.ucsc.edu/goldenPath/gbdDescriptions.html#RefFlat:
@@ -49,10 +49,8 @@ import com.affymetrix.genometry.symmetry.*;
     * </pre>
     *
  */
-public class UcscGeneSym
-  implements SeqSpan, SeqSymmetry, SupportsCdsSpan,
-	     TypedSym, com.affymetrix.igb.genometry.SymWithProps {
-  boolean SPAN_IS_SELF = true;
+public class UcscGeneSym implements SeqSpan, SeqSymmetry, SupportsCdsSpan, SymWithProps {
+  
   String geneName;
   String name;
   int txMin;
@@ -64,7 +62,6 @@ public class UcscGeneSym
   int[] emaxs;
   BioSeq seq;
   String type;
-  SeqSymmetry parent;
   Map props;
 
   public UcscGeneSym(String type, String geneName, String name,
@@ -74,7 +71,7 @@ public class UcscGeneSym
     this.type = type;
     this.geneName = geneName;
     this.name = name;
-    this.seq = seq;  // replace chrom name-string with reference to chrom BioSeq
+    this.seq = seq;
     this.forward = forward;
     this.txMin = txMin;
     this.txMax = txMax;

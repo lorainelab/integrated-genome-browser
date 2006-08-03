651e4fd87890411c1eca373450a9c006a522e5a9
chyekk <chyekk@mycompany.com>
2006-08-03 21:01:27 +0000

    Modified to work with the genoviz_sdk and genometry jars either in the top directory (as in the jar-file releases) or in the sub-directories (as in the source-file releases)
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1377 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- run_igb.sh
+++ run_igb.sh
@@ -16,13 +16,29 @@ export CLASSPATH="$CLASSPATH:ext/freehep-graphicsio.jar"
 export CLASSPATH="$CLASSPATH:ext/freehep-graphicsio-gif.jar"
 export CLASSPATH="$CLASSPATH:ext/freehep-graphicsio-ps.jar"
 
-export CLASSPATH="$CLASSPATH:genoviz.jar"
-export CLASSPATH="$CLASSPATH:genometry.jar"
+if [ -f genoviz.jar ]; then
+  export CLASSPATH="$CLASSPATH:genoviz.jar"
+else
+  export CLASSPATH="$CLASSPATH:genoviz_sdk/genoviz.jar"
+fi
+if [ -f genometry.jar ]; then
+  export CLASSPATH="$CLASSPATH:genometry.jar"
+else
+  export CLASSPATH="$CLASSPATH:genometry/genometry.jar"
+fi
 export CLASSPATH="$CLASSPATH:igb.jar"
+
+#export CLASSPATH="$CLASSPATH:/localhome/liquid_lnf/LiquidLnF.jar"
+#export VMARGS="-Dswing.defaultlaf=com.birosoft.liquid.LiquidLookAndFeel"
+
+#export CLASSPATH="$CLASSPATH:/localhome/napkinlaf.jar"
+#export VMARGS="-Dswing.defaultlaf=net.sourceforge.napkinlaf.NapkinLookAndFeel"
 
 export MAIN=com.affymetrix.igb.IGB
 
-java -mx256m -classpath $CLASSPATH $MAIN -prefs igb_prefs.xml
+export JAVA="/nfs/linux/pkg/java_pkg/jdk1.5/bin/java"
+
+$JAVA -mx256m  $VMARGS -classpath $CLASSPATH $MAIN -prefs igb_prefs.xml
 
 # Starting with version 4.0, most users no longer need an igb_prefs.xml file.
 # But one is specified here as an example.

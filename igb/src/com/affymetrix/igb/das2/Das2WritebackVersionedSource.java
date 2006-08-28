837512a1603422a4f3abbcc9bc024a07d5a44e3a
chyekk <chyekk@mycompany.com>
2006-08-28 17:22:34 +0000

    Added copyright.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1505 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das2/Das2WritebackVersionedSource.java
+++ igb/src/com/affymetrix/igb/das2/Das2WritebackVersionedSource.java
@@ -1,3 +1,15 @@
+/**
+*   Copyright (c) 2005-2006 Affymetrix, Inc.
+*
+*   Licensed under the Common Public License, Version 1.0 (the "License").
+*   A copy of the license must be included with any distribution of
+*   this source code.
+*   Distributions from Affymetrix, Inc., place this in the
+*   IGB_LICENSE.html file.
+*
+*   The license is also available at
+*   http://www.opensource.org/licenses/cpl.php
+*/
 package com.affymetrix.igb.das2;
 
 import java.net.*;
@@ -5,7 +17,7 @@ import java.util.*;
 
 public class Das2WritebackVersionedSource extends Das2VersionedSource  {
 
-  static String WRITEBACK_CAP_QUERY = "writeback";
+  static final String WRITEBACK_CAP_QUERY = "writeback";
 
   public Das2WritebackVersionedSource(Das2Source das_source, URI vers_uri, String name,
 				      String href, String description, boolean init) {

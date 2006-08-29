202fe04ae2583194ad43ef735755ac99fdeb91a1
chyekk <chyekk@mycompany.com>
2006-08-29 17:57:05 +0000

    Updated the classpath.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1518 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- run_igb.bat
+++ run_igb.bat
@@ -9,21 +9,28 @@ REM This file assumes you have the 'java' program in your PATH.
 REM If not, see http://java.sun.com/j2se/1.4.2/install-windows.html
 REM for instructions on setting the PATH variable.
 
-set CLASSPATH=%CLASSPATH%;ext/xercesImpl.jar
-set CLASSPATH=%CLASSPATH%;ext/xml-apis.jar
-set CLASSPATH=%CLASSPATH%;ext/javax.servlet.jar
-set CLASSPATH=%CLASSPATH%;ext/org.mortbay.jetty.jar
-set CLASSPATH=%CLASSPATH%;ext/commons-logging.jar
-set CLASSPATH=%CLASSPATH%;ext/affx_fusion.jar
-set CLASSPATH=%CLASSPATH%;ext/jlfgr-1_0.jar
+
+REM The genoviz.jar and genometry.jar files are stored in different
+REM places in the jar-file distribution and the source-file distribution.
+REM Both possible paths are listed below.
+
+set CLASSPATH=%CLASSPATH%;ext\xercesImpl.jar
+set CLASSPATH=%CLASSPATH%;ext\xml-apis.jar
+set CLASSPATH=%CLASSPATH%;ext\javax.servlet.jar
+set CLASSPATH=%CLASSPATH%;ext\org.mortbay.jetty.jar
+set CLASSPATH=%CLASSPATH%;ext\commons-logging.jar
+set CLASSPATH=%CLASSPATH%;ext\affx_fusion.jar
+set CLASSPATH=%CLASSPATH%;ext\jlfgr-1_0.jar
+set CLASSPATH=%CLASSPATH%;genoviz_sdk\genoviz.jar
 set CLASSPATH=%CLASSPATH%;genoviz.jar
+set CLASSPATH=%CLASSPATH%;genometry\genometry.jar
 set CLASSPATH=%CLASSPATH%;genometry.jar
 set CLASSPATH=%CLASSPATH%;igb.jar
-set CLASSPATH=%CLASSPATH%;ext/freehep-base.jar
-set CLASSPATH=%CLASSPATH%;ext/freehep-graphics2d.jar
-set CLASSPATH=%CLASSPATH%;ext/freehep-graphicsio.jar
-set CLASSPATH=%CLASSPATH%;ext/freehep-graphicsio-gif.jar
-set CLASSPATH=%CLASSPATH%;ext/freehep-graphicsio-ps.jar
+set CLASSPATH=%CLASSPATH%;ext\freehep-base.jar
+set CLASSPATH=%CLASSPATH%;ext\freehep-graphics2d.jar
+set CLASSPATH=%CLASSPATH%;ext\freehep-graphicsio.jar
+set CLASSPATH=%CLASSPATH%;ext\freehep-graphicsio-gif.jar
+set CLASSPATH=%CLASSPATH%;ext\freehep-graphicsio-ps.jar
 
 
 set MAIN=com.affymetrix.igb.IGB

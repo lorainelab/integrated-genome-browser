3e050ec926cab82432f44a6f533dd935fed0d2b7
chyekk <chyekk@mycompany.com>
2006-10-03 23:52:58 +0000

    Updated to bug-fix version 4.08.02
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1625 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/IGBConstants.java
+++ igb/src/com/affymetrix/igb/IGBConstants.java
@@ -23,5 +23,5 @@ public abstract class IGBConstants {
 
   public static String APP_NAME = "Integrated Genome Browser";
   public static String APP_SHORT_NAME = "IGB";
-  public static String IGB_VERSION = "4.48";
+  public static String IGB_VERSION = "4.48.02";
 }

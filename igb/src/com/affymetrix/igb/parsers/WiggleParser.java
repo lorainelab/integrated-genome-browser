6ac986e7c7d4b3b7244cff80e459dbcd2683f794
chyekk <chyekk@mycompany.com>
2006-11-02 17:23:50 +0000

    WiggleParser now parses all 3 wiggle formats.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1711 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/parsers/WiggleParser.java
+++ igb/src/com/affymetrix/igb/parsers/WiggleParser.java
@@ -1,15 +1,15 @@
 /**
-*   Copyright (c) 2006 Affymetrix, Inc.
-*
-*   Licensed under the Common Public License, Version 1.0 (the "License").
-*   A copy of the license must be included with any distribution of
-*   this source code.
-*   Distributions from Affymetrix, Inc., place this in the
-*   IGB_LICENSE.html file.
-*
-*   The license is also available at
-*   http://www.opensource.org/licenses/cpl.php
-*/
+ *   Copyright (c) 2006 Affymetrix, Inc.
+ *
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
 package com.affymetrix.igb.parsers;
 
 import java.io.*;
@@ -18,109 +18,183 @@ import java.util.regex.Pattern;
 
 import com.affymetrix.genometry.*;
 import com.affymetrix.igb.genometry.*;
+import com.affymetrix.igb.glyph.GraphGlyph;
+import com.affymetrix.igb.glyph.GraphState;
 import com.affymetrix.igb.util.GraphSymUtils;
-import com.affymetrix.igb.util.IntList;
-import com.affymetrix.igb.util.FloatList;
 
+/**
+ *  A parser for graph data in the UCSC browser Wiggle format.
+ *  See http://genome.ucsc.edu/google/goldenPath/help/wiggle.html
+ *  There are three sub-formats: BED4, VARSTEP, and FIXEDSTEP.
+ *  This parser reads the "Track" lines and applies some properties from
+ *  them, but ignores properties that don't easily apply to IGB.
+ */
 public class WiggleParser {
-
-  /**
-   *  Wiggle subformats:
-   *    BED4
-   *    VARSTEP
-   *    FIXEDSTEP.
-   */
-  static int UNKNOWN = 0;
-  static int BED4 = 1;
-  static int VARSTEP = 2;
-  static int FIXEDSTEP = 3;
-
+    
+  public static final int BED4 = 1;
+  public static final int VARSTEP = 2;
+  public static final int FIXEDSTEP = 3;
+  
   static Pattern field_regex = Pattern.compile("\\s+");  // one or more whitespace
-
+  
   TrackLineParser track_line_parser;
-
+  boolean ensure_unique_id = true;
+  
   public WiggleParser() {
     track_line_parser = new TrackLineParser();
   }
   
   /**
-   * NOR YET IMPLEMENTED.
-   *  Currently only reading one particular wiggle format:
-   *     variableStep two-column data; started by a declaration line and followed with
-   *       chromosome positions and data values:
-   *
-   *   variableStep  chrom=chrN  [span=windowSize}
-   *   chromStartA  dataValueA
-   *   chromStartB  dataValueB
+   *  Reads a Wiggle-formatted file using any combination of the three formats
+   *  @link{#BED4}, @link{#VARSTEP}, @link{#FIXEDSTEP}.
+   *  The format must be specified on the first line following a track line,
+   *  otherwise BED4 is assumed.
    */
   public List parse(InputStream istr, AnnotatedSeqGroup seq_group, boolean annotate_seq,
-                    String stream_name) throws IOException {
-    return parse(istr, seq_group, annotate_seq, stream_name, true);
-  }
-
-  public List parse(InputStream istr, AnnotatedSeqGroup seq_group, boolean annotate_seq,
-    String stream_name, boolean ensure_unique_id) throws IOException {
-
+      String stream_name) throws IOException {
+    
+    int current_format = BED4;
     List grafs = new ArrayList();
-    int current_format = UNKNOWN;
-    IntList xlist = null;
-    FloatList ylist = null;
-    String graph_id = stream_name;
-    String seqid = null;
-    Map graph_props = null;
+    WiggleData current_data = null;
+    Map current_datamap = null; // Map: seq_id -> WiggleData
+    
     BufferedReader br = new BufferedReader(new InputStreamReader(istr));
     String line;
-    while ((line = br.readLine()) != null) {
-      if (line.startsWith("#")) { continue; }
-      else if (line.startsWith("%")) { continue; }
+    while ((line = br.readLine()) != null && ! Thread.currentThread().isInterrupted()) {
+      // Generally should be "track" line, followed by optional "format" line
+      // (If there is no format line, BED4 format is assumed.)
+      
+      if (line.length() == 0) { continue; }
+      else if (line.startsWith("#")) { continue; } 
+      else if (line.startsWith("%")) { continue; } 
+      else if (line.startsWith("browser")) { continue; } 
       else if (line.startsWith("track")) {
-	track_line_parser.setTrackProperties(line, graph_id);
-	continue;
+        grafs.addAll(finishLine(seq_group, current_datamap, stream_name));
+        // finish previous graph(s) using previous track properties
+        
+        track_line_parser.parseTrackLine(line);
+                
+        current_format = BED4; // unless there is a format line next, assume BED4
+        current_data = null;
+        current_datamap = new HashMap(); // Map: seq_id -> WiggleData
+        continue;
+      } else if (line.startsWith("variableStep")) {
+        current_format = VARSTEP;
+        current_data = new VariableStepWiggleData(track_line_parser.getCurrentTrackHash(), line, seq_group);
+        current_datamap.put("", current_data);
+      } else if (line.startsWith("fixedStep")) {
+        current_format = FIXEDSTEP;
+        current_data = new FixedStepWiggleData(track_line_parser.getCurrentTrackHash(), line, seq_group);
+        current_datamap.put("", current_data);
       }
-      else if (line.startsWith("variableStep")) {
-	if (xlist != null && ylist != null) {
-	  if (ensure_unique_id) {
-	    BioSeq aseq = seq_group.getSeq(seqid);
-	    graph_id = GraphSymUtils.getUniqueGraphID(graph_id, aseq);
-	  }
-	  grafs.add(createGraph(seq_group, graph_id, graph_props, seqid, xlist, ylist));
-	}
-	String[] fields = field_regex.split(line);
-	for (int i=1; i<fields.length; i++) {
-	  if (fields[i].startsWith("chrom=")) {
-	    seqid = fields[i].substring(6);
-	    System.out.println("current seqid = " + seqid);
-	  }
-	}
-	current_format = VARSTEP;
-	xlist = new IntList();
-	ylist = new FloatList();
-      }
+      
+      // Else, it is a data line
       else {
-	String[] fields = field_regex.split(line);
+        String[] fields = field_regex.split(line.trim()); // trim() because lines are allowed to start with whitespace
+        
+        
+        if (current_format == VARSTEP) {
+          
+          current_data.xlist.add(Integer.parseInt(fields[0]));
+          current_data.ylist.add(Float.parseFloat(fields[1]));
+          
+        } else if (current_format == FIXEDSTEP) {
+          
+          current_data.ylist.add(Float.parseFloat(fields[0]));
+          
+        } else if (current_format == BED4) {
+          // chrom  start end value
+          String seq_id = fields[0];
+          
+          current_data = (WiggleData) current_datamap.get(seq_id);
+          if (current_data == null) {
+            current_data = new BedWiggleData(track_line_parser.getCurrentTrackHash(), seq_group, seq_id);
+            current_datamap.put(seq_id, current_data);
+          }
+          
+          int x1 = Integer.parseInt(fields[1]);
+          int x2 = Integer.parseInt(fields[2]);
+          int start = Math.min(x1, x2);
+          int width = Math.max(x1, x2) - start;
+          
+          current_data.xlist.add(x1);
+          current_data.wlist.add(width);
+          current_data.ylist.add(Float.parseFloat(fields[3]));
+          
+        } else {  // cannot happen
+          throw new RuntimeException("Format undefined");
+        }
       }
     }
-
+    
+    grafs.addAll(finishLine(seq_group, current_datamap, stream_name));
+    
     if (annotate_seq) {
       Iterator giter = grafs.iterator();
       while (giter.hasNext()) {
-	GraphSym graf = (GraphSym)giter.next();
-	MutableAnnotatedBioSeq seq = (MutableAnnotatedBioSeq)graf.getGraphSeq();
-	seq.addAnnotation(graf);
+        GraphSym graf = (GraphSym)giter.next();
+        MutableAnnotatedBioSeq seq = (MutableAnnotatedBioSeq)graf.getGraphSeq();
+        seq.addAnnotation(graf);
       }
     }
     return grafs;
   }
-
-
-  protected static GraphSym createGraph(AnnotatedSeqGroup seq_group, String gname, Map gprops, String seqid,
-					IntList xlist, FloatList ylist) {
-    BioSeq seq = seq_group.getSeq(seqid);
-    GraphSym gsym = new GraphSym(xlist.copyToArray(), ylist.copyToArray(), gname, seq);
-    // add props ??? NOT YET IMPLEMENTED
-    return gsym;
+  
+  static String parseFormatLine(String name, String format_line, String default_val) {
+    String val = default_val;
+    String[] fields = field_regex.split(format_line);
+    for (int i=1; i<fields.length; i++) {
+      if (fields[i].startsWith(name+"=")) {
+        val = fields[i].substring(name.length()+1);
+      }
+    }
+    return val;
   }
 
+
+  /** 
+   * Finishes the current data section and creates a list of GraphSym objects.
+   * (For the BED4 format, there can be multiple graphs in the list, for the 
+   *  other formats there will only be one.)
+   */
+  List finishLine(AnnotatedSeqGroup seq_group, Map m, String stream_name) {
+    if (m == null) {
+      return Collections.EMPTY_LIST;
+    }
+
+    List grafs = new ArrayList(m.size());
+    
+    Map track_hash = track_line_parser.getCurrentTrackHash();
+    String graph_id = (String) track_hash.get(TrackLineParser.NAME);
+    if (graph_id == null) {
+      graph_id = stream_name;
+    }
+    if (ensure_unique_id) {
+      graph_id = GraphSymUtils.getUniqueGraphID(graph_id, seq_group);
+    }
+    track_hash.put(TrackLineParser.NAME, graph_id);
+    
+    GraphState gstate = GraphState.getGraphState(graph_id);
+    track_line_parser.applyTrackProperties(track_hash, gstate);
+    
+    Iterator iter = m.keySet().iterator();
+    while (iter.hasNext()) {
+      String seq_id = (String) iter.next();
+      WiggleData wig = (WiggleData) m.get(seq_id);
+      GraphSym gsym = wig.createGraph(graph_id);
+  
+      gsym.getGraphState().setGraphStyle(GraphGlyph.BAR_GRAPH);
+      gsym.getGraphState().getTierStyle().setHumanName("Donkey");
+      
+      if (gsym != null) {
+        grafs.add(gsym);
+      }
+    }
+
+    return grafs;
+  }
+    
+  
   /**
    *  Currently only writing out one particular wiggle format. Specifically:
    *     variableStep two-column data; started by a declaration line and followed with
@@ -131,63 +205,30 @@ public class WiggleParser {
    *   chromStartB  dataValueB
    *</pre>
    */
-  public static boolean writeGraphs(java.util.Collection graphs, OutputStream outstream) {
+  public static boolean writeVariableStep(java.util.Collection graphs, OutputStream outstream) {
     try {
       OutputStreamWriter osw = new OutputStreamWriter(outstream);
       BufferedWriter bw = new BufferedWriter(osw);
       Iterator iter = graphs.iterator();
       while (iter.hasNext()) {
-	GraphSym graf = (GraphSym)iter.next();
-	String seqid = graf.getGraphSeq().getID();
-	String gname = graf.getGraphName();
-	bw.write("track type=wiggle_0 name=\"" + gname + "\"\n");
-	bw.write("variableStep\tchrom=" + seqid + "\n");
-	int pcount = graf.getPointCount();
-	int[] xcoords = graf.getGraphXCoords();
-	float[] ycoords = graf.getGraphYCoords();
-	for (int i=0; i<pcount; i++) {
-	  bw.write(Integer.toString(xcoords[i]));
-	  bw.write("\t");
-	  bw.write(Float.toString(ycoords[i]));
-	  bw.write("\n");
-	}
+        GraphSym graf = (GraphSym)iter.next();
+        String seqid = graf.getGraphSeq().getID();
+        String gname = graf.getGraphName();
+        bw.write("track type=wiggle_0 name=\"" + gname + "\"\n");
+        bw.write("variableStep\tchrom=" + seqid + "\n");
+        int pcount = graf.getPointCount();
+        int[] xcoords = graf.getGraphXCoords();
+        float[] ycoords = graf.getGraphYCoords();
+        for (int i=0; i<pcount; i++) {
+          bw.write(Integer.toString(xcoords[i]));
+          bw.write("\t");
+          bw.write(Float.toString(ycoords[i]));
+          bw.write("\n");
+        }
       }
-    }
-    catch (Exception ex) {
+    } catch (Exception ex) {
       ex.printStackTrace();
     }
     return true;
-  }
-
-  public static void main(String[] args) {
-    if (args.length < 2) {
-      System.err.println("Usage: WiggleParser in_file out_file [seqid]");
-      System.err.println("     (seqid is required if and only if input format is .gr)");
-      System.exit(1);
-    }
-    String in_file = args[0];
-    String out_file = args[1];
-    // read in_file using GraphSymUtils.readGraphs() ?  need to modify to handle gr (like readGraph())
-    AnnotatedSeqGroup seq_group = SingletonGenometryModel.getGenometryModel().addSeqGroup("Test Seq Group");
-
-    try {
-      InputStream istr = new FileInputStream(new File(in_file));
-      List gsyms = new ArrayList();
-      if (in_file.endsWith(".gr")) {
-	// NOT YET IMPLEMENTED
-	System.err.println("     Conversion of .gr files not yet implemented");
-      }
-      else {
-	gsyms = GraphSymUtils.readGraphs(istr, in_file, seq_group);
-      }
-      // write out_file using WiggleParser.writeGraphs();
-      System.out.println("writing out graphs in wiggle format: " + out_file);
-      OutputStream ostr = new FileOutputStream(new File(out_file));
-      WiggleParser.writeGraphs(gsyms, ostr);
-      ostr.close();
-      System.out.println("done writing out graphs");
-    }
-    catch (Exception ex) { ex.printStackTrace(); }
-  }
-
+  }  
 }

4dad7e25b70f8184e2422608eb8391d77b1239ae
gregghelt2 <gregghelt2@mycompany.com>
2006-12-20 14:39:39 +0000

    Added constructor that takes a global coordinates URI.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1843 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das2/Das2WritebackVersionedSource.java
+++ igb/src/com/affymetrix/igb/das2/Das2WritebackVersionedSource.java
@@ -24,6 +24,11 @@ public class Das2WritebackVersionedSource extends Das2VersionedSource  {
     super(das_source, vers_uri, name, href, description, init);
   }
 
+  public Das2WritebackVersionedSource(Das2Source das_source, URI vers_uri, URI coords_uri, String name,
+				      String href, String description, boolean init) {
+    super(das_source, vers_uri, coords_uri, name, href, description, init);
+  }
+
   public boolean writeAnnotations(ArrayList annots) {
     Das2Capability writecap = (Das2Capability)getCapability(WRITEBACK_CAP_QUERY);
     String write_url = writecap.getRootURI().toString();

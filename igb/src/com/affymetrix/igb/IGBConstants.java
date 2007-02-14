85ba41cb3c5a76784115ff120c6aff7b3c1fc78a
gregghelt2 <gregghelt2@mycompany.com>
2007-02-14 22:55:45 +0000

    Incrementing IGB version to 4.55 for release.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@1997 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/IGBConstants.java
+++ igb/src/com/affymetrix/igb/IGBConstants.java
@@ -23,5 +23,5 @@ public abstract class IGBConstants {
 
   public static String APP_NAME = "Integrated Genome Browser";
   public static String APP_SHORT_NAME = "IGB";
-  public static String IGB_VERSION = "4.52";
+  public static String IGB_VERSION = "4.55";
 }

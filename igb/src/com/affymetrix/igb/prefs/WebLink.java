61f40ad069f4946ebba659d1594656ffec9c2e5a
chyekk <chyekk@mycompany.com>
2007-03-19 17:35:35 +0000

    Implemented an equals() method.  Added a method getURLForSym() that can replace any "$$" in the base URL with an "id" or any other property value.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2050 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/prefs/WebLink.java
+++ igb/src/com/affymetrix/igb/prefs/WebLink.java
@@ -1,5 +1,5 @@
 /**
- *   Copyright (c) 2006 Affymetrix, Inc.
+ *   Copyright (c) 2006-2007 Affymetrix, Inc.
  *
  *   Licensed under the Common Public License, Version 1.0 (the "License").
  *   A copy of the license must be included with any distribution of
@@ -13,16 +13,25 @@
 
 package com.affymetrix.igb.prefs;
 
+import com.affymetrix.genometry.SeqSymmetry;
+import com.affymetrix.igb.genometry.SymWithProps;
 import com.affymetrix.igb.tiers.AnnotStyle;
+import com.affymetrix.swing.ListModelForLists;
+import java.net.URLEncoder;
 import java.util.*;
 import java.util.regex.*;
+import javax.swing.DefaultListModel;
+import javax.swing.ListModel;
 
 public class WebLink {
   String regex = null;
   String url = null;
   String name = null;
+  String id_field_name = null; // null implies use getId(); "xxx" means use getProperty("xxx");
+
   Pattern pattern = null;
-  
+  static DefaultListModel weblink_list = new DefaultListModel();
+    
   public WebLink() {
   }
   
@@ -31,18 +40,49 @@ public class WebLink {
     setName(name);
     setRegex(regex);
     setUrl(url);
-  }  
+  }
 
-  static List weblink_list = new ArrayList();
+  boolean equals(String s1, String s2) {
+    return ( s1 == s2 || (s1 != null && s1.equals(s2)));
+  }
   
+  public boolean equals(Object o) {
+    if (o instanceof WebLink) {
+      WebLink w = (WebLink) o;
+      return (equals(name, w.name) && equals(regex, w.regex) 
+       && equals(url, w.url) && equals(id_field_name, w.id_field_name));
+    }
+    return false;
+  }
+  
+  public int hashCode() {
+    int hash = 1;
+    if (name != null) { hash = 31*hash + name.hashCode(); }
+    if (regex != null) { hash = 31*hash + regex.hashCode(); }
+    if (url != null) { hash = 31*hash + url.hashCode(); }
+    if (id_field_name != null) { hash = 31*hash + id_field_name.hashCode(); }
+    return hash;
+  }
+    
   /**
    *  A a WebLink to the static list.  Multiple WebLink's with the same 
    *  regular expressions are allowed, as long as they have different URLs.
    */
   public static void addWebLink(WebLink wl) {
-    weblink_list.add(wl);
+    if ( weblink_list.contains(wl) ) {
+      System.out.println("Not adding duplicate web link for regex: '" + wl.regex + "'");
+    } else {
+      weblink_list.addElement(wl);
+    }
   }
 
+  /**
+   *  Remove a WebLink from the static list.
+   */
+  public static void removeWebLink(WebLink wl) {
+    weblink_list.removeElement(wl);
+  }
+  
   /** Get all web-link patterns for the given method name.
    *  These can come from regular-expression matching from the semi-obsolete
    *  XML-based preferences file, or from UCSC-style track lines in the
@@ -63,9 +103,9 @@ public class WebLink {
 
     if (method != null) {
       // This is not terribly fast, but it is not called in places where speed matters
-      Iterator iter = weblink_list.iterator();
-      while (iter.hasNext()) {
-        WebLink link = (WebLink) iter.next();
+      Enumeration en = weblink_list.elements();
+      while (en.hasMoreElements()) {
+        WebLink link = (WebLink) en.nextElement();
         if (link.matches(method) && link.url != null) {
           results.add(link);
         }
@@ -75,7 +115,11 @@ public class WebLink {
     return (WebLink[]) results.toArray(new WebLink[results.size()]);
   }
   
-  
+  /** Returns a ListModel backed by the list of WebLink items. */
+  public static ListModel getWebLinkListModel() {
+    return weblink_list;
+  }
+    
   public String getName() {
     return this.name;
   }
@@ -103,6 +147,7 @@ public class WebLink {
     }
   }
   
+  /** Return the compiled form of the regular expression. */
   public Pattern getPattern() {
     return pattern;
   }
@@ -110,6 +155,7 @@ public class WebLink {
   /** Returns the URL (or URL pattern) associated with this WebLink.
    *  If the URL pattern contains any "$$" characters, those should be
    *  replaced with URL-Encoded annotation IDs to get the final URL.
+   *  Better to use {@link #getUrlForSym(SeqSymmetry)}.
    */
   public String getUrl() {
     return this.url;
@@ -127,7 +173,44 @@ public class WebLink {
     }
   }
   
+  /** A pattern that matches the string "$$". */
+  static final Pattern DOUBLE_DOLLAR_PATTERN = Pattern.compile("[$][$]");
+  
+  public static String replacePlaceholderWithId(String url, String id) {
+    // Now replace all "$$" in the url pattern with the given id, URLEncoded
+    if (url != null && id != null) {
+      String encoded_id = URLEncoder.encode(id);
+      url = DOUBLE_DOLLAR_PATTERN.matcher(url).replaceAll(encoded_id);
+    }
+    return url;
+  }
+  
+  public String getURLForSym(SeqSymmetry sym) {
+    // Currently this just replaces any "$$" with the ID, but it could
+    // do something more sophisticated later, like replace "$$" with
+    // some other sym property.
+    if (id_field_name == null) {
+      return replacePlaceholderWithId(getUrl(), sym.getID());
+    }
+    
+    Object field_value = null;
+    if (id_field_name != null && sym instanceof SymWithProps) {
+      field_value = ((SymWithProps) sym).getProperty(id_field_name);
+    }
+    if (field_value == null) {
+      System.out.println("WARNING: Selected item has no value for property '"+id_field_name+
+          "' which is needed to construct the web link.");
+      return replacePlaceholderWithId(getUrl(), "");
+    }
+    else {
+      return replacePlaceholderWithId(getUrl(), field_value.toString());
+    }
+  }
+  
   public String toString() {
-    return "WebLink: name='"+name+"' pattern='"+pattern+"'  url='"+url+"'";
+    return "WebLink: name=" + name + 
+        ", regex=" + regex + 
+        ", url=" + url +
+        ", id_field_name=" + id_field_name;
   }
 }

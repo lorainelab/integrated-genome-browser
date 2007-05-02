2aee529ad366a75e9b328ec639f4923dedf40879
chyekk <chyekk@mycompany.com>
2007-05-02 16:53:10 +0000

    Just testing CVS tags.  Ignore.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2183 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/genometry/IntId.java
+++ igb/src/com/affymetrix/igb/genometry/IntId.java
@@ -19,6 +19,9 @@ public interface IntId {
   // $State$
 
   // $Log$
+  // Revision 1.3  2007/05/02 16:49:17  chyekk
+  // Just testing CVS tags.  Ignore.
+  //
   // Revision 1.2  2007/05/02 16:48:07  chyekk
   // Just testing CVS tags.  Ignore.
   //
--- igb/test/com/affymetrix/igb/glyph/GenericAnnotGlyphFactoryTest.java
+++ igb/test/com/affymetrix/igb/glyph/GenericAnnotGlyphFactoryTest.java
@@ -134,20 +134,4 @@ public class GenericAnnotGlyphFactoryTest extends TestCase {
       
     }    
   }
-  
-  // $Author$
-  // $Date$
-  // $Header$
-  // $ID$
-  // $Locker$
-  // $Name$
-  // $RCSfile$
-  // $Revision$
-  // $Source$
-  // $State$
-
-  // $Log$
-  // Revision 1.2  2007/05/02 16:44:57  chyekk
-  // Just testing CVS tags.  Ignore.
-  //
 }

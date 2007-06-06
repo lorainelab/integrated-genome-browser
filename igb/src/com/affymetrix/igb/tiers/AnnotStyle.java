a588a1147f4a62565c79cc7d2953325deb777d66
gregghelt2 <gregghelt2@mycompany.com>
2007-06-06 20:07:05 +0000

    Flagged potential NullPointerException bug in AnnotStyle.getInstance(), with proposed workaround, but leaving as is for now.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2289 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/tiers/AnnotStyle.java
+++ igb/src/com/affymetrix/igb/tiers/AnnotStyle.java
@@ -21,7 +21,7 @@ import com.affymetrix.igb.glyph.HeatMap;
 import com.affymetrix.igb.util.UnibrowPrefsUtil;
 
 public class AnnotStyle implements IAnnotStyle {
-  
+
   static Preferences tiers_root_node = UnibrowPrefsUtil.getTopNode().node("tiers");
 
   // A pattern that matches two or more slash "/" characters.
@@ -29,7 +29,7 @@ public class AnnotStyle implements IAnnotStyle {
   static final Pattern multiple_slashes = Pattern.compile("/{2,}");
 
   static public final String NAME_OF_DEFAULT_INSTANCE = "* DEFAULT *";
-  
+
   // The String constants named PREF_* are for use in the persistent preferences
   // They are not displayed to users, and should never change
   //static final String PREF_SHOW = "Show";
@@ -55,10 +55,10 @@ public class AnnotStyle implements IAnnotStyle {
   static final double default_height = 20.0;
   static final double default_y = 0.0;
   public static final int MAX_MAX_DEPTH = Integer.MAX_VALUE;
-  
+
   // whether to create and use a java Preferences node object for this instance
   boolean is_persistent = true;
-  
+
   boolean show = default_show;
   boolean separate = default_separate;
   boolean collapsed = default_collapsed;
@@ -72,21 +72,23 @@ public class AnnotStyle implements IAnnotStyle {
   double y = default_y;
   String url = null;
 
-  boolean color_by_score = false;  
+  boolean color_by_score = false;
   HeatMap custom_heatmap = null;
-    
+
   String unique_name;
   String human_name;
-  
+
   Preferences node;
 
   static Map static_map = new LinkedHashMap();
-  
+
   public static AnnotStyle getInstance(String unique_name) {
     return getInstance(unique_name, true);
   }
-  
+
   public static AnnotStyle getInstance(String unique_name, boolean persistent) {
+      // if unique_name is null, maybe assign it "unknown" to avoid exceptions
+      // if (unique_name == null)  { unique_name = "unknown";}
     AnnotStyle style = (AnnotStyle) static_map.get(unique_name.toLowerCase());
     if (style == null) {
       AnnotStyle template = getDefaultInstance();
@@ -100,9 +102,9 @@ public class AnnotStyle implements IAnnotStyle {
   public static java.util.List getAllLoadedInstances() {
     return new ArrayList(static_map.values());
   }
-  
+
   /** If there is no AnnotStyle with the given name, just returns the given name;
-   * else modifies the name such that there are no instances that are currently 
+   * else modifies the name such that there are no instances that are currently
    * using it.
    */
   public static String getUniqueName(String name) {
@@ -112,22 +114,22 @@ public class AnnotStyle implements IAnnotStyle {
     }
     return result;
   }
-  
+
   /** Gets the parent preference node in which all the tier preferences are stored. */
   public static Preferences getTiersRootNode() {
     return tiers_root_node;
   }
-  
+
   protected AnnotStyle() {
   }
-    
+
   /** Creates an instance associated with a case-insensitive form of the unique name.
    */
   protected AnnotStyle(String name, boolean is_persistent, AnnotStyle template) {
     this.human_name = name; // this is the default human name, and is not lower case
     this.unique_name = name.toLowerCase();
     this.is_persistent = is_persistent;
-    
+
     if (is_persistent) {
       if (unique_name.endsWith("/")) {
         unique_name = unique_name.substring(0, unique_name.length() -1);
@@ -138,7 +140,7 @@ public class AnnotStyle implements IAnnotStyle {
         //System.out.println("Preferences node name trimmed to '"+unique_name+"'");
       }
     }
-        
+
     if (template != null) {
       initFromTemplate(template);
     }
@@ -159,11 +161,11 @@ public class AnnotStyle implements IAnnotStyle {
       node = null;
     }
   }
-  
+
   // Apply a few hard-coded defaults
   // This is a hack, but helps ease the transition away from the igb_default_prefs.xml file
   void applyHardCodedDefaults() {
-    if ("contig".equals(unique_name) || "contigs".equals(unique_name) 
+    if ("contig".equals(unique_name) || "contigs".equals(unique_name)
         || "repeats".equals(unique_name) || "repeat".equals(unique_name)
         || "encode regions".equals(unique_name) || "encoderegions".equals(unique_name) || "encode".equals(unique_name)) {
       this.glyph_depth = 1;
@@ -172,7 +174,7 @@ public class AnnotStyle implements IAnnotStyle {
       this.label_field = "gene_name";
     }
   }
-  
+
   // Copies properties from the given node, using the currently-loaded values as defaults.
   // generally call initFromTemplate before this.
   // Make sure to set human_name to some default before calling this.
@@ -180,44 +182,44 @@ public class AnnotStyle implements IAnnotStyle {
   void initFromNode(Preferences node) {
     human_name = node.get(PREF_HUMAN_NAME, this.human_name);
     //factory_instance = null;
-    
+
     separate = node.getBoolean(PREF_SEPARATE, this.getSeparate());
     //show = node.getBoolean(PREF_SHOW, this.getShow());
     collapsed = node.getBoolean(PREF_COLLAPSED, this.getCollapsed());
     max_depth = node.getInt(PREF_MAX_DEPTH, this.getMaxDepth());
     color = UnibrowPrefsUtil.getColor(node, PREF_COLOR, this.getColor());
     background = UnibrowPrefsUtil.getColor(node, PREF_BACKGROUND, this.getBackground());
-  
+
     label_field = node.get(PREF_LABEL_FIELD, this.getLabelField());
     glyph_depth = node.getInt(PREF_GLYPH_DEPTH, this.getGlyphDepth());
   }
-  
+
   // Copies properties from the template into this object, but does NOT persist
   // these copied values.
   // human_name and factory_instance are not modified
   void initFromTemplate(AnnotStyle template) {
     //human_name = this.unique_name;
     //factory_instance = null;
-    
+
     separate = template.getSeparate();
     show = template.getShow();
     collapsed = template.getCollapsed();
     max_depth = template.getMaxDepth();
     color = template.getColor();
     background = template.getBackground();
-  
+
     label_field = template.getLabelField();
     glyph_depth = template.getGlyphDepth();
   }
-  
+
   // Returns the preferences node, or null if this is a non-persistent instance.
   Preferences getNode() {
     return this.node;
     // return (is_persistent ? tiers_root_node.node(this.unique_name) : null);
   }
-    
+
   static AnnotStyle default_instance = null;
-  
+
   /* Gets an instance that can be used for holding
    *  default values.  The default instance is used as a template in creating
    *  new instances.  (Although not ALL properties of the default instance are used
@@ -235,7 +237,7 @@ public class AnnotStyle implements IAnnotStyle {
     }
     return default_instance;
   }
-  
+
   public String getUniqueName() {
     return unique_name;
   }
@@ -250,60 +252,60 @@ public class AnnotStyle implements IAnnotStyle {
     }
     return this.human_name;
   }
-  
+
   public void setHumanName(String human_name) {
     this.human_name = human_name;
     if (getNode() != null) {
       getNode().put(PREF_HUMAN_NAME, human_name);
     }
   }
-  
+
   /** Whether the tier is shown or hidden. */
   public boolean getShow() {
     return show;
   }
-  
+
   /** Sets whether the tier is shown or hidden; this is a non-persistent setting. */
   public void setShow(boolean b) {
     this.show = b;
   }
-  
-  
+
+
   /** Whether PLUS and MINUS strand should be in separate tiers. */
   public boolean getSeparate() {
     return separate;
   }
-  
+
   public void setSeparate(boolean b) {
     this.separate = b;
     if (getNode() != null) {
       getNode().putBoolean(PREF_SEPARATE, b);
     }
   }
-   
+
   public boolean getCustomizable() {
     return customizable;
   }
-  
+
   /** Whether tier is collapsed. */
   public boolean getCollapsed() {
     return collapsed;
   }
-  
+
   public void setCollapsed(boolean b) {
     this.collapsed = b;
     if (getNode() != null) {
       getNode().putBoolean(PREF_COLLAPSED, b);
     }
   }
-  
+
   /** Maximum number of rows of annotations for this tier. */
   public int getMaxDepth() {
     return max_depth;
   }
-  
-  /** Set the maximum number of rows of annotations for this tier. 
-   *  Any attempt to set this less than zero or larger than MAX_MAX_DEPTH will 
+
+  /** Set the maximum number of rows of annotations for this tier.
+   *  Any attempt to set this less than zero or larger than MAX_MAX_DEPTH will
    *  fail, the value will be truncated to fit the range.
    *  @param max  a number between 0 and {@link #MAX_MAX_DEPTH}.
    */
@@ -315,7 +317,7 @@ public class AnnotStyle implements IAnnotStyle {
       getNode().putInt(PREF_MAX_DEPTH, max);
     }
   }
-    
+
   /** The color of annotations in the tier. */
   public Color getColor() {
     return color;
@@ -331,12 +333,12 @@ public class AnnotStyle implements IAnnotStyle {
       UnibrowPrefsUtil.putColor(getNode(), PREF_COLOR, c);
     }
   }
-  
+
   /** The color of the tier Background. */
   public Color getBackground() {
     return background;
   }
-  
+
   public void setBackground(Color c) {
     if (c != this.background) {
       custom_heatmap = null;
@@ -347,15 +349,15 @@ public class AnnotStyle implements IAnnotStyle {
       UnibrowPrefsUtil.putColor(getNode(), PREF_BACKGROUND, c);
     }
   }
-  
-  
-  /** Returns the field name from which the glyph labels should be taken. 
+
+
+  /** Returns the field name from which the glyph labels should be taken.
    *  This will never return null, but will return "" instead.
    */
   public String getLabelField() {
     return label_field;
   }
-  
+
   public void setLabelField(String l) {
     if (l == null || l.trim().length() == 0) { l = ""; }
     label_field = l;
@@ -363,18 +365,18 @@ public class AnnotStyle implements IAnnotStyle {
       getNode().put(PREF_LABEL_FIELD, l);
     }
   }
-  
+
   public int getGlyphDepth() {
     return glyph_depth;
   }
-  
+
   public void setGlyphDepth(int i) {
     glyph_depth = i;
     if (getNode() != null) {
       getNode().putInt(PREF_GLYPH_DEPTH, i);
     }
   }
-  
+
   public double getHeight() {
     return height;
   }
@@ -388,14 +390,14 @@ public class AnnotStyle implements IAnnotStyle {
 
   /** Currently not used, but could be used to remember tier positions. */
   public void setY(double y) {
-    this.y = y; 
+    this.y = y;
   }
 
   /** Currently not used, but could be used to remember tier positions. */
-  public double getY() { 
-    return y; 
+  public double getY() {
+    return y;
   }
-  
+
   /** A non-persistent property.  Usually set by UCSC browser "track" lines. */
   public void setUrl(String url) {
     this.url = url;
@@ -405,11 +407,11 @@ public class AnnotStyle implements IAnnotStyle {
   public String getUrl() {
     return this.url;
   }
-  
+
   public boolean getPersistent() {
     return (is_persistent && getNode() != null);
   }
-  
+
   public boolean getExpandable() {
     return expandable;
   }
@@ -420,7 +422,7 @@ public class AnnotStyle implements IAnnotStyle {
     // annotation tiers, only for graph tiers, which don't use this class
     expandable = b;
   }
-  
+
   /** Always returns false.  This class is only intended for annotation tiers,
    *  not graph tiers.
    */
@@ -429,35 +431,35 @@ public class AnnotStyle implements IAnnotStyle {
   }
 
   Map transient_properties;
-  
+
   public Map getTransientPropertyMap() {
     if (transient_properties == null) {
       transient_properties = new HashMap();
     }
     return transient_properties;
   }
-  
+
   /**
-   *  Key for the transient property map. Indicates whether the annotations 
+   *  Key for the transient property map. Indicates whether the annotations
    *  might possibly have scores that could be used for coloring.
    *  Valid values are "true" and "false".
    */
   public static final String PROP_HAS_SCORES = "HAS_SCORES";
-  
+
   /**
    *  Indicates whether the scores of the annotations should be marked by colors.
    */
   public void setColorByScore(boolean b) {
     color_by_score = b;
   }
-  
+
   /**
    *  Indicates whether the scores of the annotations should be marked by colors.
    */
   public boolean getColorByScore() {
     return color_by_score;
   }
-  
+
   /**
    *  Returns a HeatMap that interpolates between colors based on
    *  getColor() and getBackgroundColor().  The color at the low
@@ -473,7 +475,7 @@ public class AnnotStyle implements IAnnotStyle {
     }
     return custom_heatmap;
   }
-  
+
   /**
    *  Returns a color that can be used to indicate a score between 1 and 1000.
    *  This will return a color even if getColorByScore() is false.
@@ -491,15 +493,15 @@ public class AnnotStyle implements IAnnotStyle {
 
     return getCustomHeatMap().getColors()[ index ];
   }
-    
+
   /* (a possibility for later.... EEE)
   Set source_urls = new TreeSet();
-  
+
   public Set getSourceURLs() {
     return source_urls;
   }
   */
-  
+
   public void copyPropertiesFrom(IAnnotStyle g) {
     setColor(g.getColor());
     setShow(g.getShow());
@@ -509,7 +511,7 @@ public class AnnotStyle implements IAnnotStyle {
     setMaxDepth(g.getMaxDepth());
     setHeight(g.getHeight());
     setY(g.getY());
-    
+
     setExpandable(g.getExpandable());
     if (g instanceof AnnotStyle) {
       AnnotStyle as = (AnnotStyle) g;
@@ -519,7 +521,7 @@ public class AnnotStyle implements IAnnotStyle {
       setGlyphDepth(as.getGlyphDepth());
       setSeparate(as.getSeparate());
     }
-    
+
     getTransientPropertyMap().putAll(g.getTransientPropertyMap());
   }
 

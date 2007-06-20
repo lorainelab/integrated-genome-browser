93e559e19c4658ff5f60d0fd0eff29db2ff0a649
chyekk <chyekk@mycompany.com>
2007-06-20 18:38:22 +0000

    Simplified by making this inherit from DefaultIAnnotStyle.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2387 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/tiers/SimpleAnnotStyle.java
+++ igb/src/com/affymetrix/igb/tiers/SimpleAnnotStyle.java
@@ -13,24 +13,16 @@
 
 package com.affymetrix.igb.tiers;
 
-import java.awt.Color;
-import java.util.HashMap;
-import java.util.Map;
-
-public class SimpleAnnotStyle implements IAnnotStyleExtended {
-  String uName; // Unique name.  Can be read, but not reset.
+public class SimpleAnnotStyle extends DefaultIAnnotStyle implements IAnnotStyleExtended {
 
   public SimpleAnnotStyle(String name) {
-    this.uName = name;
+    super(name, false);
   }
   
   public SimpleAnnotStyle(String name, boolean is_graph) {
-    this.uName = name;
-    this.is_graph = is_graph;
+    super(name, is_graph);
   }
 
-  public String getUniqueName() { return uName; }
-  
   String url;
   public void setUrl(String url) {this.url = url;}
   public String getUrl() { return url; }
@@ -42,68 +34,14 @@ public class SimpleAnnotStyle implements IAnnotStyleExtended {
   int depth=2;
   public void setGlyphDepth(int i) {this.depth = i;}
   public int getGlyphDepth() {return this.depth;}
-  
-  Color c;
-  public Color getColor() { return this.c;}
-  public void setColor(Color c) { this.c = c; }
-  
-  boolean show;
-  public boolean getShow() {return show;}
-  public void setShow(boolean b) {this.show = show;}
-    
-  String humanName;
-  public String getHumanName() { return humanName; }
-  public void setHumanName(String s) { this.humanName = s; }
-  
-  Color bg;
-  public Color getBackground() { return bg; }
-  public void setBackground(Color c) { this.bg = c; }
-  
-  boolean collapsed;
-  public boolean getCollapsed() { return collapsed; }
-  public void setCollapsed(boolean b) { collapsed = b; }
-  
-  int maxDepth = 10;
-  public int getMaxDepth() { return maxDepth; }
-  public void setMaxDepth(int m) { maxDepth = m; }
-  
-  double height;
-  public void setHeight(double h) { height = h; }
-  public double getHeight() { return height; }
-  
-  double y;
-  public void setY(double y) { this.y = y; }
-  public double getY() { return y; }
-  
-  boolean expandable;
-  public boolean getExpandable() { return expandable; }
-  public void setExpandable(boolean b) { expandable = b; }
-  
-  boolean is_graph = false;
-  public boolean isGraphTier() { return is_graph; }
-  
-  Map transientProperties = new HashMap();
-  public Map getTransientPropertyMap() {
-    return transientProperties;
-  }
-  
+
   public void copyPropertiesFrom(IAnnotStyle g) {
-    setColor(g.getColor());
-    setShow(g.getShow());
-    setHumanName(g.getHumanName());
-    setBackground(g.getBackground());
-    setCollapsed(g.getCollapsed());
-    setMaxDepth(g.getMaxDepth());
-    setHeight(g.getHeight());
-    setY(g.getY());
-    setExpandable(g.getExpandable());
-    
+    super.copyPropertiesFrom(g);  
     if (g instanceof IAnnotStyleExtended) {
       IAnnotStyleExtended as = (IAnnotStyleExtended) g;
+      setUrl(as.getUrl());
       setColorByScore(as.getColorByScore());
       setGlyphDepth(as.getGlyphDepth());
     }
-
-    getTransientPropertyMap().putAll(g.getTransientPropertyMap());
   }
 }

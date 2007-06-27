46dff973814bd9c1b715ab59de2a6736d327624e
chyekk <chyekk@mycompany.com>
2007-06-27 17:24:38 +0000

    The genometryImpl package contains an implementation of genometry interfaces, plus some extra stuff that allows it to be compiled independantly of genoviz_SDK and independent of IGB.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2424 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ igb/src/com/affymetrix/genometryImpl/GFF3Sym.java
@@ -0,0 +1,338 @@
+/**
+ *   Copyright (c) 2006 Affymetrix, Inc.
+ *
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
+package com.affymetrix.genometryImpl;
+
+import com.affymetrix.genometryImpl.UcscGffSym;
+import com.affymetrix.genometryImpl.SingletonSymWithProps;
+import com.affymetrix.genometryImpl.Scored;
+import com.affymetrix.genometryImpl.parsers.GFF3Parser;
+import com.affymetrix.genometry.*;
+
+import java.net.URLDecoder;
+import java.util.*;
+import java.util.regex.*;
+
+/**
+ *  A sym to efficiently store GFF version 3 annotations.
+ *
+ *  See http://song.sourceforge.net/gff3.shtml
+ */
+public class GFF3Sym extends SingletonSymWithProps implements Scored { 
+
+  public static final char UNKNOWN_FRAME = UcscGffSym.UNKNOWN_FRAME;
+  public static final String UNKNOWN_SOURCE = ".";
+  
+  // Assuming that these feature types are not case-sensitive
+  public static final String FEATURE_TYPE_GENE = "gene";
+  public static final String FEATURE_TYPE_MRNA = "mrna";
+  public static final String FEATURE_TYPE_EXON = "exon";
+  public static final String FEATURE_TYPE_CDS = "cds";
+    
+  // Assuming that these ontology types are not case-sensitive
+  public static final String SOFA_GENE = "SO:0000704";
+  public static final String SOFA_MRNA = "SO:0000234";
+  public static final String SOFA_EXON = "SO:0000147";
+  public static final String SOFA_CDS = "SO:0000316";
+  
+  String source;
+  String method;
+  public String feature_type;
+  float score;
+  char frame;
+  String attributes;
+
+  /**
+   * Constructor.
+   * The coordinates should be given exactly as they appear in a GFF file.
+   * In principle, the first coordinate is supposed to be less than the second one,
+   * but in practice this isn't always followed, so this constructor will correct
+   * those errors and will also convert from base-1 to interbase-0 coordinates.
+   * @param a  The coordinate in column 4 of the GFF file.
+   * @param b  The coordinate in column 5 of the GFF file.
+   * @param attributes   Attributes, formatted in GFF3 style.
+   */
+  public GFF3Sym(BioSeq seq, String source, String feature_type, int a, int b, 
+                    float score, char strand, char frame, String attributes) {
+    super(0, 0, seq);
+    
+    // GFF spec says coord_A <= coord_B, but this is not always obeyed
+    int max = Math.max(a, b);
+    int min = Math.min(a, b);
+    // convert from base-1 numbering to interbase-0 numbering
+    min--;
+    
+    if (strand == '-') {
+      setCoords(max, min);
+    } else {
+      setCoords(min, max);
+    }
+    
+    if (! UNKNOWN_SOURCE.equals(source)) {
+      this.source = source;
+    } else {
+      this.source = UNKNOWN_SOURCE;  // Basically equivalent to this.source = source.intern()
+    }
+    this.method = null;
+    this.feature_type = feature_type;
+    this.score = score;
+    this.frame = frame;
+    this.attributes = attributes;
+    
+    // in GFF3, the property "ID" is intended to have meaning only inside the file itself.
+    // the property "Name" is more like what we think of as an ID in Genometry
+    String[] possible_names = getGFF3PropertyFromAttributes(GFF3Parser.GFF3_NAME, attributes);
+    if (possible_names.length > 0) {
+      this.id = possible_names[0];
+    } else {
+      this.id = null;
+    }
+  }
+  
+  public String getID() { 
+    // This is overridden because we only want to check the value of this.id,
+    // we do NOT want to check for a property named "id".  This is because GFF3
+    // has a very different notion of what an ID is.  In GFF3 and "ID", in upper case,
+    // only has meaning while processing the file and should be ignored later.
+    return this.id; 
+  }
+  public String getSource()  { return source; }
+  public String getFeatureType()  { return feature_type; }
+  public float getScore()  { return score; }
+  public char getFrame()  { return frame; }
+  public String getAttributes() { return attributes; }
+  
+  public Object getProperty(String name) {
+    if (name.equals("source") && source != null) { return source; }
+    else if (name.equals("method")) { return method; }
+    else if (name.equals("feature_type") || name.equals("type")) { return feature_type; }
+    else if (name.equals("score") && score != UNKNOWN_SCORE) { return new Float(score); }
+    else if (name.equals("frame") && frame != UNKNOWN_FRAME) { return new Character(frame); }
+    else if (name.equals("id")) {
+      return getID(); 
+    }
+    String[] temp = getGFF3PropertyFromAttributes(name, attributes);
+    if (temp.length == 0) {
+      return null;
+    } else if (temp.length == 1) {
+      return temp[0];
+    } else {
+      return temp;
+    }
+  }
+  
+  static final List bad_prop_names = Arrays.asList(new String[] {
+    "feature_type", "type", "score", "frame"
+  });
+  
+  /** 
+   *  Overriden such that certain properties will be stored more efficiently.
+   *  Setting certain properties this way is not supported:
+   *  these include "attributes", "score" and "frame".
+   */
+  public boolean setProperty(String name, Object val) {
+    String lc_name = name.toLowerCase();
+    if (name.equals("id")) {
+      if (val instanceof String) {
+        id = (String) val;
+        return true;
+      }
+      else {
+        //id = null;
+        return false;
+      }
+    }
+    if (name.equals("source")) {
+      if (val instanceof String) {
+        source = (String) val;
+        return true;
+      }
+      else {
+        //source = null; 
+        return false;
+      }
+    }
+    if (name.equals("method")) {
+      if (val instanceof String) {
+        method = (String) val;
+        return true;
+      }
+      else {
+        //method = null; 
+        return false;
+      }
+    }
+    else if (bad_prop_names.contains(lc_name)) {
+      // May need to handle these later, but it is unlikely to be an issue
+      throw new IllegalArgumentException("Currently can't modify property '" + name +"' via setProperty");
+    }
+
+    return super.setProperty(name, val);
+  }
+
+  public Map getProperties() {
+    return cloneProperties();
+  }
+
+  public Map cloneProperties() {
+    Map tprops = super.cloneProperties();
+    if (tprops == null) {
+      tprops = new HashMap();
+    }
+    if (getID() != null) {
+      tprops.put("id", getID());
+    }
+    if (source != null) { 
+      tprops.put("source", source);
+    }
+    if (method != null) {
+      tprops.put("method", method); 
+    }
+    if (feature_type != null) { 
+      tprops.put("feature_type", feature_type); 
+      tprops.put("type", feature_type); 
+    }
+    if (score != UNKNOWN_SCORE) {
+      tprops.put("score", new Float(getScore()));
+    }
+    if (frame != UNKNOWN_FRAME) {
+      tprops.put("frame", new Character(frame));
+    }
+    addAllAttributesFromGFF3(tprops, attributes);
+    
+    return tprops;
+  }
+
+  /** Returns the property GFF3Parser.GFF3_ID from the attributes. 
+   *  This will be a single String or null.  This ID is intended to be used
+   *  during processing of the GFF3 file, and has no meaning outside the file.
+   */
+  public static String getIdFromGFF3Attributes(String attributes) {
+    String[] possible_ids = getGFF3PropertyFromAttributes(GFF3Parser.GFF3_ID, attributes);
+    if (possible_ids.length == 0) {
+      return null;
+    } else {
+      return possible_ids[0];
+    }
+  }
+  
+  static final Pattern equalsP = Pattern.compile("=");
+  static final Pattern commaP = Pattern.compile(",");
+  
+  public static void addAllAttributesFromGFF3(Map m, String attributes) {
+    if (attributes == null) {
+      return;
+    }
+    
+    String[] tag_vals = attributes.split(";");
+    
+    for (int i=0; i<tag_vals.length; i++) {
+      if ("".equals(tag_vals[i])) {
+        continue;
+      }
+      String[] tag_and_vals = equalsP.split(tag_vals[i], 2);
+      if (tag_and_vals.length == 2) {
+        String[] vals = commaP.split(tag_and_vals[1]);
+        for (int j=0; j<vals.length; j++) {
+          vals[j] = URLDecoder.decode(vals[j]);
+        }
+        if (vals.length == 1) { // put a single String
+          m.put(tag_and_vals[0], vals[0]);
+        } else { // put a String array
+          m.put(tag_and_vals[0], vals);
+        }
+      }
+    }
+  }
+  
+  static final String[] EMPTY_RESULT = new String[0];
+  
+  /** Returns a non-null String[]. */
+  public static String[] getGFF3PropertyFromAttributes(String prop_name, String attributes) {
+    if (attributes == null) {
+      return EMPTY_RESULT;
+    }
+    String[] tag_vals = attributes.split(";");
+    String prop_with_equals = prop_name + "=";
+    String val = null;
+    
+    for (int i=0; i<tag_vals.length; i++) {
+      if (tag_vals[i].startsWith(prop_with_equals)) {
+        val = tag_vals[i].substring(prop_with_equals.length());
+        break;
+      }
+    }
+    if (val == null) {
+      return EMPTY_RESULT;
+    }
+    String[] results = val.split(",");
+    for (int i=0; i<results.length; i++) {
+      results[i] = URLDecoder.decode(results[i]);
+    }
+    return results;
+  }
+  
+  /**
+   *  Converts feature types that IGB understands into one of the constant strings:
+   *  {@link #FEATURE_TYPE_GENE}, etc.  Invalid ones, are simply interned.
+   */
+  public static String normalizeFeatureType(String s) {
+    
+    if (FEATURE_TYPE_GENE.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_GENE;
+    }
+    if (FEATURE_TYPE_EXON.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_EXON;
+    }
+    if (FEATURE_TYPE_MRNA.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_MRNA;
+    }
+    if (FEATURE_TYPE_CDS.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_CDS;
+    }
+        
+    if (SOFA_GENE.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_GENE;
+    }
+    if (SOFA_EXON.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_EXON;
+    }
+    if (SOFA_MRNA.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_MRNA;
+    }
+    if (SOFA_CDS.equalsIgnoreCase(s)) {
+      return FEATURE_TYPE_CDS;
+    }
+   
+    return s.intern();
+  }
+  
+  public String toString() {
+    return "GFF3Sym: ID = '" + getProperty(GFF3Parser.GFF3_ID) + "'  type=" + feature_type
+        + " children=" + getChildCount();
+  }
+  
+  public boolean isMultiLine() {
+    return false;
+  }
+  
+  public static class MultiLineGFF3Sym extends GFF3Sym {
+    public MultiLineGFF3Sym(BioSeq seq, String source, String feature_type, int a, int b, 
+                    float score, char strand, char frame, String attributes) {
+      super(seq, source, feature_type, a, b, score, strand, frame, attributes);
+    }
+
+    public boolean isMultiLine() {
+      return true;
+    }
+  }
+}
---
+++ igb/src/com/affymetrix/genometryImpl/style/HeatMap.java
@@ -0,0 +1,182 @@
+/**
+ *   Copyright (c) 2007 Affymetrix, Inc.
+ *
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
+
+package com.affymetrix.genometryImpl.style;
+
+import java.awt.Color;
+import java.util.*;
+
+public class HeatMap {
+
+  /** Name of the Black-and-White Standard HeatMap. */
+  public static final String HEATMAP_0 = "Black/White";
+
+  /** Name of the Violet Standard HeatMap. */
+  public static final String HEATMAP_1 = "Violet";
+
+  /** Name of the Blue/Yellow Standard HeatMap. */
+  public static final String HEATMAP_2 = "Blue/Yellow";
+
+  /** Name of the Red/Green Standard HeatMap. */
+  public static final String HEATMAP_3 = "Red/Black/Green";
+
+  /** Name of the second Blue/Yellow Standard HeatMap. */
+  public static final String HEATMAP_4 = "Blue/Yellow 2";
+
+  /** Name of the Transparent Black-and-White Standard HeatMap. */
+  public static final String HEATMAP_T_0 = "Transparent B/W";
+
+  /** Name of the Transparent Violet Standard HeatMap. */
+  public static final String HEATMAP_T_1 = "Transparent Blue";
+
+  /** Name of the Transparent Blue/Yellow Standard HeatMap. */
+  public static final String HEATMAP_T_2 = "Transparent Red";
+
+  /** Name of the Transparent Red/Green Standard HeatMap. */
+  public static final String HEATMAP_T_3 = "Transparent Green";
+
+  public static final String PREF_HEATMAP_NAME = "Default Heatmap";
+  public static final String def_heatmap_name = HEATMAP_2;
+
+  public static String[] HEATMAP_NAMES = {
+    HEATMAP_0, HEATMAP_1, HEATMAP_2, HEATMAP_3, HEATMAP_4,
+    HEATMAP_T_0, HEATMAP_T_2, HEATMAP_T_3, HEATMAP_T_1
+  };
+
+  static Map name_to_color_array = new HashMap();
+
+  String name;
+  Color[] colors;
+
+  public HeatMap(String name, Color[] colors) {
+    this.name = name;
+    this.colors = colors;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Color[] getColors() {
+    return colors;
+  }
+
+  /**
+   *  Returns one of the standard pre-defined heat maps using the names in
+   *  HEATMAP_NAMES, or null if one with the given name does not exist.
+   */
+  public static HeatMap getStandardHeatMap(String name) {
+    HeatMap hm = (HeatMap) name_to_color_array.get(name);
+    if (hm == null) {
+      int r,g,b;
+      int bins = 256;
+      Color[] cc = new Color[bins];
+
+      if (HEATMAP_0.equals(name)) {
+        r=0; g=0; b=0;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r++, g++, b++);
+        }
+      } else if (HEATMAP_1.equals(name)) {
+        r=0; g=0; b=0;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r++, g, b++);
+        }
+      } else if (HEATMAP_2.equals(name)) {
+        r=0; g=0; b=255;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r++, g++, b--);
+        }
+      } else if (HEATMAP_3.equals(name)) {
+        // red-black-green colormap
+        for (int i=0; i<bins; i++) {
+          r = Math.max(255 - 2*i, 0);
+          g = Math.min(Math.max(2 * (i-128), 0), 255);
+          b = 0;
+          cc[i] = new Color(r, g, b);
+        }
+      } else if (HEATMAP_4.equals(name)) {
+        r=0; g=0; b=128;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r++, g++, b);
+          if (i % 2 == 0)  { b--; }
+        }
+      }
+
+      // Now the transparent ones
+      else if (HEATMAP_T_0.equals(name)) {
+        r=0; g=0; b=0;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r++, g++, b++, 128);
+        }
+      } else if (HEATMAP_T_1.equals(name)) {
+        r=0; g=0; b=0;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r, g, b++, 128);
+        }
+      } else if (HEATMAP_T_2.equals(name)) {
+        r=0; g=0; b=0;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r++, g, b, 128);
+        }
+      } else if (HEATMAP_T_3.equals(name)) {
+        r=0; g=0; b=0;
+        for (int i=0; i<bins; i++) {
+          cc[i] = new Color(r, g++, b, 128);
+        }
+      } else {
+        cc = null;
+      }
+
+      if (cc != null) {
+        hm = new HeatMap(name, cc);
+        name_to_color_array.put(name, hm);
+      }
+    }
+
+    return hm;
+  }
+
+  /** Make a HeatMap that interpolates linearly between the two given colors. */
+  public static HeatMap makeLinearHeatmap(String name, Color low, Color high) {
+    Color[] colors = new Color[256];
+    HeatMap heat_map = new HeatMap(name, colors);
+
+    for (int i=0; i<256; i++) {
+      float x = (i*1.0f)/255.0f;
+      colors[i] = interpolateColor(low, high, x);
+    }
+
+    return heat_map;
+  }
+
+
+  /**
+   *  Creates a new color inbetween c1 and c2.
+   *  @param x  The fraction of the new color (0.00 to 1.00) that
+   *  should be based on color c2, the rest is based on c1.
+   */
+  public static Color interpolateColor(Color c1, Color c2, float x) {
+    if (x <= 0.0f) {
+      return c1;
+    } else if (x >= 1.0f) {
+      return c2;
+    } else {
+      int r = (int) ((1.0f - x) * c1.getRed() + x * c2.getRed());
+      int g = (int) ((1.0f - x) * c1.getGreen() + x * c2.getGreen());
+      int b = (int) ((1.0f - x) * c1.getBlue() + x * c2.getBlue());
+
+      return new Color(r, g, b);
+    }
+  }
+}

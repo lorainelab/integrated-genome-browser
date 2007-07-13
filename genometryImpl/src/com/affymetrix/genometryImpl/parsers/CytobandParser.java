ea0fc19ae5f3cf02e0784ec3ca7ff454c96ed335
gregghelt2 <gregghelt2@mycompany.com>
2007-07-13 20:03:28 +0000

    Added writeAnnotations() method to support AnnotationWriter interface. Also changed parsing to add additional layer to symmetry hierarchy to enable proper handling in DAS/2 server.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2640 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/CytobandParser.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/CytobandParser.java
@@ -13,33 +13,30 @@
 
 package com.affymetrix.genometryImpl.parsers;
 
-import com.affymetrix.genometry.BioSeq;
-import com.affymetrix.genometry.MutableAnnotatedBioSeq;
-import com.affymetrix.genometryImpl.SmartAnnotBioSeq;
+import com.affymetrix.genometry.*;
+import com.affymetrix.genometry.span.SimpleSeqSpan;
 import com.affymetrix.genometryImpl.Scored;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.SingletonSymWithProps;
+import com.affymetrix.genometryImpl.SimpleSymWithProps;
 import com.affymetrix.genometryImpl.TypedSym;
 import com.affymetrix.genometryImpl.style.HeatMap;
 import java.awt.Color;
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
  *  A parser designed to parse data from cytoBand.txt files from UCSC.
  */
-public class CytobandParser {
+public class CytobandParser implements AnnotationWriter  {
 
   static Pattern line_regex = Pattern.compile("\\t");
 
   static final HeatMap cyto_heat_map = HeatMap.makeLinearHeatmap("cytobands", Color.WHITE, Color.BLACK);
   static final Color cyto_acen_color = new Color(198,64,64); // dark red
   static final Color cyto_stalk_color = Color.GRAY;
-      
+
   /** This is the name that is used to identify the cytobands data.  It is not
    *  intended to be displayed to the user.  It should be some String that they
    *  are unlikely to want to use as the name of a tier.
@@ -54,7 +51,7 @@ public class CytobandParser {
   static {
     pref_list.add("cyt");
   }
-  
+
   public CytobandParser() {
   }
 
@@ -63,7 +60,12 @@ public class CytobandParser {
     parse(dis, seq_group, true);
   }
 
-  public List parse(InputStream dis, AnnotatedSeqGroup seq_group, boolean annotate_seq) 
+  /**
+   *  if annotating seq, makes all parsed syms children of a containing SymWithProps and adds this single sym
+   *    to the seq (and if seq is a SmartAnnotBioSeq, it will in turn create a TypeContainerSym as parent of the
+   *    single SymWithProps)
+   */
+  public List parse(InputStream dis, AnnotatedSeqGroup seq_group, boolean annotate_seq)
   throws IOException  {
 
     int band_alternator = 1; // toggles dark/light when band color is missing
@@ -71,6 +73,7 @@ public class CytobandParser {
     String line;
     Thread thread = Thread.currentThread();
     BufferedReader reader = new BufferedReader(new InputStreamReader(dis));
+    Map seq2csym = new HashMap();
     while ((line = reader.readLine()) != null && (! thread.isInterrupted())) {
       if (line.startsWith("#") || "".equals(line)) {  // skip comment lines
         continue;
@@ -83,18 +86,18 @@ public class CytobandParser {
         int min, max;
         float score = Float.NEGATIVE_INFINITY; // Float.NEGATIVE_INFINITY signifies that score is not used
         boolean forward;
-        
+
         if (fields == null || field_count < 4) {
           throw new IOException("Line has wrong number of data columns.");
         }
-        
+
         seq_name = fields[0]; // seq id field
         MutableAnnotatedBioSeq seq = seq_group.getSeq(seq_name);
         if (seq == null) {
           System.out.println("seq not recognized, creating new seq: " + seq_name);
           seq = seq_group.addSeq(seq_name, 0);
         }
-        
+
         int beg = Integer.parseInt(fields[1]);  // start field
         int end = Integer.parseInt(fields[2]);  // stop field
         annot_name = new String(fields[3]);
@@ -108,17 +111,28 @@ public class CytobandParser {
           }
           band_alternator = -band_alternator;
         }
-        
+
         if (beg > seq.getLength()) {
           seq.setLength(beg);
         }
         if (end > seq.getLength()) {
           seq.setLength(end);
         }
-        
-        CytobandSym sym = new CytobandSym(beg, end, seq, annot_name, band);   
+
+        CytobandSym sym = new CytobandSym(beg, end, seq, annot_name, band);
 	if (annotate_seq) {
-	  ((SmartAnnotBioSeq) seq).addAnnotation(sym, CYTOBAND_TIER_NAME);
+	  SimpleSymWithProps parent_sym = (SimpleSymWithProps)seq2csym.get(seq);
+	  if (parent_sym == null) {
+	    parent_sym = new SimpleSymWithProps();
+	    parent_sym.addSpan(new SimpleSeqSpan(0, seq.getLength(), seq));
+	    parent_sym.setProperty("method", CYTOBAND_TIER_NAME);
+	    parent_sym.setProperty("preferred_formats", pref_list);
+	    parent_sym.setProperty(SimpleSymWithProps.CONTAINER_PROP, Boolean.TRUE);
+	    seq2csym.put(seq, parent_sym);
+	    seq.addAnnotation(parent_sym);
+	  }
+	  parent_sym.addChild(sym);
+	  //	  ((SmartAnnotBioSeq) seq).addAnnotation(sym, CYTOBAND_TIER_NAME);
 	  if (annot_name != null) {
 	    seq_group.addToIndex(annot_name, sym);
 	  }
@@ -131,7 +145,7 @@ public class CytobandParser {
 
   public static final String BAND_STALK = "stalk";
   public static final String BAND_ACEN = "acen";
-  
+
   public static float parseScore(String s) {
     if ("gneg".equals(s)) {
       return GNEG_SCORE;
@@ -155,9 +169,54 @@ public class CytobandParser {
     }
     else return 0.0f;
   }
-  
+
+  public boolean writeAnnotations(java.util.Collection syms, BioSeq seq,
+				  String type, OutputStream outstream) {
+    System.out.println("in CytobandParser.writeAnnotations()");
+    boolean success = true;
+    try {
+      Writer bw = new BufferedWriter(new OutputStreamWriter(outstream));
+      Iterator iterator = syms.iterator();
+      while (iterator.hasNext()) {
+	SeqSymmetry sym = (SeqSymmetry)iterator.next();
+	writeCytobandFormat(bw, sym, seq);
+      }
+      bw.flush();
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+      success = false;
+    }
+    return success;
+  }
+
+  public static void writeCytobandFormat(Writer out, SeqSymmetry sym, BioSeq seq)
+    throws IOException  {
+    if (sym instanceof CytobandSym) {
+      CytobandSym cytosym = (CytobandSym)sym;
+      SeqSpan span = cytosym.getSpan(seq);
+      if (span != null) {
+	out.write(seq.getID());
+	out.write('\t');
+	int min = span.getMin();
+	int max = span.getMax();
+	out.write(Integer.toString(min));
+	out.write('\t');
+	out.write(Integer.toString(max));
+	out.write('\t');
+	out.write(cytosym.getID());
+	out.write('\t');
+	out.write(cytosym.getBand());
+	out.write('\n');
+      }
+    }
+  }
+
+  public String getMimeType()  { return "txt/plain"; }
+
   public class CytobandSym extends SingletonSymWithProps implements Scored, TypedSym {
     String band;
+
     public CytobandSym(int start, int end, BioSeq seq, String name, String band) {
       super(start, end, seq);
       this.band = band;
@@ -167,11 +226,11 @@ public class CytobandParser {
     public float getScore() {
       return parseScore(band);
     }
-    
+
     public String getBand() {
       return band;
     }
-    
+
     public Color getColor() {
       if ("acen".equals(band)) {
         return cyto_acen_color;
@@ -182,7 +241,7 @@ public class CytobandParser {
         return cyto_heat_map.getColors()[(int) (255 * 0.001 * score)];
       }
     }
-    
+
     /** Retrieves a color that will contrast well with {@link #getColor()}. */
     public Color getTextColor() {
       Color col = getColor();
@@ -190,7 +249,7 @@ public class CytobandParser {
       if (intensity > 255+128) { return Color.BLACK; }
       else return Color.WHITE;
     }
-    
+
     public boolean setProperty(String name, Object val) {
       if ("band".equals(name)) {
         band = name;
@@ -199,7 +258,7 @@ public class CytobandParser {
       }
       return true;
     }
-    
+
     public Object getProperty(String name) {
       if ("id".equals(name)) {
         return getID();
@@ -225,7 +284,7 @@ public class CytobandParser {
       props.put("band", band);
       return props;
     }
-    
+
     public String getType() {
       return CYTOBAND_TIER_NAME;
     }

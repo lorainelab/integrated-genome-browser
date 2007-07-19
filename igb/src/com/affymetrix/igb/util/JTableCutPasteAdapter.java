05b072fc03fc513cb1e2c4f78fd74dbbf7051e4f
chyekk <chyekk@mycompany.com>
2007-07-19 18:30:32 +0000

    Refactored for easier reuse.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2660 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/util/JTableCutPasteAdapter.java
+++ igb/src/com/affymetrix/igb/util/JTableCutPasteAdapter.java
@@ -1,5 +1,5 @@
 /**
-*   Copyright (c) 2001-2004 Affymetrix, Inc.
+*   Copyright (c) 2001-2007 Affymetrix, Inc.
 *    
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
@@ -13,11 +13,14 @@
 
 package com.affymetrix.igb.util;
 
+import com.affymetrix.igb.Application;
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 import java.awt.datatransfer.*;
-import java.util.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import javax.swing.*;
+
 /**
  *  An adapter to add Excel-like cut and paste facilities to JTable.
  *  Started with ExcelAdapter code at 
@@ -30,32 +33,32 @@ import java.util.*;
  *     The clipboard data format used by the adapter is compatible with
  *     the clipboard format used by Excel. This provides for clipboard
  *     interoperability between enabled JTables and Excel.
+ *
  */
 
-public class JTableCutPasteAdapter implements ActionListener {
-  // public class ExcelAdapter implements ActionListener  {
-  String rowstring,value;
-  Clipboard system;
-  StringSelection stsel;
+public class JTableCutPasteAdapter {
+
   JTable jTable1;
 
   /**
-   * The Excel Adapter is constructed with a
-   * JTable on which it enables Copy-Paste and acts
-   * as a Clipboard listener.
+   * An object that enables Copy-Paste on the given JTable.
    */
-  public JTableCutPasteAdapter(JTable myJTable) {
+  public JTableCutPasteAdapter(JTable myJTable, boolean registerDefaultKeyStrokes) {
     jTable1 = myJTable;
-    KeyStroke copy = KeyStroke.getKeyStroke(KeyEvent.VK_C,ActionEvent.CTRL_MASK,false);
-    // Identifying the copy KeyStroke user can modify this
-    // to copy on some other Key combination.
-    KeyStroke paste = KeyStroke.getKeyStroke(KeyEvent.VK_V,ActionEvent.CTRL_MASK,false);
-    // Identifying the Paste KeyStroke user can modify this
-    //to copy on some other Key combination.
-    jTable1.registerKeyboardAction(this,"Copy",copy,JComponent.WHEN_FOCUSED);
+    if (registerDefaultKeyStrokes) {
+      registerKeyStrokes();
+    }
+  }
+  
+  /** Registers the default keyboard actions for Copy (control C) and Paste (control V). */
+  public void registerKeyStrokes() {
+    KeyStroke copyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK, false);
+    KeyStroke pasteStroke = KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK, false);
 
-    jTable1.registerKeyboardAction(this,"Paste",paste,JComponent.WHEN_FOCUSED);
-    system = Toolkit.getDefaultToolkit().getSystemClipboard();
+    jTable1.registerKeyboardAction(copyAction , (String) copyAction.getValue(Action.NAME), 
+        copyStroke, JComponent.WHEN_FOCUSED);
+    jTable1.registerKeyboardAction(pasteAction, (String) pasteAction.getValue(Action.NAME), 
+        pasteStroke, JComponent.WHEN_FOCUSED);
   }
 
   /**
@@ -63,76 +66,108 @@ public class JTableCutPasteAdapter implements ActionListener {
    */
   public JTable getJTable() {return jTable1;}
   public void setJTable(JTable jTable1) {this.jTable1=jTable1;}
-  /**
-   * This method is activated on the Keystrokes we are listening to
-   * in this implementation. Here it listens for Copy and Paste ActionCommands.
-   * Selections comprising non-adjacent cells result in invalid selection and
-   * then copy action cannot be performed.
-   * Paste is done by aligning the upper left corner of the selection with the
-   * 1st element in the current selection of the JTable.
-   */
-  public void actionPerformed(ActionEvent e)
-  {
-    if (e.getActionCommand().compareTo("Copy")==0)
-      {
-	StringBuffer sbf=new StringBuffer();
-	// Check to ensure we have selected only a contiguous block of
-	// cells
-	int numcols=jTable1.getSelectedColumnCount();
-	int numrows=jTable1.getSelectedRowCount();
-	int[] rowsselected=jTable1.getSelectedRows();
-	int[] colsselected=jTable1.getSelectedColumns();
-	if (!((numrows-1==rowsselected[rowsselected.length-1]-rowsselected[0] &&
-	       numrows==rowsselected.length) &&
-	      (numcols-1==colsselected[colsselected.length-1]-colsselected[0] &&
-	       numcols==colsselected.length)))
-	  {
-            JOptionPane.showMessageDialog(null, "Invalid Copy Selection",
-                                          "Invalid Copy Selection",
-                                          JOptionPane.ERROR_MESSAGE);
-            return;
-	  }
-	for (int i=0;i<numrows;i++)
-	  {
-            for (int j=0;j<numcols;j++)
-	      {
-		sbf.append(jTable1.getValueAt(rowsselected[i],colsselected[j]));
-		if (j<numcols-1) sbf.append("\t");
-	      }
-            sbf.append("\n");
-	  }
-	stsel  = new StringSelection(sbf.toString());
-	system = Toolkit.getDefaultToolkit().getSystemClipboard();
-	system.setContents(stsel,stsel);
+
+ /**
+  * Tests to see if the current selection area is suitable for copy action.
+  * Selections comprising non-adjacent cells result in invalid selection and
+  * then copy action cannot be performed.
+  * Paste can be done as long as there is something selected.
+  */
+  public boolean isValidSelectionForCopy() {
+    int numcols=jTable1.getSelectedColumnCount();
+    int numrows=jTable1.getSelectedRowCount();
+    int[] rowsselected=jTable1.getSelectedRows();
+    int[] colsselected=jTable1.getSelectedColumns();
+    if (numrows==0 || numcols==0) {
+      return false;
+    }
+    boolean isValid = ((numrows-1==rowsselected[rowsselected.length-1]-rowsselected[0] &&
+        numrows==rowsselected.length) &&
+        (numcols-1==colsselected[colsselected.length-1]-colsselected[0] &&
+        numcols==colsselected.length));
+    return isValid;
+  }
+  
+  public void showInvalidSelectionMessage() {
+    String msg = "Invalid selection";
+    try {
+      String msg2 = Application.getSingleton().getResourceString("invalid_selection");
+      if (msg2 != null) {
+        msg = msg2;
       }
-    if (e.getActionCommand().compareTo("Paste")==0)
-      {
-	System.out.println("Trying to Paste");
-	int startRow=(jTable1.getSelectedRows())[0];
-	int startCol=(jTable1.getSelectedColumns())[0];
-	try
-          {
-	    String trstring= (String)(system.getContents(this).getTransferData(DataFlavor.stringFlavor));
-	    System.out.println("String is:"+trstring);
-	    StringTokenizer st1=new StringTokenizer(trstring,"\n");
-	    for(int i=0;st1.hasMoreTokens();i++)
-	      {
-                rowstring=st1.nextToken();
-                StringTokenizer st2=new StringTokenizer(rowstring,"\t");
-                for(int j=0;st2.hasMoreTokens();j++)
-		  {
-		    value=(String)st2.nextToken();
-		    if (startRow+i< jTable1.getRowCount()  &&
-			startCol+j< jTable1.getColumnCount())
-                      jTable1.setValueAt(value,startRow+i,startCol+j);
-		    System.out.println("Putting "+ value+"at row="+startRow+i+"column="+startCol+j);
-		  }
-	      }
-	  }
-	catch(Exception ex){ex.printStackTrace();}
+      
+      JOptionPane.showMessageDialog(null, msg, msg, JOptionPane.ERROR_MESSAGE);
+
+    } catch (Exception ex) {
+      Application.errorPanel("ERROR", ex);
+    }
+    return;
+  }
+  
+  public Action copyAction = new AbstractAction("Copy") {
+    public void actionPerformed(ActionEvent e) {
+      doCopy();
+    }
+  };
+  
+  public Action pasteAction = new AbstractAction("Paste") {
+    public void actionPerformed(ActionEvent e) {
+      try {
+        doPaste();
+      } catch (Exception ex){
+        Application.errorPanel("ERROR", ex);
       }
+    }
+  };
+  
+  public void doCopy() {
+    StringBuffer sbf=new StringBuffer();
+    // Check to ensure we have selected only a contiguous block of cells
+    int numcols=jTable1.getSelectedColumnCount();
+    int numrows=jTable1.getSelectedRowCount();
+    int[] rowsselected=jTable1.getSelectedRows();
+    int[] colsselected=jTable1.getSelectedColumns();
+    if ( ! isValidSelectionForCopy()) {
+      showInvalidSelectionMessage();
+      return;
+    }
+    for (int i=0;i<numrows;i++) {
+      for (int j=0;j<numcols;j++) {
+        sbf.append(jTable1.getValueAt(rowsselected[i],colsselected[j]));
+        if (j<numcols-1) sbf.append("\t");
+      }
+      sbf.append("\n");
+    }
+
+    StringSelection stsel  = new StringSelection(sbf.toString());
+    Clipboard system = Toolkit.getDefaultToolkit().getSystemClipboard();
+    system.setContents(stsel,stsel);
   }
 
+  /**
+   * Paste is done by aligning the upper left corner of the selection with the
+   * First element in the current selection of the JTable.
+   */
+  public void doPaste() throws UnsupportedFlavorException, IOException {
+    
+    int startRow=(jTable1.getSelectedRows())[0];
+    int startCol=(jTable1.getSelectedColumns())[0];
+    Clipboard system = Toolkit.getDefaultToolkit().getSystemClipboard();
+    String trstring= (String)(system.getContents(this).getTransferData(DataFlavor.stringFlavor));
+        
+    String rowstring,value;
+    StringTokenizer st1=new StringTokenizer(trstring,"\n");
+    for(int i=0;st1.hasMoreTokens();i++) {
+      rowstring=st1.nextToken();
+      StringTokenizer st2=new StringTokenizer(rowstring,"\t");
+      for(int j=0;st2.hasMoreTokens();j++) {
+        value=(String) st2.nextToken();
+        if (startRow+i< jTable1.getRowCount()  &&  startCol+j< jTable1.getColumnCount()) {
+          jTable1.setValueAt(value,startRow+i,startCol+j);
+        }
+      }
+    }
+  }
 
   public static void main(String[] args) {
 
@@ -154,10 +189,10 @@ public class JTableCutPasteAdapter implements ActionListener {
     cpane.setLayout(new BorderLayout());
     cpane.add("Center", test_table);
 
-    JTableCutPasteAdapter test_adapter = new JTableCutPasteAdapter(test_table);    
+    JTableCutPasteAdapter test_adapter = new JTableCutPasteAdapter(test_table, true);    
 
     frm.pack();
-    frm.show();
+    frm.setVisible(true);
   }
 
 

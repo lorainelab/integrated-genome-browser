5e1448a2e6446b0a3e270d04fa87a3904ea58757
chyekk <chyekk@mycompany.com>
2007-07-25 00:30:11 +0000

    Holds data for multiple graphs for a single chromosome.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2681 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/gchp/AffySingleChromData.java
@@ -0,0 +1,38 @@
+/**
+*   Copyright (c) 2007 Affymetrix, Inc.
+*
+*   Licensed under the Common Public License, Version 1.0 (the "License").
+*   A copy of the license must be included with any distribution of
+*   this source code.
+*   Distributions from Affymetrix, Inc., place this in the
+*   IGB_LICENSE.html file.
+*
+*   The license is also available at
+*   http://www.opensource.org/licenses/cpl.php
+*/
+
+package com.affymetrix.genometryImpl.parsers.gchp;
+
+import com.affymetrix.genometryImpl.util.ByteList;
+import com.affymetrix.genometryImpl.util.IntList;
+import java.util.*;
+
+public class AffySingleChromData {
+  
+  int start;
+  int count;
+  String displayName;
+  List<CharSequence> probeSetNames = new ArrayList<CharSequence>();
+  IntList positions = new IntList();
+  
+  List<AffyChpColumnData> columns = new ArrayList<AffyChpColumnData>();
+  
+  /** Creates a new instance of SingleChromosomeData */
+  public AffySingleChromData(String chromDisplayName, int start, int count, List<AffyChpColumnData> columns) {
+    this.displayName = chromDisplayName;
+    this.start = start;
+    this.count = count;
+    this.columns = columns;
+  }
+  
+}

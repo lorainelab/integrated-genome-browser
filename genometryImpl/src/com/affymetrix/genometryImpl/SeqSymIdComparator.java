332d8e139a9ec6e9a057300a914caa2775f46f71
gregghelt2 <gregghelt2@mycompany.com>
2007-08-02 01:34:13 +0000

    Added comparator to sort seq symmetries by lexicographical ordering of their IDs.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2718 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/src/com/affymetrix/genometryImpl/SeqSymIdComparator.java
@@ -0,0 +1,39 @@
+/**
+*   Copyright (c) 2007 Affymetrix, Inc.
+*    
+*   Licensed under the Common Public License, Version 1.0 (the "License").
+*   A copy of the license must be included with any distribution of
+*   this source code.
+*   Distributions from Affymetrix, Inc., place this in the
+*   IGB_LICENSE.html file.  
+*
+*   The license is also available at
+*   http://www.opensource.org/licenses/cpl.php
+*/
+
+package com.affymetrix.genometryImpl;
+
+import java.util.Comparator;
+import com.affymetrix.genometry.*;
+
+/**
+ *  Sorts SeqSymmetries based on lexicographic ordering of IDs 
+ */
+public class SeqSymIdComparator implements Comparator {
+  
+  public int compare(Object obj1, Object obj2) {
+    SeqSymmetry sym1 = (SeqSymmetry)obj1;
+    SeqSymmetry sym2 = (SeqSymmetry)obj2;
+    String id1 = sym1.getID();
+    String id2 = sym2.getID();
+    if (id1 == null || id2 == null) {
+      if (id1 == null && id2 == null) { return 0; }
+      else if (id1 == null) { return 1; }
+      else { return -1; }
+    }
+    else {
+      return id1.compareTo(id2);
+    }
+  }
+  
+}

5dcc2f95d36208f2b305170f743286e413796aa3
chyekk <chyekk@mycompany.com>
2007-08-22 18:04:18 +0000

    Modified this parser to take a SeqGroup as an argument and add Seq's to it based on the seq names in the specified chromosome column.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2795 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/TabDelimitedParser.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/TabDelimitedParser.java
@@ -20,12 +20,10 @@ import java.util.regex.*;
 import com.affymetrix.genometry.*;
 import com.affymetrix.genometry.seq.*;
 import com.affymetrix.genometry.span.*;
-import com.affymetrix.genometry.symmetry.*;
 import com.affymetrix.genometry.util.*;
+import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.SingletonSymWithProps;
-import com.affymetrix.genometryImpl.SymWithProps;
 import com.affymetrix.genometryImpl.SimpleSymWithProps;
-import com.affymetrix.genometryImpl.util.IntList;
 
 /**
  *  Attempts to parse "generic" tab delimited data. 
@@ -33,12 +31,15 @@ import com.affymetrix.genometryImpl.util.IntList;
  *  Which columns to use for start, end, etc. are specified in the constructor.
  */
 public class TabDelimitedParser {
+  int chromosome_col;
   int start_col;
   int end_col;     // should need only end_col or length_col, not both
   int length_col;  // should need only end_col or length_col, not both
   int strand_col;  // column to use for determining strand
   int group_col;  // column to use for grouping features...
   int type_col;   // column to use for setting feature type
+  
+  boolean addToIndex; // whether to add annotation id's to the index on the seq group
 
   int seq_col;
   int seq_col2;
@@ -80,14 +81,21 @@ public class TabDelimitedParser {
    *  Each argument tells which column to find a particular item in.
    *  -1 for any arg indicates that it is not present in the table.
    */
-  public TabDelimitedParser(int type, int start, int end, int length,
-			    int strand, int group, boolean props, boolean header) {
+  public TabDelimitedParser(int type, int chromosome, int start, int end, int length,
+			    int strand, int group, boolean props, boolean header, boolean addToIndex) {
+
+    if (chromosome < 0) {
+      throw new IllegalArgumentException("Chromosome column number must be 0 or greater.");
+    }
+    
+    chromosome_col = chromosome;
     start_col = start;
     end_col = end;
     length_col = length;
     group_col = group;
     type_col = type;
     strand_col = strand;
+    this.addToIndex = addToIndex;
 
     has_header = header;
     use_length = (length >= 0);
@@ -98,18 +106,14 @@ public class TabDelimitedParser {
     this.make_props = props;
   }
 
-  public TabDelimitedParser(String type, int start, int end, int length,
-			    int strand, int group, boolean props, boolean header) {
-    this(-1, start, end, length, strand, group, props, header);
+  public TabDelimitedParser(String type, int chromosome, int start, int end, int length,
+			    int strand, int group, boolean props, boolean header, boolean addToIndex) {
+    this(-1, chromosome, start, end, length, strand, group, props, header, addToIndex);
     default_type = type;
   }
 
-  public List parseToList(InputStream istr, MutableAnnotatedBioSeq aseq) {
-    return null;
-  }
+  public void parse(InputStream istr, AnnotatedSeqGroup seq_group) {
 
-  public MutableAnnotatedBioSeq parse(InputStream istr, MutableAnnotatedBioSeq aseq) {
-    MutableAnnotatedBioSeq result_seq = aseq;
     HashMap group_hash = new HashMap();
     MutableSeqSpan union_span = new SimpleMutableSeqSpan();
     ArrayList col_names = null;
@@ -129,6 +133,7 @@ public class TabDelimitedParser {
       while ((line = br.readLine()) != null) {
 	String[] cols = line_splitter.split(line);
 	if (cols.length <= 0) { continue; }
+        
 	int start = Integer.parseInt(cols[start_col]);
 	int end;
 	if (use_length) {
@@ -151,16 +156,32 @@ public class TabDelimitedParser {
 	  type = cols[type_col];
 	  //	  System.out.println("type = " + type);
 	}
-	SingletonSymWithProps child = new SingletonSymWithProps(start, end, aseq);
+
+        String chromName = cols[chromosome_col];        
+        MutableAnnotatedBioSeq seq = seq_group.getSeq(chromName);
+        if (seq == null) {
+          seq = seq_group.addSeq(chromName, 0);
+        }
+        
+        if (seq.getLength() < end) {
+          seq.setLength(end);
+        }
+        if (seq.getLength() < start) {
+          seq.setLength(start);
+        }
+
+        SingletonSymWithProps child = new SingletonSymWithProps(start, end, seq);
 	child.setProperty("method", type);
+        child.setProperty("id", type + ": " + SeqUtils.spanToString(child));
 	if (make_props) {
-	  for (int i=0; i<cols.length; i++) {
+	  for (int i=0; i<cols.length && i<col_names.size(); i++) {
 	    String name = (String)col_names.get(i);
 	    String val = cols[i];
 	    child.setProperty(name, val);
 	  }
 	}
-	if (use_group) {
+
+        if (use_group) {
 	  String group = cols[group_col];
 	  //	  SingletonSymWithProps parent = (SingletonSymWithProps)group_hash.get(group);
 	  SimpleSymWithProps parent = (SimpleSymWithProps)group_hash.get(group);
@@ -168,48 +189,74 @@ public class TabDelimitedParser {
 	    //	    parent = new SingletonSymWithProps(start, end, aseq);
 	    //	    parent = new SimpleSymWithProps(start, end, aseq);
 	    parent = new SimpleSymWithProps();
-	    parent.addSpan(new SimpleMutableSeqSpan(start, end, aseq));
+            SimpleMutableSeqSpan span = new SimpleMutableSeqSpan(start, end, seq);
+	    parent.addSpan(span);
 	    //	    System.out.println("parent type = " + type);
 	    parent.setProperty("method", type);
+            parent.setProperty("id", type + ": " + SeqUtils.spanToString(parent.getSpan(seq)));
 	    group_hash.put(group, parent);
 	    // or maybe should add all parents to a grandparent, and add _grandparent_ to aseq???
-	    aseq.addAnnotation(parent);
+	    seq.addAnnotation(parent);
+            seq_group.addToIndex(parent.getID(), parent);
 	  }
 	  else {
-	    MutableSeqSpan pspan = (MutableSeqSpan)parent.getSpan(aseq);
+	    MutableSeqSpan pspan = (MutableSeqSpan)parent.getSpan(seq);
 	    //	    SeqUtils.encompass((SeqSpan)parent, (SeqSpan)child, union_span);
 	    SeqUtils.encompass(pspan, (SeqSpan)child, union_span);
 	    //	    System.out.println("expanding parent");
 	    //	    parent.set(union_span.getStart(), union_span.getEnd(), aseq);
-	    pspan.set(union_span.getStart(), union_span.getEnd(), aseq);
+	    pspan.set(union_span.getStart(), union_span.getEnd(), seq);
 	  }
 	  parent.addChild(child);
 	}
 	else {
-	  System.out.println("WARNING!!!  Parsing without using groups is not yet implemented");
-	}
+//          SingletonSymWithProps sym = new SingletonSymWithProps(start, end, seq);
+//          sym.setProperty("method", type);
+          seq.addAnnotation(child);
+          seq_group.addToIndex(child.getID(), child);
+        }
       }
     }
     catch (Exception ex) {
       ex.printStackTrace();
     }
-    return result_seq;
+    return;
   }
 
   public static void main(String[] args) {
-    String fil = System.getProperty("user.dir") + "/testdata/tab/test1.tab";
+// 0 Sample	
+// 1 Chr	
+// 2 Cytoband_Start_Pos	
+// 3 Cytoband_End_Pos	
+// 4 CN_ChangeType	
+// 5 Size(kb)	
+// 6 CN_State
+// 7 Start_ProbeSet	
+// 8 End_ProbeSet	
+// 9 Start_Physical_Pos
+// 10 End_Physical_Position	
+// 11 #ProbeSet	
+// 12 %ProbeSets_withCNV	
+// 13 CNV_Annotation
+    
+    String fil = System.getProperty("user.dir") + "/data/copy_number/DUKE_US_DukeCNV_NSP_T24_1_1.rpt";
     // type, start, end, length, strand, group, boolean props, boolean has_header
-    TabDelimitedParser tester = new TabDelimitedParser(0, 1, -1, 2, 3, 4, false, false);
+    TabDelimitedParser tester = new TabDelimitedParser(0, 1, 9, 10, -1, -1, -1, true, true, true);
     try {
       FileInputStream fis = new FileInputStream(new File(fil));
-      MutableAnnotatedBioSeq aseq = new SimpleAnnotatedBioSeq("chr21", 47000000);
-      aseq = tester.parse(fis, aseq);
-      for (int i=0; i<aseq.getAnnotationCount(); i++) {
-	SeqSymmetry annot = aseq.getAnnotation(i);
-	SeqUtils.printSymmetry(annot);
+      AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
+      
+      tester.parse(fis, seq_group);
+      
+      for (int s=0; s<seq_group.getSeqCount(); s++) {
+        MutableAnnotatedBioSeq aseq = seq_group.getSeq(s);
+        for (int i=0; i<aseq.getAnnotationCount(); i++) {
+          SeqSymmetry annot = aseq.getAnnotation(i);
+          SeqUtils.printSymmetry(annot, "  ", true);
+        }
       }
-    }
-    catch (Exception ex) {
+      
+    } catch (Exception ex) {
       ex.printStackTrace();
     }
   }

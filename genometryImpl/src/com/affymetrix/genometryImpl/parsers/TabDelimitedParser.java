c71bfd7c103e878a2db6d1fef4fbb815f88e88f7
chyekk <chyekk@mycompany.com>
2007-08-22 21:48:58 +0000

    updated copyright.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2798 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/TabDelimitedParser.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/TabDelimitedParser.java
@@ -1,5 +1,5 @@
 /**
- *   Copyright (c) 2001-2004 Affymetrix, Inc.
+ *   Copyright (c) 2001-2007 Affymetrix, Inc.
  *
  *   Licensed under the Common Public License, Version 1.0 (the "License").
  *   A copy of the license must be included with any distribution of

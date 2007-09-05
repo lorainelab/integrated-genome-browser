984e713f7f4093185aafd17e1f3c5ce6ae44fab5
chyekk <chyekk@mycompany.com>
2007-09-05 18:10:28 +0000

    Extends TabDelimitedParser to read fishClones.txt files from UCSC.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2841 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/FishClonesParser.java
@@ -0,0 +1,57 @@
+/**
+ *   Copyright (c) 2007 Affymetrix, Inc.
+ *
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
+
+package com.affymetrix.genometryImpl.parsers;
+
+import com.affymetrix.genometry.MutableAnnotatedBioSeq;
+import com.affymetrix.genometry.SeqSymmetry;
+import com.affymetrix.genometry.util.SeqUtils;
+import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
+import java.io.*;
+
+/**
+ *  Parses tab-delimited output "fishClones.txt" files from UCSC.
+ *  The file extension should be {@link #FILE_EXT}.
+ */
+public class FishClonesParser extends TabDelimitedParser {
+  public static final String FILE_EXT = "fsh";
+  
+  public FishClonesParser(boolean addToIndex) {
+    super(-1, 0, 1, 2, -1, -1, -1, 3, false, false, addToIndex);
+  }
+  
+  
+  public static void main(String[] args) {
+    String filname = System.getProperty("user.dir") + "/CVS Repositories/affy/GenotypeConsoleBrowser/data/QuickLoad/hg18/fishClones.fsh";
+    File file = new File(filname);
+    
+    FishClonesParser tester = new FishClonesParser(true);
+    try {
+      FileInputStream fis = new FileInputStream(file);
+      AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
+      
+      tester.parse(fis, file.getName(), seq_group);
+      
+      for (int s=0; s<seq_group.getSeqCount() && s<3; s++) {
+        MutableAnnotatedBioSeq aseq = seq_group.getSeq(s);
+        for (int i=0; i<aseq.getAnnotationCount() && i<5; i++) {
+          SeqSymmetry annot = aseq.getAnnotation(i);
+          SeqUtils.printSymmetry(annot, "  ", true);
+        }
+      }
+      
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}

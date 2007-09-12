01152fa6d47b95786f489ea46812caae712a2784
gregghelt2 <gregghelt2@mycompany.com>
2007-09-12 18:31:25 +0000

    Added shortcut for when actual glyph creation is delegated to GenericAnnotGlyphFactory, to cut down overhead of factory init() and createGlyph() calls.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2881 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/stylesheet/XmlStylesheetGlyphFactory.java
+++ igb/src/com/affymetrix/igb/stylesheet/XmlStylesheetGlyphFactory.java
@@ -16,9 +16,11 @@ package com.affymetrix.igb.stylesheet;
 import com.affymetrix.genometry.SeqSymmetry;
 import com.affymetrix.genometryImpl.SimpleSymWithProps;
 import com.affymetrix.genometryImpl.SymWithProps;
+import com.affymetrix.genometryImpl.TypeContainerAnnot;
 import com.affymetrix.genometryImpl.style.DefaultStateProvider;
 import com.affymetrix.genometryImpl.style.IAnnotStyleExtended;
 import com.affymetrix.igb.glyph.MapViewGlyphFactoryI;
+import com.affymetrix.igb.glyph.GenericAnnotGlyphFactory;
 import com.affymetrix.igb.tiers.TierGlyph;
 import com.affymetrix.igb.view.SeqMapView;
 import java.util.*;
@@ -29,6 +31,7 @@ import java.util.*;
  *  GlyphElement.
  */
 public class XmlStylesheetGlyphFactory implements MapViewGlyphFactoryI {
+  static boolean DEBUG = false;
 
   Stylesheet stylesheet = null;
   PropertyMap context = new PropertyMap();
@@ -49,7 +52,8 @@ public class XmlStylesheetGlyphFactory implements MapViewGlyphFactoryI {
   }
 
   boolean isContainer(SeqSymmetry sym) {
-    if (sym instanceof SymWithProps) {
+    if (sym instanceof TypeContainerAnnot)  { return true; }  // faster than checking for CONTAINER_PROP
+    else if (sym instanceof SymWithProps) {
       SymWithProps swp = (SymWithProps) sym;
       if (Boolean.TRUE.equals(swp.getProperty(SimpleSymWithProps.CONTAINER_PROP))) {
         return true;
@@ -58,6 +62,7 @@ public class XmlStylesheetGlyphFactory implements MapViewGlyphFactoryI {
     return false;
   }
 
+  Map annot_factories = new LinkedHashMap();
   public void createGlyph(SeqSymmetry sym, SeqMapView gviewer, boolean next_to_axis) {
       // fixing bug encountered when sym doesn't have span on sequence it is annotating --
       //   currently should only see these as "dummy" placeholder syms that are
@@ -67,32 +72,58 @@ public class XmlStylesheetGlyphFactory implements MapViewGlyphFactoryI {
     // I'm assuming that for container glyphs, the container method is the
     // same as the contained items method
     String meth = SeqMapView.determineMethod(sym);
-    //    if (meth == null)  { SeqUtils.printSymmetry(sym, "   ", true); }
-    IAnnotStyleExtended style = DefaultStateProvider.getGlobalStateProvider().getAnnotStyle(meth);
 
-    if (isContainer(sym)) {
-      for (int i=0; i<sym.getChildCount(); i++) {
-        createGlyph(sym.getChild(i), gviewer, next_to_axis);
-      }
-      return;
+    // shortcut for delegating to other factories
+    //    (or at least GenericAnnotGlyphFactory via AssociationElement...)
+    MapViewGlyphFactoryI subfactory = (MapViewGlyphFactoryI)annot_factories.get(meth);
+
+    if (subfactory != null) {
+      subfactory.createGlyph(sym, gviewer);
     }
+    else {
+      DrawableElement drawable = stylesheet.getDrawableForSym(sym);
+      // if possible, extract a subfactory from drawable and add to annot_factories list...
+      // for now only do this if factory is a GenericAnnotGlyphFactory -- not sure how shortcut would
+      //    affect other factories...
 
-    DrawableElement drawable = stylesheet.getDrawableForSym(sym);
+      if (drawable instanceof AssociationElement) {
+	AssociationElement assel = (AssociationElement)drawable;
+	MapViewGlyphFactoryI assfac = assel.getGlyphFactory();
+	if (assfac != null) {
+	  if (assfac instanceof GenericAnnotGlyphFactory) {
+            if (DEBUG)  { System.out.println("in XmlStylesheetGlyphFactory.createGlyph(), adding factory to this.annot_factories (and initing): " + meth); }
+	    annot_factories.put(meth, assfac);
+	    // need to make sure factory is initialized!
+	    assfac.init( assel.getPropertyMap());
+            // deal with tiers, or let factory handle it?
+	    assfac.createGlyph(sym, gviewer);
+	    return;
+	  }
+	}
+      }
 
-    TierGlyph[] tiers = gviewer.getTiers(meth, next_to_axis, style, false);
-    int tier_index = (sym.getSpan(0).isForward()) ? 0 : 1;
-    TierGlyph the_tier = tiers[tier_index];
+      if (isContainer(sym)) {
+	for (int i=0; i<sym.getChildCount(); i++) {
+	  createGlyph(sym.getChild(i), gviewer, next_to_axis);
+	}
+	return;
+      }
+      IAnnotStyleExtended style = DefaultStateProvider.getGlobalStateProvider().getAnnotStyle(meth);
+      TierGlyph[] tiers = gviewer.getTiers(meth, next_to_axis, style, false);
+      int tier_index = (sym.getSpan(0).isForward()) ? 0 : 1;
+      TierGlyph the_tier = tiers[tier_index];
 
-    context.clear();
+      context.clear();
 
-    // properties set in this top-level context will be used as defaults,
-    // the stylesheet may over-ride them.
+      // properties set in this top-level context will be used as defaults,
+      // the stylesheet may over-ride them.
 
-    // Allow StyleElement access to the AnnotStyle if it needs it.
-    context.put(IAnnotStyleExtended.class.getName(), style);
-    context.put(TierGlyph.class.getName(), the_tier);
+      // Allow StyleElement access to the AnnotStyle if it needs it.
+      context.put(IAnnotStyleExtended.class.getName(), style);
+      context.put(TierGlyph.class.getName(), the_tier);
 
-    drawable.symToGlyph(gviewer, sym, the_tier, stylesheet, context);
+      drawable.symToGlyph(gviewer, sym, the_tier, stylesheet, context);
+    }
   }
 
 }

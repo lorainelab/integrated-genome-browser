6c6e6ab8c1dab3516e2297c609654ddbf5c5f745
chyekk <chyekk@mycompany.com>
2007-09-27 22:51:44 +0000

    Added generics.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2923 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/tiers/GlyphMinComparator.java
+++ igb/src/com/affymetrix/igb/tiers/GlyphMinComparator.java
@@ -1,5 +1,5 @@
 /**
-*   Copyright (c) 2001-2004 Affymetrix, Inc.
+*   Copyright (c) 2001-2007 Affymetrix, Inc.
 *    
 *   Licensed under the Common Public License, Version 1.0 (the "License").
 *   A copy of the license must be included with any distribution of
@@ -15,13 +15,14 @@ package com.affymetrix.igb.tiers;
 
 import java.util.Comparator;
 import com.affymetrix.genoviz.bioviews.GlyphI;
-import com.affymetrix.genoviz.bioviews.Rectangle2D;
 
-public class GlyphMinComparator implements Comparator {
+public class GlyphMinComparator implements Comparator<GlyphI> {
 
   public int compare(Object obj1, Object obj2) {
-    GlyphI g1 = (GlyphI)obj1;
-    GlyphI g2 = (GlyphI)obj2;
+    return compare((GlyphI)obj1, (GlyphI)obj2);
+  }
+
+  public int compare(GlyphI g1, GlyphI g2) {
     if (g1.getCoordBox().x < g2.getCoordBox().x) { return -1; }
     else if (g1.getCoordBox().x > g2.getCoordBox().x) { return 1; }
     else { return 0; }

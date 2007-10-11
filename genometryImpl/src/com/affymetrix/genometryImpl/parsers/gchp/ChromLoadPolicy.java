1bd0b867cf56d4f5a2cf19ae8566d4e8a73ccfeb
chyekk <chyekk@mycompany.com>
2007-10-11 19:29:14 +0000

    just some javadocs.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@2979 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/gchp/ChromLoadPolicy.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/gchp/ChromLoadPolicy.java
@@ -1,8 +1,24 @@
+/**
+*   Copyright (c) 2007 Affymetrix, Inc.
+*
+*   Licensed under the Common Public License, Version 1.0 (the "License").
+*   A copy of the license must be included with any distribution of
+*   this source code.
+*   Distributions from Affymetrix, Inc., place this in the
+*   IGB_LICENSE.html file.
+*
+*   The license is also available at
+*   http://www.opensource.org/licenses/cpl.php
+*/
 package com.affymetrix.genometryImpl.parsers.gchp;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ *  Specifies which chromosomes to load during file or stream parsing.
+ */
 public abstract class ChromLoadPolicy {
   
   public ChromLoadPolicy() {}

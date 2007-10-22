86df425fdbd94c95f9ca26642b0ae7e2d20d0ecd
gregghelt2 <gregghelt2@mycompany.com>
2007-10-22 01:22:40 +0000

    Changed NetAffx DAS/2 server URL to reflect change in server startup.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3043 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/das2/Das2Discovery.java
+++ igb/src/com/affymetrix/igb/das2/Das2Discovery.java
@@ -35,7 +35,7 @@ public class Das2Discovery {
   static Map cap2version = new LinkedHashMap();
 
   static {
-    name2url.put("NetAffx", "http://netaffxdas.affymetrix.com/das2/sources");
+    name2url.put("NetAffx", "http://netaffxdas.affymetrix.com/das2/genome");
     //    name2url.put("localhost", "http://localhost:9092/das2/genome");
     //    name2url.put("biopackages", "http://das.biopackages.net/das/genome");
     //    name2url.put("Sanger registry", "http://www.spice-3d.org/dasregistry/das2/sources");

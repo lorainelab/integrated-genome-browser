3b77e64138cdc05c2a6e84ce806225feb2e7e8b5
gregghelt2 <gregghelt2@mycompany.com>
2007-10-22 17:18:39 +0000

    Updated IGB_VERSION to 5.01 in preparation for new release.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3055 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/IGBConstants.java
+++ igb/src/com/affymetrix/igb/IGBConstants.java
@@ -23,5 +23,5 @@ public abstract class IGBConstants {
 
   public static String APP_NAME = "Integrated Genome Browser";
   public static String APP_SHORT_NAME = "IGB";
-  public static String IGB_VERSION = "5.00-alpha-prerelease";
+  public static String IGB_VERSION = "5.01";
 }

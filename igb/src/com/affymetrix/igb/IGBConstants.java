b62a1621af7f511c22fd3ac3bf18bf13b44241ca
chyekk <chyekk@mycompany.com>
2007-11-05 19:54:38 +0000

    Increasing the build number to 5.12
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3144 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/IGBConstants.java
+++ igb/src/com/affymetrix/igb/IGBConstants.java
@@ -23,5 +23,5 @@ public abstract class IGBConstants {
 
   public static String APP_NAME = "Integrated Genome Browser";
   public static String APP_SHORT_NAME = "IGB";
-  public static String IGB_VERSION = "5.11_internal";
+  public static String IGB_VERSION = "5.12";
 }

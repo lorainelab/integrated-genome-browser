5a6d916d9bc25512ad4ad8219a4de3b769f56772
chyekk <chyekk@mycompany.com>
2008-06-29 21:03:11 +0000

    removed method getWidget(GlyphI).
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3242 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genoviz_lite/src/com/affymetrix/genoviz/widget/NeoWidgetI.java
+++ genoviz_lite/src/com/affymetrix/genoviz/widget/NeoWidgetI.java
@@ -14,6 +14,7 @@ import com.affymetrix.genoviz.bioviews.GlyphI;
 import com.affymetrix.genoviz.bioviews.SceneI;
 
 import com.affymetrix.genoviz.bioviews.TransformI;
+import com.affymetrix.genoviz.bioviews.ViewI;
 import com.affymetrix.genoviz.event.NeoWidgetListener;
 import java.awt.*;
 import java.awt.event.*;
@@ -201,6 +202,8 @@ public interface NeoWidgetI {
    */
   public void stretchToFit(boolean xstretch, boolean ystretch);
 
+  public ViewI getView();
+  
 
   /**
    * Associates a slider
@@ -418,14 +421,14 @@ public interface NeoWidgetI {
    */
   public NeoWidgetI getWidget(int location);
 
-  /**
-   * If this widget contains other widgets, returns the internal widget
-   *    that contains the given GlyphI.
-   *
-   * @param gl the glyph to search for
-   * @return the component widget.
-   */
-  public NeoWidgetI getWidget(GlyphI gl);
+//  /**
+//   * If this widget contains other widgets, returns the internal widget
+//   *    that contains the given GlyphI.
+//   *
+//   * @param gl the glyph to search for
+//   * @return the component widget.
+//   */
+//  public NeoWidgetI getWidget(GlyphI gl);
 
   /**
    * Updates the visual appearance of the widget.

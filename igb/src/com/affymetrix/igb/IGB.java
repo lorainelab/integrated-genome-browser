27827b3be8e1737fa6a58e01a469b61cbad6d8f3
sgblanch <sgblanch@uncc.edu>
2008-10-03 21:11:48 +0000

    Configure an HTTP user agent for IGB
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3296 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/IGB.java
+++ igb/src/com/affymetrix/igb/IGB.java
@@ -66,7 +66,14 @@ public class IGB extends Application
   implements ActionListener, ContextualPopupListener, GroupSelectionListener, SeqSelectionListener  {
   static IGB singleton_igb;
   public static String APP_NAME = IGBConstants.APP_NAME;
+	public static String APP_SHORT_NAME = IGBConstants.APP_SHORT_NAME;
   public static String APP_VERSION = IGBConstants.IGB_VERSION;
+	/**
+	 * HTTP User Agent presented to servers.  Java version is appended by
+	 * the JRE at runtime.  The user agent is of the format
+	 * "APP_SHORT_NAME/APP_VERSION, os.name/os.version (os.arch)".
+	 */
+	public static String HttpUserAgent = APP_SHORT_NAME + "/" + APP_VERSION + ", " + System.getProperty("os.name") + "/" + System.getProperty("os.version") + " (" + System.getProperty("os.arch") + ")";
 
   public static boolean USE_OVERVIEW = false;
   public static boolean USE_MULTI_WINDOW_MAP = false;
@@ -202,6 +209,9 @@ public class IGB extends Application
    */
   public static void main(String[] args) {
     try {
+
+			// Configure HTTP User agent
+			System.setProperty("http.agent", HttpUserAgent);
 
     // Turn on anti-aliased fonts. (Ignored prior to JDK1.5)
     System.setProperty("swing.aatext", "true");

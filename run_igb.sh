b506f211ed70f729dd6e6ba47e14b795fed5c6e2
sgblanch <sgblanch@uncc.edu>
2009-04-15 19:59:05 +0000

    Make launcher shell script more robust
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3705 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- run_igb.sh
+++ run_igb.sh
@@ -1,16 +1,68 @@
+#!/bin/bash
+#
+# $Id$
+#
+# Simple bash launcher for IGB.  This launcher should be compatible with
+# all UNIX-like systems with bash 2.0 and above.
+#
+# Ways to Specify Java:
+#  - Set JAVA_HOME to point to an installed JDK
+#  - Set JRE_HOME to point to an installed JRE
+#  - Set JAVACMD to point to a working java executable
+#  - Have a java executable in your path
+#
+# Specifying options to the underlying VM.
+#  - Put options in VMARGS variable (app specific)
+#  - Specify on the command line using -J<vmarg>
+#
+# Specifying options to IGB
+#  - Specify directly on command line
 
-# This is an example of a shell script to run igb from jar files
-# on a Unix/Linux system.
-# You may need to edit it for your system.
 
-# IGB requires java 1.5 or higher
+# Do our best to find java
+if [ -z "$JAVACMD" ]; then
+	if [ -n "$JAVA_HOME" ]; then
+		JAVACMD=$JAVA_HOME/bin/java
+	elif [ -n "$JRE_HOME" ]; then
+		JAVACMD=$JRE_HOME/bin/java
+	else
+		JAVACMD=`which java`
+	fi
+fi
 
-export JAVA="`which java`"
+if [ -z "$JAVACMD" ]; then
+	echo "Error: could not find java"
+	exit 127
+fi
 
-$JAVA -mx1024m -jar igb_exe.jar
+# Sort VM arguments from program arguments
+while (( "$#" )); do
+	echo $1 | grep -q '^-J'
+	if [ $? -eq 0 ]; then
+		ARG=`echo $1 | sed -e 's/^-J//'`
+		VMARGS="$VMARGS $ARG"
+	else
+		ARGS="$ARGS $1"
+	fi
+	shift
+done
 
-# Starting with version 4.0, most users no longer need an igb_prefs.xml file.
-# But one is specified here as an example.
+# Find out where the jar files reside
+DIR=`readlink $0`
+test -n "$DIR" && DIR=`dirname $DIR`
+test -z "$DIR" && DIR=`dirname $PWD/$0`
 
-#$JAVA -mx1024m -jar igb_exe.jar -prefs /home/ed/igb_prefs.xml
+# Use 1GiB RAM per default 
+echo $VMARGS | grep -q '\-Xmx\|\-mx' || VMARGS="$VMARGS -Xmx1024m"
 
+# Some Apple Specific settings. Should there be a way to override these?
+if [[ `uname -s` == "Darwin" ]]; then
+	VMARGS="$VMARGS -Dapple.laf.useScreenMenuBar=true"
+	# TODO: Allow spaces in program name
+	VMARGS="$VMARGS -Xdock:name=IGB"
+	VMARGS="$VMARGS -Xdock:icon=$DIR/igb/resources/com/affymetrix/igb/affychip.gif"
+fi
+
+# Launch IGB
+echo $JAVACMD $VMARGS -jar $DIR/igb_exe.jar $ARGS
+$JAVACMD $VMARGS -jar $DIR/igb_exe.jar $ARGS

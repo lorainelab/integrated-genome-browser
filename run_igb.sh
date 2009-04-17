d9114fe7b082103b624dbe6f2920d8d4485ece21
sgblanch <sgblanch@uncc.edu>
2009-04-17 20:19:01 +0000

    Make run_igb.sh more robust
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3711 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- run_igb.sh
+++ run_igb.sh
@@ -3,7 +3,7 @@
 # $Id$
 #
 # Simple bash launcher for IGB.  This launcher should be compatible with
-# all UNIX-like systems with bash 2.0 and above.
+# all UNIX-like systems with a bash interperter.
 #
 # Ways to Specify Java:
 #  - Set JAVA_HOME to point to an installed JDK
@@ -12,56 +12,49 @@
 #  - Have a java executable in your path
 #
 # Specifying options to the underlying VM.
-#  - Put options in VMARGS variable (app specific)
 #  - Specify on the command line using -J<vmarg>
 #
 # Specifying options to IGB
 #  - Specify directly on command line
 
+# Find out where we are installed
+PREFIX=`readlink -f $0`
+PREFIX=`dirname $PREFIX`
+
+# File Locations
+JARDIR="$PREFIX/dist"
+LIBDIR="$PREFIX/ext"
+ICON="$PREFIX/igb/resources/com/affymetrix/igb/affychip.gif"
 
 # Do our best to find java
-if [ -z "$JAVACMD" ]; then
-	if [ -n "$JAVA_HOME" ]; then
-		JAVACMD=$JAVA_HOME/bin/java
-	elif [ -n "$JRE_HOME" ]; then
-		JAVACMD=$JRE_HOME/bin/java
-	else
-		JAVACMD=`which java`
-	fi
-fi
-
-if [ -z "$JAVACMD" ]; then
-	echo "Error: could not find java"
-	exit 127
-fi
+test -z "$JAVACMD" -a -n "$JAVA_HOME" && JAVACMD=$JAVA_HOME/bin/java
+test -z "$JAVACMD" -a -n "$JRE_HOME"  && JAVACMD=$JRE_HOME/bin/java
+test -z "$JAVACMD" && JAVACMD=`which java` 2> /dev/null
+test -z "$JAVACMD" && echo "$0: could not find java" >&2 && exit 127
 
 # Sort VM arguments from program arguments
 while (( "$#" )); do
 	echo $1 | grep -q '^-J'
 	if [ $? -eq 0 ]; then
-		ARG=`echo $1 | sed -e 's/^-J//'`
-		VMARGS="$VMARGS $ARG"
+		VMARGS[${#VMARGS[*]}]=`echo $1 | sed -e 's/^-J//'`
 	else
-		ARGS="$ARGS $1"
+		ARGS[${#ARGS[*]}]=$1
 	fi
 	shift
 done
 
-# Find out where the jar files reside
-DIR=`readlink -f $0`
-DIR=`dirname $DIR`
-
 # Use 1GiB RAM per default 
-echo $VMARGS | grep -q '\-Xmx\|\-mx' || VMARGS="$VMARGS -Xmx1024m"
+echo $VMARGS[*] | grep -q '\-Xmx\|\-mx' || VMARGS[${#VMARGS[*]}]="-Xmx1024m"
 
 # Some Apple Specific settings. Should there be a way to override these?
 if [[ `uname -s` == "Darwin" ]]; then
-	VMARGS="$VMARGS -Dapple.laf.useScreenMenuBar=true"
+	VMARGS[${#VMARGS[*]}]="-Dapple.laf.useScreenMenuBar=true"
 	# TODO: Allow spaces in program name
-	VMARGS="$VMARGS -Xdock:name=IGB"
-	VMARGS="$VMARGS -Xdock:icon=$DIR/igb/resources/com/affymetrix/igb/affychip.gif"
+	VMARGS[${#VMARGS[*]}]="-Xdock:name=Integrated Genome Browser"
+	VMARGS[${#VMARGS[*]}]="-Xdock:icon=$ICON"
 fi
 
 # Launch IGB
-echo $JAVACMD $VMARGS -jar $DIR/igb_exe.jar $ARGS
-$JAVACMD $VMARGS -jar $DIR/igb_exe.jar $ARGS
+IFS="
+"
+$JAVACMD ${VMARGS[*]} -jar $PREFIX/igb_exe.jar ${ARGS[*]}

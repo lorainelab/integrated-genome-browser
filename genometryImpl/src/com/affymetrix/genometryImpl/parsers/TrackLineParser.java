2ba14c8c156cfcf3d84a1bc9b243ef79d398487e
geekofnature <geekofnature@mycompany.com>
2009-05-14 22:27:45 +0000

    Reverted modifications to paths / track names due to an error caused with multiple tracks within one file
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3725 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/TrackLineParser.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/TrackLineParser.java
@@ -16,6 +16,7 @@ package com.affymetrix.genometryImpl.parsers;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.style.*;
 import java.awt.Color;
+import java.io.File;
 
 import java.util.*;
 import java.util.regex.*;
@@ -132,9 +133,13 @@ public final class TrackLineParser {
 	 *  A default track name must be provided in case none is specified by the
 	 *  track line itself.
 	 */
-	static IAnnotStyle createAnnotStyle(AnnotatedSeqGroup seq_group, Map<String,String> track_hash, String default_track_name) {
-		String name = default_track_name;
-		track_hash.put(NAME, default_track_name);
+	static IAnnotStyle createAnnotStyle(AnnotatedSeqGroup seq_group,
+			Map<String,String> track_hash, String default_track_name) {
+		String name = track_hash.get(NAME);
+		if (name == null) {
+			track_hash.put(NAME, default_track_name);
+			name = default_track_name;
+		}
 		StateProvider provider = seq_group.getStateProvider();
 		IAnnotStyle style = provider.getAnnotStyle(name);
 		applyTrackProperties(track_hash, style);

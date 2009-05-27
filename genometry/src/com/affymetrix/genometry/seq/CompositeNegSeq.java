dfad5672d15f41a7b57596806b5d05b78c050368
jnicol <jnicol@uncc.edu>
2009-05-27 16:13:25 +0000

    Combine SimpleCompositeBioSeq with CompositeNeqSeq.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3755 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometry/src/com/affymetrix/genometry/seq/CompositeNegSeq.java
+++ genometry/src/com/affymetrix/genometry/seq/CompositeNegSeq.java
@@ -16,6 +16,7 @@ package com.affymetrix.genometry.seq;
 
 import com.affymetrix.genometry.*;
 import com.affymetrix.genometry.span.*;
+import com.affymetrix.genometry.util.SeqUtils;
 
 /**
  *  A CompositeBioSeq that can have start less than 0.
@@ -31,43 +32,40 @@ import com.affymetrix.genometry.span.*;
  *  CompositeBioSeq with the desired zero-point at 0 (which pushes
  *  coords 5' to zero-point into negative coords)
  */
-public class CompositeNegSeq extends SimpleCompositeBioSeq {
+public class CompositeNegSeq implements CompositeBioSeq {
+	private static final boolean DEBUG_GET_RESIDUES = false;
 
 	/** The index of the first residue of the sequence. */
 	int start;
 	/** The index of the last residue of the sequence. */
 	int end;
-	//  double start;
-	//  double end;
-
 	/**
-	 *  Constructor. Requires that min less than max.
+	 * SeqSymmetry to store the sequence in.
 	 */
-	/*public CompositeNegSeq(String id, int min, int max) {
-	  this(id);
-	  this.start = min;
-	  this.end = max;
-	  if (min > max) {
-	  throw new IllegalArgumentException("problem in CompositeNegSeq constructor! min must be < max");
-	  }
-	//    this.length = end - start;
-	this.length = (double)end - (double)start;
-	}*/
+	protected SeqSymmetry compose;
+	
+	/**
+	 * Length of the sequence, stored as a double.  The value is always an
+	 * integer and much of the functionality of this class and its sub-classes
+	 * is lost if the length is greater than Integer.INT_MAX.
+	 */
+	protected double length = 0;
+	/**
+	 * String identifier for the sequence.  This is not guaranteed to be unique.
+	 */
+	private final String id;
 
 
 	public CompositeNegSeq(String id, int length) {
-		super(id);
+		this.id = id;
 		this.length = length;
 		start = 0;
 		end = length;
 	}
 
 	public CompositeNegSeq(String id) {
-		super(id);
+		this.id = id;
 	}
-
-	//public CompositeNegSeq() { }
-
 
 	/**
 	 * Returns the integer index of the first residue of the sequence.  Negative
@@ -128,7 +126,6 @@ public class CompositeNegSeq extends SimpleCompositeBioSeq {
 	 *
 	 * @return true if all residues on the sequence are available.
 	 */
-	@Override
 		public boolean isComplete() {
 			return isComplete(start, end);
 		}
@@ -138,7 +135,6 @@ public class CompositeNegSeq extends SimpleCompositeBioSeq {
 	 *
 	 * @return a String containing all residues on the sequence.
 	 */
-	@Override
 		public String getResidues() {
 			return getResidues(start, end);
 		}
@@ -153,7 +149,6 @@ public class CompositeNegSeq extends SimpleCompositeBioSeq {
 	 * @param  fillchar the character to fill empty residues in the sequence with.
 	 * @return          a String containing residues between start and end.
 	 */
-	@Override
 		public String getResidues(int res_start, int res_end, char fillchar) {
 			SeqSpan residue_span = new SimpleSeqSpan(res_start, res_end, this);
 			int reslength = Math.abs(res_end - res_start);
@@ -163,9 +158,139 @@ public class CompositeNegSeq extends SimpleCompositeBioSeq {
 			if (rootsym == null)  { return null; }
 			// adjusting index into array to compensate for possible seq start < 0
 			int array_offset = -start;
-			getResidues(residue_span, fillchar, rootsym, char_array, array_offset);
+			getResidues(residue_span, rootsym, char_array);
 			// Note that new String(char[]) causes the allocation of a second char array
 			String res = new String(char_array);
 			return res;
 		}
+
+	/**
+	 * Function for finding residues.  This function is a bit of a mess:
+	 * the implementation is more confusing than it needs to be.
+	 *
+	 * @param this_residue_span the SeqSpan to find residues on
+	 * @param sym               the SeqSymmetry to search for residues
+	 * @param residues          the character array to be filled with residues
+	 */
+	protected void getResidues(SeqSpan this_residue_span, SeqSymmetry sym, char[] residues) {
+		int symCount = sym.getChildCount();
+		if (symCount == 0) {
+			SeqSpan this_comp_span = sym.getSpan(this);
+			if (SeqUtils.intersects(this_comp_span, this_residue_span)) {
+				BioSeq other_seq = SeqUtils.getOtherSeq(sym, this);
+				SeqSpan other_comp_span = sym.getSpan(other_seq);
+				MutableSeqSpan ispan = new SimpleMutableSeqSpan();
+				boolean intersects = SeqUtils.intersection(this_comp_span, this_residue_span, ispan);
+				MutableSeqSpan other_residue_span = new SimpleMutableSeqSpan();
+				SeqUtils.transformSpan(ispan, other_residue_span, other_seq, sym);
+				boolean opposite_strands = this_comp_span.isForward() ^ other_comp_span.isForward();
+				boolean resultForward = opposite_strands ^ this_residue_span.isForward();
+				String spanResidues;
+				if (resultForward) {
+					spanResidues = other_seq.getResidues(other_residue_span.getMin(), other_residue_span.getMax());
+				} else {
+					spanResidues = other_seq.getResidues(other_residue_span.getMax(), other_residue_span.getMin());
+				}
+				if (spanResidues != null) {
+					if (DEBUG_GET_RESIDUES) {
+						System.out.println(spanResidues.substring(0, 15) + "..." + spanResidues.substring(spanResidues.length() - 15));
+						System.out.println("desired span: " + SeqUtils.spanToString(this_residue_span));
+						System.out.println("child residue span: " + SeqUtils.spanToString(this_comp_span));
+						System.out.println("intersect(child_span, desired_span): " + SeqUtils.spanToString(ispan));
+						System.out.println("other seq span: " + SeqUtils.spanToString(other_residue_span));
+						System.out.println("opposite strands: " + opposite_strands);
+						System.out.println("result forward: " + resultForward);
+						System.out.println("start < end: " + (other_residue_span.getStart() < other_residue_span.getEnd()));
+						System.out.println("");
+					}
+					int offset = ispan.getMin() - this_residue_span.getMin();
+					for (int j = 0; j < spanResidues.length(); j++) {
+						residues[offset + j] = spanResidues.charAt(j);
+					}
+				}
+			}
+		} else {
+			// recurse to children
+			for (int i = 0; i < symCount; i++) {
+				SeqSymmetry childSym = sym.getChild(i);
+				getResidues(this_residue_span, childSym, residues);
+			}
+		}
+	}
+
+	public SeqSymmetry getComposition() {
+		return compose;
+	}
+
+	public void setComposition(SeqSymmetry compose) {
+		this.compose = compose;
+	}
+
+	/**
+	 * Returns the number of residues in the sequence as a double.
+	 *
+	 * @return the number of residues in the sequence as a double
+	 */
+	public double getLengthDouble() {
+		return length;
+	}
+
+	public int getLength() {
+		if (length > Integer.MAX_VALUE) {
+			return Integer.MAX_VALUE - 1;
+		} else {
+			return (int) length;
+		}
+	}
+
+	/**
+	 * Returns true if all residues between start and end are available.  Unknown
+	 * if implementations of this function are inclusive or exclusive on start
+	 * and end.
+	 * <p />
+	 * <em>WARNING:</em> This implementation is flawed.  It only verifies that
+	 * all SeqSymmetrys are complete, not that the SeqSymmetrys completely
+	 * cover the range in question.
+	 *
+	 * @param  start the start index (inclusive?)
+	 * @param  end   the end index (exclusive?)
+	 * @return       true if all residues betwen start and end are available
+	 */
+	public boolean isComplete(int start, int end) {
+		// assuming that if all sequences the composite is composed of are
+		//    complete, then composite is also complete
+		//    [which is an invalid assumption! Because that further assumes that composed seq
+		//     is fully covered by the sequences that it is composed from...]
+		SeqSymmetry rootsym = this.getComposition();
+
+		if (rootsym == null) {
+			return false;
+		}
+		int comp_count = rootsym.getChildCount();
+		if (comp_count == 0) {
+			BioSeq other_seq = SeqUtils.getOtherSeq(rootsym, this);
+			return other_seq.isComplete(start, end);
+		}
+
+		boolean comp_complete = true;
+		for (int i = 0; i < comp_count; i++) {
+			SeqSymmetry comp_sym = rootsym.getChild(i);
+			BioSeq other_seq = SeqUtils.getOtherSeq(comp_sym, this);
+			comp_complete = (comp_complete & other_seq.isComplete());
+		}
+		return comp_complete;
+	}
+
+	public String getID() {
+		return id;
+	}
+
+	/**
+	 * Returns all residues on the sequence.
+	 *
+	 * @return a String containing all residues on the sequence
+	 */
+	public String getResidues(int start, int end) {
+		return getResidues(start, end, ' ');
+	}
 }

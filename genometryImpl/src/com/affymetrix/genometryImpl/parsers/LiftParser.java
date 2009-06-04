lorainelab/integrated-genome-browser

commit 3c8b1fba05dbf8e3acaebcfa28d938f0172f49fe

Merge SimpleAnnotatedBioSeq into SmartAnnotBioSeq. Change AnnotatedBioSeq interface to include getVersion().
Fix some warnings.



git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3766 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

genometryImpl/src/com/affymetrix/genometryImpl/parsers/LiftParser.java
@@ -19,7 +19,6 @@ import java.util.*;
 import java.util.regex.*;
 
 import com.affymetrix.genometry.*;
-import com.affymetrix.genometry.seq.*;
 import com.affymetrix.genometry.span.*;
 import com.affymetrix.genometry.util.SeqUtils;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
@@ -111,9 +110,9 @@ public final class LiftParser {
 				String splitname[] = re_name.split(tempname);
 				String contig_name = splitname[CONTIG_NAME_SUBFIELD];
 				// experimenting with constructing virtual sequences by using chromosomes as contigs
-				MutableAnnotatedBioSeq contig = seq_group.getSeq(contig_name);
+				SmartAnnotBioSeq contig = seq_group.getSeq(contig_name);
 				if (contig == null) {
-					contig = new SimpleAnnotatedBioSeq(contig_name, match_length);
+					contig = new SmartAnnotBioSeq(contig_name, contig.getVersion(), match_length);
 				}
 
 				contig_count++;

genometry/src/com/affymetrix/genometry/AnnotatedBioSeq.java
@@ -14,7 +14,7 @@
 package com.affymetrix.genometry;
 
 public interface AnnotatedBioSeq extends BioSeq {
-
+	public String getVersion();
 	public int getAnnotationCount();
 	public SeqSymmetry getAnnotation(int index);
 

genometry/src/com/affymetrix/genometry/seq/SimpleAnnotatedBioSeq.java
@@ -1,113 +1,0 @@
-package com.affymetrix.genometry.seq;
-
-import java.util.*;
-
-import com.affymetrix.genometry.util.DNAUtils;
-import com.affymetrix.genometry.MutableAnnotatedBioSeq;
-import com.affymetrix.genometry.SeqSymmetry;
-
-public final class SimpleAnnotatedBioSeq
-	implements MutableAnnotatedBioSeq {
-
-	protected List<SeqSymmetry> annots;
-	protected String id;
-	protected int length;
-	protected String residues;
-
-	public SimpleAnnotatedBioSeq(String id, String residues) {
-		this.id = id;
-		this.setResidues(residues);
-	}
-
-	public SimpleAnnotatedBioSeq(String id, int length)  {
-		this.id = id;
-		this.length = length;
-	}
-
-	public String getID() { return id; }
-	public int getLength() { return length; }
-	public String getResidues() {
-		return residues;
-	}
-
-	public void addAnnotation(SeqSymmetry annot) {
-		if (null == annots) { annots = new ArrayList<SeqSymmetry>(); }
-		annots.add(annot);
-	}
-
-	public void removeAnnotation(SeqSymmetry annot) {
-		if (null != annots) {
-			annots.remove(annot);
-		}
-	}
-
-	public int getAnnotationCount() {
-		if (null != annots) {
-			return annots.size();
-		}
-		else return 0;
-	}
-
-	public SeqSymmetry getAnnotation(int index) {
-		if (null != annots && index < annots.size()) {
-			return annots.get(index);
-		}
-		else {
-			return null;
-		}
-	}
-
-	//public void setID(String id) { this.id = id; }
-	public void setLength(int length) {
-		this.length = length;
-		// if length does not agree with length of residues, null out residues
-		if ((residues != null) && (residues.length() != length)) {
-			residues = null;
-		}
-	}
-
-	public boolean isComplete() {
-		return residues != null;
-	}
-
-	public boolean isComplete(int start, int end) {
-		if (residues == null) {
-			return false;
-		}
-		return residues.length() >= end;
-	}
-
-	public void setResidues(String residues) {
-		this.residues = residues;
-		this.length = residues.length();
-	}
-
-	/**
-	 * Get residues.
-	 * Assumptions. Start at 0, end at start + length, e.g. 0, 1 is 1 character.
-	 *
-	 * @return string containing characters between start and end inclusive.
-	 * If end < start, the reverse complement string will be returned.
-	 */
-	public String getResidues(int start, int end) {
-		if (residues == null) {
-			return null;
-		}
-		int residue_length = this.getLength();
-		if (start < 0 || residue_length <= 0) {
-			return null;
-		}
-		// Sanity checks on argument size.
-		start = Math.min(start, residue_length);
-		end = Math.min(end, residue_length);
-		if (start <= end) {
-			end = Math.min(end, start + residue_length);
-		} else {
-			start = Math.min(start, end + residue_length);
-		}
-		if (start <= end) {
-			return residues.substring(start, end);
-		}
-		return DNAUtils.reverseComplement(residues.substring(end, start));
-	}
-}

genometryImpl/src/com/affymetrix/genometryImpl/SeqSymmetryConverter.java
@@ -14,13 +14,10 @@
 package com.affymetrix.genometryImpl;
 
 import com.affymetrix.genometry.*;
-//import com.affymetrix.genometry.seq.*;
-import com.affymetrix.genometry.seq.SimpleAnnotatedBioSeq;
 import com.affymetrix.genometry.span.SimpleSeqSpan;
-//import com.affymetrix.genometry.span.*;
 
 /**
- *  Routines to convert generic SeqSymmetry's to PSL SeqSymmetry's.
+ *  Routines to convert generic SeqSymmetrys to PSL SeqSymmetrys.
  */
 public final class SeqSymmetryConverter {
 
@@ -100,7 +97,7 @@ public final class SeqSymmetryConverter {
 			curlength += child_tspan.getLength();
 		}
 
-		MutableBioSeq queryseq = new SimpleAnnotatedBioSeq(qname, curlength);
+		MutableBioSeq queryseq = new SmartAnnotBioSeq(qname, qname, curlength);
 		SeqSpan qspan = new SimpleSeqSpan(0, curlength, queryseq);
 
 		UcscPslSym pslsym = new UcscPslSym(type, -1, -1, -1, -1,

genometryImpl/src/com/affymetrix/genometryImpl/parsers/BrptParser.java
@@ -16,12 +16,12 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.Pattern;
 import com.affymetrix.genometry.*;
-import com.affymetrix.genometry.seq.*;
 import com.affymetrix.genometry.span.*;
 import com.affymetrix.genometry.symmetry.*;
 import com.affymetrix.genometryImpl.SimpleSymWithProps;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.SingletonGenometryModel;
+import com.affymetrix.genometryImpl.SmartAnnotBioSeq;
 import com.affymetrix.genometryImpl.SymWithProps;
 import com.affymetrix.genometryImpl.util.GeneralUtils;
 
@@ -114,7 +114,7 @@ public final class BrptParser {
 	}
 
 	public List<SeqSymmetry> readTextFormat(BufferedReader br) throws IOException {
-		int weird_length_count = 0;
+		//int weird_length_count = 0;
 		Map<String,MutableSeqSymmetry> id2psym = new HashMap<String,MutableSeqSymmetry>();
 		ArrayList<SeqSymmetry> parent_syms = new ArrayList<SeqSymmetry>();
 		int repeat_count = 0;
@@ -130,7 +130,7 @@ public final class BrptParser {
 				MutableSeqSymmetry psym = id2psym.get(seqid);
 				if (psym == null) {
 					psym = new SimpleSymWithProps();
-					seq = new SimpleAnnotatedBioSeq(seqid, 1000000000);
+					seq = new SmartAnnotBioSeq(seqid, seqid, 1000000000);
 					psym.addSpan(new SimpleSeqSpan(0, 1000000000, seq));
 					((SymWithProps) psym).setProperty(SimpleSymWithProps.CONTAINER_PROP, Boolean.TRUE);
 					id2psym.put(seqid, psym);
@@ -168,7 +168,7 @@ public final class BrptParser {
 		return parent_syms;
 	}
 
-	public List parse(InputStream istr, String annot_type, AnnotatedSeqGroup seq_group, boolean annot_seq) 
+	public List<SeqSymmetry> parse(InputStream istr, String annot_type, AnnotatedSeqGroup seq_group, boolean annot_seq)
 		throws IOException {
 		System.out.println("parsing brpt file");
 		List<SeqSymmetry> rpt_syms = null;

genometryImpl/src/com/affymetrix/genometryImpl/parsers/BsnpParser.java
@@ -17,7 +17,6 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 import com.affymetrix.genometry.*;
-import com.affymetrix.genometry.seq.*;
 import com.affymetrix.genometry.span.*;
 import com.affymetrix.genometryImpl.UcscGffSym;
 import com.affymetrix.genometryImpl.SimpleSymWithProps;
@@ -26,6 +25,7 @@ import com.affymetrix.genometryImpl.EfficientSnpSym;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.GenometryModel;
 import com.affymetrix.genometryImpl.SingletonGenometryModel;
+import com.affymetrix.genometryImpl.SmartAnnotBioSeq;
 import com.affymetrix.genometryImpl.SymWithProps;
 import com.affymetrix.genometryImpl.util.Timer;
 
@@ -93,7 +93,7 @@ import com.affymetrix.genometryImpl.util.Timer;
  *
  *</pre>
  */
-public final class BsnpParser {
+public abstract class BsnpParser {
 	//  static String default_text_infile = "c:/data/ucsc/hg15/";
 	//  static String genome_version = "H_sapiens_Apr_2003";
 
@@ -101,11 +101,11 @@ public final class BsnpParser {
 	//  type enum('SNP','INDEL','SEGMENTAL','unknown') NOT NULL default 'unknown',
 
 	//  static Pattern line_regex = Pattern.compile("\t");
-	static Pattern line_regex = Pattern.compile("\\s+");  // replaced single tab with one or more whitespace
-	Map source_hash = new HashMap();
-	Map type_hash = new HashMap();
+	private static final Pattern line_regex = Pattern.compile("\\s+");  // replaced single tab with one or more whitespace
+	//Map source_hash = new HashMap();
+	//Map type_hash = new HashMap();
 
-	public void outputBsnpFormat(List<SeqSymmetry> parents, String genome_version, DataOutputStream dos) 
+	private static void outputBsnpFormat(List<SeqSymmetry> parents, String genome_version, DataOutputStream dos)
 		throws IOException {
 		try        {
 			int pcount = parents.size();
@@ -153,17 +153,15 @@ public final class BsnpParser {
 chr1        XbaI        SNP_A-1507333        219135381        219135381        .        +        .
 	 *</pre>
 	 */
-	public List<SeqSymmetry> readGffFormat(InputStream istr, GenometryModel gmodel) throws IOException {
+	private static List<SeqSymmetry> readGffFormat(InputStream istr, GenometryModel gmodel) throws IOException {
 		AnnotatedSeqGroup seq_group = gmodel.addSeqGroup("Test Group");
 
 		List<SeqSymmetry> results = new ArrayList<SeqSymmetry>();
 		try {
 			GFFParser gff_parser = new GFFParser();
 			gff_parser.parse(istr, seq_group, true);
-			Iterator iter = seq_group.getSeqList().iterator();
 			int problem_count = 0;
-			while (iter.hasNext()) {
-				AnnotatedBioSeq aseq = (AnnotatedBioSeq)iter.next();
+			for (SmartAnnotBioSeq aseq : seq_group.getSeqList()) {
 				int acount = aseq.getAnnotationCount();
 				String seqid = aseq.getID();
 				System.out.println("seq = " + seqid + ", annots = " + acount);
@@ -172,7 +170,7 @@ chr1        XbaI        SNP_A-1507333        219135381        219135381        .
 				// need to debug this eventually...
 				if (acount >= 1) { 
 					SimpleSymWithProps new_psym = new SimpleSymWithProps();
-					MutableAnnotatedBioSeq seq = new SimpleAnnotatedBioSeq(seqid, 1000000000);
+					MutableAnnotatedBioSeq seq = new SmartAnnotBioSeq(seqid, aseq.getVersion(), 1000000000);
 					new_psym.addSpan(new SimpleSeqSpan(0, 1000000000, seq));
 					new_psym.setProperty(SimpleSymWithProps.CONTAINER_PROP, Boolean.TRUE);
 					for (int k=0; k<acount; k++) {
@@ -205,7 +203,7 @@ chr1        XbaI        SNP_A-1507333        219135381        219135381        .
 	}
 
 
-	public List<SeqSymmetry> readTextFormat(BufferedReader br) {
+	private static List<SeqSymmetry> readTextFormat(BufferedReader br) {
 		int snp_count = 0;
 		int weird_length_count = 0;
 		Map<String,SeqSymmetry> id2psym = new HashMap<String,SeqSymmetry>();
@@ -218,7 +216,7 @@ chr1        XbaI        SNP_A-1507333        219135381        219135381        .
 				MutableSeqSymmetry psym = (MutableSeqSymmetry)id2psym.get(seqid);
 				if (psym == null) {
 					psym = new SimpleSymWithProps();
-					MutableAnnotatedBioSeq seq = new SimpleAnnotatedBioSeq(seqid, 1000000000);
+					MutableAnnotatedBioSeq seq = new SmartAnnotBioSeq(seqid, seqid, 1000000000);
 					psym.addSpan(new SimpleSeqSpan(0, 1000000000, seq));
 					((SymWithProps) psym).setProperty(SimpleSymWithProps.CONTAINER_PROP, Boolean.TRUE);
 					id2psym.put(seqid, psym);
@@ -248,7 +246,7 @@ chr1        XbaI        SNP_A-1507333        219135381        219135381        .
 		return parent_syms;
 	}
 
-	public List<SeqSymmetry> parse(InputStream istr, String annot_type, AnnotatedSeqGroup seq_group, boolean annot_seq) 
+	public static List<SeqSymmetry> parse(InputStream istr, String annot_type, AnnotatedSeqGroup seq_group, boolean annot_seq)
 		throws IOException {
 		//System.out.println("parsing bsnp file");
 		Timer tim = new Timer();
@@ -312,22 +310,19 @@ chr1        XbaI        SNP_A-1507333        219135381        219135381        .
 		return snp_syms;
 	}
 
-
-
-	static boolean TEST_BINARY_PARSE = false;
-
 	public static void main(String[] args) {
 		SingletonGenometryModel gmodel = SingletonGenometryModel.getGenometryModel();
 		try {
+			boolean TEST_BINARY_PARSE = false;
 			if (TEST_BINARY_PARSE) {
 				AnnotatedSeqGroup seq_group = gmodel.addSeqGroup("Test Group");
 
 				String binfile = args[0];
 				System.out.println("parsing in snp data from .bsnp file: " + binfile);
-				BsnpParser tester = new BsnpParser();
+				//BsnpParser tester = new BsnpParser();
 				File ifil = new File(binfile);
 				InputStream istr = new FileInputStream(ifil);
-				tester.parse(istr, "snp", seq_group, true);
+				BsnpParser.parse(istr, "snp", seq_group, true);
 				System.out.println("finished parsing in snp data from .bsnp file");
 				istr.close();
 			}
@@ -347,26 +342,26 @@ chr1        XbaI        SNP_A-1507333        219135381        219135381        .
 					else {
 						bin_outfile = text_infile + ".bsnp";
 					}
-					BsnpParser tester = new BsnpParser();
+					//BsnpParser tester = new BsnpParser();
 					File ifil = new File(text_infile);
 					List<SeqSymmetry> parent_syms = null;
 					if (text_infile.endsWith(".txt")) {
 						BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(ifil)));
 						System.out.println("reading in text data from: " + text_infile);
-						parent_syms = tester.readTextFormat(br);
+						parent_syms = BsnpParser.readTextFormat(br);
 						br.close();
 					}
 					else if (text_infile.endsWith(".gff")) {
 						InputStream istr = new FileInputStream(ifil);
 						System.out.println("reading in gff data from: " + text_infile);
-						parent_syms = tester.readGffFormat(istr, gmodel);
+						parent_syms = BsnpParser.readGffFormat(istr, gmodel);
 						istr.close();
 					}
 
 					File ofil = new File(bin_outfile);
 					DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(ofil)));
 					System.out.println("outputing binary data to: " + bin_outfile);
-					tester.outputBsnpFormat(parent_syms, genome_version, dos);
+					BsnpParser.outputBsnpFormat(parent_syms, genome_version, dos);
 					dos.close();
 					System.out.println("finished converting text data to binary .bsnp format");
 				}

genometryImpl/src/com/affymetrix/genometryImpl/parsers/Das2FeatureSaxParser.java
@@ -22,7 +22,6 @@ import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.*;
 
 import com.affymetrix.genometry.*;
-import com.affymetrix.genometry.seq.*;
 import com.affymetrix.genometry.span.*;
 import com.affymetrix.genometry.util.SeqUtils;
 import com.affymetrix.genometryImpl.SmartAnnotBioSeq;

genometryImpl/src/com/affymetrix/genometryImpl/parsers/ScoredMapParser.java
@@ -14,12 +14,12 @@
 package com.affymetrix.genometryImpl.parsers;
 
 import com.affymetrix.genometry.*;
-import com.affymetrix.genometry.seq.SimpleAnnotatedBioSeq;
 import com.affymetrix.genometry.span.SimpleSeqSpan;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.IndexedSingletonSym;
 import com.affymetrix.genometryImpl.ScoredContainerSym;
 import com.affymetrix.genometryImpl.SimpleSymWithProps;
+import com.affymetrix.genometryImpl.SmartAnnotBioSeq;
 import com.affymetrix.genometryImpl.util.FloatList;
 
 import java.io.*;
@@ -106,7 +106,7 @@ public final class ScoredMapParser {
 		System.out.println("testing ScoredMapParser, parsing file: " + test_file);
 		ScoredMapParser tester = new ScoredMapParser();
 		AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
-		MutableAnnotatedBioSeq aseq = new SimpleAnnotatedBioSeq("test_seq", 50000000);
+		MutableAnnotatedBioSeq aseq = new SmartAnnotBioSeq("test_seq", "test_version", 50000000);
 		try {
 			FileInputStream fis = new FileInputStream(new File(test_file));
 			tester.parse(fis, test_name, aseq, seq_group);

commit 141cd8a3d07a3be7119760331a79b3b96c7f9e6d

Fix a NullReference exception introduced in r3766.
git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3790 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

genometryImpl/src/com/affymetrix/genometryImpl/parsers/LiftParser.java
@@ -112,7 +112,7 @@ public final class LiftParser {
 				// experimenting with constructing virtual sequences by using chromosomes as contigs
 				SmartAnnotBioSeq contig = seq_group.getSeq(contig_name);
 				if (contig == null) {
-					contig = new SmartAnnotBioSeq(contig_name, contig.getVersion(), match_length);
+					contig = new SmartAnnotBioSeq(contig_name, "", match_length);
 				}
 
 				contig_count++;
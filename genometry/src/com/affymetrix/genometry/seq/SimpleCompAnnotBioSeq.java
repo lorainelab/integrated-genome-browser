a95485ab08374e775546b519467b39b460dfaea4
jnicol <jnicol@uncc.edu>
2009-06-10 16:41:23 +0000

    The merge of the Seq files in revision 3759 introduced a bug in copying selected residues.  Reverting these files back to revision 3758, for now.
    (Also adding an edge-case test to GeneralBioSeq -- was added to 
    SmartAnnotBioSeq in revision 3778.)
    
    
    
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3816 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometry/src/com/affymetrix/genometry/seq/SimpleCompAnnotBioSeq.java
@@ -0,0 +1,121 @@
+/**
+ *   Copyright (c) 2001-2007 Affymetrix, Inc.
+ *
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
+
+package com.affymetrix.genometry.seq;
+
+import com.affymetrix.genometry.*;
+import com.affymetrix.genometry.util.DNAUtils;
+import java.util.*;
+
+public abstract class SimpleCompAnnotBioSeq
+	extends CompositeNegSeq
+	implements MutableAnnotatedBioSeq {
+
+	private static final boolean DEBUG = false;
+	// GAH 8-14-2002: need a residues field in case residues need to be cached
+	// (rather than derived from composition), or if we choose to store residues here
+	// instead of in composition seqs in case we actually want to compose/cache
+	// all residues...
+	private String residues;
+
+	private List<SeqSymmetry> annots;
+
+	public SimpleCompAnnotBioSeq(String id, int length)  {
+		super(id, length);
+	}
+
+	@Override
+	public String getResidues(int start, int end, char fillchar) {
+		int residue_length = this.getLength();
+		if (start < 0 || residue_length <= 0) {
+			return null;
+		}
+
+		// Sanity checks on argument size.
+		start = Math.min(start, residue_length);
+		end = Math.min(end, residue_length);
+		if (start <= end) {
+			end = Math.min(end, start+residue_length);
+		}
+		else {
+			start = Math.min(start, end+residue_length);
+		}
+
+		if (residues == null) {
+			return super.getResidues(start, end, fillchar);
+		}
+
+		if (start <= end) {
+			return residues.substring(start, end);
+		}
+
+		// start > end -- that means reverse complement.
+		return DNAUtils.reverseComplement(residues.substring(end, start));
+	}
+
+	@Override
+	public boolean isComplete(int start, int end) {
+		if (residues != null) { return true; }
+		else  { return super.isComplete(start, end); }
+	}
+
+	
+
+	public void addAnnotation(SeqSymmetry annot) {
+		if (null == annots) { annots = new ArrayList<SeqSymmetry>(); }
+		annots.add(annot);
+	}
+
+	public void removeAnnotation(SeqSymmetry annot) {
+		if (null != annots) {
+			annots.remove(annot);
+		}
+	}
+
+	public int getAnnotationCount() {
+		if (null != annots) return annots.size();
+		else return 0;
+	}
+
+	public SeqSymmetry getAnnotation(int index) {
+		if (null != annots && index < annots.size())
+			return annots.get(index);
+		else
+			return null;
+	}
+
+	public void setLength(int length) {
+		setBounds(0, length);  // sets start, end, bounds
+
+		// if length does not agree with length of residues, null out residues
+		if ((residues != null) && (residues.length() != length)) {
+			System.out.println("*** WARNING!!! lengths disagree: residues = " + residues.length() +
+					", seq = " + this.length + ", nulling out residues ****");
+			residues = null;
+		}
+	}
+
+	public void setResidues(String residues) {
+		if (DEBUG)  { System.out.println("**** called SimpleCompAnnotBioSeq.setResidues()"); }
+		if (residues.length() != this.length) {
+			System.out.println("********************************");
+			System.out.println("*** WARNING!!! lengths disagree: residues = " + residues.length() +
+					", seq = " + this.length + " ****");
+			System.out.println("********************************");
+		}
+		this.residues = residues;
+		this.length = residues.length();
+	}
+}
+
+
---
+++ genometryImpl/src/com/affymetrix/genometryImpl/GeneralBioSeq.java
@@ -0,0 +1,101 @@
+package com.affymetrix.genometryImpl;
+
+import com.affymetrix.genometry.seq.SimpleCompAnnotBioSeq;
+import com.affymetrix.genometryImpl.util.SearchableCharIterator;
+
+/**
+ *  <pre>
+ *  Implements SearchableCharacterIterator to allow for regex matching
+ *    without having to build a String out of the byte array (which would kind of
+ *    defeat the purpose of saving memory...)
+ * </pre>
+ */
+public abstract class GeneralBioSeq extends SimpleCompAnnotBioSeq
+	implements SearchableCharIterator {
+
+	String version;
+	SearchableCharIterator residues_provider;
+
+	public GeneralBioSeq(String seqid, String seqversion, int length) {
+		super(seqid, length);
+		this.version = seqversion;
+	}
+
+	public String getVersion() { return version; }
+	public void setVersion(String str) { this.version = str; }
+
+	public void setResiduesProvider(SearchableCharIterator chariter) {
+		if (chariter.getLength() != this.getLength()) {
+			System.out.println("WARNING -- in setResidueProvider, lengths don't match");
+		}
+		residues_provider = chariter;
+	}
+
+	public SearchableCharIterator getResiduesProvider() {
+		return residues_provider;
+	}
+
+
+	/** Gets residues.
+	 *  @param fillchar  Character to use for missing residues;
+	 *     warning: this parameter is used only if {@link #getResiduesProvider()} is null.
+	 */
+	@Override
+	public String getResidues(int start, int end, char fillchar) {
+		String result = null;
+		if (residues_provider == null)  {
+			// fall back on SimpleCompAnnotSeq (which will try both residues var and composition to provide residues)
+			//      result = super.getResidues(start, end, fillchar);
+			result = super.getResidues(start, end, '-');
+		}
+		else {
+			result = residues_provider.substring(start, end);
+		}
+		return result;
+	}
+
+
+	@Override
+	public boolean isComplete(int start, int end) {
+		if (residues_provider != null) { return true; }
+		else { return super.isComplete(start, end); }
+	}
+
+	public char charAt(int pos) {
+		if (residues_provider == null) {
+			String str = super.getResidues(pos, pos+1, '-');
+			if (str == null) { return '-'; }
+			else { return str.charAt(0); }
+		}
+		else {
+			return residues_provider.charAt(pos);
+		}
+	}
+
+	/*public boolean isEnd(int pos) {
+		return (pos >= end);
+	}*/
+
+	/*public String substring(int start) {
+		//    System.out.println("called NibbleBioSeq.substring(start)");
+		return substring(start, getLength());
+	}*/
+
+	public String substring(int start, int end) {
+		if (residues_provider == null) {
+			return super.getResidues(start, end);
+		}
+		return residues_provider.substring(start, end);
+	}
+
+	public int indexOf(String str, int fromIndex) {
+		// TODO: this will fail if residues_provider is null, so may need to call inside try/catch clause
+		if (residues_provider != null) {
+			return residues_provider.indexOf(str, fromIndex);
+		}
+		return this.getResidues().indexOf(str, fromIndex);
+	}
+}
+
+
+

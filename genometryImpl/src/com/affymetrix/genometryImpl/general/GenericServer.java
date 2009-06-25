98842361e5d25f8755d391a5331fef6598dd1904
ehsantabari <ehsantabari@mycompany.com>
2009-06-25 16:00:40 +0000

    GenericServer.toString() returns "serverName (serverType)"
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@3914 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/general/GenericServer.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/general/GenericServer.java
@@ -6,27 +6,36 @@ import com.affymetrix.genometry.util.LoadUtils.ServerType;
  * A class that's useful for visualizing a generic server.
  */
 public final class GenericServer {
-	
-	public final String serverName;   // name of the server.
-	public final String URL;          // URL/file that points to the server.
-	public final ServerType serverType;
-	public final Object serverObj;    // Das2ServerInfo, DasServerInfo, ..., QuickLoad?
 
-	/**
-	 * @param serverName
-	 * @param URL
-	 * @param serverType
-	 * @param serverObj
-	 */
-	public GenericServer(String serverName, String URL, ServerType serverType, Object serverObj) {
-		this.serverName = serverName;
-		this.URL = URL;
-		this.serverType = serverType;
-		this.serverObj = serverObj;
-	}
+    public final String serverName;   // name of the server.
+    public final String URL;          // URL/file that points to the server.
+    public final ServerType serverType;
+    public final Object serverObj;    // Das2ServerInfo, DasServerInfo, ..., QuickLoad?
 
-	@Override
-	public String toString() {
-		return this.serverName;
-	}
+    /**
+     * @param serverName
+     * @param URL
+     * @param serverType
+     * @param serverObj
+     */
+    public GenericServer(String serverName, String URL, ServerType serverType, Object serverObj) {
+        this.serverName = serverName;
+        this.URL = URL;
+        this.serverType = serverType;
+        this.serverObj = serverObj;
+    }
+
+    @Override
+    public String toString() {
+        switch (serverType) {
+            case DAS2:
+                return this.serverName + " (DAS/2)";
+            case DAS:
+                return this.serverName + " (DAS)";
+            case QuickLoad:
+                return this.serverName + " (Quickload)";
+            default:
+                return this.serverName + " (" + this.serverType + ")";
+        }
+    }
 }

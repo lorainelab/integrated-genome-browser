d3ffbee25dfcbd0b3bfb2f18a96114bacc5ebee7
tdisera <tdisera@uncc.edu>
2009-07-30 22:59:42 +0000

    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/das2_manager@4079 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genoviz_sdk/demo/src/genoviz/tutorial/SimpleSelectableMap.java
@@ -0,0 +1,118 @@
+/**
+ *   Copyright (c) 1998-2005 Affymetrix, Inc.
+ *
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
+
+package genoviz.tutorial;
+
+import com.affymetrix.genoviz.bioviews.SceneI;
+import com.affymetrix.genoviz.event.NeoRubberBandListener;
+import com.affymetrix.genoviz.event.NeoRubberBandEvent;
+
+import java.awt.Event;
+import java.awt.Frame;
+import java.awt.Rectangle;
+import java.awt.Window;
+import java.util.Vector;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class SimpleSelectableMap extends GlyphTestMap  
+	implements NeoRubberBandListener {
+
+	protected boolean isApplet = true;
+
+	public SimpleSelectableMap() {
+		super();
+		this.map.setSelectionEvent(this.map.ON_MOUSE_DOWN);
+		this.map.setSelectionAppearance(SceneI.SELECT_OUTLINE);
+		this.map.addRubberBandListener(this);
+		this.map.addMouseListener(ml);
+	}
+
+	protected void report(String s) {
+		if (isApplet) {
+			showStatus(s);
+		}
+		else {
+			System.out.println(s);
+		}
+	}
+
+	protected MouseListener ml = new MouseAdapter() {
+		public void mouseClicked(MouseEvent e) {
+			report(e.toString());
+		}
+		public void mouseEntered(MouseEvent e) {
+			report(e.toString());
+		}
+		public void mouseExited(MouseEvent e) {
+			report(e.toString());
+		}
+		public void mousePressed(MouseEvent e) {
+			report(e.toString());
+		}
+		public void mouseReleased(MouseEvent e) {
+			report(e.toString());
+		}
+	};
+
+	public String getAppletInfo() {
+		return "Simple Selectable Map - genoviz Software, Inc.";
+	}
+
+
+
+	public void rubberBandChanged(NeoRubberBandEvent theEvent) {
+		// Here we add some selection by rubberband.
+		if (theEvent.getID() == NeoRubberBandEvent.BAND_END
+				&& theEvent.getSource() == map
+				&& map.NO_SELECTION != map.getSelectionEvent())
+		{
+			Rectangle pixelBox = theEvent.getPixelBox();
+			pixelBox.setSize(pixelBox.width+1, pixelBox.height+1);
+			int fuzziness = map.getPixelFuzziness();
+			if (fuzziness <= pixelBox.height || fuzziness <= pixelBox.width) {
+				// Rubberband is non-trivial.
+				// Select items within it.
+				Vector items = map.getItems(pixelBox);
+				map.select(items);
+				map.updateWidget();
+			}
+		}
+	}
+
+	public static void main (String argv[]) {
+		SimpleSelectableMap me = new SimpleSelectableMap();
+		me.isApplet = false;
+		Frame f = new Frame("GenoViz");
+		f.add("Center", me);
+
+		f.addWindowListener( new WindowAdapter() {
+			public void windowClosing( WindowEvent e ) {
+				Window w = (Window) e.getSource();
+				w.dispose();
+			}
+			public void windowClosed( WindowEvent e ) {
+				System.exit( 0 );
+			}
+		} );
+		// me.addFileMenuItems(f);
+		f.pack();
+		f.setBounds(20, 40, 400, 500);
+		f.show();
+	}
+
+}
---
+++ genoviz_sdk/src/com/affymetrix/genoviz/glyph/TriangleGlyph.java
@@ -0,0 +1,89 @@
+/**
+ *   Copyright (c) 1998-2005 Affymetrix, Inc.
+ *    
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.  
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
+
+package com.affymetrix.genoviz.glyph;
+
+import java.awt.*;
+import java.util.*;
+import com.affymetrix.genoviz.bioviews.*;
+
+/**
+ *  A glyph that is drawn as a solid triangle.
+ */
+public class TriangleGlyph extends DirectedGlyph  {
+	int x[];
+	int y[];
+	Polygon poly;
+
+	public TriangleGlyph() {
+		super();
+		x = new int[3];
+		y = new int[3];
+		poly = new Polygon(x, y, 3);
+	}
+
+	public void draw(ViewI view) {
+		calcPixels(view);
+		Graphics g = view.getGraphics();
+		g.setColor(getBackgroundColor());
+		g.fillPolygon(x, y, 3);
+		super.draw(view);
+	}
+
+	/**
+	 * makes this glyph "point" 90 degrees (rotated clockwise) from its "direction".
+	 */
+	public void calcPixels(ViewI view) {
+		view.transformToPixels(coordbox, pixelbox);
+		x = poly.xpoints;
+		y = poly.ypoints;
+		int xcenter = pixelbox.x + pixelbox.width/2;
+		int ycenter = pixelbox.y + pixelbox.height/2;
+
+		switch ( this.getDirection() ) {
+			case EAST:
+				x[0] = xcenter - 5;
+				y[0] = ycenter - 5;
+				x[1] = xcenter;
+				y[1] = ycenter + 5;
+				x[2] = xcenter + 5;
+				y[2] = ycenter - 5;
+				break;
+			case WEST:
+				x[0] = xcenter - 5;
+				y[0] = ycenter + 5;
+				x[1] = xcenter;
+				y[1] = ycenter - 5;
+				x[2] = xcenter + 5;
+				y[2] = ycenter + 5;
+				break;
+			case SOUTH:
+				x[0] = xcenter + 5;
+				y[0] = ycenter - 5;
+				x[1] = xcenter + 5;
+				y[1] = ycenter + 5;
+				x[2] = xcenter - 5;
+				y[2] = ycenter;
+				break;
+			case NORTH:
+				x[0] = xcenter - 5;
+				y[0] = ycenter - 5;
+				x[1] = xcenter - 5;
+				y[1] = ycenter + 5;
+				x[2] = xcenter + 5;
+				y[2] = ycenter;
+				break;
+		}
+	}
+
+}

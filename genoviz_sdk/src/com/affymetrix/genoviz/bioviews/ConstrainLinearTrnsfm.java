df010578e455ee428c3e0522444f0461fe6c01f2
jnicol <jnicol@uncc.edu>
2009-10-27 01:32:17 +0000

    Remove overridden methods that were redundant with the superclass.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@4602 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genoviz_sdk/src/com/affymetrix/genoviz/bioviews/ConstrainLinearTrnsfm.java
+++ genoviz_sdk/src/com/affymetrix/genoviz/bioviews/ConstrainLinearTrnsfm.java
@@ -1,27 +1,11 @@
-/**
- *   Copyright (c) 1998-2005 Affymetrix, Inc.
- *    
- *   Licensed under the Common Public License, Version 1.0 (the "License").
- *   A copy of the license must be included with any distribution of
- *   this source code.
- *   Distributions from Affymetrix, Inc., place this in the
- *   IGB_LICENSE.html file.  
- *
- *   The license is also available at
- *   http://www.opensource.org/licenses/cpl.php
- */
-
 package com.affymetrix.genoviz.bioviews;
-
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 
 /**
  *  A transform used internally by NeoSeq, should not be used directly.
  */
 public final class ConstrainLinearTrnsfm extends LinearTransform {
 
-	protected double constrain_value;
+	private double constrain_value;
 
 	public ConstrainLinearTrnsfm() {
 		constrain_value = 1;
@@ -35,6 +19,7 @@ public final class ConstrainLinearTrnsfm extends LinearTransform {
 		return constrain_value;
 	}
 
+	@Override
 	public double transform(int orientation, double in) {
 		double out = 0;
 		if (orientation == X) {
@@ -54,58 +39,9 @@ public final class ConstrainLinearTrnsfm extends LinearTransform {
 		return out;
 	}
 
-	public double inverseTransform(int orientation, double in) {
-		double out = 0;
-		if (orientation == X) {
-			out = (in - xoffset) / xscale;
-		} else if (orientation == Y) {
-			out = (in - yoffset) / yscale;
-		}
-		return out;
-	}
-
-	public void setScale(double x, double y) {
-		xscale = x;
-		yscale = y;
-	}
-
-	public void setTranslation(double x, double y) {
-		xoffset = x;
-		yoffset = y;
-	}
-
-	public Rectangle2D.Double transform(Rectangle2D.Double src, Rectangle2D.Double dst) {
-		dst.x = src.x * xscale + xoffset;
-		dst.y = src.y * yscale + yoffset;
-		dst.width = src.width * xscale;
-		dst.height = src.height * yscale;
-		return dst;
-	}
-
-	public Rectangle2D.Double inverseTransform(Rectangle2D.Double src, Rectangle2D.Double dst) {
-		dst.x = (src.x - xoffset) / xscale;
-		dst.y = (src.y - yoffset) / yscale;
-		dst.width = src.width / xscale;
-		dst.height = src.height / yscale;
-		return dst;
-	}
-
-	public Point2D.Double transform(Point2D.Double src, Point2D.Double dst) {
-		dst.x = src.x * xscale + xoffset;
-		dst.y = src.y * yscale + yoffset;
-		return dst;
-	}
-
-	public Point2D.Double inverseTransform(Point2D.Double src, Point2D.Double dst) {
-		dst.x = (src.x - xoffset) / xscale;
-		dst.y = (src.y - yoffset) / yscale;
-		return dst;
-	}
-
 	public boolean equals(TransformI Tx) {
 		return (Tx instanceof ConstrainLinearTrnsfm) &&
 				super.equals(Tx) &&
 				(constrain_value == ((ConstrainLinearTrnsfm)Tx).getConstrainValue());
 	}
-
 }

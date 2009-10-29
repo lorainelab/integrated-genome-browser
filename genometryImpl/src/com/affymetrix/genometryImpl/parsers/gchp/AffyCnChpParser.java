e23b510708314167430384b53f5b32b20c0db374
jnicol <jnicol@uncc.edu>
2009-10-29 20:23:16 +0000

    GraphSymByte, GraphSymInt, and GraphSymShort were only being used during parsing of ByteList/IntList/ShortLists from AffySingleChromData.  Need to remove these to make further indexing simpler.
    Converted arrays to floats and used GraphSymFloat.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@4614 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/gchp/AffyCnChpParser.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/gchp/AffyCnChpParser.java
@@ -22,16 +22,8 @@ import java.io.*;
 import java.util.*;
 
 public final class AffyCnChpParser {
-
-	/** Creates a new instance of AffyCnChpParser */
-	public AffyCnChpParser() {
-	}
-
 	public void parse(File file, ChromLoadPolicy loadPolicy, InputStream istr, String stream_name, AnnotatedSeqGroup seq_group) throws IOException {
 		SingletonGenometryModel.logInfo("Parsing with " + this.getClass().getName() + ": " + stream_name);
-
-		//ArrayList results = new ArrayList();
-
 		try {
 
 			AffyGenericChpFile chpFile = AffyGenericChpFile.parse(file, loadPolicy, istr, false);
@@ -62,47 +54,6 @@ public final class AffyCnChpParser {
 				throw ioe;
 			}
 		}
-
-		//return results;
-	}
-
-	/** Parses the file, subject to the loadPolicy, and returns a 
-	 *  list of AffySingleChromData objects. 
-	 *  Does not create graphs or add any annotations to any BioSeq.
-	 *  If you do not do keep a reference the returned data, it may be garbage collected.
-	 */
-	public List<AffySingleChromData> parse2(File file, ChromLoadPolicy loadPolicy) throws IOException {
-		SingletonGenometryModel.logInfo("Parsing file with " + this.getClass().getName() + ": " + file.getName());
-
-		ArrayList<AffySingleChromData> results = new ArrayList<AffySingleChromData>();
-		InputStream istr = new BufferedInputStream(new FileInputStream(file));
-
-		try {
-
-			AffyGenericChpFile chpFile = AffyGenericChpFile.parse(file, loadPolicy, istr, false);
-
-			AffyDataGroup group = chpFile.groups.get(0);
-			AffyDataSet dataSet = group.getDataSets().get(0);
-
-
-			for (AffySingleChromData data : dataSet.getSingleChromData()) {
-				if (loadPolicy.shouldLoadChrom(data.displayName)) {
-					results.add(data);
-				}
-			}
-
-		} catch (Exception e) {
-			if (! (e instanceof IOException)) {
-				IOException ioe = new IOException("IOException for file: " + file);
-				ioe.initCause(e);
-				throw ioe;
-			}
-		} finally {
-			istr.close(); // if there is an exception, we probably do want to know about it.
-			// because it might prevent us from opening the file again later.
-		}
-
-		return results;
 	}
 
 	private MutableAnnotatedBioSeq getSeq(AnnotatedSeqGroup seq_group, String seqid) {
@@ -111,42 +62,5 @@ public final class AffyCnChpParser {
 			aseq = seq_group.addSeq(seqid, 1);
 		}
 		return aseq;
-	}
-
-	public void testFullRead(String test_file) {
-		FileInputStream fis = null;
-		BufferedInputStream bis = null;
-		AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
-
-		try {
-			File fil = new File(test_file);
-			ChromLoadPolicy loadPolicy = ChromLoadPolicy.getLoadAllPolicy();
-			fis = new FileInputStream(fil);
-			bis = new BufferedInputStream(fis);
-			System.out.println("START Parse");
-			parse(fil, loadPolicy, bis, test_file, seq_group);
-			System.out.println("END Parse");
-			System.out.println("");
-
-			//this.dump(System.out);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			try {fis.close();} catch (Exception e) { e.printStackTrace(); }
-			try {bis.close();} catch (Exception e) { e.printStackTrace(); }
-		}
-	}
-
-	/** Tests the parsing of the given filename. */
-	public static void main(String[] args) {
-		AffyCnChpParser parser = new AffyCnChpParser();
-		String fileName = "C:\\Documents and Settings\\eerwin\\My Documents\\data\\copy_number\\NA06985_GW6_C.cnchp";
-		if (args.length > 0) {
-			fileName = args[0];
-		}
-		System.out.println("Testing reading of file: " + fileName);
-		parser.testFullRead(fileName);
-	}
+	} 
 }
--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/gchp/AffySingleChromData.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/gchp/AffySingleChromData.java
@@ -15,12 +15,8 @@ package com.affymetrix.genometryImpl.parsers.gchp;
 
 import com.affymetrix.genometryImpl.MutableAnnotatedBioSeq;
 import com.affymetrix.genometryImpl.SeqSymmetry;
-import com.affymetrix.genometryImpl.GraphSymByte;
 import com.affymetrix.genometryImpl.GraphSymFloat;
-import com.affymetrix.genometryImpl.GraphSymInt;
-import com.affymetrix.genometryImpl.GraphSymShort;
 import com.affymetrix.genometryImpl.SingletonGenometryModel;
-import com.affymetrix.genometryImpl.SingletonSymWithProps;
 import com.affymetrix.genometryImpl.util.ByteList;
 import com.affymetrix.genometryImpl.util.FloatList;
 import com.affymetrix.genometryImpl.util.IntList;
@@ -29,24 +25,22 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.util.*;
 
-public final class AffySingleChromData {
+final class AffySingleChromData {
 
-	int start;
-	int rowCount;
-	int chromNum; // in chp file, chromosome is indicated both by an arbitrary byte
+	private int start;
+	private int rowCount;
 	String displayName; // AND by a display name.
 
-	List<AffyChpColumnData> columns = new ArrayList<AffyChpColumnData>();
-	AffyGenericChpFile chpFile;
+	private List<AffyChpColumnData> columns = new ArrayList<AffyChpColumnData>();
+	private AffyGenericChpFile chpFile;
 	AffyDataSet dataSet;
 
 	/** Creates a new instance of SingleChromosomeData */
 	public AffySingleChromData(AffyGenericChpFile chpFile, AffyDataSet dataSet, 
-			int chromNum, String chromDisplayName, int start, int count, 
+			int chromNum, String chromDisplayName, int start, int count,
 			List<AffyChpColumnData> columns) {
 		this.chpFile = chpFile;
 		this.dataSet = dataSet;
-		this.chromNum = chromNum;
 		this.displayName = chromDisplayName;
 		this.start = start;
 		this.rowCount = count;
@@ -54,13 +48,11 @@ public final class AffySingleChromData {
 	}
 
 	@Override
-		public String toString() {
-			return this.getClass().getName() + " [displayName=" + displayName 
-				+ ", start=" + start + ", count="
-				+ rowCount + ", columns=" + columns.size()+"]";
-		}
+	public String toString() {
+		return this.getClass().getName() + " [displayName=" + displayName + ", start=" + start + ", count=" + rowCount + ", columns=" + columns.size() + "]";
+	}
 
-	void parse(DataInputStream dis) throws IOException {
+	private void parse(DataInputStream dis) throws IOException {
 		SingletonGenometryModel.logDebug("Parsing chromData: " + this.displayName + ", " + this.rowCount);
 		for (int row=0; row < rowCount; row++) {
 			for (AffyChpColumnData col : columns) {
@@ -69,22 +61,22 @@ public final class AffySingleChromData {
 		}
 	}
 
-	int totalRowSize() {
-		int rowSize = 0;
-		for (AffyChpColumnData col : columns) {
-			rowSize += col.getByteLength();
-		}
-		return rowSize;
-	}
-
-	void skip(DataInputStream dis) throws IOException {
-		int rowSize = totalRowSize();
+	private void skip(DataInputStream dis) throws IOException {
+		int rowSize = totalRowSize(columns);
 		long skipSize = (long)rowCount * rowSize;	// cast to long before multiplying to avoid possible overflow
 
 		while (skipSize > 0) {
 			long skipped = dis.skip(skipSize);
 			skipSize -= skipped;
 		}     
+	}
+
+	private static int totalRowSize(List<AffyChpColumnData> columns) {
+		int rowSize = 0;
+		for (AffyChpColumnData col : columns) {
+			rowSize += col.getByteLength();
+		}
+		return rowSize;
 	}
 
 	void parseOrSkip(DataInputStream dis) throws IOException {
@@ -107,79 +99,75 @@ public final class AffySingleChromData {
 		IntList positions = (IntList) columns.get(2).getData();
 		positions.trimToSize();
 
+		if (positions.size() <= -1) {
+			return results;
+		}
 
-		if (positions.size() > -1) { 
-			// add a graph even if the data is of length 0
-			// because we want something to be visible in the display, even if it is
-			// simply a graph handle and axis with no graph data to draw.
-			boolean addSingletonSyms = false;
-			if (addSingletonSyms) {
-				String theMethod = "posistions";
-				//        TypeContainerAnnot tca = new TypeContainerAnnot(theMethod);
-				//        tca.setProperty("method", theMethod);
-				//        tca.setID(theMethod);
-				//        results.add(sym);
-				////        seq.addAnnotation(tca);
-				for (int i = 0; i < positions.size(); i++) {
-					//TODO: insn't there a class that accepts an IntList as the set of positions?
-					final int start_pos = positions.get(i);
-					SingletonSymWithProps sym = new SingletonSymWithProps(probeSetNames.get(i), start_pos, start_pos + 1, seq);
-					sym.setProperty("method", theMethod);
-					//          tca.addChild(sym);
-					results.add(sym);
-					//          seq.addAnnotation(sym);
+		// add a graph even if the data is of length 0
+		// because we want something to be visible in the display, even if it is
+		// simply a graph handle and axis with no graph data to draw.
+
+		// In a cnchp file, the first three columns contain non-graph data
+		// so skip them and make graphs from all the other columns
+		//TODO: maybe make this more generic for all "generic" chp files
+		for (AffyChpColumnData colData : columns.subList(3, columns.size())) {
+			String graphId = colData.name;
+			float[] y;
+			if (colData.getData() instanceof FloatList) {
+				List<Object> trimmedXandY = trimNaN(positions, (FloatList) colData.getData());
+				IntList xlist = (IntList) trimmedXandY.get(0);
+				FloatList flist = (FloatList) trimmedXandY.get(1);
+
+				xlist.trimToSize();
+				flist.trimToSize();
+				GraphSymFloat gsym = new GraphSymFloat(xlist.getInternalArray(), flist.getInternalArray(), graphId, seq);
+				results.add(gsym);
+			} else if (colData.getData() instanceof IntList) {
+				IntList ilist = (IntList) colData.getData();
+				ilist.trimToSize();
+				y = new float[ilist.size()];
+				for (int i=0;i<ilist.size();i++) {
+					y[i] = ilist.get(i);
 				}
-			}
-
-			// In a cnchp file, the first three columns contain non-graph data 
-			// so skip them and make graphs from all the other columns
-			//TODO: maybe make this more generic for all "generic" chp files
-			for (AffyChpColumnData colData : columns.subList(3,columns.size())) {
-				String graphId = colData.name;
-				if (colData.getData() instanceof FloatList) {
-					List<Object> trimmedXandY = trimNaN(positions, (FloatList) colData.getData());
+				GraphSymFloat gsym = new GraphSymFloat(positions.getInternalArray(), y, graphId, seq);
+				results.add(gsym);
+			} else if (colData.getData() instanceof ShortList) {
+				GraphSymFloat gsym;
+				if (colData.name.startsWith("CNState")) {
+					// In the "CNStateMin" and "CNStateMax" graphs, the number "255"
+					// is used to represent "unknown".  These x,y pairs should be discarded.
+					List<Object> trimmedXandY = trim255(positions, (ShortList) colData.getData());
 					IntList xlist = (IntList) trimmedXandY.get(0);
-					FloatList flist = (FloatList) trimmedXandY.get(1);
+					ShortList ilist = (ShortList) trimmedXandY.get(1);
 
 					xlist.trimToSize();
-					flist.trimToSize();
-					GraphSymFloat gsym = new GraphSymFloat(xlist.getInternalArray(), flist.getInternalArray(), graphId, seq);
-					//          seq.addAnnotation(gsym);
-					results.add(gsym);
-				} else if (colData.getData() instanceof IntList) {
-					IntList ilist = (IntList) colData.getData();
 					ilist.trimToSize();
-					GraphSymInt gsym = new GraphSymInt(positions.getInternalArray(), ilist.getInternalArray(), graphId, seq);
-					//          seq.addAnnotation(gsym);
-					results.add(gsym);
-				} else if (colData.getData() instanceof ShortList) {
-					GraphSymShort gsym;
-					if (colData.name.startsWith("CNState")) {
-						// In the "CNStateMin" and "CNStateMax" graphs, the number "255"
-						// is used to represent "unknown".  These x,y pairs should be discarded.
-						List<Object> trimmedXandY = trim255(positions, (ShortList) colData.getData());
-						IntList xlist = (IntList) trimmedXandY.get(0);
-						ShortList ilist = (ShortList) trimmedXandY.get(1);
-
-						xlist.trimToSize();
-						ilist.trimToSize();
-						gsym = new GraphSymShort(xlist.getInternalArray(), ilist.getInternalArray(), graphId, seq);
-					} else {
-						ShortList ilist = (ShortList) colData.getData();
-						ilist.trimToSize();
-						gsym = new GraphSymShort(positions.getInternalArray(), ilist.getInternalArray(), graphId, seq);
+					y = new float[ilist.size()];
+					for (int i = 0; i < ilist.size(); i++) {
+						y[i] = ilist.get(i);
 					}
-					//          seq.addAnnotation(gsym);
-					results.add(gsym);
-				} else if (colData.getData() instanceof ByteList) {
-					ByteList ilist = (ByteList) colData.getData();
-					ilist.trimToSize();
-					GraphSymByte gsym = new GraphSymByte(positions.getInternalArray(), ilist.getInternalArray(), graphId, seq);
-					//          seq.addAnnotation(gsym);
-					results.add(gsym);
+					gsym = new GraphSymFloat(xlist.getInternalArray(), y, graphId, seq);
 				} else {
-					SingletonGenometryModel.logError("Don't know how to make a graph for data of type: " + colData.type);
+					ShortList ilist = (ShortList) colData.getData();
+					ilist.trimToSize();
+					y = new float[ilist.size()];
+					for (int i = 0; i < ilist.size(); i++) {
+						y[i] = ilist.get(i);
+					}
+					gsym = new GraphSymFloat(positions.getInternalArray(), y, graphId, seq);
 				}
+				results.add(gsym);
+			} else if (colData.getData() instanceof ByteList) {
+				ByteList ilist = (ByteList) colData.getData();
+				ilist.trimToSize();
+				y = new float[ilist.getInternalArray().length];
+				for (int i = 0; i < ilist.getInternalArray().length; i++) {
+					y[i] = ilist.get(i);
+				}
+				GraphSymFloat gsym = new GraphSymFloat(positions.getInternalArray(), y, graphId, seq);
+				results.add(gsym);
+			} else {
+				SingletonGenometryModel.logError("Don't know how to make a graph for data of type: " + colData.type);
 			}
 		}
 
@@ -193,7 +181,7 @@ public final class AffySingleChromData {
 	 *  new objects.  If the given FloatList contains ONLY invalid values,
 	 *  then the returned IntList and FloatList will both be empty.
 	 */
-	List<Object> trimNaN(IntList x, FloatList y) {
+	private List<Object> trimNaN(IntList x, FloatList y) {
 		if (x.size() != y.size()) {
 			throw new IllegalArgumentException("Lists must be the same size " + x.size() + " != " + y.size());
 		}
@@ -226,7 +214,7 @@ public final class AffySingleChromData {
 	 *  new objects.  If the given ShortList contains ONLY invalid values,
 	 *  then the returned IntList and 3hortList will both be empty.
 	 */
-	List<Object> trim255(IntList x, ShortList y) {
+	private List<Object> trim255(IntList x, ShortList y) {
 		if (x.size() != y.size()) {
 			throw new IllegalArgumentException("Lists must be the same size " + x.size() + " != " + y.size());
 		}

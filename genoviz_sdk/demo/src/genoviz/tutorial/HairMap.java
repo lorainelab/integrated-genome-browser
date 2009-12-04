f18a0e538505b8f0633b8bacf6398da485415aa4
jnicol <jnicol@uncc.edu>
2009-12-04 13:48:41 +0000

    Make the HairMap work both in applet mode and when run directly.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@4783 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genoviz_sdk/demo/src/genoviz/tutorial/HairMap.java
+++ genoviz_sdk/demo/src/genoviz/tutorial/HairMap.java
@@ -1,20 +1,11 @@
-/**
- *   Copyright (c) 1998-2005 Affymetrix, Inc.
- *    
- *   Licensed under the Common Public License, Version 1.0 (the "License").
- *   A copy of the license must be included with any distribution of
- *   this source code.
- *   Distributions from Affymetrix, Inc., place this in the
- *   IGB_LICENSE.html file.  
- *
- *   The license is also available at
- *   http://www.opensource.org/licenses/cpl.php
- */
-
 package genoviz.tutorial;
 
-import com.affymetrix.genoviz.event.*;
-import com.affymetrix.genoviz.widget.*;
+import com.affymetrix.genoviz.event.NeoMouseEvent;
+import com.affymetrix.genoviz.event.NeoRangeEvent;
+import com.affymetrix.genoviz.event.NeoRangeListener;
+import com.affymetrix.genoviz.widget.NeoMap;
+import com.affymetrix.genoviz.widget.Shadow;
+import com.affymetrix.genoviz.widget.VisibleRange;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -26,17 +17,13 @@ public class HairMap extends SimpleMap3 {
 			public void rangeChanged( NeoRangeEvent e ) {
 				double midPoint = ( e.getVisibleEnd() + e.getVisibleStart() ) / 2.0f;
 
-				map.setZoomBehavior( map.X, map.CONSTRAIN_COORD, midPoint );
+				map.setZoomBehavior( NeoMap.X, NeoMap.CONSTRAIN_COORD, midPoint );
 				map.updateWidget();
 			}
 
 		};
 
 		this.zoomPoint.addListener( zoomAdjuster );
-	}
-
-	public void init() {
-		super.init();
 
 		Shadow hairline = new Shadow( this.map );
 
@@ -45,7 +32,6 @@ public class HairMap extends SimpleMap3 {
 		MouseListener zoomPointAdjuster = new MouseAdapter() {
 			public void mouseReleased( MouseEvent e ) {
 				double focus = ( ( NeoMouseEvent ) e ).getCoordX();
-
 				zoomPoint.setSpot( focus );
 			}
 
@@ -58,13 +44,10 @@ public class HairMap extends SimpleMap3 {
 	public static void main( String argv[] ) {
 		HairMap me = new HairMap();
 		Frame f = new Frame( "GenoViz" );
-
 		f.add( me, BorderLayout.CENTER );
-		// me.addFileMenuItems( f );
 		f.addWindowListener( new WindowAdapter() {
 			public void windowClosing( WindowEvent e ) {
 				Window w = ( Window ) e.getSource();
-
 				w.dispose();
 			}
 
@@ -75,7 +58,7 @@ public class HairMap extends SimpleMap3 {
 		} );
 		f.pack();
 		f.setBounds( 20, 40, 900, 400 );
-		f.show();
+		f.setVisible(true);
 	}
 
 }

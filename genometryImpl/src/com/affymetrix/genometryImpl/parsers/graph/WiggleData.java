f645c22042893c4e779539b7c408e669097ba8e5
jnicol <jnicol@uncc.edu>
2009-12-14 18:51:55 +0000

    Revert this back to revision 4657 before memory improvements.
    An assumption was made that files followed the UCSC specification (i.e., they
    are always sorted), which apparently is not true.
    
    Fixes bug 2913879.
     
    
    
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@4850 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/graph/WiggleData.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/graph/WiggleData.java
@@ -4,8 +4,6 @@ import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.GraphIntervalSym;
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.GraphSym;
-import com.affymetrix.genometryImpl.util.FloatList;
-import com.affymetrix.genometryImpl.util.IntList;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -14,16 +12,11 @@ import java.util.Comparator;
  *  A class used to temporarily hold data during processing of Wiggle-format files.
  */
 final class WiggleData {
-	private IntList xList;
-	private FloatList yList;
-	private IntList wList;
+	private final ArrayList<Point3D> data;
 	private final String seq_id;
 
 	WiggleData(String seq_id) {
-
-		this.xList = new IntList();
-		this.yList = new FloatList();
-		this.wList = new IntList();
+		this.data = new ArrayList<Point3D>();
 		this.seq_id = seq_id;
 	}
 
@@ -32,52 +25,50 @@ final class WiggleData {
 	 *  has been stored yet.
 	 */
 	GraphSym createGraph(AnnotatedSeqGroup seq_group, String graph_id) {
-		if (xList.isEmpty()) {
+		if (data.isEmpty()) {
 			return null;
 		}
 
-		int largest_x = xList.get(xList.size() - 1);
-		int largest_w = wList.get(wList.size() - 1);
+		PointComp pointcomp = new PointComp();
+		Collections.sort(data, pointcomp);
 
-		BioSeq seq = seq_group.addSeq(seq_id, largest_x + largest_w);
+		int dataSize = data.size();
 
-		int[] xArr = xList.copyToArray();
-		xList = null;
-		float[] yArr = yList.copyToArray();
-		yList = null;
-		int[] wArr = wList.copyToArray();
-		wList = null;
+		int[]   xlist = new int[dataSize];
+		float[] ylist = new float[dataSize];
+		int[]   wlist = new int[dataSize];
 
-		return new GraphIntervalSym(xArr, wArr, yArr, graph_id, seq);
+		Point3D largest = data.get(dataSize - 1);
+		int largest_x = largest.x + largest.w;
+
+		BioSeq seq = seq_group.addSeq(seq_id, largest_x);
+
+		for (int i=0; i<dataSize; i++) {
+			Point3D p = data.get(i);
+			xlist[i] = p.x;
+			ylist[i] = p.y;
+			wlist[i] = p.w;
+		}
+
+		return new GraphIntervalSym(xlist, wlist, ylist, graph_id, seq);
 	}
 
-	void add(int x, float y, int w) {
-		// see if this point obeys sorting
-		if (xList.isEmpty() || x > xList.get(xList.size()-1)) {
-			xList.add(x);
-			yList.add(y);
-			wList.add(w);
-			return;
-		}
+	public void add(int x, float y, int w) {
+		data.add(new Point3D(x, y, w));
+	}
 
-		// point does not obey sorting.  Must add the point at the proper location
-		System.out.println("WARNING: x coordinate " + x + " is not sorted.  This violates Wiggle specification and will be very slow to load.");
-
-		int index = xList.binarySearch(x);
-		if (index >= 0) {
-			System.out.println("WARNING: x coordinate " + x + " is repeated, which is invalid.  Ignoring data point.");
-			return;
+	private static final class Point3D {
+		private final float y;
+		private final int x;
+		private int w;
+		public Point3D(int x, float y,int w) {
+			this.x = x; this.y =y; this.w = w;
 		}
-		int insertionPoint = -index-1;	// from binarySearch definition
+	}
 
-		// see if previous interval covers this
-		int previousInterval = xList.get(insertionPoint) + wList.get(insertionPoint);
-		if (previousInterval > x) {
-			System.out.println("WARNING: x coordinate " + x + " is in a previous interval, which is invalid.  Ignoring data point.");
-			return;
+	private static final class PointComp implements Comparator<Point3D> {
+		public int compare(Point3D p1, Point3D p2) {
+			return ((Integer)p1.x).compareTo(p2.x);
 		}
-		xList.add(insertionPoint,x);
-		yList.add(insertionPoint,y);
-		wList.add(insertionPoint,w);
 	}
 }

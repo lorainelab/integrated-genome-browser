13aaa28c0862116b7e8a7a637b0d746ff0e4882c
biotelerock <biotelerock@uncc.edu>
2009-12-21 15:55:06 +0000

    Added support for the USeq Binary Archive data format
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@4876 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/menuitem/OpenGraphAction.java
+++ igb/src/com/affymetrix/igb/menuitem/OpenGraphAction.java
@@ -1,15 +1,15 @@
 /**
-*   Copyright (c) 2001-2006 Affymetrix, Inc.
-*
-*   Licensed under the Common Public License, Version 1.0 (the "License").
-*   A copy of the license must be included with any distribution of
-*   this source code.
-*   Distributions from Affymetrix, Inc., place this in the
-*   IGB_LICENSE.html file.
-*
-*   The license is also available at
-*   http://www.opensource.org/licenses/cpl.php
-*/
+ *   Copyright (c) 2001-2006 Affymetrix, Inc.
+ *
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
 
 package com.affymetrix.igb.menuitem;
 
@@ -26,6 +26,7 @@ import com.affymetrix.igb.Application;
 import com.affymetrix.igb.event.ThreadProgressMonitor;
 import com.affymetrix.igb.view.SeqMapView;
 import com.affymetrix.genoviz.util.ErrorHandler;
+import com.affymetrix.genometryImpl.useq.USeqUtilities;
 import com.affymetrix.genometryImpl.util.UniFileFilter;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.GraphSym;
@@ -37,140 +38,139 @@ import com.affymetrix.igb.util.GraphGlyphUtils;
 import com.affymetrix.igb.util.LocalUrlCacher;
 
 public final class OpenGraphAction extends AbstractAction {
-  private static GenometryModel gmodel = GenometryModel.getGenometryModel();
+	private static GenometryModel gmodel = GenometryModel.getGenometryModel();
 
-  private FileTracker load_dir_tracker;
-  private SeqMapView gviewer;
-  private static JFileChooser chooser = null;
+	private FileTracker load_dir_tracker;
+	private SeqMapView gviewer;
+	private static JFileChooser chooser = null;
 
 
-  public void actionPerformed(ActionEvent e) {
-    // allowing for multiple file selection, so may have multiple graphs
-    BioSeq aseq = gmodel.getSelectedSeq();
-    if (aseq == null) {
-      ErrorHandler.errorPanel("Must load a sequence before loading a graph");
-      return;
-    }
+	public void actionPerformed(ActionEvent e) {
+		// allowing for multiple file selection, so may have multiple graphs
+		BioSeq aseq = gmodel.getSelectedSeq();
+		if (aseq == null) {
+			ErrorHandler.errorPanel("Must load a sequence before loading a graph");
+			return;
+		}
 
-    File[] files = chooseGraphs();
+		File[] files = chooseGraphs();
 
-    if (files == null) return;
+		if (files == null) return;
 
-    try {
-      Thread t = loadAndShowGraphs(files, aseq, gviewer);
-      t.start();
-    } catch (MalformedURLException m) {
-      ErrorHandler.errorPanel("Error loading graphs", m);
-    }
-  }
+		try {
+			Thread t = loadAndShowGraphs(files, aseq, gviewer);
+			t.start();
+		} catch (MalformedURLException m) {
+			ErrorHandler.errorPanel("Error loading graphs", m);
+		}
+	}
 
-  private static Thread loadAndShowGraphs(File[] files, BioSeq aseq, SeqMapView gviewer)
-  throws MalformedURLException {
-    URL[] urls = new URL[files.length];
-    for (int i=0; i<files.length; i++) {
-      urls[i] = files[i].toURI().toURL();
-    }
-    return loadAndShowGraphs(urls, aseq, gviewer);
-  }
+	private static Thread loadAndShowGraphs(File[] files, BioSeq aseq, SeqMapView gviewer)
+	throws MalformedURLException {
+		URL[] urls = new URL[files.length];
+		for (int i=0; i<files.length; i++) {
+			urls[i] = files[i].toURI().toURL();
+		}
+		return loadAndShowGraphs(urls, aseq, gviewer);
+	}
 
-  public static Thread loadAndShowGraphs(final URL[] files, final BioSeq aseq, final SeqMapView gviewer) {
-    Thread t = new Thread() {
-      ThreadProgressMonitor monitor = null;
-      public void run() {
-        monitor = new ThreadProgressMonitor(
-          gviewer.getFrame(),
-          "Loading graphs...",
-          "Loading graphs",
-          this,
-          false, false);
-        monitor.showDialogEventually();
-        try {
-          AnnotatedSeqGroup seq_group = GenometryModel.getGenometryModel().getSelectedSeqGroup();
-          loadGraphFiles(files, seq_group, aseq, true, monitor, gviewer);
-        } catch (final Throwable t) { // catch Out-Of-Memory Errors, etc.
-          SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-              if (monitor != null) monitor.closeDialog();
-              ErrorHandler.errorPanel("Error loading graphs", t);
-            }
-	    });
-        }
-        if (monitor != null) {
-          monitor.closeDialogEventually();
-          monitor = null;
-        }
-      }
-    };
-    return t;
-  }
+	public static Thread loadAndShowGraphs(final URL[] files, final BioSeq aseq, final SeqMapView gviewer) {
+		Thread t = new Thread() {
+			ThreadProgressMonitor monitor = null;
+			public void run() {
+				monitor = new ThreadProgressMonitor(
+						gviewer.getFrame(),
+						"Loading graphs...",
+						"Loading graphs",
+						this,
+						false, false);
+				monitor.showDialogEventually();
+				try {
+					AnnotatedSeqGroup seq_group = GenometryModel.getGenometryModel().getSelectedSeqGroup();
+					loadGraphFiles(files, seq_group, aseq, true, monitor, gviewer);
+				} catch (final Throwable t) { // catch Out-Of-Memory Errors, etc.
+					SwingUtilities.invokeLater(new Runnable() {
+						public void run() {
+							if (monitor != null) monitor.closeDialog();
+							ErrorHandler.errorPanel("Error loading graphs", t);
+						}
+					});
+				}
+				if (monitor != null) {
+					monitor.closeDialogEventually();
+					monitor = null;
+				}
+			}
+		};
+		return t;
+	}
 
-  private static void updateViewer(final SeqMapView gviewer) {
-    SwingUtilities.invokeLater(new Runnable() {
-      public void run() {
-        gviewer.setAnnotatedSeq(gmodel.getSelectedSeq(), true, true);
-      }
-    });
-  }
+	private static void updateViewer(final SeqMapView gviewer) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				gviewer.setAnnotatedSeq(gmodel.getSelectedSeq(), true, true);
+			}
+		});
+	}
 
-  private File[] chooseGraphs() {
-    JFileChooser chooser = getFileChooser();
-    chooser.setMultiSelectionEnabled(true);
-    chooser.setCurrentDirectory(load_dir_tracker.getFile());
-    int option = chooser.showOpenDialog(gviewer.getFrame());
-    if (option != JFileChooser.APPROVE_OPTION) {  return null; }
-    File[] files = chooser.getSelectedFiles();
-    load_dir_tracker.setFile(chooser.getCurrentDirectory());
-    return files;
-  }
+	private File[] chooseGraphs() {
+		JFileChooser chooser = getFileChooser();
+		chooser.setMultiSelectionEnabled(true);
+		chooser.setCurrentDirectory(load_dir_tracker.getFile());
+		int option = chooser.showOpenDialog(gviewer.getFrame());
+		if (option != JFileChooser.APPROVE_OPTION) {  return null; }
+		File[] files = chooser.getSelectedFiles();
+		load_dir_tracker.setFile(chooser.getCurrentDirectory());
+		return files;
+	}
 
-  /** Loads graphs from a set of files.
-   *  The return List will contain all sucessfully loaded GraphSym objects;
-   *  there may be more or fewer graphs than given files.
-   *  @param aseq  a BioSeq. If null, this routine will return an empty List
-   *  @param update_viewer  If true, this will call {@link #updateViewer(SeqMapView)} after
-   *   each graph file is loaded.
-   *  @throws OutOfMemoryError  Any routine can throw an OutOfMemoryError, but
-   *  this is a reminder that this is fairly common here.  (You have to catch
-   *  "Throwable" rather than "Exception" to catch these.)
-   */
-  private static List<GraphSym> loadGraphFiles(URL[] files, AnnotatedSeqGroup seq_group, BioSeq aseq, boolean update_viewer, ThreadProgressMonitor monitor, SeqMapView gviewer)
-  throws IOException, OutOfMemoryError {
-    List<GraphSym> graphs = Collections.<GraphSym>emptyList();
-    if (aseq != null) {
+	/** Loads graphs from a set of files.
+	 *  The return List will contain all sucessfully loaded GraphSym objects;
+	 *  there may be more or fewer graphs than given files.
+	 *  @param aseq  a BioSeq. If null, this routine will return an empty List
+	 *  @param update_viewer  If true, this will call {@link #updateViewer(SeqMapView)} after
+	 *   each graph file is loaded.
+	 *  @throws OutOfMemoryError  Any routine can throw an OutOfMemoryError, but
+	 *  this is a reminder that this is fairly common here.  (You have to catch
+	 *  "Throwable" rather than "Exception" to catch these.)
+	 */
+	private static List<GraphSym> loadGraphFiles(URL[] files, AnnotatedSeqGroup seq_group, BioSeq aseq, boolean update_viewer, ThreadProgressMonitor monitor, SeqMapView gviewer)
+	throws IOException, OutOfMemoryError {
+		List<GraphSym> graphs = Collections.<GraphSym>emptyList();
+		if (aseq != null) {
 			for (URL file : files) {
-        if (monitor != null) {monitor.setMessageEventually("Loading graph from: "+file.getPath());}
-        graphs = loadGraphFile(file, seq_group, aseq);
-        if (update_viewer && ! graphs.isEmpty()) {updateViewer(gviewer);}
-      }
-    }
-    return graphs;
-  }
+				if (monitor != null) {monitor.setMessageEventually("Loading graph from: "+file.getPath());}
+				graphs = loadGraphFile(file, seq_group, aseq);
+				if (update_viewer && ! graphs.isEmpty()) {updateViewer(gviewer);}
+			}
+		}
+		return graphs;
+	}
 
-  //TODO: Make a version that works with an InputStream instead of a url
-  public static List<GraphSym> loadGraphFile(URL furl, AnnotatedSeqGroup seq_group, BioSeq aseq) throws IOException, OutOfMemoryError {
-    List<GraphSym> graphs = Collections.<GraphSym>emptyList();
-    InputStream fis = null;
-    try {
-      String path = furl.getPath();
-      if (! GraphSymUtils.isAGraphFilename(path)) {
-        throw new IOException("Filename does not match any known type of graph:\n" + path);
-      }
-      if (Application.CACHE_GRAPHS)  {
-        String graph_url = furl.toExternalForm();
-        //System.out.println("in OpenGraphAction.loadGraphFile(), url external form: " + graph_url);
-        fis = LocalUrlCacher.getInputStream(graph_url);
-      }
-      else {
-        fis = furl.openStream();
-      }
+	//TODO: Make a version that works with an InputStream instead of a url
+	public static List<GraphSym> loadGraphFile(URL furl, AnnotatedSeqGroup seq_group, BioSeq aseq) throws IOException, OutOfMemoryError {
+		List<GraphSym> graphs = Collections.<GraphSym>emptyList();
+		InputStream fis = null;
+		try {
+			String path = furl.getPath();
+			//check again to see if it is a graph?  why this is checked prior to calling! 
+			if (! GraphSymUtils.isAGraphFilename(path) && path.endsWith(USeqUtilities.USEQ_EXTENSION_NO_PERIOD) == false) {
+				throw new IOException("Filename does not match any known type of graph:\n" + path);
+			}
+			if (Application.CACHE_GRAPHS)  {
+				String graph_url = furl.toExternalForm();
+				//System.out.println("in OpenGraphAction.loadGraphFile(), url external form: " + graph_url);
+				fis = LocalUrlCacher.getInputStream(graph_url);
+			}
+			else  fis = furl.openStream();
 
-      graphs = GraphSymUtils.readGraphs(fis, furl.toExternalForm(), gmodel, seq_group);
-      
-      String graph_name = getGraphNameForURL(furl);
+			graphs = GraphSymUtils.readGraphs(fis, furl.toExternalForm(), gmodel, seq_group);
+
+			String graph_name = getGraphNameForURL(furl);
 
 			int graphSize = graphs.size();
-      // Now set the graph names (either the URL or the filename, possibly with an integer appended)
-     for (int i = 0; i < graphSize; i++) {
+			// Now set the graph names (either the URL or the filename, possibly with an integer appended)
+			for (int i = 0; i < graphSize; i++) {
 				GraphSym gg = graphs.get(i);
 
 				IAnnotStyle style = gg.getGraphState().getTierStyle();
@@ -186,81 +186,89 @@ public final class OpenGraphAction extends AbstractAction {
 					style.setHumanName(name);
 				}
 			}
-      
-    } finally {
+			//Here's a hack to set the same human name for the xxx.useq type graphs, still can't figure out, after an hour of looking, where the hell this is being set?!!!
+			if (graph_name.endsWith(USeqUtilities.USEQ_EXTENSION_NO_PERIOD)){
+				graph_name = graph_name.replace(USeqUtilities.USEQ_EXTENSION_WITH_PERIOD, "");
+				for (int i = 0; i < graphSize; i++) {
+					GraphSym gg = graphs.get(i);
+					IAnnotStyle style = gg.getGraphState().getTierStyle();
+					style.setHumanName(graph_name);	
+				}
+			}
+		} finally {
 			GeneralUtils.safeClose(fis);
-    }
-    return graphs;
-  }
+		}
+		return graphs;
+	}
 
-  /**
-   *  Return a graph name for the given URL.  The graph name is typically just
-   *  the last portion of the URL, but the entire URL may be used, depending on
-   *  the preference GraphGlyphUtils.PREF_USE_URL_AS_NAME.
-   */
-  public static String getGraphNameForURL(URL furl) {
-    String name;
-    boolean use_full_url = GraphGlyphUtils.getGraphPrefsNode().getBoolean(
-        GraphGlyphUtils.PREF_USE_URL_AS_NAME, GraphGlyphUtils.default_use_url_as_name);
-    if (use_full_url) {
-      name = furl.toExternalForm();
-    } else { // use only the filename, not the whole url
-      name = furl.getFile();
-      int index = name.lastIndexOf('/');
-      if (index > 0) {
-        String last_name = name.substring(index+1);
-        if (last_name.length()>0) { 
-          name = GeneralUtils.URLDecode(last_name);
-        }
-      }
-    }
-    return name;
-  }
+	/**
+	 *  Return a graph name for the given URL.  The graph name is typically just
+	 *  the last portion of the URL, but the entire URL may be used, depending on
+	 *  the preference GraphGlyphUtils.PREF_USE_URL_AS_NAME.
+	 */
+	public static String getGraphNameForURL(URL furl) {
+		String name;
+		boolean use_full_url = GraphGlyphUtils.getGraphPrefsNode().getBoolean(
+				GraphGlyphUtils.PREF_USE_URL_AS_NAME, GraphGlyphUtils.default_use_url_as_name);
+		if (use_full_url) {
+			name = furl.toExternalForm();
+		} else { // use only the filename, not the whole url
+			name = furl.getFile();
+			int index = name.lastIndexOf('/');
+			if (index > 0) {
+				String last_name = name.substring(index+1);
+				if (last_name.length()>0) { 
+					name = GeneralUtils.URLDecode(last_name);
+				}
+			}
+		}
+		return name;
+	}
 
-  public static String getGraphNameForFile(String name) {
-    boolean use_full_url = GraphGlyphUtils.getGraphPrefsNode().getBoolean(
-        GraphGlyphUtils.PREF_USE_URL_AS_NAME, GraphGlyphUtils.default_use_url_as_name);
-    if (use_full_url) {
-      // leave the name alone
-    } else { // use only the filename, not the whole url
-      int index = name.lastIndexOf(System.getProperty("file.separator"));
-      if (index > 0) {
-        String last_name = name.substring(index+1);
-        if (last_name.length()>0) { 
-          // shouldn't need to do URLDecoder.decode()
-          name = last_name; 
-        }
-      }
-    }
-    return name;
-  }  
+	public static String getGraphNameForFile(String name) {
+		boolean use_full_url = GraphGlyphUtils.getGraphPrefsNode().getBoolean(
+				GraphGlyphUtils.PREF_USE_URL_AS_NAME, GraphGlyphUtils.default_use_url_as_name);
+		if (use_full_url) {
+			// leave the name alone
+		} else { // use only the filename, not the whole url
+			int index = name.lastIndexOf(System.getProperty("file.separator"));
+			if (index > 0) {
+				String last_name = name.substring(index+1);
+				if (last_name.length()>0) { 
+					// shouldn't need to do URLDecoder.decode()
+					name = last_name; 
+				}
+			}
+		}
+		return name;
+	}  
 
-  private static JFileChooser getFileChooser() {
-    if (chooser == null) {
-      chooser = new JFileChooser();
-      chooser.setMultiSelectionEnabled(true);
-      chooser.addChoosableFileFilter(new UniFileFilter("bar"));
-      chooser.addChoosableFileFilter(new UniFileFilter("gr", "Text Graph"));
-      chooser.addChoosableFileFilter(new UniFileFilter("bgr"));
-      chooser.addChoosableFileFilter(new UniFileFilter("sgr"));
-      HashSet<String> all_known_endings = new HashSet<String>();
-      javax.swing.filechooser.FileFilter[] filters = chooser.getChoosableFileFilters();
+	private static JFileChooser getFileChooser() {
+		if (chooser == null) {
+			chooser = new JFileChooser();
+			chooser.setMultiSelectionEnabled(true);
+			chooser.addChoosableFileFilter(new UniFileFilter("bar"));
+			chooser.addChoosableFileFilter(new UniFileFilter("gr", "Text Graph"));
+			chooser.addChoosableFileFilter(new UniFileFilter("bgr"));
+			chooser.addChoosableFileFilter(new UniFileFilter("sgr"));
+			HashSet<String> all_known_endings = new HashSet<String>();
+			javax.swing.filechooser.FileFilter[] filters = chooser.getChoosableFileFilters();
 			for (javax.swing.filechooser.FileFilter filter : filters) {
-        if (filter instanceof UniFileFilter) {
-          UniFileFilter uff = (UniFileFilter) filter;
-          uff.addCompressionEndings(GeneralUtils.compression_endings);
-          all_known_endings.addAll(uff.getExtensions());
-        }
-      }
-      UniFileFilter all_known_types = new UniFileFilter(
-        all_known_endings.toArray(new String[all_known_endings.size()]),
-        "Known Graph Types");
-      all_known_types.setExtensionListInDescription(false);
-      all_known_types.addCompressionEndings(GeneralUtils.compression_endings);
-      chooser.addChoosableFileFilter(all_known_types);
-    }
-    return chooser;
-  }
+				if (filter instanceof UniFileFilter) {
+					UniFileFilter uff = (UniFileFilter) filter;
+					uff.addCompressionEndings(GeneralUtils.compression_endings);
+					all_known_endings.addAll(uff.getExtensions());
+				}
+			}
+			UniFileFilter all_known_types = new UniFileFilter(
+					all_known_endings.toArray(new String[all_known_endings.size()]),
+					"Known Graph Types");
+			all_known_types.setExtensionListInDescription(false);
+			all_known_types.addCompressionEndings(GeneralUtils.compression_endings);
+			chooser.addChoosableFileFilter(all_known_types);
+		}
+		return chooser;
+	}
 
 }
 

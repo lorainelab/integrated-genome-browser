e639d405a2fbbe3ae94e8affc6cbe86f564b07e4
jnicol <jnicol@uncc.edu>
2010-01-15 19:57:05 +0000

    Make CollapsePacker a subclass of CollapsedTierPacker.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@4999 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genoviz_sdk/src/com/affymetrix/genoviz/widget/tieredmap/CollapsedTierPacker.java
+++ genoviz_sdk/src/com/affymetrix/genoviz/widget/tieredmap/CollapsedTierPacker.java
@@ -1,16 +1,3 @@
-/**
- *   Copyright (c) 1998-2005 Affymetrix, Inc.
- *
- *   Licensed under the Common Public License, Version 1.0 (the "License").
- *   A copy of the license must be included with any distribution of
- *   this source code.
- *   Distributions from Affymetrix, Inc., place this in the
- *   IGB_LICENSE.html file.
- *
- *   The license is also available at
- *   http://www.opensource.org/licenses/cpl.php
- */
-
 package com.affymetrix.genoviz.widget.tieredmap;
 
 import java.util.*;
@@ -19,15 +6,13 @@ import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 
 public class CollapsedTierPacker extends AbstractCoordPacker implements PaddedPackerI {
+
 	public static final int ALIGN_TOP = 1000;
 	public static final int ALIGN_BOTTOM = 1001;
 	public static final int ALIGN_CENTER = 1002;
-
-	int alignment = ALIGN_CENTER;
-	double maxHeight = 0;
-
+	protected int alignment = ALIGN_CENTER;
+	protected double maxHeight = 0;
 	protected double parent_spacer = 2;
-
 
 	public Rectangle pack(GlyphI parent, GlyphI child, ViewI view) {
 		double height = child.getCoordBox().height;
@@ -36,19 +21,19 @@ public class CollapsedTierPacker extends AbstractCoordPacker implements PaddedPa
 			// need to repack siblings to reflect new max height!
 			adjustHeight(parent);
 			moveAllChildren(parent);
-		}
-		else {
+		} else {
 			// max height hasn't changed, just move specified child glyph
 			moveOneChild(parent, child);
 		}
 		return null;
 	}
 
-
 	@Override
 	public Rectangle pack(GlyphI parent, ViewI view) {
 		List<GlyphI> children = parent.getChildren();
-		if (children == null) { return null; }
+		if (children == null) {
+			return null;
+		}
 		for (GlyphI child : children) {
 			maxHeight = Math.max(maxHeight, child.getCoordBox().height);
 		}
@@ -66,48 +51,22 @@ public class CollapsedTierPacker extends AbstractCoordPacker implements PaddedPa
 		Rectangle2D.Double cbox = child.getCoordBox();
 
 		if (alignment == ALIGN_TOP) {
-			double top = pbox.y + parent_spacer;
-			child.moveAbsolute(cbox.x, top);
-		}
-		else if (alignment == ALIGN_BOTTOM) {
-			double bottom = pbox.y + pbox.height - parent_spacer;
-			child.moveAbsolute(cbox.x, bottom - cbox.height);
-		}
-		else  {  // alignment == ALIGN_CENTER
+			child.moveAbsolute(cbox.x, pbox.y + parent_spacer);
+		} else if (alignment == ALIGN_BOTTOM) {
+			child.moveAbsolute(cbox.x, pbox.y + pbox.height - parent_spacer - cbox.height);
+		} else if (alignment == ALIGN_CENTER) {
 			double parent_height = maxHeight + (2 * parent_spacer);
-			double center = pbox.y + parent_height / 2;
-			child.moveAbsolute(cbox.x, center - cbox.height/2);
+			child.moveAbsolute(cbox.x, pbox.y + parent_height / 2 - cbox.height / 2);
 		}
 	}
 
 	protected void moveAllChildren(GlyphI parent) {
 		List<GlyphI> children = parent.getChildren();
-		if (children == null) { return; }
-		Rectangle2D.Double pbox = parent.getCoordBox();
-		double parent_height = pbox.height;
-
-		if (alignment == ALIGN_TOP) {
-			double top = pbox.y + parent_spacer;
-			for (GlyphI child : children) {
-				child.moveAbsolute(child.getCoordBox().x, top);
-			}
+		if (children == null) {
+			return;
 		}
-
-		else if (alignment == ALIGN_BOTTOM) {
-			double bottom = pbox.y + pbox.height - parent_spacer;
-			Rectangle2D.Double cbox;
-			for (GlyphI child : children) {
-				cbox = child.getCoordBox();
-				child.moveAbsolute(cbox.x, bottom - cbox.height);
-			}
-		}
-		else  {  // alignment == ALIGN_CENTER
-			double center = pbox.y + parent_height / 2;
-			Rectangle2D.Double cbox;
-			for (GlyphI child : children) {
-				cbox = child.getCoordBox();
-				child.moveAbsolute(cbox.x, center - cbox.height/2);
-			}
+		for (GlyphI child : children) {
+			moveOneChild(parent, child);
 		}
 	}
 
@@ -122,6 +81,4 @@ public class CollapsedTierPacker extends AbstractCoordPacker implements PaddedPa
 	public void setAlignment(int val) {
 		alignment = val;
 	}
-
-
 }

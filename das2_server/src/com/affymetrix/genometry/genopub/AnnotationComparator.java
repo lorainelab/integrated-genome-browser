cd8698e6e62f179fee01e32c88ccfcd45c0b9128
jnicol <jnicol@uncc.edu>
2010-02-02 15:55:34 +0000

    Fix a few compiler warnings by making these classes generic.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@5119 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- das2_server/src/com/affymetrix/genometry/genopub/AnnotationComparator.java
+++ das2_server/src/com/affymetrix/genometry/genopub/AnnotationComparator.java
@@ -4,15 +4,8 @@ package com.affymetrix.genometry.genopub;
 import java.io.Serializable;
 import java.util.Comparator;
 
-import com.affymetrix.genometry.genopub.Annotation;
-
-public class AnnotationComparator implements Comparator, Serializable {
-	public int compare(Object o1, Object o2) {
-		Annotation a1 = (Annotation)o1;
-		Annotation a2 = (Annotation)o2;
-
-
+public class AnnotationComparator implements Comparator<Annotation>, Serializable {
+	public int compare(Annotation a1, Annotation a2) {
 		return a1.getIdAnnotation().compareTo(a2.getIdAnnotation());
-
 	}
 }
--- das2_server/src/com/affymetrix/genometry/genopub/UserGroupComparator.java
+++ das2_server/src/com/affymetrix/genometry/genopub/UserGroupComparator.java
@@ -4,15 +4,8 @@ package com.affymetrix.genometry.genopub;
 import java.io.Serializable;
 import java.util.Comparator;
 
-import com.affymetrix.genometry.genopub.UserGroup;
-
- public class UserGroupComparator implements Comparator, Serializable {
-    public int compare(Object o1, Object o2) {
-      UserGroup g1 = (UserGroup)o1;
-      UserGroup g2 = (UserGroup)o2;
-      
-      
+ public class UserGroupComparator implements Comparator<UserGroup>, Serializable {
+    public int compare(UserGroup g1, UserGroup g2) {
       return g1.getIdUserGroup().compareTo(g2.getIdUserGroup());
-      
     }
   }

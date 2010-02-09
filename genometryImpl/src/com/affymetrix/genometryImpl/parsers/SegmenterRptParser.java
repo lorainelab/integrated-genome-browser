712b9fcb8faf3961acaf29e3f9f385366b88ff40
jnicol <jnicol@uncc.edu>
2010-02-09 18:27:59 +0000

    Code cleanup; turn off warning.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@5208 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/parsers/SegmenterRptParser.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/parsers/SegmenterRptParser.java
@@ -1,20 +1,5 @@
-/**
- *   Copyright (c) 2007 Affymetrix, Inc.
- *
- *   Licensed under the Common Public License, Version 1.0 (the "License").
- *   A copy of the license must be included with any distribution of
- *   this source code.
- *   Distributions from Affymetrix, Inc., place this in the
- *   IGB_LICENSE.html file.
- *
- *   The license is also available at
- *   http://www.opensource.org/licenses/cpl.php
- */
-
 package com.affymetrix.genometryImpl.parsers;
 
-import com.affymetrix.genometryImpl.util.SeqUtils;
-import com.affymetrix.genometryImpl.SeqSymmetry;
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.SingletonSymWithProps;
@@ -31,9 +16,6 @@ import java.util.regex.*;
  *  (This was based on the TabDelimitedParser, but then specialized.)
  */
 public final class SegmenterRptParser {
-	public static final String GAIN = "Gain";
-	public static final String LOSS = "Loss";
-	//public static final String LOH = "LOH";
 	public static final String CN_REGION_FILE_EXT = "cn_segments";
 	public static final String LOH_REGION_FILE_EXT = "loh_segments";
 
@@ -41,33 +23,55 @@ public final class SegmenterRptParser {
 	 *  the file's header.  Outside classes rarely need to use this; call
 	 *  {@link #getHeaderValue(String, SymWithProps)} instead.
 	 */
-	public static final String HEADER_PROP_KEY_NAME = "SegmenterFileHeader";
+	private static final String HEADER_PROP_KEY_NAME = "SegmenterFileHeader";
 
-	int chromosome_col;
-	int start_col;
-	int end_col;     // should need only end_col or length_col, not both
-	int length_col;  // should need only end_col or length_col, not both
-	int strand_col;  // column to use for determining strand
-	int cn_change_col;  // column to use for determining cn change type ("dup", "del", etc.)
-	int file_col;  // column to use for sample file name
+	private int chromosome_col;
+	private int start_col;
+	private int end_col;     // should need only end_col or length_col, not both
+	private int length_col;  // should need only end_col or length_col, not both
+	private int strand_col;  // column to use for determining strand
+	private int cn_change_col;  // column to use for determining cn change type ("dup", "del", etc.)
+	private int file_col;  // column to use for sample file name
 
-	boolean addToIndex; // whether to add annotation id's to the index on the seq group
-
-	int seq_col;
-	int seq_col2;
-	int start_col2;
-	int end_col2;     // should need only end_col or length_col, not both
-	int strand_col2;  // column to use for determining strand
 
 	// if makeProps, then each column (other than start, end, length, group) will become a
 	//    property in the SymWithProps that is generated
-	boolean make_props = true;
+	private final boolean make_props;
 
-	boolean use_length = false;
-	boolean use_strand = false;
-	boolean has_column_names_header_line;
+	private final boolean use_length;
+	private final boolean use_strand;
+	private final boolean has_column_names_header_line;
 
-	static final Pattern line_splitter = Pattern.compile("\t");
+	private static final Pattern line_splitter = Pattern.compile("\t");
+
+
+	// Sample header
+	//#MinimumMarkersInSegment=5
+	//#MinimumGenomicSegmentSize=100
+	//#MaximumOverlapOfCNVWithSNP=100
+	//#State=CN
+	//#Arrayset=GenomeWideSNP_6
+	//#genome=hg18
+	//#gender=Female
+	//#gender_threshold_test=In Bounds
+	//#madPairDiff=0.15253
+
+	//Possible values as below
+
+	//#gender=Female, Male, Unknown
+	//#gender_threshold_test=In Bounds, Out of Bounds
+
+
+
+	private static final Pattern headerPattern = Pattern.compile("^#(.*)=(.*)$");
+	private final Map<String,String> headerMap = new HashMap<String,String>();
+
+	private static final List<String> integerColumnNames = Arrays.asList(
+			"Copy Number",
+			"Size(kb)",
+			"#Markers", "Avg_DistBetweenMarkers(kb)",
+			"Start_Linear_Pos", "End_Linear_Position"
+			);
 
 	public static boolean isCnRegionsFilename(String s) {
 		return (s != null && s.toLowerCase().endsWith("." + CN_REGION_FILE_EXT.toLowerCase()));
@@ -81,12 +85,6 @@ public final class SegmenterRptParser {
 		this(true, true);
 	}
 
-	List<String> integerColumnNames = Arrays.asList(
-			"Copy Number", 
-			"Size(kb)", 
-			"#Markers", "Avg_DistBetweenMarkers(kb)", 
-			"Start_Linear_Pos", "End_Linear_Position"
-			);
 
 
 	public SegmenterRptParser(boolean props, boolean addToIndex) {
@@ -110,33 +108,9 @@ public final class SegmenterRptParser {
 		this.use_length = (this.length_col >= 0);
 		this.use_strand = (this.strand_col >= 0);
 
-		this.addToIndex = addToIndex;
 		this.make_props = props;
 	}
 
-	// Sample header
-	//#MinimumMarkersInSegment=5
-	//#MinimumGenomicSegmentSize=100
-	//#MaximumOverlapOfCNVWithSNP=100
-	//#State=CN
-	//#Arrayset=GenomeWideSNP_6
-	//#genome=hg18
-	//#gender=Female
-	//#gender_threshold_test=In Bounds
-	//#madPairDiff=0.15253
-
-	//Possible values as below
-
-	//#gender=Female, Male, Unknown
-	public static final String GENDER_KEY = "gender";
-	//#gender_threshold_test=In Bounds, Out of Bounds
-	public static final String BOUNDS_KEY = "gender_threshold_test";
-	public static final String OUT_OF_BOUNDS_VALUE = "Out of Bounds";
-
-
-
-	Pattern headerPattern = Pattern.compile("^#(.*)=(.*)$");
-	HashMap<String,String> headerMap = new HashMap<String,String>();
 
 	void parseHeaderLine(String line) {
 		Matcher m = headerPattern.matcher(line);
@@ -149,7 +123,7 @@ public final class SegmenterRptParser {
 
 	public void parse(InputStream istr, String default_type, AnnotatedSeqGroup seq_group) {
 
-		ArrayList<String> col_names = null;
+		List<String> col_names = null;
 
 		try {
 			InputStreamReader asr = new InputStreamReader(istr);
@@ -239,12 +213,6 @@ public final class SegmenterRptParser {
 				String id = change_type + " " + seq.getID() + ":" + start + "-" + end;
 				sym.setProperty("id", id);
 
-				//        sym.setProperty(TrackLineParser.ITEM_RGB, DUP.equalsIgnoreCase(change_type) ? Color.MAGENTA : Color.YELLOW);
-				//        IAnnotStyleExtended style = DefaultStateProvider.getGlobalStateProvider().getAnnotStyle(THE_METHOD);
-				//        style.getTransientPropertyMap().put(TrackLineParser.ITEM_RGB, "on");
-				//        style.setColor(Color.CYAN);
-				//System.out.println("Set color for style: " + style.getUniqueName());
-
 				if (make_props) {
 					for (int i=0; i<cols.length && i<col_names.size(); i++) {
 						String name = col_names.get(i);
@@ -274,60 +242,12 @@ public final class SegmenterRptParser {
 		}
 	}
 
-	public String getHeaderValue(String key, SymWithProps sym) {
+	public static String getHeaderValue(String key, SymWithProps sym) {
+		@SuppressWarnings("unchecked")
 		Map<String,String> map = (Map<String,String>) sym.getProperty(HEADER_PROP_KEY_NAME);
 		if (map != null) {
 			return map.get(key);
-		} else {
-			return null;
 		}
+		return null;
 	}
-
-	public static void main(String[] args) {
-		// 0 Sample
-		// 1 Chr
-		// 2 Cytoband_Start_Pos
-		// 3 Cytoband_End_Pos
-		// 4 CN_ChangeType
-		// 5 Size(kb)
-		// 6 CN_State
-		// 7 Start_ProbeSet
-		// 8 End_ProbeSet
-		// 9 Start_Physical_Pos
-		// 10 End_Physical_Position
-		// 11 #ProbeSet
-		// 12 %ProbeSets_withCNV
-		// 13 CNV_Annotation
-
-		//0// File	Chr	Cytoband_Start_Pos	Cytoband_End_Pos	CN_ChangeType	CN_State
-		//6// Size(kb)	#ProbeSet	Avg_DistBetweenProbeSets(kB)	%ProbeSets_withCNV
-		//10// Start_ProbeSet	End_ProbeSet
-		//12// Start_Linear_Pos	End_Linear_Position	CNV_Annotation
-
-
-
-		String filname = System.getProperty("user.dir") + "/data/copy_number/NA06985_GW6_C.cn_segments";
-		File file = new File(filname);
-		// type, start, end, length, strand, group, boolean props, boolean has_header
-		SegmenterRptParser tester = new SegmenterRptParser();
-		try {
-			FileInputStream fis = new FileInputStream(file);
-			AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
-
-			tester.parse(fis, file.getName(), seq_group);
-
-			for (int s=0; s<seq_group.getSeqCount(); s++) {
-				BioSeq aseq = seq_group.getSeq(s);
-				for (int i=0; i<aseq.getAnnotationCount(); i++) {
-					SeqSymmetry annot = aseq.getAnnotation(i);
-					SeqUtils.printSymmetry(annot, "  ", true);
-				}
-			}
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
-
 }

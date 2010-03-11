b0b025925fbe7c7acad8b1334c2aeac05fc1f302
hiralv <hiralv@uncc.edu>
2010-03-11 19:31:54 +0000

    Two bit parser test
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@5502 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/test/com/affymetrix/genometryImpl/parsers/TwoBitParserTest.java
@@ -0,0 +1,101 @@
+package com.affymetrix.genometryImpl.parsers;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hiralv
+ */
+public class TwoBitParserTest {
+	
+	String input_string = "ACTGGGTCTCAGTACTAGGAATTCCGTCATAGCTAAA";
+	String infile_name = "test/data/2bit/noblocks.2bit";
+	File infile = new File(infile_name);
+	StringBuffer sb;
+	int total_residues = input_string.length();
+
+	@Before
+	public void setup() throws Exception
+	{
+		assertTrue(infile.exists());
+	}
+
+	@Test
+	public void testOriginal() throws Exception
+	{
+		sb = new StringBuffer();
+		BioSeq seq= TwoBitParser.parse(infile);
+		assertEquals(seq.getResidues(),input_string);
+	}
+
+	@Test
+	public void testCases() throws Exception
+	{
+		testCase(0,input_string.length());  //From begining to end
+
+		testCase(4,18);						// even, even
+		testCase(6,7);						// even, odd
+		testCase(1,4);						// odd , even
+		testCase(1,5);						// odd , odd
+//		testCase(-1,3);						// Start out of range
+//		testCase(11,total_residues+4);		// End out of range
+//		testCase(-5,total_residues+5);      // Start and end out of range
+
+		testCase(2,22);
+		testCase(6,7);
+		testCase(1,2);
+		testCase(5,15);
+//		testCase(-9,9);
+//		testCase(1,total_residues+9);
+//		testCase(-15,total_residues+15);
+
+		testCase(0,2);
+		testCase(0,1);
+		testCase(1,2);
+		testCase(1,3);
+//		testCase(-11,15);
+//		testCase(10,total_residues+9);
+//		testCase(-11,total_residues+15);
+//
+//		testCase(0,0);						// 0 length
+//		testCase(1,0);						// Negative length
+//		testCase(total_residues,0);
+//		testCase(total_residues,-4);
+//		testCase(total_residues,total_residues);
+//		testCase(total_residues,total_residues+1);
+//		testCase(total_residues+1,total_residues);
+//		testCase(total_residues+1,total_residues+1);
+//		testCase(total_residues+1,total_residues+5);
+//		testCase(Integer.MIN_VALUE,Integer.MAX_VALUE);
+	}
+
+	public void testCase(int start, int length) throws Exception
+	{
+		sb = new StringBuffer();
+		//ByteArrayOutputStream outstream = new ByteArrayOutputStream();
+		BioSeq seq = TwoBitParser.parse(infile);
+
+		if (start < start + length) {
+			start = Math.max(0, start);
+			start = Math.min(total_residues, start);
+
+			length = Math.max(0, start + length);
+			length = Math.min(total_residues, start + length);
+		}
+		else
+		{
+			start = 0;
+			length = 0;
+		}
+
+		//assertTrue(result);
+		assertEquals(input_string.substring(start, start + length),seq.getResidues(start, length));
+		//System.out.println(input_string.substring(start, end) + "==" +outstream.toString());
+		//outstream.close();
+	}
+}

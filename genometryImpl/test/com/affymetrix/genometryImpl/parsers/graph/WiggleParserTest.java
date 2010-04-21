a9cba2e5b7c9acfaf646c9299361db6688f86a7f
hiralv <hiralv@uncc.edu>
2010-04-21 17:51:43 +0000

    Adding test for wiggle symloader.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@5721 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/test/com/affymetrix/genometryImpl/parsers/graph/WiggleParserTest.java
+++ genometryImpl/test/com/affymetrix/genometryImpl/parsers/graph/WiggleParserTest.java
@@ -85,6 +85,93 @@ public class WiggleParserTest {
 	}
 
 	@Test
+	public void testWiggle() throws IOException {
+		String filename = "test/data/wiggle/wiggleExample.wig";
+		assertTrue(new File(filename).exists());
+
+		AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
+		Wiggle wiggle = new Wiggle(new File(filename).toURI(), filename, seq_group);
+
+		List<GraphSym> results = wiggle.getGenome();
+
+		assertEquals(3, results.size());
+
+		GraphSym gr0 = results.get(0);
+
+		BioSeq seq = gr0.getGraphSeq();
+
+		// BED format
+		assertTrue(gr0 instanceof GraphIntervalSym);
+		assertEquals("chr19", gr0.getGraphSeq().getID());
+		assertEquals(9, gr0.getPointCount());
+		assertEquals(59302000, gr0.getSpan(seq).getMin());
+		assertEquals(59304700, gr0.getSpan(seq).getMax());
+
+		// variableStep format
+		GraphSym gr1 = results.get(1);
+		assertTrue(gr1 instanceof GraphIntervalSym);
+		assertEquals(9, gr1.getChildCount());
+		assertTrue(gr1.getChild(0) instanceof Scored);
+		assertEquals(59304701 - 1, gr1.getSpan(seq).getMin());	// variableStep: 1-relative format
+		assertEquals(59308021 - 1, gr1.getSpan(seq).getMax());	// variableStep: 1-relative foramt
+
+		// fixedStep format
+		GraphSym gr2 = results.get(2);
+		assertTrue(gr2 instanceof GraphIntervalSym);
+		assertEquals(10, gr2.getChildCount());
+		assertEquals(59307401 - 1, gr2.getSpan(seq).getMin());			// fixedStep: 1-relative format
+		assertEquals(59310301 - 1, gr2.getSpan(seq).getMax());			// fixedStep: 1-relative format
+		assertEquals(300.0f, ((Scored) gr2.getChild(7)).getScore(), 0.00000001);
+
+		assertEquals("Bed Format", gr0.getID());
+		assertEquals("variableStep", gr1.getID());
+		assertEquals("fixedStep", gr2.getID());
+
+		GraphState state = gr1.getGraphState();
+		assertEquals(0.0, state.getVisibleMinY(), 0.00001);
+		assertEquals(25.0, state.getVisibleMaxY(), 0.00001);
+
+		assertEquals(59310301 - 1, seq.getLength());	// fixedStep was 1-relative format.
+
+		seq_group = new AnnotatedSeqGroup("test");
+		BioSeq aseq = seq_group.addSeq("chr19", 59310300);
+
+		results = wiggle.getChromosome(aseq);
+		gr0 = results.get(0);
+		
+		assertTrue(gr0 instanceof GraphIntervalSym);
+		assertEquals("chr19", gr0.getGraphSeq().getID());
+		assertEquals(9, gr0.getPointCount());
+		assertEquals(59302000, gr0.getSpan(seq).getMin());
+		assertEquals(59304700, gr0.getSpan(seq).getMax());
+
+		gr1 = results.get(1);
+		assertTrue(gr1 instanceof GraphIntervalSym);
+		assertEquals(9, gr1.getChildCount());
+		assertTrue(gr1.getChild(0) instanceof Scored);
+		assertEquals(59304701 - 1, gr1.getSpan(seq).getMin());	// variableStep: 1-relative format
+		assertEquals(59308021 - 1, gr1.getSpan(seq).getMax());	// variableStep: 1-relative foramt
+
+		// fixedStep format
+		gr2 = results.get(2);
+		assertTrue(gr2 instanceof GraphIntervalSym);
+		assertEquals(10, gr2.getChildCount());
+		assertEquals(59307401 - 1, gr2.getSpan(seq).getMin());			// fixedStep: 1-relative format
+		assertEquals(59310301 - 1, gr2.getSpan(seq).getMax());			// fixedStep: 1-relative format
+		assertEquals(300.0f, ((Scored) gr2.getChild(7)).getScore(), 0.00000001);
+
+		assertEquals("Bed Format", gr0.getID());
+		assertEquals("variableStep", gr1.getID());
+		assertEquals("fixedStep", gr2.getID());
+
+		List<BioSeq> allSeqs = wiggle.getChromosomeList();
+		assertEquals(allSeqs.size(),1);
+
+		seq = allSeqs.get(0);
+		assertEquals(seq.getID(),"chr19");
+	}
+	
+	@Test
 	public void testWriteBarFormat() throws IOException {
 		String filename = "test/data/wiggle/wiggleExample.wig";
 		assertTrue(new File(filename).exists());

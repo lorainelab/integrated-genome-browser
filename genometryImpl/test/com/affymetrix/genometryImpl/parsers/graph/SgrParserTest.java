413b9614d28a0a38f0fd54fd0cb4f6eb4405cb5f
hiralv <hiralv@uncc.edu>
2010-04-23 00:54:16 +0000

    Adding test for Sgr symloader.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@5738 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/test/com/affymetrix/genometryImpl/parsers/graph/SgrParserTest.java
+++ genometryImpl/test/com/affymetrix/genometryImpl/parsers/graph/SgrParserTest.java
@@ -64,48 +64,48 @@ public class SgrParserTest {
 		assertEquals(string, outstream.toString());
 	}
 
-//	@Test
-//	public void testSgr() throws IOException{
-//		String filename = "test/data/sgr/test4.sgr";
-//		assertTrue(new File(filename).exists());
-//		AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
-//
-//		Sgr sgr = new Sgr(new File(filename).toURI(), filename, seq_group);
-//
-//		List<GraphSym> results = sgr.getGenome();
-//
-//		assertEquals(4, results.size());
-//		GraphSym gr0 = results.get(0);
-//
-//		assertEquals("16", gr0.getGraphSeq().getID());
-//		assertEquals(5, gr0.getPointCount());
-//		assertEquals(-0.0447924, gr0.getGraphYCoord(2), 0.01);
-//		assertEquals(0.275948, gr0.getGraphYCoord(3), 0.01);
-//		assertEquals(948028, gr0.getGraphXCoord(3));
-//
-//		GraphSym gr1 = results.get(1);
-//
-//		assertEquals("17", gr1.getGraphSeq().getID());
-//		assertEquals(4, gr1.getPointCount());
-//		assertEquals(0.8384833, gr1.getGraphYCoord(1), 0.01);
-//		assertEquals(0.4523419, gr1.getGraphYCoord(2), 0.01);
-//		assertEquals(948030, gr1.getGraphXCoord(2));
-//
-//
-//		GraphSym gr2 = results.get(2);
-//
-//		assertEquals("18", gr2.getGraphSeq().getID());
-//		assertEquals(2, gr2.getPointCount());
-//		assertEquals(0.9203930, gr2.getGraphYCoord(1), 0.01);
-//		assertEquals(0.2789456, gr2.getGraphYCoord(0), 0.01);
-//		assertEquals(948033, gr2.getGraphXCoord(0));
-//
-//		GraphSym gr3 = results.get(3);
-//
-//		assertEquals("19", gr3.getGraphSeq().getID());
-//		assertEquals(8, gr3.getPointCount());
-//		assertEquals(-0.0447924, gr3.getGraphYCoord(2), 0.01);
-//		assertEquals(0.275948, gr3.getGraphYCoord(3), 0.01);
-//		assertEquals(948028, gr3.getGraphXCoord(3));
-//	}
+	@Test
+	public void testSgr() throws IOException{
+		String filename = "test/data/sgr/test4.sgr";
+		assertTrue(new File(filename).exists());
+		AnnotatedSeqGroup seq_group = new AnnotatedSeqGroup("test");
+
+		Sgr sgr = new Sgr(new File(filename).toURI(), filename, seq_group);
+
+		List<GraphSym> results = sgr.getGenome();
+
+		assertEquals(4, results.size());
+		GraphSym gr0 = results.get(0);
+
+		assertEquals("16", gr0.getGraphSeq().getID());
+		assertEquals(5, gr0.getPointCount());
+		assertEquals(-0.0447924, gr0.getGraphYCoord(2), 0.01);
+		assertEquals(0.275948, gr0.getGraphYCoord(3), 0.01);
+		assertEquals(948028, gr0.getGraphXCoord(3));
+
+		GraphSym gr1 = results.get(1);
+
+		assertEquals("17", gr1.getGraphSeq().getID());
+		assertEquals(4, gr1.getPointCount());
+		assertEquals(0.8384833, gr1.getGraphYCoord(1), 0.01);
+		assertEquals(0.4523419, gr1.getGraphYCoord(2), 0.01);
+		assertEquals(948030, gr1.getGraphXCoord(2));
+
+
+		GraphSym gr2 = results.get(2);
+
+		assertEquals("18", gr2.getGraphSeq().getID());
+		assertEquals(2, gr2.getPointCount());
+		assertEquals(0.9203930, gr2.getGraphYCoord(1), 0.01);
+		assertEquals(0.2789456, gr2.getGraphYCoord(0), 0.01);
+		assertEquals(948033, gr2.getGraphXCoord(0));
+
+		GraphSym gr3 = results.get(3);
+
+		assertEquals("19", gr3.getGraphSeq().getID());
+		assertEquals(8, gr3.getPointCount());
+		assertEquals(-0.0447924, gr3.getGraphYCoord(2), 0.01);
+		assertEquals(0.275948, gr3.getGraphYCoord(3), 0.01);
+		assertEquals(948028, gr3.getGraphXCoord(3));
+	}
 }

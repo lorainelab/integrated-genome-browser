c0b58ab40251c50f2b9832a68404372651c01956
jnicol <jnicol@uncc.edu>
2010-05-07 19:15:00 +0000

    Move several of the LoadFileAction methods into UrlLoaderThread, which is currently the only location using it.  Should eventually be merged with symloader.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@5889 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/event/UrlLoaderThread.java
+++ igb/src/com/affymetrix/igb/event/UrlLoaderThread.java
@@ -14,16 +14,41 @@ package com.affymetrix.igb.event;
 
 import java.net.*;
 import java.io.*;
-import javax.swing.SwingUtilities;
-import com.affymetrix.genometryImpl.BioSeq;
-import com.affymetrix.igb.Application;
+import java.awt.HeadlessException;
+import javax.swing.*;
+import java.net.URI;
+import com.affymetrix.genometryImpl.SeqSymmetry;
 import com.affymetrix.genometryImpl.GenometryModel;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.parsers.BedParser;
+import com.affymetrix.genometryImpl.parsers.BgnParser;
+import com.affymetrix.genometryImpl.parsers.Bprobe1Parser;
 import com.affymetrix.genometryImpl.parsers.BpsParser;
+import com.affymetrix.genometryImpl.parsers.BrptParser;
+import com.affymetrix.genometryImpl.parsers.BrsParser;
+import com.affymetrix.genometryImpl.parsers.BsnpParser;
+import com.affymetrix.genometryImpl.parsers.CytobandParser;
 import com.affymetrix.genometryImpl.parsers.Das2FeatureSaxParser;
+import com.affymetrix.genometryImpl.parsers.ExonArrayDesignParser;
+import com.affymetrix.genometryImpl.parsers.FastaParser;
+import com.affymetrix.genometryImpl.parsers.FishClonesParser;
+import com.affymetrix.genometryImpl.parsers.GFFParser;
+import com.affymetrix.genometryImpl.parsers.GFF3Parser;
+import com.affymetrix.genometryImpl.parsers.NibbleResiduesParser;
 import com.affymetrix.genometryImpl.parsers.PSLParser;
+import com.affymetrix.genometryImpl.parsers.SegmenterRptParser;
+import com.affymetrix.genometryImpl.parsers.VarParser;
 import com.affymetrix.genometryImpl.parsers.das.DASFeatureParser;
-import com.affymetrix.genoviz.util.GeneralUtils;
+import com.affymetrix.genometryImpl.parsers.gchp.AffyCnChpParser;
+import com.affymetrix.genometryImpl.parsers.gchp.ChromLoadPolicy;
+import com.affymetrix.genometryImpl.parsers.graph.CntParser;
+import com.affymetrix.genometryImpl.parsers.graph.ScoredMapParser;
+import com.affymetrix.genometryImpl.parsers.graph.ScoredIntervalParser;
+import com.affymetrix.genometryImpl.parsers.useq.USeqRegionParser;
+import com.affymetrix.genometryImpl.util.GeneralUtils;
+import com.affymetrix.genoviz.swing.threads.InvokeUtils;
+import com.affymetrix.igb.Application;
 import com.affymetrix.genoviz.util.ErrorHandler;
 import com.affymetrix.igb.view.SeqMapView;
 import com.affymetrix.igb.menuitem.LoadFileAction;
@@ -285,7 +310,7 @@ public final class UrlLoaderThread extends Thread {
 				// url.getPath() is OK for this purpose, url.getFile() is not because
 				// url.getFile() = url.getPath() + url.getQuery()
 				String filename = url.getPath();
-				LoadFileAction.load(gviewer.getFrame(), stream, filename, seq_group, aseq);
+				load(gviewer.getFrame(), stream, filename, seq_group, aseq);
 			} else if (content_type == null
 					|| content_type.startsWith("content/unknown")
 					|| content_type.startsWith("application/zip")
@@ -300,7 +325,7 @@ public final class UrlLoaderThread extends Thread {
 					}
 					filename += file_extension;
 				}
-				LoadFileAction.load(gviewer.getFrame(), stream, filename, seq_group, aseq);
+				load(gviewer.getFrame(), stream, filename, seq_group, aseq);
 			} else if (content_type.startsWith("binary/bps")) {
 				parseBinaryBps(stream, type);
 			} else if (content_type.startsWith(Das2FeatureSaxParser.FEATURES_CONTENT_TYPE)) {
@@ -319,6 +344,257 @@ public final class UrlLoaderThread extends Thread {
 			}
 		} finally {
 			GeneralUtils.safeClose(stream);
+		}
+	}
+
+
+
+	/** Loads from an InputStream.
+	 *  Detects the type of file based on the filename ending of the
+	 *  stream_name parameter, for example ".dasxml".
+	 *  The stream will be passed through uncompression routines
+	 *  if necessary.
+	 */
+	private static void load(JFrame gviewerFrame, InputStream instr, String stream_name, AnnotatedSeqGroup selected_group, BioSeq input_seq) throws IOException {
+		if (selected_group == null) {
+			// this should never happen
+			throw new IOException("Must select a genome before loading a file");
+		}
+
+		Logger.getLogger(UrlLoaderThread.class.getName()).log(Level.INFO,
+				"loading file: " + stream_name);
+
+		Exception the_exception = null;
+		InputStream str = null;
+
+		try {
+			StringBuffer stripped_name = new StringBuffer();
+			str = GeneralUtils.unzipStream(instr, stream_name, stripped_name);
+			stream_name = stripped_name.toString();
+
+			if (str instanceof BufferedInputStream) {
+				str = (BufferedInputStream) str;
+			} else {
+				str = new BufferedInputStream(str);
+			}
+			DoParse(str, selected_group, input_seq, stream_name, gviewerFrame);
+		} catch (Exception ex) {
+			the_exception = ex;
+			//ErrorHandler.errorPanel(gviewerFrame, "ERROR", "Error loading file", ex);
+		} finally {
+			GeneralUtils.safeClose(str);
+		}
+
+		// The purpose of calling setSelectedSeqGroup, even if identity of
+		// the seq group has not changed, is to make sure that
+		// the DataLoadView and the AnnotBrowserView update their displays.
+		// (Because the contents of the seq group may have changed.)
+		//
+		// Note that this must be done regardless of whether this load() method was
+		// called from inside this class or in loading a bookmark, etc.
+
+		gmodel.setSelectedSeqGroup(gmodel.getSelectedSeqGroup());
+
+		if (the_exception != null) {
+			if (the_exception instanceof IOException) {
+				throw (IOException) the_exception;
+			} else {
+				IOException new_exception = new IOException();
+				new_exception.initCause(the_exception);
+				throw new_exception;
+			}
+		}
+	}
+
+
+	private static void DoParse(
+					InputStream str, AnnotatedSeqGroup selected_group, BioSeq input_seq,
+					String stream_name, JFrame gviewerFrame)
+					throws IOException, InterruptedException, HeadlessException, SAXException {
+		String lcname = stream_name.toLowerCase();
+
+		int dotIndex = stream_name.lastIndexOf('.');
+		String annot_type = dotIndex <= 0 ? stream_name : stream_name.substring(0, dotIndex);
+
+		// Sequence files
+		if (lcname.endsWith(".bnib")) {
+			BioSeq aseq = NibbleResiduesParser.parse(str, selected_group);
+			if (aseq != gmodel.getSelectedSeq()) {
+				//TODO: maybe set the current seq to this seq
+				Logger.getLogger(LoadFileAction.class.getName()).log(Level.WARNING,
+					"This is not the currently-selected sequence.");
+			}
+			return;
+		}
+		if (lcname.endsWith(".fa") || lcname.endsWith(".fas") || lcname.endsWith(".fasta")) {
+			FastaParser.parseAll(str, selected_group);
+			return;
+		}
+
+		// Annotation and graphs
+		if (lcname.endsWith(".bed")) {
+			BedParser parser = new BedParser();
+			// really need to switch create_container (last argument) to true soon!
+			parser.parse(str, gmodel, selected_group, true, annot_type, false);
+			return;
+		}
+		if (lcname.endsWith(".bgn")) {
+			BgnParser parser = new BgnParser();
+			parser.parse(str, annot_type, selected_group, true);
+			return;
+		}
+
+		if (lcname.endsWith(".bps")) {
+			DataInputStream dis = new DataInputStream(str);
+			BpsParser.parse(dis, annot_type, null, selected_group, false, true);
+			return;
+		}
+		if (lcname.endsWith(".bp1") || lcname.endsWith(".bp2")) {
+			Bprobe1Parser parser = new Bprobe1Parser();
+			parser.parse(str, selected_group, true, annot_type, true);
+			return;
+		}
+		if (lcname.endsWith(".brpt")) {
+			BrptParser parser = new BrptParser();
+			List<SeqSymmetry> alist = parser.parse(str, annot_type, selected_group, true);
+			Logger.getLogger(LoadFileAction.class.getName()).log(Level.FINE,
+					"total repeats loaded: " + alist.size());
+			return;
+		}
+		if (lcname.endsWith(".brs")) {
+			BrsParser.parse(str, annot_type, selected_group);
+			return;
+		}
+		if (lcname.endsWith(".bsnp")) {
+			List<SeqSymmetry> alist = BsnpParser.parse(str, annot_type, selected_group, true);
+			Logger.getLogger(LoadFileAction.class.getName()).log(Level.FINE,
+					"total snps loaded: " + alist.size());
+			return;
+		}
+
+		if (lcname.endsWith(".cnchp") || lcname.endsWith(".lohchp")) {
+			AffyCnChpParser parser = new AffyCnChpParser();
+			parser.parse(null, ChromLoadPolicy.getLoadAllPolicy(), str, stream_name, selected_group);
+			return;
+		}
+		if (lcname.endsWith(".cnt")) {
+			CntParser parser = new CntParser();
+			parser.parse(str, selected_group);
+			return;
+		}
+		if (lcname.endsWith(".cyt")) {
+			CytobandParser parser = new CytobandParser();
+			parser.parse(str, selected_group, true);
+			return;
+		}
+		if (lcname.endsWith(".das") || lcname.endsWith(".dasxml")) {
+			DASFeatureParser parser = new DASFeatureParser();
+			try {
+				parser.parse(str, selected_group);
+			} catch (XMLStreamException ex) {
+				Logger.getLogger(LoadFileAction.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			return;
+		}
+		if (lcname.endsWith(".das2xml")) {
+			Das2FeatureSaxParser parser = new Das2FeatureSaxParser();
+			parser.parse(new InputSource(str), stream_name, selected_group, true);
+			return;
+		}
+		if (lcname.endsWith(".ead")) {
+			ExonArrayDesignParser parser = new ExonArrayDesignParser();
+			parser.parse(str, selected_group, true, annot_type);
+			return;
+		}
+		if (lcname.endsWith("." + FishClonesParser.FILE_EXT)) {
+			FishClonesParser parser = new FishClonesParser(true);
+			parser.parse(str, annot_type, selected_group);
+			return;
+		}
+		if (lcname.endsWith(".gff") || lcname.endsWith(".gtf")) {
+			// assume it's GFF1, GFF2, GTF, or GFF3 format
+			GFFParser parser = new GFFParser();
+			parser.setUseStandardFilters(true);
+			parser.parse(str, annot_type, selected_group, false);
+			return;
+		}
+		if (lcname.endsWith(".gff3")) {
+			/* Force parcing as GFF3 */
+			GFF3Parser parser = new GFF3Parser();
+			parser.parse(str, annot_type, selected_group);
+			return;
+		}
+
+		if (lcname.endsWith(".map")) {
+			ScoredMapParser parser = new ScoredMapParser();
+			parser.parse(str, stream_name, input_seq, selected_group);
+			return;
+		}
+		if (lcname.endsWith(".psl") || lcname.endsWith(".psl3")) {
+			ParsePSL(lcname, gviewerFrame, str, stream_name, selected_group);
+			return;
+		}
+		if ((lcname.endsWith("." + SegmenterRptParser.CN_REGION_FILE_EXT) || lcname.endsWith("." + SegmenterRptParser.LOH_REGION_FILE_EXT))) {
+			SegmenterRptParser parser = new SegmenterRptParser();
+			parser.parse(str, stream_name, selected_group);
+			return;
+		}
+		if (lcname.endsWith(".sin") || lcname.endsWith(".egr") || lcname.endsWith(".txt")) {
+			ScoredIntervalParser parser = new ScoredIntervalParser();
+			parser.parse(str, stream_name, selected_group);
+			return;
+		}
+		if (lcname.endsWith(".useq")) {
+			USeqRegionParser parser = new USeqRegionParser();
+			parser.parse(str, selected_group, stream_name, true, null);
+			return;
+		}
+		if (lcname.endsWith(".var")) {
+			VarParser parser = new VarParser();
+			parser.parse(str, selected_group);
+			return;
+		}
+		ErrorHandler.errorPanel(gviewerFrame, "FORMAT NOT RECOGNIZED", "Format not recognized for file: " + stream_name, null);
+	}
+
+	// Parse PSL files.  These files specifically have .psl or .psl3 extensions.
+	private static void ParsePSL(
+					String lcname, JFrame gviewerFrame, InputStream str, String stream_name, AnnotatedSeqGroup selected_group)
+					throws IOException, HeadlessException, InterruptedException {
+		PSLParser parser = new PSLParser();
+		parser.enableSharedQueryTarget(true);
+		int psl_option = -1;
+
+		// If the name ends with ".link.psl" then assume it is a mapping
+		// of probe sets to consensus seqs to genome, and thus select
+		// psl_option = 1 "target".
+
+		if (lcname.endsWith(".link.psl")) {
+			parser.setIsLinkPsl(true);
+			psl_option = 1; // "target"
+		} else if (lcname.endsWith(".psl")) {
+			psl_option = 1;
+		} else {
+			// .psl3 file
+			// the user has to tell us whether to annotate the "query" or "target" or "other"
+			Object[] options = new Object[]{"Query", "Target", "Other"};
+
+			if (SwingUtilities.isEventDispatchThread()) {
+				psl_option = JOptionPane.showOptionDialog(gviewerFrame, "Annotate which sequence?", "PSL annotation options", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, "Target");
+			} else {
+				psl_option = InvokeUtils.invokeOptionDialog(gviewerFrame, "Annotate which sequence?", "PSL annotation options", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, "Target");
+			}
+		}
+		boolean annotate_query = psl_option == 0;
+		boolean annotate_target = psl_option == 1;
+		boolean annotate_other = psl_option == 2;
+
+		if (annotate_query) {
+			parser.parse(str, stream_name, selected_group, null, null, annotate_query, annotate_target, annotate_other);
+		} else if (annotate_target) {
+			parser.parse(str, stream_name, null, selected_group, null, annotate_query, annotate_target, annotate_other);
+		} else if (annotate_other) {
+			parser.parse(str, stream_name, null, null, selected_group, annotate_query, annotate_target, annotate_other);
 		}
 	}
 
--- igb/src/com/affymetrix/igb/menuitem/LoadFileAction.java
+++ igb/src/com/affymetrix/igb/menuitem/LoadFileAction.java
@@ -15,56 +15,20 @@ package com.affymetrix.igb.menuitem;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.util.*;
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.HeadlessException;
 import java.awt.event.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
-import javax.xml.stream.XMLStreamException;
 import java.net.URI;
 import java.text.MessageFormat;
-import org.xml.sax.SAXException;
-import org.xml.sax.InputSource;
-import com.affymetrix.genometryImpl.SeqSymmetry;
-import com.affymetrix.genometryImpl.SeqSpan;
 import com.affymetrix.genometryImpl.GenometryModel;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.general.GenericFeature;
 import com.affymetrix.genometryImpl.general.GenericVersion;
-import com.affymetrix.genometryImpl.parsers.BedParser;
-import com.affymetrix.genometryImpl.parsers.BgnParser;
-import com.affymetrix.genometryImpl.parsers.Bprobe1Parser;
-import com.affymetrix.genometryImpl.parsers.BpsParser;
-import com.affymetrix.genometryImpl.parsers.BrptParser;
-import com.affymetrix.genometryImpl.parsers.BrsParser;
-import com.affymetrix.genometryImpl.parsers.BsnpParser;
-import com.affymetrix.genometryImpl.parsers.CytobandParser;
-import com.affymetrix.genometryImpl.parsers.Das2FeatureSaxParser;
-import com.affymetrix.genometryImpl.parsers.ExonArrayDesignParser;
-import com.affymetrix.genometryImpl.parsers.FastaParser;
 import com.affymetrix.genometryImpl.parsers.FishClonesParser;
-import com.affymetrix.genometryImpl.parsers.GFFParser;
-import com.affymetrix.genometryImpl.parsers.GFF3Parser;
-import com.affymetrix.genometryImpl.parsers.NibbleResiduesParser;
-import com.affymetrix.genometryImpl.parsers.PSLParser;
 import com.affymetrix.genometryImpl.parsers.SegmenterRptParser;
-import com.affymetrix.genometryImpl.parsers.VarParser;
-import com.affymetrix.genometryImpl.parsers.das.DASFeatureParser;
-import com.affymetrix.genometryImpl.parsers.das.DASSymmetry;
-import com.affymetrix.genometryImpl.parsers.gchp.AffyCnChpParser;
-import com.affymetrix.genometryImpl.parsers.gchp.ChromLoadPolicy;
-import com.affymetrix.genometryImpl.parsers.graph.CntParser;
-import com.affymetrix.genometryImpl.parsers.graph.ScoredMapParser;
-import com.affymetrix.genometryImpl.parsers.graph.ScoredIntervalParser;
-import com.affymetrix.genometryImpl.parsers.useq.USeqRegionParser;
 import com.affymetrix.genometryImpl.util.GeneralUtils;
 import com.affymetrix.genometryImpl.util.LoadUtils.ServerStatus;
 import com.affymetrix.genometryImpl.util.UniFileFilter;
-import com.affymetrix.genoviz.swing.threads.InvokeUtils;
 import com.affymetrix.genoviz.util.FileDropHandler;
 import com.affymetrix.genoviz.util.ErrorHandler;
 import com.affymetrix.igb.Application;
@@ -72,10 +36,8 @@ import com.affymetrix.igb.IGB;
 import com.affymetrix.igb.general.ServerList;
 import com.affymetrix.igb.symloader.QuickLoad;
 import com.affymetrix.igb.util.MergeOptionChooser;
-import com.affymetrix.igb.util.ThreadUtils;
 import com.affymetrix.igb.view.DataLoadView;
 import com.affymetrix.igb.view.load.GeneralLoadUtils;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
@@ -303,67 +265,6 @@ public final class LoadFileAction extends AbstractAction {
 	}
 
 
-
-	/** Loads from an InputStream.
-	 *  Detects the type of file based on the filename ending of the
-	 *  stream_name parameter, for example ".dasxml".
-	 *  The stream will be passed through uncompression routines
-	 *  if necessary.
-	 */
-	public static BioSeq load(JFrame gviewerFrame, InputStream instr, String stream_name, AnnotatedSeqGroup selected_group, BioSeq input_seq) throws IOException {
-		if (selected_group == null) {
-			// this should never happen
-			throw new IOException("Must select a genome before loading a file");
-		}
-
-		Logger.getLogger(LoadFileAction.class.getName()).log(Level.INFO,
-				"loading file: " + stream_name);
-
-		Exception the_exception = null;
-		BioSeq aseq = null;
-		InputStream str = null;
-
-		try {
-			StringBuffer stripped_name = new StringBuffer();
-			str = GeneralUtils.unzipStream(instr, stream_name, stripped_name);
-			stream_name = stripped_name.toString();
-
-			if (str instanceof BufferedInputStream) {
-				str = (BufferedInputStream) str;
-			} else {
-				str = new BufferedInputStream(str);
-			}
-			aseq = DoParse(str, selected_group, input_seq, stream_name, gviewerFrame);
-		} catch (Exception ex) {
-			the_exception = ex;
-			//ErrorHandler.errorPanel(gviewerFrame, "ERROR", "Error loading file", ex);
-		} finally {
-			GeneralUtils.safeClose(str);
-		}
-
-		// The purpose of calling setSelectedSeqGroup, even if identity of
-		// the seq group has not changed, is to make sure that
-		// the DataLoadView and the AnnotBrowserView update their displays.
-		// (Because the contents of the seq group may have changed.)
-		//
-		// Note that this must be done regardless of whether this load() method was
-		// called from inside this class or in loading a bookmark, etc.
-
-		GenometryModel gmodel = GenometryModel.getGenometryModel();
-		gmodel.setSelectedSeqGroup(gmodel.getSelectedSeqGroup());
-
-		if (the_exception != null) {
-			if (the_exception instanceof IOException) {
-				throw (IOException) the_exception;
-			} else {
-				IOException new_exception = new IOException();
-				new_exception.initCause(the_exception);
-				throw new_exception;
-			}
-		}
-		return aseq;
-	}
-
 	private static void openURLAction(JFrame gviewerFrame,String url){
 		try {
 			URI uri = new URI(url.trim());
@@ -404,218 +305,4 @@ public final class LoadFileAction extends AbstractAction {
 		
 		return true;
 	}
-
-	private static BioSeq DoParse(
-					InputStream str, AnnotatedSeqGroup selected_group, BioSeq input_seq,
-					String stream_name, JFrame gviewerFrame)
-					throws IOException, InterruptedException, HeadlessException, SAXException {
-		String lcname = stream_name.toLowerCase();
-
-		int dotIndex = stream_name.lastIndexOf('.');
-		String annot_type = dotIndex <= 0 ? stream_name : stream_name.substring(0, dotIndex);
-
-		// Sequence files
-		if (lcname.endsWith(".bnib")) {
-			BioSeq aseq = NibbleResiduesParser.parse(str, selected_group);
-			GenometryModel gmodel = GenometryModel.getGenometryModel();
-			if (aseq != gmodel.getSelectedSeq()) {
-				//TODO: maybe set the current seq to this seq
-				Logger.getLogger(LoadFileAction.class.getName()).log(Level.WARNING,
-					"This is not the currently-selected sequence.");
-			}
-			return aseq;
-		}
-		if (lcname.endsWith(".fa") || lcname.endsWith(".fas") || lcname.endsWith(".fasta")) {
-			List<BioSeq> seqs = FastaParser.parseAll(str, selected_group);
-			if (input_seq != null && seqs.contains(input_seq)) {
-				return input_seq;
-			}
-			if (!seqs.isEmpty()) {
-				return seqs.get(0);
-			}
-			return null;
-		}
-
-		// Annotation and graphs
-		if (lcname.endsWith(".bed")) {
-			BedParser parser = new BedParser();
-			// really need to switch create_container (last argument) to true soon!
-			GenometryModel gmodel = GenometryModel.getGenometryModel();
-			parser.parse(str, gmodel, selected_group, true, annot_type, false);
-			return input_seq;
-		}
-		if (lcname.endsWith(".bgn")) {
-			BgnParser parser = new BgnParser();
-			parser.parse(str, annot_type, selected_group, true);
-			return input_seq;
-		}
-		
-		if (lcname.endsWith(".bps")) {
-			DataInputStream dis = new DataInputStream(str);
-			BpsParser.parse(dis, annot_type, null, selected_group, false, true);
-			return input_seq;
-		}
-		if (lcname.endsWith(".bp1") || lcname.endsWith(".bp2")) {
-			Bprobe1Parser parser = new Bprobe1Parser();
-			parser.parse(str, selected_group, true, annot_type, true);
-			return input_seq;
-		}
-		if (lcname.endsWith(".brpt")) {
-			BrptParser parser = new BrptParser();
-			List<SeqSymmetry> alist = parser.parse(str, annot_type, selected_group, true);
-			Logger.getLogger(LoadFileAction.class.getName()).log(Level.FINE,
-					"total repeats loaded: " + alist.size());
-			return input_seq;
-		}
-		if (lcname.endsWith(".brs")) {
-			BrsParser.parse(str, annot_type, selected_group);
-			return input_seq;
-		}
-		if (lcname.endsWith(".bsnp")) {
-			List<SeqSymmetry> alist = BsnpParser.parse(str, annot_type, selected_group, true);
-			Logger.getLogger(LoadFileAction.class.getName()).log(Level.FINE,
-					"total snps loaded: " + alist.size());
-			return input_seq;
-		}
-
-		if (lcname.endsWith(".cnchp") || lcname.endsWith(".lohchp")) {
-			AffyCnChpParser parser = new AffyCnChpParser();
-			parser.parse(null, ChromLoadPolicy.getLoadAllPolicy(), str, stream_name, selected_group);
-			return input_seq;
-		}
-		if (lcname.endsWith(".cnt")) {
-			CntParser parser = new CntParser();
-			parser.parse(str, selected_group);
-			return input_seq;
-		}
-		if (lcname.endsWith(".cyt")) {
-			CytobandParser parser = new CytobandParser();
-			parser.parse(str, selected_group, true);
-			return input_seq;
-		}
-		if (lcname.endsWith(".das") || lcname.endsWith(".dasxml")) {
-			DASFeatureParser parser = new DASFeatureParser();
-			Collection<DASSymmetry> results;
-			try {
-				results = parser.parse(str, selected_group);
-				return LoadFileAction.<DASSymmetry>getFirstSeq(results);
-			} catch (XMLStreamException ex) {
-				Logger.getLogger(LoadFileAction.class.getName()).log(Level.SEVERE, null, ex);
-			}
-			return null;
-		}
-		if (lcname.endsWith(".das2xml")) {
-			Das2FeatureSaxParser parser = new Das2FeatureSaxParser();
-			List<SeqSymmetry> results = parser.parse(new InputSource(str), stream_name, selected_group, true);
-			return LoadFileAction.<SeqSymmetry>getFirstSeq(results);
-		}
-		if (lcname.endsWith(".ead")) {
-			ExonArrayDesignParser parser = new ExonArrayDesignParser();
-			parser.parse(str, selected_group, true, annot_type);
-			return input_seq;
-		}
-		if (lcname.endsWith("." + FishClonesParser.FILE_EXT)) {
-			FishClonesParser parser = new FishClonesParser(true);
-			parser.parse(str, annot_type, selected_group);
-			return input_seq;
-		}
-		if (lcname.endsWith(".gff") || lcname.endsWith(".gtf")) {
-			// assume it's GFF1, GFF2, GTF, or GFF3 format
-			GFFParser parser = new GFFParser();
-			parser.setUseStandardFilters(true);
-			parser.parse(str, annot_type, selected_group, false);
-			return null;
-		}
-		if (lcname.endsWith(".gff3")) {
-			/* Force parcing as GFF3 */
-			GFF3Parser parser = new GFF3Parser();
-			parser.parse(str, annot_type, selected_group);
-			return input_seq;
-		}
-		
-		if (lcname.endsWith(".map")) {
-			ScoredMapParser parser = new ScoredMapParser();
-			parser.parse(str, stream_name, input_seq, selected_group);
-			return input_seq;
-		}
-		if (lcname.endsWith(".psl") || lcname.endsWith(".psl3")) {
-			ParsePSL(lcname, gviewerFrame, str, stream_name, selected_group);
-			return input_seq;
-		}
-		if ((lcname.endsWith("." + SegmenterRptParser.CN_REGION_FILE_EXT) || lcname.endsWith("." + SegmenterRptParser.LOH_REGION_FILE_EXT))) {
-			SegmenterRptParser parser = new SegmenterRptParser();
-			parser.parse(str, stream_name, selected_group);
-			return input_seq;
-		}
-		if (lcname.endsWith(".sin") || lcname.endsWith(".egr") || lcname.endsWith(".txt")) {
-			ScoredIntervalParser parser = new ScoredIntervalParser();
-			parser.parse(str, stream_name, selected_group);
-			return input_seq;
-		}
-		if (lcname.endsWith(".useq")) {
-			USeqRegionParser parser = new USeqRegionParser();
-			parser.parse(str, selected_group, stream_name, true, null);
-			return input_seq;
-		}
-		if (lcname.endsWith(".var")) {
-			VarParser parser = new VarParser();
-			parser.parse(str, selected_group);
-			return input_seq;
-		}
-		ErrorHandler.errorPanel(gviewerFrame, "FORMAT NOT RECOGNIZED", "Format not recognized for file: " + stream_name, null);
-		return null;
-	}
-
-	// Parse PSL files.  These files specifically have .psl or .psl3 extensions.
-	private static void ParsePSL(
-					String lcname, JFrame gviewerFrame, InputStream str, String stream_name, AnnotatedSeqGroup selected_group)
-					throws IOException, HeadlessException, InterruptedException {
-		PSLParser parser = new PSLParser();
-		parser.enableSharedQueryTarget(true);
-		int psl_option = -1;
-
-		// If the name ends with ".link.psl" then assume it is a mapping
-		// of probe sets to consensus seqs to genome, and thus select
-		// psl_option = 1 "target".
-
-		if (lcname.endsWith(".link.psl")) {
-			parser.setIsLinkPsl(true);
-			psl_option = 1; // "target"
-		} else if (lcname.endsWith(".psl")) {
-			psl_option = 1;
-		} else {
-			// .psl3 file
-			// the user has to tell us whether to annotate the "query" or "target" or "other"
-			Object[] options = new Object[]{"Query", "Target", "Other"};
-
-			if (SwingUtilities.isEventDispatchThread()) {
-				psl_option = JOptionPane.showOptionDialog(gviewerFrame, "Annotate which sequence?", "PSL annotation options", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, "Target");
-			} else {
-				psl_option = InvokeUtils.invokeOptionDialog(gviewerFrame, "Annotate which sequence?", "PSL annotation options", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, "Target");
-			}
-		}
-		boolean annotate_query = psl_option == 0;
-		boolean annotate_target = psl_option == 1;
-		boolean annotate_other = psl_option == 2;
-
-		if (annotate_query) {
-			parser.parse(str, stream_name, selected_group, null, null, annotate_query, annotate_target, annotate_other);
-		} else if (annotate_target) {
-			parser.parse(str, stream_name, null, selected_group, null, annotate_query, annotate_target, annotate_other);
-		} else if (annotate_other) {
-			parser.parse(str, stream_name, null, null, selected_group, annotate_query, annotate_target, annotate_other);
-		}
-	}
-
-	/** Returns the first BioSeq on the first SeqSymmetry in the given list, or null. */
-	private static <S extends SeqSymmetry> BioSeq getFirstSeq(Collection<S> syms) {
-		BioSeq first_seq = null;
-		if (syms != null && !syms.isEmpty()) {
-			SeqSymmetry fsym = syms.iterator().next();
-			SeqSpan fspan = fsym.getSpan(0);
-			first_seq = fspan.getBioSeq();
-		}
-		return first_seq;
-	}
-
 }

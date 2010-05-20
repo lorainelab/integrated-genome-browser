aa2a88cdded685ed25b4476759adedeb5ce2b2ad
jnicol <jnicol@uncc.edu>
2010-05-20 20:03:16 +0000

    Adding vikram's change (with modifications) to allow printing without a dialog.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@5974 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genoviz_sdk/src/com/affymetrix/genoviz/util/ComponentPagePrinter.java
+++ genoviz_sdk/src/com/affymetrix/genoviz/util/ComponentPagePrinter.java
@@ -10,13 +10,11 @@
  *   The license is also available at
  *   http://www.opensource.org/licenses/cpl.php
  */
-
 package com.affymetrix.genoviz.util;
 
 import java.awt.*;
 import java.awt.print.*;
 import javax.swing.*;
-
 
 /**
  * Prints a component.
@@ -27,92 +25,105 @@ import javax.swing.*;
  * <p> Turns off double-buffering in Swing components.
  */
 public class ComponentPagePrinter implements Printable {
-  static boolean DEBUG = false;
-  boolean DISABLE_SWING_BUFFERING = true;
 
-  // The component to be printed.
-  Component comp;
+	static boolean DEBUG = false;
+	boolean DISABLE_SWING_BUFFERING = true;
+	// The component to be printed.
+	Component comp;
 
-  public ComponentPagePrinter(Component c) {
-    comp = c;
-  }
+	public ComponentPagePrinter(Component c) {
+		comp = c;
+	}
 
-  public void print(int orientation) throws PrinterException {
-    if (orientation != PageFormat.LANDSCAPE &&
-        orientation != PageFormat.PORTRAIT &&
-        orientation != PageFormat.REVERSE_LANDSCAPE) {
-      throw new PrinterException("Orientation not recognized");
-    }
+	public void print(int orientation) throws PrinterException {
+		print(orientation, false);
+	}
 
-    PrinterJob pjob = PrinterJob.getPrinterJob();
-    PageFormat preformat = pjob.defaultPage();
-    preformat.setOrientation(orientation);
+	/*  prints this component with option to show dialogue box
+	 *	Date: 5/19/2010
+	 *	Author: vikram
+	 */
+	public void print(int orientation, boolean noDialog) throws PrinterException {
+		if (orientation != PageFormat.LANDSCAPE &&
+				orientation != PageFormat.PORTRAIT &&
+				orientation != PageFormat.REVERSE_LANDSCAPE) {
+			throw new PrinterException("Orientation not recognized");
+		}
 
-    // show the page format dialog, allow user changes to format
-    PageFormat postformat = pjob.pageDialog(preformat);
-    // if preformat is identical to (the same object as) postformat,
-    // that means that the user pressed the "Cancel" button.
-    if (preformat != postformat) {
-      pjob.setPrintable(this, postformat);
+		PrinterJob pjob = PrinterJob.getPrinterJob();
+		PageFormat preformat = pjob.defaultPage();
+		preformat.setOrientation(orientation);
+		if (noDialog) {
+			pjob.setPrintable(this, preformat);
 
-      // show the print dialog, allow user to change options or cancel
-      if (pjob.printDialog()) {
-        pjob.print();
-      }
-    }
-  }
+			pjob.print();
+		} else {
+			PageFormat postformat = pjob.pageDialog(preformat);
+			// if preformat is identical to (the same object as) postformat,
+			// that means that the user pressed the "Cancel" button.
+			if (preformat != postformat) {
+				pjob.setPrintable(this, postformat);
+				
+				// show the print dialog, allow user to change options or cancel
+				if (pjob.printDialog()) {
+					pjob.print();
+				}
+			}
 
-  public void print() throws PrinterException {
-    print(PageFormat.LANDSCAPE);
-  }
+		}
+	}
 
-  /**
-   * callback method called by PrinterJob
-   * in response to PrinterJob.print() call.
-   */
-  public int print(Graphics g, PageFormat format, int page_index) {
-    // must tell the PrintJob to stop printing after first page
-    if (page_index > 0) {
-      return Printable.NO_SUCH_PAGE;
-    }
+	public void print() throws PrinterException {
+		print(PageFormat.LANDSCAPE);
+	}
 
-    //int orientation = format.getOrientation();
+	/**
+	 * callback method called by PrinterJob
+	 * in response to PrinterJob.print() call.
+	 */
+	public int print(Graphics g, PageFormat format, int page_index) {
+		// must tell the PrintJob to stop printing after first page
+		if (page_index > 0) {
+			return Printable.NO_SUCH_PAGE;
+		}
 
-    // get the bounds of the component
-    Dimension dim = comp.getSize();
-    double cHeight = dim.getHeight();
-    double cWidth = dim.getWidth();
+		//int orientation = format.getOrientation();
 
-    // get the bounds of the printable area
-    double pHeight = format.getImageableHeight();
-    double pWidth = format.getImageableWidth();
+		// get the bounds of the component
+		Dimension dim = comp.getSize();
+		double cHeight = dim.getHeight();
+		double cWidth = dim.getWidth();
 
-    double pXStart = format.getImageableX();
-    double pYStart = format.getImageableY();
+		// get the bounds of the printable area
+		double pHeight = format.getImageableHeight();
+		double pWidth = format.getImageableWidth();
 
-    double xRatio = pWidth / cWidth;
-    double yRatio = pHeight / cHeight;
-    if (DEBUG) {
-      System.out.println("Page: width = " + pWidth + ", height = " + pHeight);
-    }
+		double pXStart = format.getImageableX();
+		double pYStart = format.getImageableY();
 
-    Graphics2D g2 = (Graphics2D) g;
-    g2.translate(pXStart, pYStart);
-    g2.scale(xRatio, yRatio);
+		double xRatio = pWidth / cWidth;
+		double yRatio = pHeight / cHeight;
+		if (DEBUG) {
+			System.out.println("Page: width = " + pWidth + ", height = " + pHeight);
+		}
 
-    RepaintManager currentManager = RepaintManager.currentManager(comp);
-    if (DISABLE_SWING_BUFFERING) {
-      // turning double buffering off in Swing components
-      currentManager.setDoubleBufferingEnabled(false);
-    }
+		Graphics2D g2 = (Graphics2D) g;
+		g2.translate(pXStart, pYStart);
+		g2.scale(xRatio, yRatio);
 
-    comp.paint(g2);
+		RepaintManager currentManager = RepaintManager.currentManager(comp);
+		if (DISABLE_SWING_BUFFERING) {
+			// turning double buffering off in Swing components
+			currentManager.setDoubleBufferingEnabled(false);
+		}
 
-    if (DISABLE_SWING_BUFFERING) {
-      // turning double buffering back on in Swing components
-      currentManager.setDoubleBufferingEnabled(true);
-    }
+		comp.paint(g2);
 
-    return Printable.PAGE_EXISTS;
-  }
+		if (DISABLE_SWING_BUFFERING) {
+			// turning double buffering back on in Swing components
+			currentManager.setDoubleBufferingEnabled(true);
+		}
+
+		return Printable.PAGE_EXISTS;
+	}
 }

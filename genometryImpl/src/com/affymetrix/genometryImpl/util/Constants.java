a34d0c4a5d059ea7c90dbb34603525706d3054a3
hiralv <hiralv@uncc.edu>
2010-06-21 20:47:11 +0000

    Adding new constants.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6208 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/util/Constants.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/util/Constants.java
@@ -14,4 +14,8 @@ public interface Constants {
 	public static final String annotsXml = "annots.xml";
 	public static final String liftAllLft = "liftAll.lft";
 	public static final String modChromInfoTxt = "mod_chromInfo.txt";
+
+	//Cached Server filenames
+	public static final String serverMapping = "serverMapping.txt";
+	public static final String xml_ext = ".xml";
 }

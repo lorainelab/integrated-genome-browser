66dcea83aa2e979f8c6903ef5eb494b314c4add8
hiralv <hiralv@uncc.edu>
2010-06-22 14:23:17 +0000

    Use common method to get files from quickload and das2.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6215 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/util/CacheScript.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/util/CacheScript.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -39,19 +39,39 @@ import org.w3c.dom.NodeList;
  */
 public class CacheScript {
 
+	/** Local path where data is cached. **/
 	private static final String path = ".";
-	
+
+	/** For files too be looked up on server. **/
+	private static final Set<String> quickloadFiles = new HashSet<String>();
+	private static final Set<String> das2Files = new HashSet<String>();
+
+	/** Add filed to be looked up. **/
+	static{
+		quickloadFiles.add(Constants.annotsTxt);
+		quickloadFiles.add(Constants.annotsXml);
+		quickloadFiles.add(Constants.modChromInfoTxt);
+		quickloadFiles.add(Constants.liftAllLft);
+	}
+
+	static{
+		das2Files.add(Das2VersionedSource.TYPES_CAP_QUERY);
+		das2Files.add(Das2VersionedSource.SEGMENTS_CAP_QUERY);
+		das2Files.add(Das2VersionedSource.FEATURES_CAP_QUERY);
+	}
+
+	/** Default server list. **/
 	private static final String defaultList =	" <servers> " +
 
-												" <server type='das2' name='NetAffx Das2' url='http://netaffxdas.affymetrix.com/das2/genome' />" +
-												" <server type='quickload' name='NetAffx Quickload' url='http://netaffxdas.affymetrix.com/quickload_data' />" +
+//												" <server type='das2' name='NetAffx Das2' url='http://netaffxdas.affymetrix.com/das2/genome' />" +
+//												" <server type='quickload' name='NetAffx Quickload' url='http://netaffxdas.affymetrix.com/quickload_data' />" +
 
 												" <server type='das2' name='Bioviz Das2' url='http://bioviz.org/das2/genome' />" +
 												" <server type='quickload' name='Bioviz Quickload' url='http://bioviz.org/quickload/' />" +
 
-												" <server type='das' name='UCSC Das' url='http://genome.cse.ucsc.edu/cgi-bin/das/dsn' />" +
+//												" <server type='das' name='UCSC Das' url='http://genome.cse.ucsc.edu/cgi-bin/das/dsn' />" +
 
-												" <server type='quickload' name='HughesLab' url='http://hugheslab.ccbr.utoronto.ca/igb/' />" +
+//												" <server type='quickload' name='HughesLab' url='http://hugheslab.ccbr.utoronto.ca/igb/' />" +
 
 
 //												" <server type='das' name='Ensembl' url='http://www.ensembl.org/das/dsn' enabled='false' />" +
@@ -102,35 +122,14 @@ public class CacheScript {
 			return false;
 
 		for(String genome_name : genome_names){
-			String server_path = gServer.URL + "/" + genome_name;
+			String server_path = gServer.URL + genome_name;
 			String local_path = serverCachePath+ "/" + genome_name;
 			makeDir(local_path);
 
-			getAllQuickLoadFiles(server_path,local_path);
-
+			getAllFiles(gServer.serverType,server_path,local_path);
 		}
 
 		return true;
-	}
-
-	/**
-	 * Gets files for a genome and copies it to it's directory.
-	 * @param server_path	Server path from where mapping is to be copied.
-	 * @param local_path	Local path from where mapping is to saved.
-	 */
-	private static void getAllQuickLoadFiles(String server_path,String local_path){
-		File file;
-		final Set<String> quickloadFile = new HashSet<String>();
-
-		quickloadFile.add(Constants.annotsTxt);
-		quickloadFile.add(Constants.annotsXml);
-		quickloadFile.add(Constants.modChromInfoTxt);
-		quickloadFile.add(Constants.liftAllLft);
-
-		for(String fileName : quickloadFile){
-			file = getFile(server_path+"/"+fileName);
-			moveFileTo(file,fileName,local_path);
-		}
 	}
 
 	/**
@@ -152,12 +151,11 @@ public class CacheScript {
 		for (Das2Source source : sources.values()) {
 			// Das/2 has versioned sources.  Get each version.
 			for (Das2VersionedSource versionSource : source.getVersions().values()) {
-				String serverPath = gServer.URL + "/" + versionSource.getName();
-				String localPath = serverCachePath + "/" + versionSource.getName();
-				makeDir(localPath);
+				String server_path = gServer.URL + "/" + versionSource.getName();
+				String local_path = serverCachePath + "/" + versionSource.getName();
+				makeDir(local_path);
 
-				getAllDas2Files(serverPath,localPath);
-
+				getAllFiles(gServer.serverType,server_path,local_path);
 			}
 		}
 
@@ -169,20 +167,34 @@ public class CacheScript {
 	 * @param server_path	Server path from where mapping is to be copied.
 	 * @param local_path	Local path from where mapping is to saved.
 	 */
-	private static void getAllDas2Files(String server_path,String local_path){
+	private static boolean getAllFiles(ServerType servertype, String server_path, String local_path){
 		File file;
-		final Set<String> Das2Files = new HashSet<String>();
+		Set<String> files = null;
 
-		Das2Files.add(Das2VersionedSource.TYPES_CAP_QUERY);
-		Das2Files.add(Das2VersionedSource.SEGMENTS_CAP_QUERY);
-		Das2Files.add(Das2VersionedSource.FEATURES_CAP_QUERY);
-	
-		for(String fileName : Das2Files){
-			file = getFile(server_path+"/"+fileName);
-			moveFileTo(file,fileName+ Constants.xml_ext,local_path);
+		switch(servertype){
+			case QuickLoad:
+				files = quickloadFiles;
+				break;
+
+			case DAS2:
+				files = das2Files;
+				break;
+
+			default:
+				return false;
 		}
-	}
+		
+		for(String fileName : files){
+			file = getFile(server_path+"/"+fileName);
 
+			if(servertype.equals(ServerType.DAS2))
+				fileName += Constants.xml_ext;
+			
+			moveFileTo(file,fileName,local_path);
+		}
+
+		return true;
+	}
 	/**
 	 * Gets files for all genomes from Das server and copies it to appropriate directory.
 	 * @param gServer	GenericServer from where mapping are fetched.
@@ -259,8 +271,7 @@ public class CacheScript {
 			return false;
 
 		File newLocation = new File(path+ "/" +fileName);
-		file.renameTo(newLocation);
-		return true;
+		return file.renameTo(newLocation);
 	}
 
 	/**
@@ -359,20 +370,25 @@ public class CacheScript {
 			Set<GenericServer> server_list = parseServerList(istr);
 			for (final GenericServer gServer : server_list) {
 
-				Executor vexec = Executors.newSingleThreadExecutor();
+				final Timer ser_tim = new Timer();
+				ExecutorService vexec = Executors.newSingleThreadExecutor();
 				SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
 
-					protected Void doInBackground() throws Exception {
-						Timer ser_tim = new Timer();
+					protected Void doInBackground(){				
 						ser_tim.start();
 						processServer(gServer);
+						return null;
+					}
+
+					@Override
+					public void done() {
 						out.println(gServer.friendlyURL.toExternalForm() + "\t" + gServer.serverName);
 						Logger.getLogger(CacheScript.class.getName()).log(Level.INFO, "Time required to cache " + gServer.serverName + " :" + (ser_tim.read()/ 1000f), ser_tim);
-						return null;
 					}
 
 				};
 				vexec.execute(worker);
+				vexec.shutdown();
 			}
 		} catch (Exception ex) {
 			Logger.getLogger(CacheScript.class.getName()).log(Level.SEVERE, null, ex);

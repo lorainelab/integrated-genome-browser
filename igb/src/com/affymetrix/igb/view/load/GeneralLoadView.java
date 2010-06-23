d3579af8db556693297db4afca0082999b42d2cf
hiralv <hiralv@uncc.edu>
2010-06-23 15:20:21 +0000

    If server is primary server then do not process it.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6243 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/load/GeneralLoadView.java
+++ igb/src/com/affymetrix/igb/view/load/GeneralLoadView.java
@@ -182,7 +182,7 @@ public final class GeneralLoadView extends JComponent
 
 		ServerList.addServerInitListener(this);
 
-		//GeneralLoadUtils.loadServerMapping();
+		GeneralLoadUtils.loadServerMapping();
 		populateSpeciesData();
 		addListeners();
 	}

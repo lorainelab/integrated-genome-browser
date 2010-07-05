8b6752690394d32bc7a3d7a90aef370d039603d0
loraine <aloraine@uncc.edu>
2010-07-05 13:57:41 +0000

    Making window smaller on startup
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6355 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- protannot/src/org/bioviz/protannot/ProtAnnotMain.java
+++ protannot/src/org/bioviz/protannot/ProtAnnotMain.java
@@ -235,8 +235,8 @@ final public class ProtAnnotMain implements WindowListener {
 		frm.setTransferHandler(fdh);
 		frm.setIconImage(imageIcon);
         screen = frm.getToolkit().getScreenSize();
-        int frm_width = (int) (screen.width * .8f);
-        int frm_height = (int) (screen.height * .8f);
+        int frm_width = (int) (screen.width * .5f);
+        int frm_height = (int) (screen.height * .5f);
         frm.setSize(frm_width, frm_height);
         frm.setLocation((int) (screen.width * .1f), (int) (screen.height * .05f));
         setUpPanels();
@@ -247,6 +247,7 @@ final public class ProtAnnotMain implements WindowListener {
             setupSamplesFromServer();
 
         frm.addWindowListener(this);
+		//frm.pack();
         frm.setVisible(true);
 		String file = getArgumentValue(Arguments.FILENAME);
         if(file != null)

df4fec1026038467ecbb08a42acbe0ee30b97c35
jnicol <jnicol@uncc.edu>
2010-07-15 18:29:15 +0000

    Remove the methods which call unit tests (simply run the unit tests). Comment out printlns, which repeat what's done in the asserts.  Ideally, we want as little output from the unit tests as possible... we just want to know if they succeeded or not.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6434 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/test/com/affymetrix/genometryImpl/parsers/graph/UseqParserTest.java
+++ genometryImpl/test/com/affymetrix/genometryImpl/parsers/graph/UseqParserTest.java
@@ -1,16 +1,12 @@
 package com.affymetrix.genometryImpl.parsers.graph;
 
-import com.affymetrix.genometryImpl.parsers.useq.*;
-import com.affymetrix.genometryImpl.parsers.useq.data.RegionScoreTextData;
+import com.affymetrix.genometryImpl.parsers.useq.USeqRegionParser;
+import com.affymetrix.genometryImpl.parsers.useq.USeqGraphParser;
+import com.affymetrix.genometryImpl.parsers.useq.USeqArchive;
 import com.affymetrix.genometryImpl.SeqSymmetry;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
-import com.affymetrix.genometryImpl.span.SimpleSeqSpan;
-import com.affymetrix.genometryImpl.symmetry.SimpleMutableSeqSymmetry;
-import com.affymetrix.genometryImpl.*;
-import com.affymetrix.genometryImpl.symloader.BED;
 import com.affymetrix.genometryImpl.GraphSym;
 import com.affymetrix.genometryImpl.GenometryModel;
-import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.SeqSpan;
 import com.affymetrix.genometryImpl.UcscBedSym;
 
@@ -26,18 +22,7 @@ import static org.junit.Assert.*;
  */
 public class UseqParserTest {
 
-
 	@Test
-	public void TestParseFromFile() throws IOException {
-		
-		System.out.println("Graph file parsing tests.............");
-		testGraphFileParsing();
-		
-		System.out.println("\nRegion file parsing tests.............");
-		testRegionFileParsing();
-		
-	}
-	
 	public void testRegionFileParsing() throws IOException{
 		//test region parsing
 		String stream_name = "chr17";
@@ -51,33 +36,34 @@ public class UseqParserTest {
 		List<SeqSymmetry> results = up.parse(istr, group, stream_name, false, null);
 		
 		//size of results
-		System.out.println("NumberRegions "+results.size());
+		//System.out.println("NumberRegions "+results.size());
 		assertEquals(677, results.size());
 		
 		UcscBedSym sym = (UcscBedSym) results.get(5);
 		SeqSpan span = sym.getSpan(0);
 		
 		//span count
-		System.out.println("SpanCount "+sym.getSpanCount());
+		//System.out.println("SpanCount "+sym.getSpanCount());
 		assertEquals(1, sym.getSpanCount());
 		
 		//start
-		System.out.println("SpanStart "+span.getMin());
+		//System.out.println("SpanStart "+span.getMin());
 		assertEquals(860852, span.getMin());
 		
 		//stop
-		System.out.println("SpanStop "+span.getMax());
+		//System.out.println("SpanStop "+span.getMax());
 		assertEquals(861836, span.getMax());
 		
 		//strand
-		System.out.println("SpanStrand "+span.isForward());
+		//System.out.println("SpanStrand "+span.isForward());
 		assertEquals(true, span.isForward());
 		
 		//score
-		System.out.println("SpanScore "+sym.getScore());
+		//System.out.println("SpanScore "+sym.getScore());
 		assertEquals(new Float(401.0), new Float(sym.getScore()));
 	}
-	
+
+	@Test
 	public void testGraphFileParsing() throws IOException{
 		//test graph parsing
 		String filename = "test/data/useq/chr17_H_sapiens_Mar_2006_Graph.useq";
@@ -95,39 +81,30 @@ public class UseqParserTest {
 		List<GraphSym> results = up.parseGraphSyms(istr, gmodel, "test", null);
 
 		//size
-		System.out.println("ResultsSize "+results.size());
+		//System.out.println("ResultsSize "+results.size());
 		assertEquals(2, results.size());
 		
 		//fetch first graph
 		GraphSym gr0 = results.get(0);
 		
 		//check stream name
-		System.out.println("StreamName "+gr0.getGraphSeq().getID());
+		//System.out.println("StreamName "+gr0.getGraphSeq().getID());
 		assertEquals(stream_name, gr0.getGraphSeq().getID());
 		
 		//check point count
-		System.out.println("PointCount "+gr0.getPointCount());
+		//System.out.println("PointCount "+gr0.getPointCount());
 		assertEquals(20000, gr0.getPointCount());
 		
 		//check X coor
-		System.out.println("XCoor "+gr0.getGraphXCoord(3));
+		//System.out.println("XCoor "+gr0.getGraphXCoord(3));
 		assertEquals(12168484, gr0.getGraphXCoord(3));
 		
 		//check Y coor
-		System.out.println("YCoor "+gr0.getGraphYCoord(1));
+		//System.out.println("YCoor "+gr0.getGraphYCoord(1));
 		assertEquals(new Float(1), new Float(gr0.getGraphYCoord(1)));
 	}
-	
+
 	@Test
-	public void TestWriteAnnotations() throws Exception {
-		System.out.println("\nTesting writing Graph data to file..........");
-		testWritingGraphToFile();
-		
-		System.out.println("\nTesting writing Regions to file..........");
-		testWritingRegionsToFile();
-		
-	}
-	
 	public void testWritingRegionsToFile() throws Exception {
 		//read in data
 		String filename = "test/data/useq/chr17_H_sapiens_Mar_2006_Region.useq";
@@ -146,7 +123,8 @@ public class UseqParserTest {
 		assertTrue(wrote);
 		
 	}
-	
+
+	@Test
 	public void testWritingGraphToFile() throws Exception {
 		//read in data
 		String filename = "test/data/useq/chr17_H_sapiens_Mar_2006_Graph.useq";

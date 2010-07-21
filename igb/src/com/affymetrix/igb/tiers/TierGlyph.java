4c2fad63685f1f2f07a07f44d4efff27cdf24270
jnicol <jnicol@uncc.edu>
2010-07-21 18:40:21 +0000

    Fix a nasty overflow condition in the Genoviz View code.
    When converting from coord space to pixel space, it could happen
    that x,y,width, or height would overflow integer values.  
    
    Fix by normalizing x to width, and y to height.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6471 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/tiers/TierGlyph.java
+++ igb/src/com/affymetrix/igb/tiers/TierGlyph.java
@@ -289,10 +289,7 @@ public class TierGlyph extends SolidGlyph {
 	@Override
 	public void draw(ViewI view) {
 		view.transformToPixels(coordbox, pixelbox);
-		
-		pixelbox.x = Math.max(pixelbox.x, 0); //TODO:Fix this.
-											  //Temporary solution to avoid width becoming negative.
-
+	
 		pixelbox.width = Math.max(pixelbox.width, min_pixels_width);
 		pixelbox.height = Math.max(pixelbox.height, min_pixels_height);
 
@@ -333,6 +330,7 @@ public class TierGlyph extends SolidGlyph {
 				drawHandle(view);
 			}
 		}
+
 
 		super.draw(view);
 	}

e72f8d684bc5af6f4897d8c8fcd93c3c7bf0fbab
hiralv <hiralv@uncc.edu>
2010-08-03 21:06:40 +0000

    Adding more test cases.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6587 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/test/com/affymetrix/genometryImpl/parsers/PSLParserTest.java
+++ genometryImpl/test/com/affymetrix/genometryImpl/parsers/PSLParserTest.java
@@ -1,21 +1,26 @@
 package com.affymetrix.genometryImpl.parsers;
 
+import java.io.FileNotFoundException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import com.affymetrix.genometryImpl.SeqSymmetry;
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.SeqSpan;
 import com.affymetrix.genometryImpl.UcscPslSym;
+import com.affymetrix.genometryImpl.general.SymLoader;
 import com.affymetrix.genometryImpl.symloader.PSL;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.List;
 
 
 public class PSLParserTest {
@@ -66,6 +71,50 @@ public class PSLParserTest {
 			assertEquals(string, outstream.toString());
 
 		}
+
+	//@Test
+	public void testFiles() throws FileNotFoundException, IOException{
+		File file = new File("test/data/psl/test1.psl");
+		InputStream istr = new FileInputStream(file);
+		AnnotatedSeqGroup group = new AnnotatedSeqGroup("Test Group");
+		BioSeq seq = group.addSeq("chr1", 0);
+
+
+		PSLParser instance = new PSLParser();
+		List<UcscPslSym> syms = instance.parse(istr, file.getName(), null, group, true, true);
+		List<SeqSymmetry> syms1 = SymLoader.filterResultsByChromosome(syms, seq);
+		
+
+		PSL psl = new PSL(file.toURI(), file.getName(), group, null, null,
+			true, true, false);
+		List<UcscPslSym> syms2 = psl.getChromosome(seq);
+
+		testSeqSymmetry(syms1, syms2);
+		
+		testSeqSymmetry(syms, psl.getGenome());
+
+	}
+
+	private void testSeqSymmetry(List<? extends SeqSymmetry> syms1, List<? extends SeqSymmetry> syms2){
+		assertEquals(syms1.size(), syms2.size());
+		SeqSymmetry sym1, sym2;
+		for(int i=0; i<syms1.size(); i++){
+			sym1 = syms1.get(i);
+			sym2 = syms2.get(i);
+
+			assertEquals(sym1.getID(), sym2.getID());
+			assertEquals(sym1.getChildCount(), sym2.getChildCount());
+			assertEquals(sym1.getSpanCount(), sym2.getSpanCount());
+
+			for(int j=0; j<sym1.getSpanCount(); j++){
+				SeqSpan span1 = sym1.getSpan(j);
+				SeqSpan span2 = sym2.getSpan(j);
+				assertEquals(span1.getBioSeq().getID(), span1.getBioSeq().getID());
+				assertEquals(span1.getMinDouble(), span2.getMinDouble(), 0);
+				assertEquals(span1.getMaxDouble(), span2.getMaxDouble(), 0);
+			}
+		}
+	}
 
 	/**
 	 * Test of getMimeType method.

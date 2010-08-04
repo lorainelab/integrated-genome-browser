0f638e6ccf0bec7d1936981175f6187fe176a38d
hiralv <hiralv@uncc.edu>
2010-08-04 14:04:52 +0000

    Refactoring.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6591 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/test/com/affymetrix/genometryImpl/parsers/LinkPSLParserTest.java
+++ genometryImpl/test/com/affymetrix/genometryImpl/parsers/LinkPSLParserTest.java
@@ -1,6 +1,7 @@
 package com.affymetrix.genometryImpl.parsers;
 
 import com.affymetrix.genometryImpl.SeqSpan;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
@@ -52,24 +53,8 @@ public class LinkPSLParserTest {
 			parser.setCreateContainerAnnot(true);
 
 			List result = parser.parse(gzstr, type, null, group, null, false, true, false);
-			assertNotNull(result);
-			assertEquals(2103,result.size());	// all types of symmetries
 
-			List<SeqSymmetry> consensusSyms = group.findSyms(Pattern.compile(".*"));
-			assertEquals(1131, consensusSyms.size());	// only the consensus symmetries
-
-			ByteArrayOutputStream outstream = null;
-			ProbeSetDisplayPlugin parser2 = null;
-
-			outstream = new ByteArrayOutputStream();
-			parser2 = new ProbeSetDisplayPlugin();
-			parser2.writeAnnotations(consensusSyms, seq, consensusType, outstream);
-
-			// 1131 consensus syms.
-			// 972 probeset symmetries, of which 879 are actually used by the consensus symmetries.
-			// 2 "track" lines.
-			// 1131 + 879 + 2
-			assertEquals(1131 + 879 + 2, outstream.toString().split("\n").length);
+			writeAnnotation(result, group, seq, consensusType);
 
 		} catch (Exception ex) {
 			Logger.getLogger(LinkPSLParserTest.class.getName()).log(Level.SEVERE, null, ex);
@@ -77,6 +62,23 @@ public class LinkPSLParserTest {
 		} finally {
 			GeneralUtils.safeClose(istr);
 		}
+	}
+
+	private void writeAnnotation(List result, AnnotatedSeqGroup group, BioSeq seq, String consensusType) throws IOException {
+		assertNotNull(result);
+		assertEquals(2103, result.size()); // all types of symmetries
+		List<SeqSymmetry> consensusSyms = group.findSyms(Pattern.compile(".*"));
+		assertEquals(1131, consensusSyms.size()); // only the consensus symmetries
+		ByteArrayOutputStream outstream = null;
+		ProbeSetDisplayPlugin parser2 = null;
+		outstream = new ByteArrayOutputStream();
+		parser2 = new ProbeSetDisplayPlugin();
+		parser2.writeAnnotations(consensusSyms, seq, consensusType, outstream);
+		// 1131 consensus syms.
+		// 972 probeset symmetries, of which 879 are actually used by the consensus symmetries.
+		// 2 "track" lines.
+		// 1131 + 879 + 2
+		assertEquals(1131 + 879 + 2, outstream.toString().split("\n").length);
 	}
 
 	@Test
@@ -101,35 +103,35 @@ public class LinkPSLParserTest {
 			parser.setCreateContainerAnnot(true);
 
 			List result = parser.parse(gzstr, type, null, group, null, false, true, false);
-			assertNotNull(result);
-			assertEquals(2103, result.size());	// all types of symmetries
-			@SuppressWarnings("unchecked")
-			Comparator<UcscPslSym> USCCCompare = parser.getComparator(seq);
-			List<SeqSymmetry> sortedSyms = IndexingUtils.getSortedAnnotationsForChrom(
-					result, seq, USCCCompare);	// the syms on chr1, sorted.
-			assertNotNull(sortedSyms);
-			assertEquals(168, sortedSyms.size());
-			assertEquals(12722644, ((UcscPslSym) sortedSyms.get(0)).getTargetMin());
-			assertEquals(267887419, ((UcscPslSym) sortedSyms.get(sortedSyms.size() - 1)).getTargetMin());
-
-			// Now that we have the sorted consensus for chr1, verify its probesets.
-			ByteArrayOutputStream outstream = null;
-			ProbeSetDisplayPlugin parser2 = null;
-
-			outstream = new ByteArrayOutputStream();
-			parser2 = new ProbeSetDisplayPlugin();
-			parser2.writeAnnotations(sortedSyms, seq, consensusType, outstream);
-
-			// 168 consensus syms on chr1.
-			// 143 probesets correspond to those consensus syms.
-			// 2 "track" lines.
-			// 168 + 143 + 2
-			assertEquals(168 + 143 + 2, outstream.toString().split("\n").length);
+			
+			splittingByChromosome(result, parser.getComparator(seq), seq, consensusType);
 
 		} catch (Exception ex) {
 			Logger.getLogger(LinkPSLParserTest.class.getName()).log(Level.SEVERE, null, ex);
 			fail();
 		}
+	}
+
+	private void splittingByChromosome(List result, Comparator<UcscPslSym> USCCCompare, BioSeq seq, String consensusType) throws IOException {
+		assertNotNull(result);
+		assertEquals(2103, result.size()); // all types of symmetries
+
+		List<SeqSymmetry> sortedSyms = IndexingUtils.getSortedAnnotationsForChrom(result, seq, USCCCompare); // the syms on chr1, sorted.
+		assertNotNull(sortedSyms);
+		assertEquals(168, sortedSyms.size());
+		assertEquals(12722644, ((UcscPslSym) sortedSyms.get(0)).getTargetMin());
+		assertEquals(267887419, ((UcscPslSym) sortedSyms.get(sortedSyms.size() - 1)).getTargetMin());
+		// Now that we have the sorted consensus for chr1, verify its probesets.
+		ByteArrayOutputStream outstream = null;
+		ProbeSetDisplayPlugin parser2 = null;
+		outstream = new ByteArrayOutputStream();
+		parser2 = new ProbeSetDisplayPlugin();
+		parser2.writeAnnotations(sortedSyms, seq, consensusType, outstream);
+		// 168 consensus syms on chr1.
+		// 143 probesets correspond to those consensus syms.
+		// 2 "track" lines.
+		// 168 + 143 + 2
+		assertEquals(168 + 143 + 2, outstream.toString().split("\n").length);
 	}
 
 
@@ -161,76 +163,67 @@ public class LinkPSLParserTest {
 			// optimize genome by replacing second-level syms with IntervalSearchSyms
 			Optimize.genome(group);
 
-			assertNotNull(result);
-			assertEquals(2103, result.size());	// all types of symmetries
-			@SuppressWarnings("unchecked")
-
-			Comparator<UcscPslSym> USCCCompare = parser.getComparator(seq);
-
-			List<SeqSymmetry> sortedSyms = IndexingUtils.getSortedAnnotationsForChrom(
-					result, seq, USCCCompare);	// the syms on chr1, sorted.
-			assertNotNull(sortedSyms);
-			assertEquals(168, sortedSyms.size());
-			assertEquals(12722644, ((UcscPslSym) sortedSyms.get(0)).getTargetMin());
-			assertEquals(267887419, ((UcscPslSym) sortedSyms.get(sortedSyms.size() - 1)).getTargetMin());
-
-			String testFileName = "test/data/psl/RT_U34TEST.link.psl";
-			File testFile = new File(testFileName);
-			IndexedSyms iSyms = new IndexedSyms(sortedSyms.size(), testFile, "RT_U34", (IndexWriter) parser);
-			IndexingUtils.writeIndexedAnnotations(sortedSyms, seq, group, iSyms, testFileName);
-
-			String overlap = "3000000:160000000";
-			SeqSpan overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
-			List<SeqSymmetry>newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span,iSyms,"testOUT",group);
-			assertEquals(96, newResults.size());	// first 96 lines of file are < 160000000
-
-			overlap = "160000000:160254000";
-			overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
-			newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span,iSyms,"testOUT",group);
-			assertEquals(0, newResults.size()); // no spans here
-
-			overlap = "160254000:160254500";
-			overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
-			newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span,iSyms,"testOUT",group);
-			assertEquals(1, newResults.size());
-			assertEquals("RT-U34:S78284_S_AT", newResults.get(0).getID());	// only feature in this span.
-
-			overlap = "160254500:160254600";
-			overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
-			newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span,iSyms,"testOUT",group);
-			assertEquals(1, newResults.size());
-			assertEquals("RT-U34:S78284_S_AT", newResults.get(0).getID());	// only feature in this span.
-
-			overlap = "160254600:160254700";
-			overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
-			newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span,iSyms,"testOUT",group);
-			assertEquals(0, newResults.size()); // no spans here
-
-			overlap = "122000000:123000000";
-			overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
-			newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span,iSyms,"testOUT",group);
-			assertEquals(1, newResults.size());
-			assertEquals("RT-U34:L29232_AT", newResults.get(0).getID()); // only feature in this span
-			
-			overlap = "12722772:12723220";
-			overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
-			newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span,iSyms,"testOUT",group);
-			assertEquals(3, newResults.size());	// first three in the file
-
-			if (testFile.exists()) {
-				testFile.delete();
-			}
-			String testFileName2 = "test/data/psl/RT_U34TEST.link2.psl";
-			File testFile2 = new File(testFileName2);
-			if (testFile2.exists()) {
-				testFile2.delete();
-			}
+			indexing(result, parser.getComparator(seq), parser, seq, group, seqid);
 
 		} catch (Exception ex) {
 			Logger.getLogger(BgnParserTest.class.getName()).log(Level.SEVERE, null, ex);
 			fail();
 		}
 
+	}
+
+	private void indexing(List result, Comparator<UcscPslSym> USCCCompare, IndexWriter writer, BioSeq seq, AnnotatedSeqGroup group, String seqid) throws IOException {
+		assertNotNull(result);
+		assertEquals(2103, result.size()); // all types of symmetries
+
+		List<SeqSymmetry> sortedSyms = IndexingUtils.getSortedAnnotationsForChrom(result, seq, USCCCompare); // the syms on chr1, sorted.
+		assertNotNull(sortedSyms);
+		assertEquals(168, sortedSyms.size());
+		assertEquals(12722644, ((UcscPslSym) sortedSyms.get(0)).getTargetMin());
+		assertEquals(267887419, ((UcscPslSym) sortedSyms.get(sortedSyms.size() - 1)).getTargetMin());
+		String testFileName = "test/data/psl/RT_U34TEST.link.psl";
+		File testFile = new File(testFileName);
+		IndexedSyms iSyms = new IndexedSyms(sortedSyms.size(), testFile, "RT_U34", writer);
+		IndexingUtils.writeIndexedAnnotations(sortedSyms, seq, group, iSyms, testFileName);
+		String overlap = "3000000:160000000";
+		SeqSpan overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
+		List<SeqSymmetry> newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span, iSyms, "testOUT", group);
+		assertEquals(96, newResults.size()); // first 96 lines of file are < 160000000
+		overlap = "160000000:160254000";
+		overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
+		newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span, iSyms, "testOUT", group);
+		assertEquals(0, newResults.size()); // no spans here
+		overlap = "160254000:160254500";
+		overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
+		newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span, iSyms, "testOUT", group);
+		assertEquals(1, newResults.size());
+		assertEquals("RT-U34:S78284_S_AT", newResults.get(0).getID()); // only feature in this span.
+		overlap = "160254500:160254600";
+		overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
+		newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span, iSyms, "testOUT", group);
+		assertEquals(1, newResults.size());
+		assertEquals("RT-U34:S78284_S_AT", newResults.get(0).getID()); // only feature in this span.
+		overlap = "160254600:160254700";
+		overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
+		newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span, iSyms, "testOUT", group);
+		assertEquals(0, newResults.size()); // no spans here
+		overlap = "122000000:123000000";
+		overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
+		newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span, iSyms, "testOUT", group);
+		assertEquals(1, newResults.size());
+		assertEquals("RT-U34:L29232_AT", newResults.get(0).getID()); // only feature in this span
+		overlap = "12722772:12723220";
+		overlap_span = ServerUtils.getLocationSpan(seqid, overlap, group);
+		newResults = ServerUtils.getIndexedOverlappedSymmetries(overlap_span, iSyms, "testOUT", group);
+		assertEquals(3, newResults.size()); // first three in the file
+		if (testFile.exists()) {
+			testFile.delete();
+		}
+		String testFileName2 = "test/data/psl/RT_U34TEST.link2.psl";
+		File testFile2 = new File(testFileName2);
+		if (testFile2.exists()) {
+			testFile2.delete();
+		}
 	}
 }
 

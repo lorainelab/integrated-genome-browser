0389d8c122b264757ebd32c1ed9001d90acc24af
hiralv <hiralv@uncc.edu>
2010-08-05 19:51:36 +0000

    Adding method to write file.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6610 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/util/SortTabFile.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/util/SortTabFile.java
@@ -1,11 +1,14 @@
 package com.affymetrix.genometryImpl.util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -23,12 +26,13 @@ public class SortTabFile {
 
 	static private final Pattern tab_regex = Pattern.compile("\t");
 
-	public static void sort(File file, int column){
+	public static boolean sort(File file, int column){
 		
 		BufferedReader br = null;
 		String line = null;
+		List<String> list = new ArrayList<String>();
 		try {
-			List<String> list = new ArrayList<String>();
+			
 			List<String> templist = new ArrayList<String>();
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 
@@ -46,18 +50,47 @@ public class SortTabFile {
 			}
 			Collections.sort(templist, new LineComparator(column));
 			list.addAll(templist);
-			
-			for(String ln : list){
-				System.out.println(ln);
-			}
-			
+						
 		} catch (FileNotFoundException ex) {
-			Logger.getLogger(SortTabFile.class.getName()).log(Level.SEVERE, null, ex);
+			Logger.getLogger(SortTabFile.class.getName()).log(Level.SEVERE, "Could not find file " + file, ex);
+			return false;
 		} catch (IOException ex){
 			Logger.getLogger(SortTabFile.class.getName()).log(Level.SEVERE, null, ex);
+			return false;
 		} finally {
 			GeneralUtils.safeClose(br);
 		}
+
+		return writeFile(file, list);
+	}
+
+	private static boolean writeFile(File file, List<String> lines){
+		BufferedWriter bw = null;
+		try {
+			
+			if(!file.canWrite()){
+				Logger.getLogger(SortTabFile.class.getName()).log(Level.SEVERE, "Cannot write to file {0}", file);
+				return false;
+			}
+
+			bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+
+			for(String line : lines){
+				bw.write(line);
+			}
+
+			bw.flush();
+			
+		} catch (FileNotFoundException ex) {
+			Logger.getLogger(SortTabFile.class.getName()).log(Level.SEVERE, "Could not find file " + file, ex);
+			return false;
+		} catch (IOException ex){
+			Logger.getLogger(SortTabFile.class.getName()).log(Level.SEVERE, null, ex);
+			return false;
+		} finally {
+			GeneralUtils.safeClose(bw);
+		}
+		return true;
 	}
 
 	static class LineComparator implements Comparator<String>{

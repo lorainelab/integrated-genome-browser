593fd293ad04dedfd7856380539d62b240d7a75b
hiralv <hiralv@uncc.edu>
2010-08-10 13:40:46 +0000

    Override methods in symloaderinst and extend symloaderinstnc from it.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@6634 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/symloader/SymLoaderInst.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/symloader/SymLoaderInst.java
@@ -4,6 +4,8 @@ import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.comparator.BioSeqComparator;
 import com.affymetrix.genometryImpl.general.SymLoader;
+import com.affymetrix.genometryImpl.SeqSymmetry;
+import com.affymetrix.genometryImpl.SeqSpan;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -22,20 +24,46 @@ public class SymLoaderInst extends SymLoader{
 	}
 
 	@Override
-	public void init() {
-		if (this.isInitialized) {
+	public void init(){
+		if(this.isInitialized){
 			return;
 		}
 		super.init();
-		getGenome();
+		addChromosomeToList();
+	}
+
+	private void addChromosomeToList(){
 		chromosomeList.addAll(group.getSeqList());
 		Collections.sort(chromosomeList,new BioSeqComparator());
 	}
 
 	@Override
 	public List<BioSeq> getChromosomeList(){
+		if(!this.isInitialized)
+			getGenome();
+		
 		init();
 		return chromosomeList;
 	}
 
+	@Override
+	 public List<? extends SeqSymmetry> getGenome() {
+		List<? extends SeqSymmetry> syms = super.getGenome();
+		init();
+		return syms;
+	 }
+
+	@Override
+	public List<? extends SeqSymmetry> getChromosome(BioSeq seq) {
+		List<? extends SeqSymmetry> syms = super.getChromosome(seq);
+		init();
+		return syms;
+	}
+
+	@Override
+	public List<? extends SeqSymmetry> getRegion(SeqSpan overlapSpan) {
+		List<? extends SeqSymmetry> syms = super.getRegion(overlapSpan);
+		init();
+		return syms;
+    }
 }
--- genometryImpl/src/com/affymetrix/genometryImpl/symloader/SymLoaderInstNC.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/symloader/SymLoaderInstNC.java
@@ -1,22 +1,16 @@
 package com.affymetrix.genometryImpl.symloader;
 
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
-import com.affymetrix.genometryImpl.BioSeq;
-import com.affymetrix.genometryImpl.comparator.BioSeqComparator;
-import com.affymetrix.genometryImpl.general.SymLoader;
 import com.affymetrix.genometryImpl.util.LoadUtils.LoadStrategy;
 import java.util.List;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  *
  * @author hiralv
  */
-public class SymLoaderInstNC extends SymLoader{
-
-	private final List<BioSeq> chromosomeList = new ArrayList<BioSeq>();
+public class SymLoaderInstNC extends SymLoaderInst{
 	
 	private static List<LoadStrategy> strategyList = new ArrayList<LoadStrategy>();
 	static {
@@ -33,21 +27,4 @@ public class SymLoaderInstNC extends SymLoader{
 		return strategyList;
 	}
 
-	@Override
-	public void init() {
-		if (this.isInitialized) {
-			return;
-		}
-		super.init();
-		getGenome();
-		chromosomeList.addAll(group.getSeqList());
-		Collections.sort(chromosomeList,new BioSeqComparator());
-	}
-
-	@Override
-	public List<BioSeq> getChromosomeList(){
-		init();
-		return chromosomeList;
-	}
-	
 }

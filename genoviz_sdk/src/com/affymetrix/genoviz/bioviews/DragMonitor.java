d2722abe4f09628e0e6021c6cd7d1b550de2f11b
jnicol <jnicol@uncc.edu>
2010-10-15 19:07:55 +0000

    Replacing NeoTimer with javax.swing.timer.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@7037 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genoviz_sdk/src/com/affymetrix/genoviz/bioviews/DragMonitor.java
+++ genoviz_sdk/src/com/affymetrix/genoviz/bioviews/DragMonitor.java
@@ -14,29 +14,26 @@
 package com.affymetrix.genoviz.bioviews;
 
 import java.awt.event.*;
-import java.util.*;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import com.affymetrix.genoviz.awt.NeoCanvas;
 import com.affymetrix.genoviz.event.NeoDragEvent;
 import com.affymetrix.genoviz.event.NeoDragListener;
-import com.affymetrix.genoviz.event.NeoTimerEvent;
-import com.affymetrix.genoviz.event.NeoTimerListener;
 import com.affymetrix.genoviz.util.NeoConstants;
 import java.awt.Dimension;
+import javax.swing.Timer;
 
 @SuppressWarnings(value="deprecation")
 public class DragMonitor
-	implements NeoConstants, MouseListener, MouseMotionListener, NeoTimerListener {
+	implements NeoConstants, MouseListener, MouseMotionListener, ActionListener {
 
-	NeoCanvas can;
-	Set<NeoDragListener> listeners = new CopyOnWriteArraySet<NeoDragListener>();
+	private final NeoCanvas can;
+	final Set<NeoDragListener> listeners = new CopyOnWriteArraySet<NeoDragListener>();
 	boolean already_dragging_outside = false;
 
-	protected NeoTimerEventClock timer = null;
+	private Timer timer = null;
 	protected int initial_delay = 250;
 	protected int timer_interval = 100;
-
-	protected int time_count = 0;
 
   public DragMonitor(NeoCanvas can) {
 		this.can = can;
@@ -63,50 +60,41 @@ public class DragMonitor
 		Dimension dim = can.getSize();
 		int x = evt.getX();
 		int y = evt.getY();
-		if ((!already_dragging_outside) &&
-				(x < 0 || x > dim.width ||
-				 y < 0 || y > dim.height)) {
-			if (timer != null) { timer.stop(); }
+		if ((!already_dragging_outside)
+				&& (x < 0 || x > dim.width
+				|| y < 0 || y > dim.height)) {
+			if (timer != null) {
+				timer.stop();
+			}
 			already_dragging_outside = true;
 
 			int direction;
 			// direction constants are from com.affymetrix.genoviz.util.NeoConstants
-			if (x < 0) { direction = WEST; }
-			else if (y < 0) { direction = NORTH; }
-			else if (x > dim.width) { direction = EAST; }
-			else if (y > dim.height) { direction = SOUTH; }
-			else { direction = NONE; }
+			if (x < 0) {
+				direction = WEST;
+			} else if (y < 0) {
+				direction = NORTH;
+			} else if (x > dim.width) {
+				direction = EAST;
+			} else if (y > dim.height) {
+				direction = SOUTH;
+			} else {
+				direction = NONE;
+			}
 			Integer dirobj = new Integer(direction);
-
-			timer = new NeoTimerEventClock(initial_delay, timer_interval, dirobj);
-			timer.addTimerListener(this);
+			timer = new Timer(timer_interval, this);
+			timer.setInitialDelay(initial_delay);
+			timer.setActionCommand(dirobj.toString());
 			timer.start();
-				 }
-		else if (already_dragging_outside &&
-				(x > 0 && x < dim.width &&
-				 y > 0 && y < dim.height)) {
+		} else if (already_dragging_outside
+				&& (x > 0 && x < dim.width
+				&& y > 0 && y < dim.height)) {
 			already_dragging_outside = false;
 			if (timer != null) {
 				timer.stop();
 				timer = null;
 			}
-				 }
-	}
-
-	/**
-	 *  If NeoTimerEventClock's lazy_event_posting is turned on, calls to
-	 *  heardTimerEvent() should be running on the event dispatch thread,
-	 *  and therefore be running synchronously with AWT event and paint calls
-	 */
-	public void heardTimerEvent(NeoTimerEvent evt) {
-		Object arg = evt.getArg();
-		if (!(arg instanceof Integer)) { return; }
-		int direction = ((Integer)arg).intValue();
-		NeoDragEvent new_event = new NeoDragEvent(this, direction);
-		for (NeoDragListener l : listeners) {
-			l.heardDragEvent(new_event);
 		}
-		time_count++;
 	}
 
 	public void addDragListener(NeoDragListener listener) {
@@ -119,6 +107,15 @@ public class DragMonitor
 
 	public Set<NeoDragListener> getDragListeners() {
 		return listeners;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		String arg = e.getActionCommand();
+		int direction = Integer.valueOf(arg);
+		NeoDragEvent new_event = new NeoDragEvent(this, direction);
+		for (NeoDragListener l : listeners) {
+			l.heardDragEvent(new_event);
+		}
 	}
 
 }

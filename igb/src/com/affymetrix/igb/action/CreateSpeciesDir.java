bd6fa2fd324eac8912f609efb092a7932e0ee20d
hiralv <hiralv@uncc.edu>
2010-11-18 16:22:09 +0000

    Merging changes of 7173-7174. Using standard names for species.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_4@7175 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/CreateSpeciesDir.java
+++ igb/src/com/affymetrix/igb/action/CreateSpeciesDir.java
@@ -1,5 +1,6 @@
 package com.affymetrix.igb.action;
 
+import com.affymetrix.genometryImpl.util.SpeciesLookup;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import com.affymetrix.genometryImpl.GenometryModel;
@@ -87,7 +88,7 @@ public class CreateSpeciesDir extends AbstractAction {
 
 		List<String> gVersions = GeneralLoadUtils.getGenericVersions(speciesName);
 
-		speciesName = speciesName.trim().replaceAll("\\s+", "_");
+		speciesName = SpeciesLookup.getStandardName(speciesName);
 		File speciesDir = GeneralUtils.makeDir(path+speciesName);
 		
 		//Write Contents.txt
@@ -102,13 +103,17 @@ public class CreateSpeciesDir extends AbstractAction {
 	private static void getAllVisbileFeatures(String path, String version){
 
 		AnnotatedSeqGroup group = GenometryModel.getGenometryModel().getSeqGroup(version);
+		List<GenericFeature> features = GeneralLoadUtils.getFeatures(group);
+
+		//Don't output empty version
+		if(features.isEmpty())
+			return;
 
 		version = version.trim().replaceAll("\\s+", "_");
 
 		//Create version directory
 		GeneralUtils.makeDir(path + "/" +version);
 
-		List<GenericFeature> features = GeneralLoadUtils.getFeatures(group);
 		List<String[][]> annots = new ArrayList<String[][]>();
 		Set<BioSeq> chromInfo = new HashSet<BioSeq>();
 

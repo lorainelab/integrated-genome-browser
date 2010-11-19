b6152feb7a9b0e952dea223028ae23ce4efbfeb0
hiralv <hiralv@uncc.edu>
2010-11-19 20:08:05 +0000

    Don't allow null values for color.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@7188 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/TierPrefsView.java
+++ igb/src/com/affymetrix/igb/view/TierPrefsView.java
@@ -411,7 +411,9 @@ public final class TierPrefsView extends IPrefEditorComponent implements ListSel
       TrackStyle style = tier_styles.get(row);
       switch (col) {
         case COL_COLOR:
-          style.setColor((Color) value);
+		  if(value != null){
+			style.setColor((Color) value);
+		  }
           break;
         case COL_SEPARATE:
           style.setSeparate(((Boolean) value).booleanValue());
@@ -429,7 +431,9 @@ public final class TierPrefsView extends IPrefEditorComponent implements ListSel
           }
           break;
         case COL_BACKGROUND:
-          style.setBackground((Color) value);
+		  if(value != null){
+			style.setBackground((Color) value);
+		  }
           break;
         case COL_GLYPH_DEPTH:
           if (Boolean.TRUE.equals(value)) {

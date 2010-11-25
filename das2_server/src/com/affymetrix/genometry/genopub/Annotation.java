393426d757eda211ec05ecd3e18daae0ddfeac33
biotelerock <biotelerock@uncc.edu>
2010-11-25 01:09:20 +0000

    More modifications to support nested bam file distribution via das2
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@7221 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- das2_server/src/com/affymetrix/genometry/genopub/Annotation.java
+++ das2_server/src/com/affymetrix/genometry/genopub/Annotation.java
@@ -30,118 +30,118 @@ import com.affymetrix.genometryImpl.parsers.useq.USeqUtilities;
 public class Annotation implements Serializable, Owned {
 
 	public static final String PROP_NAME                = "name";
-    public static final String PROP_SUMMARY             = "summary";
-    public static final String PROP_DESCRIPTION         = "description";
-    public static final String PROP_OWNER               = "owner";
-    public static final String PROP_OWNER_EMAIL         = "owner_institute";
-    public static final String PROP_OWNER_INSTITUTE     = "owner_email";
-    public static final String PROP_GROUP               = "group";
-    public static final String PROP_GROUP_CONTACT       = "group_contact";
-    public static final String PROP_GROUP_EMAIL         = "group_email";
-    public static final String PROP_GROUP_INSTITUTE     = "group_institute";
-    public static final String PROP_VISIBILITY          = "visibility";
-    public static final String PROP_INSTITUTE           = "institute";
-    public static final String PROP_ANALYSIS_TYPE       = "analysis_type";
-    public static final String PROP_EXPERIMENT_METHOD   = "experiment_method";
-    public static final String PROP_EXPERIMENT_PLATFORM = "experiment_platform";
-    public static final String PROP_URL                 = "url";
-    
+	public static final String PROP_SUMMARY             = "summary";
+	public static final String PROP_DESCRIPTION         = "description";
+	public static final String PROP_OWNER               = "owner";
+	public static final String PROP_OWNER_EMAIL         = "owner_institute";
+	public static final String PROP_OWNER_INSTITUTE     = "owner_email";
+	public static final String PROP_GROUP               = "group";
+	public static final String PROP_GROUP_CONTACT       = "group_contact";
+	public static final String PROP_GROUP_EMAIL         = "group_email";
+	public static final String PROP_GROUP_INSTITUTE     = "group_institute";
+	public static final String PROP_VISIBILITY          = "visibility";
+	public static final String PROP_INSTITUTE           = "institute";
+	public static final String PROP_ANALYSIS_TYPE       = "analysis_type";
+	public static final String PROP_EXPERIMENT_METHOD   = "experiment_method";
+	public static final String PROP_EXPERIMENT_PLATFORM = "experiment_platform";
+	public static final String PROP_URL                 = "url";
 
-    private Integer             idAnnotation;
-    private String              name;
-    private String              summary;
-    private String              description;
-    private String              codeVisibility;
-    private String              fileName;
-    private Integer             idGenomeVersion;
-    private Integer             idAnalysisType;
-    private Integer             idExperimentMethod;
-    private Integer             idExperimentPlatform;
-    private Set                 annotationGroupings;
-    private Integer             idUser;
-    private Integer             idUserGroup;
-    private String              createdBy;
-    private java.sql.Date       createDate;
-    private String              isLoaded;
 
-    
-    private Map<String, Object> props;  // tag/value representation of annotation properties
-    
-    
-    
-    public Integer getIdAnnotation() {
-        return idAnnotation;
-    }
-    public void setIdAnnotation(Integer idAnnotation) {
-        this.idAnnotation = idAnnotation;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getCodeVisibility() {
-        return codeVisibility;
-    }
-    public void setCodeVisibility(String codeVisibility) {
-        this.codeVisibility = codeVisibility;
-    }
-    public String getFileName() {
-        return fileName;
-    }
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-    public Integer getIdGenomeVersion() {
-        return idGenomeVersion;
-    }
-    public void setIdGenomeVersion(Integer idGenomeVersion) {
-        this.idGenomeVersion = idGenomeVersion;
-    }
-    public Integer getIdAnalysisType() {
-        return idAnalysisType;
-    }
-    public void setIdAnalysisType(Integer idAnalysisType) {
-        this.idAnalysisType = idAnalysisType;
-    }
-    public Integer getIdExperimentMethod() {
-        return idExperimentMethod;
-    }
-    public void setIdExperimentMethod(Integer idExperimentMethod) {
-        this.idExperimentMethod = idExperimentMethod;
-    }
-    public Integer getIdExperimentPlatform() {
-        return idExperimentPlatform;
-    }
-    public void setIdExperimentPlatform(Integer idExperimentPlatform) {
-        this.idExperimentPlatform = idExperimentPlatform;
-    }
+	private Integer             idAnnotation;
+	private String              name;
+	private String              summary;
+	private String              description;
+	private String              codeVisibility;
+	private String              fileName;
+	private Integer             idGenomeVersion;
+	private Integer             idAnalysisType;
+	private Integer             idExperimentMethod;
+	private Integer             idExperimentPlatform;
+	private Set                 annotationGroupings;
+	private Integer             idUser;
+	private Integer             idUserGroup;
+	private String              createdBy;
+	private java.sql.Date       createDate;
+	private String              isLoaded;
+
+
+	private Map<String, Object> props;  // tag/value representation of annotation properties
+
+
+
+	public Integer getIdAnnotation() {
+		return idAnnotation;
+	}
+	public void setIdAnnotation(Integer idAnnotation) {
+		this.idAnnotation = idAnnotation;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCodeVisibility() {
+		return codeVisibility;
+	}
+	public void setCodeVisibility(String codeVisibility) {
+		this.codeVisibility = codeVisibility;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public Integer getIdGenomeVersion() {
+		return idGenomeVersion;
+	}
+	public void setIdGenomeVersion(Integer idGenomeVersion) {
+		this.idGenomeVersion = idGenomeVersion;
+	}
+	public Integer getIdAnalysisType() {
+		return idAnalysisType;
+	}
+	public void setIdAnalysisType(Integer idAnalysisType) {
+		this.idAnalysisType = idAnalysisType;
+	}
+	public Integer getIdExperimentMethod() {
+		return idExperimentMethod;
+	}
+	public void setIdExperimentMethod(Integer idExperimentMethod) {
+		this.idExperimentMethod = idExperimentMethod;
+	}
+	public Integer getIdExperimentPlatform() {
+		return idExperimentPlatform;
+	}
+	public void setIdExperimentPlatform(Integer idExperimentPlatform) {
+		this.idExperimentPlatform = idExperimentPlatform;
+	}
 	public Set getAnnotationGroupings() {
-    	return annotationGroupings;
-    }
+		return annotationGroupings;
+	}
 	public void setAnnotationGroupings(Set annotationGroupings) {
-    	this.annotationGroupings = annotationGroupings;
-    }
+		this.annotationGroupings = annotationGroupings;
+	}
 	public Integer getIdUser() {
-    	return idUser;
-    }
+		return idUser;
+	}
 	public void setIdUser(Integer idUser) {
-    	this.idUser = idUser;
-    }
+		this.idUser = idUser;
+	}
 	public Integer getIdUserGroup() {
-    	return idUserGroup;
-    }
+		return idUserGroup;
+	}
 	public void setIdUserGroup(Integer idUserGroup) {
-    	this.idUserGroup = idUserGroup;
-    }
-	
+		this.idUserGroup = idUserGroup;
+	}
+
 	public boolean isOwner(Integer idUser) {
 		if (this.getIdUser() != null && this.getIdUser().equals(idUser)) {
 			return true;
@@ -157,24 +157,24 @@ public class Annotation implements Serializable, Owned {
 		}
 	}    
 	public String getSummary() {
-    	return summary;
-    }
+		return summary;
+	}
 	public void setSummary(String summary) {
-    	this.summary = summary;
-    }
+		this.summary = summary;
+	}
 
-	
+
 	@SuppressWarnings("unchecked")
 	public Document getXML(GenoPubSecurity genoPubSecurity, DictionaryHelper dh, String data_root) throws Exception {
 		Document doc = DocumentHelper.createDocument();
 		Element root = doc.addElement("Annotation");
-		
+
 		GenomeVersion genomeVersion = dh.getGenomeVersion(this.getIdGenomeVersion());
 		if (genomeVersion == null) {
 			Logger.getLogger(Annotation.class.getName()).log(Level.SEVERE,"Unable to find genome version " + this.getIdGenomeVersion() + " for annotation " + this.getName());
 			throw new Exception("Unable to find genome version " + this.getIdGenomeVersion() + " for annotation " + this.getName());
 		}
-				
+
 		root.addAttribute("idAnnotation", this.getIdAnnotation().toString());
 		root.addAttribute("label", this.getName());
 		root.addAttribute("name", this.getName());
@@ -194,7 +194,7 @@ public class Annotation implements Serializable, Owned {
 		root.addAttribute("createdBy", this.getCreatedBy() != null ? this.getCreatedBy() : "");
 		root.addAttribute("createDate", this.getCreateDate() != null ? Util.formatDate(this.getCreateDate()) : "");
 		root.addAttribute("annotationGroupingCount", Integer.valueOf(this.getAnnotationGroupings().size()).toString());
-		
+
 		// Only show annotation groupings and annotation files for detail
 		// (when data_root is provided).
 		if (data_root != null) {
@@ -204,25 +204,25 @@ public class Annotation implements Serializable, Owned {
 				agNode.addAttribute("name", ag.getQualifiedName());
 			}
 			Element filesNode = root.addElement("Files");
-			
-			String filePath = getDirectory(data_root);
-		    File fd = new File(filePath);
-		    if (fd.exists()) {
-		    	
 
-			    Element fileNode = filesNode.addElement("Dir");
+			String filePath = getDirectory(data_root);
+			File fd = new File(filePath);
+			if (fd.exists()) {
+
+
+				Element fileNode = filesNode.addElement("Dir");
 				fileNode.addAttribute("name", this.getFileName());
 				fileNode.addAttribute("url", filePath);
-			    appendFileXML(filePath, fileNode, null);	    	
-		    }			
+				appendFileXML(filePath, fileNode, null);	    	
+			}			
 		}
-		
+
 		root.addAttribute("canRead", genoPubSecurity.canRead(this) ? "Y" : "N");
 		root.addAttribute("canWrite", genoPubSecurity.canWrite(this) ? "Y" : "N");
-			
+
 		return doc;
 	}
-	
+
 	public static void appendFileXML(String filePath, Element parentNode, String subDirName) {
 		File fd = new File(filePath);
 
@@ -245,14 +245,14 @@ public class Annotation implements Serializable, Owned {
 					fileNode.addAttribute("name", displayName);
 					fileNode.addAttribute("url", fileName);
 					appendFileXML(fileName, fileNode,
-					        subDirName != null ? subDirName + "/"
-					                + f1.getName() : f1.getName());
+							subDirName != null ? subDirName + "/"
+									+ f1.getName() : f1.getName());
 				} else {
 					Element fileNode = parentNode.addElement("File");
 
 					long kb = Util.getKilobytes(f1.length());
 					String kilobytes = kb + " kb";
-					
+
 					fileNode.addAttribute("name", displayName);
 					fileNode.addAttribute("url", fileName);
 					fileNode.addAttribute("size", kilobytes);
@@ -262,16 +262,16 @@ public class Annotation implements Serializable, Owned {
 			}
 		}
 	}
-	
+
 	public void removeFiles(String data_root) throws IOException {
-		
+
 		String filePath = getDirectory(data_root);
-	    File dir = new File(filePath);
-	    
-	    if (dir.exists()) {
-		    // Delete the files in the directory
-		    String[] childFileNames = dir.list();
-		    if (childFileNames != null) {
+		File dir = new File(filePath);
+
+		if (dir.exists()) {
+			// Delete the files in the directory
+			String[] childFileNames = dir.list();
+			if (childFileNames != null) {
 				for (int x = 0; x < childFileNames.length; x++) {
 					String fileName = filePath + "/" + childFileNames[x];
 					File f = new File(fileName);
@@ -280,148 +280,153 @@ public class Annotation implements Serializable, Owned {
 						Logger.getLogger(Annotation.class.getName()).log(Level.WARNING, "Unable to delete file " + fileName);
 					}
 				}
-		    	
-		    }
-			
+
+			}
+
 			// Delete the annotation directory
-		    boolean success = dir.delete();	    	
+			boolean success = dir.delete();	    	
 			if (!success) {
 				Logger.getLogger(Annotation.class.getName()).log(Level.WARNING, "Unable to delete directory " + filePath);
 			}
-	    }
+		}
 	}
 
-	
+
 	public List<File> getFiles(String data_root) throws IOException {
-		
+
 		ArrayList<File> files = new ArrayList<File>();
-		
+
 		String filePath = getDirectory(data_root);
-	    File dir = new File(filePath);
-	    
-	    if (dir.exists()) {
-		    // Delete the files in the directory
-		    String[] childFileNames = dir.list();
-		    if (childFileNames != null) {
+		File dir = new File(filePath);
+
+		if (dir.exists()) {
+			// Delete the files in the directory
+			String[] childFileNames = dir.list();
+			if (childFileNames != null) {
 				for (int x = 0; x < childFileNames.length; x++) {
 					String fileName = filePath + "/" + childFileNames[x];
 					File f = new File(fileName);
 					files.add(f);
 				}
-		    	
-		    }
-	    }
-	    
-	    return files;
+
+			}
+		}
+
+		return files;
 	}
 	public boolean isBarGraphData(String data_root) throws IOException {
 		boolean isExtension = false;
 		String filePath = getDirectory(data_root);
-	    File dir = new File(filePath);
-	    
-	    if (dir.exists()) {
-		    // Delete the files in the directory
-		    String[] childFileNames = dir.list();
-		    if (childFileNames != null) {
+		File dir = new File(filePath);
+
+		if (dir.exists()) {
+			// Delete the files in the directory
+			String[] childFileNames = dir.list();
+			if (childFileNames != null) {
 				for (int x = 0; x < childFileNames.length; x++) {
 					if (childFileNames[x].endsWith("bar")) {
 						isExtension = true;
 						break;
 					}
 				}
-		    	
-		    }
-	    }
-	    return isExtension;
+
+			}
+		}
+		return isExtension;
 	}
-	
+
 	public boolean isBamData(String data_root) throws IOException {
 		boolean isExtension = false;
 		String filePath = getDirectory(data_root);
-	    File dir = new File(filePath);
-	    
-	    if (dir.exists()) {
-		    String[] childFileNames = dir.list();
-		    if (childFileNames != null) {
+		File dir = new File(filePath);
+
+		if (dir.exists()) {
+			String[] childFileNames = dir.list();
+			if (childFileNames != null) {
 				for (int x = 0; x < childFileNames.length; x++) {
 					if (childFileNames[x].endsWith("bam")) {
 						isExtension = true;
 						break;
 					}
 				}
-		    	
-		    }
-	    }
-	    return isExtension;
+
+			}
+		}
+		return isExtension;
 	}
 
 	public boolean isUseqGraphData(String data_root) throws IOException {
 		boolean isExtension = false;
 		String filePath = getDirectory(data_root);
-	    File dir = new File(filePath);
-	    
-	    if (dir.exists()) {
-		    // Delete the files in the directory
-		    String[] childFileNames = dir.list();
-		    if (childFileNames != null) {
+		File dir = new File(filePath);
+
+		if (dir.exists()) {
+			// Delete the files in the directory
+			String[] childFileNames = dir.list();
+			if (childFileNames != null) {
 				for (int x = 0; x < childFileNames.length; x++) {
 					if (USeqUtilities.USEQ_ARCHIVE.matcher(childFileNames[x]).matches() ) {
 						isExtension = true;
 						break;
 					}
 				}
-		    	
-		    }
-	    }
-	    return isExtension;
+
+			}
+		}
+		return isExtension;
 	}
 
 	public int getFileCount(String data_root) throws IOException {
 		int fileCount = 0;
 		String filePath = getDirectory(data_root);
-	    File dir = new File(filePath);
-	    
-	    if (dir.exists()) {
-		    // Delete the files in the directory
-		    String[] childFileNames = dir.list();
-		    if (childFileNames != null) {
-		    	fileCount = childFileNames.length;
-		    }
-	    }
-	    return fileCount;
+		File dir = new File(filePath);
+
+		if (dir.exists()) {
+			// Delete the files in the directory
+			String[] childFileNames = dir.list();
+			if (childFileNames != null) {
+				fileCount = childFileNames.length;
+			}
+		}
+		return fileCount;
 	}
-	
+
 	public String getQualifiedFileName(String data_root) {
 		if (this.getFileName() == null || this.getFileName().equals("")) {
 			return "";
 		}
 		String filePath =  data_root + this.getFileName();
 		File file = new File(filePath);
-		
+
 		File[] files = file.listFiles();
-		
-		//one file return file
-		if (files.length == 1){
-			String[] childFileNames = file.list();
-			filePath += "/" + childFileNames[0];
-		}
-		//two file possibly bam
-		else if (files.length == 2){
-			for (int i=0; i< files.length; i++){
-				if (files[i].getName().endsWith("bam")) {
-					filePath += "/" + files[i].getName();
-					break;
+
+		if (files != null) {
+			//one file return file
+			if (files.length == 1){
+				String[] childFileNames = file.list();
+				filePath += "/" + childFileNames[0];
+			}
+			//two file possibly bam
+			else if (files.length == 2){
+				for (int i=0; i< files.length; i++){
+					if (files[i].getName().endsWith("bam")) {
+						filePath += "/" + files[i].getName();
+						break;
+					}
 				}
 			}
 		}
+		else {
+System.out.println("\nNix GetQualifiedFileName null files "+filePath + " "+this.idGenomeVersion);
+		}
 		return filePath;
-		
+
 	}
-	
+
 	public String getDirectory(String data_root) {
 		return data_root  + this.getFileName();
 	}
-	
+
 	public Map<String, Object> loadProps(DictionaryHelper dictionaryHelper) {
 		props = new TreeMap<String, Object>();
 		props.put(PROP_NAME, this.getName());
@@ -440,7 +445,7 @@ public class Annotation implements Serializable, Owned {
 		props.put(PROP_EXPERIMENT_PLATFORM, dictionaryHelper.getExperimentPlatform(this.getIdExperimentPlatform()));
 		props.put(PROP_EXPERIMENT_PLATFORM, dictionaryHelper.getExperimentPlatform(this.getIdExperimentPlatform()));
 		return props;
-    }
+	}
 
 	public Map<String,Object> getProperties() {
 		return props;
@@ -448,7 +453,7 @@ public class Annotation implements Serializable, Owned {
 	public Map<String,Object> cloneProperties() {
 		return props;
 	}
-	
+
 	public Object getProperty(String key) {
 		if (props != null) {
 			return props.get(key);
@@ -465,23 +470,23 @@ public class Annotation implements Serializable, Owned {
 		}
 	}
 	public String getCreatedBy() {
-    	return createdBy;
-    }
+		return createdBy;
+	}
 	public void setCreatedBy(String createdBy) {
-    	this.createdBy = createdBy;
-    }
+		this.createdBy = createdBy;
+	}
 	public java.sql.Date getCreateDate() {
-    	return createDate;
-    }
+		return createDate;
+	}
 	public void setCreateDate(java.sql.Date createDate) {
-    	this.createDate = createDate;
-    }
+		this.createDate = createDate;
+	}
 
 	public String getIsLoaded() {
-    	return isLoaded;
-    }
+		return isLoaded;
+	}
 	public void setIsLoaded(String isLoaded) {
-    	this.isLoaded = isLoaded;
-    }
-	
+		this.isLoaded = isLoaded;
+	}
+
 }

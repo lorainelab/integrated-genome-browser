b1f0ffeb85972f51b3a7e6e5e0f8190362b0d51f
biotelerock <biotelerock@uncc.edu>
2010-11-25 01:06:52 +0000

    New class for catching bad bam files
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@7215 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ das2_server/src/com/affymetrix/genometry/genopub/MalformedBamFileException.java
@@ -0,0 +1,9 @@
+package com.affymetrix.genometry.genopub;
+
+
+public class MalformedBamFileException extends Exception {
+
+  public MalformedBamFileException(String message) {
+    super(message);
+  }
+}

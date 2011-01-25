662339cb3975786700555394fb1f82510fc87781
lfrohman <lfrohman@mycompany.com>
2011-01-25 23:14:50 +0000

    remove dead code
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@7380 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/igb_service/src/com/affymetrix/igb/osgi/service/ExtensionPointRegistry.java
+++ plugins/igb_service/src/com/affymetrix/igb/osgi/service/ExtensionPointRegistry.java
@@ -1,18 +1,14 @@
 package com.affymetrix.igb.osgi.service;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class ExtensionPointRegistry {
 	private static ExtensionPointRegistry instance = new ExtensionPointRegistry();
 	private final HashMap<String, ExtensionPoint<?>> extensionPointMap;
-	private final HashMap<String, Set<ExtensionPointRegisterListener>> extensionPointRegisterListeners;
 	
 	private ExtensionPointRegistry() {
 		super();
 		extensionPointMap = new HashMap<String, ExtensionPoint<?>>();
-		extensionPointRegisterListeners = new HashMap<String, Set<ExtensionPointRegisterListener>>();
 	}
 
 	public static ExtensionPointRegistry getInstance() {
@@ -25,27 +21,5 @@ public class ExtensionPointRegistry {
 
 	public void registerExtensionPoint(String name, ExtensionPoint<?> extensionPoint) {
 		extensionPointMap.put(name, extensionPoint);
-		Set<ExtensionPointRegisterListener> extensionPointRegisterListenerSet = extensionPointRegisterListeners.get(name);
-		if (extensionPointRegisterListenerSet != null) {
-			for (ExtensionPointRegisterListener extensionPointRegisterListener : extensionPointRegisterListenerSet) {
-				extensionPointRegisterListener.extensionPointAdded();
-			}
-		}
-	}
-
-	public void addExtensionPointRegisterListener(String extensionName, ExtensionPointRegisterListener extensionPointRegisterListener) {
-		Set<ExtensionPointRegisterListener> extensionPointRegisterListenerSet = extensionPointRegisterListeners.get(extensionName);
-		if (extensionPointRegisterListenerSet == null) {
-			extensionPointRegisterListenerSet = new HashSet<ExtensionPointRegisterListener>();
-			extensionPointRegisterListeners.put(extensionName, extensionPointRegisterListenerSet);
-		}
-		extensionPointRegisterListenerSet.add(extensionPointRegisterListener);
-	}
-
-	public void removeExtensionPointRegisterListener(String extensionName, ExtensionPointRegisterListener extensionPointRegisterListener) {
-		Set<ExtensionPointRegisterListener> extensionPointRegisterListenerSet = extensionPointRegisterListeners.get(extensionName);
-		if (extensionPointRegisterListenerSet != null) {
-			extensionPointRegisterListenerSet.remove(extensionPointRegisterListener);
-		}
 	}
 }

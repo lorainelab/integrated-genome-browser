ce39590ed652f41ad48ebf757cdf4f2a3048fa4c
tdisera <tdisera@uncc.edu>
2011-01-26 17:57:20 +0000

    Fix run_igb.bat to invoke correct java main for IGB.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@7385 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- run_igb.bat
+++ run_igb.bat
@@ -7,5 +7,5 @@ REM IGB REQUIRES JAVA 1.6 or higher
 REM This file assumes you have the 'java' program in your PATH.
 REM If not, go to http://java.com and verify your installation.
 
-java -mx1024m -classpath "igb_exe.jar" "com.affymetrix.igb.IGB"
+java -Xmx1024m -classpath "igb_exe.jar" "com.affymetrix.main.Main"
 

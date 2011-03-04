fc24b7f4aaafae23116a5f2362313f5804d08079
lfrohman <lfrohman@mycompany.com>
2011-03-04 01:14:14 +0000

    javadocs
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@7661 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/plugins_page/src/com/affymetrix/igb/plugins/BundleFilter.java
+++ plugins/plugins_page/src/com/affymetrix/igb/plugins/BundleFilter.java
@@ -6,5 +6,10 @@ import org.osgi.framework.Bundle;
  * interface for filtering bundles.
  */
 public interface BundleFilter {
+	/**
+	 * check a bundle to see if it passes the filter
+	 * @param bundle the bundle to check
+	 * @return if the bundle passes the filter
+	 */
 	public boolean filterBundle(Bundle bundle);
 }

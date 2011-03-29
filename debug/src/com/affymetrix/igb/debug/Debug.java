009bcf9f24c031b30b6e3ea19972888cfa1a7f44
lfrohman <lfrohman@mycompany.com>
2011-03-29 15:57:31 +0000

    remove dead code
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_5@7881 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- debug/src/com/affymetrix/igb/debug/Debug.java
+++ debug/src/com/affymetrix/igb/debug/Debug.java
@@ -19,7 +19,6 @@ public class Debug {
 		activators.add(new com.affymetrix.igb.property.Activator());
 		activators.add(new com.affymetrix.igb.restrictions.Activator());
 		activators.add(new com.affymetrix.igb.search.Activator());
-		activators.add(new com.affymetrix.genometryImpl.Activator());
 	}
 	/**
 	 * Start the program.

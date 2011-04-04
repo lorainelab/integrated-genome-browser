e79506ba0031d6a4e75f03045cee4e8104ea71d5
vbishnoi <vbishnoi@uncc.edu>
2011-04-04 20:46:39 +0000

    --Merging 7953
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_5@7954 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/SeqMapView.java
+++ igb/src/com/affymetrix/igb/view/SeqMapView.java
@@ -1664,6 +1664,7 @@ public class SeqMapView extends JPanel
 		List<SeqSymmetry> selected_syms = getSelectedSyms();
 		if (!selected_syms.isEmpty()) {
 			popup.add(selectParentMI);
+			if(!(selected_syms.get(0) instanceof GraphSym))
 			popup.add(new JMenuItem(seqviewer));
 //			seqViewerOptions.add(viewFeatureinSequenceViewer);
 //			seqViewerOptions.add(viewParentinSequenceViewer);

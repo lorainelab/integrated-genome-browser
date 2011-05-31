5e4893d54952f179b5eadc5b2a5f1f85c7cd64a6
hiralv <hiralv@uncc.edu>
2011-05-31 16:20:51 +0000

    Adding button to show running threads.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@8200 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/StatusBar.java
+++ igb/src/com/affymetrix/igb/view/StatusBar.java
@@ -1,23 +1,24 @@
 package com.affymetrix.igb.view;
 
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
 
-import com.affymetrix.igb.view.load.GeneralLoadView;
 import com.jidesoft.status.MemoryStatusBarItem;
+import com.affymetrix.igb.thread.ThreadHandler;
+import com.affymetrix.igb.util.IGBUtils;
 
-public final class StatusBar extends JPanel {
+public final class StatusBar extends JPanel{
 	private static final long serialVersionUID = 1l;
-
+	
+	private static final ImageIcon closeIcon = IGBUtils.getIcon("x_icon.gif");
+	
 	private final JLabel status_ta;
+	private final MemoryStatusBarItem memory_item;
+	private final JButton mainCancel;
 	public final JProgressBar progressBar;
-	MemoryStatusBarItem memory_item;
-	private JButton stopBtn;
-
+		
 	public StatusBar() {
 		String tt_status = "Shows Selected Item, or other Message";
 	
@@ -25,8 +26,10 @@ public final class StatusBar extends JPanel {
 		progressBar = new JProgressBar();
 		memory_item = new MemoryStatusBarItem();
 		memory_item.setShowMaxMemory(true);
-		stopBtn = new JButton("x");
-		
+		mainCancel = new JButton(closeIcon);
+		mainCancel.setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 0));
+		ThreadHandler.getThreadHandler().addPopupHandler(mainCancel);
+
 		status_ta.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
 		progressBar.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
 	
@@ -34,7 +37,7 @@ public final class StatusBar extends JPanel {
 		progressBar.setMaximumSize(new Dimension(150, 5));
 		progressBar.setIndeterminate(true);
 		progressBar.setVisible(false);
-
+		
 		GroupLayout layout = new GroupLayout(this);
 		setLayout(layout);
 
@@ -43,17 +46,19 @@ public final class StatusBar extends JPanel {
 		layout.setHonorsVisibility(false);
 
 		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addComponent(mainCancel)
 				.addComponent(status_ta)
 				.addGap(1, 1, Short.MAX_VALUE)
 				.addComponent(progressBar)
 				.addComponent(memory_item, 1, 200, 200));
 
 		layout.setVerticalGroup(layout.createParallelGroup(Alignment.CENTER)
+				.addComponent(mainCancel)
 				.addComponent(status_ta)
 				.addGap(1, 1, Short.MAX_VALUE)
 				.addComponent(progressBar)
 				.addComponent(memory_item));
-
+		
 	}
 
 	/** Sets the String in the status bar.

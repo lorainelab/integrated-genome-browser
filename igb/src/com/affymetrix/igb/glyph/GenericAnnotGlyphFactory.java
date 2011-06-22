b7c5dd0e65bb360d9d0d137ae4c2aa76d044b25f
hiralv <hiralv@uncc.edu>
2011-06-22 17:15:14 +0000

    Fixing arrow in wrong direction bug.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@8323 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/glyph/GenericAnnotGlyphFactory.java
+++ igb/src/com/affymetrix/igb/glyph/GenericAnnotGlyphFactory.java
@@ -317,7 +317,7 @@ public final class GenericAnnotGlyphFactory implements MapViewGlyphFactoryI {
 				if (cspan.getLength() == 0) {
 					cglyph = new DeletionGlyph();
 				} else {
-					if(i==childCount-1){
+					if(i==childCount-1 && cspan.isForward() || i==0 && !cspan.isForward()){
 						cglyph =  (GlyphI) directed_child_glyph_class.newInstance();
 					}else{
 						cglyph = (GlyphI) child_glyph_class.newInstance();

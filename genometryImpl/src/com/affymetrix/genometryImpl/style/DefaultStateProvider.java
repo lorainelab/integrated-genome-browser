5dd1442eb350f546e485d5daad6ca173cc14e795
hiralv <hiralv@uncc.edu>
2011-07-18 15:10:34 +0000

    Fixing build.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@8472 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/style/DefaultStateProvider.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/style/DefaultStateProvider.java
@@ -67,11 +67,11 @@ public class DefaultStateProvider implements StateProvider {
 		return state;
 	}
 
-	public ITrackStyleExtended getAnnotStyle(String name, String human_name, String file_type, Map<String, String> props) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
 	public ITrackStyleExtended getAnnotStyle(String name, String human_name, String file_type) {
+		return getAnnotStyle(name);
+	}
+	
+	public ITrackStyleExtended getAnnotStyle(String name, String human_name, String file_type, Map<String, String> props) {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 

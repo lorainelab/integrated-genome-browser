46d073596f039101ba30bd69e6e7702882a613e2
hiralv <hiralv@uncc.edu>
2011-09-06 19:12:56 +0000

    Merging changes 8852: Adding hack for edge case.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@8853 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
+++ igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
@@ -639,6 +639,10 @@ public final class GeneralLoadUtils {
 		}
 		if (gFeature.getLoadStrategy() == LoadStrategy.VISIBLE || gFeature.getLoadStrategy() == LoadStrategy.AUTOLOAD) {
 			overlap = gviewer.getVisibleSpan();
+			// TODO: Investigate edge case at max
+			if(overlap.getMin() == selected_seq.getMin() && overlap.getMax() == selected_seq.getMax()){
+				overlap = new SimpleSeqSpan(selected_seq.getMin(), selected_seq.getMax() - 1, selected_seq);
+			}
 		} else if (gFeature.getLoadStrategy() == LoadStrategy.GENOME || gFeature.getLoadStrategy() == LoadStrategy.CHROMOSOME) {
 			// TODO: Investigate edge case at max
 			overlap = new SimpleSeqSpan(selected_seq.getMin(), selected_seq.getMax() - 1, selected_seq);

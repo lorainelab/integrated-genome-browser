3c5b265e1f8bd7529f969d0902b7dd3373d93653
hiralv <hiralv@uncc.edu>
2011-09-06 15:51:31 +0000

    Merging changes 8844: Fixing derived data not getting removed while removing the feature.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@8846 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/TrackView.java
+++ igb/src/com/affymetrix/igb/view/TrackView.java
@@ -374,11 +374,12 @@ public class TrackView {
 		if(dd_list == null)
 			return;
 		
+		List<DependentData> remove_list = new ArrayList<DependentData>();
 		for (int i = 0; i < dd_list.size(); i++) {
 			dd = dd_list.get(i);
 			if ((method == null ? dd.getParentMethod() == null : method.equals(dd.getParentMethod()))
 					|| method.equals(dd.getID())) {
-				dd_list.remove(dd);
+				remove_list.add(dd);
 //				GlyphI glyph = map.getItem(dd.getSym());
 //				if(glyph != null){
 //					map.removeItem(glyph);
@@ -386,6 +387,12 @@ public class TrackView {
 //				seq.unloadAnnotation(dd.getSym());
 			}
 		}
+		
+		for(DependentData r : remove_list){
+			dd_list.remove(r);
+		}
+		
+		remove_list.clear();
 	}
 
 }

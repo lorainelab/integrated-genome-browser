194d15ddec4b66daf69a895661617fecd982e0b3
hiralv <hiralv@uncc.edu>
2011-09-23 14:36:38 +0000

    Updating welcome page to include images. (Note: It is still a prototype.)
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@8981 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/load/Welcome.java
+++ igb/src/com/affymetrix/igb/view/load/Welcome.java
@@ -2,23 +2,26 @@ package com.affymetrix.igb.view.load;
 
 import java.awt.Font;
 import java.awt.Color;
-import javax.swing.JComboBox;
+import java.awt.Image;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.image.BufferedImage;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.geom.Rectangle2D;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 
 import com.affymetrix.genometryImpl.style.DefaultTrackStyle;
 
 import com.affymetrix.genoviz.widget.NeoMap;
 import com.affymetrix.genoviz.glyph.StringGlyph;
-import com.affymetrix.genoviz.bioviews.ViewI;
+import com.affymetrix.genoviz.glyph.BasicImageGlyph;
 
 import com.affymetrix.igb.Application;
 import com.affymetrix.igb.IGBConstants;
 import com.affymetrix.igb.shared.FasterExpandPacker;
-import com.affymetrix.igb.shared.TierGlyph;
+import com.affymetrix.igb.shared.TransformTierGlyph;
+import com.affymetrix.igb.util.IGBUtils;
 import com.affymetrix.igb.view.SeqMapView;
 
 /**
@@ -32,34 +35,27 @@ public class Welcome implements ItemListener, ComponentListener{
 	private static final Color COLOR_2 = Color.YELLOW;
 	private static final float FONT_SIZE_1 = 36.0f;
 	private static final float FONT_SIZE_2 = 22.0f;
+	private static final float FONT_SIZE_3 = 12.0f;
 	
-	private static final Message MESSAGES[] = new Message[]{new Message("", FONT_SIZE_1, COLOR_1),
+	private static final Message MESSAGES[] = new Message[]{
 												new Message("Welcome to", FONT_SIZE_1, COLOR_1), 
 												new Message("Integrated Genome Browser", FONT_SIZE_1, COLOR_2),
 												new Message("To get started, choose a species and genome version.", FONT_SIZE_2, COLOR_1)};
+	private static final Message SPECIES[] = new Message[]{
+												new Message("a_lyrata.gif", "A. lyrata", FONT_SIZE_3, COLOR_2),
+												new Message("h_sapiens.gif", "H. Sapiens", FONT_SIZE_3, COLOR_2),
+												new Message("m_musculus.gif", "M. Musculus", FONT_SIZE_3, COLOR_2),
+												new Message("a_thaliana.gif", "A. Thaliana", FONT_SIZE_3, COLOR_2),
+												new Message("d_melnogaster.gif", "D. Melnogaster", FONT_SIZE_3, COLOR_2),};
 	
 	private static final SeqMapView smv = Application.getSingleton().getMapView();
-
+	
 	private final NeoMap map;
-	private final TierGlyph parent;
+	private final TransformTierGlyph parent;
 	
 	public Welcome() {
-		parent = new TierGlyph(new DefaultTrackStyle()) {
-
-			@Override
-			public void pack(ViewI view, boolean manual) {
-				packer.pack(this, view, manual);
-				Rectangle2D.Double mbox = scene.getCoordBox();
-				Rectangle2D.Double cbox = this.getCoordBox();
-				this.setCoords(mbox.x, cbox.y, mbox.width, cbox.height);
-
-			}
-			
-			@Override
-			protected boolean shouldDrawToolBar(){
-				return false;
-			}
-		};
+		parent = new TransformTierGlyph(new DefaultTrackStyle());
+		parent.setHitable(false);
 		FasterExpandPacker packer = new FasterExpandPacker();
 		packer.setSpacing(5);
 		parent.setPacker(packer);
@@ -77,14 +73,38 @@ public class Welcome implements ItemListener, ComponentListener{
 			sg = new StringGlyph(MESSAGES[i].str);
 			sg.setHitable(false);
 			sg.setForegroundColor(MESSAGES[i].color);
-			int l = sg.getString().length();
-			sg.setCoords((map_length - l) / 2, 0, l, 10);
+
+			sg.setCoords(0, 0, map_length, 10);
 			Font font = sg.getFont();
 			font = font.deriveFont(MESSAGES[i].font_size);
 			sg.setFont(font);
 			parent.addChild(sg);
 		}
-				
+		
+		for (int i = 0; i < SPECIES.length; i++) {
+			sg = new StringGlyph(SPECIES[i].str);
+			sg.setVerticalPlacement(StringGlyph.BELOW);
+			sg.setHitable(false);
+			sg.setForegroundColor(SPECIES[i].color);
+			sg.setCoords(20 * i, 0, 15, 40);
+			Font font = sg.getFont();
+			font = font.deriveFont(SPECIES[i].font_size);
+			sg.setFont(font);
+			
+			ImageIcon ii = IGBUtils.getIcon(SPECIES[i].image_name);
+			Image image = ii.getImage();
+			image = resizeImage(image, BufferedImage.TYPE_INT_RGB);
+			map.prepareImage(image, map);
+			
+			BasicImageGlyph big = new BasicImageGlyph();
+			big.setSelectable(false);
+			big.setImage(image, map);
+			big.setCoords(20 * i, 0, 15, 30);
+			sg.addChild(big);
+			
+			parent.addChild(sg);
+		}
+		
 		map.addItem(parent);
 		parent.pack(map.getView(), false);
 		parent.setVisibility(true);
@@ -116,15 +136,32 @@ public class Welcome implements ItemListener, ComponentListener{
 
 	private static final class Message{
 		
+		final String image_name;
 		final String str;
 		final float font_size;
 		final Color color;
 		
 		Message(String str, float font_size, Color color){
+			this.image_name = null;
 			this.str = str;
 			this.font_size = font_size;
 			this.color = color;
 		}
 		
+		Message(String image_name, String str, float font_size, Color color){
+			this.image_name = image_name;
+			this.str = str;
+			this.font_size = font_size;
+			this.color = color;
+		}
+	}
+	
+	private static Image resizeImage(Image image, int type) {
+		BufferedImage resizedImage = new BufferedImage(100, 100, type);
+		java.awt.Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(image, 0, 0, 100, 100, null);
+		g.dispose();
+
+		return resizedImage;
 	}
 }

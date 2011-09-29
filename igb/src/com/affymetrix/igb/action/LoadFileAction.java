dd0fbe8775193ac3303a886bb2b305ec592dfd66
hiralv <hiralv@uncc.edu>
2011-09-29 13:59:17 +0000

    Show error panel if try to open remote sam file.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_6@9029 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/LoadFileAction.java
+++ igb/src/com/affymetrix/igb/action/LoadFileAction.java
@@ -278,7 +278,12 @@ public final class LoadFileAction extends AbstractAction {
 		String unzippedStreamName = GeneralUtils.stripEndings(uri.toString());
 		String extension = ParserController.getExtension(unzippedStreamName);
 
-		if(extension.equals(".bam")){
+		if(extension.equals(".sam")){
+			if(!LocalUrlCacher.isFile(uri)){
+				ErrorHandler.errorPanel("Cannot open file","Only local sam file is permitted.");
+				version = null;
+			}
+		}else if(extension.equals(".bam")){
 			if(!handleBam(uri)){
 				ErrorHandler.errorPanel("Cannot open file","Could not find index file");
 				version = null;

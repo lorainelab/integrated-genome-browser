e4175a550a3bc9854e4d650fcfa884b82ec26aa4
lfrohman <lfrohman@mycompany.com>
2011-09-29 20:52:39 +0000

    GenericAction fix
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9058 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/WebLinksAction.java
+++ igb/src/com/affymetrix/igb/action/WebLinksAction.java
@@ -1,25 +1,13 @@
 package com.affymetrix.igb.action;
 
-import com.affymetrix.igb.prefs.WebLink;
-import com.affymetrix.igb.shared.FileTracker;
-import com.affymetrix.igb.view.WebLinkEditorPanel;
 import com.affymetrix.igb.view.WebLinksView;
-import com.affymetrix.genoviz.swing.MenuUtil;
-import com.affymetrix.genoviz.util.ErrorHandler;
 import com.affymetrix.genometryImpl.event.GenericAction;
-import com.affymetrix.genometryImpl.util.UniFileChooser;
-import com.affymetrix.genometryImpl.util.PreferenceUtils;
-import com.affymetrix.genometryImpl.util.DisplayUtils;
 
-import static com.affymetrix.igb.IGBConstants.APP_NAME;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
 
-import java.awt.*;
 import java.awt.event.*;
-import java.io.*;
 import java.text.MessageFormat;
 import javax.swing.*;
-import javax.swing.event.*;
 
 /**
  *  A panel for viewing and editing weblinks.

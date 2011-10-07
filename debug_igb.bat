98bf12e7f6325847fc0f530ad2b3d203cd20e9f1
lfrohman <lfrohman@mycompany.com>
2011-10-07 19:45:15 +0000

    debug
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9167 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- debug_igb.bat
+++ debug_igb.bat
@@ -1,1 +1,1 @@
-jdb -Xmx1024m -Xdebug -classpath main/build;common/build;etc;ext/felix.jar com.affymetrix.main.Main
+jdb -Xmx1024m -Xdebug -classpath main/build;common/build;common/resources;etc;ext/felix.jar com.affymetrix.main.Main

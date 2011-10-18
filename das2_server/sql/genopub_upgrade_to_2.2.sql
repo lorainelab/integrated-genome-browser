41abcd0781e187ccc5ba60a6495b618e88bcaccb
biotelerock <biotelerock@uncc.edu>
2011-10-18 16:47:38 +0000

    Added mods for customizing the UCSC url
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9229 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- das2_server/sql/genopub_upgrade_to_2.2.sql
+++ das2_server/sql/genopub_upgrade_to_2.2.sql
@@ -1,4 +1,9 @@
 --
 -- Add UCSC string name for genome verisons to table `GenomeVersion`
 --
-ALTER TABLE genopub.GenomeVersion add column ucscName varchar(50) default NULL;
+ALTER TABLE genopub.GenomeVersion add column ucscName varchar(100) default NULL;
+
+--
+-- Add UCSC string name for url to table `User`
+--
+ALTER TABLE genopub.User add column ucscUrl varchar(250) default 'http://genome.ucsc.edu';

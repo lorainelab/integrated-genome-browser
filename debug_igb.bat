3caf9e93766534a2860f1d4c42f9d927978d1e91
lfrohman <lfrohman@mycompany.com>
2011-10-25 20:24:04 +0000

    minor fix
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9286 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- debug_igb.bat
+++ debug_igb.bat
@@ -1,1 +1,4 @@
-jdb -Xmx1024m -Xdebug -classpath main/build;common/build;common/resources;etc;ext/felix.jar com.affymetrix.main.Main
+jdb -Xmx1024m -Xdebug -classpath igb_exe.jar
+REM (when started, enter these commands at the jdb prompt or enter "help")
+REM > stop in MyClass.MyMethod()
+REM > run com.simontuffs.onejar.Boot

3fbfaea6647869878f7724cf6b764fd0742c8c51
lfrohman <lfrohman@mycompany.com>
2011-11-03 15:07:22 +0000

    modify comments
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9351 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- debug_igb.bat
+++ debug_igb.bat
@@ -1,4 +1,4 @@
 jdb -Xmx1024m -Xdebug -classpath igb_exe.jar
 REM (when started, enter these commands at the jdb prompt or enter "help")
 REM > stop in MyClass.MyMethod()
-REM > run com.simontuffs.onejar.Boot
+REM > run com.affymetrix.main.Main

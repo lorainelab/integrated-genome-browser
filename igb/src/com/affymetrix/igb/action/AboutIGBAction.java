7ad3b32f18fb90c2533bcf398735d911bc6fd67e
dcnorris <dcnorris@uncc.edu>
2011-11-09 14:04:24 +0000

    removed an obsolete method
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9388 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/AboutIGBAction.java
+++ igb/src/com/affymetrix/igb/action/AboutIGBAction.java
@@ -138,19 +138,6 @@ public class AboutIGBAction extends GenericAction {
 		dialog.setVisible(true);
 	}
 
-	static void showDir(int indent, File file) throws IOException {
-		for (int i = 0; i < indent; i++) {
-			System.out.print('-');
-		}
-		System.out.println(file.getName());
-		if (file.isDirectory()) {
-			File[] files = file.listFiles();
-			for (int i = 0; i < files.length; i++) {
-				showDir(indent + 4, files[i]);
-			}
-		}
-	}
-
 	@Override
 	public String getText() {
 		return MessageFormat.format(

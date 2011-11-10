611900fcac2b8a37ef6ed6e14e01aeb7fc8bd6bd
lfrohman <lfrohman@mycompany.com>
2011-11-10 16:15:31 +0000

    windows batch file
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9402 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- run_igb.bat
+++ run_igb.bat
@@ -1,11 +1,4 @@
-
-REM This is an EXAMPLE of a shell script to run igb from jar files
-REM on a Windows system.
-REM You may need to edit it for your system.
-REM IGB REQUIRES JAVA 1.6 or higher
-
-REM This file assumes you have the 'java' program in your PATH.
-REM If not, go to http://java.com and verify your installation.
-
-java -Xmx1024m -jar igb_exe.jar %*
-
+echo off
+set FOUND=false
+for %%G in ("%path:;=" "%") do if exist %%G"\java.exe" set FOUND=true
+if %FOUND%==true (java -Xmx1024m -jar igb_exe.jar %*) else (echo %0: could not find java)

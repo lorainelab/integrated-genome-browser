ba85a4ce9398fafa82163e69ba73468081efb719
hiralv <hiralv@uncc.edu>
2011-12-13 17:06:52 +0000

    Check if residue are already loaded. If new residue is loaded then refresh UI.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9541 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/LoadResidueAction.java
+++ igb/src/com/affymetrix/igb/action/LoadResidueAction.java
@@ -1,10 +1,6 @@
 
 package com.affymetrix.igb.action;
 
-import com.affymetrix.genometryImpl.SeqSpan;
-import com.affymetrix.genometryImpl.event.GenericAction;
-import com.affymetrix.igb.Application;
-import com.affymetrix.igb.view.load.GeneralLoadView;
 import java.awt.Point;
 import javax.swing.Box;
 import javax.swing.JDialog;
@@ -12,51 +8,81 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 
+import com.affymetrix.genometryImpl.SeqSpan;
+import com.affymetrix.genometryImpl.event.GenericAction;
+import com.affymetrix.genometryImpl.util.ThreadUtils;
+
+import com.affymetrix.igb.Application;
+import com.affymetrix.igb.view.SeqMapView;
+import com.affymetrix.igb.view.load.GeneralLoadView;
+
 /**
  *
  * @author hiralv
  */
 public class LoadResidueAction extends GenericAction {
-
+	final SeqMapView smv;
+	
+	public LoadResidueAction(SeqMapView smv){
+		this.smv = smv;
+	}
+	
 	@Override
 	public String getText() {
 		return "Load Residue";
 	}
 	
-	public void loadResidue(SeqSpan span, boolean tryFull) {
-		Application app = Application.getSingleton();
-		JFrame frame = (app == null) ? null : app.getFrame();
-		final JDialog dialog = new JDialog(frame, "Loading...");
-		JLabel message = new JLabel("Residues are being loaded please wait");
-		JProgressBar progressBar = new JProgressBar();
+	public boolean loadResidue(final SeqSpan span, boolean tryFull) {
 		
-		//progressBar.setMaximumSize(new Dimension(150, 5));
-		progressBar.setIndeterminate(true);
+		boolean new_residue_loaded = false;
 		
-		Box box = Box.createVerticalBox();
-		box.add(Box.createGlue());
-		box.add(message);
-		box.add(progressBar);
-		
-		dialog.getContentPane().add(box, "Center");
-		
-		//dialog.setUndecorated(true);
-		dialog.setResizable(false);
-		dialog.validate();
-		dialog.pack();
-		
-		if(frame != null){
-			Point location = frame.getLocation();
-			dialog.setLocation(location.x + frame.getWidth()/2 - dialog.getWidth()/2, location.y + frame.getHeight()/2 - dialog.getHeight()/2);
-		}
-		
+		//Check if sequence is already loaded
+		if (!span.getBioSeq().isAvailable(span)) {
+			Application app = Application.getSingleton();
+			JFrame frame = (app == null) ? null : app.getFrame();
+			final JDialog dialog = new JDialog(frame, "Loading...");
+			JLabel message = new JLabel("Residues are being loaded please wait");
+			JProgressBar progressBar = new JProgressBar();
 
-		dialog.setVisible(true);
-		GeneralLoadView.getLoadView().loadResidues(span, tryFull);
-		
-		dialog.setVisible(false);
-		dialog.dispose();
-		
+			//progressBar.setMaximumSize(new Dimension(150, 5));
+			progressBar.setIndeterminate(true);
+
+			Box box = Box.createVerticalBox();
+			box.add(Box.createGlue());
+			box.add(message);
+			box.add(progressBar);
+
+			dialog.getContentPane().add(box, "Center");
+
+			//dialog.setUndecorated(true);
+			dialog.setResizable(false);
+			dialog.validate();
+			dialog.pack();
+
+			if (frame != null) {
+				Point location = frame.getLocation();
+				dialog.setLocation(location.x + frame.getWidth() / 2 - dialog.getWidth() / 2, location.y + frame.getHeight() / 2 - dialog.getHeight() / 2);
+			}
+
+
+			dialog.setVisible(true);
+			new_residue_loaded = GeneralLoadView.getLoadView().loadResidues(span, tryFull);
+
+			dialog.setVisible(false);
+			dialog.dispose();
+			
+			if (new_residue_loaded) {
+				ThreadUtils.runOnEventQueue(new Runnable() {
+
+					public void run() {
+						smv.setAnnotatedSeq(span.getBioSeq(), true, true, true);
+					}
+				});
+			}
+		}
+
 		actionDone();
+
+		return new_residue_loaded;
 	}
 }

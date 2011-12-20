5a12395a082c8625142f71f799fa691e5b82d12d
lfrohman <lfrohman@mycompany.com>
2011-12-20 19:56:56 +0000

    Updated: (IGBB-125) error message for when annots.xml is malformed XML
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9601 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/src/com/affymetrix/genometryImpl/event/ReportBugAction.java
@@ -0,0 +1,40 @@
+package com.affymetrix.genometryImpl.event;
+
+import com.affymetrix.genometryImpl.util.GeneralUtils;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author sgblanch
+ * @version $Id: ReportBugAction.java 9589 2011-12-20 15:54:10Z lfrohman $
+ */
+public class ReportBugAction extends GenericAction {
+	private static final long serialVersionUID = 1l;
+	private static final ReportBugAction ACTION = new ReportBugAction();
+
+	public static ReportBugAction getAction() {
+		return ACTION;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		super.actionPerformed(e);
+		GeneralUtils.browse("http://sourceforge.net/tracker/?group_id=129420&atid=714744");
+	}
+
+	@Override
+	public String getText() {
+		return "Report a bug";
+	}
+
+	@Override
+	public int getMnemonic() {
+		return KeyEvent.VK_R;
+	}
+
+	@Override
+	public boolean isPopup() {
+		return true;
+	}
+}

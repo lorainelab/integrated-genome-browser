38b3cb5b63cede71600697595b74856afae8d251
lfrohman <lfrohman@mycompany.com>
2011-12-22 22:53:58 +0000

    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9630 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/window_service/src/com/affymetrix/igb/window/service/IWindowService.java
+++ plugins/window_service/src/com/affymetrix/igb/window/service/IWindowService.java
@@ -5,7 +5,6 @@ import java.util.Set;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 

736ed9108fcb36e6b7df986f42fdee2959b018fe
dcnorris <dcnorris@uncc.edu>
2012-01-10 15:32:53 +0000

    Updated Welcome Title Text
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9724 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/welcome/WelcomeTitle.java
+++ igb/src/com/affymetrix/igb/view/welcome/WelcomeTitle.java
@@ -20,63 +20,52 @@ import javax.swing.JPanel;
  */
 public class WelcomeTitle extends JPanel {
 
-	public WelcomeTitle(){
+	public WelcomeTitle() {
 		this.setBackground(Color.black);
-                
 	}
 	final String first = "Welcome to";
 	final String second = "Integrated Genome Browser";
-	final String third = "To get started, choose a ";
-	final String fourth = "species ";
-	final String fifth = "and ";
-	final String sixth = "genome version.";
+	final String third = "To get started, choose a genome version using the Current Genome tab.";
+	final String fourth = "Or click an image below.";
+
 	@Override
 	public void paintComponent(Graphics g2) {
 		super.paintComponent(g2);
 		Graphics2D g = (Graphics2D) g2;
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-								RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-		
+				RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+
 		Font f = new Font("Sans Serif", Font.PLAIN, 20);
 		Font f2 = new Font("Sans Serif", Font.PLAIN, 30);
 		FontMetrics m = g.getFontMetrics(f);
 		FontMetrics m2 = g.getFontMetrics(f2);
 
-		int start = (int)( .50 * this.getHeight() ) - 43;
+		int start = (int) (.50 * this.getHeight()) - 43;
 		//g.drawString( this.getWidth() +","+this.getHeight(), 20 , 20);
 
 		g.setFont(f);
 		g.setColor(Color.decode("#FFFFFF"));//Color.decode("#fffb86") );
-		int width = (this.getWidth() / 2 ) - m.stringWidth( first )/2;
+		int width = (this.getWidth() / 2) - m.stringWidth(first) / 2;
 		//+ " "+ this.getWidth() + "," + this.getHeight()
-		g.drawString(first, width , start);
+		g.drawString(first, width, start);
 
 
 		g.setFont(f2);
 		g.setColor(Color.decode("#fffb86"));
-		width = (this.getWidth() / 2 ) - m2.stringWidth( second )/2;
+		width = (this.getWidth() / 2) - m2.stringWidth(second) / 2;
 		start += 48;
-		g.drawString(second , width , start);
+		g.drawString(second, width, start);
 
 
-		int line3 = start + 48;
 		g.setFont(f);
-		width = (this.getWidth() / 2 ) - m.stringWidth( third + fourth + fifth + sixth )/2;
+		width = (this.getWidth() / 2) - m.stringWidth(third) / 2;
 		g.setColor(Color.decode("#FFFFFF"));
-		g.drawString(third, width , line3 );
+		start += 48;
+		g.drawString(third, width, start);
 
-
-		width += m.stringWidth(third);
+		width = (this.getWidth() / 2) - m.stringWidth(fourth) / 2;
 		g.setColor(Color.decode("#fffb86"));
-		g.drawString(fourth, width , line3);
-
-		width += m.stringWidth(fourth);
-		g.setColor(Color.decode("#FFFFFF"));
-		g.drawString(fifth, width , line3);
-
-		width += m.stringWidth(fifth);
-		g.setColor(Color.decode("#fffb86"));
-g.drawString(sixth, width , line3);
+		start += 48;
+		g.drawString(fourth, width, start);
 	}
-	
 }

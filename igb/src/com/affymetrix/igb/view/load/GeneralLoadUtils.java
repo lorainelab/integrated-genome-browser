1156a97c61838f63207c10de9bd2a828f2dda4cc
hiralv <hiralv@uncc.edu>
2012-01-20 15:49:52 +0000

    Remove features when it's server is removed.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9894 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
+++ igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
@@ -176,6 +176,7 @@ public final class GeneralLoadUtils {
 				version = versionIterator.next();
 
 				if (version.gServer == server) {
+					GeneralLoadView.getLoadView().removeAllFeautres(version.getFeatures());
 					version.clear();
 					versionIterator.remove();
 				}

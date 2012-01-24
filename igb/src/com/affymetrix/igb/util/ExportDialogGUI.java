b4e4e4cdcef4dafae3991dc8c0562f5728e232d6
imnick <imnick@uncc.edu>
2012-01-24 21:24:36 +0000

    Miner GUI adjustment for windows.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@9940 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/util/ExportDialogGUI.java
+++ igb/src/com/affymetrix/igb/util/ExportDialogGUI.java
@@ -391,7 +391,7 @@ public class ExportDialogGUI extends JPanel {
                     .add(extComboBox, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .add(0, 0, 0)
                 .add(imageSizePanel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 95, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(0, 0, 0)
                 .add(previewPanel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
         );
     }// </editor-fold>//GEN-END:initComponents

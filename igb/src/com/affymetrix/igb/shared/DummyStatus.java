ebe288bc88d0d3fba5cb0d2523f76a703ba4941a
lfrohman <lfrohman@mycompany.com>
2012-01-25 21:46:05 +0000

    add DummyStatus
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@9978 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ igb/src/com/affymetrix/igb/shared/DummyStatus.java
@@ -0,0 +1,11 @@
+package com.affymetrix.igb.shared;
+
+public class DummyStatus implements IStatus {
+	private static DummyStatus instance = new DummyStatus();
+	public static DummyStatus getInstance() { return instance; }
+	private DummyStatus() {
+		super();
+	}
+	@Override
+	public void setStatus(String text) {}
+}

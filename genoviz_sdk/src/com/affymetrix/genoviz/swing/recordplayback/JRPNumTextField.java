42cd10972d579be98b98d634fda35375f9da6d68
hiralv <hiralv@uncc.edu>
2012-01-26 19:35:00 +0000

    Merging changes r9999:10001. Number only text field.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@10002 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genoviz_sdk/src/com/affymetrix/genoviz/swing/recordplayback/JRPNumTextField.java
@@ -0,0 +1,45 @@
+package com.affymetrix.genoviz.swing.recordplayback;
+
+import java.awt.event.KeyEvent;
+import java.util.regex.Pattern;
+import javax.swing.text.Document;
+
+/**
+ *
+ * @author hiralv
+ */
+public class JRPNumTextField extends JRPTextField {
+
+	private static final Pattern NUMBERS = Pattern.compile("[0-9]");
+	
+	public JRPNumTextField(String id) {
+		super(id);
+	}
+	public JRPNumTextField(String id, Document doc, String text, int columns) {
+		super(id, doc, text, columns);
+	}
+	public JRPNumTextField(String id, int columns) {
+		super(id, columns);
+	}
+	public JRPNumTextField(String id, String text) {
+		super(id, text);
+	}
+	public JRPNumTextField(String id, String text, int columns) {
+		super(id, text, columns);
+	}
+	
+	@Override
+	public void processKeyEvent(KeyEvent ev) {
+		char ch = ev.getKeyChar();
+		
+		if(NUMBERS.matcher(String.valueOf(ch)).matches() || 
+				ch == KeyEvent.VK_BACK_SPACE || ch == KeyEvent.VK_DELETE){
+			super.processKeyEvent(ev);
+			return;
+		}
+			
+		ev.consume();
+	}
+
+	
+}

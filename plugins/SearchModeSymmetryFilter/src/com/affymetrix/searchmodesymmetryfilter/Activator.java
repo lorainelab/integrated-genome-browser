075b08f14023c6f74b8fa30201d106759f15c0b3
lfrohman <lfrohman@mycompany.com>
2012-01-27 20:34:05 +0000

    TODO-52 Find Annotations page
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10013 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ plugins/SearchModeSymmetryFilter/src/com/affymetrix/searchmodesymmetryfilter/Activator.java
@@ -0,0 +1,62 @@
+package com.affymetrix.searchmodesymmetryfilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
+
+//import com.affymetrix.genometryImpl.filter.SymmetryFilterSearchId;
+import com.affymetrix.genometryImpl.filter.SymmetryFilterSearchProps;
+import com.affymetrix.igb.osgi.service.IGBService;
+import com.affymetrix.igb.shared.ISearchModeSym;
+
+public class Activator implements BundleActivator {
+	private BundleContext bundleContext;
+	private List<ServiceRegistration<ISearchModeSym>> searchModeSymmetryFilterRegistrations = new ArrayList<ServiceRegistration<ISearchModeSym>>();
+
+	private void registerService(ServiceReference<IGBService> igbServiceReference) {
+        try
+        {
+        	IGBService igbService = bundleContext.getService(igbServiceReference);
+			System.out.println("searchModeSymmetryFilterRegistrations.registerService");
+        	searchModeSymmetryFilterRegistrations.add(bundleContext.registerService(ISearchModeSym.class, new SearchModeSymmetryFilter(igbService, new SymmetryFilterSearchProps(), 2000), null));
+//        	searchModeSymmetryFilterRegistrations.add(bundleContext.registerService(ISearchModeSym.class, new SearchModeSymmetryFilter(igbService, new SymmetryFilterSearchId(), 4000), null));
+        }
+        catch (Exception ex) {
+            System.out.println(this.getClass().getName() + " - Exception in Activator.createPage() -> " + ex.getMessage());
+            ex.printStackTrace(System.out);
+        }
+	}
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		this.bundleContext = bundleContext;
+    	ServiceReference<IGBService> igbServiceReference = bundleContext.getServiceReference(IGBService.class);
+
+        if (igbServiceReference != null)
+        {
+        	registerService(igbServiceReference);
+        }
+        else
+        {
+        	ServiceTracker<IGBService,Object> serviceTracker = new ServiceTracker<IGBService,Object>(bundleContext, IGBService.class.getName(), null) {
+        	    public Object addingService(ServiceReference<IGBService> igbServiceReference) {
+        	    	registerService(igbServiceReference);
+        	        return super.addingService(igbServiceReference);
+        	    }
+        	};
+        	serviceTracker.open();
+        }
+    }
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		for (ServiceRegistration<ISearchModeSym> searchModeSymmetryFilterRegistration : searchModeSymmetryFilterRegistrations) {
+			searchModeSymmetryFilterRegistration.unregister();
+		}
+	}
+}
---
+++ plugins/SearchModeSymmetryFilter/src/com/affymetrix/searchmodesymmetryfilter/SearchModeSymmetryFilter.java
@@ -0,0 +1,131 @@
+package com.affymetrix.searchmodesymmetryfilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.TypeContainerAnnot;
+import com.affymetrix.genometryImpl.filter.SearchResult;
+import com.affymetrix.genometryImpl.filter.SymmetryFilterSearchI;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genoviz.bioviews.Glyph;
+import com.affymetrix.igb.osgi.service.IGBService;
+import com.affymetrix.igb.shared.ISearchModeSym;
+import com.affymetrix.igb.shared.IStatus;
+
+public class SearchModeSymmetryFilter implements ISearchModeSym {
+	private final int searchAllOrdinal;
+	private final IGBService igbService;
+	private final SymmetryFilterSearchI filter;
+	
+	public SearchModeSymmetryFilter(IGBService igbService, SymmetryFilterSearchI filter, int searchAllOrdinal) {
+		super();
+		this.igbService = igbService;
+		this.filter = filter;
+		this.searchAllOrdinal = searchAllOrdinal;
+	}
+
+	@Override
+	public String getName() {
+		return "Filter " + filter.getName();
+	}
+
+	@Override
+	public int searchAllUse() {
+		return searchAllOrdinal;
+	}
+
+	@Override
+	public String getTooltip() {
+		return getName();
+	}
+
+	@Override
+	public String getOptionName(int i) {
+		return null;
+	}
+
+	@Override
+	public String getOptionTooltip(int i) {
+		return null;
+	}
+
+	@Override
+	public boolean getOptionEnable(int i) {
+		return false;
+	}
+
+	@Override
+	public boolean useOption() {
+		return false;
+	}
+
+	@Override
+	public void clear() {}
+
+	@Override
+	public boolean useGenomeInSeqList() {
+		return false;
+	}
+
+	@Override
+	public String checkInput(String search_text, BioSeq vseq, String seq) {
+		return filter.setParam(search_text) ? null : "Error setting param " + search_text;
+	}
+
+	@Override
+	public void finished(BioSeq vseq) {
+	}
+
+	@Override
+	public List<SeqSymmetry> search(String search_text, BioSeq chrFilter,
+			IStatus statusHolder, boolean option) {
+		List<SeqSymmetry> results = new ArrayList<SeqSymmetry>();
+		if (!search_text.equals(filter.getParam())) {
+			throw new IllegalStateException("filter value changed from " + filter.getParam() + " to " + search_text);
+		}
+		List<Glyph> glyphs = igbService.getAllTierGlyphs();
+		for (Glyph selectedTierGlyph : glyphs) {
+			Object info = selectedTierGlyph.getInfo();
+			if (info instanceof TypeContainerAnnot) {
+				List<SearchResult> searchResults = searchTrack(search_text, chrFilter,
+					(TypeContainerAnnot)info, statusHolder, option);
+				if (searchResults != null) {
+					for (SearchResult searchResult : searchResults) {
+						results.add(searchResult.getSym());
+					}
+				}
+			}
+		}
+		return results;
+	}
+
+	private List<SearchResult> searchSym(SeqSymmetry sym) {
+		List<SearchResult> searchResults = new ArrayList<SearchResult>();
+		SearchResult searchResult = filter.searchSymmetry(sym);
+		if (searchResult != null) {
+			searchResults.add(searchResult);
+		}
+		int childCount = sym.getChildCount();
+		for (int i = 0; i < childCount; i++) {
+			//if(current_thread.isInterrupted())
+			//	break;
+			
+			searchResults.addAll(searchSym(sym.getChild(i)));
+		}
+		return searchResults;
+	}
+	
+	@Override
+	public List<SearchResult> searchTrack(String search_text, BioSeq chrFilter,
+			TypeContainerAnnot trackSym, IStatus statusHolder, boolean option) {
+		if (!search_text.equals(filter.getParam())) {
+			throw new IllegalStateException("filter value changed from " + filter.getParam() + " to " + search_text);
+		}
+		return searchSym(trackSym);
+	}
+
+	@Override
+	public void valueChanged(SeqSymmetry sym) {
+	}
+}

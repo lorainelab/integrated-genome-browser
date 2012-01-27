fb68a29ee37bff8581e3d59ed7a051e5341a8e40
lfrohman <lfrohman@mycompany.com>
2012-01-27 22:33:40 +0000

    add BigWigHandler
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10019 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ plugins/BigWigHandler/src/com/gene/bigwighandler/Activator.java
@@ -0,0 +1,47 @@
+package com.gene.bigwighandler;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.affymetrix.genometryImpl.parsers.FileTypeHandler;
+import com.affymetrix.igb.osgi.service.IGBService;
+
+public class Activator implements BundleActivator {
+	private BundleContext bundleContext;
+	private ServiceRegistration<FileTypeHandler> bigwigHandlerRegistration;
+
+	private void registerServices(IGBService igbService) {
+		bigwigHandlerRegistration = bundleContext.registerService(FileTypeHandler.class, new BigWigHandler(), null);
+	}
+
+	@Override
+	public void start(BundleContext bundleContext_) throws Exception {
+		this.bundleContext = bundleContext_;
+    	ServiceReference<IGBService> igbServiceReference = bundleContext.getServiceReference(IGBService.class);
+
+        if (igbServiceReference != null)
+        {
+        	IGBService igbService = bundleContext.getService(igbServiceReference);
+        	registerServices(igbService);
+        }
+        else
+        {
+        	ServiceTracker<IGBService,Object> serviceTracker = new ServiceTracker<IGBService,Object>(bundleContext, IGBService.class, null) {
+        	    public Object addingService(ServiceReference<IGBService> igbServiceReference) {
+                	IGBService igbService = bundleContext.getService(igbServiceReference);
+                   	registerServices(igbService);
+                    return super.addingService(igbServiceReference);
+        	    }
+        	};
+        	serviceTracker.open();
+        }
+    }
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		bigwigHandlerRegistration.unregister();
+	}
+}

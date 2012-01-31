685ecd6c78a9f08d24a13fba02e49929a0dbeeed
dcnorris <dcnorris@uncc.edu>
2012-01-31 16:14:29 +0000

    Attempted fix for windows bug  "Exception in thread "AWT-EventQueue-0" java.lang.ArrayIndexOutOfBoundsException: 1 >= 1"
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@10052 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/prefs/KeyStrokesView.java
+++ igb/src/com/affymetrix/igb/prefs/KeyStrokesView.java
@@ -1,16 +1,15 @@
 /**
-*   Copyright (c) 2001-2007 Affymetrix, Inc.
-*    
-*   Licensed under the Common Public License, Version 1.0 (the "License").
-*   A copy of the license must be included with any distribution of
-*   this source code.
-*   Distributions from Affymetrix, Inc., place this in the
-*   IGB_LICENSE.html file.  
-*
-*   The license is also available at
-*   http://www.opensource.org/licenses/cpl.php
-*/
-
+ *   Copyright (c) 2001-2007 Affymetrix, Inc.
+ *    
+ *   Licensed under the Common Public License, Version 1.0 (the "License").
+ *   A copy of the license must be included with any distribution of
+ *   this source code.
+ *   Distributions from Affymetrix, Inc., place this in the
+ *   IGB_LICENSE.html file.  
+ *
+ *   The license is also available at
+ *   http://www.opensource.org/licenses/cpl.php
+ */
 package com.affymetrix.igb.prefs;
 
 import java.util.*;
@@ -27,124 +26,138 @@ import java.awt.BorderLayout;
  *  A panel that shows the preferences mapping between KeyStroke's and Actions. 
  */
 public final class KeyStrokesView extends IPrefEditorComponent implements ListSelectionListener,
-  PreferenceChangeListener {
-  private static final long serialVersionUID = 1L;
+		PreferenceChangeListener {
 
-  private final JTable table = new JTable();
+	private static final long serialVersionUID = 1L;
+	private final JTable table = new JTable();
 //  private final static String[] col_headings = {"Action", "Key Stroke", "Toolbar ?"};
-  private final static String[] col_headings = {"Action", "Key Stroke"};
-  private final DefaultTableModel model;
-  private final ListSelectionModel lsm;
-  private final TableRowSorter<DefaultTableModel> sorter;
-  KeyStrokeEditPanel edit_panel = null;
+	private final static String[] col_headings = {"Action", "Key Stroke"};
+	private final DefaultTableModel model;
+	private final ListSelectionModel lsm;
+	private final TableRowSorter<DefaultTableModel> sorter;
+	KeyStrokeEditPanel edit_panel = null;
 
-  public KeyStrokesView() {
-    super();
-    this.setName("Shortcuts");
-	this.setToolTipText("Edit Locations");
-    this.setLayout(new BorderLayout());
+	public KeyStrokesView() {
+		super();
+		this.setName("Shortcuts");
+		this.setToolTipText("Edit Locations");
+		this.setLayout(new BorderLayout());
 
-    JScrollPane scroll_pane = new JScrollPane(table);
-    this.add(scroll_pane, BorderLayout.CENTER);
+		JScrollPane scroll_pane = new JScrollPane(table);
+		this.add(scroll_pane, BorderLayout.CENTER);
 
-    model = new DefaultTableModel() {
-    	  private static final long serialVersionUID = 1L;
+		model = new DefaultTableModel() {
+
+			private static final long serialVersionUID = 1L;
+
 			@Override
-      public boolean isCellEditable(int row, int column) {return false;}
-            @Override
-      public Class<?> getColumnClass(int column) {
-        return String.class;
-      }
-    };
-    model.setDataVector(new Object[0][0], col_headings);
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
 
-    lsm = table.getSelectionModel();
-    lsm.addListSelectionListener(this);
-    lsm.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			@Override
+			public Class<?> getColumnClass(int column) {
+				return String.class;
+			}
+		};
+		model.setDataVector(new Object[0][0], col_headings);
 
-	sorter = new TableRowSorter<DefaultTableModel>(model);
+		lsm = table.getSelectionModel();
+		lsm.addListSelectionListener(this);
+		lsm.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-    table.setModel(model);
-    table.setRowSorter(sorter);
-    table.setRowSelectionAllowed(true);
-    table.setEnabled( true );
+		sorter = new TableRowSorter<DefaultTableModel>(model);
 
-    edit_panel = new KeyStrokeEditPanel();
-    edit_panel.setEnabled(false);
-    this.add("South", edit_panel);
+		table.setModel(model);
+		table.setRowSorter(sorter);
+		table.setRowSelectionAllowed(true);
+		table.setEnabled(true);
 
-    try {PreferenceUtils.getKeystrokesNode().flush();} catch (Exception e) {}
-    PreferenceUtils.getKeystrokesNode().addPreferenceChangeListener(this);
+		edit_panel = new KeyStrokeEditPanel();
+		edit_panel.setEnabled(false);
+		this.add("South", edit_panel);
 
-    showShortcuts();
-    validate();
-  }
+		try {
+			PreferenceUtils.getKeystrokesNode().flush();
+		} catch (Exception e) {
+		}
+		PreferenceUtils.getKeystrokesNode().addPreferenceChangeListener(this);
+
+		showShortcuts();
+		validate();
+	}
 
 //  private static Object[][] buildRows(Preferences keystroke_node, Preferences toolbar_node) {
-  private static Object[][] buildRows(Preferences keystroke_node) {
-    Collection<String> keys = PreferenceUtils.getKeystrokesNodeNames();
-	Object[][] rows;
+	private static Object[][] buildRows(Preferences keystroke_node) {
+		Collection<String> keys = PreferenceUtils.getKeystrokesNodeNames();
+		Object[][] rows;
 
-	synchronized (keys) {
-		int num_rows = keys.size();
+		synchronized (keys) {
+			int num_rows = keys.size();
 //		int num_cols = 3;
-		int num_cols = 2;
-		rows = new Object[num_rows][num_cols];
-		Iterator<String> iter = keys.iterator();
-		for (int i=0; iter.hasNext(); i++) {
-			String key = iter.next();
-			rows[i][0] = key;
-			rows[i][1] = keystroke_node.get(key, "");
+			int num_cols = 2;
+			rows = new Object[num_rows][num_cols];
+			Iterator<String> iter = keys.iterator();
+			for (int i = 0; iter.hasNext(); i++) {
+				String key = iter.next();
+				rows[i][0] = key;
+				rows[i][1] = keystroke_node.get(key, "");
 //			rows[i][2] = toolbar_node.getBoolean(key, false) ? "x" : "";
+			}
+		}
+		return rows;
+	}
+
+	/** Re-populates the table with the shortcut data. */
+	private void showShortcuts() {
+		Object[][] rows = null;
+//    rows = buildRows(PreferenceUtils.getKeystrokesNode(), PreferenceUtils.getToolbarNode());
+		rows = buildRows(PreferenceUtils.getKeystrokesNode());
+		model.setDataVector(rows, col_headings);
+	}
+
+	public void refresh() {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+				showShortcuts();
+				model.fireTableDataChanged();
+			}
+		});
+	}
+
+	/** This is called when the user selects a row of the table;
+	 */
+	public void valueChanged(ListSelectionEvent evt) {
+		if (evt.getSource() == lsm && !evt.getValueIsAdjusting()) {
+			int srow = table.getSelectedRow();
+			if (srow >= 0) {
+				String id = (String) table.getModel().getValueAt(srow, 0);
+				editKeystroke(id);
+			} else {
+//        edit_panel.setPreferenceKey(null, null, null, null);
+				edit_panel.setPreferenceKey(null, null, null);
+			}
 		}
 	}
-    return rows;
-  }
 
-  /** Re-populates the table with the shortcut data. */
-  private void showShortcuts() {
-    Object[][] rows = null;
-//    rows = buildRows(PreferenceUtils.getKeystrokesNode(), PreferenceUtils.getToolbarNode());
-    rows = buildRows(PreferenceUtils.getKeystrokesNode());
-    model.setDataVector(rows, col_headings);
-  }
-
-  public void refresh() {
-    showShortcuts();
-  }
-
-  /** This is called when the user selects a row of the table;
-   */
-  public void valueChanged(ListSelectionEvent evt) {
-    if (evt.getSource()==lsm && ! evt.getValueIsAdjusting()) {
-      int srow = table.getSelectedRow();
-      if (srow >= 0) {
-        String id = (String) table.getModel().getValueAt(srow, 0);
-        editKeystroke(id);
-      } else {
-//        edit_panel.setPreferenceKey(null, null, null, null);
-        edit_panel.setPreferenceKey(null, null, null);
-      }
-    }
-  }
-  
-  private void editKeystroke(String id) {
+	private void editKeystroke(String id) {
 //    edit_panel.setPreferenceKey(PreferenceUtils.getKeystrokesNode(), PreferenceUtils.getToolbarNode(), id, "");
-    edit_panel.setPreferenceKey(PreferenceUtils.getKeystrokesNode(), id, "");
-  }
+		edit_panel.setPreferenceKey(PreferenceUtils.getKeystrokesNode(), id, "");
+	}
 
-  public void preferenceChange(PreferenceChangeEvent evt) {
-    if (evt.getNode() != PreferenceUtils.getKeystrokesNode()) {
-      return;
-    }
-    // Each time a keystroke preference is changed, update the
-    // whole table.  Inelegant, but works.
-    refresh();
-  }  
+	public void preferenceChange(PreferenceChangeEvent evt) {
+		if (evt.getNode() != PreferenceUtils.getKeystrokesNode()) {
+			return;
+		}
+		// Each time a keystroke preference is changed, update the
+		// whole table.  Inelegant, but works.
+		refresh();
+	}
 
-  /*public void destroy() {
-    removeAll();
-    if (lsm != null) {lsm.removeListSelectionListener(this);}
-    PrefenceUtils.getKeystrokesNode().removePreferenceChangeListener(this);
-  }*/
+	/*public void destroy() {
+	removeAll();
+	if (lsm != null) {lsm.removeListSelectionListener(this);}
+	PrefenceUtils.getKeystrokesNode().removePreferenceChangeListener(this);
+	}*/
 }

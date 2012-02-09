63a6e46a932e85c9bbcbd5996f166f82a4dd00c2
imnick <imnick@uncc.edu>
2012-02-09 18:28:12 +0000

    Make value column editable in Properties table
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@10231 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/Bookmark/src/com/affymetrix/igb/bookmarks/BookmarkTableComponent.java
+++ plugins/Bookmark/src/com/affymetrix/igb/bookmarks/BookmarkTableComponent.java
@@ -1,9 +1,13 @@
 package com.affymetrix.igb.bookmarks;
 
+import com.affymetrix.genometryImpl.util.ErrorHandler;
 import java.awt.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.TableCellEditor;
 
 /**
@@ -14,60 +18,80 @@ import javax.swing.table.TableCellEditor;
  * Call {@link #setGUIFromBookmark(Bookmark)} to update the display.
  * Call {@link #setBookmarkFromGUI(Bookmark)} when a "submit" button is pressed.
  * @author  ed
+ * Modified by nick
  */
 public final class BookmarkTableComponent {
-  private static final boolean DEBUG = true;
 
-  private final JPanel main_box = new JPanel();
-  
-  private final BookmarkTableModel data_model;
-  private final JTable table;
-  
-  public BookmarkTableComponent() {
-    main_box.setLayout(new BorderLayout());
+	private static final boolean DEBUG = true;
+	private final JPanel main_box = new JPanel();
+	private final BookmarkTableModel data_model;
+	private final JTable table;
+	private static Bookmark bookmark;
 
-    data_model = new BookmarkTableModel();
-    table = new JTable(data_model);
+	public BookmarkTableComponent() {
+		main_box.setLayout(new BorderLayout());
 
-    JScrollPane scrollpane = new JScrollPane(table);
+		data_model = new BookmarkTableModel();
+		data_model.addTableModelListener(new TableModelListener() {
+			public void tableChanged(TableModelEvent e) {
+				setBookmarkFromPropertyGUI();
+			}
+		});
 
-    scrollpane.setMinimumSize(new java.awt.Dimension(700, 300));
+		table = new JTable(data_model);
 
-    main_box.add(scrollpane, BorderLayout.CENTER);
-  }
-  
-  public Component getComponent() {
-    return main_box;
-  }
-    
-  public void stopEditing() {
-    TableCellEditor ed = table.getCellEditor();
-    if (ed != null) {
-      ed.stopCellEditing();
-    }
-  }
+		JScrollPane scrollpane = new JScrollPane(table);
 
-  public void cancelEditing() {
-    TableCellEditor ed = table.getCellEditor();
-    if (ed != null) {
-      ed.cancelCellEditing();
-    }
-  }
-  
-  public void setGUIFromBookmark(Bookmark bm) {
-    cancelEditing(); // just in case!
-    if (bm == null) {
-      data_model.setValuesFromMap(Collections.<String,String[]>emptyMap());
-    } else {
-      URL url = bm.getURL();
-      String url_base = bm.getURL().toExternalForm();
-      int index = url_base.indexOf('?');
-      if (index > 0) {
-        url_base = url_base.substring(0, index);
-      }
+		scrollpane.setMinimumSize(new java.awt.Dimension(700, 300));
 
-      data_model.setValuesFromMap(Bookmark.parseParameters(url));
-    }
-  }  
+		main_box.add(scrollpane, BorderLayout.CENTER);
+	}
+
+	public Component getComponent() {
+		return main_box;
+	}
+
+	public void cancelEditing() {
+		TableCellEditor ed = table.getCellEditor();
+		if (ed != null) {
+			ed.cancelCellEditing();
+		}
+	}
+
+	public void setGUIFromBookmark(Bookmark bm) {
+		bookmark = bm;
+		cancelEditing(); // just in case!
+		if (bm == null) {
+			data_model.setValuesFromMap(Collections.<String, String[]>emptyMap());
+		} else {
+			URL url = bm.getURL();
+			String url_base = bm.getURL().toExternalForm();
+			int index = url_base.indexOf('?');
+			if (index > 0) {
+				url_base = url_base.substring(0, index);
+			}
+
+			data_model.setValuesFromMap(Bookmark.parseParameters(url));
+		}
+	}
+
+	public void setBookmarkFromPropertyGUI() {
+		if (bookmark != null) {
+			URL url = bookmark.getURL();
+			String url_base = bookmark.getURL().toExternalForm();
+			int index = url_base.indexOf('?');
+			if (index > 0) {
+				url_base = url_base.substring(0, index);
+			}
+
+			String str = Bookmark.constructURL(url_base, data_model.getValuesAsMap());
+			try {
+				url = new URL(str);
+				bookmark.setURL(url);
+			} catch (MalformedURLException e) {
+				JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, null);
+				ErrorHandler.errorPanel(frame, "Error", "Cannot construct bookmark: " + e.getMessage(), null);
+			}
+		}
+	}
 }
- 

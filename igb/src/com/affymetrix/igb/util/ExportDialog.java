2ebc7b1ab2690740674abd81c6e4eb4918c91cab
imnick <imnick@uncc.edu>
2012-02-09 19:14:44 +0000

    Fixed the bug - failed to export if extension removed.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@10235 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/util/ExportDialog.java
+++ igb/src/com/affymetrix/igb/util/ExportDialog.java
@@ -288,14 +288,13 @@ public class ExportDialog implements ExportConstants {
 	public boolean okButtonActionPerformed() throws IOException {
 		String previousPath = exportFile.getAbsolutePath();
 		String newPath = filePathTextField.getText();
-		String ext = ParserController.getExtension(newPath);
-
 		exportFile = new File(newPath);
 
-		if (!isValidExportFile(ext, previousPath)) {
+		if (!isValidExportFile(previousPath)) {
 			return false;
 		}
 
+		String ext = ((ExportFileType) extComboBox.getSelectedItem()).getExtension();
 		exportScreenshot(exportFile, ext);
 
 		exportNode.put(PREF_FILE, exportFile.getAbsolutePath());
@@ -326,20 +325,22 @@ public class ExportDialog implements ExportConstants {
 		}
 	}
 
-	private boolean isValidExportFile(String ext, String path) {
+	private boolean isValidExportFile(String previousPath) {
 		if (!exportFile.getParentFile().isDirectory()) {
 			// if output path is invalid, reset to previous correct path
 			ErrorHandler.errorPanel("The output path is invalid.");
-			filePathTextField.setText(path);
+			filePathTextField.setText(previousPath);
 			filePathTextField.grabFocus();
-			exportFile = new File(path);
+			exportFile = new File(previousPath);
 			return false;
 		}
 
+		String ext = ParserController.getExtension(exportFile.getAbsolutePath());
+
 		if (!isExt(ext)) {
 			// if file format is not exist, add current selected format to the end
-			String newPath = exportFile.getAbsolutePath()
-					+ ((ExportFileType) extComboBox.getSelectedItem()).getExtension();
+			ext = ((ExportFileType) extComboBox.getSelectedItem()).getExtension();
+			String newPath = exportFile.getAbsolutePath() + ext;
 
 			exportFile = new File(newPath);
 		}

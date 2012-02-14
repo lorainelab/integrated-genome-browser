ff768bec944df8cf292768dbda5b6253246e53d1
hiralv <hiralv@uncc.edu>
2012-02-14 22:02:04 +0000

    Adding not operator.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10285 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/src/com/affymetrix/genometryImpl/operator/NotOperator.java
@@ -0,0 +1,88 @@
+
+package com.affymetrix.genometryImpl.operator;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.SeqSpan;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import com.affymetrix.genometryImpl.span.SimpleSeqSpan;
+import com.affymetrix.genometryImpl.symmetry.*;
+import java.util.List;
+import java.util.Map;
+
+public class NotOperator implements Operator{
+
+	@Override
+	public String getName() {
+		return "not";
+	}
+
+	@Override
+	public SeqSymmetry operate(BioSeq seq, List<SeqSymmetry> symList) {
+		return getNot(symList, seq, true);
+	}
+
+	private static SeqSymmetry getNot(List<SeqSymmetry> syms, BioSeq seq, boolean include_ends) {
+		SeqSymmetry union = SeqSymSummarizer.getUnion(syms, seq);
+		int spanCount = union.getChildCount();
+
+		// rest of this is pretty much pulled directly from SeqUtils.inverse()
+		if (! include_ends )  {
+			if (spanCount <= 1) {  return null; }  // no gaps, no resulting inversion
+		}
+		MutableSeqSymmetry invertedSym = new SimpleSymWithProps();
+		if (include_ends) {
+			if (spanCount < 1) {
+				// no spans, so just return sym of whole range of seq
+				invertedSym.addSpan(new SimpleSeqSpan(0, seq.getLength(), seq));
+				return invertedSym;
+			}
+			else {
+				SeqSpan firstSpan = union.getChild(0).getSpan(seq);
+				if (firstSpan.getMin() > 0) {
+					SeqSymmetry beforeSym = new SingletonSeqSymmetry(0, firstSpan.getMin(), seq);
+					invertedSym.addChild(beforeSym);
+				}
+			}
+		}
+		for (int i=0; i<spanCount-1; i++) {
+			SeqSpan preSpan = union.getChild(i).getSpan(seq);
+			SeqSpan postSpan = union.getChild(i+1).getSpan(seq);
+			SeqSymmetry gapSym =
+				new SingletonSeqSymmetry(preSpan.getMax(), postSpan.getMin(), seq);
+			invertedSym.addChild(gapSym);
+		}
+		if (include_ends) {
+			SeqSpan lastSpan = union.getChild(spanCount-1).getSpan(seq);
+			if (lastSpan.getMax() < seq.getLength()) {
+				SeqSymmetry afterSym = new SingletonSeqSymmetry(lastSpan.getMax(), seq.getLength(), seq);
+				invertedSym.addChild(afterSym);
+			}
+		}
+		if (include_ends) {
+			invertedSym.addSpan(new SimpleSeqSpan(0, seq.getLength(), seq));
+		}
+		else {
+			int min = union.getChild(0).getSpan(seq).getMax();
+			int max = union.getChild(spanCount-1).getSpan(seq).getMin();
+			invertedSym.addSpan(new SimpleSeqSpan(min, max, seq));
+		}
+		return invertedSym;
+	}
+
+	public int getOperandCountMin(FileTypeCategory category) {
+		return category == FileTypeCategory.Annotation ? 1 : 0;
+	}
+
+	public int getOperandCountMax(FileTypeCategory category) {
+		return category == FileTypeCategory.Annotation ? 1 : 0;
+	}
+
+	public Map<String, Class<?>> getParameters() {
+		return null;
+	}
+
+	public boolean setParameters(Map<String, Object> obj) {
+		return false;
+	}
+	
+}

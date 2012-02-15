7f231e31b8e8790d140d52862f81e70e09b93c07
lfrohman <lfrohman@mycompany.com>
2012-02-15 17:27:20 +0000

    add RootSeqSymmetry
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10300 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/src/com/affymetrix/genometryImpl/symmetry/RootSeqSymmetry.java
@@ -0,0 +1,7 @@
+package com.affymetrix.genometryImpl.symmetry;
+
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+
+public interface RootSeqSymmetry {
+	public FileTypeCategory getCategory();
+}

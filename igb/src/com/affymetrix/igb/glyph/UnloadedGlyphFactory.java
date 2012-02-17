7b0779fe96db5c363ba7f091887244abc5b8a9be
lfrohman <lfrohman@mycompany.com>
2012-02-17 20:34:50 +0000

    merge view mode glyphs into factories
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10361 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/glyph/UnloadedGlyphFactory.java
+++ igb/src/com/affymetrix/igb/glyph/UnloadedGlyphFactory.java
@@ -1,28 +1,104 @@
 package com.affymetrix.igb.glyph;
 
+import java.awt.geom.Rectangle2D;
 import java.util.Map;
 
 import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.SeqSpan;
 import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
 import com.affymetrix.genometryImpl.parsers.FileTypeHandler;
 import com.affymetrix.genometryImpl.parsers.FileTypeHolder;
 import com.affymetrix.genometryImpl.style.ITrackStyleExtended;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SimpleMutableSeqSymmetry;
+import com.affymetrix.genometryImpl.util.SeqUtils;
+import com.affymetrix.genoviz.bioviews.Glyph;
+import com.affymetrix.genoviz.bioviews.ViewI;
+import com.affymetrix.genoviz.glyph.FillRectGlyph;
 import com.affymetrix.igb.IGB;
+import com.affymetrix.igb.shared.FasterExpandPacker;
 import com.affymetrix.igb.shared.MapViewGlyphFactoryI;
 import com.affymetrix.igb.shared.SeqMapViewExtendedI;
+import com.affymetrix.igb.shared.StyleGlyphI;
 import com.affymetrix.igb.shared.TierGlyph.Direction;
 import com.affymetrix.igb.shared.ViewModeGlyph;
 import com.affymetrix.igb.view.SeqMapView;
 
 public class UnloadedGlyphFactory implements MapViewGlyphFactoryI {
-	private SeqMapView smv;
-
 	private static final UnloadedGlyphFactory instance = new UnloadedGlyphFactory();
 	public static UnloadedGlyphFactory getInstance() {
 		return instance;
 	}
+	private SeqMapView smv;
+
+	// glyph class
+	private class UnloadedGlyph extends AbstractViewModeGlyph implements StyleGlyphI {
+
+		public UnloadedGlyph(BioSeq seq, SeqSymmetry sym, int slots, double height, ITrackStyleExtended style) {
+			super();
+			setStyle(style);
+			this.setPacker(new FasterExpandPacker());
+			if (getChildCount() <= 0) {
+				Glyph glyph;
+
+				for (int i = 0; i < slots; i++) {
+					// Add empty child.
+					glyph = new Glyph() {
+					};
+					glyph.setCoords(0, 0, 0, height);
+					addChild(glyph);
+				}
+
+				// Add middle glyphs.
+				SeqSymmetry inverse = SeqUtils.inverse(sym, seq);
+				int child_count = inverse.getChildCount();
+				//If any request was made.
+				if (child_count > 0) {
+					for (int i = 0; i < child_count; i++) {
+						SeqSymmetry child = inverse.getChild(i);
+						for (int j = 0; j < child.getSpanCount(); j++) {
+							SeqSpan ospan = child.getSpan(j);
+							if (ospan.getLength() > 1) {
+								glyph = new FillRectGlyph();
+								glyph.setCoords(ospan.getMin(), 0, ospan.getLength() - 1, 0);
+								addMiddleGlyph(glyph);
+							}
+						}
+					}
+				} else {
+					glyph = new FillRectGlyph();
+					glyph.setCoords(seq.getMin(), 0, seq.getLength() - 1, 0);
+					addMiddleGlyph(glyph);
+				}
+			}
+		}
+
+		@Override
+		public void setPreferredHeight(double height, ViewI view) {
+		}
+
+		@Override
+		public int getActualSlots() {
+			return 1;
+		}
+
+		// overriding pack to ensure that tier is always the full width of the scene
+		@Override
+		public void pack(ViewI view, boolean manual) {
+			super.pack(view, manual);
+			Rectangle2D.Double mbox = getScene().getCoordBox();
+			Rectangle2D.Double cbox = this.getCoordBox();
+			this.setCoords(mbox.x, cbox.y, mbox.width, cbox.height);
+		}
+
+		@Override
+		public void draw(ViewI view) {
+			drawMiddle(view);
+			super.draw(view);
+		}
+	}
+	// end glyph class
+
 	private UnloadedGlyphFactory() {
 		super();
 		this.smv = IGB.getSingleton().getMapView();

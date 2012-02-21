2f4b56f09f6ce042d52dbb494123b1d7b6c85d24
hiralv <hiralv@uncc.edu>
2012-02-21 19:05:42 +0000

    Creating mismatch operators.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10398 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ igb/src/com/affymetrix/igb/view/MismatchOperator.java
@@ -0,0 +1,26 @@
+
+package com.affymetrix.igb.view;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.operator.Operator;
+import com.affymetrix.genometryImpl.symmetry.SeqSymSummarizer;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import java.util.List;
+
+/**
+ *
+ * @author hiralv
+ */
+public class MismatchOperator extends AbstractMismatchOperator implements Operator {
+
+	@Override
+	public String getName() {
+		return "mismatch";
+	}
+
+	@Override
+	public SeqSymmetry getMismatch(List<SeqSymmetry> syms, BioSeq seq, boolean binary_depth, String id, int start, int end) {
+		return SeqSymSummarizer.getMismatchGraph(syms, seq, false, id, start, end, false);
+	}
+
+}
---
+++ igb/src/com/affymetrix/igb/view/MismatchPipeupOperator.java
@@ -0,0 +1,24 @@
+package com.affymetrix.igb.view;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.operator.Operator;
+import com.affymetrix.genometryImpl.symmetry.SeqSymSummarizer;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import java.util.List;
+
+/**
+ *
+ * @author hiralv
+ */
+public class MismatchPipeupOperator extends AbstractMismatchOperator implements Operator {
+
+	@Override
+	public String getName() {
+		return "mismatchpileup";
+	}
+	
+	@Override
+	public SeqSymmetry getMismatch(List<SeqSymmetry> syms, BioSeq seq, boolean binary_depth, String id, int start, int end) {
+		return SeqSymSummarizer.getMismatchGraph(syms, seq, false, id, start, end, true);
+	}
+}

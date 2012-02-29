3ced69e21579ebadd0a96745ec61c44c4ebc94eb
lfrohman <lfrohman@mycompany.com>
2012-02-29 16:23:54 +0000

    new Operators
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10513 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ genometryImpl/src/com/affymetrix/genometryImpl/operator/CopyAlignmentOperator.java
@@ -0,0 +1,69 @@
+package com.affymetrix.genometryImpl.operator;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genometryImpl.symmetry.TypeContainerAnnot;
+
+public class CopyAlignmentOperator implements Operator {
+
+	@Override
+	public String getName() {
+		return "copyalignment";
+	}
+
+	@Override
+	public SeqSymmetry operate(BioSeq aseq, List<SeqSymmetry> symList) {
+		if (symList.size() != 1 || !(symList.get(0) instanceof TypeContainerAnnot)) {
+			return null;
+		}
+		TypeContainerAnnot result = null;
+		TypeContainerAnnot t = (TypeContainerAnnot)symList.get(0);
+		result = new TypeContainerAnnot(t.getType());
+		// copy children
+		for (int i = 0; i < t.getChildCount(); i++) {
+			result.addChild(t.getChild(i));
+		}
+		// copy spans
+		for (int i = 0; i < t.getSpanCount(); i++) {
+			result.addSpan(t.getSpan(i));
+		}
+		// copy properties
+		result.setProperties(new HashMap<String,Object>(t.getProperties()));
+		return result;
+	}
+
+	@Override
+	public int getOperandCountMin(FileTypeCategory category) {
+		return category == FileTypeCategory.Alignment ? 1 : 0;
+	}
+
+	@Override
+	public int getOperandCountMax(FileTypeCategory category) {
+		return category == FileTypeCategory.Alignment ? 1 : 0;
+	}
+
+	@Override
+	public Map<String, Class<?>> getParameters() {
+		return null;
+	}
+
+	@Override
+	public boolean setParameters(Map<String, Object> obj) {
+		return false;
+	}
+
+	@Override
+	public boolean supportsTwoTrack() {
+		return false;
+	}
+
+	@Override
+	public FileTypeCategory getOutputCategory() {
+		return FileTypeCategory.Alignment;
+	}
+}
--- genometryImpl/src/com/affymetrix/genometryImpl/operator/CopySequenceOperator.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/operator/CopySequenceOperator.java
@@ -10,42 +10,21 @@ import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SimpleSymWithResidues;
 
-/**
- * operation for sequenc tracks that returns the complement base at each location
- */
-public class ComplementSequenceOperator implements Operator {
-	private static final char[] COMPLEMENT = new char[256];
-	static {
-		COMPLEMENT['A'] = 'T';
-		COMPLEMENT['T'] = 'A';
-		COMPLEMENT['C'] = 'G';
-		COMPLEMENT['G'] = 'C';
-		COMPLEMENT['N'] = 'N';
-		COMPLEMENT['a'] = 't';
-		COMPLEMENT['t'] = 'a';
-		COMPLEMENT['c'] = 'g';
-		COMPLEMENT['g'] = 'c';
-		COMPLEMENT['n'] = 'n';
-	}
+public final class CopySequenceOperator implements Operator {
 
 	@Override
 	public String getName() {
-		return "complement";
+		return "copysequence";
 	}
 
 	@Override
 	public SeqSymmetry operate(BioSeq aseq, List<SeqSymmetry> symList) {
 		SimpleSymWithResidues residueSym = (SimpleSymWithResidues)symList.get(0).getChild(0);
-		String residues = residueSym.getResidues();
-		StringBuffer complementResidues = new StringBuffer(residues.length());
-		for (int i = 0; i < residues.length(); i++) {
-			complementResidues.append(COMPLEMENT[residues.charAt(i)]);
-		}
-		return new SimpleSymWithResidues("complement:" + residueSym.getType(), residueSym.getBioSeq(), residueSym.getMin(),
+		return new SimpleSymWithResidues(residueSym.getType(), residueSym.getBioSeq(), residueSym.getMin(),
 				residueSym.getMax(), residueSym.getName(), residueSym.getScore(), residueSym.isForward(),
 				residueSym.hasCdsSpan() ? residueSym.getCdsSpan().getMin() : Integer.MIN_VALUE,
 				residueSym.hasCdsSpan() ? residueSym.getCdsSpan().getMax() : Integer.MAX_VALUE,
-				ArrayUtils.clone(residueSym.getBlockMins()), ArrayUtils.clone(residueSym.getBlockMaxs()), complementResidues.toString());
+				ArrayUtils.clone(residueSym.getBlockMins()), ArrayUtils.clone(residueSym.getBlockMaxs()), new String(residueSym.getResidues()));
 	}
 
 	@Override

8d16053ad95a3f8b4992d901893a7a2eb3500ea6
lfrohman <lfrohman@mycompany.com>
2012-03-01 15:53:14 +0000

    View Modes appear half height
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10528 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/tiers/TrackConstants.java
+++ igb/src/com/affymetrix/igb/tiers/TrackConstants.java
@@ -49,7 +49,7 @@ public interface TrackConstants {
 	static final String default_view_mode = "default";
 	static final String default_operator = "none";
 	static final int default_glyphDepth = 2;
-	static final double default_height = 25.0;
+	static final double default_height = 50.0;//25.0;
 	static final double default_y = 0.0;
 	static final float default_track_name_size = 12;
 	static final DIRECTION_TYPE default_direction_type = DIRECTION_TYPE.NONE;

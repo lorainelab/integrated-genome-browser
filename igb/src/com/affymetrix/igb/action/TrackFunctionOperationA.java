6cbfea9e36deb88b24dd43b62bf3fb2d0a65f789
hiralv <hiralv@uncc.edu>
2012-03-09 16:16:08 +0000

    Add non update able track if one of the feature is null.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10659 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/TrackFunctionOperationA.java
+++ igb/src/com/affymetrix/igb/action/TrackFunctionOperationA.java
@@ -1,7 +1,9 @@
 package com.affymetrix.igb.action;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.GenometryModel;
 import com.affymetrix.genometryImpl.event.GenericAction;
 import com.affymetrix.genometryImpl.general.GenericFeature;
@@ -9,6 +11,9 @@ import com.affymetrix.genometryImpl.general.GenericVersion;
 import com.affymetrix.genometryImpl.operator.Operator;
 import com.affymetrix.genometryImpl.style.DefaultStateProvider;
 import com.affymetrix.genometryImpl.symloader.Delegate;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genometryImpl.symmetry.SimpleSymWithProps;
+import com.affymetrix.genometryImpl.symmetry.UcscBedSym;
 import com.affymetrix.genometryImpl.util.GeneralUtils;
 import com.affymetrix.genometryImpl.util.LoadUtils;
 import com.affymetrix.genoviz.bioviews.GlyphI;
@@ -16,14 +21,16 @@ import com.affymetrix.igb.general.ServerList;
 import com.affymetrix.igb.osgi.service.SeqMapViewI;
 import com.affymetrix.igb.shared.TierGlyph;
 import com.affymetrix.igb.tiers.TrackStyle;
+import com.affymetrix.igb.util.TrackUtils;
 import com.affymetrix.igb.view.load.GeneralLoadUtils;
 import com.affymetrix.igb.view.load.GeneralLoadView;
+import com.affymetrix.igb.viewmode.MapViewModeHolder;
 
 public abstract class TrackFunctionOperationA extends GenericAction {
 	private static final long serialVersionUID = 1L;
 	protected final SeqMapViewI gviewer;
 	private final Operator operator;
-
+	
 	public TrackFunctionOperationA(SeqMapViewI gviewer, Operator operator) {
 		super();
 		this.gviewer = gviewer;
@@ -40,6 +47,11 @@ public abstract class TrackFunctionOperationA extends GenericAction {
 		for (GlyphI tier : tiers) {
 			symsStr.add(((TierGlyph)tier).getAnnotStyle().getMethodName());
 			meth.append(((TierGlyph)tier).getAnnotStyle().getTrackName()).append(", ");
+			if(((TierGlyph)tier).getAnnotStyle().getFeature() == null){
+				addNonUpdateableTier(tiers);
+				return;
+			}
+				
 			features.add(((TierGlyph)tier).getAnnotStyle().getFeature());
 		}
 
@@ -66,6 +78,39 @@ public abstract class TrackFunctionOperationA extends GenericAction {
 		GeneralLoadUtils.loadAndDisplayAnnotations(feature);
 	}
 
+	private void addNonUpdateableTier(List<? extends GlyphI> tiers){
+		BioSeq aseq = GenometryModel.getGenometryModel().getSelectedSeq();
+		TrackStyle preferredStyle = null;
+		List<SeqSymmetry> seqSymList = new ArrayList<SeqSymmetry>();
+		for (GlyphI tier : tiers) {
+			SeqSymmetry rootSym = (SeqSymmetry)tier.getInfo();
+			if (rootSym == null && tier.getChildCount() > 0) {
+				rootSym = (SeqSymmetry)tier.getChild(0).getInfo();
+			}
+			if (rootSym != null) {
+				seqSymList.add(rootSym);
+				if (rootSym instanceof SimpleSymWithProps && preferredStyle == null && ((SimpleSymWithProps)rootSym).getProperty("method") != null) {
+					preferredStyle = TrackStyle.getInstance(((SimpleSymWithProps)rootSym).getProperty("method").toString(), false);
+				}
+			}
+		}
+		SeqSymmetry result_sym = operator.operate(aseq, seqSymList);
+		if (result_sym != null) {
+			StringBuilder meth = new StringBuilder();
+			if (result_sym instanceof UcscBedSym) {
+				meth.append(((UcscBedSym)result_sym).getType());
+			}
+			else {
+				meth.append(operator.getName()).append(": ");
+				for (GlyphI tier : tiers) {
+					meth.append(((TierGlyph)tier).getAnnotStyle().getTrackName()).append(", ");
+				}
+			}
+			preferredStyle.setViewMode(MapViewModeHolder.getInstance().getDefaultFactoryFor(operator.getOutputCategory()).getName());
+			TrackUtils.getInstance().addTrack(result_sym, meth.toString(), preferredStyle);
+		}
+	}
+	
 	@Override
 	public String getText() {
 		return "";

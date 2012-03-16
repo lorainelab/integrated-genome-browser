cd0bc05087d2bc01a89083a72a82987107c10b65
lfrohman <lfrohman@mycompany.com>
2012-03-16 16:30:36 +0000

    remove comments
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10780 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/GraphAdjuster/src/com/affymetrix/igb/graph/Activator.java
+++ plugins/GraphAdjuster/src/com/affymetrix/igb/graph/Activator.java
@@ -14,34 +14,6 @@ public class Activator extends WindowActivator implements BundleActivator {
 	protected IGBTabPanel getPage(IGBService igbService) {
 		SimpleGraphTabGUI.init(igbService);
 		final SimpleGraphTabGUI simpleGraphTabGUI = SimpleGraphTabGUI.getSingleton();
-/*
-		ExtensionPointHandler<FloatTransformer> floatTransformerExtensionPoint = ExtensionPointHandler.getOrCreateExtensionPoint(bundleContext, FloatTransformer.class);
-		floatTransformerExtensionPoint.addListener(new ExtensionPointListener<FloatTransformer>() {
-			
-			@Override
-			public void removeService(FloatTransformer floatTransformer) {
-				simpleGraphTabGUI.sgt.removeFloatTransformer(floatTransformer);
-			}
-			
-			@Override
-			public void addService(FloatTransformer floatTransformer) {
-				simpleGraphTabGUI.sgt.addFloatTransformer(floatTransformer);
-			}
-		});
-		ExtensionPointHandler<GraphOperator> graphOperatorExtensionPoint = ExtensionPointHandler.getOrCreateExtensionPoint(bundleContext, GraphOperator.class);
-		graphOperatorExtensionPoint.addListener(new ExtensionPointListener<GraphOperator>() {
-
-			@Override
-			public void addService(GraphOperator graphOperator) {
-				simpleGraphTabGUI.sgt.addGraphOperator(graphOperator);
-			}
-
-			@Override
-			public void removeService(GraphOperator graphOperator) {
-				simpleGraphTabGUI.sgt.removeGraphOperator(graphOperator);
-			}
-		});
-		*/
 		ExtensionPointHandler<Operator> operatorExtensionPoint = ExtensionPointHandler.getOrCreateExtensionPoint(bundleContext, Operator.class);
 		operatorExtensionPoint.addListener(new ExtensionPointListener<Operator>() {
 

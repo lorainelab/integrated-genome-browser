986477439164618010697cd15d342ea8d0cc14c3
hiralv <hiralv@uncc.edu>
2012-03-16 16:58:48 +0000

    Minor refactoring.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10783 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/TrackFunctionOperationA.java
+++ igb/src/com/affymetrix/igb/action/TrackFunctionOperationA.java
@@ -7,17 +7,23 @@ import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.GenometryModel;
 import com.affymetrix.genometryImpl.event.GenericAction;
 import com.affymetrix.genometryImpl.general.GenericFeature;
+import com.affymetrix.genometryImpl.general.GenericVersion;
 import com.affymetrix.genometryImpl.operator.Operator;
+import com.affymetrix.genometryImpl.symloader.Delegate;
 import com.affymetrix.genometryImpl.symloader.Delegate.DelegateParent;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SimpleSymWithProps;
 import com.affymetrix.genometryImpl.symmetry.UcscBedSym;
+import com.affymetrix.genometryImpl.util.GeneralUtils;
+import com.affymetrix.genometryImpl.util.LoadUtils;
 import com.affymetrix.genoviz.bioviews.GlyphI;
+import com.affymetrix.igb.general.ServerList;
 import com.affymetrix.igb.osgi.service.SeqMapViewI;
 import com.affymetrix.igb.shared.TierGlyph;
 import com.affymetrix.igb.tiers.TrackStyle;
 import com.affymetrix.igb.shared.TrackUtils;
 import com.affymetrix.igb.view.load.GeneralLoadUtils;
+import com.affymetrix.igb.view.load.GeneralLoadView;
 import com.affymetrix.igb.viewmode.MapViewModeHolder;
 
 public abstract class TrackFunctionOperationA extends GenericAction {
@@ -33,26 +39,31 @@ public abstract class TrackFunctionOperationA extends GenericAction {
 
 	protected void addTier(List<? extends GlyphI> tiers) {
 		java.util.List<DelegateParent> dps = new java.util.ArrayList<DelegateParent>();
-		StringBuilder meth = new StringBuilder();
-		meth.append(operator.getName()).append(": ");
-
+		
 		for (GlyphI tier : tiers) {
-			if(((TierGlyph)tier).getAnnotStyle().getFeature() == null
-					|| ((TierGlyph)tier).getAnnotStyle().getOperator() != null){
+			if(((TierGlyph)tier).getAnnotStyle().getFeature() == null){
 				addNonUpdateableTier(tiers);
 				return;
 			}
 			
-			meth.append(((TierGlyph)tier).getAnnotStyle().getTrackName()).append(", ");
 			dps.add(new DelegateParent(((TierGlyph)tier).getAnnotStyle().getMethodName(), 
 					isForward((TierGlyph)tier), ((TierGlyph)tier).getAnnotStyle().getFeature()));
 			
 		}
 
-		GenericFeature feature = TrackUtils.getInstance().createFeature(meth.toString(), operator, dps);
+		GenericFeature feature = createFeature(getMethod(tiers), operator, dps);
 		GeneralLoadUtils.loadAndDisplayAnnotations(feature);
 	}
 
+	protected String getMethod(List<? extends GlyphI> tiers) {
+		StringBuilder meth = new StringBuilder();
+		meth.append(operator.getName()).append(": ");
+		for (GlyphI tier : tiers) {			
+			meth.append(((TierGlyph)tier).getAnnotStyle().getTrackName()).append(", ");
+		}
+		return meth.toString();
+	}
+	
 	private void addNonUpdateableTier(List<? extends GlyphI> tiers){
 		BioSeq aseq = GenometryModel.getGenometryModel().getSelectedSeq();
 		TrackStyle preferredStyle = null;
@@ -103,4 +114,21 @@ public abstract class TrackFunctionOperationA extends GenericAction {
 		return tier.getDirection() == TierGlyph.Direction.FORWARD;
 	}
 	
+	public GenericFeature createFeature(String featureName, Operator operator, List<Delegate.DelegateParent> dps) {
+		String method = GeneralUtils.URLEncode(featureName);	
+		method = "file:/"+TrackStyle.getUniqueName(method);
+		
+		GenericVersion version = GeneralLoadUtils.getIGBFilesVersion(GenometryModel.getGenometryModel().getSelectedSeqGroup(), GeneralLoadView.getLoadView().getSelectedSpecies());
+		java.net.URI uri = java.net.URI.create(method);
+		
+		GenericFeature feature = new GenericFeature(featureName, null, version, new Delegate(uri, featureName, version, operator, dps), null, false);
+		version.addFeature(feature);
+		feature.setVisible(); // this should be automatically checked in the feature tree
+		
+		ServerList.getServerInstance().fireServerInitEvent(ServerList.getServerInstance().getIGBFilesServer(), LoadUtils.ServerStatus.Initialized, true, true);
+		
+		GeneralLoadView.getLoadView().createFeaturesTable();
+		
+		return feature;
+	}
 }

63c7d7439187b5838a3660ac3b2c4c1ca5bf82ae
hiralv <hiralv@uncc.edu>
2012-03-26 15:02:48 +0000

    Creating abstract export file action.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@10871 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/action/ExportFileAction.java
+++ igb/src/com/affymetrix/igb/action/ExportFileAction.java
@@ -1,96 +1,26 @@
 package com.affymetrix.igb.action;
 
-import java.awt.event.ActionEvent;
-import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.swing.JFileChooser;
 
 import com.affymetrix.genometryImpl.BioSeq;
-import com.affymetrix.genometryImpl.GenometryModel;
-import com.affymetrix.genometryImpl.event.GenericAction;
-import com.affymetrix.genometryImpl.parsers.AlignmentFileExporter;
-import com.affymetrix.genometryImpl.parsers.AnnotationFileExporter;
 import com.affymetrix.genometryImpl.parsers.FileExporterI;
-import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
-import com.affymetrix.genometryImpl.parsers.FileTypeHandler;
-import com.affymetrix.genometryImpl.parsers.FileTypeHolder;
-import com.affymetrix.genometryImpl.parsers.GraphFileExporter;
-import com.affymetrix.genometryImpl.parsers.SequenceFileExporter;
-import com.affymetrix.genometryImpl.symmetry.RootSeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
-import com.affymetrix.genometryImpl.util.ErrorHandler;
-import com.affymetrix.genometryImpl.util.GeneralUtils;
-import com.affymetrix.genometryImpl.util.UniFileChooser;
-import com.affymetrix.genoviz.bioviews.Glyph;
-import com.affymetrix.igb.IGBServiceImpl;
-import com.affymetrix.igb.shared.FileTracker;
 import com.affymetrix.igb.shared.TierGlyph;
 
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
 
-public class ExportFileAction extends GenericAction {
-	private static final long serialVersionUID = 1l;
-	private static final GenometryModel gmodel = GenometryModel.getGenometryModel();
+public class ExportFileAction extends AbstractExportFileAction{
 	private static final ExportFileAction ACTION = new ExportFileAction();
-	private static final Map<FileTypeCategory, FileExporterI> fileExporters = new HashMap<FileTypeCategory, FileExporterI>();
-	static {
-		fileExporters.put(FileTypeCategory.Annotation, new AnnotationFileExporter());
-		fileExporters.put(FileTypeCategory.Alignment, new AlignmentFileExporter());
-		fileExporters.put(FileTypeCategory.Graph, new GraphFileExporter());
-		fileExporters.put(FileTypeCategory.Sequence, new SequenceFileExporter());
-//		fileExporters.put(FileTypeCategory.Variant, null); // vcf ???
-//		fileExporters.put(FileTypeCategory.Mismatch, new MismatchFileExporter()); // wait until tally format is finalized
-	}
+	
 	public static ExportFileAction getAction() {
 		return ACTION;
 	}
-
-	public void actionPerformed(ActionEvent e) {
-		super.actionPerformed(e);
-		List<Glyph> current_tiers = IGBServiceImpl.getInstance().getSelectedTierGlyphs();
-		if (current_tiers.size() > 1) {
-			ErrorHandler.errorPanel(BUNDLE.getString("multTrackError"));
-		}
-		else if (current_tiers.size() == 0) {
-			ErrorHandler.errorPanel(BUNDLE.getString("noTrackError"));
-		}
-		else {
-			TierGlyph current_tier = (TierGlyph)current_tiers.get(0);
-			saveAsFile(current_tier);
-		}
+				
+	@Override
+	protected void exportFile(FileExporterI fileExporter, DataOutputStream dos, BioSeq aseq, TierGlyph atier) throws java.io.IOException{
+		fileExporter.exportFile(dos, (SeqSymmetry)atier.getViewModeGlyph().getInfo(), aseq);
 	}
-
-	private void saveAsFile(TierGlyph atier) {
-		RootSeqSymmetry rootSym = (RootSeqSymmetry)atier.getViewModeGlyph().getInfo();
-		FileExporterI fileExporter = fileExporters.get(rootSym.getCategory());
-		String extension = fileExporter.getFileExtension();
-		FileTypeHandler fth = FileTypeHolder.getInstance().getFileTypeHandler(extension);
-		JFileChooser chooser = UniFileChooser.getFileChooser(fth.getName(), extension);
-		chooser.setCurrentDirectory(FileTracker.DATA_DIR_TRACKER.getFile());
-
-		int option = chooser.showSaveDialog(null);
-		if (option == JFileChooser.APPROVE_OPTION) {
-			FileTracker.DATA_DIR_TRACKER.setFile(chooser.getCurrentDirectory());
-			BioSeq aseq = gmodel.getSelectedSeq();
-			DataOutputStream dos = null;
-			try {
-				File fil = chooser.getSelectedFile();
-				dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(fil)));
-				fileExporter.exportFile(dos, (SeqSymmetry)rootSym, aseq);
-			} catch (Exception ex) {
-				ErrorHandler.errorPanel("Problem saving file", ex);
-			} finally {
-				GeneralUtils.safeClose(dos);
-			}
-		}
-	}
-
+	
 	@Override
 	public String getText() {
 		return BUNDLE.getString("saveTrackAction");

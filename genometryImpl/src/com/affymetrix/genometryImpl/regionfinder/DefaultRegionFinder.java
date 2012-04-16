ca39ecd7aee446db7f50435cf835b63bc4d6b50f
hiralv <hiralv@uncc.edu>
2012-04-16 21:10:45 +0000

    Modify default region to improve speed.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11120 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/regionfinder/DefaultRegionFinder.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/regionfinder/DefaultRegionFinder.java
@@ -7,6 +7,7 @@ import com.affymetrix.genometryImpl.symmetry.MutableSeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SimpleMutableSeqSymmetry;
 import com.affymetrix.genometryImpl.util.SeqUtils;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,8 +17,17 @@ import java.util.List;
 public class DefaultRegionFinder implements RegionFinder{
 
 	public SeqSpan findInterestingRegion(BioSeq aseq, List<SeqSymmetry> syms) {
+		List<SeqSymmetry> less_syms  = new ArrayList<SeqSymmetry>();
+		if(syms.size() > 100){
+			for(int i=0; i<100; i++){
+				less_syms.add(syms.get(i));
+			}
+		}else{
+			less_syms = syms;
+		}
+		
 		MutableSeqSymmetry resultSym = new SimpleMutableSeqSymmetry();
-		SeqUtils.union(syms, resultSym, aseq);
+		SeqUtils.union(less_syms, resultSym, aseq);
 		return resultSym.getSpan(aseq);
 	}
 	

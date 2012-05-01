01047b32c93a59c8c674574154b449e5607656e4
hiralv <hiralv@uncc.edu>
2012-05-01 20:14:44 +0000

    Paint background/foreground color.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11351 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/shared/JRPColoredButton.java
+++ igb/src/com/affymetrix/igb/shared/JRPColoredButton.java
@@ -2,6 +2,7 @@ package com.affymetrix.igb.shared;
 
 import com.affymetrix.genoviz.swing.recordplayback.JRPButton;
 import com.affymetrix.igb.action.ChangeColorActionA;
+import java.awt.Graphics;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -16,8 +17,14 @@ public class JRPColoredButton extends JRPButton implements ListSelectionListener
 	}
 
 	public void valueChanged(ListSelectionEvent e) {
-		this.setBackground(((ChangeColorActionA) this.getAction()).getColor());
-		this.repaint();
+		setBackground(((ChangeColorActionA)getAction()).getColor());
+		repaint();
 	}
 
+	@Override
+	public void paint(Graphics g){
+		super.paint(g);
+		g.setColor(getBackground());
+		g.fillRect(2, 2, getSize().width-4, getSize().height-4);
+	}
 }

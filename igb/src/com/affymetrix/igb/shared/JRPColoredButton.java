d5e8c22682514edd4f57e9bb203b92da73206523
hiralv <hiralv@uncc.edu>
2012-05-02 14:53:14 +0000

    Draw border for letter.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11363 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/shared/JRPColoredButton.java
+++ igb/src/com/affymetrix/igb/shared/JRPColoredButton.java
@@ -2,6 +2,7 @@ package com.affymetrix.igb.shared;
 
 import com.affymetrix.genoviz.swing.recordplayback.JRPButton;
 import com.affymetrix.igb.action.ChangeColorActionA;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.event.ListSelectionEvent;
@@ -12,6 +13,8 @@ import javax.swing.event.ListSelectionListener;
  * @author hiralv
  */
 public class JRPColoredButton extends JRPButton implements ListSelectionListener{
+	private static final Font background_font = new Font(Font.MONOSPACED, Font.BOLD, 34);
+	private static final Font foreground_font = new Font(Font.MONOSPACED, Font.BOLD, 28);
 	
 	public JRPColoredButton(String id, ChangeColorActionA a) {
 		super(id, a);
@@ -32,8 +35,13 @@ public class JRPColoredButton extends JRPButton implements ListSelectionListener
 		g.fillRect(2, 2, getSize().width-4, getSize().height-4);
 		
 		// Draw Foreground
-		g.setFont(new Font(Font.MONOSPACED, Font.BOLD, 34));
-		g.setColor(getForeground());
+		g.setFont(background_font);
+		g.setColor(Color.BLACK);
 		g.drawString("A", 6, 22);
+		
+		// Draw Foreground
+		g.setFont(foreground_font);
+		g.setColor(getForeground());
+		g.drawString("A", 8, 20);
 	}
 }

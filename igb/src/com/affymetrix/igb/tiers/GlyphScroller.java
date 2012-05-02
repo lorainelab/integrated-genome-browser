d04f46741e4edcac4a938d2f2c9f44675c4160f7
hiralv <hiralv@uncc.edu>
2012-05-02 14:56:45 +0000

    Show scroll window.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11365 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/tiers/GlyphScroller.java
+++ igb/src/com/affymetrix/igb/tiers/GlyphScroller.java
@@ -1,10 +1,17 @@
-
 package com.affymetrix.igb.tiers;
 
+import com.affymetrix.igb.Application;
 import com.affymetrix.igb.shared.ScrollableViewModeGlyph;
 import com.affymetrix.igb.shared.TierGlyph;
+import com.affymetrix.igb.shared.TierGlyph.Direction;
+import java.awt.Dimension;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import javax.swing.JComponent;
+import javax.swing.JScrollBar;
+import javax.swing.JWindow;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -13,10 +20,16 @@ import javax.swing.event.ListSelectionListener;
  * @author hiralv
  */
 public class GlyphScroller {
+	private static final int SCROLLBAR_WIDTH = 12;
+	private static final int WINDOW_OFFSET = 4;
 	private static final int SPEED = 10;
-	MouseWheelAndListListener listener;
+	
+	JWindow scroll_window;
+	JScrollBar scrollbar;
+	Listeners listener;
+	
 	AffyLabelledTierMap map;
-	ScrollableViewModeGlyph ag;
+	ScrollableViewModeGlyph svmg;
 	
 	public GlyphScroller(AffyLabelledTierMap map){
 		this.map = map;
@@ -28,35 +41,80 @@ public class GlyphScroller {
 			return;
 		}
 		
-		listener = new MouseWheelAndListListener();
-		ag = (ScrollableViewModeGlyph)tier.getViewModeGlyph();
-		
+		// Initialize all variables
+		svmg = (ScrollableViewModeGlyph)tier.getViewModeGlyph();
+		scrollbar = getScrollBar(tier, svmg.getOffset());
+		scroll_window = getWindow(scrollbar);
+
 		// Add listeners
+		listener = new Listeners();
 		map.getLabelMap().getNeoCanvas().addMouseWheelListener(listener);
 		map.addListSelectionListener(listener);
+		scrollbar.addAdjustmentListener(listener);
+		
+		// Set scroll window properties
+		scroll_window.setPreferredSize(new Dimension(SCROLLBAR_WIDTH, tier.getPixelBox(map.getView()).height - WINDOW_OFFSET));
+		scroll_window.setLocation(map.getLocationOnScreen().x + Math.min(0, tier.getPixelBox(map.getView()).x) + WINDOW_OFFSET, 
+				map.getLocationOnScreen().y + tier.getPixelBox(map.getView()).y + WINDOW_OFFSET);
+		scroll_window.pack();
+		scroll_window.setVisible(true);
 	}
 	
 	private void scroll(int i){
-		ag.setOffset(ag.getOffset() + (i * SPEED));
+		svmg.setOffset(i);
 		map.updateWidget(true);
 	}
 	
 	private void stopscroll(){
 		map.getLabelMap().getNeoCanvas().removeMouseWheelListener(listener);
 		map.removeListSelectionListener(listener);
+		scrollbar.removeAdjustmentListener(listener);
+		
+		// Dispose window
+		scroll_window.dispose();
 		
 		// Helps with garbage collection
-		ag = null;
+		scrollbar = null;
+		svmg = null;
 		map = null;
 		listener = null;
 	}
 	
-	private class MouseWheelAndListListener implements MouseWheelListener, ListSelectionListener {
+	private static JWindow getWindow(JComponent... components){
+		JWindow window = new JWindow(Application.getSingleton().getFrame());
+		for(int i=0; i<components.length; i++){
+			window.add(components[i]);
+		}
+		
+		return window;
+	}
+	
+	private static JScrollBar getScrollBar(TierGlyph tier, int sb_curr){
+		int style_height = (int) tier.getViewModeGlyph().getChildHeight() * tier.getActualSlots()  + 75;
+		if(tier.getDirection() != Direction.REVERSE){
+			style_height *= -1;
+		}
+		
+		int sb_min = Math.min(1, style_height);
+		int sb_max = Math.max(1, style_height);
+		sb_max = Math.max(sb_curr, sb_max);
+		
+		return new JScrollBar(JScrollBar.VERTICAL, sb_curr, 0, sb_min, sb_max);
+	}
+	
+	private class Listeners implements MouseWheelListener, ListSelectionListener, AdjustmentListener {
 
+		@Override
 		public void mouseWheelMoved(MouseWheelEvent e) {
-			scroll(e.getWheelRotation() * e.getScrollAmount());
+			scrollbar.setValue(svmg.getOffset() + (e.getWheelRotation() * e.getScrollAmount() * SPEED));
 		}
 
+		@Override
+		public void adjustmentValueChanged(AdjustmentEvent e) {
+			scroll(e.getValue());
+		}
+			
+		@Override
 		public void valueChanged(ListSelectionEvent e) {
 			stopscroll();
 		}

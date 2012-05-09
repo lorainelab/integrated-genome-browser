719db9d5ab5c723438e460754a3b8776061b1ae1
lfrohman <lfrohman@mycompany.com>
2012-05-09 23:50:42 +0000

    job management
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11489 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/view/StatusBar.java
+++ igb/src/com/affymetrix/igb/view/StatusBar.java
@@ -1,6 +1,7 @@
 package com.affymetrix.igb.view;
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,14 +17,18 @@ import com.affymetrix.genometryImpl.event.GenericAction;
 import com.affymetrix.genometryImpl.thread.CThreadEvent;
 import com.affymetrix.genometryImpl.thread.CThreadHolder;
 import com.affymetrix.genometryImpl.thread.CThreadListener;
+import com.affymetrix.genometryImpl.thread.CThreadWorker;
 import com.affymetrix.genometryImpl.util.DisplaysError;
 import com.affymetrix.genoviz.swing.recordplayback.JRPButton;
 import com.affymetrix.igb.action.CancelAllAction;
 import com.affymetrix.igb.action.ThreadHandlerAction;
 import java.awt.Color;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.logging.Level;
 
 public final class StatusBar extends JPanel implements DisplaysError, CThreadListener {
@@ -38,7 +43,7 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
 	private final MemoryStatusBarItem memory_item;
 	private final JRPButton mainCancel,stopAction;
 	private final JButton updateAvailable;
-	private final JProgressBar progressBar;
+	private final JPanel progressPanel;
 	public static Timer timer;
 		
 	public StatusBar() {
@@ -47,7 +52,7 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
 		final String update = "Update";
 		
 		status_ta = new JLabel("");
-		progressBar = new JProgressBar();
+		progressPanel = new JPanel();
 		memory_item = new MemoryStatusBarItem();
 		memory_item.setShowMaxMemory(true);
 		updateAvailable = new JButton(alertIcon);
@@ -62,7 +67,7 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
 		mainCancel.setHideActionText(true);
 //		ThreadHandler.getThreadHandler().addPopupHandler(mainCancel);
 		CThreadHolder.getInstance().addListener(this);
-		progressBar.addMouseListener(
+		progressPanel.addMouseListener(
 			new MouseAdapter() {
 			    public void mouseClicked(MouseEvent e) {
 			    	ThreadHandlerAction.getAction().actionPerformed(null);
@@ -71,10 +76,10 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
 		);
 		
 		status_ta.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
-		progressBar.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+		progressPanel.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
 	
 		status_ta.setToolTipText(tt_status);
-		progressBar.setMaximumSize(new Dimension(150, 5));
+		progressPanel.setMaximumSize(new Dimension(150, 5));
 		displayProgress(false);
 //		progressBar.setVisible(false);
 		
@@ -94,7 +99,7 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
 				
 		layout.setHorizontalGroup(layout.createSequentialGroup()
 				.addComponent(mainCancel)
-				.addComponent(progressBar)
+				.addComponent(progressPanel)
 				.addComponent(stopAction)
 				.addComponent(status_ta)
 				.addGap(1, 1, Short.MAX_VALUE)
@@ -103,7 +108,7 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
 
 		layout.setVerticalGroup(layout.createParallelGroup(Alignment.CENTER)
 				.addComponent(mainCancel)
-				.addComponent(progressBar)
+				.addComponent(progressPanel)
 				.addComponent(stopAction)
 				.addComponent(status_ta)
 				.addGap(1, 1, Short.MAX_VALUE)
@@ -127,9 +132,9 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
 
 	public void displayProgress(boolean b) {
 		mainCancel.setVisible(b);
-		progressBar.setVisible(b);
+		progressPanel.setVisible(b);
 //		progressBar.setEnabled(b);
-		progressBar.setIndeterminate(b);
+//		progressBar.setIndeterminate(b);
 	}
 
 	public String getStatus() {
@@ -164,8 +169,46 @@ public final class StatusBar extends JPanel implements DisplaysError, CThreadLis
         }, 5000, 5000);
 	}
 
+	private static final int MAX_PROGRESS_BARS = 5;
+	private void rebuildProgress(int workerCount) {
+		int barCount = Math.min(workerCount, MAX_PROGRESS_BARS);
+		progressPanel.removeAll();
+		GridLayout progressLayout = new GridLayout(1, barCount);
+		progressPanel.setLayout(progressLayout);
+		int width = progressPanel.getWidth() / barCount;
+		int count = 0;
+		for (CThreadWorker<?, ?> worker : new CopyOnWriteArraySet<CThreadWorker<?, ?>>(CThreadHolder.getInstance().getAllCThreadWorkers())) {
+			if (count > barCount) {
+				break;
+			}
+			final JProgressBar progressBar = new JProgressBar(0, 100);
+			progressBar.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+			progressBar.setMaximumSize(new Dimension(width, 25));
+			worker.addPropertyChangeListener(
+			     new PropertyChangeListener() {
+			         public void propertyChange(PropertyChangeEvent evt) {
+			             if ("progress".equals(evt.getPropertyName())) {
+			         		if (progressPanel.isShowing()) {
+			        			progressBar.setValue((Integer)evt.getNewValue());
+			                	progressBar.repaint();
+			        		}
+			             }
+			         }
+			     }
+			);
+			progressPanel.add(progressBar);
+        	progressBar.repaint();
+			count++;
+		}
+		progressPanel.validate();
+	}
+
 	@Override
 	public void heardThreadEvent(CThreadEvent cte) {
-		displayProgress(CThreadHolder.getInstance().getCThreadWorkerCount() > 0);
+		int workerCount = CThreadHolder.getInstance().getCThreadWorkerCount();
+		displayProgress(workerCount > 0);
+		if (workerCount > 0) {
+			rebuildProgress(workerCount);
+		}
 	}
 }

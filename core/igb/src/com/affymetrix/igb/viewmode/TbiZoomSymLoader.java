073f0dea9e282f0f367a8cf68fba0cb2d2510825
lfrohman <lfrohman@mycompany.com>
2012-05-17 20:39:31 +0000

    clean up code
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11608 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/viewmode/TbiZoomSymLoader.java
+++ core/igb/src/com/affymetrix/igb/viewmode/TbiZoomSymLoader.java
@@ -16,11 +16,9 @@ import java.util.logging.Logger;
 
 import org.broad.tribble.source.tabix.TabixReader;
 
-
 import com.affymetrix.genometryImpl.AnnotatedSeqGroup;
 import com.affymetrix.genometryImpl.parsers.FileTypeHolder;
 import com.affymetrix.genometryImpl.symloader.SymLoader;
-import com.affymetrix.genometryImpl.symloader.SymLoaderTabix;
 import com.affymetrix.genometryImpl.util.GeneralUtils;
 
 public class TbiZoomSymLoader extends IndexZoomSymLoader {
@@ -30,18 +28,18 @@ public class TbiZoomSymLoader extends IndexZoomSymLoader {
 		super(uri, featureName, group);
 	}
 
-	private URI getFileURI(URI baiUri) throws Exception {
-		String bamUriString = baiUri.toString().substring(0, baiUri.toString().length() - ".tbi".length());
-		if (!bamUriString.startsWith("file:") && !bamUriString.startsWith("http:") && !bamUriString.startsWith("https:") && !bamUriString.startsWith("ftp:")) {
-			bamUriString = GeneralUtils.getFileScheme() + bamUriString;
+	private URI getFileURI(URI tbiUri) throws Exception {
+		String baseUriString = tbiUri.toString().substring(0, tbiUri.toString().length() - ".tbi".length());
+		if (!baseUriString.startsWith("file:") && !baseUriString.startsWith("http:") && !baseUriString.startsWith("https:") && !baseUriString.startsWith("ftp:")) {
+			baseUriString = GeneralUtils.getFileScheme() + baseUriString;
 		}
-		return new URI(bamUriString);
+		return new URI(baseUriString);
 	}
 
 	@Override
 	protected SymLoader getDataFileSymLoader() throws Exception {
-		URI fileUri = getFileURI(uri);
-		return FileTypeHolder.getInstance().getFileTypeHandlerForURI(fileUri.toString()).createSymLoader(fileUri, featureName, group);
+		URI baseUri = getFileURI(uri);
+		return FileTypeHolder.getInstance().getFileTypeHandlerForURI(baseUri.toString()).createSymLoader(baseUri, featureName, group);
 	}
 
     @Override
@@ -50,16 +48,13 @@ public class TbiZoomSymLoader extends IndexZoomSymLoader {
 			return;
 		}
 		try {
-			String uriString = uri.toString();
-			if (uriString.startsWith(FILE_PREFIX)) {
-				uriString = GeneralUtils.fixFileName(uriString);
-			}
+			String uriString = GeneralUtils.fixFileName(uri.toString());
 			uriString = uriString.toString().substring(0, uriString.toString().length() - ".tbi".length());
 			tabixReader = new TabixReader(uriString);
 			tabixReader.readIndex();
 		}
 		catch (Exception x) {
-			Logger.getLogger(SymLoaderTabix.class.getName()).log(Level.SEVERE,
+			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,
 						"Could not read tabix for {0}.",
 						new Object[]{featureName});
 			return;
@@ -103,9 +98,9 @@ public class TbiZoomSymLoader extends IndexZoomSymLoader {
 		};
 	}
 
-	private java.util.Iterator<Map<Integer, List<List<Long>>>> getBinIterator(final HashMap<Integer, Object[]> bins) {
-		return new java.util.Iterator<Map<Integer, List<List<Long>>>>() {
-			java.util.Iterator<Integer> binIter = bins.keySet().iterator();
+	private Iterator<Map<Integer, List<List<Long>>>> getBinIterator(final HashMap<Integer, Object[]> bins) {
+		return new Iterator<Map<Integer, List<List<Long>>>>() {
+			Iterator<Integer> binIter = bins.keySet().iterator();
 			@Override
 			public boolean hasNext() {
 				return binIter.hasNext();
@@ -115,9 +110,9 @@ public class TbiZoomSymLoader extends IndexZoomSymLoader {
 				final Integer binNo = binIter.next();
 				return new AbstractMap<Integer, List<List<Long>>>() {
 					@Override
-					public Set<java.util.Map.Entry<Integer, List<List<Long>>>> entrySet() {
-						Set<java.util.Map.Entry<Integer, List<List<Long>>>> entrySet =
-							new HashSet<java.util.Map.Entry<Integer, List<List<Long>>>>();
+					public Set<Map.Entry<Integer, List<List<Long>>>> entrySet() {
+						Set<Map.Entry<Integer, List<List<Long>>>> entrySet =
+							new HashSet<Map.Entry<Integer, List<List<Long>>>>();
 						final Object[] chunks = bins.get(binNo);
 						entrySet.add(new SimpleEntry<Integer, List<List<Long>>>(binNo, getChunkList(chunks)));
 						return entrySet;
@@ -151,7 +146,7 @@ public class TbiZoomSymLoader extends IndexZoomSymLoader {
 		return -1;
 	}
 
-	private java.util.Iterator<Map<Integer, List<List<Long>>>> getBinIter(Map<String, String> synonymMap, String seq) {
+	private Iterator<Map<Integer, List<List<Long>>>> getBinIter(Map<String, String> synonymMap, String seq) {
 		int refno = getRefNo(synonymMap, seq.toString());
 		if (refno != -1) {
 			try {

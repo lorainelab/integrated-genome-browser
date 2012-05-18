f36e4c0588e67959021955947bb6dbda3a0d2e87
lfrohman <lfrohman@mycompany.com>
2012-05-18 16:36:46 +0000

    comments
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11617 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/operator/Operator.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/operator/Operator.java
@@ -7,11 +7,17 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * An abstraction for generating a SeqSymetry from a list of others.
+ * An abstraction for performing an operation on
+ * one or more SeqSymmetries to create a new SeqSymetry.
+ * input type (and quantity) and output are specified by
+ * FileTypeCategory. Parameters for the Operator are optional,
+ * as well as ordering (for the UI).
  */
 public interface Operator  {
 
 	/**
+	 * Unique identifier for the Operator, MUST be unique,
+	 * and should not be displayed to the users, use getDisplay().
 	 * This should help you keep track of different operators.
 	 * Note that this should be different for each instance.
 	 * @return a name suitable for identifying this operator.
@@ -19,24 +25,39 @@ public interface Operator  {
 	public String getName();
 
 	/**
+	 * user display
 	 * @return a string suitable for showing a user.
 	 */
 	public String getDisplay();
 
 	/**
-	 * Carry out the operation.
-	 * @param aseq What is this for?
+	 * Carry out the operation, only on the specified BioSeq.
+	 * @param aseq - BioSeq - necessary because some SeqSymmetries
+	 *   can refer to multiple BioSeqs.
 	 * @param symList symmetries used as input.
-	 * @return a new symmetry.
+	 * @return output, a new symmetry.
 	 */
 	public SeqSymmetry operate(BioSeq aseq, List<SeqSymmetry> symList);
 
-	/** @return the smallest size list allowed. */
+	/**
+	 * specify the minimum number of SeqSymmetries of the specified
+	 * FileTypeCategory, can be zero
+	 * @return the smallest size list allowed.
+	 */
 	public int getOperandCountMin(FileTypeCategory category);
 
-	/** @return the largest size list allowed. */
+	/**
+	 * specify the maximum number of SeqSymmetries of the specified
+	 * FileTypeCategory, can be zero
+	 * @return the largest size list allowed.
+	 */
 	public int getOperandCountMax(FileTypeCategory category);
 
+	/**
+	 * Some operators can take parameters for different
+	 * instances of the same Operator class.
+	 * @return parameters for this Operator
+	 */
 	public Map<String, Class<?>> getParameters();
 
 	/**
@@ -46,18 +67,24 @@ public interface Operator  {
 	 */
 	public boolean setParameters( Map<String, Object> parms);
 
-	/** Indicates whether or not forward and reverse tracks are supported. */
+	/**
+	 * Indicates whether or not forward and reverse tracks are supported.
+	 * @return true if for/rev supported, false otherwise
+	 */
 	public boolean supportsTwoTrack();
 
 	/**
+	 * specify the FileTypeCategory of the result SeqSymmetry
 	 * @return the file type category appropriate for the BioSeq
 	 * returned by {@link #operate(BioSeq, List<SeqSymetry>)).
 	 */
 	public FileTypeCategory getOutputCategory();
 
-	/** What is this for? */
+	/**
+	 * optional interface for Operators, used to order
+	 * the list of Operators in the UI
+	 */
 	public static interface Order{
 		public int getOrder();
 	}
-
 }

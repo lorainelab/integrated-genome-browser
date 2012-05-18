b074fbf502f08ea0e17d6650ff9511935ab2e108
hiralv <hiralv@uncc.edu>
2012-05-18 16:04:46 +0000

    Adding some constants to show load info.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@11614 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/general/GenericFeature.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/general/GenericFeature.java
@@ -37,6 +37,10 @@ import java.util.logging.Logger;
  */
 public final class GenericFeature {
 
+	public static final String howtoloadmsg = "To display your data, zoom in and click Load Data.";
+	public static final String show_how_to_load = "Show how to load";
+	public static final boolean default_show_how_to_load = true;
+
 	public final String featureName;      // friendly name of the feature.
 	public final Map<String, String> featureProps;
 	public final GenericVersion gVersion;        // Points to the version that uses this feature.

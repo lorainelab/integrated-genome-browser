222ed2fc448858a75d78746c3c83088a04eb3519
tallpapab <tallpapab@uncc.edu>
2012-05-24 21:15:50 +0000

    Added scaling. Fixed the widget refresh at the end.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11698 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/CanonicalizeTracksAction.java
+++ core/igb/src/com/affymetrix/igb/action/CanonicalizeTracksAction.java
@@ -10,22 +10,29 @@
  */
 package com.affymetrix.igb.action;
 
+import com.affymetrix.genometryImpl.symmetry.GraphSym;
 import com.affymetrix.genoviz.bioviews.GlyphI;
+import com.affymetrix.igb.shared.AbstractGraphGlyph;
 import com.affymetrix.igb.shared.TierGlyph;
+import com.affymetrix.igb.shared.ViewModeGlyph;
 import java.awt.event.ActionEvent;
 import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import javax.swing.Action;
+import javax.swing.JOptionPane;
 
 /**
  * Make all the selected tracks easy to compare
  * by giving them the same size and scale.
+ * When no tracks are selected, do them all.
+ * Scale selected graphs to the smallest scale (largest MaxY).
  * <p> TODO
  * </p>
  * <ol>
  * <li> What's the correct way to resize? setPreferredHeight?
- * <li> Scale selected graphs. What to do beyond setting heights?
- * <li> Set the same stack depth for selected annotation tracks.
+ * <li> Set the same stack depth for selected annotation tracks?
  * <li> Listen to ListSelectionModel to setEnabled depending on selection.
  * This last could get a bit complicated with numbers and types of tracks to consider.
  * </ol>
@@ -50,13 +57,48 @@ public class CanonicalizeTracksAction extends SeqMapViewActionA {
     @Override
     public void actionPerformed(ActionEvent e) {
 		List<? extends GlyphI> l = this.getSeqMapView().getSelectedTiers();
+		if (l.isEmpty()) { // then no tracks are selected.
+			// Canonicalize all the tracks.
+			l = this.getSeqMapView().getTierManager().getAllTierGlyphs();
+		}
+		if (1 == l.size()) {
+			JOptionPane.showMessageDialog(null,
+					"Select more (or fewer) than one track.\n" +
+					"Actually you shouldn't be able to get here.\n" +
+					"File a bug report.",
+					this.getClass().getSimpleName(),
+					JOptionPane.INFORMATION_MESSAGE);
+			return;
+		}
 		double maxHeight = 0;
+		float maxMax = Float.MIN_VALUE;
 		for (GlyphI g: l) {
 			double h = g.getCoordBox().height;
-			if (maxHeight < h) {
+			TierGlyph tg = (TierGlyph) g;
+			if (tg.isManuallyResizable() && maxHeight < h) {
 				maxHeight = h;
 			}
+			Object info = tg.getInfo();
+			if (info instanceof GraphSym) {
+				System.out.println("   We gots a graph!");
+				GraphSym graph = (GraphSym) info;
+				float[] y = graph.getGraphYCoords();
+				float m = Float.MIN_VALUE;
+				for (int i = 0; i < y.length; i++) {
+					if (m < y[i]) {
+						m = y[i];
+					}
+				}
+				maxMax = Math.max(maxMax, m);
+			}
+			ViewModeGlyph vg = tg.getViewModeGlyph();
+			System.out.println(this.getClass().getName() + ": " + info.toString());
+			System.out.println(this.getClass().getName() + ": " + vg.toString());
 		}
+		JOptionPane.showMessageDialog(null,
+				"Thank you for your interest. Setting heights to " + maxMax,
+				this.getClass().getSimpleName(),
+				JOptionPane.INFORMATION_MESSAGE);
 		for (GlyphI g: l) {
 			Rectangle2D.Double b = g.getCoordBox();
 			System.out.println("Glyph: " + g.getClass().getName());
@@ -66,10 +108,16 @@ public class CanonicalizeTracksAction extends SeqMapViewActionA {
 						maxHeight,
 						this.getSeqMapView().getSeqMap().getView()
 				);
+				if (Float.MIN_VALUE < maxMax) {
+					ViewModeGlyph vg = tg.getViewModeGlyph();
+					if (vg instanceof AbstractGraphGlyph) {
+						AbstractGraphGlyph gg = (AbstractGraphGlyph) vg;
+						gg.setVisibleMaxY(maxMax);
+					}
+				}
 			}
-			//g.setCoords(b.x, b.y, b.width, maxHeight);
 		}
-		this.getSeqMapView().updateUI(); // Is this right? Need we do more?
+		this.refreshMap(true, false);
 	}
 
 }

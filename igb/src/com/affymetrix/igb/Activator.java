193c4539b2fc8ac4d46a6ee25a41ac7c819088c6
lfrohman <lfrohman@mycompany.com>
2012-05-29 17:08:40 +0000

    IGBB-374 = specify bookmark port
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@11717 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- igb/src/com/affymetrix/igb/Activator.java
+++ igb/src/com/affymetrix/igb/Activator.java
@@ -58,6 +58,7 @@ public class Activator implements BundleActivator {
     			System.out.println(APP_NAME + " " + APP_VERSION_FULL);
 				System.out.println("Options:");
 				System.out.println("-offline - set the URL caching to offline");
+				System.out.println("-port - bookmarks use the port specified");
 				System.out.println("-scriptfile - load a script file");
 				System.out.println("-convert - convert the fasta file to bnib");
 				System.out.println("-clrprf - clear the preferences");

a43de04a75deedf92b5f481672a8dd215d9c3114
lfrohman <lfrohman@mycompany.com>
2012-06-05 11:44:26 +0000

    test panel
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11804 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/TrackAdjuster/src/com/affymetrix/igb/trackAdjuster/Activator.java
+++ plugins/TrackAdjuster/src/com/affymetrix/igb/trackAdjuster/Activator.java
@@ -1,5 +1,18 @@
 package com.affymetrix.igb.trackAdjuster;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.Popup;
+import javax.swing.PopupFactory;
+
+import com.affymetrix.genometryImpl.event.GenericAction;
+import com.affymetrix.genoviz.swing.MenuUtil;
+import com.affymetrix.genoviz.swing.recordplayback.JRPMenu;
+import com.affymetrix.genoviz.swing.recordplayback.JRPMenuItem;
 import com.affymetrix.igb.osgi.service.IGBService;
 import com.affymetrix.igb.osgi.service.IGBTabPanel;
 import com.affymetrix.igb.window.service.WindowActivator;
@@ -8,9 +21,25 @@ import org.osgi.framework.BundleActivator;
 public class Activator extends WindowActivator implements BundleActivator {
 
 	@Override
-	protected IGBTabPanel getPage(IGBService igbService) {
+	protected IGBTabPanel getPage(final IGBService igbService) {
 		TrackAdjusterTabGUI.init(igbService);
 		final TrackAdjusterTabGUI trackAdjusterTabGUI = TrackAdjusterTabGUI.getSingleton();
+// test
+		JRPMenu file_menu = igbService.getMenu("edit");
+		int index = file_menu.getItemCount() - 1;
+		file_menu.insertSeparator(index);
+		MenuUtil.insertIntoMenu(file_menu, new JRPMenuItem("",
+			new GenericAction("test",null, null) {
+				private static final long serialVersionUID = 1L;
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						Popup popup = PopupFactory.getSharedInstance().getPopup(trackAdjusterTabGUI, new TrackPropertiesGUI(), 10, 10);
+						popup.show();
+					}
+				}
+			),
+		index);
+// end test
 		return trackAdjusterTabGUI;
 	}
 }

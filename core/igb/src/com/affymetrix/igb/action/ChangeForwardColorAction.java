a265afc34d6f01606834767304d21d66575d825d
lfrohman <lfrohman@mycompany.com>
2012-06-05 16:08:05 +0000

    forward/reverse color actions
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11809 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/igb/src/com/affymetrix/igb/action/ChangeForwardColorAction.java
@@ -0,0 +1,34 @@
+package com.affymetrix.igb.action;
+
+import javax.swing.JColorChooser;
+
+import com.affymetrix.genometryImpl.event.GenericActionHolder;
+import com.affymetrix.genometryImpl.style.ITrackStyleExtended;
+
+import com.affymetrix.igb.IGBConstants;
+
+public class ChangeForwardColorAction extends ChangeColorActionA {
+	private static final long serialVersionUID = 1L;
+	private static final ChangeForwardColorAction ACTION = new ChangeForwardColorAction();
+
+	static{
+		GenericActionHolder.getInstance().addGenericAction(ACTION);
+	}
+	public static ChangeForwardColorAction getAction() {
+		return ACTION;
+	}
+
+	public ChangeForwardColorAction() {
+		super(IGBConstants.BUNDLE.getString("changeForwardColorAction"), "16x16/actions/go-next.png", "22x22/actions/go-next.png");
+	}
+
+	@Override
+	protected void setChooserColor(JColorChooser chooser, ITrackStyleExtended style) {
+		chooser.setColor(style.getForwardColor());
+	}
+
+	@Override
+	protected void setStyleColor(JColorChooser chooser, ITrackStyleExtended style) {
+		style.setForwardColor(chooser.getColor());
+	}
+}
---
+++ core/igb/src/com/affymetrix/igb/action/ChangeReverseColorAction.java
@@ -0,0 +1,34 @@
+package com.affymetrix.igb.action;
+
+import javax.swing.JColorChooser;
+
+import com.affymetrix.genometryImpl.event.GenericActionHolder;
+import com.affymetrix.genometryImpl.style.ITrackStyleExtended;
+
+import com.affymetrix.igb.IGBConstants;
+
+public class ChangeReverseColorAction extends ChangeColorActionA {
+	private static final long serialVersionUID = 1L;
+	private static final ChangeReverseColorAction ACTION = new ChangeReverseColorAction();
+
+	static{
+		GenericActionHolder.getInstance().addGenericAction(ACTION);
+	}
+	public static ChangeReverseColorAction getAction() {
+		return ACTION;
+	}
+
+	public ChangeReverseColorAction() {
+		super(IGBConstants.BUNDLE.getString("changeReverseColorAction"), "16x16/actions/go-previous.png", "22x22/actions/go-previous.png");
+	}
+
+	@Override
+	protected void setChooserColor(JColorChooser chooser, ITrackStyleExtended style) {
+		chooser.setColor(style.getReverseColor());
+	}
+
+	@Override
+	protected void setStyleColor(JColorChooser chooser, ITrackStyleExtended style) {
+		style.setReverseColor(chooser.getColor());
+	}
+}

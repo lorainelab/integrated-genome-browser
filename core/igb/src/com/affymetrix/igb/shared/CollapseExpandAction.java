f5a79ba75eb2d56327b23edb2cdcb57f9da0cb90
tallpapab <tallpapab@uncc.edu>
2012-06-07 00:38:25 +0000

    Listen for selection changes. Enable only if selection is not empty. Enable or disable clients appropriately. Fixes Jira IGBC-212.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11858 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/shared/CollapseExpandAction.java
+++ core/igb/src/com/affymetrix/igb/shared/CollapseExpandAction.java
@@ -1,12 +1,19 @@
 package com.affymetrix.igb.shared;
 
+import com.affymetrix.genometryImpl.GenometryModel;
 import com.affymetrix.genometryImpl.event.GenericActionHolder;
 import com.affymetrix.genometryImpl.event.SymSelectionEvent;
 import com.affymetrix.genometryImpl.event.SymSelectionListener;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genoviz.bioviews.GlyphI;
 import com.affymetrix.igb.action.CollapseAction;
 import com.affymetrix.igb.action.ExpandAction;
 import com.affymetrix.igb.action.SeqMapToggleAction;
 import com.affymetrix.igb.action.SeqMapViewActionA;
+import com.affymetrix.igb.view.SeqMapView;
+import com.affymetrix.igb.viewmode.AnnotationGlyph;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CollapseExpandAction extends SeqMapToggleAction implements SymSelectionListener {
 	private static final long serialVersionUID = 1L;
@@ -25,6 +32,9 @@ public class CollapseExpandAction extends SeqMapToggleAction implements SymSelec
 	 */
 	public static void createSingleton() {
 		GenericActionHolder.getInstance().addGenericAction(ACTION);
+		GenometryModel.getGenometryModel().addSymSelectionListener(ACTION);
+		ACTION.setEnabled(false); // until such time as a track is selected.
+
 	}
 
 	protected CollapseExpandAction(SeqMapViewActionA a, SeqMapViewActionA b) {
@@ -38,6 +48,41 @@ public class CollapseExpandAction extends SeqMapToggleAction implements SymSelec
 
 	@Override
 	public void symSelectionChanged(SymSelectionEvent evt) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		Object o = evt.getSource();
+		SeqMapView gviewer = getSeqMapView();
+		if (!(o == gviewer || o == gviewer.getSeqMap())) {
+			return;
+		}
+
+		// Suppress unchecked warning when casting the list.
+		// In this case the warning is over zealous.
+		@SuppressWarnings("unchecked")
+		List<GlyphI> l = (List<GlyphI>) gviewer.getSelectedTiers();
+		List<SeqSymmetry> selected_syms = SeqMapView.glyphsToSyms(l);
+		// Only pay attention to selections from the main SeqMapView or its map.
+		// Ignore the splice view as well as events coming from this class itself.
+
+		changeActionDisplay(selected_syms);
+		this.setEnabled(0 < selected_syms.size());
 	}
+
+	private void changeActionDisplay(List<SeqSymmetry> selected_syms) {
+		boolean hasCollapsed = false;
+		boolean hasExpanded = false;
+		for (TierGlyph tg : getSeqMapView().getTierManager().getVisibleTierGlyphs()) {
+			ViewModeGlyph vg = tg.getViewModeGlyph();
+			if (vg instanceof AnnotationGlyph && !(vg instanceof MultiGraphGlyph)) {
+				SeqSymmetry ss = (SeqSymmetry) vg.getInfo();
+				if (selected_syms.contains(ss)) {
+					boolean collapsed = vg.getAnnotStyle().getCollapsed();
+					hasCollapsed |= collapsed;
+					hasExpanded |= !collapsed;
+				}
+			}
+		}
+		// Note that the following depends on the client actions being singletons.
+		ExpandAction.getAction().setEnabled(hasCollapsed);
+		CollapseAction.getAction().setEnabled(hasExpanded);
+	}
+
 }

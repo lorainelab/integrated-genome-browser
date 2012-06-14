e0deb4c620b4533442b16f5f03dfc976e8a85a06
lfrohman <lfrohman@mycompany.com>
2012-06-14 19:19:29 +0000

    IGBC-289 = summary/detail threshold absolute instead of % of horz zoomer
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@11995 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/util/ThresholdReader.java
+++ core/igb/src/com/affymetrix/igb/util/ThresholdReader.java
@@ -1,39 +1,42 @@
 package com.affymetrix.igb.util;
 
-import javax.swing.JSlider;
-
+import com.affymetrix.genoviz.widget.NeoAbstractWidget;
 import com.affymetrix.genoviz.widget.NeoMap;
 import com.affymetrix.igb.IGB;
 
 public class ThresholdReader {
-	public static final int default_threshold = 80;
+	public static final int threshold_increment = 10000;
+	public static final int threshold_min = threshold_increment;
+	public static final int threshold_max = 20000000;
+	public static final int default_threshold = 200000;
 	private static final ThresholdReader instance = new ThresholdReader();
 	public static final ThresholdReader getInstance() {
 		return instance;
 	}
-	private final JSlider zoomer;
+	private final NeoMap seqmap;
 	private ThresholdReader() {
 		super();
-		this.zoomer = (JSlider)((IGB)IGB.getSingleton()).getMapView().getSeqMap().getZoomer(NeoMap.X);
+		this.seqmap = ((IGB)IGB.getSingleton()).getMapView().getSeqMap();
 	}
 
 	public int getCurrentThresholdValue() {
-		return (zoomer.getValue() * 100 / zoomer.getMaximum());
-	}
-
-	public int getIncrement() {
-		return Math.min(100, getCurrentThresholdValue() + 1);
-	}
-
-	public int getDecrement() {
-		return Math.max(0, getCurrentThresholdValue() - 1);
-	}
-
-	public boolean isDetail(int threshold) {
-		return getCurrentThresholdValue() >= threshold;
+		return (int)seqmap.getView().getCoordBox().getWidth();
 	}
 
 	public double getAsZoomerPercent(int threshold) {
-		return threshold / 100.0;
+		double scale = ((IGB)IGB.getSingleton()).getMapView().getPixelsToCoord(0, threshold);
+		return seqmap.zoomerValueFromScale(NeoAbstractWidget.X, scale);
+	}
+
+	public int getIncrement() {
+		return Math.min(threshold_max, getCurrentThresholdValue() - threshold_increment);
+	}
+
+	public int getDecrement() {
+		return Math.max(threshold_min, getCurrentThresholdValue() + threshold_increment);
+	}
+
+	public boolean isDetail(int threshold) {
+		return getCurrentThresholdValue() <= threshold;
 	}
 }

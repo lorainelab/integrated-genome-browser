c8d75cc1554570260a8beeef75e9b30eb81c9d27
tallpapab <tallpapab@uncc.edu>
2012-06-29 19:59:12 +0000

    Skip over graph tracks. Jira 340.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12226 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ChangeExpandMaxAllAction.java
+++ core/igb/src/com/affymetrix/igb/action/ChangeExpandMaxAllAction.java
@@ -1,14 +1,22 @@
 package com.affymetrix.igb.action;
 
+import com.affymetrix.genometryImpl.event.GenericActionHolder;
+import static com.affymetrix.igb.IGBConstants.BUNDLE;
+import com.affymetrix.igb.shared.TierGlyph;
+import com.affymetrix.igb.shared.TrackstylePropertyMonitor;
+import com.affymetrix.igb.tiers.TierLabelGlyph;
+import com.affymetrix.igb.tiers.TierLabelManager;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.affymetrix.genometryImpl.event.GenericActionHolder;
-import com.affymetrix.igb.tiers.TierLabelGlyph;
 
-import static com.affymetrix.igb.IGBConstants.BUNDLE;
-import com.affymetrix.igb.shared.TrackstylePropertyMonitor;
-
+/**
+ * Changes the maximum stack depth (max expand) of all the tiers in the map,
+ * not just those selected.
+ * It does not affect graph tracks.
+ * @author blossome
+ */
 public class ChangeExpandMaxAllAction extends ChangeExpandMaxActionA {
 	private static final long serialVersionUID = 1L;
 	private static final ChangeExpandMaxAllAction ACTION = new ChangeExpandMaxAllAction();
@@ -27,7 +35,20 @@ public class ChangeExpandMaxAllAction extends ChangeExpandMaxActionA {
 
 	@Override
 	protected List<TierLabelGlyph> getTiers() {
-		return getTierManager().getAllTierLabels();
+		List<TierLabelGlyph> answer = new ArrayList<TierLabelGlyph>();
+		TierLabelManager m = getTierManager();
+		List<TierLabelGlyph> allTiers = m.getAllTierLabels();
+		if (null == allTiers) {
+			return answer;
+		}
+		for (TierLabelGlyph t : allTiers) {
+			TierGlyph tg = t.getReferenceTier();
+			if (tg.getAnnotStyle().isGraphTier()) {
+				continue;
+			}
+			answer.add(t);
+		}
+		return answer;
 	}
 	
 	@Override

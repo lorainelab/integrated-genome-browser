c463426baeaf7e43902988a126aa7bf766667a40
hiralv <hiralv@uncc.edu>
2012-07-06 14:21:36 +0000

    Reverting back r12295.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12307 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/tiers/TrackStyle.java
+++ core/igb/src/com/affymetrix/igb/tiers/TrackStyle.java
@@ -1010,12 +1010,12 @@ public class TrackStyle implements ITrackStyleExtended, TrackConstants, Property
 	 */
 	@Override
 	public void setGraphTier(boolean b) {
-		if (view_mode == null || MapViewModeHolder.getInstance().getViewFactory(view_mode).isCategorySupported(FileTypeCategory.Graph)) {
+		//if (view_mode == null || MapViewModeHolder.getInstance().getViewFactory(view_mode).isCategorySupported(FileTypeCategory.Graph)) {
 			is_graph = b;
 			if (is_graph) {
 				setSeparate(false);
 			}
-		}
+		//}
 	}
 
 	public Map<String, Object> getTransientPropertyMap() {

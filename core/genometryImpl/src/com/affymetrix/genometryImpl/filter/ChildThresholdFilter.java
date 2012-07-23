d48b8c80cc1f4ec155d3ea537731975bb7792d22
anuj4159 <anuj4159@uncc.edu>
2012-07-23 13:26:41 +0000

    FindJunction Operators and Filters added
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12488 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/ChildThresholdFilter.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.affymetrix.genometryImpl.filter;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.SeqSpan;
+import com.affymetrix.genometryImpl.filter.SymmetryFilterI;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+
+/**
+ *
+ * @author Anuj
+ */
+public class ChildThresholdFilter implements SymmetryFilterI{
+
+    int threshold;
+    
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean setParam(Object o) {
+        threshold = (Integer)o;
+        return true;
+    }
+
+    @Override
+    public Object getParam() {
+        return threshold;
+    }
+
+    @Override
+    public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry ss) {
+        SeqSpan span = ss.getSpan(bioseq);
+        if((span.getMax() - span.getMin()) < threshold)
+            return false;
+        else
+            return true;
+    }
+}
---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/NoIntronFilter.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.affymetrix.genometryImpl.filter;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.filter.SymmetryFilterI;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+
+/**
+ *
+ * @author auser
+ */
+public class NoIntronFilter implements SymmetryFilterI{
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean setParam(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object getParam() {
+        return null;
+    }
+
+    @Override
+    public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry ss) {
+        if(ss.getChildCount() <= 1)
+            return false;
+        return true;
+    }
+    
+}

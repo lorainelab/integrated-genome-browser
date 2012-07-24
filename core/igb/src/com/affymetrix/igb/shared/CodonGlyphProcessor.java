962a27c8a8b377e10114b0a2b61b71c3626f489c
hiralv <hiralv@uncc.edu>
2012-07-24 12:36:43 +0000

    Use interface.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12498 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/shared/CodonGlyphProcessor.java
+++ core/igb/src/com/affymetrix/igb/shared/CodonGlyphProcessor.java
@@ -9,16 +9,16 @@ import com.affymetrix.genoviz.bioviews.GlyphI;
 
 public class CodonGlyphProcessor implements GlyphProcessor {
 	private CodonGlyph saveCodonGlyph;
-	@Override
+	
 	/*
 	 * note - if the transcript has UTR, it will have two glyphs, one with the UTR and one without.
 	 * the one without should be used, but it is created first, so it would be covered by the one
 	 * with. So this kludge causes the one without to not be drawn on its turn, but the one with
 	 * will draw the one without on its turn.
 	 */
+	@Override
 	public void processGlyph(GlyphI glyph) {
-		if (glyph.getParent() != null && 
-				(glyph.getParent().getInfo() instanceof UcscGeneSym || glyph.getParent().getInfo() instanceof UcscBedSym)) {
+		if (glyph.getParent() != null && glyph.getParent().getInfo() instanceof SymSpanWithCds) {
 			CodonGlyph codonGlyph = new CodonGlyph();
 			if (hasUTR((SymSpanWithCds)glyph.getParent().getInfo(), (SeqSymmetry)glyph.getInfo())) {
 				if (saveCodonGlyph != null) {

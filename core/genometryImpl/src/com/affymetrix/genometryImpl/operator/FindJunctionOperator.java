4fcfb4dfb6c774b1f7f7eb4baae1c807fca1dd7e
hiralv <hiralv@uncc.edu>
2012-07-24 13:51:08 +0000

    Moving helper class into find junction operator.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12500 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/operator/FindJunctionOperator.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/operator/FindJunctionOperator.java
@@ -202,4 +202,50 @@ public class FindJunctionOperator implements Operator{
         }
     }
     
+	//Helper seqsymmetry class
+	private class JunctionUcscBedSym extends UcscBedSym {
+
+		int positiveScore, negativeScore;
+		float localScore = 1;
+		boolean canonical;
+
+		private JunctionUcscBedSym(String type, BioSeq seq, int txMin, int txMax, String name, float score, boolean forward,
+				int cdsMin, int cdsMax, int[] blockMins, int[] blockMaxs, int positiveScore, int negativeScore, boolean canonical) {
+			super(type, seq, txMin, txMax, name, score, forward, cdsMin, cdsMax, blockMins, blockMaxs);
+			this.positiveScore = positiveScore;
+			this.negativeScore = negativeScore;
+			this.canonical = canonical;
+		}
+
+		private void updateScore(boolean isForward) {
+			localScore++;
+			if (!canonical) {
+				if (isForward) {
+					this.positiveScore++;
+				} else {
+					this.negativeScore++;
+				}
+			}
+		}
+
+		@Override
+		public float getScore() {
+			return localScore;
+		}
+
+		@Override
+		public String getName() {
+			return getID();
+		}
+
+		@Override
+		public String getID() {
+			return super.getID() + (isForward() ? "+" : "-");
+		}
+
+		@Override
+		public boolean isForward() {
+			return canonical ? super.isForward() : positiveScore > negativeScore ? true : false;
+		}
+	}
 }

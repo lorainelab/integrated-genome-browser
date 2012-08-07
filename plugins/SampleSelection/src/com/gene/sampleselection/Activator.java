998fe64a8cda0d7ca9dc4e38bf3549c80633c5e6
hiralv <hiralv@uncc.edu>
2012-08-07 10:17:33 +0000

    Adding Sample selection.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12568 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ plugins/SampleSelection/src/com/gene/sampleselection/Activator.java
@@ -0,0 +1,25 @@
+package com.gene.sampleselection;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import com.affymetrix.igb.osgi.service.IGBService;
+import com.affymetrix.igb.osgi.service.IGBTabPanel;
+import com.affymetrix.igb.shared.TrackClickListener;
+import com.affymetrix.igb.window.service.WindowActivator;
+
+public class Activator extends WindowActivator implements BundleActivator {
+	private ServiceRegistration<TrackClickListener> trackClickListenerRegistration;
+
+	@Override
+	protected IGBTabPanel getPage(IGBService igbService) {
+		trackClickListenerRegistration = bundleContext.registerService(TrackClickListener.class, new VCFListener(igbService), null);
+        return new SampleSelectionView(igbService);
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		trackClickListenerRegistration.unregister();
+	}
+}

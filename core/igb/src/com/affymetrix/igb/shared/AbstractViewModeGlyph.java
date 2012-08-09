5d2fad3f651afea8131ca851609575772fd7d8b4
hiralv <hiralv@uncc.edu>
2012-08-09 17:55:35 +0000

    Merging AbstractViewModeGlyph and ViewModeGlyph.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12607 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/shared/AbstractViewModeGlyph.java
+++ core/igb/src/com/affymetrix/igb/shared/AbstractViewModeGlyph.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.affymetrix.genometryImpl.BioSeq;
@@ -15,59 +14,28 @@ import com.affymetrix.genometryImpl.style.ITrackStyleExtended;
 import com.affymetrix.genometryImpl.symmetry.RootSeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SimpleMutableSeqSymmetry;
-import com.affymetrix.genometryImpl.util.LoadUtils;
 import com.affymetrix.genometryImpl.util.SeqUtils;
-import com.affymetrix.genometryImpl.util.ThreadUtils;
-import com.affymetrix.genoviz.bioviews.AbstractCoordPacker;
 import com.affymetrix.genoviz.bioviews.Glyph;
 import com.affymetrix.genoviz.bioviews.GlyphI;
 import com.affymetrix.genoviz.bioviews.ViewI;
-import com.affymetrix.genoviz.event.NeoRangeEvent;
-import com.affymetrix.genoviz.event.NeoRangeListener;
 import com.affymetrix.genoviz.glyph.FillRectGlyph;
 import com.affymetrix.igb.Application;
-import com.affymetrix.igb.action.SetSummaryThresholdAction;
 import com.affymetrix.igb.shared.TierGlyph.Direction;
 import com.affymetrix.igb.tiers.TrackConstants;
 import com.affymetrix.igb.view.load.GeneralLoadUtils;
-import com.affymetrix.igb.view.load.GeneralLoadView;
 import java.util.Collections;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.SwingWorker;
 
-public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements NeoRangeListener{
-	protected ITrackStyleExtended style;
-	protected Direction direction = Direction.NONE;
-	/*
-	 * other_fill_color is derived from fill_color whenever setFillColor() is called.
-	 * if there are any "middle" glyphs, then background is drawn with other_fill_color and
-	 *    middle glyphs are drawn with fill_color
-	 * if no "middle" glyphs, then background is drawn with fill_color
-	 */
-	protected Color other_fill_color = null;
-	/** glyphs to be drawn in the "middleground" --
-	 *    in front of the solid background, but behind the child glyphs
-	 *    For example, to indicate how much of the xcoord range has been covered by feature retrieval attempts
-	 */
-	private final List<GlyphI> middle_glyphs = new ArrayList<GlyphI>();
-	private static final int handle_width = 10;  // width of handle in pixels
-	private final Rectangle pixel_hitbox = new Rectangle();  // caching rect for hit detection
-	protected String label = null;
-	SwingWorker previousWorker, worker;
+public abstract class AbstractViewModeGlyph extends ViewModeGlyph {
 	
 	@Override
 	protected RootSeqSymmetry loadRegion(SeqSpan span){
 		loadData(span);
 		return (RootSeqSymmetry) this.getInfo();
 	}
-	
-	@Override
-	protected ViewModeGlyph createGlyphs(RootSeqSymmetry rootSym, MapViewGlyphFactoryI factory, SeqMapViewExtendedI smv) {
-		return factory.getViewModeGlyph(rootSym, style, direction, smv);
-	}
-	
+		
 	@Override
 	protected void updateParent(ViewModeGlyph vmg){
 		//Do Nothing
@@ -94,90 +62,6 @@ public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements Neo
 
 		return Collections.<SeqSymmetry>emptyList();
 	}
-	
-	@Override
-	protected void loadAndDisplayRegion(final SeqMapViewExtendedI smv, final MapViewGlyphFactoryI factory) throws Exception {
-		if (previousWorker != null && !previousWorker.isCancelled() && !previousWorker.isDone()) {
-			previousWorker.cancel(true);
-			previousWorker = null;
-		}
-
-		worker = new SwingWorker() {
-
-			@Override
-			protected Void doInBackground() throws Exception {
-				RootSeqSymmetry rootSym = loadRegion(smv.getVisibleSpan());
-				if (rootSym.getChildCount() > 0) {
-					final ViewModeGlyph vmg = createGlyphs(rootSym, factory, smv);
-					ThreadUtils.runOnEventQueue(new Runnable() {
-
-						public void run() {
-							updateParent(vmg);
-							GeneralLoadUtils.setLastRefreshStatus(style.getFeature(), vmg.getChildCount() > 0);
-							GeneralLoadView.getLoadView().refreshDataManagementView();
-							//TODO: Find a way to avoid this
-							//if (lastUsedGlyph == saveDetailGlyph) {
-							smv.repackTheTiers(true, true);
-							smv.getSeqMap().updateWidget();
-							Application.getSingleton().removeNotLockedUpMsg("Loading " + getAnnotStyle().getTrackName());
-							//}
-						}
-					});
-				}else{
-					Application.getSingleton().removeNotLockedUpMsg("Loading " + getAnnotStyle().getTrackName());
-				}
-				return null;
-			}
-
-		};
-		worker.execute();
-		previousWorker = worker;
-		worker = null;
-	}
-	
-	protected boolean isAutoLoadMode() {
-		if (this.getAnnotStyle() == null) {
-			return false;
-		}
-
-		if (this.getAnnotStyle().getFeature() == null) {
-			return false;
-		}
-
-		if (this.getAnnotStyle().getFeature().getLoadStrategy() != LoadUtils.LoadStrategy.AUTOLOAD) {
-			return false;
-		}
-		return true;
-	}
-	
-	public boolean isDetail(ViewI view) {
-		return SetSummaryThresholdAction.getAction().isDetail(getAnnotStyle());
-	}
-	
-	@Override
-	public void rangeChanged(NeoRangeEvent evt){
-		if(evt.getSource() instanceof SeqMapViewExtendedI){
-			rangeChanged(((SeqMapViewExtendedI)evt.getSource()));
-		}
-	}
-	
-	protected void rangeChanged(SeqMapViewExtendedI smv){
-		if(isAutoLoadMode() && isDetail(smv.getSeqMap().getView())){
-			try {
-				MapViewGlyphFactoryI factory = MapViewModeHolder.getInstance().getViewFactory(getAnnotStyle().getViewMode());
-				if(factory != null){
-					loadAndDisplayRegion(smv, factory);
-				}
-			} catch (Exception ex) {
-				Logger.getLogger(AbstractViewModeGlyph.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-	};
-	
-	@Override
-	public ITrackStyleExtended getAnnotStyle() {
-		return style;
-	}
 
 	public void setStyle(ITrackStyleExtended style) {
 		this.style = style;
@@ -190,11 +74,6 @@ public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements Neo
 		//If any visibilty bug occurs, fix here. -HV 22/03/2012
 		setVisibility(style.getShow());
 		setLabel(style.getTrackName());
-	}
-
-	@Override
-	public Direction getDirection() {
-		return direction;
 	}
 
 	/**
@@ -213,36 +92,6 @@ public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements Neo
 	@Override
 	public String getLabel() {
 		return label;
-	}
-
-	/** Returns the color used to draw the tier background, or null
-	if there is no background. */
-	public Color getFillColor() {
-		return style.getBackground();
-	}
-
-	/** Sets the color used to fill the tier background, or null if no color
-	 *  @param col  A color, or null if no background color is desired.
-	 */
-	@Override
-	public void setFillColor(Color col) {
-		if (style.getBackground() != col) {
-			style.setBackground(col);
-		}
-
-		// Now set the "middleground" color based on the fill color
-		if (col == null) {
-			other_fill_color = Color.DARK_GRAY;
-		} else {
-			int intensity = col.getRed() + col.getGreen() + col.getBlue();
-			if (intensity == 0) {
-				other_fill_color = Color.darkGray;
-			} else if (intensity > (255 + 127)) {
-				other_fill_color = col.darker();
-			} else {
-				other_fill_color = col.brighter();
-			}
-		}
 	}
 	
 	@Override
@@ -308,60 +157,12 @@ public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements Neo
 			}
 		}
 	}
-		
-	@Override
-	public boolean toolBarHit(Rectangle2D.Double coord_hitbox, ViewI view){
-		if (shouldDrawToolBar() && isVisible() && coord_hitbox.intersects(getCoordBox())) {
-			// overlapping handle ?  (need to do this one in pixel space?)
-			Rectangle hpix = new Rectangle();
-			view.transformToPixels(coord_hitbox, hpix);
-			if (getToolbarPixel(view).intersects(hpix)) {
-				return true;
-			}
-		}
-		return false;
-	}
-		
+				
 	@Override
 	protected boolean shouldDrawToolBar(){
 		return style.drawCollapseControl();
 	}
-
-	private Rectangle getToolbarPixel(ViewI view){
-		pixel_hitbox.setBounds(getPixelBox().x + 4, getPixelBox().y + 4, handle_width, handle_width);
-		return pixel_hitbox;
-	}
 		
-	/**
-	 *  Adds "middleground" glyphs, which are drawn in front of the background but
-	 *    behind all "real" child glyphs.
-	 *  These are generally not considered children of
-	 *    the glyph.  The TierGlyph will render these glyphs, but they can't be selected since they
-	 *    are not considered children in pickTraversal() method.
-	 *  The only way to remove these is via removeAllChildren() method,
-	 *    there is currently no external access to them.
-	 */
-	@Override
-	public void addMiddleGlyph(GlyphI gl) {
-		middle_glyphs.add(gl);
-	}
-	
-	/**
-	 * Remove all children of the glyph,
-	 * including those added with addMiddleGlyph(GlyphI).
-	 */
-	@Override
-	public void removeAllChildren() {
-		super.removeAllChildren();
-		// also remove all middleground glyphs
-		// this is currently the only place where middleground glyphs are treated as if they were children
-		//   maybe should rename this method clear() or something like that...
-		// only reference to middle glyphs should be in this.middle_glyphs, so should be able to GC them by
-		//     clearing middle_glyphs.  These glyphs never have setScene() called on them,
-		//     so it is not necessary to call setScene(null) on them.
-		middle_glyphs.clear();
-	}
-	
 	@Override
 	public void setInfo(Object info) {
 		super.setInfo(info);
@@ -370,52 +171,7 @@ public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements Neo
 		}
 	}
 	
-		
-	@Override
-	public void copyChildren(ViewModeGlyph temp) {
-		if(temp == null)
-			return;
-		
-		List<GlyphI> childrens = new ArrayList<GlyphI>();
-		childrens.addAll(temp.getChildren());
-
-		for (int i = 0; i < childrens.size(); i++) {
-			addChild(childrens.get(i));
-		}
-		
-		childrens.clear();
-		childrens.addAll(((AbstractViewModeGlyph)temp).middle_glyphs);
-		
-		for (int i = 0; i < childrens.size(); i++) {
-			addMiddleGlyph(childrens.get(i));
-		}
-		
-		//TODO: Set list of all getInfo
-//		if(!(getInfo() instanceof List)){
-//			List<Object> info = new ArrayList<Object>();
-//			info.add(getInfo());
-//			setInfo(info);
-//		}else{
-//			((List)(getInfo())).add(temp.getInfo());
-//		}
-		
-	}
-	
-	@Override
-	public List<SeqSymmetry> getSelected(){
-	
-		int childCount = getChildCount();
-		List<SeqSymmetry> selectedSyms = new ArrayList<SeqSymmetry>(childCount);
-		for(int i=0;i<childCount;i++){
-			if(getChild(i).isSelected()){
-				if(getChild(i).getInfo() instanceof SeqSymmetry){
-					selectedSyms.add((SeqSymmetry)(getChild(i).getInfo()));
-				}
-			}
-		}
-		return selectedSyms;
-	}
-
+			
 	@Override
 	public boolean initUnloaded() {
 		Glyph glyph;
@@ -454,35 +210,6 @@ public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements Neo
 		}
 		return false;
 	}
-	
-	@Override
-	public final double getChildHeight(){
-		double child_height = MapViewGlyphFactoryI.DEFAULT_CHILD_HEIGHT;
-		child_height = AbstractViewModeGlyph.useLabel(getAnnotStyle()) ? child_height * 2 : child_height;
-		child_height = child_height + getSpacing() * 2;
-		return child_height;
-	}
-	
-	protected final double getSpacing() {
-		if(getPacker() instanceof AbstractCoordPacker){
-			return ((AbstractCoordPacker)getPacker()).getSpacing();
-		}
-		return 2;
-	}
-	
-	@Override
-	public final int getStyleDepth(){
-		switch(getDirection()){
-			case REVERSE:
-				return getAnnotStyle().getReverseMaxDepth();
-			
-			case FORWARD:
-				return getAnnotStyle().getForwardMaxDepth();
-						
-			default:
-				return getAnnotStyle().getMaxDepth();
-		}
-	}
 		
 	@Override
 	public void pack(ViewI view) {
@@ -506,9 +233,4 @@ public abstract class AbstractViewModeGlyph extends ViewModeGlyph implements Neo
 		}
 	}
 	
-	public static boolean useLabel(ITrackStyleExtended style){
-		return style.getLabelField() != null && 
-		!style.getLabelField().equals(TrackConstants.NO_LABEL) && 
-		(style.getLabelField().trim().length() > 0);
-	}
 }
--- core/igb/src/com/affymetrix/igb/shared/ViewModeGlyph.java
+++ core/igb/src/com/affymetrix/igb/shared/ViewModeGlyph.java
@@ -4,15 +4,30 @@ import com.affymetrix.genometryImpl.SeqSpan;
 import com.affymetrix.genometryImpl.style.ITrackStyleExtended;
 import com.affymetrix.genometryImpl.symmetry.RootSeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genometryImpl.util.LoadUtils;
+import com.affymetrix.genometryImpl.util.ThreadUtils;
+import com.affymetrix.genoviz.bioviews.AbstractCoordPacker;
 import com.affymetrix.genoviz.bioviews.GlyphI;
 import com.affymetrix.genoviz.bioviews.ViewI;
+import com.affymetrix.genoviz.event.NeoRangeEvent;
+import com.affymetrix.genoviz.event.NeoRangeListener;
 import com.affymetrix.genoviz.glyph.SolidGlyph;
+import com.affymetrix.igb.Application;
+import com.affymetrix.igb.action.SetSummaryThresholdAction;
 import com.affymetrix.igb.shared.TierGlyph.Direction;
+import com.affymetrix.igb.tiers.TrackConstants;
+import com.affymetrix.igb.view.load.GeneralLoadUtils;
+import com.affymetrix.igb.view.load.GeneralLoadView;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingWorker;
 
 /**
  *  !!! the TierGlyph and ViewModeGlyph and all its subordinate ViewModeGlyphs should
@@ -20,33 +35,277 @@ import java.util.Map;
  *  This is the glyph that displays the contents of a Tier/Track. Each TierGlyph
  *  contains a ViewModeGlyph and delegates all calls to the ViewModeGlyph.
  */
-public abstract class ViewModeGlyph extends SolidGlyph {
+public abstract class ViewModeGlyph extends SolidGlyph implements NeoRangeListener {
 	private TierGlyph tierGlyph;
+	SwingWorker previousWorker, worker;
+	protected ITrackStyleExtended style;
+	protected Direction direction = Direction.NONE;
+	private static final int handle_width = 10;  // width of handle in pixels
+	private final Rectangle pixel_hitbox = new Rectangle();  // caching rect for hit detection
+	/** glyphs to be drawn in the "middleground" --
+	 *    in front of the solid background, but behind the child glyphs
+	 *    For example, to indicate how much of the xcoord range has been covered by feature retrieval attempts
+	 */
+	protected final List<GlyphI> middle_glyphs = new ArrayList<GlyphI>();
+	/*
+	 * other_fill_color is derived from fill_color whenever setFillColor() is called.
+	 * if there are any "middle" glyphs, then background is drawn with other_fill_color and
+	 *    middle glyphs are drawn with fill_color
+	 * if no "middle" glyphs, then background is drawn with fill_color
+	 */
+	protected Color other_fill_color = null;
+	
+	protected String label = null;
+	
 	public abstract void setPreferredHeight(double height, ViewI view);
-	public abstract Color getFillColor();
-	public abstract void setFillColor(Color col);
 	public abstract int getActualSlots();
-	public abstract ITrackStyleExtended getAnnotStyle();
 	public abstract void setStyle(ITrackStyleExtended style);
 	public abstract String getLabel();
 	public abstract void setLabel(String str);
-	public abstract Direction getDirection();
 	public abstract void setDirection(Direction d);
 	public abstract void drawMiddle(ViewI view);
 	public abstract Map<String,Class<?>> getPreferences();
 	public abstract void setPreferences(Map<String,Object> preferences);
-	public abstract void copyChildren(ViewModeGlyph temp);
-	public abstract void addMiddleGlyph(GlyphI gl);	
-	public abstract List<SeqSymmetry> getSelected();
 	public abstract boolean initUnloaded();
-	public abstract boolean toolBarHit(Rectangle2D.Double coord_hitbox, ViewI view);
-	public abstract double getChildHeight();
-	public abstract int getStyleDepth();
 	protected abstract boolean shouldDrawToolBar();
 	protected abstract RootSeqSymmetry loadRegion(SeqSpan span);
-	protected abstract ViewModeGlyph createGlyphs(RootSeqSymmetry rootSym, MapViewGlyphFactoryI factory, SeqMapViewExtendedI smv);
 	protected abstract void updateParent(ViewModeGlyph vmg);
-	protected abstract void loadAndDisplayRegion(final SeqMapViewExtendedI smv, final MapViewGlyphFactoryI factory) throws Exception;
+	
+	/**
+	 * Remove all children of the glyph,
+	 * including those added with addMiddleGlyph(GlyphI).
+	 */
+	@Override
+	public void removeAllChildren() {
+		super.removeAllChildren();
+		// also remove all middleground glyphs
+		// this is currently the only place where middleground glyphs are treated as if they were children
+		//   maybe should rename this method clear() or something like that...
+		// only reference to middle glyphs should be in this.middle_glyphs, so should be able to GC them by
+		//     clearing middle_glyphs.  These glyphs never have setScene() called on them,
+		//     so it is not necessary to call setScene(null) on them.
+		middle_glyphs.clear();
+	}
+	
+	protected boolean isAutoLoadMode() {
+		if (this.getAnnotStyle() == null) {
+			return false;
+		}
+
+		if (this.getAnnotStyle().getFeature() == null) {
+			return false;
+		}
+
+		if (this.getAnnotStyle().getFeature().getLoadStrategy() != LoadUtils.LoadStrategy.AUTOLOAD) {
+			return false;
+		}
+		return true;
+	}
+		
+	public boolean isDetail(ViewI view) {
+		return SetSummaryThresholdAction.getAction().isDetail(getAnnotStyle());
+	}
+	
+	@Override
+	public void rangeChanged(NeoRangeEvent evt){
+		if(evt.getSource() instanceof SeqMapViewExtendedI){
+			rangeChanged(((SeqMapViewExtendedI)evt.getSource()));
+		}
+	}
+	
+	protected void rangeChanged(SeqMapViewExtendedI smv){
+		if(isAutoLoadMode() && isDetail(smv.getSeqMap().getView())){
+			try {
+				MapViewGlyphFactoryI factory = MapViewModeHolder.getInstance().getViewFactory(getAnnotStyle().getViewMode());
+				if(factory != null){
+					loadAndDisplayRegion(smv, factory);
+				}
+			} catch (Exception ex) {
+				Logger.getLogger(AbstractViewModeGlyph.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	};
+	
+	/** Returns the color used to draw the tier background, or null
+	if there is no background. */
+	public final Color getFillColor() {
+		return style.getBackground();
+	}
+	
+	/** Sets the color used to fill the tier background, or null if no color
+	 *  @param col  A color, or null if no background color is desired.
+	 */
+	public final void setFillColor(Color col) {
+		if (style.getBackground() != col) {
+			style.setBackground(col);
+		}
+
+		// Now set the "middleground" color based on the fill color
+		if (col == null) {
+			other_fill_color = Color.DARK_GRAY;
+		} else {
+			int intensity = col.getRed() + col.getGreen() + col.getBlue();
+			if (intensity == 0) {
+				other_fill_color = Color.darkGray;
+			} else if (intensity > (255 + 127)) {
+				other_fill_color = col.darker();
+			} else {
+				other_fill_color = col.brighter();
+			}
+		}
+	}
+	
+	public final ITrackStyleExtended getAnnotStyle() {
+		return style;
+	}
+		
+	public final Direction getDirection() {
+		return direction;
+	}
+	
+	public final void copyChildren(ViewModeGlyph temp) {
+		if(temp == null)
+			return;
+		
+		List<GlyphI> childrens = new ArrayList<GlyphI>();
+		childrens.addAll(temp.getChildren());
+
+		for (int i = 0; i < childrens.size(); i++) {
+			addChild(childrens.get(i));
+		}
+		
+		childrens.clear();
+		childrens.addAll(((AbstractViewModeGlyph)temp).middle_glyphs);
+		
+		for (int i = 0; i < childrens.size(); i++) {
+			addMiddleGlyph(childrens.get(i));
+		}
+		
+		//TODO: Set list of all getInfo
+//		if(!(getInfo() instanceof List)){
+//			List<Object> info = new ArrayList<Object>();
+//			info.add(getInfo());
+//			setInfo(info);
+//		}else{
+//			((List)(getInfo())).add(temp.getInfo());
+//		}
+		
+	}
+		
+	/**
+	 *  Adds "middleground" glyphs, which are drawn in front of the background but
+	 *    behind all "real" child glyphs.
+	 *  These are generally not considered children of
+	 *    the glyph.  The TierGlyph will render these glyphs, but they can't be selected since they
+	 *    are not considered children in pickTraversal() method.
+	 *  The only way to remove these is via removeAllChildren() method,
+	 *    there is currently no external access to them.
+	 */
+	public final void addMiddleGlyph(GlyphI gl) {
+		middle_glyphs.add(gl);
+	}
+	
+	public final List<SeqSymmetry> getSelected(){
+	
+		int childCount = getChildCount();
+		List<SeqSymmetry> selectedSyms = new ArrayList<SeqSymmetry>(childCount);
+		for(int i=0;i<childCount;i++){
+			if(getChild(i).isSelected()){
+				if(getChild(i).getInfo() instanceof SeqSymmetry){
+					selectedSyms.add((SeqSymmetry)(getChild(i).getInfo()));
+				}
+			}
+		}
+		return selectedSyms;
+	}
+		
+	public final boolean toolBarHit(Rectangle2D.Double coord_hitbox, ViewI view){
+		if (shouldDrawToolBar() && isVisible() && coord_hitbox.intersects(getCoordBox())) {
+			// overlapping handle ?  (need to do this one in pixel space?)
+			Rectangle hpix = new Rectangle();
+			view.transformToPixels(coord_hitbox, hpix);
+			if (getToolbarPixel(view).intersects(hpix)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	protected Rectangle getToolbarPixel(ViewI view){
+		pixel_hitbox.setBounds(getPixelBox().x + 4, getPixelBox().y + 4, handle_width, handle_width);
+		return pixel_hitbox;
+	}
+		
+	public final double getChildHeight(){
+		double child_height = MapViewGlyphFactoryI.DEFAULT_CHILD_HEIGHT;
+		child_height = useLabel(getAnnotStyle()) ? child_height * 2 : child_height;
+		child_height = child_height + getSpacing() * 2;
+		return child_height;
+	}
+	
+	protected final double getSpacing() {
+		if(getPacker() instanceof AbstractCoordPacker){
+			return ((AbstractCoordPacker)getPacker()).getSpacing();
+		}
+		return 2;
+	}
+	
+	public final int getStyleDepth(){
+		switch(getDirection()){
+			case REVERSE:
+				return getAnnotStyle().getReverseMaxDepth();
+			
+			case FORWARD:
+				return getAnnotStyle().getForwardMaxDepth();
+						
+			default:
+				return getAnnotStyle().getMaxDepth();
+		}
+	}
+	
+	protected final ViewModeGlyph createGlyphs(RootSeqSymmetry rootSym, MapViewGlyphFactoryI factory, SeqMapViewExtendedI smv){
+		return factory.getViewModeGlyph(rootSym, style, direction, smv);
+	};
+	
+	protected final void loadAndDisplayRegion(final SeqMapViewExtendedI smv, final MapViewGlyphFactoryI factory) throws Exception{
+		if (previousWorker != null && !previousWorker.isCancelled() && !previousWorker.isDone()) {
+			previousWorker.cancel(true);
+			previousWorker = null;
+		}
+
+		worker = new SwingWorker() {
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				RootSeqSymmetry rootSym = loadRegion(smv.getVisibleSpan());
+				if (rootSym.getChildCount() > 0) {
+					final ViewModeGlyph vmg = createGlyphs(rootSym, factory, smv);
+					ThreadUtils.runOnEventQueue(new Runnable() {
+
+						public void run() {
+							updateParent(vmg);
+							GeneralLoadUtils.setLastRefreshStatus(style.getFeature(), vmg.getChildCount() > 0);
+							GeneralLoadView.getLoadView().refreshDataManagementView();
+							//TODO: Find a way to avoid this
+							//if (lastUsedGlyph == saveDetailGlyph) {
+							smv.repackTheTiers(true, true);
+							smv.getSeqMap().updateWidget();
+							Application.getSingleton().removeNotLockedUpMsg("Loading " + getAnnotStyle().getTrackName());
+							//}
+						}
+					});
+				}else{
+					Application.getSingleton().removeNotLockedUpMsg("Loading " + getAnnotStyle().getTrackName());
+				}
+				return null;
+			}
+
+		};
+		worker.execute();
+		previousWorker = worker;
+		worker = null;
+	};
+	
 	public boolean isGarbage() {
 		return getChildCount() == 0;
 	}
@@ -85,5 +344,11 @@ public abstract class ViewModeGlyph extends SolidGlyph {
 		w += 4; // padding left
 		java.awt.Dimension minTierSizeInPixels = new java.awt.Dimension(w, h);
 		setMinimumPixelBounds(minTierSizeInPixels);
+	}
+	
+	public static boolean useLabel(ITrackStyleExtended style){
+		return style.getLabelField() != null && 
+		!style.getLabelField().equals(TrackConstants.NO_LABEL) && 
+		(style.getLabelField().trim().length() > 0);
 	}
 }

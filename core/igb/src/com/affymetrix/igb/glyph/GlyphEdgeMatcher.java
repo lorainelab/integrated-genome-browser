029abc916ba2345819379ff9893f26402a288c4d
hiralv <hiralv@uncc.edu>
2012-08-17 19:20:44 +0000

    Use TierGlyph instead of ViewModeGlyph in GlyphEdgeMatcher.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12673 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/glyph/GlyphEdgeMatcher.java
+++ core/igb/src/com/affymetrix/igb/glyph/GlyphEdgeMatcher.java
@@ -184,7 +184,7 @@ public final class GlyphEdgeMatcher  {
   // Result undefined if the glyph is not in a tier.
   GlyphI getTier(GlyphI target) {
     GlyphI p = target;
-    while (p.getParent() != null && ! (p instanceof com.affymetrix.igb.shared.ViewModeGlyph)) {
+    while (p.getParent() != null && ! (p instanceof com.affymetrix.igb.shared.TierGlyph)) {
       p = p.getParent();
     }
     return p;

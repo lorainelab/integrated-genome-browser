17e54bea9658c0c9ea5ea7f358a50e603c7eea1d
fwang4 <fwang4@uncc.edu>
2012-08-27 14:13:51 +0000

    Change 'symbol' to 'gene name' for bed detail, also supports the 'Gene Name' in searching.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_6_7@12750 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- genometryImpl/src/com/affymetrix/genometryImpl/symmetry/UcscBedDetailSym.java
+++ genometryImpl/src/com/affymetrix/genometryImpl/symmetry/UcscBedDetailSym.java
@@ -7,15 +7,15 @@ import java.util.Map;
 import com.affymetrix.genometryImpl.BioSeq;
 
 public class UcscBedDetailSym extends UcscBedSym {
-	private final String symbol;
+	private final String geneName;
 	private String description;
 
 	public UcscBedDetailSym(String type, BioSeq seq, int txMin, int txMax,
 			String name, float score, boolean forward, int cdsMin, int cdsMax,
-			int[] blockMins, int[] blockMaxs, String symbol, String description) {
+			int[] blockMins, int[] blockMaxs, String geneName, String description) {
 		super(type, seq, txMin, txMax, name, score, forward, cdsMin, cdsMax, blockMins,
 				blockMaxs);
-		this.symbol = symbol;
+		this.geneName = geneName;
 		this.description = description;
 	}
 
@@ -27,26 +27,26 @@ public class UcscBedDetailSym extends UcscBedSym {
 		this.description = description;
 	}
 
-	public String getSymbol() {
-		return symbol;
+	public String getGeneName() {
+		return geneName;
 	}
 
 	public Map<String,Object> cloneProperties() {
 		Map<String,Object> tprops = super.cloneProperties();
-		tprops.put("symbol", symbol);
+		tprops.put("gene name", geneName);
 		tprops.put("description", description);
 		return tprops;
 	}
 
 	public Object getProperty(String key) {
-		if (key.equals("symbol")) { return symbol; }
+		if (key.equals("gene name")) { return geneName; }
 		else if (key.equals("description")) { return description; }
 		else return super.getProperty(key);
 	}
 
 	protected void outputAdditional(DataOutputStream out) throws IOException  {
 		out.write('\t');
-		out.write(symbol.getBytes());
+		out.write(geneName.getBytes());
 		out.write('\t');
 		out.write(description.getBytes());
 	}

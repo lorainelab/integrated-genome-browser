cc8265fef2a0c56fb79fc53a0000d4474e2fc169
hiralv <hiralv@uncc.edu>
2012-09-17 16:09:28 +0000

    Removing minimum tier glyph height check for now.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12897 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/tiers/AffyTieredMap.java
+++ core/igb/src/com/affymetrix/igb/tiers/AffyTieredMap.java
@@ -211,10 +211,10 @@ public class AffyTieredMap extends NeoMap {
 				if(mtg instanceof DefaultTierGlyph && !((DefaultTierGlyph)mtg).isHeightFixed()){
 					fixed_coord_height += mtg.getCoordBox().height;
 					//TODO: Glyph.getMinPixelHeight() can be used
-					int pixel_height = mtg.getPixelBox(this.getView()).height;
-					if(TierGlyph.MINIMUM_TIER_HEIGHT > pixel_height){
-						fixed_pixel_height += TierGlyph.MINIMUM_TIER_HEIGHT - pixel_height;
-					}
+//					int pixel_height = mtg.getPixelBox(this.getView()).height;
+//					if(TierGlyph.MINIMUM_TIER_HEIGHT > pixel_height){
+//						fixed_pixel_height += TierGlyph.MINIMUM_TIER_HEIGHT - pixel_height;
+//					}
 				}else{
 					TransformTierGlyph transtier = (TransformTierGlyph) mtg;
 					transtier.fitToPixelHeight(this.getView());
@@ -222,10 +222,10 @@ public class AffyTieredMap extends NeoMap {
 				}
 			} else {
 				fixed_coord_height += mtg.getCoordBox().height;
-				int pixel_height = mtg.getPixelBox(this.getView()).height;
-				if(TierGlyph.MINIMUM_TIER_HEIGHT > pixel_height){
-					fixed_pixel_height += TierGlyph.MINIMUM_TIER_HEIGHT - pixel_height;
-				}
+//				int pixel_height = mtg.getPixelBox(this.getView()).height;
+//				if(TierGlyph.MINIMUM_TIER_HEIGHT > pixel_height){
+//					fixed_pixel_height += TierGlyph.MINIMUM_TIER_HEIGHT - pixel_height;
+//				}
 			}
 			height = mtg.getCoordBox().height;
 			// need to call moveAbsolute to trigger recursive move of

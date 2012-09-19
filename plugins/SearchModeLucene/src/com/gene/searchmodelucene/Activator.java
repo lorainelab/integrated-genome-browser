04e68fc702dc1ac8e1ec875552f74502a3873430
hiralv <hiralv@uncc.edu>
2012-09-19 20:42:22 +0000

    Adding lucene search hints.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12920 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/SearchModeLucene/src/com/gene/searchmodelucene/Activator.java
+++ plugins/SearchModeLucene/src/com/gene/searchmodelucene/Activator.java
@@ -7,17 +7,20 @@ import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTracker;
 
 import com.affymetrix.igb.osgi.service.IGBService;
+import com.affymetrix.igb.shared.ISearchHints;
 import com.affymetrix.igb.shared.ISearchModeSym;
 
 public class Activator implements BundleActivator {
 	private BundleContext bundleContext;
 	private ServiceRegistration<ISearchModeSym> searchModeLuceneRegistration;
-
+	private ServiceRegistration<ISearchHints> searchHints;
+	
 	private void registerService(ServiceReference<IGBService> igbServiceReference) {
         try
         {
         	IGBService igbService = bundleContext.getService(igbServiceReference);
     		searchModeLuceneRegistration = bundleContext.registerService(ISearchModeSym.class, new SearchModeLucene(igbService), null);
+			searchHints = bundleContext.registerService(ISearchHints.class, new SearchHints(), null);
         }
         catch (Exception ex) {
             System.out.println(this.getClass().getName() + " - Exception in Activator.createPage() -> " + ex.getMessage());
@@ -49,5 +52,6 @@ public class Activator implements BundleActivator {
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		searchModeLuceneRegistration.unregister();
+		searchHints.unregister();
 	}
 }

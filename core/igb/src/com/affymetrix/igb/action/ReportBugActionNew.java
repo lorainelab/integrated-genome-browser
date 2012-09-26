4f74ecdadf05dc4db9f7e51dc2e64cecc5aaf7e1
anuj4159 <anuj4159@uncc.edu>
2012-09-26 14:56:23 +0000

    Added New Feature Request and Bug Report Window
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@12937 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/igb/src/com/affymetrix/igb/action/ReportBugActionNew.java
@@ -0,0 +1,42 @@
+package com.affymetrix.igb.action;
+
+import com.affymetrix.genometryImpl.event.GenericAction;
+import com.affymetrix.genometryImpl.event.GenericActionHolder;
+import com.affymetrix.igb.IGB;
+import com.affymetrix.igb.util.BugOrFeatureRequestForm;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sgblanch
+ * @version $Id: ReportBugAction.java 9589 2011-12-20 15:54:10Z lfrohman $
+ */
+public class ReportBugActionNew extends GenericAction {
+	private static final long serialVersionUID = 1l;
+	private static final ReportBugActionNew ACTION = new ReportBugActionNew();
+
+	static{
+		GenericActionHolder.getInstance().addGenericAction(ACTION);
+	}
+	
+	public static ReportBugActionNew getAction() {
+		return ACTION;
+	}
+
+	private ReportBugActionNew() {
+		super("Report a bug", null,
+				"16x16/actions/report bug.png",
+				"22x22/actions/report bug.png",
+				KeyEvent.VK_R, null, true);
+		this.ordinal = 130;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JFrame frame = new BugOrFeatureRequestForm(false);
+		frame.setLocationRelativeTo(IGB.getSingleton().getFrame());
+		frame.setVisible(true);
+	}
+}
---
+++ core/igb/src/com/affymetrix/igb/action/RequestFeatureActionNew.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.affymetrix.igb.action;
+
+import com.affymetrix.genometryImpl.event.GenericAction;
+import com.affymetrix.genometryImpl.event.GenericActionHolder;
+import com.affymetrix.genometryImpl.util.GeneralUtils;
+import com.affymetrix.genometryImpl.util.PreferenceUtils;
+import com.affymetrix.igb.IGB;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import static com.affymetrix.igb.IGBConstants.BUNDLE;
+import com.affymetrix.igb.util.BugOrFeatureRequestForm;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/**
+ *
+ * @author auser
+ */
+public class RequestFeatureActionNew extends GenericAction {
+
+private static final long serialVersionUID = 1l;
+private static final RequestFeatureActionNew ACTION = new RequestFeatureActionNew();
+
+	static{
+		GenericActionHolder.getInstance().addGenericAction(ACTION);
+	}
+
+	public static RequestFeatureActionNew getAction() {
+		return ACTION;
+	}
+
+	private RequestFeatureActionNew() {
+		super(BUNDLE.getString("requestAFeature"), null,
+				"16x16/actions/mail-forward.png",
+				"22x22/actions/mail-forward.png",
+				KeyEvent.VK_R, null, true);
+		this.ordinal = 140;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JFrame frame = new BugOrFeatureRequestForm(true);
+		frame.setLocationRelativeTo(IGB.getSingleton().getFrame());
+		frame.setVisible(true);
+	}
+}

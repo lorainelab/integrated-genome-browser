42758ce0a2994746992842ac29165a0a91819b9d
hiralv <hiralv@uncc.edu>
2012-10-03 17:59:48 +0000

    Adding operations panel in annotations tab.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13054 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/TrackAdjuster/src/com/affymetrix/igb/trackAdjuster/Activator.java
+++ plugins/TrackAdjuster/src/com/affymetrix/igb/trackAdjuster/Activator.java
@@ -1,7 +1,11 @@
 package com.affymetrix.igb.trackAdjuster;
 
+import com.affymetrix.common.ExtensionPointHandler;
+import com.affymetrix.common.ExtensionPointListener;
+import com.affymetrix.genometryImpl.operator.Operator;
 import com.affymetrix.igb.osgi.service.IGBService;
 import com.affymetrix.igb.osgi.service.IGBTabPanel;
+import com.affymetrix.igb.shared.OperationsImpl;
 import com.affymetrix.igb.window.service.WindowActivator;
 import org.osgi.framework.BundleActivator;
 
@@ -9,6 +13,24 @@ public class Activator extends WindowActivator implements BundleActivator {
 
 	@Override
 	protected IGBTabPanel getPage(final IGBService igbService) {
-		return new AnnotationTrackPanel(igbService);
+		AnnotationTrackPanel tabPanel = new AnnotationTrackPanel(igbService);
+		final OperationsImpl trackOperation = new OperationsImpl(igbService);
+		tabPanel.addPanel(trackOperation);
+		
+		ExtensionPointHandler<Operator> operatorExtensionPoint = ExtensionPointHandler.getOrCreateExtensionPoint(bundleContext, Operator.class);
+		operatorExtensionPoint.addListener(new ExtensionPointListener<Operator>() {
+
+			@Override
+			public void addService(Operator operator) {
+				trackOperation.addOperator(operator);
+			}
+
+			@Override
+			public void removeService(Operator operator) {
+				trackOperation.removeOperator(operator);
+			}
+		});
+		
+		return tabPanel;
 	}
 }

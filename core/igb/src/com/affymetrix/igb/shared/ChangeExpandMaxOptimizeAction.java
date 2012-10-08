022459465445766ef320c9504219d1b98eee8f39
anuj4159 <anuj4159@uncc.edu>
2012-10-08 16:59:26 +0000

    Deleted "CopyExpandMaxOptimize" Action from Shortcuts Tab
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13154 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/shared/ChangeExpandMaxOptimizeAction.java
+++ core/igb/src/com/affymetrix/igb/shared/ChangeExpandMaxOptimizeAction.java
@@ -17,10 +17,6 @@ public class ChangeExpandMaxOptimizeAction extends ChangeExpandMaxActionA {
 	private static final long serialVersionUID = 1L;
 	private static final ChangeExpandMaxOptimizeAction ACTION
 			= new ChangeExpandMaxOptimizeAction();
-
-	static{
-		GenericActionHolder.getInstance().addGenericAction(ACTION);
-	}
 	
 	public static ChangeExpandMaxOptimizeAction getAction() {
 		return ACTION;

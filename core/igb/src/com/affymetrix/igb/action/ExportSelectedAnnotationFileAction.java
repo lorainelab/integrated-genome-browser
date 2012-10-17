a13cf0eec36bb9b83a54a6d7c3a6238767129054
anuj4159 <anuj4159@uncc.edu>
2012-10-17 15:15:57 +0000

    Added "Save Selected Annotations to a File" action to file menu
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13269 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ExportSelectedAnnotationFileAction.java
+++ core/igb/src/com/affymetrix/igb/action/ExportSelectedAnnotationFileAction.java
@@ -4,11 +4,16 @@ import java.awt.event.KeyEvent;
 import java.io.DataOutputStream;
 
 import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.GenometryModel;
 import com.affymetrix.genometryImpl.event.GenericActionHolder;
+import com.affymetrix.genometryImpl.event.SymSelectionEvent;
 import com.affymetrix.genometryImpl.parsers.AnnotationWriter;
+import com.affymetrix.genoviz.bioviews.Glyph;
 import com.affymetrix.igb.shared.TierGlyph;
 
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
+import com.affymetrix.igb.IGBServiceImpl;
+import java.util.List;
 
 public class ExportSelectedAnnotationFileAction extends AbstractExportFileAction{
 	private static final long serialVersionUID = 1L;
@@ -16,6 +21,8 @@ public class ExportSelectedAnnotationFileAction extends AbstractExportFileAction
 	
 	static{
 		GenericActionHolder.getInstance().addGenericAction(ACTION);
+		ACTION.setEnabled(false);
+		GenometryModel.getGenometryModel().addSymSelectionListener(ACTION);
 	}
 	
 	public static ExportSelectedAnnotationFileAction getAction() {
@@ -23,11 +30,15 @@ public class ExportSelectedAnnotationFileAction extends AbstractExportFileAction
 	}
 				
 	private ExportSelectedAnnotationFileAction() {
-		super(BUNDLE.getString("saveSelectedAnnotationAction"), null, null, null, KeyEvent.VK_UNDEFINED, null, true);
+		super(BUNDLE.getString("saveSelectedAnnotationAction"), null, "16x16/actions/blank_placeholder.png", null, KeyEvent.VK_UNDEFINED, null, true);
 	}
 
 	@Override
 	protected void exportFile(AnnotationWriter annotationWriter, DataOutputStream dos, BioSeq aseq, TierGlyph atier) throws java.io.IOException{
 		annotationWriter.writeAnnotations(atier.getSelected(), aseq, "", dos);
+	}
+	public void symSelectionChanged(SymSelectionEvent evt){
+		List<Glyph> answer = IGBServiceImpl.getInstance().getSelectedTierGlyphs();
+		ExportSelectedAnnotationFileAction.getAction().setEnabled((1 == answer.size()) && !(((TierGlyph)answer.get(0)).getSelected().isEmpty()));
 	}
 }

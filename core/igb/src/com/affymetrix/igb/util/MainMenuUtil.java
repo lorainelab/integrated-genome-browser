4cf7b1ebeb19c1fcfc7805a476229b96f87fe503
anuj4159 <anuj4159@uncc.edu>
2012-10-17 19:50:02 +0000

    Removed "Save Selected Annotations To a File" from File Menu
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13284 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/util/MainMenuUtil.java
+++ core/igb/src/com/affymetrix/igb/util/MainMenuUtil.java
@@ -67,7 +67,6 @@ public class MainMenuUtil {
 		MenuUtil.addToMenu(file_menu, new JRPMenuItem(id + "_main_fileMenu_printFrame", PrintFrameAction.getAction()));
 		MenuUtil.addToMenu(file_menu, new JRPMenuItem(id + "_main_fileMenu_exportImage", ExportImageAction.getAction()));
 		MenuUtil.addToMenu(file_menu, new JRPMenuItem(id + "_main_fileMenu_exportFile", ExportFileAction.getAction()));
-		MenuUtil.addToMenu(file_menu, new JRPMenuItem(id + "_main_fileMenu_exportSelectedAnnotationFile", ExportSelectedAnnotationFileAction.getAction()));
 		MenuUtil.addToMenu(file_menu, new JRPMenuItem(id + "_main_fileMenu_closeTracks", CloseTracksAction.getAction()));
 		file_menu.addSeparator();
 		MenuUtil.addToMenu(file_menu, new JRPMenuItem(id + "_main_fileMenu_preferences", PreferencesAction.getAction()));

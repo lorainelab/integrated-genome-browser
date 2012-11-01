d23c36975b0525fa905b0c2a377ac6ccc9f8abf0
hiralv <hiralv@uncc.edu>
2012-11-01 14:33:37 +0000

    Removing unused code and variable.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13410 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/BigWigHandler/src/com/gene/bigwighandler/Activator.java
+++ plugins/BigWigHandler/src/com/gene/bigwighandler/Activator.java
@@ -1,11 +1,7 @@
 package com.gene.bigwighandler;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTracker;
@@ -13,43 +9,13 @@ import org.osgi.util.tracker.ServiceTracker;
 import com.affymetrix.common.CommonUtils;
 import com.affymetrix.genometryImpl.parsers.FileTypeHandler;
 import com.affymetrix.igb.osgi.service.IGBService;
-import com.affymetrix.igb.shared.MapTierGlyphFactoryI;
 
 public class Activator implements BundleActivator {
 	private BundleContext bundleContext;
 	private ServiceRegistration<FileTypeHandler> bigwigHandlerRegistration;
-	private ServiceRegistration<MapTierGlyphFactoryI> annotationBigwigSemanticZoomGlyphFactoryRegistration;
-	private ServiceRegistration<MapTierGlyphFactoryI> alignmentBigwigSemanticZoomGlyphFactoryRegistration;
-	private ServiceRegistration<MapTierGlyphFactoryI> graphBigwigSemanticZoomGlyphFactoryRegistration;
-	private MapTierGlyphFactoryI annotationGlyphFactory = null;
-	private MapTierGlyphFactoryI alignmentGlyphFactory = null;
-	private MapTierGlyphFactoryI graphGlyphFactory = null;
-    private ServiceTracker<MapTierGlyphFactoryI,Object> serviceTrackerMapViewGlyphFactoryI = null;
-	private boolean factoryCreated = false;
-
-	private void checkReference(ServiceReference<MapTierGlyphFactoryI> reference) {
-    	MapTierGlyphFactoryI factory = bundleContext.getService(reference);
-		if ("annotation".equals(factory.getName())) {
-			annotationGlyphFactory = factory;
-		}
-		else if ("alignment".equals(factory.getName())) {
-			alignmentGlyphFactory = factory;
-		}
-		else if ("stairstepgraph".equals(factory.getName())) {
-			graphGlyphFactory = factory;
-		}
-	}
 
 	private void registerServices(final IGBService igbService) {
 		bigwigHandlerRegistration = bundleContext.registerService(FileTypeHandler.class, new BigWigHandler(), null);
-		try {
-			for (ServiceReference<MapTierGlyphFactoryI> reference : bundleContext.getServiceReferences(MapTierGlyphFactoryI.class, null)) {
-				checkReference(reference);
-			}
-		}
-		catch (InvalidSyntaxException x) {
-			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "could not get MapViewGlyphFactoryI service references", x);
-		}
 	}
 
 	@Override
@@ -81,14 +47,5 @@ public class Activator implements BundleActivator {
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		bigwigHandlerRegistration.unregister();
-		if (annotationBigwigSemanticZoomGlyphFactoryRegistration != null) {
-			annotationBigwigSemanticZoomGlyphFactoryRegistration.unregister();
-		}
-		if (alignmentBigwigSemanticZoomGlyphFactoryRegistration != null) {
-			alignmentBigwigSemanticZoomGlyphFactoryRegistration.unregister();
-		}
-		if (graphBigwigSemanticZoomGlyphFactoryRegistration != null) {
-			graphBigwigSemanticZoomGlyphFactoryRegistration.unregister();
-		}
 	}
 }

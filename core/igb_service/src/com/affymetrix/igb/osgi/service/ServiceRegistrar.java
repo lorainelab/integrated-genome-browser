f0b830a7ec4fa3474b605aa5ae37881809e5c6a1
hiralv <hiralv@uncc.edu>
2012-11-01 14:44:35 +0000

    Creating service registrar class.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13411 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/igb_service/src/com/affymetrix/igb/osgi/service/ServiceRegistrar.java
@@ -0,0 +1,69 @@
+package com.affymetrix.igb.osgi.service;
+
+import com.affymetrix.common.CommonUtils;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ *
+ * @author hiralv
+ */
+public abstract class ServiceRegistrar implements BundleActivator {
+	protected BundleContext bundleContext;
+	protected ServiceRegistration<?> registrations[];
+	
+	private void registerService(ServiceReference<IGBService> igbServiceReference) {
+        try
+        {
+        	IGBService igbService = bundleContext.getService(igbServiceReference);
+			registerService(igbService);
+        }
+        catch (Exception ex) {
+            System.out.println(this.getClass().getName() + " - Exception in Activator.registerService() -> " + ex.getMessage());
+            ex.printStackTrace(System.out);
+        }
+	}
+
+	protected abstract ServiceRegistration<?>[] registerService(IGBService igbService) throws Exception;
+	
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		this.bundleContext = bundleContext;
+		if (CommonUtils.getInstance().isExit(bundleContext)) {
+    		return;
+    	}
+		
+    	ServiceReference<IGBService> igbServiceReference = bundleContext.getServiceReference(IGBService.class);
+
+        if (igbServiceReference != null)
+        {
+        	registerService(igbServiceReference);
+        }
+        else
+        {
+        	ServiceTracker<IGBService, Object> serviceTracker = new ServiceTracker<IGBService, Object>(bundleContext, IGBService.class.getName(), null) {
+				@Override
+        	    public Object addingService(ServiceReference<IGBService> igbServiceReference) {
+        	    	registerService(igbServiceReference);
+        	        return super.addingService(igbServiceReference);
+        	    }
+        	};
+        	serviceTracker.open();
+        }
+    }
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		if(registrations != null){
+			for(ServiceRegistration registration : registrations){
+				registration.unregister();
+			}
+			registrations = null;
+		}
+		bundleContext = null;
+	}
+}
+

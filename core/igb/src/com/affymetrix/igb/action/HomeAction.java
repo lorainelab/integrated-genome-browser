7a686df81df65e26801a219e36a39c793edb157e
hiralv <hiralv@uncc.edu>
2012-11-19 18:00:53 +0000

    Renaming text and tool tip for home action.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13567 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/HomeAction.java
+++ core/igb/src/com/affymetrix/igb/action/HomeAction.java
@@ -19,7 +19,7 @@ public class HomeAction extends GenericAction{
 	}
 
 	private HomeAction() {
-		super("Home Position", "Zoom out fully", "16x16/actions/home.png",
+		super("Return to home screen", "Returns to home screen", "16x16/actions/home.png",
 				"22x22/actions/home.png", KeyEvent.VK_UNDEFINED);
 	}
 

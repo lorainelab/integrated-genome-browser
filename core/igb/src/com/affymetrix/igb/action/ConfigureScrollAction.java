44537e04c1060f1659ec7a17f7e18ca9d3987227
hiralv <hiralv@uncc.edu>
2012-11-20 15:07:01 +0000

    Remove start, stop and bases per pixel from configure. Also adding apply button.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13572 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ConfigureScrollAction.java
+++ core/igb/src/com/affymetrix/igb/action/ConfigureScrollAction.java
@@ -5,7 +5,6 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyAdapter;
-import java.awt.geom.Rectangle2D;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -26,11 +25,8 @@ public class ConfigureScrollAction extends SeqMapViewActionA {
 	/*
 	 *  units to scroll are either in pixels or bases
 	 */
-	private int as_bases_per_pix = 75;
 	private int as_pix_to_scroll = 4;
 	private int as_time_interval = 20;
-	private int as_start_pos = 0;
-	private int as_end_pos;
 	private AutoScroll autoScroll;
 
 	private static final ConfigureScrollAction ACTION = new ConfigureScrollAction();
@@ -71,96 +67,58 @@ public class ConfigureScrollAction extends SeqMapViewActionA {
 		
 		JPanel pan = new JPanel();
 
-		Rectangle2D.Double cbox = seqMapView.getSeqMap().getViewBounds();
-		int bases_in_view = (int) cbox.width;
-		as_start_pos = (int) cbox.x;
-		as_end_pos = seqMapView.getViewSeq().getLength();
-		int pixel_width = seqMapView.getSeqMap().getView().getPixelBox().width;
-		as_bases_per_pix = bases_in_view / pixel_width;
-
-		// as_bases_per_pix *should* be a float, or else should simply
-		// use the current resolution without asking the user,
-		// but since it is an integer, we have to set the minimum value as 1
-		if (as_bases_per_pix < 1) {
-			as_bases_per_pix = 1;
-		}
-
-		final JRPTextField bases_per_pixTF = new JRPTextField("AutoScrollAction_bases_per_pix", "" + as_bases_per_pix);
 		final JRPTextField pix_to_scrollTF = new JRPTextField("AutoScrollAction_pix_to_scroll", "" + as_pix_to_scroll);
 		final JRPTextField time_intervalTF = new JRPTextField("AutoScrollAction_time_interval", "" + as_time_interval);
-		final JRPTextField start_posTF = new JRPTextField("AutoScrollAction_start_pos", "" + as_start_pos);
-		final JRPTextField end_posTF = new JRPTextField("AutoScrollAction_end_pos", "" + as_end_pos);
 
 		float bases_per_minute = (float) // 1000 ==> ms/s , 60 ==> s/minute, as_time_interval ==> ms/scroll
-				(1.0 * as_bases_per_pix * as_pix_to_scroll * 1000 * 60 / as_time_interval);
+				(1.0 * autoScroll.get_bases_per_pix() * as_pix_to_scroll * 1000 * 60 / as_time_interval);
 		bases_per_minute = Math.abs(bases_per_minute);
 		float minutes_per_seq = seqMapView.getViewSeq().getLength() / bases_per_minute;
 		final JLabel bases_per_minuteL = new JLabel("" + (bases_per_minute / 1000000));
 		final JLabel minutes_per_seqL = new JLabel("" + (minutes_per_seq));
 
-		pan.setLayout(new GridLayout(8, 3));
-		pan.add(new JLabel("<html><B>  Key </B></html>"));
-		pan.add(new JLabel("<html><B>  Value </B></html>"));
-		pan.add(new JLabel("<html><B>  [Current Value] </B></html>"));
-		pan.add(new JLabel("Resolution (bases per pixel)"));
-		pan.add(bases_per_pixTF);
-		pan.add(new JLabel("[" + autoScroll.get_bases_per_pix() + "]"));
+		pan.setLayout(new GridLayout(4, 2));
 		pan.add(new JLabel("Scroll increment (pixels)"));
 		pan.add(pix_to_scrollTF);
-		pan.add(new JLabel("[" + autoScroll.get_pix_to_scroll() + "]"));
-		pan.add(new JLabel("Starting base position"));
-		pan.add(start_posTF);
-		pan.add(new JLabel("[" + autoScroll.get_start_pos() + "]"));
-		pan.add(new JLabel("Ending base position"));
-		pan.add(end_posTF);
-		pan.add(new JLabel("[" + autoScroll.get_end_pos() + "]"));
 		pan.add(new JLabel("Time interval (milliseconds)"));
 		pan.add(time_intervalTF);
-		pan.add(new JLabel("[" + autoScroll.get_time_interval() + "]"));
 		pan.add(new JLabel("Megabases per minute:  "));
 		pan.add(bases_per_minuteL);
-		pan.add(new JLabel(""));
 		pan.add(new JLabel("Total minutes for seq:  "));
 		pan.add(minutes_per_seqL);
-		pan.add(new JLabel(""));
 
 		KeyAdapter kl = new KeyAdapter() {
 			
 			@Override
 			public void keyReleased(KeyEvent e) {
-				as_bases_per_pix = normalizeTF(bases_per_pixTF, as_bases_per_pix, 1, Integer.MAX_VALUE);
 				as_pix_to_scroll = normalizeTF(pix_to_scrollTF, as_pix_to_scroll, -1000, 1000);
 				as_time_interval = normalizeTF(time_intervalTF, as_time_interval, 1, 1000);
-				as_end_pos = normalizeTF(end_posTF, as_end_pos, 1, seqMapView.getViewSeq().getLength());
-				as_start_pos = normalizeTF(start_posTF, as_start_pos, 0, as_end_pos);
-
+				
 				float bases_per_minute = (float) // 1000 ==> ms/s , 60 ==> s/minute, as_time_interval ==> ms/scroll
-						(1.0 * as_bases_per_pix * as_pix_to_scroll * 1000 * 60 / as_time_interval);
+						(1.0 * autoScroll.get_bases_per_pix() * as_pix_to_scroll * 1000 * 60 / as_time_interval);
 				bases_per_minute = Math.abs(bases_per_minute);
-				float minutes_per_seq = (as_end_pos - as_start_pos) / bases_per_minute;
+				float minutes_per_seq = (autoScroll.get_end_pos() - autoScroll.get_start_pos()) / bases_per_minute;
 				bases_per_minuteL.setText("" + (bases_per_minute / 1000000));
 				minutes_per_seqL.setText("" + (minutes_per_seq));
 			}
 		};
 
-		bases_per_pixTF.addKeyListener(kl);
 		pix_to_scrollTF.addKeyListener(kl);
 		time_intervalTF.addKeyListener(kl);
-		start_posTF.addKeyListener(kl);
-		end_posTF.addKeyListener(kl);
 		
-		Object[] options = {"Start", "Cancel"};
+		Object[] options = {"Start", "Apply", "Cancel"};
 		int val = JOptionPane.showOptionDialog(seqMapView, pan, "AutoScroll Parameters",
-				JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.YES_NO_CANCEL_OPTION,
 				JOptionPane.PLAIN_MESSAGE,
 				null, options, options[0]);
-		if (val == JOptionPane.OK_OPTION) {
-			as_bases_per_pix = normalizeTF(bases_per_pixTF, as_bases_per_pix, 1, Integer.MAX_VALUE);
+		if (val == JOptionPane.YES_OPTION || val == JOptionPane.NO_OPTION) {
 			as_pix_to_scroll = normalizeTF(pix_to_scrollTF, as_pix_to_scroll, -1000, 1000);
 			as_time_interval = normalizeTF(time_intervalTF, as_time_interval, 1, 1000);
+			autoScroll.configure(as_pix_to_scroll, as_time_interval);
 			
-			autoScroll.configure(as_bases_per_pix, as_pix_to_scroll, as_time_interval, as_start_pos, as_end_pos);
-			StartAutoScrollAction.getAction().start();
+			if(val == JOptionPane.YES_OPTION){
+				StartAutoScrollAction.getAction().start();
+			}
 		}
 	}
 
--- core/igb/src/com/affymetrix/igb/action/StartAutoScrollAction.java
+++ core/igb/src/com/affymetrix/igb/action/StartAutoScrollAction.java
@@ -1,9 +1,9 @@
 package com.affymetrix.igb.action;
 
+import java.awt.geom.Rectangle2D;
 import java.awt.event.ActionEvent;
 import com.affymetrix.genometryImpl.event.GenericActionHolder;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
-import java.awt.geom.Rectangle2D;
 
 /**
  *
@@ -30,21 +30,26 @@ public class StartAutoScrollAction extends SeqMapViewActionA {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		super.actionPerformed(e);
-		
+		start();
+	}
+	
+	public void start(){
+		// Calculate start, end and bases per pixels
 		Rectangle2D.Double cbox = getTierMap().getViewBounds();
 		int bases_in_view = (int) cbox.width;
 		int start_pos = (int) cbox.x;
 		int end_pos = getSeqMapView().getViewSeq().getLength();
 		int pixel_width = getTierMap().getView().getPixelBox().width;
 		int bases_per_pix = bases_in_view / pixel_width;
+		
+		// as_bases_per_pix *should* be a float, or else should simply
+		// use the current resolution without asking the user,
+		// but since it is an integer, we have to set the minimum value as 1
 		if (bases_per_pix < 1) {
 			bases_per_pix = 1;
 		}
+		
 		getSeqMapView().getAutoScroll().configure(bases_per_pix, start_pos, end_pos);
-		start();
-	}
-	
-	public void start(){
 		getSeqMapView().getAutoScroll().start(this.getTierMap());
 		setEnabled(false);
 		StopAutoScrollAction.getAction().setEnabled(true);

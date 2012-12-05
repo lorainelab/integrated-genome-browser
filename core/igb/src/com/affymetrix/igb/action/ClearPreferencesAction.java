6015842fd6ecda3601f987366d4665a81e7a016f
hiralv <hiralv@uncc.edu>
2012-12-05 16:04:04 +0000

    Show clear preferences on top of preferences window.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13669 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ClearPreferencesAction.java
+++ core/igb/src/com/affymetrix/igb/action/ClearPreferencesAction.java
@@ -12,6 +12,7 @@ import com.affymetrix.genometryImpl.util.PreferenceUtils;
 import com.affymetrix.genoviz.util.ErrorHandler;
 import com.affymetrix.igb.Application;
 import com.affymetrix.igb.IGB;
+import com.affymetrix.igb.prefs.PreferencesPanel;
 import com.affymetrix.igb.stylesheet.XmlStylesheetParser;
 
 public class ClearPreferencesAction extends GenericAction {
@@ -37,7 +38,7 @@ public class ClearPreferencesAction extends GenericAction {
 		// that "No" is the default choice.
 		String[] options = {"Yes", "No"};
 		if (JOptionPane.YES_OPTION == JOptionPane.showOptionDialog(
-				Application.getSingleton().getFrame(), "Really reset all preferences to defaults?\n(this will also exit the application)", "Clear preferences?",
+				PreferencesPanel.getSingleton(), "Really reset all preferences to defaults?\n(this will also exit the application)", "Clear preferences?",
 				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
 				options, options[1])) {
 

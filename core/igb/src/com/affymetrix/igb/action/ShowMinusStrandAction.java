01d453c93b69b17dce37c727f8081ff44909f065
anuj4159 <anuj4159@uncc.edu>
2012-12-05 17:47:25 +0000

    Added All in the label of committed actions
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13675 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ShowMinusStrandAction.java
+++ core/igb/src/com/affymetrix/igb/action/ShowMinusStrandAction.java
@@ -20,7 +20,7 @@ public class ShowMinusStrandAction extends GenericAction {
 	}
 
 	private ShowMinusStrandAction() {
-		super("Show (-) tiers", "16x16/actions/blank_placeholder.png", null);
+		super("Show All (-) tiers", "16x16/actions/blank_placeholder.png", null);
 		this.putValue(SELECTED_KEY, AffyTieredMap.isShowMinus());
 	}
 

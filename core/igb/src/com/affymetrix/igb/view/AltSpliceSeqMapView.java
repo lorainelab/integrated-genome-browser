d832f4c0881aaf183f2fdb7745c91ec777e075f5
fwang4 <fwang4@uncc.edu>
2012-12-21 20:01:48 +0000

    Fix a typo. BUG-383
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@13799 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/view/AltSpliceSeqMapView.java
+++ core/igb/src/com/affymetrix/igb/view/AltSpliceSeqMapView.java
@@ -99,7 +99,7 @@ final class AltSpliceSeqMapView extends SeqMapView implements SeqMapRefreshed {
 			style2Style.put(style, style_copy);
 		}else{
 			// Properties that needs to be retained.
-			boolean show = style.getShow();
+			boolean show = style_copy.getShow();
 			
 			// Apply all properties.
 			style_copy.copyPropertiesFrom(style);

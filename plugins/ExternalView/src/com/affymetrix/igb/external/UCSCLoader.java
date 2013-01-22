e5a6cc56a79eb2f2d3aaac61050c6154467c7174
hiralv <hiralv@uncc.edu>
2013-01-22 17:32:22 +0000

    Refactoring ucsc finder.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_7_0@14065 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/ExternalView/src/com/affymetrix/igb/external/UCSCLoader.java
+++ plugins/ExternalView/src/com/affymetrix/igb/external/UCSCLoader.java
@@ -20,7 +20,11 @@ import javax.imageio.ImageIO;
 public class UCSCLoader extends BrowserLoader {
 
 	private static final Pattern fileNamePattern = Pattern.compile("(hgt_genome.*png)");
-
+	private static final Pattern sideNamePattern = Pattern.compile("(side_genome.*png)");
+	
+	private static final String fileNameBaseUrl = "http://genome.ucsc.edu/trash/hgt/";
+	private static final String sideNameBaseUrl = "http://genome.ucsc.edu/trash/hgtSide/";
+	
 	public String getUrlForView(Loc loc, int pixWidth) {
 		String width = "pix=" + pixWidth + "&";
 		return "http://genome.ucsc.edu/cgi-bin/hgTracks?" + width +"db="+loc.db+"&position="+loc.chr+":"+loc.start+"-"+loc.end;
@@ -28,7 +32,7 @@ public class UCSCLoader extends BrowserLoader {
 
 	public ImageError getImage(Loc loc, int pixWidth, Map<String, String> cookies) {
 		String url = getUrlForView(loc, pixWidth);
-		url = getImageUrl(url, UCSCView.UCSCUSERID + "=" + cookies.get(UCSCView.UCSCUSERID), new UCSCURLFinder());
+		url = getImageUrl(url, UCSCView.UCSCUSERID + "=" + cookies.get(UCSCView.UCSCUSERID), new UCSCURLFinder(fileNameBaseUrl, fileNamePattern));
 		if (url.startsWith("http")) {
 			try {
 				return new ImageError(ImageIO.read(new URL(url)),"");
@@ -40,15 +44,22 @@ public class UCSCLoader extends BrowserLoader {
 	}
 
 	class UCSCURLFinder implements URLFinder {
-
+		final String base_url;
+		final Pattern pattern;
+		
+		UCSCURLFinder(String base_url, Pattern pattern){
+			this.base_url = base_url;
+			this.pattern = pattern;
+		}
+		
 		public String findUrl(BufferedReader reader, URL redirectedUrl) throws IOException {
-			String inputLine = "";
+			String inputLine;
 			while ((inputLine = reader.readLine()) != null) {
-				Matcher m = fileNamePattern.matcher(inputLine);
+				Matcher m = pattern.matcher(inputLine);
 				if (m.find() && m.groupCount() == 1) {
-					Logger.getLogger(UCSCLoader.class.getName()).log(Level.FINE, "found fileName " + inputLine);
+					Logger.getLogger(UCSCLoader.class.getName()).log(Level.FINE, "found fileName {0}", inputLine);
 					String fileName = m.group(1);
-					return "http://genome.ucsc.edu/trash/hgt/" + fileName;
+					return  base_url + fileName;
 				}
 			}
 			return MessageFormat.format(ExternalViewer.BUNDLE.getString("findImageURLError"), "");

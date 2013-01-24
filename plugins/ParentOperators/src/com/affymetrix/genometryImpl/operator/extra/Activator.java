7aed202e5142248e38f6749ee138a35c8b5560ef
hiralv <hiralv@uncc.edu>
2013-01-24 19:49:09 +0000

    Creating a parent operators plugin
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_7_0@14108 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ plugins/ParentOperators/src/com/affymetrix/genometryImpl/operator/extra/Activator.java
@@ -0,0 +1,22 @@
+package com.affymetrix.genometryImpl.operator.extra;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.ServiceRegistration;
+
+import com.affymetrix.genometryImpl.operator.Operator;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import com.affymetrix.igb.osgi.service.IGBService;
+import com.affymetrix.igb.osgi.service.ServiceRegistrar;
+
+public class Activator extends ServiceRegistrar implements BundleActivator {
+	
+	@Override
+	protected ServiceRegistration<?>[] registerService(IGBService igbService) throws Exception {
+		return new ServiceRegistration[] {
+			bundleContext.registerService(Operator.class, new ParentNotOperator(FileTypeCategory.Annotation), null),
+			bundleContext.registerService(Operator.class, new ParentNotOperator(FileTypeCategory.Alignment), null),
+			bundleContext.registerService(Operator.class, new ParentNotOperator(FileTypeCategory.ProbeSet), null)
+		};
+    }
+
+}

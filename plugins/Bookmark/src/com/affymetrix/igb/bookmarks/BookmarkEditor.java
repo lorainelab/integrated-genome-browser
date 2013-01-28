22763026497fbdec1c53d6469f2f24434da2636b
fwang4 <fwang4@uncc.edu>
2013-01-28 22:29:39 +0000

    Allow user to continue working on IGB main view when 'Add Bookmark' dialog is open. IGBD 191
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14127 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/Bookmark/src/com/affymetrix/igb/bookmarks/BookmarkEditor.java
+++ plugins/Bookmark/src/com/affymetrix/igb/bookmarks/BookmarkEditor.java
@@ -4,6 +4,9 @@ import com.affymetrix.genometryImpl.util.ErrorHandler;
 import com.affymetrix.genometryImpl.util.PreferenceUtils;
 import com.affymetrix.igb.bookmarks.action.AddBookmarkAction;
 import com.affymetrix.igb.bookmarks.action.BookmarkActionManager;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.util.logging.Level;
 import javax.swing.*;
@@ -23,6 +26,8 @@ public class BookmarkEditor {
 	private static JRadioButton positionDataB;
 	private static JCheckBox useDefaultName;
 	private static ButtonGroup group;
+	private static JButton cancelButton;
+	private static JButton okButton;
 	private static JOptionPane op;
 	private static JScrollPane scrollpane;
 	private static JDialog dialog;
@@ -45,6 +50,24 @@ public class BookmarkEditor {
 			commentField.setWrapStyleWord(true);
 			positionOnlyB = new JRadioButton("Position Only");
 			positionDataB = new JRadioButton("Position and Data", true);
+
+			cancelButton = new JButton("Cancel");
+			okButton = new JButton("OK");
+			okButton.addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent e) {
+					addBookmark();
+					dialog.dispose();
+				}
+			});
+
+			cancelButton.addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent e) {
+					dialog.dispose();
+				}
+			});
+
 			useDefaultName = PreferenceUtils.createCheckBox(PREF_USE_DEFAULT_NAME, PreferenceUtils.getTopNode(),
 					PREF_USE_DEFAULT_NAME, defaultUseDefaultName);
 			useDefaultName.addActionListener(new java.awt.event.ActionListener() {
@@ -58,6 +81,7 @@ public class BookmarkEditor {
 			group.add(positionDataB);
 			scrollpane = new JScrollPane(commentField);
 			scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+			scrollpane.setMinimumSize(new Dimension(100, 120));
 			op = new JOptionPane(null, JOptionPane.PLAIN_MESSAGE,
 					JOptionPane.CANCEL_OPTION, null, null);
 		}
@@ -69,13 +93,74 @@ public class BookmarkEditor {
 	private static void initDialog() {
 		setNameField();
 		commentField.setText("");
-		op.setMessage(new Object[]{"", useDefaultName,
-					"Name:", nameField, "Comment:", scrollpane,
-					positionOnlyB, positionDataB});
-		dialog = op.createDialog("Enter Bookmark Information...");
+		dialog = new JDialog();
+
+		dialog.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		dialog.setTitle("Enter Bookmark Information...");
+		c.gridx = 0;
+		c.gridy = 0;
+		c.insets = new Insets(0, 5, 0, 0);
+		c.anchor = GridBagConstraints.FIRST_LINE_START;
+		dialog.add(useDefaultName, c);
+		c.gridx = 0;
+		c.gridy = 1;
+		c.insets = new Insets(0, 8, 0, 0);
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(new JLabel("Name:"), c);
+		c.gridx = 0;
+		c.gridy = 2;
+		c.insets = new Insets(0, 5, 0, 5);
+		c.anchor = GridBagConstraints.LINE_START;
+		c.weightx = 1.0;
+		c.gridwidth = 5;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		dialog.add(nameField, c);
+		c.gridx = 0;
+		c.gridy = 3;
+		c.insets = new Insets(0, 8, 0, 0);
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(new JLabel("Comment:"), c);
+		c.gridx = 0;
+		c.gridy = 4;
+		c.insets = new Insets(0, 8, 0, 8);
+		c.anchor = GridBagConstraints.LINE_START;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		c.gridwidth = 3;
+		c.fill = GridBagConstraints.BOTH;
+		dialog.add(scrollpane, c);
+		c.gridx = 0;
+		c.gridy = 5;
+		c.weighty = 0;
+		c.insets = new Insets(0, 5, 0, 0);
+		c.anchor = GridBagConstraints.LINE_START;
+		c.fill = GridBagConstraints.NONE;
+		dialog.add(positionOnlyB, c);
+		c.gridx = 0;
+		c.gridy = 6;
+		c.insets = new Insets(0, 5, 0, 0);
+		c.weighty = 0;
+		c.anchor = GridBagConstraints.LINE_START;
+		c.fill = GridBagConstraints.NONE;
+		dialog.add(positionDataB, c);
+
+		JPanel okCancelButtonPanel = new JPanel(new FlowLayout());
+		okCancelButtonPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		okCancelButtonPanel.add(cancelButton);
+		okCancelButtonPanel.add(okButton);
+		c.gridx = 0;
+		c.gridy = 7;
+		c.weighty = 0;
+		c.anchor = GridBagConstraints.LAST_LINE_END;
+		dialog.add(okCancelButtonPanel, c);
+
 		dialog.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+		dialog.setLocationRelativeTo(null);
 		dialog.setAlwaysOnTop(true);
 		dialog.setResizable(true);
+		dialog.setModal(false);
 		dialog.setVisible(true);
 		dialog.pack();
 	}
@@ -93,37 +178,30 @@ public class BookmarkEditor {
 	 */
 	public static void run() {
 		initDialog();
+	}
 
-		int result = JOptionPane.CANCEL_OPTION;
-
-		if (op.getValue() != null) {
-			result = (Integer) op.getValue();
+	private static void addBookmark() {
+		if (positionDataB.isSelected()) {
+			// create a new bookmark includes position and data
+			// otherwise, the bookmark is just position only
+			try {
+				bookmark = BookmarkController.getCurrentBookmark(true,
+						BookmarkActionManager.getInstance().getVisibleSpan());
+			} catch (MalformedURLException m) {
+				ErrorHandler.errorPanel("Couldn't add bookmark", m, Level.SEVERE);
+				return;
+			}
 		}
 
-		if (result == JOptionPane.OK_OPTION) {
-			if (positionDataB.isSelected()) {
-				// create a new bookmark includes position and data
-				// otherwise, the bookmark is just position only
-				try {
-					bookmark = BookmarkController.getCurrentBookmark(true,
-							BookmarkActionManager.getInstance().getVisibleSpan());
-				} catch (MalformedURLException m) {
-					ErrorHandler.errorPanel("Couldn't add bookmark", m, Level.SEVERE);
-					return;
-				}
-			}
+		String name = nameField.getText();
+		String comment = commentField.getText();
 
-			String name = nameField.getText();
-			String comment = commentField.getText();
-
-			if (name.trim().length() == 0) {
-				name = "IGB BOOKMARK";
-			}
-
-			bookmark.setName(name);
-			bookmark.setComment(comment);
-			AddBookmarkAction.addBookmark(bookmark);
+		if (name.trim().length() == 0) {
+			name = "IGB BOOKMARK";
 		}
 
+		bookmark.setName(name);
+		bookmark.setComment(comment);
+		AddBookmarkAction.addBookmark(bookmark);
 	}
 }

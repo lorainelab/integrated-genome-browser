120600117ff08d0f93036402a388943447b252c5
hiralv <hiralv@uncc.edu>
2013-02-01 14:45:55 +0000

    Adding graph comparator and graph packers.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14152 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/igb/src/com/affymetrix/igb/shared/GraphGlyphPosComparator.java
@@ -0,0 +1,35 @@
+package com.affymetrix.igb.shared;
+
+import com.affymetrix.genometryImpl.style.GraphState;
+import com.affymetrix.genoviz.bioviews.GlyphI;
+import com.affymetrix.genoviz.comparator.GlyphMinXComparator;
+import java.util.Comparator;
+
+/**
+ *
+ * @author hiralv
+ */
+public class GraphGlyphPosComparator implements Comparator<GlyphI> {
+	private static final Comparator<GlyphI> child_sorter = new GlyphMinXComparator();
+	
+	@Override
+	public int compare(GlyphI g1, GlyphI g2) {
+		if (!(g1 instanceof GraphGlyph) || !(g2 instanceof GraphGlyph)) {
+			return Double.compare(g1.getCoordBox().x, g2.getCoordBox().x);
+		}
+
+		GraphGlyph gg1 = (GraphGlyph) g1;
+		GraphState gs1 = gg1.graf.getGraphState();
+
+		GraphGlyph gg2 = (GraphGlyph) g2;
+		GraphState gs2 = gg2.graf.getGraphState();
+
+		int ret = Double.compare(gs1.getPosition(), gs2.getPosition());
+		
+		if(ret == 0){
+			ret = child_sorter.compare(g1, g2);
+		}
+		
+		return ret;
+	}
+}

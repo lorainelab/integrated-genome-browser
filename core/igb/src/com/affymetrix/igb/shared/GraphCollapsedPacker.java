d9b519fe8481c9ce57d8fa43522dfb9802a91d60
hiralv <hiralv@uncc.edu>
2013-02-01 14:54:41 +0000

    Merging changes of r14152-14153. Sort joined graph based on selection.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_7_0@14154 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/shared/GraphCollapsedPacker.java
+++ core/igb/src/com/affymetrix/igb/shared/GraphCollapsedPacker.java
@@ -1,17 +1,17 @@
 package com.affymetrix.igb.shared;
 
-import java.awt.Rectangle;
-import java.util.Collections;
-
 import com.affymetrix.genoviz.bioviews.GlyphI;
 import com.affymetrix.genoviz.bioviews.ViewI;
+import com.affymetrix.genoviz.widget.tieredmap.CollapsedTierPacker;
+import java.awt.Rectangle;
+import java.util.Collections;
 
 /**
  *
  * @author hiralv
  */
-public class GraphFasterExpandPacker extends FasterExpandPacker {
-
+public class GraphCollapsedPacker extends CollapsedTierPacker {
+	
 	private static final GraphGlyphPosComparator comparator = new GraphGlyphPosComparator();
 
 	@Override
@@ -21,6 +21,8 @@ public class GraphFasterExpandPacker extends FasterExpandPacker {
 			return null;
 		}
 		Collections.sort(parent.getChildren(), comparator);
+		Collections.reverse(parent.getChildren());
 		return super.pack(parent, view);
 	}
+	
 }

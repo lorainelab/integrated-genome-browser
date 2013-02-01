1cb2587e52f2ac3f2cdef9ac6740d949f468a654
hiralv <hiralv@uncc.edu>
2013-02-01 19:33:55 +0000

    Refactoring survey.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14155 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/Survey/src/com/affymetrix/igb/survey/ShowSurvey.java
+++ plugins/Survey/src/com/affymetrix/igb/survey/ShowSurvey.java
@@ -3,8 +3,6 @@ package com.affymetrix.igb.survey;
 import com.affymetrix.common.CommonUtils;
 import com.affymetrix.genometryImpl.util.GeneralUtils;
 import com.affymetrix.genometryImpl.util.PreferenceUtils;
-import java.util.Timer;
-import java.util.TimerTask;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
@@ -16,34 +14,31 @@ import javax.swing.JOptionPane;
 public class ShowSurvey {
 	private final static String[] options = new String[]{"Yes", "No", "Later"};
 	
-	public static void show(final Survey survey) {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				JOptionPane op = new JOptionPane(survey.getDescription(),
-						JOptionPane.PLAIN_MESSAGE, JOptionPane.YES_NO_CANCEL_OPTION,
-						CommonUtils.getInstance().getApplicationSmallIcon(),
-						options);
+	public static boolean showSurvey(final Survey survey) {
+		JOptionPane op = new JOptionPane(survey.getDescription(),
+				JOptionPane.PLAIN_MESSAGE, JOptionPane.YES_NO_CANCEL_OPTION,
+				CommonUtils.getInstance().getApplicationSmallIcon(),
+				options);
 
-				JDialog dialog = op.createDialog(survey.getName());
-				dialog.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
-				dialog.setResizable(true);
-				dialog.setAlwaysOnTop(true);
-				dialog.pack();
-				dialog.setVisible(true);
+		JDialog dialog = op.createDialog(survey.getName());
+		dialog.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+		dialog.setResizable(true);
+		dialog.setAlwaysOnTop(true);
+		dialog.pack();
+		dialog.setVisible(true);
 
-				if (op.getValue() != null) {
-					//Mark survey as taken
-					if(op.getValue() != options[2]){
-						PreferenceUtils.getTopNode().putBoolean(survey.getId(), true);
-					}
-					
-					if (op.getValue() == options[0]) {
-						GeneralUtils.browse(survey.getLink());
-					}
-				}
+		boolean ret = false;
+		if (op.getValue() != null) {
+			//Mark survey as taken
+			if (op.getValue() != options[2]) {
+				PreferenceUtils.getTopNode().putBoolean(survey.getId(), true);
+				ret = true;
 			}
-		}, 10000);
+
+			if (op.getValue() == options[0]) {
+				GeneralUtils.browse(survey.getLink());
+			}
+		}
+		return ret;
 	}
 }

20d94351b04f8adc38500225483a63d41d4ffe33
hiralv <hiralv@uncc.edu>
2013-02-12 21:30:23 +0000

    Adding potential fix for dot graphs not visible at high zoom.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14226 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/graphTypes/DotGraphType.java
+++ core/igb/src/com/affymetrix/igb/graphTypes/DotGraphType.java
@@ -26,8 +26,11 @@ public class DotGraphType extends GraphGlyph.GraphStyle {
 			int draw_end_index, double offset, double yscale, ViewI view, int i) {
 		if (!graphSym.hasWidth()) {
 			g.drawLine(curr_point.x, curr_point.y, curr_point.x, curr_point.y); // point
+//			g.fillRect(curr_point.x, curr_point.y, curr_x_plus_width.x, 4); // point
 		} else {
 			g.drawLine(curr_point.x, curr_point.y, curr_x_plus_width.x, curr_point.y);
+//			final int width = Math.max(1, curr_x_plus_width.x - curr_point.x);
+//			g.fillRect(curr_point.x, curr_point.y, width, 4);
 		}
 	}
 

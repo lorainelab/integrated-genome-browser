5ea71e50196e47e51b5920f8b3cdfbe0227d63e6
hiralv <hiralv@uncc.edu>
2013-04-08 14:18:58 +0000

    Refactoring configure scroll to accept only values in range.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14290 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ConfigureScrollAction.java
+++ core/igb/src/com/affymetrix/igb/action/ConfigureScrollAction.java
@@ -1,21 +1,25 @@
 package com.affymetrix.igb.action;
 
-import com.affymetrix.genometryImpl.event.GenericActionHolder;
-import com.affymetrix.genoviz.swing.recordplayback.JRPTextField;
-import com.affymetrix.genoviz.widget.AutoScroll;
-import static com.affymetrix.igb.IGBConstants.BUNDLE;
-import com.affymetrix.igb.view.SeqMapView;
 import java.awt.Frame;
 import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.*;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AbstractDocument;
 
+import com.affymetrix.genometryImpl.event.GenericActionHolder;
+import com.affymetrix.genoviz.swing.NumericFilter;
+import com.affymetrix.genoviz.swing.recordplayback.JRPTextField;
+import com.affymetrix.genoviz.widget.AutoScroll;
+import com.affymetrix.igb.view.SeqMapView;
+import static com.affymetrix.igb.IGBConstants.BUNDLE;
 /**
  *
  * @author sgblanch
@@ -68,57 +72,52 @@ public class ConfigureScrollAction extends SeqMapViewActionA {
 		
 		// Show the config dialog
 		AutoScrollConfigDialog autoScrollConfigDialog = new AutoScrollConfigDialog(null, seqMapView);
-		autoScrollConfigDialog.showDialog();
-	}
-
-	// Normalize a text field so that it holds an integer, with a fallback value
-	// if there is a problem, and a minimum and maximum
-	private static int normalizeTF(JRPTextField tf, int fallback, int min, int max) {
-		assert min <= fallback && fallback <= max;
-		int result;
-		try {
-			result = Integer.parseInt(tf.getText());
-		} catch (NumberFormatException nfe) {
-			Toolkit.getDefaultToolkit().beep();
-			result = fallback;
-		}
-		if (result < min) {
-			result = min;
-		} else if (result > max) {
-			result = max;
-		}
-		tf.setText(Integer.toString(result));
-		return result;
+		autoScrollConfigDialog.setVisible(true);
 	}
 
 	class AutoScrollConfigDialog extends JDialog {
 
 		private JOptionPane optionPane;
-		private JPanel pan;
-		private final JRPTextField pix_to_scrollTF;
-		private final JRPTextField time_intervalTF;
-		private final Object[] startOptions = new Object[]{"Start", "Close"};
-		private final Object[] stopOptions = new Object[]{"Stop", "Close"};
+		private JRPTextField pix_to_scrollTF, time_intervalTF;
+		private JLabel bases_per_minuteL, minutes_per_seqL;
+		private JButton startOption, stopOption, closeOption;
+		private Object[] startOptions, stopOptions;
 
 		/**
 		 * Creates the reusable dialog.
 		 */
-		public AutoScrollConfigDialog (Frame aFrame, SeqMapView seqMapView) {
+		public AutoScrollConfigDialog (final Frame aFrame, final SeqMapView seqMapView) {
 			super(aFrame, true);
-			
-			setLocationRelativeTo(seqMapView);
-			
-			pan = new JPanel();
-			setLocationRelativeTo(seqMapView);
+			init(seqMapView);
+			addListeners();
+		}
+
+		private void init(final SeqMapView seqMapView) throws SecurityException {
+			JPanel pan = new JPanel();
 			pix_to_scrollTF = new JRPTextField("AutoScrollAction_pix_to_scroll", "" + as_pix_to_scroll);
 			time_intervalTF = new JRPTextField("AutoScrollAction_time_interval", "" + as_time_interval);
+			startOption = new JButton("Start");
+			stopOption = new JButton("Stop");
+			closeOption = new JButton("Close");
+			startOptions = new Object[]{startOption, closeOption};
+			stopOptions = new Object[]{stopOption, closeOption};
+			optionPane = new JOptionPane(pan, JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION, 
+											null, startOptions, startOptions[0]);
 
+			setTitle("AutoScroll Parameters");
+			setResizable(false);
+			setLocationRelativeTo(seqMapView);
+			setContentPane(optionPane);
+			//setModal(false);
+			setAlwaysOnTop(false);
+			setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			
 			float bases_per_minute = (float) // 1000 ==> ms/s , 60 ==> s/minute, as_time_interval ==> ms/scroll
 					(1.0 * autoScroll.get_bases_per_pix() * as_pix_to_scroll * 1000 * 60 / as_time_interval);
 			bases_per_minute = Math.abs(bases_per_minute);
 			float minutes_per_seq = seqMapView.getViewSeq().getLength() / bases_per_minute;
-			final JLabel bases_per_minuteL = new JLabel("" + (bases_per_minute / 1000000));
-			final JLabel minutes_per_seqL = new JLabel("" + (minutes_per_seq));
+			bases_per_minuteL = new JLabel("" + (bases_per_minute / 1000000));
+			minutes_per_seqL = new JLabel("" + (minutes_per_seq));
 
 			pan.setLayout(new GridLayout(4, 2));
 			pan.add(new JLabel("Scroll increment (pixels)"));
@@ -130,119 +129,94 @@ public class ConfigureScrollAction extends SeqMapViewActionA {
 			pan.add(new JLabel("Total minutes for seq:  "));
 			pan.add(minutes_per_seqL);
 
-			KeyAdapter kl = new KeyAdapter() {
+			pack();
+		}
 
-				@Override
-				public void keyReleased(KeyEvent e) {
-					as_pix_to_scroll = normalizeTF(pix_to_scrollTF, as_pix_to_scroll, -1000, 1000);
-					as_time_interval = normalizeTF(time_intervalTF, as_time_interval, 1, 1000);
-
-					float bases_per_minute = (float) // 1000 ==> ms/s , 60 ==> s/minute, as_time_interval ==> ms/scroll
-							(1.0 * autoScroll.get_bases_per_pix() * as_pix_to_scroll * 1000 * 60 / as_time_interval);
-					bases_per_minute = Math.abs(bases_per_minute);
-					float minutes_per_seq = (autoScroll.get_end_pos() - autoScroll.get_start_pos()) / bases_per_minute;
-					bases_per_minuteL.setText("" + (bases_per_minute / 1000000));
-					minutes_per_seqL.setText("" + (minutes_per_seq));
-				}
+		private void addListeners() {
+			DocumentListener dl = new DocumentListener(){
+				public void insertUpdate(DocumentEvent e)  { update(e); }
+				public void removeUpdate(DocumentEvent e)  { update(e); }
+				public void changedUpdate(DocumentEvent e) { update(e); }
 			};
 
-			pix_to_scrollTF.addKeyListener(kl);
-			time_intervalTF.addKeyListener(kl);
-			
-			// Click on pix to scroll and time interval text boxs will stop the auto scroll
-			MouseListener ml = new MouseListener(){
-
-				public void mouseClicked(MouseEvent me) {
-				}
-
-				public void mousePressed(MouseEvent me) {
-				}
-
-				public void mouseReleased(MouseEvent me) {
-					if(optionPane.getOptions()[0].equals("Stop")) {
-						StopAutoScrollAction.getAction().actionPerformed(null);
-						optionPane.setOptions(startOptions);
-						optionPane.setInitialValue(startOptions[0]);
+			ActionListener al = new ActionListener(){
+				public void actionPerformed(ActionEvent ae) {
+					if (ae.getSource() == startOption) {
+						start();
+					} else if (ae.getSource() == stopOption) {
+						stop();
+					} else if (ae.getSource() == closeOption) {
+						close();
 					}
-					((JRPTextField)me.getSource()).selectAll();
-					((JRPTextField)me.getSource()).requestFocusInWindow();
-				}
-
-				public void mouseEntered(MouseEvent me) {
-				}
-
-				public void mouseExited(MouseEvent me) {
 				}
 			};
 			
-			pix_to_scrollTF.addMouseListener(ml);
-			time_intervalTF.addMouseListener(ml);
+			((AbstractDocument)pix_to_scrollTF.getDocument()).setDocumentFilter(new NumericFilter.IntegerNumericFilter(-1000, 1000));
+			((AbstractDocument)time_intervalTF.getDocument()).setDocumentFilter(new NumericFilter.IntegerNumericFilter(1, 1000));
+			((AbstractDocument)pix_to_scrollTF.getDocument()).addDocumentListener(dl);
+			((AbstractDocument)time_intervalTF.getDocument()).addDocumentListener(dl);
+			
+			startOption.addActionListener(al);
+			stopOption.addActionListener(al);
+			closeOption.addActionListener(al);
 		}
 		
-		private void showDialog() {
+		private void start(){
+			StartAutoScrollAction.getAction().start();
+			optionPane.setOptions(stopOptions); // Change Start button to Stop
+			optionPane.setInitialValue(stopOptions[0]); // Press Enter will trigger the new Stop button
+		}
+		
+		private void stop(){
+			StopAutoScrollAction.getAction().actionPerformed(null);
+			optionPane.setOptions(startOptions);
+			optionPane.setInitialValue(startOptions[0]);
+		}
+		
+		private void close(){
+			StopAutoScrollAction.getAction().actionPerformed(null); // Stop auto scroll when closing config dialog
+			optionPane.setOptions(startOptions);
+			dispose();
+		}
+		
+		private void update(DocumentEvent e) {
+			// Stop before try/catch block
+			if (optionPane.getOptions()[0].equals(stopOption)) {
+				stop();
 
-			optionPane = new JOptionPane(pan,
-					JOptionPane.QUESTION_MESSAGE,
-					JOptionPane.YES_NO_OPTION,
-					null,
-					startOptions,
-					startOptions[0]);
+				// Regain the focus
+				// Todo: Need to get source from the event
+				if (e.getDocument() == pix_to_scrollTF.getDocument()) {
+					pix_to_scrollTF.requestFocusInWindow();
+				} else if (e.getDocument() == time_intervalTF.getDocument()) {
+					time_intervalTF.requestFocusInWindow();
+				}
+			}
 
-			setTitle("AutoScroll Parameters");
-			setResizable(false);
-			setContentPane(optionPane);
-			//setModal(false);
-			setAlwaysOnTop(false);
-			setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			
-			//Register an event handler that reacts to option pane state changes.
-			optionPane.addPropertyChangeListener(
-					new PropertyChangeListener() {
+			try {
+				as_pix_to_scroll = Integer.parseInt(pix_to_scrollTF.getText());
+				as_time_interval = Integer.parseInt(time_intervalTF.getText());
 
-						@Override
-						public void propertyChange(PropertyChangeEvent e) {
+				float bases_per_minute = (float) // 1000 ==> ms/s , 60 ==> s/minute, as_time_interval ==> ms/scroll
+						(1.0 * autoScroll.get_bases_per_pix() * as_pix_to_scroll * 1000 * 60 / as_time_interval);
+				bases_per_minute = Math.abs(bases_per_minute);
+				float minutes_per_seq = (autoScroll.get_end_pos() - autoScroll.get_start_pos()) / bases_per_minute;
+				bases_per_minuteL.setText("" + (bases_per_minute / 1000000));
+				minutes_per_seqL.setText("" + (minutes_per_seq));
 
-							String prop = e.getPropertyName();
+				startOption.setEnabled(true);
+				stopOption.setEnabled(true);
 
-							if (isVisible()
-									&& (e.getSource() == optionPane)
-									&& (JOptionPane.VALUE_PROPERTY.equals(prop)
-									|| JOptionPane.INPUT_VALUE_PROPERTY.equals(prop))) {
+				autoScroll.configure(as_pix_to_scroll, as_time_interval);
+				
+			} catch (NumberFormatException nfe) {
+				bases_per_minuteL.setText("N/A");
+				minutes_per_seqL.setText("N/A");
 
-								Object value = optionPane.getValue();
-
-								if (value == JOptionPane.UNINITIALIZED_VALUE) {
-									return;
-								}
-								
-								//Reset the JOptionPane's value.
-								//If you don't do this, then if the user presses the same button next time, no property change event will be fired.
-								optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
-								
-								if (value.equals(startOptions[0]) || value.equals(stopOptions[0])) {
-									as_pix_to_scroll = normalizeTF(pix_to_scrollTF, as_pix_to_scroll, -1000, 1000);
-									as_time_interval = normalizeTF(time_intervalTF, as_time_interval, 1, 1000);
-									autoScroll.configure(as_pix_to_scroll, as_time_interval);
-
-									if (value.equals(startOptions[0])) {
-										StartAutoScrollAction.getAction().start();
-										optionPane.setOptions(stopOptions); // Change Start button to Stop
-										optionPane.setInitialValue(stopOptions[0]); // Press Enter will trigger the new Stop button
-									} else if (value.equals(stopOptions[0])) {
-										StopAutoScrollAction.getAction().actionPerformed(null);
-										optionPane.setOptions(startOptions);
-										optionPane.setInitialValue(startOptions[0]);
-									} 
-								} else if (value.equals("Close")) {
-										StopAutoScrollAction.getAction().actionPerformed(null); // Stop auto scroll when closing config dialog
-										optionPane.setOptions(startOptions);
-										dispose();
-								}
-							}
-						}
-					});
-			pack();
-			setVisible(true);
+				startOption.setEnabled(false);
+				stopOption.setEnabled(false);
+			}
+	
 		}
 	}
 }

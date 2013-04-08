67d0100fb8489fd04ff6eecc5160fa823076b49b
hiralv <hiralv@uncc.edu>
2013-04-08 14:49:08 +0000

    Adding numeric filter with range.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14291 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genoviz_sdk/src/com/affymetrix/genoviz/swing/NumericFilter.java
+++ core/genoviz_sdk/src/com/affymetrix/genoviz/swing/NumericFilter.java
@@ -20,14 +20,57 @@ public abstract class NumericFilter extends DocumentFilter {
 	public void replace(DocumentFilter.FilterBypass fb, int offset, int length,
 			String text, AttributeSet attr) throws BadLocationException {
 		fb.replace(offset, length, text.replaceAll(getRegex(), ""), attr);
+		fb.replace(0, fb.getDocument().getLength(), checkRange(fb.getDocument().getText(0, fb.getDocument().getLength()), text.length()), attr);
 	}
 	
 	protected abstract String getRegex();
 	
+	protected String checkRange(String check, int insLen){
+		return check;
+	}
+	
 	public static class IntegerNumericFilter extends NumericFilter{
+		private final int min, max;
+		private final String regex;
+		
+		public IntegerNumericFilter(){
+			this(0, Integer.MAX_VALUE);
+		}
+		
+		public IntegerNumericFilter(int min, int max){
+			this.min = min;
+			this.max = max;
+			if(min < 0){
+				regex = "[^0-9\\-]";
+			} else {
+				regex = "\\D";
+			}
+		}
+	
+		@Override
+		protected String checkRange(String check, int insLen){
+			if(check.length() == 1 && insLen == 1 && check.equals("-")){
+				return check;
+			}
+					
+			boolean decrease = false;
+			try {
+				int result = Integer.parseInt(check);
+				if(result > max || result < min){
+					decrease = true;
+				}
+			} catch (NumberFormatException nfe) {
+				decrease = true;
+			}
+			if(decrease && check.length() >= insLen){
+				return check.substring(0, check.length() - insLen);
+			}
+			return check;
+		}
+		
 		@Override
 		protected String getRegex() {
-			return "\\D";
+			return regex;
 		}
 	}
 	

9ac95d064f6d9a5bca19432da7dd64c4c11eb176
hiralv <hiralv@uncc.edu>
2013-04-25 13:14:32 +0000

    Updating color by action to use get parameters.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14388 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ColorByAction.java
+++ core/igb/src/com/affymetrix/igb/action/ColorByAction.java
@@ -1,12 +1,19 @@
 package com.affymetrix.igb.action;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -15,14 +22,16 @@ import javax.swing.JTextField;
 import com.jidesoft.combobox.ColorComboBox;
 
 import com.affymetrix.genometryImpl.color.ColorProvider;
+import com.affymetrix.genometryImpl.color.RGB;
+import com.affymetrix.genometryImpl.color.Score;
+import com.affymetrix.genometryImpl.color.Strand;
 import com.affymetrix.genometryImpl.event.GenericActionHolder;
 import com.affymetrix.genometryImpl.style.ITrackStyleExtended;
 import com.affymetrix.genometryImpl.util.ThreadUtils;
 import com.affymetrix.igb.tiers.TierLabelGlyph;
 import com.affymetrix.igb.view.SeqMapView;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+
 
 /**
  *
@@ -64,67 +73,101 @@ public class ColorByAction extends SeqMapViewActionA {
 		private JOptionPane optionPane;
 		private JComboBox comboBox;
 		private JPanel paramsPanel;
+		private Map<String, Class<? extends ColorProvider>> options;
 		
 		/**
 		 * Creates the reusable dialog.
 		 */
 		public AutoScrollConfigDialog (final SeqMapView seqMapView) {
 			super((Frame)null, true);
+			
+			options = new HashMap<String, Class<? extends ColorProvider>>();
+			options.put("RGB", RGB.class);
+			options.put("Score", Score.class);
+			options.put("Strand", Strand.class);
+			
 			init(seqMapView);
 			addListeners();
 		}
 
 		private void init(final SeqMapView seqMapView) throws SecurityException {
 			JPanel pan = new JPanel();
-			pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
-			
+			pan.setLayout(new BorderLayout());
+	
 			optionPane = new JOptionPane(pan, JOptionPane.QUESTION_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null);
 			optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
 			
 			setTitle("Color By");
-			setResizable(false);
+			setResizable(true);
 			setLocationRelativeTo(seqMapView);
 			setContentPane(optionPane);
 			//setModal(false);
 			setAlwaysOnTop(false);
 			setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			
-			comboBox = new JComboBox(new String[]{"None", "RGB", "Score", "Strand"});
-			Box optionsBox = Box.createHorizontalBox();
+			comboBox = new JComboBox(options.keySet().toArray());
+			JPanel optionsBox = new JPanel();
+			optionsBox.setLayout(new BoxLayout(optionsBox, BoxLayout.X_AXIS));
 			optionsBox.add(new JLabel("Color By :  "));
 			optionsBox.add(comboBox);
 			
-			final JPanel emptyBox = new JPanel();
-			emptyBox.setLayout(new BoxLayout(emptyBox, BoxLayout.X_AXIS));
-			emptyBox.add(new JLabel("            "));
-			
 			paramsPanel = new JPanel();
-			paramsPanel.setLayout(new BoxLayout(paramsPanel, BoxLayout.X_AXIS));
-						
-			emptyBox.add(paramsPanel);
-			pan.add(optionsBox);
-			pan.add(emptyBox);
+			paramsPanel.setLayout(new BoxLayout(paramsPanel, BoxLayout.LINE_AXIS));
+			
+			pan.add(optionsBox, BorderLayout.CENTER);
+			pan.add(paramsPanel, BorderLayout.PAGE_END);
+			initParamPanel();
 			pack();
 		}
 
-		private void addOptions(String label, Class clazz, JPanel pan){
-			pan.add(new JLabel(label));
-			if(Number.class.isAssignableFrom(clazz)){
-				JTextField tf = new JTextField(6);
-				tf.setMaximumSize(new java.awt.Dimension(50, 20));
-				tf.setPreferredSize(new java.awt.Dimension(50, 20));
-				tf.setMaximumSize(new java.awt.Dimension(50, 20));
-				pan.add(tf);
-			} else if(Color.class.isAssignableFrom(clazz)) {
-				ColorComboBox colorComboBox = new ColorComboBox();
-				colorComboBox.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
-				colorComboBox.setButtonVisible(false);
-				colorComboBox.setColorValueVisible(false);
-				colorComboBox.setMaximumSize(new java.awt.Dimension(20, 20));
-				colorComboBox.setPreferredSize(new java.awt.Dimension(20, 20));
-				colorComboBox.setMaximumSize(new java.awt.Dimension(20, 20));
-				//colorComboBox.setStretchToFit(true);
-				pan.add(colorComboBox);
+		private void addOptions(ColorProvider cp, JPanel pan) {
+			pan.removeAll();
+			pan.add(new JLabel("                "));
+			if (cp.getParameters() != null) {
+				for (Entry<String, Class<?>> entry : cp.getParameters().entrySet()) {
+					String label = entry.getKey();
+					Class<?> clazz = entry.getValue();
+					JComponent component = null;
+
+					if (Number.class.isAssignableFrom(clazz)) {
+						JTextField tf = new JTextField(6);
+						tf.setMaximumSize(new java.awt.Dimension(60, 20));
+						tf.setPreferredSize(new java.awt.Dimension(60, 20));
+						tf.setMaximumSize(new java.awt.Dimension(60, 20));
+						component = tf;
+
+					} else if (Color.class.isAssignableFrom(clazz)) {
+						ColorComboBox colorComboBox = new ColorComboBox();
+						colorComboBox.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
+						colorComboBox.setButtonVisible(false);
+						colorComboBox.setColorValueVisible(false);
+						colorComboBox.setMaximumSize(new java.awt.Dimension(20, 20));
+						colorComboBox.setPreferredSize(new java.awt.Dimension(20, 20));
+						colorComboBox.setMaximumSize(new java.awt.Dimension(20, 20));
+						//colorComboBox.setStretchToFit(true);
+						component = colorComboBox;
+					}
+					
+					if (component != null) {
+						pan.add(new JLabel(label));
+						pan.add(component);
+						pan.add(Box.createHorizontalStrut(30));
+					}
+				}
+			}
+		}
+		
+		private void initParamPanel() {
+			try {
+				ColorProvider cp = options.get(comboBox.getSelectedItem().toString()).getConstructor().newInstance();
+				addOptions(cp, paramsPanel);
+
+				ThreadUtils.runOnEventQueue(new Runnable() {
+					public void run() {
+						pack();
+					}
+				});
+			} catch (Exception ex) {
 			}
 		}
 		
@@ -132,24 +175,7 @@ public class ColorByAction extends SeqMapViewActionA {
 			comboBox.addItemListener(new ItemListener() {
 				
 				public void itemStateChanged(ItemEvent e) {
-					paramsPanel.removeAll();
-					if(e.getItem() == "RGB"){
-						// Add Nothing
-					} else if (e.getItem() == "Score") {
-						addOptions("min", Float.class, paramsPanel);
-						paramsPanel.add(Box.createHorizontalStrut(30));
-						addOptions("min", Float.class, paramsPanel);
-					} else if (e.getItem() == "Strand") {
-						addOptions("+", Color.class, paramsPanel);
-						paramsPanel.add(Box.createHorizontalStrut(30));
-						addOptions("-", Color.class, paramsPanel);
-					}
-					
-					ThreadUtils.runOnEventQueue(new Runnable() {
-						public void run() {
-							pack();
-						}
-					});
+					initParamPanel();
 				}
 			});
 

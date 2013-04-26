050ba7faa02b28396db746b96edb2b2e1f8e547c
hiralv <hiralv@uncc.edu>
2013-04-26 19:15:15 +0000

    Adding color by length.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14405 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/igb/src/com/affymetrix/igb/colorproviders/Length.java
@@ -0,0 +1,22 @@
+package com.affymetrix.igb.colorproviders;
+
+import com.affymetrix.genometryImpl.GenometryModel;
+import com.affymetrix.genometryImpl.color.ColorProvider;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genoviz.color.ColorPalette;
+import com.affymetrix.genoviz.color.ColorScheme;
+import java.awt.Color;
+
+/**
+ *
+ * @author hiralv
+ */
+public class Length extends ColorProvider {
+	private static GenometryModel model = GenometryModel.getGenometryModel();
+	ColorPalette cp = new ColorPalette(ColorScheme.ACCENT8);
+	
+	@Override
+	public Color getColor(SeqSymmetry sym){
+		return cp.getColor(String.valueOf(sym.getSpan(model.getSelectedSeq()).getLength()));
+	}
+}

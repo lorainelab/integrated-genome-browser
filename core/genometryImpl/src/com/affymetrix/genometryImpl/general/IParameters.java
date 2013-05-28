e180ec03386816400255829a61258f7f442e220b
hiralv <hiralv@uncc.edu>
2013-05-28 18:12:23 +0000

    Adding comments to interface.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14579 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/general/IParameters.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/general/IParameters.java
@@ -8,12 +8,28 @@ import java.util.Map;
  */
 public interface IParameters {
 
+	/**
+	 * @param key
+	 * @return Returns values for given key
+	 */
 	public Object getParameterValue(String key);
 
+	/**
+	 * @return parameters for this instance
+	 */
 	public Map<String, Class<?>> getParametersType();
 
+	/**
+	 * Sets value for given key
+	 * @param key
+	 * @param value
+	 * @return 
+	 */
 	public boolean setParameterValue(String key, Object value);
 
+	/**
+	 * Used for properties specific to a type of instance.
+	 */
 	public boolean setParametersValue(Map<String, Object> params);
     
 }

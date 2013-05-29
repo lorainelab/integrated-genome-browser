a9e0af0986b46d29d5ed9f11420c78f34474e97e
hiralv <hiralv@uncc.edu>
2013-05-29 20:28:12 +0000

    Creating track operation with parameter action.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14591 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/igb/src/com/affymetrix/igb/shared/TrackOperationWithParametersAction.java
@@ -0,0 +1,32 @@
+package com.affymetrix.igb.shared;
+
+import com.affymetrix.genometryImpl.operator.Operator;
+import com.affymetrix.igb.util.ConfigureOptionsDialog;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hiralv
+ */
+public class TrackOperationWithParametersAction extends TrackOperationAction {
+	private static final long serialVersionUID = 1L;
+
+	public TrackOperationWithParametersAction(Operator operator) {
+		super(operator);
+	}
+
+	@Override
+	public void actionPerformed(java.awt.event.ActionEvent e) {
+		ConfigureOptionsDialog<Operator> optionDialog = new ConfigureOptionsDialog<Operator>(Operator.class, "Track Operation");
+		optionDialog.setTitle("Track Operation");
+		optionDialog.setLocationRelativeTo(getSeqMapView());
+		optionDialog.setInitialValue(getOperator());
+		optionDialog.setEnabled(false);
+		optionDialog.setVisible(true);
+		Object value = optionDialog.getValue();
+		
+		if(value != null && (Integer)value == JOptionPane.OK_OPTION){
+			super.actionPerformed(e);
+		}	
+	}
+}

cab8637e2966076a385eba6a55a4b39fc4aecf16
hiralv <hiralv@uncc.edu>
2013-06-05 19:27:34 +0000

    Adding boolean variable to suggest loading sequence file as track.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_7_0@14644 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
+++ core/igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
@@ -1241,7 +1241,6 @@ public final class GeneralLoadUtils {
 				return null;
 			SymLoader symL = ServerUtils.determineLoader(SymLoader.getExtension(uri), uri, QuickLoadSymLoader.detemineFriendlyName(uri), version.group);
 			if (symL != null && symL.isResidueLoader() && loadAsTrack) {
-				symL = new ResidueTrackSymLoader(symL);
 				featureProps = new HashMap<String, String>();
 				featureProps.put("collapsed", "true");
 				featureProps.put("show2tracks", "false");
@@ -1249,7 +1248,7 @@ public final class GeneralLoadUtils {
 			String friendlyName = QuickLoadSymLoader.detemineFriendlyName(uri);
 			QuickLoadSymLoader quickLoad = SymLoader.getExtension(uri).endsWith("chp")
 					? new QuickLoadSymLoaderChp(uri, friendlyName, version.group)
-					: new QuickLoadSymLoader(uri, friendlyName, version.group);
+					: new QuickLoadSymLoader(uri, friendlyName, version.group, loadAsTrack);
 			gFeature = new GenericFeature(fileName, featureProps, version, quickLoad, File.class, autoload);
 
 			version.addFeature(gFeature);

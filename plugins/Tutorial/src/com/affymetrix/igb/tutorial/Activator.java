5d79269b359f90a69706db66fb3beb532124e3e8
hiralv <hiralv@uncc.edu>
2013-06-05 14:54:04 +0000

    Using nested XServiceRegistrar in tutorial activator.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14639 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- plugins/Tutorial/src/com/affymetrix/igb/tutorial/Activator.java
+++ plugins/Tutorial/src/com/affymetrix/igb/tutorial/Activator.java
@@ -14,82 +14,22 @@ import javax.swing.JMenuItem;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.util.tracker.ServiceTracker;
 
-import com.affymetrix.common.CommonUtils;
 import com.affymetrix.genometryImpl.event.GenericActionHolder;
 import com.affymetrix.genometryImpl.util.GeneralUtils;
 import com.affymetrix.genoviz.swing.AMenuItem;
 import com.affymetrix.genoviz.swing.recordplayback.JRPMenu;
 import com.affymetrix.igb.osgi.service.IGBService;
-import com.affymetrix.igb.osgi.service.ServiceRegistrar;
+import com.affymetrix.igb.osgi.service.SimpleServiceRegistrar;
+import com.affymetrix.igb.osgi.service.XServiceRegistrar;
 import com.affymetrix.igb.window.service.IWindowService;
 
-public class Activator extends ServiceRegistrar implements BundleActivator {
+public class Activator extends SimpleServiceRegistrar implements BundleActivator {
 	
 	private static final String DEFAULT_PREFS_TUTORIAL_RESOURCE = "/tutorial_default_prefs.xml";
-	private static final Logger ourLogger =
-			Logger.getLogger(Activator.class.getPackage().getName());
+	private static final Logger ourLogger = Logger.getLogger(Activator.class.getPackage().getName());
 
-	private ServiceRegistration<AMenuItem> menuRegistration;
-	
-	private void handleWindowService(final ServiceReference<IWindowService> windowServiceReference, final BundleContext bundleContext) {
-		loadDefaultTutorialPrefs();
-		try {
-			IWindowService windowService = bundleContext.getService(windowServiceReference);
-			ServiceReference<IGBService> igbServiceReference = bundleContext.getServiceReference(IGBService.class);
-			IGBService igbService = bundleContext.getService(igbServiceReference);
-			final TutorialManager tutorialManager = new TutorialManager(igbService, windowService);
-			GenericActionHolder.getInstance().addGenericActionListener(tutorialManager);
-			JRPMenu tutorialMenu = new JRPMenu("Tutorial_tutorialMenu", "Tutorials");
-			Properties tutorials = new Properties();
-			Preferences tutorialsNode = getTutorialsNode();
-			try {
-				for (String key : tutorialsNode.keys()) {
-					String tutorialUri = tutorialsNode.get(key, null);
-					tutorials.clear();
-					tutorials.load(new URL(tutorialUri + "/tutorials.properties").openStream());
-					Enumeration<?> tutorialNames = tutorials.propertyNames();
-					while (tutorialNames.hasMoreElements()) {
-						String name = (String) tutorialNames.nextElement();
-						String description = (String) tutorials.get(name);
-						RunTutorialAction rta = new RunTutorialAction(tutorialManager, description, tutorialUri + "/" + name);
-						JMenuItem item = new JMenuItem(rta);
-						tutorialMenu.add(item);
-					}
-				}
-			menuRegistration = bundleContext.registerService(AMenuItem.class, new AMenuItem(tutorialMenu, "help"), null);
-			} catch (FileNotFoundException fnfe) {
-				ourLogger.log(Level.WARNING,
-						"Could not find file {0}.\n          coninuing...",
-						fnfe.getMessage());
-			} catch (java.net.ConnectException ce) {
-				ourLogger.log(Level.WARNING,
-						"Could not connect: {0}.\n          coninuing...",
-						ce.getMessage());
-			}
-		} catch (Exception ex) {
-			ourLogger.logp(Level.SEVERE, this.getClass().getName(),
-					"handleWindowService", "?", ex);
-			ourLogger.severe("          continuing...");
-		}
-	}
-
-
-	@Override
-	public void stop(BundleContext bundleContext) throws Exception {
-		if(menuRegistration != null){
-			menuRegistration.unregister();
-		}
-	}
-
-	private void initActions(){
-		TweeningZoomAction.getAction();
-		VerticalStretchZoomAction.getAction();
-	}
-	
 	/**
 	 * Load default prefs from jar (with Preferences API).
 	 * This will be the standard method soon.
@@ -120,29 +60,68 @@ public class Activator extends ServiceRegistrar implements BundleActivator {
 	private Preferences getTutorialsNode() {
 		return getTopNode().node("tutorials");
 	}
-
-	@Override
-	protected ServiceRegistration<?>[] registerService(final BundleContext bundleContext, final IGBService igbService) throws Exception {
+	
+	private void initActions(){
+		TweeningZoomAction.getAction();
+		VerticalStretchZoomAction.getAction();
+	}
+	
+	private ServiceRegistration<?>[] registerService(final BundleContext bundleContext,
+			final IGBService igbService, final IWindowService windowService) throws Exception {
+		final TutorialManager tutorialManager = new TutorialManager(igbService, windowService);
+		GenericActionHolder.getInstance().addGenericActionListener(tutorialManager);
+		JRPMenu tutorialMenu = new JRPMenu("Tutorial_tutorialMenu", "Tutorials");
+		Properties tutorials = new Properties();
+		Preferences tutorialsNode = getTutorialsNode();
+		
 		try {
-			ServiceReference<IWindowService> windowServiceReference = bundleContext.getServiceReference(IWindowService.class);
-			if (windowServiceReference != null) {
-				handleWindowService(windowServiceReference, bundleContext);
-			} else {
-				ServiceTracker<IWindowService, Object> serviceTracker = new ServiceTracker<IWindowService, Object>(bundleContext, IWindowService.class.getName(), null) {
+			for (String key : tutorialsNode.keys()) {
+				String tutorialUri = tutorialsNode.get(key, null);
+				tutorials.clear();
+				tutorials.load(new URL(tutorialUri + "/tutorials.properties").openStream());
+				Enumeration<?> tutorialNames = tutorials.propertyNames();
+				while (tutorialNames.hasMoreElements()) {
+					String name = (String) tutorialNames.nextElement();
+					String description = (String) tutorials.get(name);
+					RunTutorialAction rta = new RunTutorialAction(tutorialManager, description, tutorialUri + "/" + name);
+					JMenuItem item = new JMenuItem(rta);
+					tutorialMenu.add(item);
+				}
+			}
+
+			return new ServiceRegistration[]{bundleContext.registerService(AMenuItem.class, new AMenuItem(tutorialMenu, "help"), null)};
+
+		} catch (FileNotFoundException fnfe) {
+			ourLogger.log(Level.WARNING, "Could not find file {0}.\n          coninuing...", fnfe.getMessage());
+		} catch (java.net.ConnectException ce) {
+			ourLogger.log(Level.WARNING, "Could not connect: {0}.\n          coninuing...", ce.getMessage());
+		}
+
+		return null;
+	}
+	
+	@Override
+	protected ServiceRegistration<?>[] getServices(BundleContext bundleContext) throws Exception {
+		initActions();
+		
+		XServiceRegistrar<IGBService> igbServiceRegistrar = new XServiceRegistrar<IGBService>(IGBService.class) {
+			
+			@Override
+			protected ServiceRegistration<?>[] registerService(final BundleContext bundleContext, final IGBService igbService) throws Exception {
+
+				XServiceRegistrar<IWindowService> windowServiceRegistrar = new XServiceRegistrar<IWindowService>(IWindowService.class) {
+					
 					@Override
-					public Object addingService(ServiceReference<IWindowService> windowServiceReference) {
-						handleWindowService(windowServiceReference, bundleContext);
-						return super.addingService(windowServiceReference);
+					protected ServiceRegistration<?>[] registerService(final BundleContext bundleContext, final IWindowService windowService) throws Exception {
+						return registerService(bundleContext, igbService, windowService);
 					}
 				};
-				serviceTracker.open();
+				windowServiceRegistrar.start(bundleContext);
+				return new ServiceRegistration[] { bundleContext.registerService(BundleActivator.class, windowServiceRegistrar, null) };
 			}
-		} catch (Exception ex) {
-			ourLogger.logp(Level.SEVERE, this.getClass().getName(),
-					"handleIGBService", "?", ex);
-			ourLogger.severe("          continuing...");
-		}
-		initActions();
-		return null;
+		};
+
+		igbServiceRegistrar.start(bundleContext);
+		return new ServiceRegistration[] { bundleContext.registerService(BundleActivator.class, igbServiceRegistrar, null) };
 	}
 }

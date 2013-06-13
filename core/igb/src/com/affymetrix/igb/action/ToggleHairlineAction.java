6f5bf3a5eb73d8c8b07cf54b720e7a07f657abe4
hiralv <hiralv@uncc.edu>
2013-06-13 16:11:16 +0000

    Using new method from preference utils to save action state.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14695 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/action/ToggleHairlineAction.java
+++ core/igb/src/com/affymetrix/igb/action/ToggleHairlineAction.java
@@ -3,14 +3,14 @@ package com.affymetrix.igb.action;
 import com.affymetrix.genometryImpl.event.GenericAction;
 import com.affymetrix.genometryImpl.event.GenericActionHolder;
 import com.affymetrix.genometryImpl.util.PreferenceUtils;
-import com.affymetrix.igb.view.UnibrowHairline;
 
+import com.affymetrix.igb.view.UnibrowHairline;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
 /**
  *
  * @author hiralv
  */
-public class ToggleHairlineAction extends GenericAction implements java.util.prefs.PreferenceChangeListener {
+public class ToggleHairlineAction extends GenericAction {
 	private static final long serialVersionUID = 1;
 	private static final ToggleHairlineAction ACTION = new ToggleHairlineAction();
 	
@@ -21,37 +21,16 @@ public class ToggleHairlineAction extends GenericAction implements java.util.pre
 		//this.putValue(MNEMONIC_KEY, java.awt.event.KeyEvent.VK_H);
 		/* TODO: This is only correct for English Locale" */
 		//this.putValue(DISPLAYED_MNEMONIC_INDEX_KEY, 5);
-
-		this.putValue(SELECTED_KEY, PreferenceUtils.getBooleanParam(
-				UnibrowHairline.PREF_KEEP_HAIRLINE_IN_VIEW, UnibrowHairline.default_keep_hairline_in_view));
 		this.ordinal = -4003000;
-		PreferenceUtils.getTopNode().addPreferenceChangeListener(this);
 	}
 	
 	static{
 		GenericActionHolder.getInstance().addGenericAction(ACTION);
+		PreferenceUtils.saveToPreferences(UnibrowHairline.PREF_KEEP_HAIRLINE_IN_VIEW, UnibrowHairline.default_keep_hairline_in_view, ACTION);
 	}
 	
 	public static ToggleHairlineAction getAction() {
 		return ACTION;
-	}
-
-	@Override
-	public void actionPerformed(java.awt.event.ActionEvent e) {
-		super.actionPerformed(e);
-		PreferenceUtils.getTopNode().putBoolean(
-				UnibrowHairline.PREF_KEEP_HAIRLINE_IN_VIEW, (Boolean)getValue(SELECTED_KEY));
-		
-	}
-
-	public void preferenceChange(java.util.prefs.PreferenceChangeEvent pce) {
-		if (! pce.getNode().equals(PreferenceUtils.getTopNode())) {
-          return;
-        }
-		if (pce.getKey().equals(UnibrowHairline.PREF_KEEP_HAIRLINE_IN_VIEW)) {
-			this.putValue(SELECTED_KEY, PreferenceUtils.getBooleanParam(
-				UnibrowHairline.PREF_KEEP_HAIRLINE_IN_VIEW, UnibrowHairline.default_keep_hairline_in_view));
-        }
 	}
 
 	@Override
--- core/igb/src/com/affymetrix/igb/action/ToggleHairlineLabelAction.java
+++ core/igb/src/com/affymetrix/igb/action/ToggleHairlineLabelAction.java
@@ -1,13 +1,12 @@
 package com.affymetrix.igb.action;
 
+import java.awt.event.KeyEvent;
+
 import com.affymetrix.genometryImpl.event.GenericAction;
 import com.affymetrix.genometryImpl.event.GenericActionHolder;
 import com.affymetrix.genometryImpl.util.PreferenceUtils;
+
 import com.affymetrix.igb.view.UnibrowHairline;
-
-import java.awt.event.KeyEvent;
-import java.util.prefs.PreferenceChangeListener;
-
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
 
 /**
@@ -15,7 +14,7 @@ import static com.affymetrix.igb.IGBConstants.BUNDLE;
  * @author sgblanch
  * @version $Id: ToggleHairlineLabelAction.java 11366 2012-05-02 14:57:55Z anuj4159 $
  */
-public class ToggleHairlineLabelAction extends GenericAction implements PreferenceChangeListener {
+public class ToggleHairlineLabelAction extends GenericAction {
 	private static final long serialVersionUID = 1;
 	private static final ToggleHairlineLabelAction ACTION = new ToggleHairlineLabelAction();
 
@@ -23,35 +22,16 @@ public class ToggleHairlineLabelAction extends GenericAction implements Preferen
 		super(BUNDLE.getString("toggleHairlineLabel"), null, "16x16/apps/office-calendar.png", "22x22/apps/office-calendar.png", KeyEvent.VK_Z);
 		/* TODO: This is only correct for English Locale" */
 		this.putValue(DISPLAYED_MNEMONIC_INDEX_KEY, 5);
-		this.putValue(SELECTED_KEY, PreferenceUtils.getBooleanParam(
-				UnibrowHairline.PREF_HAIRLINE_LABELED, UnibrowHairline.default_show_hairline_label));
 		this.ordinal = -4003100;
-		PreferenceUtils.getTopNode().addPreferenceChangeListener(this);
 	}
 	
 	static{
 		GenericActionHolder.getInstance().addGenericAction(ACTION);
+		PreferenceUtils.saveToPreferences(UnibrowHairline.PREF_HAIRLINE_LABELED, UnibrowHairline.default_show_hairline_label, ACTION);
 	}
 	
 	public static ToggleHairlineLabelAction getAction() {
 		return ACTION;
-	}
-
-	@Override
-	public void actionPerformed(java.awt.event.ActionEvent e) {
-		super.actionPerformed(e);
-		PreferenceUtils.getTopNode().putBoolean(
-				UnibrowHairline.PREF_HAIRLINE_LABELED, (Boolean)getValue(SELECTED_KEY));
-	}
-
-	public void preferenceChange(java.util.prefs.PreferenceChangeEvent pce) {
-		if (! pce.getNode().equals(PreferenceUtils.getTopNode())) {
-          return;
-        }
-		if (pce.getKey().equals(UnibrowHairline.PREF_HAIRLINE_LABELED)) {
-			this.putValue(SELECTED_KEY, PreferenceUtils.getBooleanParam(
-				UnibrowHairline.PREF_HAIRLINE_LABELED, UnibrowHairline.default_show_hairline_label));
-        }
 	}
 
 	@Override

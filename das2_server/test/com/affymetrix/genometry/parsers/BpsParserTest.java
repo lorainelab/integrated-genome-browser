331ffc0c8fd08f7a559a955e62650d7344948760
hiralv <hiralv@uncc.edu>
2013-07-01 17:29:54 +0000

    Moving bps parser indexing test to das2.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@14799 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- das2_server/test/com/affymetrix/genometry/parsers/BpsParserTest.java
+++ das2_server/test/com/affymetrix/genometry/parsers/BpsParserTest.java
@@ -6,6 +6,7 @@ import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.SeqSpan;
 import com.affymetrix.genometryImpl.parsers.BpsParser;
 import com.affymetrix.genometryImpl.parsers.IndexWriter;
+import com.affymetrix.genometryImpl.parsers.PSLParser;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.UcscPslSym;
 import com.affymetrix.genometryImpl.util.DasServerUtils;
@@ -18,11 +19,13 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class BpsParserTest {
 	
@@ -136,6 +139,103 @@ public class BpsParserTest {
 		assertEquals(138, result.size());
 	}
 
+	/**
+	 * Test indexing code.
+	 */
+	@Test
+	public void testIndexingNew() {
+		String string =
+		"70	1	0	0	0	0	2	165	+	EL049618	71	0	71	chr1	30432563	455031	455267	3	9,36,26,	0,9,45,	455031,455111,455241,\n" +
+		"71	0	0	0	0	0	2	176	+	EL049618	71	0	71	chr1	30432563	457618	457865	3	9,36,26,	0,9,45,	457618,457715,457839,\n" +
+		"70	1	0	0	0	0	2	165	+	EL049618	71	0	71	chr2	30432563	455031	455267	3	9,36,26,	0,9,45,	455031,455111,455241,\n" +
+		"71	0	0	0	0	0	2	176	+	EL049500	71	0	71	chr1	30432563	457617	457864	3	9,36,26,	0,9,45,	457617,457714,457838,\n" ;
+
+		InputStream istr = new ByteArrayInputStream(string.getBytes());
+		AnnotatedSeqGroup group = new AnnotatedSeqGroup("Test Group");
+		boolean annot_seq = true;
+
+		List<UcscPslSym> syms = null;
+		try {
+			PSLParser parser = new PSLParser();
+			syms = parser.parse(istr, "stream_test", group, group, annot_seq, true);
+		} catch (IOException ex) {
+			Logger.getLogger(com.affymetrix.genometryImpl.parsers.BpsParserTest.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		assertEquals(4, syms.size());	// precisely 4 symmetries.
+
+		BioSeq seq = group.getSeq("chr1");
+
+		BpsParser bps = new BpsParser();
+			Comparator<UcscPslSym> USCCCompare = bps.getComparator(seq);
+		List<SeqSymmetry> sortedSyms = IndexingUtils.getSortedAnnotationsForChrom(
+				syms, seq, USCCCompare);
+
+		assertEquals(3,sortedSyms.size());	// precisely 3 symmetries on chr1.
+
+		assertEquals(457617, ((UcscPslSym)sortedSyms.get(1)).getTargetMin());	// the middle symmetry (after sorting) should have a start coord of 457617.
+		
+	}
+
+
+	/**
+	 * Test indexing code.
+	 */
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testIndexing2() {
+
+		try {
+			String filename = "test/data/bps/test1.bps";
+			String testFileName = "test/data/bps/testNEW.bps";
+			assertTrue(new File(filename).exists());
+			
+			AnnotatedSeqGroup group = new AnnotatedSeqGroup("Test Group");
+			
+			List<UcscPslSym> syms = null;
+			syms = bpsParse(filename, "stream_test", group);
+
+			BioSeq seq = group.getSeq("chr1");
+		
+			IndexWriter iWriter = new BpsParser();
+			List<SeqSymmetry> sortedSyms = IndexingUtils.getSortedAnnotationsForChrom(
+					syms, seq, iWriter.getComparator(seq));
+
+			assertEquals(15,sortedSyms.size());
+
+			File testFile = new File(testFileName);
+			IndexedSyms iSyms = new IndexedSyms(sortedSyms.size(), testFile, "test", "bps", iWriter);
+			IndexingUtils.writeIndexedAnnotations(sortedSyms, seq, group, iSyms);
+
+			assertEquals(iSyms.min.length, iSyms.max.length);
+			assertEquals(iSyms.min.length + 1, iSyms.filePos.length);
+			assertEquals(sortedSyms.size(), iSyms.min.length);
+
+			testOutputIndexedSymmetries(iSyms.min,iSyms.max,iSyms.filePos);
+
+			if (testFile.exists()) {
+				testFile.delete();
+			}
+		} catch (Exception ex) {
+			Logger.getLogger(BpsParserTest.class.getName()).log(Level.SEVERE, null, ex);
+			fail();
+		}
+	}
+
+	private void testOutputIndexedSymmetries(int [] min, int [] max, long [] pos){
+		int [] overlapRange = new int[2];
+		int [] outputRange = new int[2];
+		overlapRange[0] = 2455539;
+		overlapRange[1] = 2455600;
+		IndexingUtils.findMaxOverlap(overlapRange, outputRange, min, max);
+		
+		int minPos = outputRange[0];
+		//System.out.println("val: " + min[minPos] + " " + max[minPos] + " " + pos[minPos]);
+
+		minPos = outputRange[1];
+		//System.out.println("position: " + minPos);
+	}
+	
 	public static List<UcscPslSym> bpsParse(String file_name, String annot_type, AnnotatedSeqGroup seq_group)
 			throws IOException {
 		Logger.getLogger(BpsParser.class.getName()).log(

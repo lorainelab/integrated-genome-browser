9a04af6187839506665e04624fcf132d12bec830
hiralv <hiralv@uncc.edu>
2013-08-06 17:24:48 +0000

    Using basic math transform.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15058 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/Activator.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/Activator.java
@@ -131,5 +131,9 @@ public class Activator implements BundleActivator {
 		bundleContext.registerService(Operator.class, new com.affymetrix.genometryImpl.operator.GraphMultiplexer(), null);
 		bundleContext.registerService(Operator.class, new com.affymetrix.genometryImpl.operator.FindJunctionOperator(false), null);
 		bundleContext.registerService(Operator.class, new com.affymetrix.genometryImpl.operator.FindJunctionOperator(true), null);
+		bundleContext.registerService(Operator.class, new com.affymetrix.genometryImpl.operator.AddMathTransform(), null);
+		bundleContext.registerService(Operator.class, new com.affymetrix.genometryImpl.operator.DivideMathTransform(), null);
+		bundleContext.registerService(Operator.class, new com.affymetrix.genometryImpl.operator.MultiplyMathTransform(), null);
+		bundleContext.registerService(Operator.class, new com.affymetrix.genometryImpl.operator.SubtractMathTransform(), null);
 	}
 }

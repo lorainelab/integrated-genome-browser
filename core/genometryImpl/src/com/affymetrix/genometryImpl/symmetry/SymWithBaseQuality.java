708c290913c526fe81e1b2903edbfaaee45cc713
hiralv <hiralv@uncc.edu>
2013-08-14 17:26:34 +0000

    Adding interface SymWithBaseQuality.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15091 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/symmetry/SymWithBaseQuality.java
@@ -0,0 +1,12 @@
+package com.affymetrix.genometryImpl.symmetry;
+
+/**
+ *
+ * @author hiralv
+ */
+public interface SymWithBaseQuality extends SymWithResidues {
+	
+	public String getBaseQuality();
+	
+	public String getBaseQuality(int start, int end);
+}

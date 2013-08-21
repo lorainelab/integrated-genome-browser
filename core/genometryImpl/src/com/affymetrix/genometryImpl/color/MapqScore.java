83ba0049955fbe2c2b9de564fdf8c45b12589bec
hiralv <hiralv@uncc.edu>
2013-08-21 18:58:24 +0000

    Set default values for mapqscore color provider.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15126 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/color/MapqScore.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/color/MapqScore.java
@@ -1,5 +1,8 @@
 package com.affymetrix.genometryImpl.color;
 
+import com.affymetrix.genometryImpl.general.Parameter;
+import com.affymetrix.genometryImpl.style.HeatMap;
+import com.affymetrix.genometryImpl.style.HeatMapExtended;
 import com.affymetrix.genometryImpl.symmetry.BAMSym;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import java.awt.Color;
@@ -8,7 +11,34 @@ import java.awt.Color;
  *
  * @author hiralv
  */
-public class MapqScore extends Score {
+public class MapqScore extends ColorProvider {
+	
+	private final static String HEATMAP = "heatmap";
+	public final static float DEFAULT_MIN_SCORE = 0.0f;
+	public final static float DEFAULT_MAX_SCORE = 255.0f;
+	public final static HeatMap DEFAULT_HEATMAP = new HeatMapExtended("DEFAULT_MAPQSCORE_HEATMAP", 
+			null, new float[]{0,50,200,255}, new Color[]{Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE});
+		
+	private float min_score_color  = DEFAULT_MIN_SCORE;
+	private float max_score_color  = DEFAULT_MAX_SCORE;
+	private float range			   = max_score_color - min_score_color;
+	
+	private Parameter<HeatMap> custom_heatmap = new Parameter<HeatMap>(DEFAULT_HEATMAP){
+		@Override
+		public boolean set(Object e){
+			super.set(e);
+			HeatMapExtended heatmap = (HeatMapExtended)e;
+			min_score_color = heatmap.getValues()[0];
+			max_score_color = heatmap.getValues()[heatmap.getValues().length - 1];
+			range = max_score_color - min_score_color;
+			return true;
+		}
+	};
+	
+	public MapqScore(){
+		super();
+		parameters.addParameter(HEATMAP, HeatMapExtended.class, custom_heatmap);
+	}
 	
 	@Override
 	public Color getColor(SeqSymmetry sym){
@@ -19,5 +49,17 @@ public class MapqScore extends Score {
 			}
 		}
 		return null;
+	}
+
+	protected Color getScoreColor(float score) {
+		if (score < min_score_color) {
+			score = min_score_color;
+		} else if (score >= max_score_color) {
+			score = max_score_color;
+		}
+
+		int index = (int) (((score - min_score_color) / range) * 255);
+
+		return custom_heatmap.get().getColors()[index];
 	}
 }

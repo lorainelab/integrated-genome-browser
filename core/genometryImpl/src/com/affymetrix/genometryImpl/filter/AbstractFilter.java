6c299d571fe0fbfedaa5632b0b2d6f61758de72f
hiralv <hiralv@uncc.edu>
2013-08-26 20:01:43 +0000

    Implement parameters in abstract filter.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15165 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/filter/AbstractFilter.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/AbstractFilter.java
@@ -1,10 +1,39 @@
 package com.affymetrix.genometryImpl.filter;
 
+import com.affymetrix.genometryImpl.general.IParameters;
+import com.affymetrix.genometryImpl.general.Parameters;
+import java.util.Map;
+
 /**
  *
  * @author hiralv
  */
-public abstract class AbstractFilter implements SymmetryFilterI {
+public abstract class AbstractFilter implements SymmetryFilterI, IParameters {
+	protected Parameters parameters;
+	
+	protected AbstractFilter(){
+		parameters = new Parameters();
+	}
+	
+	@Override
+	public Map<String, Class<?>> getParametersType(){
+		return parameters.getParametersType();
+	}
+
+	@Override
+	public final boolean setParametersValue(Map<String, Object> params){
+		return parameters.setParametersValue(params);
+	}
+	
+	@Override
+	public boolean setParameterValue(String key, Object value) {
+		return parameters.setParameterValue(key, value);
+	}
+
+	@Override
+	public Object getParameterValue(String key) {
+		return parameters.getParameterValue(key);
+	}
 	
 	@Override
 	public SymmetryFilterI newInstance(){

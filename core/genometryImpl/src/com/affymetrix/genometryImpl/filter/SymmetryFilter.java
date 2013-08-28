70abcc31b6a5c55279f9c1b2ff292832ce26f3e5
hiralv <hiralv@uncc.edu>
2013-08-28 15:00:04 +0000

    Adding symmetry filter.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15197 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/SymmetryFilter.java
@@ -0,0 +1,60 @@
+package com.affymetrix.genometryImpl.filter;
+
+import com.affymetrix.genometryImpl.GenometryConstants;
+import com.affymetrix.genometryImpl.general.IParameters;
+import com.affymetrix.genometryImpl.general.Parameters;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import java.util.Map;
+
+/**
+ *
+ * @author hiralv
+ */
+public abstract class SymmetryFilter implements SymmetryFilterI, IParameters {
+	protected Parameters parameters;
+	
+	protected SymmetryFilter(){
+		parameters = new Parameters();
+	}
+	
+	@Override
+	public Map<String, Class<?>> getParametersType(){
+		return parameters.getParametersType();
+	}
+
+	@Override
+	public final boolean setParametersValue(Map<String, Object> params){
+		return parameters.setParametersValue(params);
+	}
+	
+	@Override
+	public boolean setParameterValue(String key, Object value) {
+		return parameters.setParameterValue(key, value);
+	}
+
+	@Override
+	public Object getParameterValue(String key) {
+		return parameters.getParameterValue(key);
+	}
+	
+	@Override
+	public String getDisplay() {
+		return GenometryConstants.BUNDLE.getString("filter_" + getName());
+	}
+	
+	@Override
+	public boolean isFileTypeCategorySupported(FileTypeCategory fileTypeCategory) {
+		return fileTypeCategory == FileTypeCategory.Annotation 
+				|| fileTypeCategory == FileTypeCategory.Alignment
+				|| fileTypeCategory == FileTypeCategory.ProbeSet;
+	}
+	
+	@Override
+	public SymmetryFilterI newInstance(){
+		try {
+			return getClass().getConstructor().newInstance();
+		} catch (Exception ex) {
+		}
+		return null;
+	}
+}

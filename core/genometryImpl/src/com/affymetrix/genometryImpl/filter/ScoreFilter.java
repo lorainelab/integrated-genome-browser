bcd42f7d2975b76502b62821dcb2d163099decc7
hiralv <hiralv@uncc.edu>
2013-08-30 19:45:32 +0000

    Adding score filter.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15220 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/ScoreFilter.java
@@ -0,0 +1,38 @@
+package com.affymetrix.genometryImpl.filter;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.Scored;
+import com.affymetrix.genometryImpl.general.Parameter;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+
+/**
+ *
+ * @author hiralv
+ */
+public class ScoreFilter extends SymmetryFilter {
+	private final static String SCORE = "score";
+	private final static int DEFAULT_SCORE = 10;
+	
+    private Parameter<Integer> score = new Parameter<Integer>(DEFAULT_SCORE);
+	
+	public ScoreFilter(){
+		super();
+		parameters.addParameter(SCORE, Integer.class, score);
+	}
+	
+	@Override
+    public String getName() {
+        return "score";
+    }
+	
+    @Override
+    public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry sym) {
+		if(sym instanceof Scored){
+			float s = ((Scored)sym).getScore();
+			return Float.compare(s, Scored.UNKNOWN_SCORE) != 0 && s >= score.get();
+		}
+		return false;
+	}
+	
+}
+

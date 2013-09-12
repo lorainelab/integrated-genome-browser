5a8757397c55a5261c8edbcdad7a4aa3a461bb2f
hiralv <hiralv@uncc.edu>
2013-09-12 17:14:53 +0000

    Using comparator interface in score.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15301 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/filter/ScoreFilter.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/ScoreFilter.java
@@ -3,6 +3,8 @@ package com.affymetrix.genometryImpl.filter;
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.Scored;
 import com.affymetrix.genometryImpl.general.Parameter;
+import com.affymetrix.genometryImpl.operator.comparator.ComparatorI;
+import com.affymetrix.genometryImpl.operator.comparator.GreaterThanEqualComparator;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 
 /**
@@ -11,13 +13,17 @@ import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
  */
 public class ScoreFilter extends SymmetryFilter {
 	private final static String SCORE = "score";
+	private final static String COMPARATOR = "comparator";
 	private final static int DEFAULT_SCORE = 10;
+	private final static ComparatorI DEFAULT_COMPARATOR = new GreaterThanEqualComparator();
 	
     private Parameter<Integer> score = new Parameter<Integer>(DEFAULT_SCORE);
+	private Parameter<ComparatorI> comparator = new Parameter<ComparatorI>(DEFAULT_COMPARATOR);
 	
 	public ScoreFilter(){
 		super();
 		parameters.addParameter(SCORE, Integer.class, score);
+		parameters.addParameter(COMPARATOR, ComparatorI.class, comparator);
 	}
 	
 	@Override
@@ -29,7 +35,7 @@ public class ScoreFilter extends SymmetryFilter {
     public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry sym) {
 		if(sym instanceof Scored){
 			float s = ((Scored)sym).getScore();
-			return Float.compare(s, Scored.UNKNOWN_SCORE) != 0 && s >= score.get();
+			return Float.compare(s, Scored.UNKNOWN_SCORE) != 0 && comparator.get().operate(s, (float)score.get());
 		}
 		return false;
 	}
@@ -37,7 +43,7 @@ public class ScoreFilter extends SymmetryFilter {
 	@Override
 	public String getPrintableString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(SCORE).append(" >= ").append(score.get());
+		sb.append(SCORE).append(" ").append(comparator.get().getSymbol()).append(" ").append(score.get());
 		return sb.toString();
 	}
 }

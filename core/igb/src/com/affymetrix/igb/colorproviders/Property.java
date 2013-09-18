8625fb1ad260c278d6f9e832b46dff5a7d43bd7d
hiralv <hiralv@uncc.edu>
2013-09-18 18:26:39 +0000

    Updating color by property to include value, match color and not match color.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15325 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/colorproviders/Property.java
+++ core/igb/src/com/affymetrix/igb/colorproviders/Property.java
@@ -4,11 +4,8 @@ import java.awt.Color;
 
 import com.affymetrix.genometryImpl.color.ColorProvider;
 import com.affymetrix.genometryImpl.general.Parameter;
-import com.affymetrix.genometryImpl.style.HeatMapExtended;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
 import com.affymetrix.genometryImpl.symmetry.SymWithProps;
-import com.affymetrix.genoviz.color.ColorPalette;
-import com.affymetrix.genoviz.color.ColorScheme;
 
 /**
  *
@@ -17,7 +14,14 @@ import com.affymetrix.genoviz.color.ColorScheme;
 public class Property extends ColorProvider {
 	private final static String PROPERTY = "property";
 	public final static String DEFAULT_PROPERTY = "id";
-	
+	private final static String PROPERTY_VALUE = "value";
+	public final static String DEFAULT_PROPERTY_VALUE = "";
+	private final static String MATCH_COLOR = "match";
+	private final static Color DEFAULT_MATCH_COLOR = Color.GREEN;
+	private final static String NOT_MATCH_COLOR = "not_match";
+	private final static Color DEFAULT_NOT_MATCH_COLOR = Color.RED;
+	private Float float_property_value = null;
+		
 	protected Parameter<String> property = new Parameter<String>(DEFAULT_PROPERTY) {
 		@Override
 		public boolean set(Object e){
@@ -30,21 +34,42 @@ public class Property extends ColorProvider {
 		}
 	};
 	
-	private ColorPalette cp = new ColorPalette(ColorScheme.ACCENT8);
+	protected Parameter<String> property_value = new Parameter<String>(DEFAULT_PROPERTY_VALUE){
+		@Override
+		public boolean set(Object e){
+			try {
+				float_property_value = Float.parseFloat(e.toString());
+			} catch(Exception ex) {
+			}
+			return super.set(e);
+		}
+	};
+	
+	private Parameter<Color> matchColor = new Parameter<Color>(DEFAULT_MATCH_COLOR);
+	private Parameter<Color> notMatchColor = new Parameter<Color>(DEFAULT_NOT_MATCH_COLOR);
+	
+//	private ColorPalette cp = new ColorPalette(ColorScheme.ACCENT8);
 	
 	public Property(){
 		super();
 		parameters.addParameter(PROPERTY, String.class, property);
+		parameters.addParameter(PROPERTY_VALUE, String.class, property_value);
+		parameters.addParameter(MATCH_COLOR, Color.class, matchColor);
+		parameters.addParameter(NOT_MATCH_COLOR, Color.class, notMatchColor);
 	}
 	
 	@Override
 	public Color getColor(SeqSymmetry sym){
 		if(sym instanceof SymWithProps){
 			Object value = ((SymWithProps)sym).getProperty(property.get());
-			if(value != null){
-				return cp.getColor(value.toString());
+			if((value instanceof Float || value instanceof Double) 
+					&& float_property_value != null && Float.compare(float_property_value, (Float)value) == 0) {
+				return matchColor.get();
+			} else if(value != null && property_value.get() != null && value.toString().equals(property_value.get())){
+				return matchColor.get();
+//				return cp.getColor(value.toString());
 			}
 		}
-		return null;
+		return notMatchColor.get();
 	}
 }

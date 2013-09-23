56a79fb22a8c2156c0e196d649c7e860ad23c361
hiralv <hiralv@uncc.edu>
2013-09-23 17:05:34 +0000

    Adding match filter.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15327 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/MatchFilter.java
@@ -0,0 +1,50 @@
+package com.affymetrix.genometryImpl.filter;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genometryImpl.symmetry.SymWithResidues;
+import java.util.BitSet;
+
+/**
+ *
+ * @author hiralv
+ */
+public class MatchFilter extends SymmetryFilter {
+	
+	@Override
+    public String getName() {
+        return "match";
+    }
+	
+	@Override
+	public boolean isFileTypeCategorySupported(FileTypeCategory fileTypeCategory){
+		return fileTypeCategory == FileTypeCategory.Alignment;
+	}
+	
+    @Override
+    public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry ss) {
+		boolean filter = true;
+		int child_count = ss.getChildCount();
+		if (child_count > 0) {
+			SeqSymmetry child;
+			for (int i = 0; i < child_count; i++) {
+				child = ss.getChild(i);
+				if (child instanceof SymWithResidues) {
+					filter &= filter((SymWithResidues)child);
+				}
+			}
+		} else if (ss instanceof SymWithResidues) {
+			filter &= filter((SymWithResidues)ss);
+		}
+		return filter;
+    }
+  
+	private boolean filter(SymWithResidues swr) {
+		BitSet bitSet = swr.getResidueMask();
+		if (bitSet == null || bitSet.cardinality() == 0) {
+			return true;
+		}
+		return false;
+	}
+}

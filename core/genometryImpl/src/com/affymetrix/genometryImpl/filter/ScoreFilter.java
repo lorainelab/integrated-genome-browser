f9a2dec6c274029ce452d6fa15dd0d6fd7fdeff0
hiralv <hiralv@uncc.edu>
2013-09-24 14:47:25 +0000

    Use possible values in score interface.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15336 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/filter/ScoreFilter.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/ScoreFilter.java
@@ -2,10 +2,12 @@ package com.affymetrix.genometryImpl.filter;
 
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.Scored;
+import com.affymetrix.genometryImpl.general.BoundedParameter;
 import com.affymetrix.genometryImpl.general.Parameter;
-import com.affymetrix.genometryImpl.operator.comparator.GreaterThanEqualMathComparisonOperator;
 import com.affymetrix.genometryImpl.operator.comparator.MathComparisonOperator;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -15,10 +17,18 @@ public class ScoreFilter extends SymmetryFilter {
 	private final static String SCORE = "score";
 	private final static String COMPARATOR = "comparator";
 	private final static int DEFAULT_SCORE = 10;
-	private final static MathComparisonOperator DEFAULT_COMPARATOR = new GreaterThanEqualMathComparisonOperator();
+	private final static List<MathComparisonOperator> COMPARATOR_VALUES = new LinkedList<MathComparisonOperator>();
+	static {
+		COMPARATOR_VALUES.add(new com.affymetrix.genometryImpl.operator.comparator.GreaterThanEqualMathComparisonOperator());
+		COMPARATOR_VALUES.add(new com.affymetrix.genometryImpl.operator.comparator.GreaterThanMathComparisonOperator());
+		COMPARATOR_VALUES.add(new com.affymetrix.genometryImpl.operator.comparator.EqualMathComparisonOperator());
+		COMPARATOR_VALUES.add(new com.affymetrix.genometryImpl.operator.comparator.LessThanMathComparisonOperator());
+		COMPARATOR_VALUES.add(new com.affymetrix.genometryImpl.operator.comparator.LessThanEqualMathComparisonOperator());
+		COMPARATOR_VALUES.add(new com.affymetrix.genometryImpl.operator.comparator.NotEqualMathComparisonOperator());
+	}
 	
     private Parameter<Integer> score = new Parameter<Integer>(DEFAULT_SCORE);
-	private Parameter<MathComparisonOperator> comparator = new Parameter<MathComparisonOperator>(DEFAULT_COMPARATOR);
+	private Parameter<MathComparisonOperator> comparator = new BoundedParameter<MathComparisonOperator>(COMPARATOR_VALUES);
 	
 	public ScoreFilter(){
 		super();

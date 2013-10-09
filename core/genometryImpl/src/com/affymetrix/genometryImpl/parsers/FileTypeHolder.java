03caf8f2da3e80bded42b13f4b7d575f90cc4856
hiralv <hiralv@uncc.edu>
2013-10-09 18:51:17 +0000

    Avoid manual array copy.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15448 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/parsers/FileTypeHolder.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/parsers/FileTypeHolder.java
@@ -552,9 +552,7 @@ public class FileTypeHolder {
 				extensions = new ArrayList<String>();
 				nameToExtensionMap.put(name, extensions);
 			}
-			for (String ext : fileTypeHandler.getExtensions()) {
-				extensions.add(ext);
-			}
+			extensions.addAll(Arrays.asList(fileTypeHandler.getExtensions()));
 		}
 		return nameToExtensionMap;
 	}

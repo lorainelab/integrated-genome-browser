7f31950debf9619a3f601583f7414970357e81f0
hiralv <hiralv@uncc.edu>
2013-10-16 17:18:50 +0000

    Adding pair by run number.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15532 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/color/PariedByRunNo.java
@@ -0,0 +1,44 @@
+package com.affymetrix.genometryImpl.color;
+
+import com.affymetrix.genometryImpl.general.Parameter;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import com.affymetrix.genometryImpl.symmetry.BAMSym;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import java.awt.Color;
+
+/**
+ *
+ * @author hiralv
+ */
+public class PariedByRunNo extends ColorProvider {
+	private final static String FIRST_RUN_COLOR = "first_run";
+	private final static String SECOND_RUN_COLOR = "second_run";
+	private final static Color DEFAULT_FIRST_RUN_COLOR = new Color(204, 255, 255);
+	private final static Color DEFAULT_SECOND_RUN_COLOR = new Color(51, 255, 255);
+	
+	private Parameter<Color> firstRunColor = new Parameter<Color>(DEFAULT_FIRST_RUN_COLOR);
+	private Parameter<Color> secondRunColor = new Parameter<Color>(DEFAULT_SECOND_RUN_COLOR);
+	
+	public PariedByRunNo(){
+		super();
+		parameters.addParameter(FIRST_RUN_COLOR, Color.class, firstRunColor);
+		parameters.addParameter(SECOND_RUN_COLOR, Color.class, secondRunColor);
+	}
+	
+	@Override
+	public boolean isFileTypeCategorySupported(FileTypeCategory fileTypeCategory) {
+		return fileTypeCategory == FileTypeCategory.Alignment;
+	}
+	
+	@Override
+	public Color getColor(SeqSymmetry sym) {
+		if(sym instanceof BAMSym && ((BAMSym)sym).getReadPairedFlag()){
+			if(((BAMSym)sym).getFirstOfPairFlag()) {
+				return firstRunColor.get();
+			} else if(((BAMSym)sym).getSecondOfPairFlag()){
+				return secondRunColor.get();
+			}
+		}
+		return null;
+	}
+}

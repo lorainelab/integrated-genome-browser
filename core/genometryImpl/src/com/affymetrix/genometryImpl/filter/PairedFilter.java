21c8de8ca5bb9e54994776ec744285247aa8c119
hiralv <hiralv@uncc.edu>
2013-10-16 15:40:19 +0000

    Adding paired filter.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15521 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/PairedFilter.java
@@ -0,0 +1,50 @@
+package com.affymetrix.genometryImpl.filter;
+
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.general.BoundedParameter;
+import com.affymetrix.genometryImpl.general.Parameter;
+import com.affymetrix.genometryImpl.operator.comparator.MathComparisonOperator;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import com.affymetrix.genometryImpl.symmetry.BAMSym;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author hiralv
+ */
+public class PairedFilter extends SymmetryFilter {
+	private final static String COMPARATOR = "comparator";
+	private final static List<Boolean> COMPARATOR_VALUES = new LinkedList<Boolean>();
+	static {
+		COMPARATOR_VALUES.add(true);
+		COMPARATOR_VALUES.add(false);
+	}
+	private Parameter<Boolean> comparator = new BoundedParameter<Boolean>(COMPARATOR_VALUES);
+	
+	public PairedFilter(){
+		super();
+		parameters.addParameter(COMPARATOR, Boolean.class, comparator);
+	}
+	
+	@Override
+    public String getName() {
+        return "paired";
+    }
+	
+	@Override
+	public boolean isFileTypeCategorySupported(FileTypeCategory fileTypeCategory){
+		return fileTypeCategory == FileTypeCategory.Alignment;
+	}
+	
+    @Override
+    public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry ss) {
+		if (ss instanceof BAMSym) {
+			return comparator.get() == ((BAMSym)ss).getReadPairedFlag();
+		}
+		return false;
+    }
+
+}
+

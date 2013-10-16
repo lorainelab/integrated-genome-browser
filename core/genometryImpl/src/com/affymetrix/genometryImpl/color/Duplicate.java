a8fa8c1f3728f5bdfdc4bb419805a264057b44eb
hiralv <hiralv@uncc.edu>
2013-10-16 18:14:17 +0000

    Merging changes of r15540-15541. Adding and activating duplicate color by.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/branches/igb_8_0@15545 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/color/Duplicate.java
@@ -0,0 +1,43 @@
+package com.affymetrix.genometryImpl.color;
+
+import com.affymetrix.genometryImpl.general.Parameter;
+import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
+import com.affymetrix.genometryImpl.symmetry.BAMSym;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import java.awt.Color;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hiralv
+ */
+public class Duplicate extends ColorProvider {
+	private final static String DUPLICATE_COLOR = "duplicate";
+	private final static Color DEFAULT_DUPLICATE_COLOR = new Color(204, 255, 255);
+
+	private Parameter<Color> duplicateColor = new Parameter<Color>(DEFAULT_DUPLICATE_COLOR);
+		
+	public Duplicate(){
+		super();
+		parameters.addParameter(DUPLICATE_COLOR, Color.class, duplicateColor);
+	}
+	
+	@Override
+	public boolean isFileTypeCategorySupported(FileTypeCategory fileTypeCategory) {
+		return fileTypeCategory == FileTypeCategory.Alignment;
+	}
+	
+	@Override
+	public String getName() {
+		return "duplicate";
+	}
+	
+	@Override
+	public Color getColor(SeqSymmetry sym) {
+		if(sym instanceof BAMSym && ((BAMSym)sym).getDuplicateReadFlag()){
+			return duplicateColor.get();
+		}
+		return null;
+	}
+}
+

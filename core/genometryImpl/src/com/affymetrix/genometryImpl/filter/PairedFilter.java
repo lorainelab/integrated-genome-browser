cf2833fee079eed0e5cee306ba6cd79fc91e000a
hiralv <hiralv@uncc.edu>
2013-10-17 18:56:37 +0000

    Change option wordings.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15559 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/genometryImpl/src/com/affymetrix/genometryImpl/filter/PairedFilter.java
+++ core/genometryImpl/src/com/affymetrix/genometryImpl/filter/PairedFilter.java
@@ -3,7 +3,6 @@ package com.affymetrix.genometryImpl.filter;
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.general.BoundedParameter;
 import com.affymetrix.genometryImpl.general.Parameter;
-import com.affymetrix.genometryImpl.operator.comparator.MathComparisonOperator;
 import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
 import com.affymetrix.genometryImpl.symmetry.BAMSym;
 import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
@@ -15,17 +14,37 @@ import java.util.List;
  * @author hiralv
  */
 public class PairedFilter extends SymmetryFilter {
-	private final static String COMPARATOR = "show";
-	private final static List<Boolean> COMPARATOR_VALUES = new LinkedList<Boolean>();
-	static {
-		COMPARATOR_VALUES.add(Boolean.TRUE);
-		COMPARATOR_VALUES.add(Boolean.FALSE);
+	private static enum SHOW {
+		READ_WITH_MATES("Read with mates only", Boolean.TRUE), READ_WITHOUT_MATES("Read without mates only", Boolean.FALSE);
+		
+		String name;
+		boolean value;
+		SHOW(String name, boolean value){
+			this.name = name;
+			this.value = value;
+		}
+		
+		public boolean value(){
+			return value;
+		}
+		
+		@Override
+		public String toString(){
+			return name;
+		}
 	}
-	private Parameter<Boolean> comparator = new BoundedParameter<Boolean>(COMPARATOR_VALUES);
+		
+	private final static String COMPARATOR = "show";
+	private final static List<SHOW> COMPARATOR_VALUES = new LinkedList<SHOW>();
+	static {
+		COMPARATOR_VALUES.add(SHOW.READ_WITH_MATES);
+		COMPARATOR_VALUES.add(SHOW.READ_WITHOUT_MATES);
+	}
+	private Parameter<SHOW> comparator = new BoundedParameter<SHOW>(COMPARATOR_VALUES);
 	
 	public PairedFilter(){
 		super();
-		parameters.addParameter(COMPARATOR, Boolean.class, comparator);
+		parameters.addParameter(COMPARATOR, SHOW.class, comparator);
 	}
 	
 	@Override
@@ -41,7 +60,7 @@ public class PairedFilter extends SymmetryFilter {
     @Override
     public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry ss) {
 		if (ss instanceof BAMSym) {
-			return comparator.get() == ((BAMSym)ss).getReadPairedFlag();
+			return comparator.get().value() == ((BAMSym)ss).getReadPairedFlag();
 		}
 		return false;
     }

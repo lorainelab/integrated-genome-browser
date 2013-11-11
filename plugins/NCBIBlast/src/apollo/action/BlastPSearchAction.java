1a16ce21d54048636c781f88b08f2c6f63ca29d3
hiralv <hiralv@uncc.edu>
2013-11-11 18:52:05 +0000

    Adding blastp search action.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15792 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

---
+++ plugins/NCBIBlast/src/apollo/action/BlastPSearchAction.java
@@ -0,0 +1,113 @@
+package apollo.action;
+
+import apollo.analysis.RemoteBlastNCBI;
+import apollo.datamodel.Sequence;
+import apollo.datamodel.StrandedFeatureSet;
+import apollo.datamodel.StrandedFeatureSetI;
+import com.affymetrix.genometryImpl.AminoAcid;
+import com.affymetrix.genometryImpl.BioSeq;
+import com.affymetrix.genometryImpl.SeqSpan;
+import com.affymetrix.genometryImpl.event.GenericAction;
+import com.affymetrix.genometryImpl.event.GenericActionDoneCallback;
+import com.affymetrix.genometryImpl.symmetry.MutableSeqSymmetry;
+import com.affymetrix.genometryImpl.symmetry.MutableSingletonSeqSymmetry;
+import com.affymetrix.genometryImpl.symmetry.SeqSymmetry;
+import com.affymetrix.genometryImpl.symmetry.SimpleMutableSeqSymmetry;
+import com.affymetrix.genometryImpl.symmetry.SingletonSeqSymmetry;
+import com.affymetrix.genometryImpl.thread.CThreadHolder;
+import com.affymetrix.genometryImpl.util.ErrorHandler;
+import com.affymetrix.genometryImpl.util.GeneralUtils;
+import com.affymetrix.genometryImpl.util.SeqUtils;
+import com.affymetrix.igb.osgi.service.SeqMapViewI;
+import com.affymetrix.igb.shared.SequenceLoader;
+import java.awt.event.ActionEvent;
+import java.util.List;
+import java.util.logging.Level;
+
+/**
+ *
+ * @author hiralv
+ */
+public class BlastPSearchAction extends GenericAction {
+	private static final long serialVersionUID = 1l;
+	
+	private final SeqMapViewI smv;
+	
+	public BlastPSearchAction(SeqMapViewI smv) {
+		super(RemoteBlastNCBI.BlastType.blastp.toString().toUpperCase() + " nr protein database", null, null);
+		this.smv = smv;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		super.actionPerformed(e);
+		try {
+			final SeqSymmetry residues_sym = getResidueSym();
+			final BioSeq aseq = smv.getAnnotatedSeq();
+			final SeqSpan span = residues_sym.getSpan(aseq);
+			
+			final GenericActionDoneCallback doneback = new GenericActionDoneCallback() {
+				public void actionDone(GenericAction action) {
+					try {
+						String residues = SeqUtils.getResidues(residues_sym, aseq);
+						String proteinAnnotation = AminoAcid.getAminoAcid(residues, 1, span.isForward(), "");
+						StrandedFeatureSetI sf = new StrandedFeatureSet();
+						Sequence seq = new Sequence(aseq.getID(), proteinAnnotation);
+						
+						RemoteBlastNCBI blast = new RemoteBlastNCBI(RemoteBlastNCBI.BlastType.blastp, new RemoteBlastNCBI.BlastOptions());
+						String url = blast.runAnalysis(sf, seq, 1);
+						
+						GeneralUtils.browse(url);
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+				}
+			};
+			
+			SequenceLoader worker = new SequenceLoader("RemoteBlastNCBI", residues_sym.getSpan(aseq), doneback);
+			CThreadHolder.getInstance().execute(this, worker);
+		} catch (Exception ex) {
+			ErrorHandler.errorPanel("Problem occured in copying sequences to sequence viewer", ex, Level.WARNING);
+		}
+	}
+
+	private SeqSymmetry getResidueSym() {
+		SeqSymmetry residues_sym = null;
+		List<SeqSymmetry> syms = smv.getSelectedSyms();
+		if (syms.size() >= 1) {
+			if (syms.size() == 1) {
+				residues_sym = syms.get(0);
+			} else {
+				BioSeq aseq = smv.getAnnotatedSeq();
+				if (syms.size() > 1 || smv.getSeqSymmetry() != null) {
+					MutableSeqSymmetry temp = new SimpleMutableSeqSymmetry();
+					SeqUtils.union(syms, temp, aseq);
+
+					// Make all reverse children forward
+					residues_sym = new SimpleMutableSeqSymmetry();
+					SeqSymmetry child;
+					SeqSpan childSpan;
+					for (int i = 0; i < temp.getChildCount(); i++) {
+						child = temp.getChild(i);
+						childSpan = child.getSpan(aseq);
+						((MutableSeqSymmetry) residues_sym).addChild(new MutableSingletonSeqSymmetry(childSpan.getMin(), childSpan.getMax(), childSpan.getBioSeq()));
+					}
+					((MutableSeqSymmetry) residues_sym).addSpan(temp.getSpan(aseq));
+				}
+			}
+		} else {
+			residues_sym = smv.getSeqSymmetry();
+		}
+		SeqSpan span = residues_sym.getSpan(smv.getAnnotatedSeq());
+		int remainder = span.getLength() % 3;
+		if(remainder != 0) {
+			if(span.isForward()) {
+				residues_sym = new SingletonSeqSymmetry(span.getMin(), span.getMax() + 3 - remainder, span.getBioSeq());
+			} else {
+				residues_sym = new SingletonSeqSymmetry(span.getMin() + 3 - remainder, span.getMax(), span.getBioSeq());
+			}
+		}
+		return residues_sym;
+	}
+}
+

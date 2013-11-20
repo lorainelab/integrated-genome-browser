4dcee58d7f805e4b104d1fec6001cabe0bb7946d
hiralv <hiralv@uncc.edu>
2013-11-20 16:11:49 +0000

    Changing default foreground, background and edge match color.
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15830 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/tiers/TrackConstants.java
+++ core/igb/src/com/affymetrix/igb/tiers/TrackConstants.java
@@ -56,8 +56,8 @@ public interface TrackConstants {
 	static final boolean default_showResidueMask = true;
 	static final boolean default_shadeBasedOnQualityScore = false;
 	static final int default_max_depth = 10;
-	static final Color default_foreground = Color.decode("0x333399"); //Color.CYAN;
-	static final Color default_background = Color.decode("0xDEE0E0"); //Color.BLACK;
+	static final Color default_foreground = Color.decode("0x0247FE"); //Blue Color;
+	static final Color default_background = Color.WHITE;
 	static final Color default_start = new Color(204, 255, 255);
 	static final Color default_end = new Color(51, 255, 255);
 	static final String default_label_field = "";

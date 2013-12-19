aa5fd2acf8eea3138f05d5240d10c87f484075ca
tkanapar <tkanapar@uncc.edu>
2013-12-19 15:58:05 +0000

    IGBD-424 Fix for stopping large data sets
    git-svn-id: http://svn.code.sf.net/p/genoviz/code/trunk@15957 80d5a9c2-7d8c-4c9f-9369-4e03bca4d2bd

--- core/igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
+++ core/igb/src/com/affymetrix/igb/view/load/GeneralLoadUtils.java
@@ -971,8 +971,7 @@ public final class GeneralLoadUtils {
 	private static boolean checkBamAndSamLoading(GenericFeature feature, SeqSymmetry optimized_sym) {
 		//start max
 		boolean check = GeneralLoadView.getLoadView().isLoadingConfirm();
-		GeneralLoadView.getLoadView().setShowLoadingConfirm(false);
-		if (check && optimized_sym != null && feature.getExtension() != null
+		if (optimized_sym != null && feature.getExtension() != null
 				&& (feature.getExtension().endsWith("bam") || feature.getExtension().endsWith("sam"))) {
 			String message = "Region in view is big (> 500k), do you want to continue?";
 			int childrenCount = optimized_sym.getChildCount();
@@ -985,6 +984,10 @@ public final class GeneralLoadUtils {
 			}
 
 			if (spanWidth > 500000) {
+				if(!check ){
+					return !check;
+				}
+				GeneralLoadView.getLoadView().setShowLoadingConfirm(!check);
 				return !(Application.confirmPanel(message, 
 						PreferenceUtils.CONFIRM_BEFORE_LOAD, PreferenceUtils.default_confirm_before_load));
 			}

345fb211f15a6edd9ccd1951631b09e10f8bea8e
dcnorris <dcnorris@uncc.edu>
2014-06-04 15:18:21 -0400

    renamed GenericServerPref and changed this class from an interfact to a normal class

---
+++ core/genometryImpl/src/main/java/com/affymetrix/genometryImpl/general/GenericServerPrefKeys.java
@@ -0,0 +1,28 @@
+package com.affymetrix.genometryImpl.general;
+
+/**
+ * This class contains constant values that are used to stored preferences for
+ * the generic server.
+ *
+ */
+public class GenericServerPrefKeys {
+
+    private GenericServerPrefKeys() {
+        //private construtor to prevent instantiation
+    }
+    /**
+     * Key to retrieve the value for the server name
+     */
+    public static final String SERVER_NAME = "name";
+    public static final String IS_SERVER_ENABLED = "enabled";
+    public static final String CHECK_AVAILABILITY = "checkAvailibility";
+    public static final String SERVER_LOGIN = "login";
+    public static final String SERVER_PASSWORD = "password";
+    public static final String SERVER_URL = "url";
+    /**
+     * Key to retrieve the server type (DAS2, DAS, Quickload or other)
+     */
+    public static final String SERVER_TYPE = "type";
+    public static final String SERVER_ORDER = "order";
+    public static final String IS_DEFAULT_SERVER = "default";
+}

689de409f2b4bc654a9417325858468526b5b4e1
dcnorris <dcnorris@uncc.edu>
2014-08-05 10:59:20 -0400

    formatted file and added serialVersionUID

--- core/igb/src/main/java/com/affymetrix/igb/util/OptionChooserImpl.java
+++ core/igb/src/main/java/com/affymetrix/igb/util/OptionChooserImpl.java
@@ -6,69 +6,72 @@ import static com.affymetrix.igb.IGBConstants.BUNDLE;
 import com.affymetrix.igb.shared.OpenURIAction;
 import com.affymetrix.igb.view.load.GeneralLoadUtils;
 import com.affymetrix.igb.view.load.GeneralLoadView;
+
 /**
  *
  * @author hiralv
  */
-public class OptionChooserImpl extends OptionChooser{
+public class OptionChooserImpl extends OptionChooser {
 
-	private static final String SELECT_SPECIES = BUNDLE.getString("speciesCap");
-	
-	@Override
-	protected void speciesCBActionPerformedA(java.awt.event.ActionEvent evt) {
-		populateVersionCB();
-		if(getSpeciesCB().getSelectedIndex() == 0) {
-			getSpeciesCB().setEditable(true);
-		}else {
-			getSpeciesCB().setEditable(false);
-		}
-		getVersionCB().setSelectedIndex(0);
-	}
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void versionCBActionPerformedA(java.awt.event.ActionEvent evt) {
-		if (getVersionCB().getSelectedIndex() == 0) {
-			getVersionCB().setEditable(true);
-		} else {
-			getVersionCB().setEditable(false);
-		}
-	}
-	
-	@Override
-	protected void loadAsSeqCBActionPerformedA(java.awt.event.ActionEvent evt) {
-		// Do nothing
-	}
+    private static final String SELECT_SPECIES = BUNDLE.getString("speciesCap");
 
-	public void refreshSpeciesList(){
-		getSpeciesCB().removeAllItems();
-		getSpeciesCB().addItem(OpenURIAction.UNKNOWN_SPECIES_PREFIX + " " + OpenURIAction.unknown_group_count);
-		for(String species : GeneralLoadUtils.getSpeciesList()){
-			getSpeciesCB().addItem(species);
-		}
+    @Override
+    protected void speciesCBActionPerformedA(java.awt.event.ActionEvent evt) {
+        populateVersionCB();
+        if (getSpeciesCB().getSelectedIndex() == 0) {
+            getSpeciesCB().setEditable(true);
+        } else {
+            getSpeciesCB().setEditable(false);
+        }
+        getVersionCB().setSelectedIndex(0);
+    }
 
-		String speciesName = GeneralLoadView.getLoadView().getSelectedSpecies();
+    @Override
+    protected void versionCBActionPerformedA(java.awt.event.ActionEvent evt) {
+        if (getVersionCB().getSelectedIndex() == 0) {
+            getVersionCB().setEditable(true);
+        } else {
+            getVersionCB().setEditable(false);
+        }
+    }
 
-		if(!SELECT_SPECIES.equals(speciesName)) {
-			getSpeciesCB().setSelectedItem(speciesName);
-		}else {
-			getSpeciesCB().setSelectedIndex(0);
-		}
+    @Override
+    protected void loadAsSeqCBActionPerformedA(java.awt.event.ActionEvent evt) {
+        // Do nothing
+    }
 
-		AnnotatedSeqGroup group = GenometryModel.getGenometryModel().getSelectedSeqGroup();
-		if (group != null) {
-			getVersionCB().setSelectedItem(group.getID());
-		} else {
-			getVersionCB().setSelectedIndex(0);
-		}
-	}
-	
-	private void populateVersionCB() {
-		String speciesName = (String) getSpeciesCB().getSelectedItem();
-		getVersionCB().removeAllItems();
-		getVersionCB().addItem(OpenURIAction.UNKNOWN_GENOME_PREFIX + " " + OpenURIAction.unknown_group_count);
-		for(String version : GeneralLoadUtils.getGenericVersions(speciesName)){
-			getVersionCB().addItem(version);
-		}
-	}
-	
+    public void refreshSpeciesList() {
+        getSpeciesCB().removeAllItems();
+        getSpeciesCB().addItem(OpenURIAction.UNKNOWN_SPECIES_PREFIX + " " + OpenURIAction.unknown_group_count);
+        for (String species : GeneralLoadUtils.getSpeciesList()) {
+            getSpeciesCB().addItem(species);
+        }
+
+        String speciesName = GeneralLoadView.getLoadView().getSelectedSpecies();
+
+        if (!SELECT_SPECIES.equals(speciesName)) {
+            getSpeciesCB().setSelectedItem(speciesName);
+        } else {
+            getSpeciesCB().setSelectedIndex(0);
+        }
+
+        AnnotatedSeqGroup group = GenometryModel.getGenometryModel().getSelectedSeqGroup();
+        if (group != null) {
+            getVersionCB().setSelectedItem(group.getID());
+        } else {
+            getVersionCB().setSelectedIndex(0);
+        }
+    }
+
+    private void populateVersionCB() {
+        String speciesName = (String) getSpeciesCB().getSelectedItem();
+        getVersionCB().removeAllItems();
+        getVersionCB().addItem(OpenURIAction.UNKNOWN_GENOME_PREFIX + " " + OpenURIAction.unknown_group_count);
+        for (String version : GeneralLoadUtils.getGenericVersions(speciesName)) {
+            getVersionCB().addItem(version);
+        }
+    }
+
 }

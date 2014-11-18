6b8c5cdec4387a648e1d89953582af2515407dc2
dcnorris <dcnorris@localhost.localdomain>
2014-11-18 16:03:19 -0500

    fix for bug when no matching category is looked up

--- core/igb/src/main/java/com/affymetrix/igb/shared/PreprocessorTypeReference.java
+++ core/igb/src/main/java/com/affymetrix/igb/shared/PreprocessorTypeReference.java
@@ -5,28 +5,29 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  *
  * @author dcnorris
  */
 public class PreprocessorTypeReference {
-
+    
     private final Table<String, FileTypeCategory, GlyphPreprocessorI> preprocessorTypeReferenceTable;
-
+    
     private PreprocessorTypeReference() {
         preprocessorTypeReferenceTable = HashBasedTable.create();
     }
-
+    
     public static PreprocessorTypeReference getInstance() {
         return PreprocessorTypeReferenceHolder.INSTANCE;
     }
-
+    
     private static class PreprocessorTypeReferenceHolder {
-
+        
         private static final PreprocessorTypeReference INSTANCE = new PreprocessorTypeReference();
     }
-
+    
     public void addPreprocessor(FileTypeCategory category, GlyphPreprocessorI factory) {
         checkNotNull(category);
         checkNotNull(factory);
@@ -34,17 +35,20 @@ public class PreprocessorTypeReference {
             preprocessorTypeReferenceTable.put(factory.getName(), category, factory);
         }
     }
-
+    
     public void removePreprocessor(GlyphPreprocessorI factory) {
         checkNotNull(factory);
         if (preprocessorTypeReferenceTable.containsValue(factory)) {
             preprocessorTypeReferenceTable.remove(factory.getName(), factory);
         }
     }
-
+    
     public Collection<GlyphPreprocessorI> getPreprocessorsForType(FileTypeCategory category) {
         checkNotNull(category);
-        return preprocessorTypeReferenceTable.columnMap().get(category).values();
+        if (preprocessorTypeReferenceTable.columnMap().containsKey(category)) {
+            return preprocessorTypeReferenceTable.columnMap().get(category).values();
+        }
+        return Collections.<GlyphPreprocessorI>emptyList();
     }
-
+    
 }

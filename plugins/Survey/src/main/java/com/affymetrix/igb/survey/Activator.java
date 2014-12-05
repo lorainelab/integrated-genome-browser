624d3f56186bfbc7a696833dc7a0be23f1c632d8
dcnorris <dcnorris@uncc.edu>
2014-12-05 11:01:02 -0500

    small patch to survey plugin

--- plugins/Survey/src/main/java/com/affymetrix/igb/survey/Activator.java
+++ plugins/Survey/src/main/java/com/affymetrix/igb/survey/Activator.java
@@ -27,13 +27,17 @@ import com.affymetrix.igb.osgi.service.XServiceRegistrar;
 import static com.affymetrix.igb.survey.ShowSurvey.*;
 import com.google.common.io.Resources;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Logger;
 
 /**
  *
  * @author hiralv
  */
 public class Activator extends XServiceRegistrar<IGBService> implements BundleActivator {
+
+    private static final Logger LOG = Logger.getLogger(Activator.class.getName());
 
     public Activator() {
         super(IGBService.class);
@@ -45,55 +49,59 @@ public class Activator extends XServiceRegistrar<IGBService> implements BundleAc
         JRPMenu surveysMenu = new JRPMenu("Survey_surveysMenu", "News and Surveys");
 
         InputStream inputStream = null;
-        //inputStream = Activator.class.getResourceAsStream("/surveys.xml");        
-        URL url = new URL(BUNDLE.getString("surveys"));
-        inputStream = new ByteArrayInputStream(Resources.toByteArray(url));
-        if (inputStream != null) {
-            List<Survey> surveys = SurveyParser.parse(inputStream);
-            GeneralUtils.safeClose(inputStream);
+        try {
+            //inputStream = Activator.class.getResourceAsStream("/surveys.xml");        
+            URL url = new URL(BUNDLE.getString("surveys"));
+            inputStream = new ByteArrayInputStream(Resources.toByteArray(url));
+            if (inputStream != null) {
+                List<Survey> surveys = SurveyParser.parse(inputStream);
+                GeneralUtils.safeClose(inputStream);
 
-            Collections.sort(surveys, new Comparator<Survey>() {
-                public int compare(Survey o1, Survey o2) {
-                    return o1.getEnd().compareTo(o2.getEnd());
-                }
-            });
+                Collections.sort(surveys, new Comparator<Survey>() {
+                    public int compare(Survey o1, Survey o2) {
+                        return o1.getEnd().compareTo(o2.getEnd());
+                    }
+                });
 
-            Date today = Calendar.getInstance().getTime();
-            for (final Survey survey : surveys) {
-                if (today.compareTo(survey.getStart()) >= 0
-                        && today.compareTo(survey.getEnd()) < 0) {
-                    JMenuItem item = new JMenuItem(
-                            new GenericAction(survey.getName(), null, null) {
-                                @Override
-                                public void actionPerformed(ActionEvent e) {
-                                    super.actionPerformed(e);
-                                    GeneralUtils.browse(survey.getLink());
+                Date today = Calendar.getInstance().getTime();
+                for (final Survey survey : surveys) {
+                    if (today.compareTo(survey.getStart()) >= 0
+                            && today.compareTo(survey.getEnd()) < 0) {
+                        JMenuItem item = new JMenuItem(
+                                new GenericAction(survey.getName(), null, null) {
+                                    @Override
+                                    public void actionPerformed(ActionEvent e) {
+                                        super.actionPerformed(e);
+                                        GeneralUtils.browse(survey.getLink());
+                                    }
                                 }
+                        );
+                        surveysMenu.add(item);
+                    }
+                }
+                for (final Survey survey : surveys) {
+                    if (today.compareTo(survey.getStart()) >= 0
+                            && today.compareTo(survey.getEnd()) < 0
+                            && !PreferenceUtils.getBooleanParam(survey.getId(), false)) {
+
+                        Timer timer = new Timer();
+                        timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                showSurvey(survey);
                             }
-                    );
-                    surveysMenu.add(item);
+                        }, 10000);
+
+                        break;
+                    }
                 }
             }
-            for (final Survey survey : surveys) {
-                if (today.compareTo(survey.getStart()) >= 0
-                        && today.compareTo(survey.getEnd()) < 0
-                        && !PreferenceUtils.getBooleanParam(survey.getId(), false)) {
 
-                    Timer timer = new Timer();
-                    timer.schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            showSurvey(survey);
-                        }
-                    }, 10000);
-
-                    break;
-                }
+            if (surveysMenu.getItemCount() > 0) {
+                return new ServiceRegistration<?>[]{bundleContext.registerService(AMenuItem.class, new AMenuItem(surveysMenu, "help"), null)};
             }
-        }
-
-        if (surveysMenu.getItemCount() > 0) {
-            return new ServiceRegistration<?>[]{bundleContext.registerService(AMenuItem.class, new AMenuItem(surveysMenu, "help"), null)};
+        } catch (IOException ex) {
+            LOG.info("No survey.xml currently published");
         }
 
         return null;

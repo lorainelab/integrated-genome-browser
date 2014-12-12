a89d036dc46a4f932796f1a095132982de5fbfed
dcnorris <dcnorris@uncc.edu>
2014-12-11 19:11:43 -0500

    updated logging

--- core/genometryImpl/src/main/java/com/affymetrix/genometryImpl/quickload/QuickLoadServerModel.java
+++ core/genometryImpl/src/main/java/com/affymetrix/genometryImpl/quickload/QuickLoadServerModel.java
@@ -30,9 +30,9 @@ import java.net.URL;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import org.jdom.JDOMException;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
 
 import org.xml.sax.SAXParseException;
@@ -43,6 +43,7 @@ import org.xml.sax.SAXParseException;
  */
 public final class QuickLoadServerModel {
 
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(QuickLoadServerModel.class);
     private static final SynonymLookup LOOKUP = SynonymLookup.getDefaultLookup();
     private static final Pattern tab_regex = Pattern.compile("\t");
     private final String root_url;
@@ -81,7 +82,7 @@ public final class QuickLoadServerModel {
         primary_url = pri_url;
         primaryServer = priServer;
 
-        Logger.getLogger(QuickLoadServerModel.class.getName()).log(Level.FINE, "( {0}, {1} )", new Object[]{root_url, primary_url});
+        logger.trace("( {}, {} )", new Object[]{root_url, primary_url});
     }
 
     /**
@@ -189,8 +190,7 @@ public final class QuickLoadServerModel {
     }
 
     private synchronized boolean initGenome(String genome_name) {
-        Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                Level.FINE, "initializing data for genome: {0}", genome_name);
+        logger.debug("initializing data for genome: {}", genome_name);
         boolean metaOK = loadSeqInfo(genome_name);
         if (metaOK && loadAnnotationNames(genome_name)) {
             initialized.add(genome_name);
@@ -214,10 +214,9 @@ public final class QuickLoadServerModel {
      */
     private boolean loadAnnotationNames(String genome_name) {
         genome_name = LOOKUP.findMatchingSynonym(genome_names, genome_name);
-        Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                Level.FINE, "loading list of available annotations for genome: {0}", genome_name);
+        logger.debug("loading list of available annotations for genome: {}", genome_name);
 
-		// Make a new list of typeNames, in case this is being re-initialized
+        // Make a new list of typeNames, in case this is being re-initialized
         // If this search fails, then we're just returning an empty map.
         List<AnnotMapElt> annotList = new ArrayList<AnnotMapElt>();
         genome2annotsMap.put(genome_name, annotList);
@@ -233,13 +232,13 @@ public final class QuickLoadServerModel {
             try {
                 annots_found = processAnnotsXml(istr, validationIstr, annotList);
             } catch (SAXParseException x) {
-                String errorMessage = "QuickLoad Server {0} has an invalid annotations (annots.xml) file for {1}: {2}. Please contact the server administrators or the IGB development team to let us know about the problem.";
+                String errorMessage = "QuickLoad Server {} has an invalid annotations (annots.xml) file for {}: {}. Please contact the server administrators or the IGB development team to let us know about the problem.";
                 String errorText = MessageFormat.format(errorMessage, root_url, genome_name, x.getMessage());
                 String title = "Invalid annots.xml file";
                 ErrorHandler.errorPanelWithReportBug(title, errorText, Level.SEVERE);
                 return false;
             } catch (JDOMException x) {
-                String errorMessage = "QuickLoad Server {0} has an invalid annotations (annots.xml) file for {1}. Please contact the server administrators or the IGB development team to let us know about the problem.";
+                String errorMessage = "QuickLoad Server {} has an invalid annotations (annots.xml) file for {}. Please contact the server administrators or the IGB development team to let us know about the problem.";
                 String errorText = MessageFormat.format(errorMessage, root_url, genome_name);
                 String title = "Invalid annots.xml file";
                 ErrorHandler.errorPanelWithReportBug(title, errorText, Level.SEVERE);
@@ -247,33 +246,28 @@ public final class QuickLoadServerModel {
             }
 
             if (annots_found) {
-                Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                        Level.FINE, "Found {0} files in {1} on server {2}.", new Object[]{annotList.size(), genome_name, root_url});
+                logger.debug("Found {} files in {} on server {}.", new Object[]{annotList.size(), genome_name, root_url});
                 return true;
             }
 
-            Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                    Level.FINE, "Couldn''t found annots.xml for {0}. Looking for annots.txt now.", genome_name);
+            logger.debug("Couldn''t found annots.xml for {}. Looking for annots.txt now.", genome_name);
             filename = getPath(genome_name, Constants.annotsTxt);
             istr = getInputStream(filename, getCacheAnnots(), false);
 
             annots_found = processAnnotsTxt(istr, annotList);
 
             if (!annots_found) {
-                ErrorHandler.errorPanelWithReportBug("Missing Required File", MessageFormat.format("QuickLoad Server {0} does not contain required annots.xml/annots.txt metadata "
-                        + "file for requested genome version {1}. "
+                ErrorHandler.errorPanelWithReportBug("Missing Required File", MessageFormat.format("QuickLoad Server {} does not contain required annots.xml/annots.txt metadata "
+                        + "file for requested genome version {}. "
                         + "IGB may not be able to display this genome.", new Object[]{root_url, genome_name}), Level.SEVERE);
             } else {
-                Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                        Level.FINE, "Found {0} files in {1} on server {2}.", new Object[]{annotList.size(), genome_name, root_url});
+                logger.debug("Found {} files in {} on server {}.", new Object[]{annotList.size(), genome_name, root_url});
             }
 
             return annots_found;
 
         } catch (Exception ex) {
-            Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                    Level.SEVERE, "Couldn''t found either annots.xml or annots.txt for {0}", genome_name);
-            ex.printStackTrace();
+            logger.error("Couldn''t found either annots.xml or annots.txt for {}", genome_name, ex);
             return false;
         } finally {
             GeneralUtils.safeClose(istr);
@@ -345,8 +339,7 @@ public final class QuickLoadServerModel {
                 lift_stream = getInputStream(lift_path, getCacheAnnots(), true);
             } catch (Exception ex) {
                 // exception can be ignored, since we'll look for modChromInfo file.
-                Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                        Level.FINE, "couldn''t find {0}, looking instead for {1}", new Object[]{liftAll, modChromInfo});
+                logger.debug("couldn''t find {}, looking instead for {}", new Object[]{liftAll, modChromInfo});
                 lift_stream = null;
             }
 
@@ -355,8 +348,7 @@ public final class QuickLoadServerModel {
                 try {
                     ginfo_stream = getInputStream(ginfo_path, getCacheAnnots(), false);
                 } catch (Exception ex) {
-                    Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                            Level.WARNING, "couldn''t find {0}, looking instead for {1}", new Object[]{liftAll, genomeTxt});
+                    logger.error("couldn''t find {}, looking instead for {}", new Object[]{liftAll, genomeTxt});
                     ginfo_stream = null;
                 }
             }
@@ -366,9 +358,7 @@ public final class QuickLoadServerModel {
                 try {
                     cinfo_stream = getInputStream(cinfo_path, getCacheAnnots(), false);
                 } catch (Exception ex) {
-                    Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                            Level.WARNING, "ERROR: could find {0} or {1} or {2}", new Object[]{lift_path, ginfo_path, cinfo_path});
-                    ex.printStackTrace();
+                    logger.error("ERROR: could find {} or {} or {}", new Object[]{lift_path, ginfo_path, cinfo_path, ex});
                     cinfo_stream = null;
                 }
             }
@@ -402,8 +392,8 @@ public final class QuickLoadServerModel {
             GeneralUtils.safeClose(chrom_stream);
         }
 //		if(!success){
-//			ErrorHandler.errorPanel("Missing Required File", MessageFormat.format("QuickLoad Server {0} does not contain required sequence metadata "
-//					+ "file for requested genome version {1}. "
+//			ErrorHandler.errorPanel("Missing Required File", MessageFormat.format("QuickLoad Server {} does not contain required sequence metadata "
+//					+ "file for requested genome version {}. "
 //					+ "IGB may not be able to display this genome.",new Object[]{root_url,genome_name}));
 //		}
         return success;
@@ -424,13 +414,12 @@ public final class QuickLoadServerModel {
             try {
                 istr = getInputStream(contentsTxt, getCacheAnnots(), false, false); // Html is not allowed for requesting contents.txt
             } catch (Exception e) {
-                Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                        Level.WARNING, "ERROR: Couldn''t open ''{0}{1}\n:  {2}", new Object[]{getLoadURL(), contentsTxt, e.toString()});
+                logger.error("ERROR: Couldn''t open ''{}{}\n:  {}", new Object[]{getLoadURL(), contentsTxt, e.toString()});
                 istr = null; // dealt with below
             }
 
             if (istr == null) {
-                throw new FileNotFoundException(MessageFormat.format("Could not load QuickLoad contents from\n{0}{1}", new Object[]{getLoadURL(), contentsTxt}));
+                throw new FileNotFoundException(MessageFormat.format("Could not load QuickLoad contents from\n{}{}", new Object[]{getLoadURL(), contentsTxt}));
             }
 
             ireader = new InputStreamReader(istr);
@@ -448,7 +437,7 @@ public final class QuickLoadServerModel {
                     genome_name = fields[0];
                     genome_name = genome_name.trim();
                     if (genome_name.length() == 0) {
-                        Logger.getLogger(QuickLoadServerModel.class.getName()).log(Level.INFO, "Found blank QuickLoad genome -- skipping");
+                        logger.info("Found blank QuickLoad genome -- skipping");
                         continue;
                     }
                     group = this.getSeqGroup(genome_name);  // returns existing group if found, otherwise creates a new group
@@ -506,16 +495,14 @@ public final class QuickLoadServerModel {
          */
         if (istr == null && isLoadingFromPrimary() && !fileMayNotExist) {
 
-            Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                    Level.WARNING, "Primary Server :{0} is not responding. So disabling it for this session.", primaryServer.serverName);
+            logger.warn("Primary Server :{} is not responding. So disabling it for this session.", primaryServer.serverName);
             primaryServer.setServerStatus(ServerStatus.NotResponding);
 
             load_url = getLoadURL() + append_url;
             istr = LocalUrlCacher.getInputStream(load_url, write_to_cache, null, fileMayNotExist, allowHtml);
         }
 
-        Logger.getLogger(QuickLoadServerModel.class.getName()).log(
-                Level.FINE, "Load URL: {0}", load_url);
+        logger.debug( "Load URL: {}", load_url);
         return istr;
     }
 
@@ -534,7 +521,7 @@ public final class QuickLoadServerModel {
         try {
             stream = getInputStream(Constants.speciesTxt, false, true);
         } catch (IOException ex) {
-            Logger.getLogger(QuickLoadServerModel.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(null, ex);
         }
         return stream;
     }

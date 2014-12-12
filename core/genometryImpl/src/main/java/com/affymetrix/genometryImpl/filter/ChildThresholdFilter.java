903e54d131df76e0676e0da17308ae5abfcc55bc
dcnorris <dcnorris@uncc.edu>
2014-12-12 16:15:02 -0500

    applied formatting

--- core/genometryImpl/src/main/java/com/affymetrix/genometryImpl/filter/ChildThresholdFilter.java
+++ core/genometryImpl/src/main/java/com/affymetrix/genometryImpl/filter/ChildThresholdFilter.java
@@ -7,38 +7,39 @@ import com.affymetrix.genometryImpl.symmetry.impl.SeqSymmetry;
 
 /**
  *
- * @author Anuj
- * This class is used to filter the given symmetry based on its span length 
- * with the threshold value of the filter this is used in FindJunctionOperator class
+ * @author Anuj This class is used to filter the given symmetry based on its
+ * span length with the threshold value of the filter this is used in
+ * FindJunctionOperator class
  */
-public class ChildThresholdFilter extends SymmetryFilter{
-	private final static String THRESHOLD = "threshold";
-	private final static int DEFAULT_THRESHOLD = 5;
-	
+public class ChildThresholdFilter extends SymmetryFilter {
+
+    private final static String THRESHOLD = "threshold";
+    private final static int DEFAULT_THRESHOLD = 5;
+
     private Parameter<Integer> threshold = new Parameter<Integer>(DEFAULT_THRESHOLD);
-	
-	public ChildThresholdFilter(){
-		super();
-		parameters.addParameter(THRESHOLD, Integer.class, threshold);
-	}
-	
+
+    public ChildThresholdFilter() {
+        super();
+        parameters.addParameter(THRESHOLD, Integer.class, threshold);
+    }
+
     @Override
     public String getName() {
         return null;
     }
 
-	@Override
-	public String getDisplay() {
-		return getName();
-	}
-	
+    @Override
+    public String getDisplay() {
+        return getName();
+    }
+
     @Override
     public boolean filterSymmetry(BioSeq bioseq, SeqSymmetry ss) {
-		SeqSpan span = ss.getSpan(bioseq);
-        if((span.getMax() - span.getMin()) < threshold.get()){
+        SeqSpan span = ss.getSpan(bioseq);
+        if ((span.getMax() - span.getMin()) < threshold.get()) {
             return false;
-		} else {
+        } else {
             return true;
-		}
+        }
     }
 }

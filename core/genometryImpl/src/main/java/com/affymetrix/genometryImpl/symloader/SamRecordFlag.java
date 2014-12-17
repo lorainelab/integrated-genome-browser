cfba65be41338bd3af1222a260e948440d07b0e9
dcnorris <dcnorris@uncc.edu>
2014-12-17 15:05:40 -0500

    added SamRecordFlag class and test coverage

---
+++ core/genometryImpl/src/main/java/com/affymetrix/genometryImpl/symloader/SamRecordFlag.java
@@ -0,0 +1,113 @@
+package com.affymetrix.genometryImpl.symloader;
+
+import com.affymetrix.genometryImpl.tooltip.ToolTipConstants;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author dcnorris code partially borrowed from IGV
+ * org.broad.igv.sam.SAMAlignment java class
+ * @author jrobinso
+ *
+ *
+ * From Sam Spec: FLAG: bitwise FLAG. Each bit is explained in the following
+ *
+ * 0x1 template having multiple segments in sequencing
+ * 0x2 each segment properly aligned according to the aligner
+ * 0x4 segment unmapped
+ * 0x8 next segment in the template unmapped
+ * 0x10 SEQ being reverse complemented
+ * 0x20 SEQ of the next segment in the template being reversed
+ * 0x40 the first segment in the template
+ * 0x80 the last segment in the template
+ * 0x100 secondary alignment
+ * 0x200 not passing quality controls
+ * 0x400 PCR or optical duplicate
+ * 0x800 supplementary alignment
+ *
+ */
+public class SamRecordFlag {
+
+    private static final int READ_PAIRED_FLAG = 0x1;
+    private static final int PROPER_PAIR_FLAG = 0x2;
+    private static final int READ_UNMAPPED_FLAG = 0x4;
+    private static final int MATE_UNMAPPED_FLAG = 0x8;
+    private static final int READ_STRAND_FLAG = 0x10;
+    private static final int MATE_STRAND_FLAG = 0x20;
+    private static final int FIRST_OF_PAIR_FLAG = 0x40;
+    private static final int SECOND_OF_PAIR_FLAG = 0x80;
+    private static final int NOT_PRIMARY_ALIGNMENT_FLAG = 0x100;
+    private static final int READ_FAILS_VENDOR_QUALITY_CHECK_FLAG = 0x200;
+    private static final int DUPLICATE_READ_FLAG = 0x400;
+    private static final int SUPPLEMENTARY_ALIGNMENT_FLAG = 0x800;
+
+    private final int flag;
+
+    public SamRecordFlag(int flag) {
+        this.flag = flag;
+    }
+
+    //read paired
+    public boolean isPaired() {
+        return (flag & READ_PAIRED_FLAG) != 0;
+    }
+
+    //read mapped in proper pair
+    public boolean isProperPair() {
+        return ((flag & READ_PAIRED_FLAG) != 0) && ((flag & PROPER_PAIR_FLAG) != 0);
+    }
+
+    // read unmapped flag inverse
+    public boolean isMapped() {
+        return (flag & READ_UNMAPPED_FLAG) == 0;
+    }
+
+    //mate unmapped
+    public boolean isMateUnmapped() {
+        return (flag & MATE_UNMAPPED_FLAG) != 0;
+    }
+
+    //read reverse strand
+    public boolean isReverseStrand() {
+        return (flag & READ_STRAND_FLAG) != 0;
+    }
+
+    //mate reverse strand
+    public boolean isMateReverseStrand() {
+        return (flag & MATE_STRAND_FLAG) != 0;
+    }
+
+    //first in pair
+    public boolean isFirstOfPair() {
+        return isPaired() && (flag & FIRST_OF_PAIR_FLAG) != 0;
+    }
+
+    //second in pair
+    public boolean isSecondOfPair() {
+        return isPaired() && (flag & SECOND_OF_PAIR_FLAG) != 0;
+    }
+
+    //not primary alignment flag inverse
+    public boolean isPrimary() {
+        return (flag & NOT_PRIMARY_ALIGNMENT_FLAG) == 0;
+    }
+
+    //read fails platform/vendor quality checks
+    public boolean isVendorFailedRead() {
+        return (flag & READ_FAILS_VENDOR_QUALITY_CHECK_FLAG) != 0;
+    }
+
+    //read is PCR or optical duplicate
+    public boolean isDuplicate() {
+        return (flag & DUPLICATE_READ_FLAG) != 0;
+    }
+
+    //supplementary alignment
+    public boolean isSupplementary() {
+        return (flag & SUPPLEMENTARY_ALIGNMENT_FLAG) != 0;
+    }
+
+
+
+}

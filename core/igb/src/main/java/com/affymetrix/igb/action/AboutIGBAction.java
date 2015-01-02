50173424def4ce53071dc082df82bc2442291ed1
dcnorris <dcnorris@uncc.edu>
2015-01-02 10:16:40 -0500

    merged in Dr. Loraine's content changes with some refactoring added

--- core/igb/src/main/java/com/affymetrix/igb/action/AboutIGBAction.java
+++ core/igb/src/main/java/com/affymetrix/igb/action/AboutIGBAction.java
@@ -5,7 +5,6 @@ import com.affymetrix.genometryImpl.event.GenericActionHolder;
 import com.affymetrix.genometryImpl.util.PreferenceUtils;
 import static com.affymetrix.igb.IGBConstants.APP_NAME;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
-import java.awt.Component;
 import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Insets;
@@ -15,12 +14,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Open a window showing information about Integrated Genome Browser.
@@ -31,6 +30,7 @@ public class AboutIGBAction extends GenericAction {
 
     private static final long serialVersionUID = 1l;
     private static final AboutIGBAction ACTION = new AboutIGBAction();
+    private static final Logger logger = LoggerFactory.getLogger(AboutIGBAction.class);
 
     static {
         GenericActionHolder.getInstance().addGenericAction(ACTION);
@@ -57,15 +57,14 @@ public class AboutIGBAction extends GenericAction {
         pane.setEditable(false);
         pane.setText(text);
         pane.addHyperlinkListener(new HyperlinkListener() {
+            @Override
             public void hyperlinkUpdate(HyperlinkEvent e) {
                 if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
                     if (Desktop.isDesktopSupported()) {
                         try {
                             Desktop.getDesktop().browse(e.getURL().toURI());
-                        } catch (IOException ex) {
-                            Logger.getLogger(AboutIGBAction.class.getName()).log(Level.SEVERE, null, ex);
-                        } catch (URISyntaxException ex) {
-                            Logger.getLogger(AboutIGBAction.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IOException | URISyntaxException ex) {
+                            logger.error("Error navigating to hyperlink in about IGB window", ex);
                         }
                     }
                 }
@@ -73,7 +72,7 @@ public class AboutIGBAction extends GenericAction {
         });
         pane.setMargin(new Insets(10, 10, 10, 10));
         JFrame j = new JFrame("About Integrated Genome Browser");
-        Component add = j.add(pane);
+        j.add(pane);
         j.setSize(new Dimension(500, 500));
         j.setVisible(true);
     }
@@ -84,30 +83,45 @@ public class AboutIGBAction extends GenericAction {
      * @return String text
      */
     public String makeText() {
-        String text = "<html><body bgcolor=\"FFFFFF\"><h1><center>About Integrated Genome Browser</center></h1>"
-                + "<p>IGB (pronounced ig-bee) is a fast, flexible, desktop genome browser"
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("</head>");
+        sb.append("<body bgcolor=\"FFFFFF\">");
+        sb.append("<h1><center>About Integrated Genome Browser</center></h1>");
+        sb.append("<p>");
+        sb.append("IGB (pronounced ig-bee) is a fast, flexible, desktop genome browser"
                 + " first developed at <a href=\"http://www.affymetrix.com\">Affymetrix</a> for tiling arrays."
                 + " IGB is now open source software supported by grants and donations."
-                + " To find out more, visit <a href=\"http://www.bioviz.org/igb\">BioViz.org</a>.</p>"
-                + "<p>If you use IGB in your research, please cite "
+                + " To find out more, visit <a href=\"http://www.bioviz.org/igb\">BioViz.org</a>."
+        );
+        sb.append("</p>");
+        sb.append("<p>");
+        sb.append("If you use IGB in your research, please cite "
                 + "Nicol JW, Helt GA, Blanchard SG Jr, Raja A, Loraine AE. "
                 + "<a href=\"http://www.ncbi.nlm.nih.gov/pubmed/19654113\">The Integrated Genome "
                 + " Browser: free software for distribution and exploration of"
-                + "genome-scale datasets.</a> Bioinformatics. 2009 25(20):2730-1.</p>";
+                + "genome-scale datasets.</a> Bioinformatics. 2009 25(20):2730-1."
+        );
+        sb.append("</p>");
+
         String cache_root = com.affymetrix.genometryImpl.util.LocalUrlCacher.getCacheRoot();
         File cache_file = new File(cache_root);
         if (cache_file.exists()) {
-            text = text + "<p>Cached data are stored in "
-                    + cache_file.getAbsolutePath() + ".</p>";
+            sb.append("<p>");
+            sb.append("Cached data are stored in ").append(cache_file.getAbsolutePath());
+            sb.append("</p>");
         }
         String data_dir = PreferenceUtils.getAppDataDirectory();
         if (data_dir != null) {
             File data_dir_f = new File(data_dir);
-            text = text + "<p>Application data stored in "
-                    + data_dir_f.getAbsolutePath() + ".</p>";
+            sb.append("<p>");
+            sb.append("Application data stored in ").append(data_dir_f.getAbsolutePath());
+            sb.append("</p>");
         }
-        text = text + "</body></html>";
-        return text;
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
     }
 
 }

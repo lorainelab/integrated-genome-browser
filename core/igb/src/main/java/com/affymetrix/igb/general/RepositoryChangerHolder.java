f543a5b7a2cb56bf73978149927253bef40875d7
dcnorris <dcnorris@uncc.edu>
2015-02-08 17:06:17 -0500

    organized code

--- core/igb/src/main/java/com/affymetrix/igb/general/RepositoryChangerHolder.java
+++ core/igb/src/main/java/com/affymetrix/igb/general/RepositoryChangerHolder.java
@@ -4,8 +4,8 @@ import com.affymetrix.genometry.event.GenericServerInitListener;
 import com.affymetrix.genometry.event.RepositoryChangeListener;
 import com.affymetrix.genometry.general.GenericServer;
 import com.affymetrix.genometry.util.LoadUtils.ServerStatus;
-import com.affymetrix.igb.service.api.RepositoryChangeHolderI;
 import com.affymetrix.igb.prefs.PreferencesPanel;
+import com.affymetrix.igb.service.api.RepositoryChangeHolderI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,9 +13,14 @@ import java.util.Map;
 import javax.swing.JFrame;
 
 public class RepositoryChangerHolder implements RepositoryChangeHolderI {
-	private static RepositoryChangerHolder instance = new RepositoryChangerHolder();
-	
-	private final GenericServerInitListener genericServerListener = evt -> {
+
+    private static final RepositoryChangerHolder instance = new RepositoryChangerHolder();
+
+    public static RepositoryChangerHolder getInstance() {
+        return instance;
+    }
+
+    private final GenericServerInitListener genericServerListener = evt -> {
         GenericServer gServer = (GenericServer) evt.getSource();
         if (gServer.getServerStatus() == ServerStatus.Initialized) {
             repositoryAdded(gServer.URL);
@@ -23,87 +28,92 @@ public class RepositoryChangerHolder implements RepositoryChangeHolderI {
             repositoryRemoved(gServer.URL);
         }
     };
-			
-	private RepositoryChangerHolder() {
-		super();
-		ServerList.getRepositoryInstance().addServerInitListener(genericServerListener);
-	}
-	
-	public static RepositoryChangerHolder getInstance() {
-		return instance;
-	}
 
-	private List<RepositoryChangeListener> repositoryChangeListeners = new ArrayList<>();
+    private final List<RepositoryChangeListener> repositoryChangeListeners = new ArrayList<>();
 
+    private RepositoryChangerHolder() {
+        super();
+        ServerList.getRepositoryInstance().addServerInitListener(genericServerListener);
+    }
 
-	/**
-	 * add a RepositoryChangeListener, to be called when there
-	 * is a change to the Bundle Repositories on the Bundle
-	 * Repository tab of the Preferences page
-	 * @param repositoryChangeListener the listener
-	 */
-	public void addRepositoryChangeListener(RepositoryChangeListener repositoryChangeListener) {
-		repositoryChangeListeners.add(repositoryChangeListener);
-	}
+    /**
+     * add a RepositoryChangeListener, to be called when there
+     * is a change to the Bundle Repositories on the Bundle
+     * Repository tab of the Preferences page
+     *
+     * @param repositoryChangeListener the listener
+     */
+    @Override
+    public void addRepositoryChangeListener(RepositoryChangeListener repositoryChangeListener) {
+        repositoryChangeListeners.add(repositoryChangeListener);
+    }
 
-	/**
-	 * remove a RepositoryChangeListener - so that is is no longer
-	 * called for changes to the Bundle Repository tab of
-	 * the Perferences page
-	 * @param repositoryChangeListener the listener
-	 */
-	public void removeRepositoryChangeListener(RepositoryChangeListener repositoryChangeListener) {
-		repositoryChangeListeners.remove(repositoryChangeListener);
-	}
+    /**
+     * remove a RepositoryChangeListener - so that is is no longer
+     * called for changes to the Bundle Repository tab of
+     * the Perferences page
+     *
+     * @param repositoryChangeListener the listener
+     */
+    @Override
+    public void removeRepositoryChangeListener(RepositoryChangeListener repositoryChangeListener) {
+        repositoryChangeListeners.remove(repositoryChangeListener);
+    }
 
+    @Override
+    public boolean repositoryAdded(String url) {
+        boolean addedOK = true;
+        for (RepositoryChangeListener repositoryChangeListener : repositoryChangeListeners) {
+            addedOK &= repositoryChangeListener.repositoryAdded(url);
+        }
+        return addedOK;
+    }
 
-	public boolean repositoryAdded(String url) {
-		boolean addedOK = true;
-		for (RepositoryChangeListener repositoryChangeListener : repositoryChangeListeners) {
-			addedOK &= repositoryChangeListener.repositoryAdded(url);
-		}
-		return addedOK;
-	}
+    @Override
+    public void repositoryRemoved(String url) {
+        for (RepositoryChangeListener repositoryChangeListener : repositoryChangeListeners) {
+            repositoryChangeListener.repositoryRemoved(url);
+        }
+    }
 
-	public void repositoryRemoved(String url) {
-		for (RepositoryChangeListener repositoryChangeListener : repositoryChangeListeners) {
-			repositoryChangeListener.repositoryRemoved(url);
-		}
-	}
+    /**
+     * get the list of all repositories from the
+     * Bundle Repository Preferences tab
+     *
+     * @return the list of bundle repositories (URLs)
+     */
+    @Override
+    public Map<String, String> getRepositories() {
+        Map<String, String> repositories = new HashMap<>();
+        ServerList.getRepositoryInstance().getAllServers().stream().filter(repositoryServer -> repositoryServer.isEnabled()).forEach(repositoryServer -> {
+            repositories.put(repositoryServer.serverName, repositoryServer.URL);
+        });
+        return repositories;
+    }
 
-	/**
-	 * get the list of all repositories from the 
-	 * Bundle Repository Preferences tab
-	 * @return the list of bundle repositories (URLs)
-	 */
-	public Map<String, String> getRepositories() {
-		Map<String, String> repositories = new HashMap<>();
-		ServerList.getRepositoryInstance().getAllServers().stream().filter(repositoryServer -> repositoryServer.isEnabled()).forEach(repositoryServer -> {
-			repositories.put(repositoryServer.serverName, repositoryServer.URL);
-		});
-		return repositories;
-	}
+    /**
+     * mark a bundle repository as down / unavailable
+     * due to an error trying to connect
+     *
+     * @param url the URL to mark
+     */
+    @Override
+    public void failRepository(String url) {
+        ServerList.getRepositoryInstance().getServer(url).setEnabled(false);
+    }
 
-	/**
-	 * mark a bundle repository as down / unavailable
-	 * due to an error trying to connect
-	 * @param url the URL to mark
-	 */
-	public void failRepository(String url) {
-		ServerList.getRepositoryInstance().getServer(url).setEnabled(false);
-	}
-
-	/**
-	 * display the Bundle Repository tab of the Preferences page
-	 */
-	public void displayRepositoryPreferences() {
-		if (PreferencesPanel.TAB_PLUGIN_PREFS != -1) {
-			PreferencesPanel pv = PreferencesPanel.getSingleton();
-			pv.setTab(PreferencesPanel.TAB_PLUGIN_PREFS);	// Repository preferences tab
-			JFrame f = pv.getFrame();
-			f.setVisible(true);
-		} else {
-			System.out.println("Plugin Repository Preferences not instantiated");
-		}
-	}
+    /**
+     * display the Bundle Repository tab of the Preferences page
+     */
+    @Override
+    public void displayRepositoryPreferences() {
+        if (PreferencesPanel.TAB_PLUGIN_PREFS != -1) {
+            PreferencesPanel pv = PreferencesPanel.getSingleton();
+            pv.setTab(PreferencesPanel.TAB_PLUGIN_PREFS);	// Repository preferences tab
+            JFrame f = pv.getFrame();
+            f.setVisible(true);
+        } else {
+            System.out.println("Plugin Repository Preferences not instantiated");
+        }
+    }
 }

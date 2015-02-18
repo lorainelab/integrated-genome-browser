bb0ce859214e5ae1660982c3a8100ea60b27baa8
dcnorris <dcnorris@uncc.edu>
2015-02-18 12:20:12 -0500

    some minor cleanup

--- core/igb/src/main/java/com/affymetrix/igb/util/GraphicsUtil.java
+++ core/igb/src/main/java/com/affymetrix/igb/util/GraphicsUtil.java
@@ -7,44 +7,37 @@ import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
 
 /**
- * A Graphics Util class for IGB, it is designed to add and implement all the
- * graphic related methods and use globally.
  *
  * @author nick
  */
 public class GraphicsUtil {
 
-	/**
-	 * Get device compatible image by passed width and height.
-	 */
-	public static BufferedImage getDeviceCompatibleImage(int width, int height) {
-		GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		GraphicsDevice screenDevice = graphicsEnvironment.getDefaultScreenDevice();
-		GraphicsConfiguration graphicConfiguration = screenDevice.getDefaultConfiguration();
-		BufferedImage image = graphicConfiguration.createCompatibleImage(width, height);
-				//, Transparency.TRANSLUCENT);
+    /**
+     * Get device compatible image by passed width and height.
+     */
+    public static BufferedImage getDeviceCompatibleImage(int width, int height) {
+        GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice screenDevice = graphicsEnvironment.getDefaultScreenDevice();
+        GraphicsConfiguration graphicConfiguration = screenDevice.getDefaultConfiguration();
+        BufferedImage image = graphicConfiguration.createCompatibleImage(width, height);
+        return image;
+    }
 
-		return image;
-	}
+    /**
+     * Resize the passed image by passed width and height.
+     */
+    public static BufferedImage resizeImage(BufferedImage image,
+            int width, int height) {
+        if (image.getWidth() == width
+                && image.getHeight() == height) {
+            return image;
+        }
 
-	/**
-	 * Resize the passed image by passed width and height.
-	 */
-	public static BufferedImage resizeImage(BufferedImage image,
-			int width, int height) {
-		if (image.getWidth() == width
-				&& image.getHeight() == height) {
-			return image;
-		}
-
-		int type = image.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : image.getType();
-		BufferedImage resizedImage = new BufferedImage(width, height, type);
-		Graphics2D g = resizedImage.createGraphics();
-//		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-//				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-		g.drawImage(image, 0, 0, width, height, null);
-		g.dispose();
-
-		return resizedImage;
-	}
+        int type = image.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : image.getType();
+        BufferedImage resizedImage = new BufferedImage(width, height, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(image, 0, 0, width, height, null);
+        g.dispose();
+        return resizedImage;
+    }
 }

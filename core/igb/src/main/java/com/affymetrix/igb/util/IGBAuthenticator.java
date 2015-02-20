56af2494791593be4aefdead17d5037607ece32f
Tarun K. Mall <c2tarun@hotmail.com>
2015-02-20 17:03:03 -0500

    Fixed problem on closing authentication box instead of cancelling it.

--- core/igb/src/main/java/com/affymetrix/igb/util/IGBAuthenticator.java
+++ core/igb/src/main/java/com/affymetrix/igb/util/IGBAuthenticator.java
@@ -10,6 +10,8 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Authenticator;
@@ -263,6 +265,12 @@ public class IGBAuthenticator extends Authenticator {
 		
 		JOptionPane optionPane = buildDialog(serverObject, authOptional, urlString, errorString, anon, auth, username, password, remember);
 		JDialog jdg = optionPane.createDialog(parent, null);
+                jdg.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent evt) {
+                        authenticationRequestCancelled = true;
+                    }
+                });
 		jdg.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		jdg.setVisible(true);
 		if(optionPane.getValue() == (Integer) JOptionPane.CANCEL_OPTION){

780354cdac8d684c0bb14d755f2bb01c0caec57c
dcnorris <dcnorris@localhost.localdomain>
2015-03-30 20:18:08 -0400

    replaced Implicit toString used on an array

--- plugins/Tutorial/src/main/java/com/affymetrix/igb/tutorial/TutorialManager.java
+++ plugins/Tutorial/src/main/java/com/affymetrix/igb/tutorial/TutorialManager.java
@@ -19,6 +19,7 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -191,7 +192,7 @@ public class TutorialManager implements GenericActionListener, GenericActionDone
         }
         if (step.getHighlight() != null) {
             if (!highlightWidget(step.getHighlight())) {
-                ErrorHandler.errorPanel("Tutorial Error", "error in tutorial, unable to find widget " + step.getHighlight(), Level.SEVERE);
+                ErrorHandler.errorPanel("Tutorial Error", "error in tutorial, unable to find widget " + Arrays.toString(step.getHighlight()), Level.SEVERE);
             }
         }
         if (step.getExecute() != null) {

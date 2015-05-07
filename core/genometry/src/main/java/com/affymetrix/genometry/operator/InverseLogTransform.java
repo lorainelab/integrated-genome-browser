bf660e4bbaac8817246526dd2ec80919af178d65
dcnorris <dcnorris@uncc.edu>
2015-05-07 12:47:27 -0400

    conflict's resolved for merge of be4e6df39534c6c246286514bdad67444ea61140 from igb_8_4

--- core/genometry/src/main/java/com/affymetrix/genometry/operator/InverseLogTransform.java
+++ core/genometry/src/main/java/com/affymetrix/genometry/operator/InverseLogTransform.java
@@ -1,34 +1,67 @@
-
 package com.affymetrix.genometry.operator;
+
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author auser
  */
-public class InverseLogTransform extends AbstractLogTransform implements Operator{
-	private static final String BASE_NAME = "inverse_log";
-	private static final String PARAMETER_NAME = "base";
-	
-	public InverseLogTransform(){
-		this(0.0);
-	}
-	
-	public InverseLogTransform(Double base) {
-		super(base);
-	}
-	
-	@Override
-	protected String getParameterName(){
-		return PARAMETER_NAME;
-	}
-	
-	@Override
-	protected String getBaseName() {
-		return BASE_NAME;
-	}
-	
-	@Override
-	public float transform(float x) {
-		return (float)(Math.pow(base, x));
-	}
+public class InverseLogTransform extends AbstractLogTransform implements Operator {
+
+    private static final String BASE_NAME = "inverse_log";
+    private static final String PARAMETER_NAME = "base";
+    private boolean hideParamConfigOptions;
+    private static final Logger logger = LoggerFactory.getLogger(InverseLogTransform.class);
+
+    public InverseLogTransform() {
+        this(0.0);
+    }
+
+    public InverseLogTransform(Double base) {
+        super(base);
+    }
+
+    public InverseLogTransform(Double base, boolean hideParamConfigOptions) {
+        super(base);
+        this.hideParamConfigOptions = hideParamConfigOptions;
+    }
+
+    @Override
+    protected String getParameterName() {
+        return PARAMETER_NAME;
+    }
+
+    @Override
+    protected String getBaseName() {
+        return BASE_NAME;
+    }
+
+    @Override
+    public float transform(float x) {
+        return (float) (Math.pow(base, x));
+    }
+
+    @Override
+    public Map<String, Class<?>> getParametersType() {
+        if (hideParamConfigOptions) {
+            return null;
+        }
+        return super.getParametersType();
+    }
+
+    @Override
+    public Operator newInstance() {
+        try {
+            if (hideParamConfigOptions) {
+                return getClass().getConstructor(Double.class, boolean.class).newInstance(base, true);
+            } else {
+                return super.newInstance();
+            }
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+        return null;
+    }
 }
--- core/genometry/src/main/java/com/affymetrix/genometry/operator/LogTransform.java
+++ core/genometry/src/main/java/com/affymetrix/genometry/operator/LogTransform.java
@@ -1,46 +1,80 @@
 package com.affymetrix.genometry.operator;
 
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public final class LogTransform extends AbstractLogTransform implements Operator{
-	private static final String BASE_NAME = "log";
-	private static final String PARAMETER_NAME = "base";
-	
-	double LN_BASE;
-	float LOG_1;
+public final class LogTransform extends AbstractLogTransform implements Operator {
 
-	public LogTransform() {
-		this(0.0);
-	}
-	
-	public LogTransform(Double base) {
-		super(base);
-		LN_BASE = Math.log(base);
-		LOG_1 = (float)(Math.log(1)/LN_BASE);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(LogTransform.class);
+    private static final String BASE_NAME = "log";
+    private static final String PARAMETER_NAME = "base";
+    private boolean hideParamConfigOptions;
+    double LN_BASE;
+    float LOG_1;
 
-	@Override
-	protected String getParameterName(){
-		return PARAMETER_NAME;
-	}
-	
-	@Override
-	protected String getBaseName() {
-		return BASE_NAME;
-	}
-	
-	public float transform(float x) {
-		return (x <= 1) ? LOG_1 : (float)(Math.log(x)/LN_BASE);
-	}
+    public LogTransform() {
+        this(0.0);
+    }
 
-	@Override
-	public boolean setParameterValue(String key, Object value) {
-		if (parameterized && super.setParameterValue(key, value)) {
-			if (!("e".equals(value.toString().trim().toLowerCase()))) {
-				LN_BASE = Math.log(base);
-				LOG_1 = (float) (Math.log(1) / LN_BASE);
-				return true;
-			}
-		}
-		return false;
-	}
+    public LogTransform(Double base) {
+        super(base);
+        LN_BASE = Math.log(base);
+        LOG_1 = (float) (Math.log(1) / LN_BASE);
+    }
+
+    public LogTransform(Double base, boolean hideParamConfigOptions) {
+        super(base);
+        LN_BASE = Math.log(base);
+        LOG_1 = (float) (Math.log(1) / LN_BASE);
+        this.hideParamConfigOptions = hideParamConfigOptions;
+    }
+
+    @Override
+    protected String getParameterName() {
+        return PARAMETER_NAME;
+    }
+
+    @Override
+    protected String getBaseName() {
+        return BASE_NAME;
+    }
+
+    public float transform(float x) {
+        return (x <= 1) ? LOG_1 : (float) (Math.log(x) / LN_BASE);
+    }
+
+    @Override
+    public boolean setParameterValue(String key, Object value) {
+        if (parameterized && super.setParameterValue(key, value)) {
+            if (!("e".equals(value.toString().trim().toLowerCase()))) {
+                LN_BASE = Math.log(base);
+                LOG_1 = (float) (Math.log(1) / LN_BASE);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Map<String, Class<?>> getParametersType() {
+        if (hideParamConfigOptions) {
+            return null;
+        }
+        return super.getParametersType();
+    }
+
+    @Override
+    public Operator newInstance() {
+        try {
+            if (hideParamConfigOptions) {
+                return getClass().getConstructor(Double.class, boolean.class).newInstance(base, true);
+            } else {
+                return super.newInstance();
+            }
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+        return null;
+    }
 }

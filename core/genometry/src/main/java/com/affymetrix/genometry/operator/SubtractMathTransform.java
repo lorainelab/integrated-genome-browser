4da6e5d55d552bc7c9c2f5560db79e4789460da8
Tarun K. Mall <c2tarun@hotmail.com>
2015-05-12 11:34:43 -0400

    Allowed comma in input params for track operations
    Conflicts:
    	core/genometry/src/main/java/com/affymetrix/genometry/operator/SubtractMathTransform.java

--- core/genometry/src/main/java/com/affymetrix/genometry/operator/SubtractMathTransform.java
+++ core/genometry/src/main/java/com/affymetrix/genometry/operator/SubtractMathTransform.java
@@ -5,22 +5,22 @@ package com.affymetrix.genometry.operator;
  * @author hiralv
  */
 public class SubtractMathTransform extends AbstractMathTransform {
-	
-	private static final String BASE_NAME = "subtract";
-	private static final String PARAMETER_NAME = "subtract by";
-	
-	@Override
-	protected String getParameterName(){
-		return PARAMETER_NAME;
-	}
 
-	@Override
-	protected String getBaseName() {
-		return BASE_NAME;
-	}
-	
-	@Override
-	public float transform(float x) {
-		return (float)(x - base);
-	}
+    private static final String BASE_NAME = "subtract";
+    private static final String PARAMETER_NAME = "subtract";
+
+    @Override
+    protected String getParameterName() {
+        return PARAMETER_NAME;
+    }
+
+    @Override
+    protected String getBaseName() {
+        return BASE_NAME;
+    }
+
+    @Override
+    public float transform(float x) {
+        return (float) (x - base);
+    }
 }

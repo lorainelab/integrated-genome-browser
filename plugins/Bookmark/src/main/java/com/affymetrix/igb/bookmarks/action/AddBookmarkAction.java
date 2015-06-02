cae3c7b4a20d44424285e16449f375854ef2d19e
Tarun K. Mall <c2tarun@hotmail.com>
2015-06-02 15:41:42 -0400

    Added all actions to KeyStrokeBindingRegistry

--- plugins/Bookmark/src/main/java/com/affymetrix/igb/bookmarks/action/AddBookmarkAction.java
+++ plugins/Bookmark/src/main/java/com/affymetrix/igb/bookmarks/action/AddBookmarkAction.java
@@ -1,8 +1,6 @@
 package com.affymetrix.igb.bookmarks.action;
 
-import aQute.bnd.annotation.component.Component;
-import aQute.bnd.annotation.component.Reference;
-import com.affymetrix.genometry.event.GenericAction;
+import com.affymetrix.genometry.event.GenericActionHolder;
 import com.affymetrix.igb.bookmarks.BookmarkEditor;
 import static com.affymetrix.igb.bookmarks.BookmarkManagerView.BUNDLE;
 import com.lorainelab.igb.services.IgbService;
@@ -10,28 +8,28 @@ import java.awt.event.ActionEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(name = AddBookmarkAction.COMPONENT_NAME, immediate = true, provide = GenericAction.class)
 public class AddBookmarkAction extends BookmarkAction {
-    public static final String COMPONENT_NAME = "AddBookmarkAction";
+
     private static final long serialVersionUID = 1L;
     private static AddBookmarkAction ACTION;
-    private IgbService igbService;
+    private final IgbService igbService;
     private final int TOOLBAR_INDEX = 9;
     private static final Logger logger = LoggerFactory.getLogger(AddBookmarkAction.class);
+
+    public static void createAction(IgbService igbService) {
+        ACTION = new AddBookmarkAction(igbService);
+        GenericActionHolder.getInstance().addGenericAction(ACTION);
+    }
 
     public static AddBookmarkAction getAction() {
         return ACTION;
     }
-    
-    @Reference(optional = false)
-    public void setIgbService(IgbService igbService) {
-        this.igbService = igbService;
-    }
 
-    public AddBookmarkAction() {
+    private AddBookmarkAction(IgbService igbService) {
         super(BUNDLE.getString("addBookmark"), "16x16/actions/bookmark-new.png",
                 "22x22/actions/bookmark-new.png");
         setKeyStrokeBinding("ctrl D");
+        this.igbService = igbService;
     }
 
     @Override

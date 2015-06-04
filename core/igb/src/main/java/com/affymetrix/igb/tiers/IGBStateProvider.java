4798fb11aeddf95001d5c974adfaf86df20aabd7
Tarun K. Mall <c2tarun@hotmail.com>
2015-06-04 09:18:14 -0400

    Fixed windows specific problem while extracting filename from URL

--- core/igb/src/main/java/com/affymetrix/igb/tiers/IGBStateProvider.java
+++ core/igb/src/main/java/com/affymetrix/igb/tiers/IGBStateProvider.java
@@ -1,5 +1,6 @@
 package com.affymetrix.igb.tiers;
 
+import static com.affymetrix.common.CommonUtils.IS_WINDOWS;
 import com.affymetrix.genometry.parsers.FileTypeCategory;
 import com.affymetrix.genometry.parsers.FileTypeHandler;
 import com.affymetrix.genometry.parsers.FileTypeHolder;
@@ -8,6 +9,7 @@ import com.affymetrix.genometry.style.GraphState;
 import com.affymetrix.genometry.style.ITrackStyleExtended;
 import com.affymetrix.genometry.util.GeneralUtils;
 import com.affymetrix.genometry.util.PreferenceUtils;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -43,8 +45,9 @@ public final class IGBStateProvider extends DefaultStateProvider {
     }
 
     /**
-     * If there is no AnnotStyle with the given name, just returns the given name; else modifies the name such that
-     * there are no instances that are currently using it.
+     * If there is no AnnotStyle with the given name, just returns the given
+     * name; else modifies the name such that there are no instances that are
+     * currently using it.
      */
     public static String getUniqueName(String name) {
         String result = name.toLowerCase();
@@ -136,7 +139,11 @@ public final class IGBStateProvider extends DefaultStateProvider {
 
             if (!getShowFullFilePathInTrackMark()) {
                 if (track_name != null) {
-                    track_name = track_name.substring(track_name.lastIndexOf(java.io.File.separator) + 1);
+                    if (IS_WINDOWS && !track_name.startsWith("http")) {
+                        track_name = track_name.substring(track_name.lastIndexOf(File.separator) + 1);
+                    } else {
+                        track_name = track_name.substring(track_name.lastIndexOf("/") + 1);
+                    }
                 }
             } else {
                 track_name = unique_name;

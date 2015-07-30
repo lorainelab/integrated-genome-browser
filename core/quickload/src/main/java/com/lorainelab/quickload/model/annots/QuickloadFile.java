d20cbb3fc8a2032fe0d69b8f1716b2f2cf9c7a5c
dcnorris <dcnorris@localhost>
2015-07-30 16:55:36 -0400

    added a filter to prevent possible duplicates in annots.xml

--- core/quickload/src/main/java/com/lorainelab/quickload/model/annots/QuickloadFile.java
+++ core/quickload/src/main/java/com/lorainelab/quickload/model/annots/QuickloadFile.java
@@ -3,6 +3,7 @@ package com.lorainelab.quickload.model.annots;
 import com.google.common.base.Strings;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -254,6 +255,32 @@ public class QuickloadFile {
         if (!Strings.isNullOrEmpty(value)) {
             props.put("collapsed", value);
         }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 61 * hash + Objects.hashCode(this.name);
+        hash = 61 * hash + Objects.hashCode(this.title);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final QuickloadFile other = (QuickloadFile) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        return true;
     }
 
 }

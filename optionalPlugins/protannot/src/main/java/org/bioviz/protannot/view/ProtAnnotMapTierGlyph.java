f9eeee440dd753f623ce9bc3dd8851b8a1e2c60c
John Eckstein <jeckste@gmail.com>
2015-07-31 17:47:26 -0400

    remove unnecessary reflection

--- optionalPlugins/protannot/src/main/java/org/bioviz/protannot/view/ProtAnnotMapTierGlyph.java
+++ optionalPlugins/protannot/src/main/java/org/bioviz/protannot/view/ProtAnnotMapTierGlyph.java
@@ -5,12 +5,10 @@
  */
 package org.bioviz.protannot.view;
 
-import com.affymetrix.genoviz.bioviews.Glyph;
 import com.affymetrix.genoviz.bioviews.GlyphI;
 import com.affymetrix.genoviz.widget.tieredmap.GlyphSearchNode;
 import com.affymetrix.genoviz.widget.tieredmap.MapTierGlyph;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import org.slf4j.Logger;
@@ -24,95 +22,95 @@ public class ProtAnnotMapTierGlyph extends MapTierGlyph {
     
     private static final Logger logger = LoggerFactory.getLogger(ProtAnnotMapTierGlyph.class);
     
-    @Override
-    public void addChild(GlyphI glyph, int position) {
-        try {
-            Field gsnField = MapTierGlyph.class.getDeclaredField("gsn");
-            gsnField.setAccessible(true);
-            GlyphSearchNode gsn = (GlyphSearchNode) gsnField.get(this);
-            
-            parentAddChild(glyph, position);
-            gsn.addGlyph(glyph);
-            gsnField.setAccessible(false);
-        } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
-        }
-    }
-    
-    @Override
-    public void addChild(GlyphI glyph) {
-        try {
-            Field gsnField = MapTierGlyph.class.getDeclaredField("gsn");
-            gsnField.setAccessible(true);
-            GlyphSearchNode gsn = (GlyphSearchNode) gsnField.get(this);
-            
-            Field lrpField = MapTierGlyph.class.getDeclaredField("last_removed_position");
-            lrpField.setAccessible(true);
-            int last_removed_position = (Integer) lrpField.get(this);
-            
-            if (last_removed_position != -1) {
-                parentAddChild(glyph, last_removed_position);
-
-                last_removed_position = -1;
-                lrpField.setInt(this, last_removed_position);
-                
-            } else {
-                parentAddChild(glyph);
-            }
-            
-            lrpField.setAccessible(false);
-            gsn.addGlyph(glyph);
-            gsnField.setAccessible(false);
-        } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
-        }
-    }
-    
-    public void parentAddChild(GlyphI glyph, int position) {
-        if (this == glyph) {
-            throw new IllegalArgumentException(
-                    "Illegal to add a Glyph as a child of itself!");
-        }
-        GlyphI prev_parent = glyph.getParent();
-        if (prev_parent != null) {
-            prev_parent.removeChild(glyph);
-        }
-        
-        if (getChildren() == null) {
-            resetChildren();
-        }
-        if (position == getChildren().size()) {
-            getChildren().add(glyph);
-        } else {
-            getChildren().add(position, glyph);
-        }
-        // setParent() also calls setScene()
-        glyph.setParent(this);
-    }
-    
-    public void parentAddChild(GlyphI glyph) {
-        GlyphI prev_parent = glyph.getParent();
-        if (prev_parent != null) {
-            prev_parent.removeChild(glyph);
-        }
-        if (getChildren() == null) {
-            resetChildren();
-        }
-        getChildren().add(glyph);
-        glyph.setParent(this);
-    }
-    
-    @Override
-    public void resetChildren() {
-        try {
-            Field childrenField = Glyph.class.getDeclaredField("children");
-            childrenField.setAccessible(true);
-            childrenField.set(this, new ArrayList<GlyphI>());
-            childrenField.setAccessible(false);
-        } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
-        }
-    }
+//    @Override
+//    public void addChild(GlyphI glyph, int position) {
+//        try {
+//            Field gsnField = MapTierGlyph.class.getDeclaredField("gsn");
+//            gsnField.setAccessible(true);
+//            GlyphSearchNode gsn = (GlyphSearchNode) gsnField.get(this);
+//            
+//            parentAddChild(glyph, position);
+//            gsn.addGlyph(glyph);
+//            gsnField.setAccessible(false);
+//        } catch (Exception ex) {
+//            logger.error(ex.getMessage(), ex);
+//        }
+//    }
+//    
+//    @Override
+//    public void addChild(GlyphI glyph) {
+//        try {
+//            Field gsnField = MapTierGlyph.class.getDeclaredField("gsn");
+//            gsnField.setAccessible(true);
+//            GlyphSearchNode gsn = (GlyphSearchNode) gsnField.get(this);
+//            
+//            Field lrpField = MapTierGlyph.class.getDeclaredField("last_removed_position");
+//            lrpField.setAccessible(true);
+//            int last_removed_position = (Integer) lrpField.get(this);
+//            
+//            if (last_removed_position != -1) {
+//                parentAddChild(glyph, last_removed_position);
+//
+//                last_removed_position = -1;
+//                lrpField.setInt(this, last_removed_position);
+//                
+//            } else {
+//                parentAddChild(glyph);
+//            }
+//            
+//            lrpField.setAccessible(false);
+//            gsn.addGlyph(glyph);
+//            gsnField.setAccessible(false);
+//        } catch (Exception ex) {
+//            logger.error(ex.getMessage(), ex);
+//        }
+//    }
+//    
+//    public void parentAddChild(GlyphI glyph, int position) {
+//        if (this == glyph) {
+//            throw new IllegalArgumentException(
+//                    "Illegal to add a Glyph as a child of itself!");
+//        }
+//        GlyphI prev_parent = glyph.getParent();
+//        if (prev_parent != null) {
+//            prev_parent.removeChild(glyph);
+//        }
+//        
+//        if (getChildren() == null) {
+//            resetChildren();
+//        }
+//        if (position == getChildren().size()) {
+//            getChildren().add(glyph);
+//        } else {
+//            getChildren().add(position, glyph);
+//        }
+//        // setParent() also calls setScene()
+//        glyph.setParent(this);
+//    }
+//    
+//    public void parentAddChild(GlyphI glyph) {
+//        GlyphI prev_parent = glyph.getParent();
+//        if (prev_parent != null) {
+//            prev_parent.removeChild(glyph);
+//        }
+//        if (getChildren() == null) {
+//            resetChildren();
+//        }
+//        getChildren().add(glyph);
+//        glyph.setParent(this);
+//    }
+//    
+//    @Override
+//    public void resetChildren() {
+//        try {
+//            Field childrenField = Glyph.class.getDeclaredField("children");
+//            childrenField.setAccessible(true);
+//            childrenField.set(this, new ArrayList<GlyphI>());
+//            childrenField.setAccessible(false);
+//        } catch (Exception ex) {
+//            logger.error(ex.getMessage(), ex);
+//        }
+//    }
 
     /**
      * Remove all children of the glyph

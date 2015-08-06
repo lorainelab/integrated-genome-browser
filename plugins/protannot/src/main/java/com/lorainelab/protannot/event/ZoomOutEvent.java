9afd1e715c6b93a31f4b3e432450e554182301f3
dcnorris <dcnorris@localhost>
2015-08-06 12:04:32 -0400

    moved event classes to new package

--- plugins/protannot/src/main/java/com/lorainelab/protannot/event/ZoomOutEvent.java
+++ plugins/protannot/src/main/java/com/lorainelab/protannot/event/ZoomOutEvent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.bioviz.protannot.event;
+package com.lorainelab.protannot.event;
 
 import com.affymetrix.genometry.event.GenericAction;
 import java.awt.Adjustable;

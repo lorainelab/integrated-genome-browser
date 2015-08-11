dffcb57efaf6d6c4b01534187739dcc6a9e4ec03
dcnorris <dcnorris@localhost>
2015-08-11 10:57:27 -0400

    reverting native fileDialog in loadFileAction for linux due to known bug in JRE

--- core/igb/src/main/java/com/affymetrix/igb/action/LoadFileAction.java
+++ core/igb/src/main/java/com/affymetrix/igb/action/LoadFileAction.java
@@ -9,9 +9,11 @@
  */
 package com.affymetrix.igb.action;
 
+import static com.affymetrix.common.CommonUtils.IS_LINUX;
 import com.affymetrix.genometry.GenomeVersion;
 import com.affymetrix.genometry.GenometryModel;
 import com.affymetrix.genometry.event.GenericActionHolder;
+import com.affymetrix.genometry.parsers.FileTypeCategory;
 import com.affymetrix.genometry.util.ErrorHandler;
 import com.affymetrix.genometry.util.FileDropHandler;
 import com.affymetrix.genometry.util.FileTracker;
@@ -20,14 +22,18 @@ import com.affymetrix.genometry.util.UniFileFilter;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
 import com.affymetrix.igb.shared.OpenURIAction;
 import com.affymetrix.igb.view.load.GeneralLoadView;
+import com.google.common.collect.Sets;
 import java.awt.FileDialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
+import javax.swing.JFileChooser;
 import javax.swing.TransferHandler;
 import javax.swing.filechooser.FileFilter;
 import org.slf4j.Logger;
@@ -142,36 +148,60 @@ public final class LoadFileAction extends OpenURIAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         super.actionPerformed(e);
-
-        FileDialog fileChooser = new FileDialog(igbService.getApplicationFrame());
+        String speciesName = GeneralLoadView.getLoadView().getSelectedSpecies();
+        GenomeVersion loadGroup = GenometryModel.getInstance().getSelectedGenomeVersion();
         File currDir = load_dir_tracker.getFile();
         if (currDir == null) {
             currDir = new File(System.getProperty("user.home"));
         }
-        fileChooser.setDirectory(currDir.getAbsolutePath());
-        fileChooser.setMultipleMode(true);
-
-        String speciesName = GeneralLoadView.getLoadView().getSelectedSpecies();
-        GenomeVersion loadGroup = GenometryModel.getInstance().getSelectedGenomeVersion();
         File[] files = null;
-
-        if (!SELECT_SPECIES.equals(speciesName) && loadGroup != null) {
-            fileChooser.setVisible(true);
-            files = fileChooser.getFiles();
+        if (IS_LINUX) {
+            final JFileChooser chooser = new JFileChooser(currDir.getAbsolutePath());
+            chooser.setMultiSelectionEnabled(true);
+            Set<String> filters = Sets.newHashSet();
+            Arrays.asList(FileTypeCategory.values()).stream().forEach(fileTypeCategory -> {
+                getSupportedFiles(fileTypeCategory).stream().forEach(filter -> {
+                    chooser.addChoosableFileFilter(filter);
+                    filters.addAll(filter.getExtensions());
+                });
+            });
+            chooser.setAcceptAllFileFilterUsed(false);
+            UniFileFilter allFilter = new UniFileFilter(filters, "All Supported Files", true);
+            allFilter.setExtensionListInDescription(false);
+            chooser.addChoosableFileFilter(allFilter);
+            chooser.setFileFilter(allFilter);
+            if (!SELECT_SPECIES.equals(speciesName) && loadGroup != null) {
+                int option = chooser.showOpenDialog(igbService.getApplicationFrame());
+                if (option != JFileChooser.APPROVE_OPTION) {
+                    return;
+                }
+                load_dir_tracker.setFile(chooser.getCurrentDirectory());
+                files = chooser.getSelectedFiles();
+            } else {
+                ErrorHandler.errorPanel(BUNDLE.getString("noGenomeSelectedTitle"),
+                        BUNDLE.getString("noGenomeSelectedMessage"), Level.INFO);
+            }
         } else {
-            ErrorHandler.errorPanel(BUNDLE.getString("noGenomeSelectedTitle"),
-                    BUNDLE.getString("noGenomeSelectedMessage"), Level.INFO);
+            FileDialog fileChooser = new FileDialog(igbService.getApplicationFrame());
+            fileChooser.setDirectory(currDir.getAbsolutePath());
+            fileChooser.setMultipleMode(true);
+            if (!SELECT_SPECIES.equals(speciesName) && loadGroup != null) {
+                fileChooser.setVisible(true);
+                files = fileChooser.getFiles();
+            } else {
+                ErrorHandler.errorPanel(BUNDLE.getString("noGenomeSelectedTitle"),
+                        BUNDLE.getString("noGenomeSelectedMessage"), Level.INFO);
+            }
+            if (files == null || files.length == 0) {
+                return;
+            }
+            load_dir_tracker.setFile(new File(fileChooser.getDirectory()));
         }
-
-        if (files == null || files.length == 0) {
-            return;
-        }
-
-        load_dir_tracker.setFile(new File(fileChooser.getDirectory()));
-
-        for (File file : files) {
-            URI uri = file.toURI();
-            openURI(uri, file.getName(), true, loadGroup, speciesName, false);
+        if (files != null) {
+            for (File file : files) {
+                URI uri = file.toURI();
+                openURI(uri, file.getName(), true, loadGroup, speciesName, false);
+            }
         }
     }
 

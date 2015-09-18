dae0d72a35709f90c2fe10c9ed3af0924b3c19bb
John Eckstein <jeckste@gmail.com>
2015-09-18 15:09:57 -0400

    setup menu

--- plugins/feedback/src/main/java/com/lorainelab/feedback/FeedbackMenuProvider.java
+++ plugins/feedback/src/main/java/com/lorainelab/feedback/FeedbackMenuProvider.java
@@ -5,28 +5,59 @@
  */
 package com.lorainelab.feedback;
 
+import aQute.bnd.annotation.component.Component;
+import com.affymetrix.genometry.event.GenericAction;
 import com.affymetrix.igb.swing.JRPMenuItem;
 import com.lorainelab.igb.services.window.menus.IgbMenuItemProvider;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.ResourceBundle;
 
 /**
  *
  * @author dcnorris
  */
-public class FeedbackMenuProvider implements IgbMenuItemProvider {
+@Component(immediate = true)
+public class FeedbackMenuProvider extends GenericAction implements IgbMenuItemProvider {
+
+    public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("bundle");
+    private final int menuWeight;
+    private final String parentMenuName;
+    private final JRPMenuItem menuItem;
+
+    public FeedbackMenuProvider() {
+        super(getProperty("menu.name"),
+                getProperty("menu.tooltip"),
+                "16x16/actions/help.png",
+                "22x22/actions/help.png",
+                KeyEvent.VK_UNDEFINED);
+        menuWeight = Integer.parseInt(getProperty("menu.weight"));
+        parentMenuName = getProperty("menu.parent.name");
+        menuItem = new JRPMenuItem(getProperty("menu.name"), this, menuWeight);
+    }
 
     @Override
     public String getParentMenuName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return parentMenuName;
     }
 
     @Override
     public JRPMenuItem getMenuItem() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return menuItem;
     }
 
     @Override
     public int getMenuItemWeight() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return menuWeight;
+    }
+
+    private static String getProperty(String property) {
+        return BUNDLE.getString(property);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 
 

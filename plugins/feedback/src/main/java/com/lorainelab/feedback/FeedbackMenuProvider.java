b9424df5cc511cd55b45b0c947a92f5a603889d9
dcnorris <dcnorris@reg-10-23-250-125.uncc.edu>
2015-09-18 14:08:40 -0400

    adding scaffold for new feedback module

---
+++ plugins/feedback/src/main/java/com/lorainelab/feedback/FeedbackMenuProvider.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lorainelab.feedback;
+
+import com.affymetrix.igb.swing.JRPMenuItem;
+import com.lorainelab.igb.services.window.menus.IgbMenuItemProvider;
+
+/**
+ *
+ * @author dcnorris
+ */
+public class FeedbackMenuProvider implements IgbMenuItemProvider {
+
+    @Override
+    public String getParentMenuName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public JRPMenuItem getMenuItem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getMenuItemWeight() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+}

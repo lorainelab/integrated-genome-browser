32856fa6b56c9f708d619d4fe2ff309e2cb0723d
Tarun K. Mall <c2tarun@hotmail.com>
2015-10-12 11:07:35 -0400

    Select and scroll mode buttons working in protannot

---
+++ plugins/protannot/src/main/java/com/lorainelab/protannot/ProtannotViewSelectAction.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lorainelab.protannot;
+
+import com.affymetrix.genometry.event.GenericAction;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Tarun
+ */
+public class ProtannotViewSelectAction extends GenericAction {
+    
+    GenomeView seqMapView;
+    
+    public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("protannot");
+    
+    public ProtannotViewSelectAction(GenomeView seqMapView) {
+        super(BUNDLE.getString("selectMode"),
+                BUNDLE.getString("selectModeToolTip"),
+                "16x16/actions/arrow.png", null, KeyEvent.VK_UNDEFINED);
+        this.seqMapView = seqMapView;
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        super.actionPerformed(e);
+        seqMapView.setMapMode(GenomeView.MapMode.MapSelectMode);
+    }
+    
+}

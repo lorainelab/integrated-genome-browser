eb7e21ef95f8a99f533629ff73fbed1608d1c946
Tarun K. Mall <c2tarun@hotmail.com>
2015-11-11 16:59:22 -0500

    Pending task of organizing properties

--- core/genometry/src/main/java/com/affymetrix/genometry/tooltip/ToolTipConstants.java
+++ core/genometry/src/main/java/com/affymetrix/genometry/tooltip/ToolTipConstants.java
@@ -99,13 +99,15 @@ public class ToolTipConstants {
     public static final String MISC_CATEGORY = "Misc";
 
     // BAM Insertion
-    public static final List<String> BAM_INS_INFO_GRP = Arrays.asList(FEATURE_TYPE, ID, CHROMOSOME, START, END, LENGTH);
-    public static final List<String> BAM_INS_LOC_GRP = Arrays.asList(STRAND, RESIDUES, FORWARD);
+    public static final List<String> BAM_INS_INFO_GRP = Arrays.asList(FEATURE_TYPE, LENGTH, RESIDUES, STRAND, START, END, ID);
+    public static final List<String> BAM_INS_IGNORE_GRP = Arrays.asList();
+//    public static final List<String> BAM_INS_LOC_GRP = Arrays.asList(STRAND, RESIDUES, FORWARD);
 
     public static final List<String> BAM_INS_PROP_LIST = new ArrayList<>();
+    public static final List<String> BAM_INS_IGNORE_LIST = new ArrayList<>();
 
     public static final Map<String, List<String>> BAM_INS_INFO_CATEGORY = ImmutableMap.of(BASIC__INFO_CATEGORY, BAM_INS_INFO_GRP);
-    public static final Map<String, List<String>> BAM_INS_LOCATION_CATEGORY = ImmutableMap.of("Bam Info", BAM_INS_LOC_GRP);
+//    public static final Map<String, List<String>> BAM_INS_LOCATION_CATEGORY = ImmutableMap.of("Bam Info", BAM_INS_LOC_GRP);
 
     // BED-14
     public static final List<String> BED14_INFO_GRP = Arrays.asList(TITLE, ID, DESCRIPTION);
@@ -166,7 +168,7 @@ public class ToolTipConstants {
         BAM_PROP_LIST.addAll(BAM_DETAILS_GRP);
 
         BAM_INS_PROP_LIST.addAll(BAM_INS_INFO_GRP);
-        BAM_INS_PROP_LIST.addAll(BAM_INS_LOC_GRP);
+//        BAM_INS_PROP_LIST.addAll(BAM_INS_LOC_GRP);
 
         BED14_PROP_LIST.addAll(BED14_INFO_GRP);
         BED14_PROP_LIST.addAll(BED14_LOC_GRP);

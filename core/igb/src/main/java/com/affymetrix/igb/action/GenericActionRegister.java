879e12843940e20774fa144e574f9d4a695593e6
John Eckstein <jeckste@gmail.com>
2015-11-18 13:38:51 -0500

    fix dependency on igb issue

--- core/igb/src/main/java/com/affymetrix/igb/action/GenericActionRegister.java
+++ core/igb/src/main/java/com/affymetrix/igb/action/GenericActionRegister.java
@@ -6,8 +6,14 @@ import com.affymetrix.common.PreferenceUtils;
 import com.affymetrix.genometry.event.GenericAction;
 import com.affymetrix.genometry.event.GenericActionHolder;
 import com.affymetrix.igb.IGB;
+import com.affymetrix.igb.IgbServiceDependencyManager;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.swing.Action;
 import javax.swing.KeyStroke;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -18,12 +24,42 @@ public class GenericActionRegister {
 
     public static final String COMPONENT_NAME = "GenericActionRegister";
 
+    private static final Logger logger = LoggerFactory.getLogger(GenericActionRegister.class);
+
+    Set<GenericAction> pendingActions;
+
+    private IgbServiceDependencyManager igbServiceDependencyManager;
+
     public GenericActionRegister() {
+        this.pendingActions = Collections.newSetFromMap(new ConcurrentHashMap<GenericAction, Boolean>());
         GenericActionHolder.getInstance();
+    }
+
+    @Reference
+    public void setIgbServiceDependencyManager(IgbServiceDependencyManager igbServiceDependencyManager) {
+        this.igbServiceDependencyManager = igbServiceDependencyManager;
+        pendingActions.stream().forEach((action) -> {
+            try {
+                processGenericAction(action);
+            } catch (Exception ex) {
+                logger.error(ex.getMessage(), ex);
+            }
+        });
+        pendingActions.clear();
     }
 
     @Reference(optional = true, multiple = true, dynamic = true, unbind = "removeGenericAction")
     public void addGenericAction(GenericAction genericAction) {
+        synchronized (this) {
+            if (igbServiceDependencyManager == null) {
+                pendingActions.add(genericAction);
+            } else {
+                processGenericAction(genericAction);
+            }
+        }
+    }
+
+    private void processGenericAction(GenericAction genericAction) {
         if (genericAction.getId() != null) {
             KeyStroke ks = genericAction.getKeyStroke();
             if (ks != null) {

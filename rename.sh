f4f603d97d378028d029a06c976d84b679a80c47
John Eckstein <jeckste@gmail.com>
2016-01-04 14:24:25 -0500

    rename pacakges

--- rename.sh
+++ rename.sh
@@ -4,4 +4,6 @@ while read -r line; do
   echo "$line"
   newpath=`echo "$line" | sed 's/java\/com\/lorainelab/java\/org\/lorainelab\/igb/g'`
   echo $newpath
-done < <(find -follow | grep "src/main/java/com/lorainelab")
+  mkdir -p "$newpath"
+  mv "$line" "$newpath"
+done < <(find -follow | grep "src/main/java/com/lorainelab$")

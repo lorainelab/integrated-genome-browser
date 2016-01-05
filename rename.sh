bfd655cfb600339bebdfeb447700a1df72078ed6
John Eckstein <jeckste@gmail.com>
2016-01-05 10:18:26 -0500

    fix prot

--- rename.sh
+++ rename.sh
@@ -2,8 +2,11 @@
 
 while read -r line; do
   echo "$line"
-  #newpath=`echo "$line" | sed 's/java\/com\/lorainelab/java\/org\/lorainelab\/igb/g'`
-  #echo $newpath
+  newpath=`echo "$line" | sed 's/java\/org\/lorainelab\/igb\/lorainelab/java\/org\/lorainelab\/igb/g'`
+  echo $newpath
+  mv "$line" /tmp/staging
+  cp -R /tmp/staging/* $newpath
+  rm -rf /tmp/staging
   #mkdir -p "$newpath"
  # mv "$line" "$newpath"
 done < <(find -follow | grep "src/main/java/org/lorainelab/igb/lorainelab$")

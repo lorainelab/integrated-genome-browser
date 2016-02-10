00f9afd32298fbe0c8a902fc940ab9b205258937
dcnorris <dcnorris@reg-10-23-250-63.uncc.edu>
2016-02-10 12:02:22 -0500

    small changes to menu api (removed a few netbeans generated comments)

--- core/menu-api/src/main/java/org/lorainelab/igb/menu/api/model/MenuIcon.java
+++ core/menu-api/src/main/java/org/lorainelab/igb/menu/api/model/MenuIcon.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.lorainelab.igb.menu.api.model;
 
 import com.google.common.io.BaseEncoding;

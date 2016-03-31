3f96867c6af4109462b99709785c223c229d40a8
dcnorris <dcnorris@reg-10-23-250-63.uncc.edu>
2016-03-31 13:57:45 -0400

    adding app manager menu item to tools menu

---
+++ plugins/plugin-manager/src/main/java/com/lorainelab/igb/plugins/AppManagerToolsMenuEntry.java
@@ -0,0 +1,55 @@
+package com.lorainelab.igb.plugins;
+
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+import com.affymetrix.genometry.event.GenericAction;
+import com.affymetrix.igb.swing.JRPMenuItem;
+import com.lorainelab.igb.services.window.menus.IgbMenuItemProvider;
+import java.awt.event.ActionEvent;
+import javafx.application.Platform;
+
+/**
+ * A panel for viewing and editing weblinks.
+ */
+@Component(immediate = true, provide = {IgbMenuItemProvider.class, GenericAction.class})
+public final class AppManagerToolsMenuEntry extends GenericAction implements IgbMenuItemProvider {
+
+    private final JRPMenuItem appManagerToolsMenuEntry;
+    private static final int MENU_ITEM_WEIGHT = 7;
+    private AppManagerFrame frame;
+
+    public AppManagerToolsMenuEntry() {
+        super("App Manager",
+                "16x16/categories/fa-circle.png",
+                "22x22/categories/applications-internet.png");
+        appManagerToolsMenuEntry = new JRPMenuItem("App Manager", this, MENU_ITEM_WEIGHT);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        super.actionPerformed(evt);
+        Platform.runLater(() -> {
+            frame.setVisible(true);
+        });
+    }
+
+    @Override
+    public String getParentMenuName() {
+        return "tools";
+    }
+
+    @Override
+    public JRPMenuItem getMenuItem() {
+        return appManagerToolsMenuEntry;
+    }
+
+    @Override
+    public int getMenuItemWeight() {
+        return MENU_ITEM_WEIGHT;
+    }
+
+    @Reference
+    public void setFxPanel(AppManagerFrame frame) {
+        this.frame = frame;
+    }
+}

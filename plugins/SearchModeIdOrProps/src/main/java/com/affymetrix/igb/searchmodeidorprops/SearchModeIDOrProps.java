9454be80e681da8e4b29c6b88f8edfe73d92e382
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2017-02-22 15:16:33 -0500

    fixed issues with search

--- plugins/SearchModeIdOrProps/src/main/java/com/affymetrix/igb/searchmodeidorprops/SearchModeIDOrProps.java
+++ plugins/SearchModeIdOrProps/src/main/java/com/affymetrix/igb/searchmodeidorprops/SearchModeIDOrProps.java
@@ -5,6 +5,8 @@ import com.affymetrix.genometry.GenomeVersion;
 import com.affymetrix.genometry.GenometryModel;
 import com.affymetrix.genometry.SeqSpan;
 import com.affymetrix.genometry.search.SearchUtils;
+import com.affymetrix.genometry.symmetry.SupportsGeneName;
+import com.affymetrix.genometry.symmetry.SymWithProps;
 import com.affymetrix.genometry.symmetry.impl.SeqSymmetry;
 import com.affymetrix.genometry.symmetry.impl.TypeContainerAnnot;
 import com.affymetrix.genometry.util.Constants;
@@ -20,6 +22,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
+import org.apache.commons.lang3.StringUtils;
 
 public abstract class SearchModeIDOrProps implements ISearchModeSym {
 
@@ -155,14 +158,6 @@ public abstract class SearchModeIDOrProps implements ISearchModeSym {
         }
 
         List<SeqSymmetry> tableRows = filterBySeq(localSymList, chrFilter);
-        Collections.sort(tableRows, new Comparator<SeqSymmetry>() {
-            public int compare(SeqSymmetry s1, SeqSymmetry s2) {
-                if (s1.getID() == null || s2.getID() == null) {
-                    return 0;
-                }
-                return s1.getID().compareTo(s2.getID());
-            }
-        });
 
         return new SearchResults<>(getName(), search_text, chrFilter != null ? chrFilter.getId() : "genome", statusStr, tableRows);
     }
@@ -180,6 +175,32 @@ public abstract class SearchModeIDOrProps implements ISearchModeSym {
         String friendlySearchStr = MessageFormat.format(FRIENDLY_PATTERN, text, seq);
         statusHolder.setStatus(MessageFormat.format(BUNDLE.getString("searchSearchingLocal"), friendlySearchStr));
         List<SeqSymmetry> localSymList = SearchUtils.findLocalSyms(genomeVersion, chrFilter, regex, search_props);
+        //added sorting based on ID, Name and proprties if properties are searched
+        if (!search_props) {
+            Collections.sort(localSymList, (SeqSymmetry o1, SeqSymmetry o2) -> {
+                String search = search_text.toLowerCase();
+                Integer i1 = StringUtils.getLevenshteinDistance(o1.getID().toLowerCase(), search.toLowerCase());
+                Integer i2 = StringUtils.getLevenshteinDistance(o2.getID().toLowerCase(), search.toLowerCase());
+                if (o1 instanceof SupportsGeneName && o2 instanceof SupportsGeneName) {
+                    int j1 = StringUtils.getLevenshteinDistance(((SupportsGeneName) o1).getGeneName().toLowerCase(), search.toLowerCase());
+                    int j2 = StringUtils.getLevenshteinDistance(((SupportsGeneName) o2).getGeneName().toLowerCase(), search.toLowerCase());
+                    i1 = i1 < j1 ? i1 : j1;
+                    i2 = i2 < j2 ? i2 : j2;
+                }
+                return (i2 < i1) ? 1 : -1;
+            });
+        } else {
+            Collections.sort(localSymList, (SeqSymmetry o1, SeqSymmetry o2) -> {
+                //SymWithProps
+                if (o1 instanceof SymWithProps && o2 instanceof SymWithProps) {
+                    int[] dist = {0, 0};
+                    ((SymWithProps) o1).getProperties().values().stream().mapToInt(val -> StringUtils.getLevenshteinDistance(val.toString(), text)).min().ifPresent(min -> dist[0] = min);
+                    ((SymWithProps) o2).getProperties().values().stream().mapToInt(val -> StringUtils.getLevenshteinDistance(val.toString(), text)).min().ifPresent(min -> dist[1] = min);
+                    return dist[0] - dist[1];
+                }
+                return 0;
+            });
+        }
         return localSymList;
     }
 

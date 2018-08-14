be7773876d9f1705f15b4bd273f1f8aeb4c69bfd
KOREY-XPS15\kiran <kkorey@uncc.edu>
2018-08-14 12:05:30 -0400

    IGBF-1362:Added default trust store to validate certificates

--- core/igb/src/main/java/com/affymetrix/igb/util/IGBTrustManager.java
+++ core/igb/src/main/java/com/affymetrix/igb/util/IGBTrustManager.java
@@ -3,12 +3,18 @@ package com.affymetrix.igb.util;
 import com.affymetrix.genometry.util.ModalUtils;
 import com.affymetrix.common.PreferenceUtils;
 import com.affymetrix.igb.IGB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.swing.JComponent;
 
 /**
@@ -19,9 +25,21 @@ import javax.swing.JComponent;
  */
 public class IGBTrustManager implements X509TrustManager {
 
+    static X509TrustManager defaultTm = null;
+    private static final Logger logger = LoggerFactory.getLogger(IGBTrustManager.class);
     public static void installTrustManager() {
         // Install the all-trusting trust manager
         try {
+            //kiran:IGBF-1362: Added default trust store also as the trust manager
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            // Using null here initialises the TMF with the default trust store.
+            tmf.init((KeyStore) null);
+            for (TrustManager tm : tmf.getTrustManagers()) {
+                if (tm instanceof X509TrustManager) {
+                    defaultTm = (X509TrustManager) tm;
+                    break;
+                }
+            }
             SSLContext sc = SSLContext.getInstance("SSL");
             sc.init(null, new TrustManager[]{new IGBTrustManager()}, new java.security.SecureRandom());
             HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
@@ -39,17 +57,25 @@ public class IGBTrustManager implements X509TrustManager {
     }
 
     public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+
         StringBuilder certificates = new StringBuilder("\n\n");
+        IGB app = IGB.getInstance();
         for (X509Certificate cert : certs) {
             certificates.append(cert.getIssuerX500Principal().getName()).append(",").append("\n");
         }
-        IGB app = IGB.getInstance();
         JComponent comp = (app == null) ? null : app.getFrame().getRootPane();
-        boolean response = ModalUtils.confirmPanel(comp, "Trust following certificate? " + certificates.toString(),
-                PreferenceUtils.getCertificatePrefsNode(), certificates.toString(), true, "Do not show this again for the publisher above");
+        try {
+            //kiran:IGBF-1362: First try to validate the certificate using the default trust store
+            defaultTm.checkServerTrusted(certs,authType);
+            logger.info("Authenticated {} certificates using default trust store",certificates.toString().replace("\n", "").replace("\r", ""));
+        } catch (CertificateException e) {
+            //if certificate not found then ask the user to validate the certificate
+            boolean response = ModalUtils.confirmPanel(comp, "Trust following certificate? " + certificates.toString(),
+                    PreferenceUtils.getCertificatePrefsNode(), certificates.toString(), true, "Do not show this again for the publisher above");
 
-        if (!response) {
-            throw new RuntimeException("Untrusted certificate.");
+            if (!response) {
+                throw new RuntimeException("Untrusted certificate.");
+            }
         }
     }
 }

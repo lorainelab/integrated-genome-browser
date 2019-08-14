c5d50b406ca6c5ee152e6e93b0d87c620f791c79
Noor Zahara <nzahara@uncc.edu>
2019-08-14 12:31:05 -0400

    IGBF-1902 Add netbeans version in the comment - EditBundleRepositoryFrame class.

--- plugins/plugin-manager/src/main/java/org/lorainelab/igb/plugin/manager/repos/view/EditBundleRepositoryFrame.java
+++ plugins/plugin-manager/src/main/java/org/lorainelab/igb/plugin/manager/repos/view/EditBundleRepositoryFrame.java
@@ -1,8 +1,11 @@
 /*
+ *  Netbeans IDE 8.2
+ *
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package org.lorainelab.igb.plugin.manager.repos.view;
 
 import com.affymetrix.igb.swing.JRPTextField;

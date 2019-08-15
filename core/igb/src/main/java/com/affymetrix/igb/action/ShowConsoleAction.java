e90096af82129f08c28210013306cde3c638477a
Prutha Kulkarni <pruthakulkarni@Pruthas-MacBook-Air.local>
2019-08-15 10:39:31 -0400

    IGBF-1864 : Add Information about IGB Show Console Action

--- core/igb/src/main/java/com/affymetrix/igb/action/ShowConsoleAction.java
+++ core/igb/src/main/java/com/affymetrix/igb/action/ShowConsoleAction.java
@@ -6,13 +6,14 @@ import com.affymetrix.genometry.event.GenericActionHolder;
 import com.affymetrix.igb.IGB;
 import static com.affymetrix.igb.IGBConstants.BUNDLE;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import org.lorainelab.igb.services.window.tabs.IgbTabPanel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Open a window showing information about Integrated Genome Browser.
+ * Open a window showing logging information about Integrated Genome Browser.
+ * The action is triggered on selection of Help> Show Console on the IGB platform.
+ * Show console action selection will open IGB Console Tab (../../plugins/ConsoleTab) in the window mode.
  *
  * @author pruthakulkarni
  */
@@ -34,6 +35,7 @@ public class ShowConsoleAction extends GenericAction {
         super(BUNDLE.getString("showConsole"),
                 "16x16/actions/console.png",
                 "22x22/actions/console.png");
+        
         this.ordinal = 120;
     }
 

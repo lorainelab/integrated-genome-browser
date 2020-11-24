4e38b02012390a43c9ee902fb193899ee8905b28
aloraine <aloraine@uncc.edu>
2020-11-24 09:39:26 -0500

    IGBF-2647 Revert "IGBF-1443 : Adding comments for new methods"
    This reverts commit aeb1873cb8c2239c219ab739dbd4e9dcdbbebc6a.

--- core/genometry/src/main/java/com/affymetrix/genometry/util/SeqUtils.java
+++ core/genometry/src/main/java/com/affymetrix/genometry/util/SeqUtils.java
@@ -195,11 +195,6 @@ public class SeqUtils {
      * @param seq
      * @param leafs
      */
-    /**
-      * This method populate the leafs collection with only soft-clip portions of aligned reads.
-      * Please go through Jira ticket IGBF-1443 for complete sequence of added methods 
-      *  for implementing Depth Graph(Soft-clip) feature.
-    */
     public static void collectSoftclipSpans(SeqSymmetry sym, BioSeq seq, Collection<SeqSpan> leafs) {
   
         int childCount = sym.getChildCount();
@@ -211,13 +206,12 @@ public class SeqUtils {
         } 
         else {
             for (int i = 0; i < childCount; i++) {
-                //Checking if SeqSymmetry is of type BamSym(BamSym only have soft clips)
                 if (isBamSym(sym)){
-                    //Typecasting SeqSymmetry to BAMSym
                     BAMSym bamSym = (BAMSym)sym;
                     for(int ii = 0; ii < bamSym.getSoftChildCount(); ii++){
                         collectSoftclipSpans(bamSym.getSoftChild(ii), seq, leafs);
                     }
+  
                 }
                 else{
                     collectSoftclipSpans(sym.getChild(i), seq, leafs);

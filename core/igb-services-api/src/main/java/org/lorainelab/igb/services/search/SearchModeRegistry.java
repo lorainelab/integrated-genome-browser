23901ecda0914ff9a3f660ef9b990fd737e33946
DESKTOP-9TTSBJ2\Karthik Raveendran <kraveend@uncc.edu>
2023-08-16 06:33:32 -0400

    IGBF-3379 Update annotations for Core - igb-services-api

--- core/igb-services-api/src/main/java/org/lorainelab/igb/services/search/SearchModeRegistry.java
+++ core/igb-services-api/src/main/java/org/lorainelab/igb/services/search/SearchModeRegistry.java
@@ -5,11 +5,14 @@
  */
 package org.lorainelab.igb.services.search;
 
-import aQute.bnd.annotation.component.Component;
-import aQute.bnd.annotation.component.Reference;
 import com.google.common.collect.Sets;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +20,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author dcnorris
  */
-@Component(name = SearchModeRegistry.COMPONENT_NAME, immediate = true, provide = SearchModeRegistry.class)
+@Component(name = SearchModeRegistry.COMPONENT_NAME, immediate = true, service = SearchModeRegistry.class)
 public class SearchModeRegistry {
 
     private static final Logger logger = LoggerFactory.getLogger(SearchModeRegistry.class);
@@ -31,7 +34,7 @@ public class SearchModeRegistry {
         searchModes.remove(searchMode);
     }
 
-    @Reference(multiple = true, optional = false, dynamic = true, unbind = "removeSearchModeService")
+    @Reference( cardinality = ReferenceCardinality.AT_LEAST_ONE, policy = ReferencePolicy.DYNAMIC, unbind = "removeSearchModeService")
     public void addSearchModeService(ISearchMode searchMode) {
         searchModes.add(searchMode);
     }

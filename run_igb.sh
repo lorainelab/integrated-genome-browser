94b3a53ed6f226866b470d183e3d884c7f337187
dcnorris <dcnorris@reg-10-23-250-63.uncc.edu>
2023-12-06 09:46:41 -0500

    updating bash script to test some additional jdk internal exposures for fixing macos jidesoft laf issue

--- run_igb.sh
+++ run_igb.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
 JAVA_OPTS="\
+--add-opens=java.desktop/java.awt.event=ALL-UNNAMED \
+--add-opens=java.desktop/sun.font=ALL-UNNAMED \
+--add-opens=java.desktop/java.awt=ALL-UNNAMED \
+--add-opens=java.desktop/sun.awt=ALL-UNNAMED \
+--add-opens=java.base/java.lang=ALL-UNNAMED \
+--add-opens=java.base/java.util=ALL-UNNAMED \
+--add-opens=java.desktop/javax.swing=ALL-UNNAMED \
+--add-opens=java.desktop/sun.swing=ALL-UNNAMED \
+--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED \
+--add-opens=java.desktop/java.awt.peer=ALL-UNNAMED \
+--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED \
+--add-exports=java.desktop/sun.font=ALL-UNNAMED \
+--add-exports=java.desktop/com.apple.eawt=ALL-UNNAMED \
+--add-exports=java.desktop/com.apple.laf=ALL-UNNAMED \
+--add-exports=java.desktop/com.apple.eawt.event=ALL-UNNAMED \
+--add-exports java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED \
+--add-exports java.desktop/com.apple.laf=ALL-UNNAMED \
 --add-opens=java.base/jdk.internal.loader=ALL-UNNAMED \
---add-opens java.base/java.net=ALL-UNNAMED" 
+--add-opens java.base/java.net=ALL-UNNAMED"
 
-# Update the java command
-#java $JAVA_OPTS --module-path $MODULE_PATH_ARG --add-modules javafx.controls,javafx.graphics,javafx.media,javafx.swing,javafx.fxml,javafx.web -jar igb_exe.jar
+# Check if the script was passed the -d or --debug argument
+if [[ $* == *-d* ]] || [[ $* == *--debug* ]]; then
+  # Enable debugging with the specified port and suspend
+  JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=127.0.0.1:5005"
+fi
 
 java $JAVA_OPTS -jar igb_exe.jar
-#java $JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=127.0.0.1:5005 -jar igb_exe.jar

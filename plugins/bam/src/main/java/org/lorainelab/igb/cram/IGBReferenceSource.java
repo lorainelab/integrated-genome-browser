05068e22f54a0e0532635edc67c5819ad6b13b78
kgopu <kgopu@uncc.edu>
2023-12-14 21:19:35 +0530

    IGBF-3478: Handle outside of the bound case

--- plugins/bam/src/main/java/org/lorainelab/igb/cram/IGBReferenceSource.java
+++ plugins/bam/src/main/java/org/lorainelab/igb/cram/IGBReferenceSource.java
@@ -2,6 +2,7 @@ package org.lorainelab.igb.cram;
 
 import com.affymetrix.genometry.BioSeq;
 import com.affymetrix.genometry.GenometryModel;
+import com.affymetrix.igb.view.load.GeneralLoadUtils;
 import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.cram.ref.CRAMReferenceSource;
 
@@ -16,9 +17,20 @@ public class IGBReferenceSource implements CRAMReferenceSource {
     @Override
     public byte[] getReferenceBasesByRegion(SAMSequenceRecord sequenceRecord, int zeroBasedStart, int requestedRegionLength) {
         final BioSeq bioSeq = GenometryModel.getInstance().getSelectedSeq().orElse(null);
+        int start = zeroBasedStart;
+        int end = zeroBasedStart+requestedRegionLength;
         if(bioSeq!=null){
-            byte[] bytes = bioSeq.getResidues(zeroBasedStart,zeroBasedStart+requestedRegionLength).getBytes(StandardCharsets.US_ASCII);
-            return bytes;
+            byte[] bytes = null;
+            if(bioSeq.isAvailable(start, end)){
+                bytes = bioSeq.getResidues(start, end).getBytes(StandardCharsets.US_ASCII);
+                return bytes;
+            }else{
+                if(GeneralLoadUtils.loadResidues(bioSeq, start, end)){
+                    String residues = bioSeq.getResidues(start, end);
+                    bytes = bioSeq.getResidues(start, end).getBytes(StandardCharsets.US_ASCII);
+                    return bytes;
+                }
+            }
         }
         return new byte[]{};
     }

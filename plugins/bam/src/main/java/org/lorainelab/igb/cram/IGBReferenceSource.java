b6e9e9d5513dc1b4fe0fcd06074dc9ee44f17248
kgopu <kgopu@uncc.edu>
2023-12-21 09:40:51 +0530

    IGBF-3478: Fix extension error

--- plugins/bam/src/main/java/org/lorainelab/igb/cram/IGBReferenceSource.java
+++ plugins/bam/src/main/java/org/lorainelab/igb/cram/IGBReferenceSource.java
@@ -9,11 +9,26 @@ import htsjdk.samtools.cram.ref.CRAMReferenceSource;
 import java.nio.charset.StandardCharsets;
 
 public class IGBReferenceSource implements CRAMReferenceSource {
+    /**
+     *
+     * @param sequenceRecord the SAMSequenceRecord identifying the reference being requested
+     * @param tryNameVariants if true, attempt to match the requested sequence name against the reference by
+     *                       using common name variations, such as adding or removing a leading "chr" prefix
+     *                       from the requested name. if false, use exact match
+     * @return
+     */
     @Override
     public byte[] getReferenceBases(SAMSequenceRecord sequenceRecord, boolean tryNameVariants) {
         return getReferenceBasesByRegion(sequenceRecord,0, sequenceRecord.getSequenceLength());
     }
 
+    /**
+     *
+     * @param sequenceRecord the SAMSequenceRecord for the reference contig being requested
+     * @param zeroBasedStart the zero based offset of the starting reference base, must be >= 0
+     * @param requestedRegionLength the length of the requested reference region
+     * @return
+     */
     @Override
     public byte[] getReferenceBasesByRegion(SAMSequenceRecord sequenceRecord, int zeroBasedStart, int requestedRegionLength) {
         final BioSeq bioSeq = GenometryModel.getInstance().getSelectedSeq().orElse(null);
@@ -26,7 +41,6 @@ public class IGBReferenceSource implements CRAMReferenceSource {
                 return bytes;
             }else{
                 if(GeneralLoadUtils.loadResidues(bioSeq, start, end)){
-                    String residues = bioSeq.getResidues(start, end);
                     bytes = bioSeq.getResidues(start, end).getBytes(StandardCharsets.US_ASCII);
                     return bytes;
                 }

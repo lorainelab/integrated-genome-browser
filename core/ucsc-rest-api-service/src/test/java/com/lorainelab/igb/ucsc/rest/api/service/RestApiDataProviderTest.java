f81f7e4b2a75b87b943a9a071cfe3389788462e9
sravani <jsirigin@uncc.edu>
2024-02-27 15:05:11 -0500

    IGBF-3625: Rebased main-JDK21-with-javafx-no-theme into IGBF-3501 branch and resolved conflicts and dependency issues.

--- core/ucsc-rest-api-service/src/test/java/com/lorainelab/igb/ucsc/rest/api/service/RestApiDataProviderTest.java
+++ core/ucsc-rest-api-service/src/test/java/com/lorainelab/igb/ucsc/rest/api/service/RestApiDataProviderTest.java
@@ -1,9 +1,8 @@
 package com.lorainelab.igb.ucsc.rest.api.service;
 
-import com.affymetrix.genometry.GenomeVersion;
-import junit.framework.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.lorainelab.igb.ucsc.rest.api.service.RestApiDataProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,14 +14,14 @@ public class RestApiDataProviderTest {
     private static final String HUMAN_GENOME_ID = "hg38";
     private static RestApiDataProvider restApiDataProvider;
 
-    @BeforeClass
+    @BeforeAll
     public static void setup() {
         restApiDataProvider = new RestApiDataProvider(UCSC_REST_URL, "UCSC", 1);
     }
 
     @Test
     public void retrieveSupportedGenomeVersions() {
-        Assert.assertTrue(restApiDataProvider.getSupportedGenomeVersionNames().contains(HUMAN_GENOME_ID));
+        Assertions.assertTrue(restApiDataProvider.getSupportedGenomeVersionNames().contains(HUMAN_GENOME_ID));
         restApiDataProvider.getSupportedGenomeVersionNames().forEach(logger::info);
     }
 }

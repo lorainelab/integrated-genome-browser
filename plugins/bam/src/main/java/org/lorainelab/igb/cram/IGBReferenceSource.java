5f9add4acf6dc78120f53fa0bd68c3e4a68016b2
kgopu <kgopu@uncc.edu>
2024-05-09 13:08:43 -0400

    IGBF-3642: Incorporate case sensitivity (to upper case) in sequence data

--- plugins/bam/src/main/java/org/lorainelab/igb/cram/IGBReferenceSource.java
+++ plugins/bam/src/main/java/org/lorainelab/igb/cram/IGBReferenceSource.java
@@ -5,10 +5,13 @@ import com.affymetrix.genometry.GenometryModel;
 import com.affymetrix.igb.view.load.GeneralLoadUtils;
 import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.cram.ref.CRAMReferenceSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
 
 public class IGBReferenceSource implements CRAMReferenceSource {
+    private static Logger LOG = LoggerFactory.getLogger(IGBReferenceSource.class.getName());
     /**
      *
      * @param sequenceRecord the SAMSequenceRecord identifying the reference being requested
@@ -35,16 +38,20 @@ public class IGBReferenceSource implements CRAMReferenceSource {
         int start = zeroBasedStart;
         int end = zeroBasedStart+requestedRegionLength;
         if(bioSeq!=null){
-            byte[] bytes = null;
-            if(bioSeq.isAvailable(start, end)){
-                bytes = bioSeq.getResidues(start, end).getBytes(StandardCharsets.US_ASCII);
-                return bytes;
-            }else{
-                if(GeneralLoadUtils.loadResidues(bioSeq, start, end)){
-                    bytes = bioSeq.getResidues(start, end).getBytes(StandardCharsets.US_ASCII);
-                    return bytes;
+            byte[] bases = null;
+            try {
+                if(bioSeq.isAvailable(start, end)){
+                    bases = bioSeq.getResidues(start, end).toUpperCase().getBytes(StandardCharsets.US_ASCII);
+                }else{
+                    if(GeneralLoadUtils.loadResidues(bioSeq, start, end)){
+                        bases = bioSeq.getResidues(start, end).toUpperCase().getBytes(StandardCharsets.US_ASCII);
+                    }
                 }
+            } catch (Exception e) {
+                LOG.error("An error occurred while retrieving sequence data from the IGB, returning empty result.");
+                return new byte[]{};
             }
+            return bases;
         }
         return new byte[]{};
     }
